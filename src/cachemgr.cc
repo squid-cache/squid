198b2ca4c603b4ae4afb4ab8e463389072adc200
wessels <Unknown>
1996-02-29 14:23:05 +0000

    change HARVEST to SQUID; remove rcsid and copyright

--- src/cachemgr.cc
+++ src/cachemgr.cc
@@ -1,54 +1,5 @@
-static char rcsid[] = "$Id: cachemgr.cc,v 1.2 1996/02/23 05:41:20 wessels Exp $";
-/*
- *  cachemgr.c - CGI interface to the Cache Manager.
- *
- *  ----------------------------------------------------------------
- *
- *  Copyright (c) 1994, 1995.  All rights reserved.
- *  
- *          Mic Bowman of Transarc Corporation.
- *          Peter Danzig of the University of Southern California.
- *          Darren R. Hardy of the University of Colorado at Boulder.
- *          Udi Manber of the University of Arizona.
- *          Michael F. Schwartz of the University of Colorado at Boulder. 
- *          Duane Wessels of the University of Colorado at Boulder. 
- *  
- *  This copyright notice applies to all code in Harvest other than
- *  subsystems developed elsewhere, which contain other copyright notices
- *  in their source text.
- *  
- *  The Harvest software was developed by the Internet Research Task
- *  Force Research Group on Resource Discovery (IRTF-RD).  The Harvest
- *  software may be used for academic, research, government, and internal
- *  business purposes without charge.  If you wish to sell or distribute
- *  the Harvest software to commercial clients or partners, you must
- *  license the software.  See
- *  http://harvest.cs.colorado.edu/harvest/copyright,licensing.html#licensing.
- *  
- *  The Harvest software is provided ``as is'', without express or
- *  implied warranty, and with no support nor obligation to assist in its
- *  use, correction, modification or enhancement.  We assume no liability
- *  with respect to the infringement of copyrights, trade secrets, or any
- *  patents, and are not responsible for consequential damages.  Proper
- *  use of the Harvest software is entirely the responsibility of the user.
- *  
- *  For those who are using Harvest for non-commercial purposes, you may
- *  make derivative works, subject to the following constraints:
- *  
- *  - You must include the above copyright notice and these accompanying 
- *    paragraphs in all forms of derivative works, and any documentation 
- *    and other materials related to such distribution and use acknowledge 
- *    that the software was developed at the above institutions.
- *  
- *  - You must notify IRTF-RD regarding your distribution of the 
- *    derivative work.
- *  
- *  - You must clearly notify users that your are distributing a modified 
- *    version and not the original Harvest software.
- *  
- *  - Any derivative product is also subject to the restrictions of the 
- *    copyright, including distribution and use limitations.
- */
+/* $Id: cachemgr.cc,v 1.3 1996/02/29 07:23:06 wessels Exp $ */
+
 #include "config.h"
 #include <stdio.h>
 #include <string.h>
