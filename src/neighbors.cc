2f2e4070514cd4efc7943a2f4bfb91714ca9d4af
wessels <>
1996-03-28 03:20:06 +0000

    fix byte order conversion bug on echo port

--- src/neighbors.cc
+++ src/neighbors.cc
@@ -1,4 +1,4 @@
-/* $Id: neighbors.cc,v 1.5 1996/03/27 18:15:50 wessels Exp $ */
+/* $Id: neighbors.cc,v 1.6 1996/03/27 20:20:06 wessels Exp $ */
 
 #include "squid.h"
 
@@ -341,7 +341,7 @@ void neighbors_open(fd)
 	    echo_hdr.shostid = our_socket_name.sin_addr.s_addr;
 
 	    sep = getservbyname("echo", "udp");
-	    echo_port = sep ? sep->s_port : 7;
+	    echo_port = ntohs((u_short) (sep ? sep->s_port : 7));
 	}
     }
 }
