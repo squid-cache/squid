c4fb974e162a133805130025a26f981aff48838b
wessels <>
1996-03-28 03:21:11 +0000

    create setMaxFD() in tools.c

--- src/tools.cc
+++ src/tools.cc
@@ -1,5 +1,5 @@
 
-/* $Id: tools.cc,v 1.12 1996/03/27 18:50:59 wessels Exp $ */
+/* $Id: tools.cc,v 1.13 1996/03/27 20:21:11 wessels Exp $ */
 
 #include "squid.h"
 
@@ -348,4 +348,36 @@ void writePidFile()
     }
     fprintf(pid_fp, "%d\n", (int) getpid());
     fclose(pid_fp);
+}
+
+
+void setMaxFD()
+{
+#if defined(HAVE_SETRLIMIT)
+    /* try to use as many file descriptors as possible */
+    /* System V uses RLIMIT_NOFILE and BSD uses RLIMIT_OFILE */
+    struct rlimit rl;
+#if defined(RLIMIT_NOFILE)
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+	perror("getrlimit: RLIMIT_NOFILE");
+    } else {
+	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
+	if (setrlimit(RLIMIT_NOFILE, &rl) < 0) {
+	    perror("setrlimit: RLIMIT_NOFILE");
+	}
+    }
+#elif defined(RLIMIT_OFILE)
+    if (getrlimit(RLIMIT_OFILE, &rl) < 0) {
+	perror("getrlimit: RLIMIT_OFILE");
+    } else {
+	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
+	if (setrlimit(RLIMIT_OFILE, &rl) < 0) {
+	    perror("setrlimit: RLIMIT_OFILE");
+	}
+    }
+#endif
+    debug(0,1,"setMaxFD: Using %d file descriptors\n", rl.rlim_max);
+#else /* HAVE_SETRLIMIT */
+    debug(0,1,"setMaxFD: Cannot increase: setrlimit() not supported on this system");
+#endif
 }
