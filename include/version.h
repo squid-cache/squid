6cad78ccd3f17e92edecfa133e3fc8aa7087a21c
wessels <>
1996-03-30 07:33:48 +0000

    1.0.alpha4

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
-/* $Id: version.h,v 1.4 1996/03/29 01:08:19 wessels Exp $
+/* $Id: version.h,v 1.5 1996/03/30 00:33:48 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.0.alpha3"
+#define SQUID_VERSION	"1.0.alpha4"
 #endif
