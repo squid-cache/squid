ffbd5d3180307fdd8f5e83aa18e37e0e4b730e20
wessels <Unknown>
1996-04-14 09:29:36 +0000

    1.0.alpha15

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
-/* $Id: version.h,v 1.14 1996/04/11 23:53:54 wessels Exp $
+/* $Id: version.h,v 1.15 1996/04/14 03:29:36 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.0.alpha14"
+#define SQUID_VERSION	"1.0.alpha15"
 #endif
