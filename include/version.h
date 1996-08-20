c7f11c37c0582b5bc13f2fca63766e483dc581e6
wessels <>
1996-08-20 05:09:59 +0000

    1.1.alpha10

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
-/* $Id: version.h,v 1.28 1996/08/19 23:09:46 wessels Exp $
+/* $Id: version.h,v 1.29 1996/08/19 23:09:59 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.1.alpha9"
+#define SQUID_VERSION	"1.1.alpha10"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
