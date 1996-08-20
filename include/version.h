e973b94a2a669a41790542de01469131aea33561
wessels <>
1996-08-20 05:09:42 +0000

    1.0.7 -> 1.0.8 merge

--- include/version.h
+++ include/version.h
@@ -1,7 +1,11 @@
-/* $Id: version.h,v 1.27 1996/08/14 22:57:03 wessels Exp $
+/* $Id: version.h,v 1.28 1996/08/19 23:09:46 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
 #define SQUID_VERSION	"1.1.alpha9"
+#endif
+
+#ifndef SQUID_RELEASE_TIME
+#define SQUID_RELEASE_TIME squid_curtime
 #endif
