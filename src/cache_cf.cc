e3ea7b83906486357ac6e412aeb058a58dfb079c
wessels <>
1996-08-24 03:15:45 +0000

    remove restrictions on hot_vm_factor

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: cache_cf.cc,v 1.69 1996/08/20 15:43:41 wessels Exp $
+ * $Id: cache_cf.cc,v 1.70 1996/08/23 21:15:45 wessels Exp $
  *
  * DEBUG: section 3     Configuration File Parsing
  * AUTHOR: Harvest Derived
@@ -1442,15 +1442,4 @@ static void configDoConfigure()
 	getMyHostname(), Config.Port.http);
     if (Config.errHtmlText == NULL)
 	Config.errHtmlText = xstrdup("");
-
-#if !ALLOW_HOT_CACHE
-    if (!httpd_accel_mode || Config.Accel.withProxy) {
-	/* Not running strict httpd_accel--force hot_vm_factor to be 0 */
-	if (Config.hotVmFactor != 0.0) {
-	    printf("WARNING: Non-zero hot_vm_factor not allowed unless running only\n");
-	    printf("         in httpd_accel mode.  Setting hot_vm_factor to 0.\n");
-	    Config.hotVmFactor = 0.0;
-	}
-    }
-#endif /* !ALLOW_HOT_CACHE */
 }
