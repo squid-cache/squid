3136c7ab80cf5e74fc1400e0fbabcd02165ce860
wessels <Unknown>
1996-08-27 04:01:03 +0000

    1.1.alpha12

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
-/* $Id: version.h,v 1.32 1996/08/26 22:00:04 wessels Exp $
+/* $Id: version.h,v 1.33 1996/08/26 22:01:03 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.1.alpha7.BuGless"
+#define SQUID_VERSION	"1.1.alpha12"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
