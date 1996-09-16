aabcd584461ca6fee332482921febad3efcaffb1
wessels <>
1996-09-16 22:28:38 +0000

    use SQUIDHOSTNAMELEN instead of MAX_HOST_NAME

--- src/ipcache.cc
+++ src/ipcache.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: ipcache.cc,v 1.57 1996/09/15 05:04:34 wessels Exp $
+ * $Id: ipcache.cc,v 1.58 1996/09/16 16:28:39 wessels Exp $
  *
  * DEBUG: section 14    IP Cache
  * AUTHOR: Harvest Derived
@@ -109,7 +109,6 @@
 
 #define IP_LOW_WATER       90
 #define IP_HIGH_WATER      95
-#define MAX_HOST_NAME	  256
 
 struct _ip_pending {
     int fd;
@@ -781,7 +780,7 @@ ipcache_init()
     static_result->h_length = 4;
     static_result->h_addr_list = xcalloc(2, sizeof(char *));
     *(static_result->h_addr_list + 0) = xcalloc(1, 4);
-    static_result->h_name = xcalloc(1, MAX_HOST_NAME + 1);
+    static_result->h_name = xcalloc(1, SQUIDHOSTNAMELEN + 1);
 
     ipcache_high = (long) (((float) Config.ipcache.size *
 	    (float) Config.ipcache.high) / (float) 100);
@@ -995,7 +994,7 @@ ipcacheCheckNumeric(char *name)
     if ((ip = inet_addr(name)) == INADDR_NONE)
 	return NULL;
     *((u_num32 *) (void *) static_result->h_addr_list[0]) = ip;
-    strncpy(static_result->h_name, name, MAX_HOST_NAME);
+    strncpy(static_result->h_name, name, SQUIDHOSTNAMELEN);
     return static_result;
 }
 
