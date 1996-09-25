023552a9bcafb4a1f53ad450b81d92fbb1f5fb22
wessels <>
1996-09-25 00:44:29 +0000

    From:    webadm@cc.fh-lippe.de (Cord Beermann, Webmaster der FH Lippe) Here is a small fix, which corrects the Cachemgr.cgi-Output for non-table-Browsers.

--- src/cachemgr.cc
+++ src/cachemgr.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: cachemgr.cc,v 1.28 1996/09/20 06:28:29 wessels Exp $
+ * $Id: cachemgr.cc,v 1.29 1996/09/24 18:44:29 wessels Exp $
  *
  * DEBUG: Section 0     CGI Cache Manager
  * AUTHOR: Harvest Derived
@@ -881,13 +881,13 @@ main(int argc, char *argv[])
 			s1, &d1, &d2, &d3, &d4, &f1, &d5, &d6, &d7);
 		    if (sn == 1) {
 			if (hasTables)
-			    printf("<tr><td align=\"right\"><B>%s</B>", s1);
+			    printf("<tr><td align=\"right\"><STRONG>%s</STRONG>\n", s1);
 			else
-			    printf("%8s", s1);
+			    printf("%s-Requests\n", s1);
 			break;
 		    }
 		    if (hasTables)
-			printf("<tr><td align=\"right\"><B>%s</B><td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%4.2f<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d",
+			printf("<tr><td align=\"right\"><STRONG>%s</STRONG><td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%4.2f<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d\n",
 			    s1, d1, d2, d3, d4, f1, d5, d6, d7);
 		    else
 			printf("%8s %7d %9d %9d %9d %4.2f %6d %9d %10d\n",
