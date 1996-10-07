d35720846292c70eda3cf3e297724e6314125ded
wessels <Unknown>
1996-10-07 20:59:05 +0000

    better explain "Found non-ICP cache as SIBLING"

--- src/neighbors.cc
+++ src/neighbors.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: neighbors.cc,v 1.57 1996/09/20 06:28:58 wessels Exp $
+ * $Id: neighbors.cc,v 1.58 1996/10/07 14:59:05 wessels Exp $
  *
  * DEBUG: section 15    Neighbor Routines
  * AUTHOR: Harvest Derived
@@ -644,7 +644,8 @@ neighborsUdpAck(int fd, char *url, icp_common_t * header, struct sockaddr_in *fr
 	    debug(15, 1, "Ignoring DECHO from non-neighbor %s\n",
 		inet_ntoa(from->sin_addr));
 	} else if (e->type == EDGE_SIBLING) {
-	    fatal_dump("neighborsUdpAck: Found non-ICP cache as SIBLING\n");
+	    debug_trap("neighborsUdpAck: Found non-ICP cache as SIBLING\n");
+	    debug_trap("neighborsUdpAck: non-ICP neighbors must be a PARENT\n");
 	} else {
 	    w_rtt = tvSubMsec(mem->start_ping, current_time) / e->weight;
 	    if (mem->w_rtt == 0 || w_rtt < mem->w_rtt) {
