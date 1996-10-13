16161068479d1e24f0972f988782fd7013988232
wessels <Unknown>
1996-10-13 15:14:01 +0000

    check next_ping_time before pinging

--- src/net_db.cc
+++ src/net_db.cc
@@ -197,6 +197,10 @@ netdbSendPing(int fdunused, ipcache_addrs * ia, void *data)
 void
 netdbPingSite(char *hostname)
 {
+    netdbEntry *n;
+    if ((n = netdbLookupHost(hostname)) != NULL)
+	if (n->next_ping_time > squid_curtime)
+	    return;
     ipcache_nbgethostbyname(hostname,
 	-1,
 	netdbSendPing,
