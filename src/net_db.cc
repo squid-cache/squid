7c6679053fdf539ce5ca72af71e089bb0226debf
wessels <>
1996-10-15 10:58:27 +0000

    gindent

--- src/net_db.cc
+++ src/net_db.cc
@@ -330,34 +330,37 @@ netdbDump(StoreEntry * sentry)
     struct _net_db_name *x;
     int k;
     int i;
-    storeAppendPrintf(sentry, "{Network DB Statistics:\n");	/* } */
-    storeAppendPrintf(sentry, "{%-16.16s %9s %7s %5s %s}\n",
-	"Network",
-	"recv/sent",
-	"RTT",
-	"Hops",
-	"Hostnames");
-    list = xcalloc(meta_data.netdb_addrs, sizeof(netdbEntry *));
-    i = 0;
-    for (n = netdbGetFirst(addr_table); n; n = netdbGetNext(addr_table))
-	*(list + i++) = n;
-    qsort((char *) list,
-	i,
-	sizeof(netdbEntry *),
-	(QS) sortByHops);
-    for (k = 0; k < i; k++) {
-	n = *(list + k);
-	storeAppendPrintf(sentry, "{%-16.16s %4d/%4d %7.1f %5.1f",	/* } */
-	    n->network,
-	    n->pings_recv,
-	    n->pings_sent,
-	    n->rtt,
-	    n->hops);
-	for (x = n->hosts; x; x = x->next)
-	    storeAppendPrintf(sentry, " %s", x->name);
-	storeAppendPrintf(sentry, close_bracket);
-    }
+    storeAppendPrintf(sentry, "{Network DB Statistics:\n");
+    */
+} */
+
+storeAppendPrintf(sentry, "{%-16.16s %9s %7s %5s %s}\n",
+    "Network",
+    "recv/sent",
+    "RTT",
+    "Hops",
+    "Hostnames");
+list = xcalloc(meta_data.netdb_addrs, sizeof(netdbEntry *));
+i = 0;
+for (n = netdbGetFirst(addr_table); n; n = netdbGetNext(addr_table))
+    *(list + i++) = n;
+qsort((char *) list,
+    i,
+    sizeof(netdbEntry *),
+    (QS) sortByHops);
+for (k = 0; k < i; k++) {
+    n = *(list + k);
+    storeAppendPrintf(sentry, "{%-16.16s %4d/%4d %7.1f %5.1f",	/* } */
+	n->network,
+	n->pings_recv,
+	n->pings_sent,
+	n->rtt,
+	n->hops);
+    for (x = n->hosts; x; x = x->next)
+	storeAppendPrintf(sentry, " %s", x->name);
     storeAppendPrintf(sentry, close_bracket);
-    xfree(list);
+}
+storeAppendPrintf(sentry, close_bracket);
+xfree(list);
 #endif
 }
