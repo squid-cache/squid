423792703121547431969e85f045a780a03227e3
wessels <Unknown>
1996-11-08 10:51:06 +0000

    1.1.beta16

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
-/* $Id: version.h,v 1.54 1996/11/07 00:33:53 wessels Exp $
+/* $Id: version.h,v 1.55 1996/11/08 03:51:06 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.1.beta15"
+#define SQUID_VERSION	"1.1.beta16"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
