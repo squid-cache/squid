21db5b6320941eb476120a2a306daaf03bb99590
wessels <Unknown>
1997-01-11 06:23:27 +0000

    1.1.4

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
-/* $Id: version.h,v 1.73 1997/01/07 23:10:47 wessels Exp $
+/* $Id: version.h,v 1.74 1997/01/10 23:23:27 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"NOVM.1.1.2"
+#define SQUID_VERSION	"1.1.4"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
