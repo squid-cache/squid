05b470c20258a18ff4be2b80b89c130410864b4f
wessels <Unknown>
1997-01-14 05:58:03 +0000

    fix ru.ru_maxrss in various ways

--- src/tools.cc
+++ src/tools.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: tools.cc,v 1.93 1997/01/07 20:31:25 wessels Exp $
+ * $Id: tools.cc,v 1.94 1997/01/13 22:58:04 wessels Exp $
  *
  * DEBUG: section 21    Misc Functions
  * AUTHOR: Harvest Derived
@@ -212,10 +212,10 @@ PrintRusage(void (*f) (void), FILE * lf)
     getrusage(RUSAGE_SELF, &rusage);
     fprintf(lf, "CPU Usage: user %d sys %d\n",
 	(int) rusage.ru_utime.tv_sec, (int) rusage.ru_stime.tv_sec);
-#if defined(_SQUID_SGI_) || defined(_SQUID_OSF_)
-    fprintf(lf, "Memory Usage: rss %ld KB\n", rusage.ru_maxrss);
+#if defined(_SQUID_SGI_) || defined(_SQUID_OSF_) || defined(BSD4_4)
+    fprintf(lf, "Maximum Resident Size: %ld KB\n", rusage.ru_maxrss);
 #else /* _SQUID_SGI_ */
-    fprintf(lf, "Memory Usage: rss %ld KB\n",
+    fprintf(lf, "Maximum Resident Size: %ld KB\n",
 	(rusage.ru_maxrss * getpagesize()) >> 10);
 #endif /* _SQUID_SGI_ */
     fprintf(lf, "Page faults with physical i/o: %ld\n",
