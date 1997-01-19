10cab007d4409e605993b09f7e7441956c8fcd11
wessels <>
1997-01-19 15:14:07 +0000

    oops, divide by days

--- src/stat.cc
+++ src/stat.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: stat.cc,v 1.118 1997/01/19 08:10:51 wessels Exp $
+ * $Id: stat.cc,v 1.119 1997/01/19 08:14:07 wessels Exp $
  *
  * DEBUG: section 18    Cache Manager Statistics
  * AUTHOR: Harvest Derived
@@ -761,9 +761,8 @@ info_get(const cacheinfo * obj, StoreEntry * sentry)
 	storeGetSwapSize() >> 10);
     storeAppendPrintf(sentry, "{\tStorage Mem size:\t%d KB}\n",
 	store_mem_size >> 10);
-    if (Config.referenceAge < 0)
-        storeAppendPrintf(sentry, "{\tStorage LRU Expiration Age:\t%d days}\n",
-    	    (int) storeExpiredReferenceAge());
+    storeAppendPrintf(sentry, "{\tStorage LRU Expiration Age:\t%6.2f days}\n",
+    	(double) storeExpiredReferenceAge());
 
 #if HAVE_GETRUSAGE && defined(RUSAGE_SELF)
     storeAppendPrintf(sentry, "{Resource usage for %s:}\n", appname);
