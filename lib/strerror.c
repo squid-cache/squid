ca8361af03d3040eb377c7502b5cbb7f525f00ba
wessels <>
1997-02-25 12:44:13 +0000

    adding

---
+++ lib/strerror.c
@@ -0,0 +1,46 @@
+/*
+ * $Id: strerror.c,v 1.1 1997/02/25 05:44:13 wessels Exp $
+ *
+ * DEBUG: 
+ * AUTHOR: Duane Wessels
+ *
+ * SQUID Internet Object Cache  http://squid.nlanr.net/Squid/
+ * --------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from the
+ *  Internet community.  Development is led by Duane Wessels of the
+ *  National Laboratory for Applied Network Research and funded by
+ *  the National Science Foundation.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
+
+#include "config.h"
+
+#if HAVE_ERRNO_H
+#include <errno.h>
+#endif
+
+extern int sys_nerr;
+#if NEED_SYS_ERRLIST && !defined(_SQUID_NETBSD_)
+extern char *sys_errlist[];
+#endif
+
+char *
+strerror(int ern)
+{
+	return sys_errlist[ern];
+}
