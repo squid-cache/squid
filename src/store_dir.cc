a1c69058835c9c721a42e1aa403dabdaccd1aa60
wessels <>
1997-05-16 05:43:14 +0000

    assert(dirn < ncache_dirs)

--- src/store_dir.cc
+++ src/store_dir.cc
@@ -224,6 +224,7 @@ storeDirSwapLog(const StoreEntry * e)
     if (e->swap_file_number < 0)
 	fatal_dump("storeDirSwapLog: swap_file_number < 0");
     dirn = e->swap_file_number >> SWAP_DIR_SHIFT;
+    assert(dirn < ncache_dirs);
     /* Note this printf format appears in storeWriteCleanLog() too */
     sprintf(logmsg, "%08x %08x %08x %08x %9d %s\n",
 	(int) e->swap_file_number,
