d892b1558970366e2d768ee2b054755d7a84e4a7
wessels <>
1997-10-18 06:01:06 +0000

    add headers, GPL statement, change debug section to 51

--- src/fd.cc
+++ src/fd.cc
@@ -1,3 +1,33 @@
+/*
+ * $Id: fd.cc,v 1.13 1997/10/18 00:01:06 wessels Exp $
+ *
+ * DEBUG: section 51    Filedescriptor Functions
+ * AUTHOR: Duane Wessels
+ *
+ * SQUID Internet Object Cache  http://squid.nlanr.net/Squid/
+ * --------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from the
+ *  Internet community.  Development is led by Duane Wessels of the
+ *  National Laboratory for Applied Network Research and funded by
+ *  the National Science Foundation.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
+
 #include "squid.h"
 
 static void fdUpdateBiggest _PARAMS((int fd, unsigned int status));
@@ -81,6 +111,6 @@ fdDumpOpen(void)
 	    continue;
 	if (i == fileno(debug_log))
 	    continue;
-	debug(5, 1) ("Open FD %4d %s\n", i, F->desc);
+	debug(51, 1) ("Open FD %4d %s\n", i, F->desc);
     }
 }
