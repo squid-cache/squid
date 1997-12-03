611109c1700cf6f4527595d4e0d22ff26b830c92
wessels <Unknown>
1997-12-03 08:29:35 +0000

    remove include/snmp_config.h because its specific to the src directory.  its contents have been relocated to src/structs.h and src/defines.h

--- src/structs.h
+++ src/structs.h
@@ -40,6 +40,36 @@ struct _snmpconf {
     int type;
     struct _snmpconf *next;
 };
+
+typedef struct _viewEntry {
+    char viewName[32];
+    int viewIndex;
+    int viewType;
+    int viewSubtreeLen;
+    oid viewSubtree[32];
+    struct _viewEntry *next;
+} viewEntry;
+
+typedef struct _communityEntry {
+    char name[64];
+    int readView;
+    int writeView;
+    struct _communityEntry *next;
+} communityEntry;
+
+typedef struct _usecEntry {
+    u_char userName[32];
+    int userLen;
+    int qoS;
+    u_char authKey[16];
+    u_char privKey[16];
+    int noauthReadView;
+    int noauthWriteView;
+    int authReadView;
+    int authWriteView;
+    struct _usecEntry *next;
+} usecEntry;
+
 #endif
 
 struct _acl {
