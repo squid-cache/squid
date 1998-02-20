5ba18ba6fb69dbc520c465c8754d0af534492919
wessels <>
1998-02-20 06:31:19 +0000

    move function to the non-PUBLIC area

--- src/pconn.cc
+++ src/pconn.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: pconn.cc,v 1.12 1998/02/19 23:28:40 wessels Exp $
+ * $Id: pconn.cc,v 1.13 1998/02/19 23:31:19 wessels Exp $
  *
  * DEBUG: section 48    Persistent Connections
  * AUTHOR: Duane Wessels
@@ -119,6 +119,34 @@ pconnRead(int fd, void *data)
     comm_close(fd);
 }
 
+static void
+pconnHistDump(StoreEntry * e)
+{
+    int i;
+    storeAppendPrintf(e,
+        "Client-side persistent connection counts:\n"
+        "\n"
+        "\treq/\n"
+        "\tconn      count\n"
+        "\t----  ---------\n");
+    for (i = 0; i < PCONN_HIST_SZ; i++) {
+        if (client_pconn_hist[i] == 0)
+            continue;
+        storeAppendPrintf(e, "\t%4d  %9d\n", i, client_pconn_hist[i]);
+    }
+    storeAppendPrintf(e,
+        "\n"
+        "Server-side persistent connection counts:\n"
+        "\n"
+        "\treq/\n"
+        "\tconn      count\n"
+        "\t----  ---------\n");
+    for (i = 0; i < PCONN_HIST_SZ; i++) {
+        if (server_pconn_hist[i] == 0)
+            continue;
+        storeAppendPrintf(e, "\t%4d  %9d\n", i, server_pconn_hist[i]);
+    }
+}
 
 /* ========== PUBLIC FUNCTIONS ============================================ */
 
@@ -197,33 +225,4 @@ pconnHistCount(int what, int i)
         server_pconn_hist[i]++;
     else
         assert(0);
-}
-
-static void
-pconnHistDump(StoreEntry * e)
-{
-    int i;
-    storeAppendPrintf(e,
-        "Client-side persistent connection counts:\n"
-        "\n"
-        "\treq/\n"
-        "\tconn      count\n"
-        "\t----  ---------\n");
-    for (i = 0; i < PCONN_HIST_SZ; i++) {
-        if (client_pconn_hist[i] == 0)
-            continue;
-        storeAppendPrintf(e, "\t%4d  %9d\n", i, client_pconn_hist[i]);
-    }
-    storeAppendPrintf(e,
-        "\n"
-        "Server-side persistent connection counts:\n"
-        "\n"
-        "\treq/\n"
-        "\tconn      count\n"
-        "\t----  ---------\n");
-    for (i = 0; i < PCONN_HIST_SZ; i++) {
-        if (server_pconn_hist[i] == 0)
-            continue;
-        storeAppendPrintf(e, "\t%4d  %9d\n", i, server_pconn_hist[i]);
-    }
 }
