bce0cf8c6280419a5789d7629fc209a8f6c76e3a
rousskov <>
1998-03-06 12:34:44 +0000

    - Changed POST action in host/port form to GET.

--- src/cachemgr.cc
+++ src/cachemgr.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: cachemgr.cc,v 1.75 1998/03/05 00:42:47 wessels Exp $
+ * $Id: cachemgr.cc,v 1.76 1998/03/06 05:34:44 rousskov Exp $
  *
  * DEBUG: section 0     CGI Cache Manager
  * AUTHOR: Duane Wessels
@@ -235,7 +235,7 @@ auth_html(char *host, int port, const char *user_name)
     printf("<P>This is a WWW interface to the instrumentation interface\n");
     printf("for the Squid object cache.</P>\n");
     printf("<HR>\n");
-    printf("<FORM METHOD=\"POST\" ACTION=\"%s\">\n", script_name);
+    printf("<FORM METHOD=\"GET\" ACTION=\"%s\">\n", script_name);
     printf("<TABLE BORDER=0>\n");
     printf("<TR><TH ALIGN=\"left\">Cache Host:</TH><TD><INPUT NAME=\"host\" ");
     printf("SIZE=30 VALUE=\"%s\"></TD></TR>\n", host);
