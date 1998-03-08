5e91f8bc3ffbbc75bb1d91cd68efd7e79616c91e
rousskov <>
1998-03-08 03:17:44 +0000

    - Fixed a bug that prevented some tables from being displayed. We did not   close a <table> if there were no text after it.

--- src/cachemgr.cc
+++ src/cachemgr.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: cachemgr.cc,v 1.76 1998/03/06 05:34:44 rousskov Exp $
+ * $Id: cachemgr.cc,v 1.77 1998/03/07 20:17:44 rousskov Exp $
  *
  * DEBUG: section 0     CGI Cache Manager
  * AUTHOR: Duane Wessels
@@ -480,7 +480,7 @@ read_reply(int s, cachemgr_request * req)
 	    if (parse_menu)
 		printf("</UL>\n");
 	    else
-		printf("</PRE>\n");
+		printf("</table></PRE>\n");
 	    print_trailer();
 	    istate = isSuccess;
 	    break;
@@ -662,7 +662,7 @@ make_pub_auth(cachemgr_request * req)
     /* host | time | user | passwd */
     snprintf(buf, sizeof(buf), "%s|%d|%s|%s",
 	req->hostname,
-	now,
+	(int)now,
 	req->user_name ? req->user_name : "",
 	req->passwd);
     debug(3) fprintf(stderr, "cmgr: pre-encoded for pub: %s\n", buf);
