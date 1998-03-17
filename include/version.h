0063350a67964857391dc3911fbbe9e352a1ab95
wessels <Unknown>
1998-03-17 12:07:46 +0000

    1.2.beta17

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 /*
- * $Id: version.h,v 1.107 1998/03/05 00:38:55 wessels Exp $
+ * $Id: version.h,v 1.108 1998/03/17 05:07:46 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.2.beta16"
+#define SQUID_VERSION	"1.2.beta17"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
