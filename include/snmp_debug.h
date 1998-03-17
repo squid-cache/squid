a35bb072d0847f2afd30dcfd519d42bd799c32b4
wessels <>
1998-03-17 03:31:09 +0000

    remove snmplib_debug_hook prototype -- it doesnt need to be global

--- include/snmp_debug.h
+++ include/snmp_debug.h
@@ -1,15 +1,10 @@
 #ifndef SNMP_DEBUG_H
 #define SNMP_DEBUG_H
 
-#ifndef SQUID_H
 #ifdef __STDC__
 extern void snmplib_debug(int,char *,...);
-extern void (*snmplib_debug_hook) (int,char *,...);
 #else
 extern void snmplib_debug(va_alist);
-extern void (*snmplib_debug_hook) (va_alist);
-#endif
-
 #endif
 
 #endif
