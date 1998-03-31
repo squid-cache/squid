97d93ecd264fcc159a6c34a3c243a39a19ec3da1
rousskov <>
1998-03-31 07:10:10 +0000

    - commented out printf()s on bad log entries.

--- src/test_cache_digest.cc
+++ src/test_cache_digest.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: test_cache_digest.cc,v 1.3 1998/03/31 00:03:55 rousskov Exp $
+ * $Id: test_cache_digest.cc,v 1.4 1998/03/31 00:10:10 rousskov Exp $
  *
  * AUTHOR: Alex Rousskov
  *
@@ -393,8 +393,8 @@ cacheIndexScanAccessLog(CacheIndex *idx, const char *fname, FILE *file)
 	    fprintf(stderr, "%s scanned %d K entries (%d bad)\n", 
 		fname, scanned_count/1000, scanned_count-count-1);
 	if (!url || !hier) {
-	    fprintf(stderr, "%s:%d: strange access log entry '%s'\n", 
-		fname, scanned_count, buf);
+	    /*fprintf(stderr, "%s:%d: strange access log entry '%s'\n", 
+		fname, scanned_count, buf);*/
 	    continue;
 	}
 	method = url;
@@ -406,8 +406,8 @@ cacheIndexScanAccessLog(CacheIndex *idx, const char *fname, FILE *file)
 	method += 2;
 	method_id = cacheIndexParseMethod(method);
 	if (method_id == METHOD_NONE) {
-	    fprintf(stderr, "%s:%d: invalid method %s in '%s'\n", 
-		fname, scanned_count, method, buf);
+	    /*fprintf(stderr, "%s:%d: invalid method %s in '%s'\n", 
+		fname, scanned_count, method, buf);*/
 	    continue;
 	}
 	while (*url) url--;
