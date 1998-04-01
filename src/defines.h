40608e8e3d9d12c76217545d9002a1dff957c1a1
wessels <>
1998-04-01 07:13:14 +0000

    remove special SYSLOG case for debug() macro.  MOST systems have SYSLOG anyway, but more importantly, the _db_level var is needed for stderr debugging.

--- src/defines.h
+++ src/defines.h
@@ -42,13 +42,8 @@
 #define COMM_NOCLOEXEC		0x02
 #define COMM_REUSEADDR		0x04
 
-#ifdef HAVE_SYSLOG
 #define debug(SECTION, LEVEL) \
         ((_db_level = (LEVEL)) > debugLevels[SECTION]) ? (void) 0 : _db_print
-#else
-#define debug(SECTION, LEVEL) \
-        ((LEVEL) > debugLevels[SECTION]) ? (void) 0 : _db_print
-#endif
 
 #define safe_free(x)	if (x) { xxfree(x); x = NULL; }
 
