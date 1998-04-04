51251ed16f102aebab17664f12db2e010246179b
wessels <>
1998-04-04 07:22:15 +0000

    commenting

--- src/MemBuf.cc
+++ src/MemBuf.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: MemBuf.cc,v 1.6 1998/02/27 09:11:43 rousskov Exp $
+ * $Id: MemBuf.cc,v 1.7 1998/04/04 00:22:15 wessels Exp $
  *
  * DEBUG: section 59    auto-growing Memory Buffer with printf
  * AUTHOR: Alex Rousskov
@@ -88,7 +88,7 @@
  * comm_write(buf.buf, memBufFreeFunc(&buf), ...);
  * 
  * -- *iff* you did not give the buffer away, free it yourself
- * -- memBufFree(&buf);
+ * -- memBufClean(&buf);
  * }
  */
 
