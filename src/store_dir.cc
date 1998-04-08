a0418209cb569d7561ec3217328dba43bcbacd89
wessels <Unknown>
1998-04-08 06:42:16 +0000

    windows NT fixes (barf)

--- src/store_dir.cc
+++ src/store_dir.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store_dir.cc,v 1.61 1998/04/04 04:52:22 wessels Exp $
+ * $Id: store_dir.cc,v 1.62 1998/04/08 00:42:24 wessels Exp $
  *
  * DEBUG: section 47    Store Directory Routines
  * AUTHOR: Duane Wessels
@@ -457,11 +457,11 @@ storeDirCloseTmpSwapLog(int dirn)
     char *new_path = xstrdup(storeDirSwapLogFile(dirn, ".new"));
     SwapDir *SD = &Config.cacheSwap.swapDirs[dirn];
     int fd;
+    file_close(SD->swaplog_fd);
     if (rename(new_path, swaplog_path) < 0) {
 	debug(50, 0) ("%s,%s: %s\n", new_path, swaplog_path, xstrerror());
 	fatal("storeDirCloseTmpSwapLog: rename failed");
     }
-    file_close(SD->swaplog_fd);
     fd = file_open(swaplog_path, O_WRONLY | O_CREAT, NULL, NULL, NULL);
     if (fd < 0) {
 	debug(50, 1) ("%s: %s\n", swaplog_path, xstrerror());
