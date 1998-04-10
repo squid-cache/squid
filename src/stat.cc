1543ab6cbe1d1bf17e1534707ab585cd9692a398
rousskov <>
1998-04-10 06:39:32 +0000

    - fixed bytes read bug (DW: s/swap_status/store_status) - added bytes/messages sent stats - other minor stats

--- src/stat.cc
+++ src/stat.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: stat.cc,v 1.233 1998/04/09 23:53:47 rousskov Exp $
+ * $Id: stat.cc,v 1.234 1998/04/10 00:39:33 rousskov Exp $
  *
  * DEBUG: section 18    Cache Manager Statistics
  * AUTHOR: Harvest Derived
@@ -1105,18 +1105,16 @@ statPeerSelect(StoreEntry * sentry)
 	    (int) peer->digest.stats.kbytes_recv.kb);
 	storeAppendPrintf(sentry, "peer.local_memory = %d\n",
 	    peer->digest.cd ? peer->digest.cd->mask_size/1024 : 0);
-	storeAppendPrintf(sentry, "\n");
-	if (peer->digest.cd) {
+	storeAppendPrintf(sentry, "digest state: inited: %d, disabled: %d usable: %d requested: %d\n",
+	    0 < EBIT_TEST(peer->digest.flags, PD_INITED),
+	    0 < EBIT_TEST(peer->digest.flags, PD_DISABLED),
+	    0 < EBIT_TEST(peer->digest.flags, PD_USABLE),
+	    0 < EBIT_TEST(peer->digest.flags, PD_REQUESTED)
+	);
+	if (peer->digest.cd)
 	    cacheDigestReport(peer->digest.cd, peer->host, sentry);
-	    storeAppendPrintf(sentry, "digest inited: %d, disabled: %d usable: %d requested: %d\n",
-		0 < EBIT_TEST(peer->digest.flags, PD_INITED),
-		0 < EBIT_TEST(peer->digest.flags, PD_DISABLED),
-		0 < EBIT_TEST(peer->digest.flags, PD_USABLE),
-		0 < EBIT_TEST(peer->digest.flags, PD_REQUESTED)
-		);
-	} else {
-	    storeAppendPrintf(sentry, "peer %s has no cache digest\n", peer->host);
-	}
+	else
+	    storeAppendPrintf(sentry, "no cache digest from peer %s\n", peer->host);
 	storeAppendPrintf(sentry, "\n");
     }
 
