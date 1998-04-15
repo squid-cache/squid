efa78387bb6105f32552e77b895e1ff3f2741815
rousskov <Unknown>
1998-04-15 06:34:34 +0000

    - added printouts of server_svc_time histograms

--- src/stat.cc
+++ src/stat.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: stat.cc,v 1.238 1998/04/14 16:38:23 rousskov Exp $
+ * $Id: stat.cc,v 1.239 1998/04/15 00:34:34 rousskov Exp $
  *
  * DEBUG: section 18    Cache Manager Statistics
  * AUTHOR: Harvest Derived
@@ -955,8 +955,12 @@ statCountersHistograms(StoreEntry *sentry)
     statHistDump(&f->icp.query_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\nicp.reply_svc_time histogram:\n");
     statHistDump(&f->icp.reply_svc_time, sentry, NULL);
+    storeAppendPrintf(sentry, "\nicp.server_svc_time histogram:\n");
+    statHistDump(&f->icp.server_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\nicp.client_svc_time histogram:\n");
     statHistDump(&f->icp.client_svc_time, sentry, NULL);
+    storeAppendPrintf(sentry, "\ncd.server_svc_time histogram:\n");
+    statHistDump(&f->cd.server_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\ncd.client_svc_time histogram:\n");
     statHistDump(&f->cd.client_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\ncd.peer_choice_count histogram:\n");
