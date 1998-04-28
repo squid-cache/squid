a3f9588e7ac21e4539dc45be86e00e885eaa6935
wessels <>
1998-04-28 01:15:59 +0000

    compiler warnings by Ilia Zadorozhko <falcon@netsy.com>

--- src/debug.cc
+++ src/debug.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: debug.cc,v 1.64 1998/03/06 23:22:25 wessels Exp $
+ * $Id: debug.cc,v 1.65 1998/04/27 19:16:08 wessels Exp $
  *
  * DEBUG: section 0     Debug Routines
  * AUTHOR: Harvest Derived
@@ -384,11 +384,11 @@ debugLogTime(time_t t)
  */
 static int Ctx_Warn_Level = 32;
 /* all descriptions has been printed up to this level */
-static Ctx_Reported_Level = -1;
+static int Ctx_Reported_Level = -1;
 /* descriptions are still valid or active up to this level */
-static Ctx_Valid_Level = -1;
+static int Ctx_Valid_Level = -1;
 /* current level, the number of nested ctx_enter() calls */
-static Ctx_Current_Level = -1;
+static int Ctx_Current_Level = -1;
 /* saved descriptions (stack) */
 static const char *Ctx_Descrs[CTX_MAX_LEVEL + 1];
 /* "safe" get secription */
