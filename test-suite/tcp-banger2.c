ba8550353408731c15918ec70e73b76bc811903f
wessels <Unknown>
1998-05-26 21:30:11 +0000

    log timeouts

--- test-suite/tcp-banger2.c
+++ test-suite/tcp-banger2.c
@@ -444,6 +444,7 @@ main(argc, argv)
 	    if (FD[i].cb == NULL)
 		continue;
 	    if (now.tv_sec - FD[i].start > lifetime) {
+		fprintf(stderr, "WARNING: fd %d lifetime timeout\n", i);
 		fd_close(i);
 		continue;
 	    }
