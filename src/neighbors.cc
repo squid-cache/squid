cafadc422d9188784169f40e640d61c0e47d4470
wessels <>
1998-05-29 05:43:45 +0000

    neighbors.c:192: warning: unused variable a

--- src/neighbors.cc
+++ src/neighbors.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: neighbors.cc,v 1.218 1998/05/28 20:47:53 wessels Exp $
+ * $Id: neighbors.cc,v 1.219 1998/05/28 23:43:45 wessels Exp $
  *
  * DEBUG: section 15    Neighbor Routines
  * AUTHOR: Harvest Derived
@@ -189,7 +189,6 @@ peerAllowedToUse(const peer * p, request_t * request)
 {
     const struct _domain_ping *d = NULL;
     int do_ping = 1;
-    const struct _acl_list *a = NULL;
     aclCheck_t checklist;
     assert(request != NULL);
     if (EBIT_TEST(request->flags, REQ_NOCACHE))
