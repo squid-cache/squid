a1dda3d36cb721543de468da40bea76ec26f184d
rousskov <>
1998-06-06 00:33:55 +0000

    - added missing else to "skip size checks for now"

--- test-suite/tcp-banger2.c
+++ test-suite/tcp-banger2.c
@@ -245,6 +245,7 @@ reply_done(int fd, void *data)
     struct _request *r = data;
     if (opt_range)
 	; /* skip size checks for now */
+    else
     if (r->bodysize != r->content_length)
 	fprintf(stderr,"ERROR: %s expected %d bytes got %d\n",
 		r->url, r->content_length, r->bodysize);
