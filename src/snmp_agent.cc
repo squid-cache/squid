2d370af0f0ccb362263345c49dc8d986fe0866da
wessels <>
1998-07-23 09:13:11 +0000

    glenn fix

--- src/snmp_agent.cc
+++ src/snmp_agent.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: snmp_agent.cc,v 1.51 1998/07/22 21:18:20 wessels Exp $
+ * $Id: snmp_agent.cc,v 1.52 1998/07/23 03:13:11 wessels Exp $
  *
  * DEBUG: section 49     SNMP Interface
  * AUTHOR: Kostas Anagnostakis
@@ -58,6 +58,7 @@ snmp_basicFn(variable_list * Var, snint * ErrP)
 {
     variable_list *Answer;
     char *pp;
+    oid object_id[LEN_SQUID_OBJ_ID] = {SQUID_OBJ_ID};
 
     debug(49, 5) ("snmp_basicFn: Processing request with magic %d!\n", Var->name[7]);
 
@@ -72,10 +73,9 @@ snmp_basicFn(variable_list * Var, snint * ErrP)
 	Answer->val.string = (u_char *) xstrdup(pp);
 	break;
     case SYS_OBJECT_ID:
-	pp = SQUID_VERSION;
-	Answer->type = ASN_OCTET_STR;
-	Answer->val_len = strlen(pp);
-	Answer->val.string = (u_char *) xstrdup(pp);
+	Answer->type = ASN_OBJECT_ID;
+        Answer->val_len = sizeof(object_id);
+        Answer->val.objid = oiddup(object_id, LEN_SQUID_OBJ_ID);
 	break;
     case SYS_UPTIME:
 	Answer->val_len = sizeof(snint);
