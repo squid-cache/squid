6e59f3f85f9a0077ac47ea6e63acb4dddf80e329
wessels <Unknown>
1998-08-14 00:32:01 +0000

    change debug BUG to assertion

--- src/HttpRequest.cc
+++ src/HttpRequest.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: HttpRequest.cc,v 1.15 1998/07/31 00:15:30 wessels Exp $
+ * $Id: HttpRequest.cc,v 1.16 1998/08/13 18:32:01 wessels Exp $
  *
  * DEBUG: section 73    HTTP Request
  * AUTHOR: Duane Wessels
@@ -80,14 +80,10 @@ requestUnlink(request_t * request)
 {
     if (!request)
 	return;
-    request->link_count--;
-    if (request->link_count > 0)
+    assert(link_count > 0);
+    if (--request->link_count > 0)
 	return;
-    if (request->link_count == 0)
-	requestDestroy(request);
-    else
-	debug(73, 1) ("requestUnlink: BUG: negative link_count: %d. Ignored.\n",
-	    request->link_count);
+    requestDestroy(request);
 }
 
 int
