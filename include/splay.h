a49fd4c6a7947106894b10beb862039f8e5025a3
wessels <>
1998-08-19 02:48:39 +0000

    clean up splay interface

--- include/splay.h
+++ include/splay.h
@@ -5,10 +5,13 @@ typedef struct _splay_node {
     struct _splay_node *right;
 } splayNode;
 
-typedef int SPCMP(const void *, splayNode *);
+typedef int SPLAYCMP(const void *, splayNode *);
+typedef void SPLAYWALKEE(void *, void *);
+typedef void SPLAYFREE(void *);
 
 extern int splayLastResult;
 
-splayNode *splay_insert(void *, splayNode *, SPCMP *);
-splayNode *splay_splay(const void *, splayNode *, SPCMP *);
-void splay_destroy(splayNode *, void (*)(void *));
+extern splayNode *splay_insert(void *, splayNode *, SPLAYCMP *);
+extern splayNode *splay_splay(const void *, splayNode *, SPLAYCMP *);
+extern void splay_destroy(splayNode *, SPLAYFREE *);
+extern void splay_walk(splayNode *, SPLAYWALKEE *, void *);
