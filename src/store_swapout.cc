ba07c4e41430b8ffa4c1215374798b0a4c3b9796
wessels <Unknown>
1998-08-20 08:49:10 +0000

    when write fails because a disk is full, only lower *that* cache_dir and recompute the maxSize.  'storedir' stats should now be consistent

--- src/store_swapout.cc
+++ src/store_swapout.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store_swapout.cc,v 1.23 1998/08/19 04:32:14 wessels Exp $
+ * $Id: store_swapout.cc,v 1.24 1998/08/20 02:49:13 wessels Exp $
  *
  * DEBUG: section 20    Storage Manager Swapout Functions
  * AUTHOR: Duane Wessels
@@ -79,12 +79,12 @@ storeSwapOutHandle(int fdnotused, int flag, size_t len, void *data)
 	if (e->swap_file_number > -1) {
 	    storeUnlinkFileno(e->swap_file_number);
 	    storeDirMapBitReset(e->swap_file_number);
+	    if (flag == DISK_NO_SPACE_LEFT) {
+		storeDirDiskFull(e->swap_file_number);
+		storeDirConfigure();
+		storeConfigure();
+	    }
 	    e->swap_file_number = -1;
-	}
-	if (flag == DISK_NO_SPACE_LEFT) {
-	    /* reduce the swap_size limit to the current size. */
-	    Config.Swap.maxSize = store_swap_size;
-	    storeConfigure();
 	}
 	storeReleaseRequest(e);
 	storeSwapOutFileClose(e);
