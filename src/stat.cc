3d7da954e3ae4a0410fdfd2af2d3e689e904742f
wessels <Unknown>
1998-08-21 22:58:11 +0000

    gindent

--- src/stat.cc
+++ src/stat.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: stat.cc,v 1.280 1998/08/21 07:37:24 wessels Exp $
+ * $Id: stat.cc,v 1.281 1998/08/21 16:58:12 wessels Exp $
  *
  * DEBUG: section 18    Cache Manager Statistics
  * AUTHOR: Harvest Derived
@@ -269,7 +269,7 @@ statStoreEntry(StoreEntry * s, StoreEntry * e)
 	    (int) mem->swapout.done_offset,
 	    (int) mem->swapout.queue_offset,
 	    mem->swapout.fd);
-	for (i=0, sc = &mem->clients[i]; sc != NULL; sc = sc->next, i++) {
+	for (i = 0, sc = &mem->clients[i]; sc != NULL; sc = sc->next, i++) {
 	    if (sc->callback_data == NULL)
 		continue;
 	    storeAppendPrintf(s, "\tClient #%d\n", i);
