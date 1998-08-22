4a3986a324f1b9c5c57c4703db43300cce024ed5
wessels <Unknown>
1998-08-22 01:28:57 +0000

    compiler

--- src/ftp.cc
+++ src/ftp.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: ftp.cc,v 1.248 1998/08/21 18:59:48 wessels Exp $
+ * $Id: ftp.cc,v 1.249 1998/08/21 19:28:57 wessels Exp $
  *
  * DEBUG: section 9     File Transfer Protocol (FTP)
  * AUTHOR: Harvest Derived
@@ -2233,7 +2233,7 @@ ftpAppendSuccessHeader(FtpStateData * ftpState)
     storeBufferFlush(e);
     reply->hdr_sz = e->mem_obj->inmem_hi;
     storeTimestampsSet(e);
-    if (EBIT_TEST(entry->flag, ENTRY_CACHABLE))
+    if (EBIT_TEST(e->flag, ENTRY_CACHABLE))
 	storeSetPublicKey(e);
 }
 
