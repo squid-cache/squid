7612f45514d67a8776762077424809a238cd8eab
wessels <Unknown>
1998-08-27 12:28:54 +0000

    renamed icp_ping_data to just ping_data (for HTCP)

--- src/access_log.cc
+++ src/access_log.cc
@@ -1,7 +1,7 @@
 
 
 /*
- * $Id: access_log.cc,v 1.41 1998/08/17 18:48:57 wessels Exp $
+ * $Id: access_log.cc,v 1.42 1998/08/27 06:28:54 wessels Exp $
  *
  * DEBUG: section 46    Access Log
  * AUTHOR: Duane Wessels
@@ -177,7 +177,7 @@ accessLogSquid(AccessLogEntry * al, MemBuf * mb)
 	al->private.method_str,
 	al->url,
 	al->cache.ident,
-	al->hier.icp.timedout ? "TIMEOUT_" : "",
+	al->hier.ping.timedout ? "TIMEOUT_" : "",
 	hier_strings[al->hier.code],
 	al->hier.host,
 	al->http.content_type);
@@ -312,15 +312,15 @@ accessLogClose(void)
 void
 hierarchyNote(HierarchyLogEntry * hl,
     hier_code code,
-    icp_ping_data * icpdata,
+    ping_data * pingdata,
     const char *cache_peer)
 {
     assert(hl != NULL);
     hl->code = code;
-    if (icpdata)
-	hl->icp = *icpdata;
+    if (pingdata)
+	hl->ping = *pingdata;
     xstrncpy(hl->host, cache_peer, SQUIDHOSTNAMELEN);
-    hl->icp.stop = current_time;
+    hl->ping.stop = current_time;
 }
 
 void
