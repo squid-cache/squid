17d961b6dc769675ca47d51f1fdb100ff1400709
wessels <Unknown>
1998-10-14 22:09:30 +0000

    FMR bug

--- src/helper.cc
+++ src/helper.cc
@@ -191,15 +191,15 @@ helperStateFree(int fd, void *data)
     if (srv->wfd != srv->rfd)
 	comm_close(srv->wfd);
     dlinkDelete(&srv->link, &hlp->servers);
-    cbdataFree(srv);
     hlp->n_running--;
     assert(hlp->n_running >= 0);
-    if (shutting_down || reconfiguring)
-	return;
-    debug(34, 0) ("WARNING: %s #%d (FD %d) exited\n",
-	hlp->id_name, srv->index + 1, fd);
-    if (hlp->n_running < hlp->n_to_start / 2)
-	fatalf("Too few %s processes are running", hlp->id_name);
+    if (!shutting_down && !reconfiguring) {
+        debug(34, 0) ("WARNING: %s #%d (FD %d) exited\n",
+	    hlp->id_name, srv->index + 1, fd);
+        if (hlp->n_running < hlp->n_to_start / 2)
+	    fatalf("Too few %s processes are running", hlp->id_name);
+    }
+    cbdataFree(srv);
 }
 
 static void
