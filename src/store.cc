7a1024eed0119b1e110e4f908e673674330d7bcb
wessels <>
1999-01-12 23:23:22 +0000

    typo

--- src/store.cc
+++ src/store.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store.cc,v 1.479 1999/01/12 16:22:16 wessels Exp $
+ * $Id: store.cc,v 1.480 1999/01/12 16:23:22 wessels Exp $
  *
  * DEBUG: section 20    Storage Manager
  * AUTHOR: Harvest Derived
@@ -566,8 +566,8 @@ storeComplete(StoreEntry * e)
 	 * if we're not STORE_PENDING, then probably we got aborted
 	 * and there should be NO clients on this entry
 	 */
-	assert(EBIT_TEST(entry->flags, ENTRY_ABORTED));
-	assert(entry->mem_obj->nclients == 0);
+	assert(EBIT_TEST(e->flags, ENTRY_ABORTED));
+	assert(e->mem_obj->nclients == 0);
 	return;
     }
     e->mem_obj->object_sz = e->mem_obj->inmem_hi;
