16e833641a185b8b77a7d9d01d9b071bd3541527
wessels <>
1999-05-03 22:29:45 +0000

    removed leftover? assertion from debugging days

--- src/helper.cc
+++ src/helper.cc
@@ -231,7 +231,6 @@ helperServerFree(int fd, void *data)
     if (!srv->flags.shutdown) {
 	debug(34, 0) ("WARNING: %s #%d (FD %d) exited\n",
 	    hlp->id_name, srv->index + 1, fd);
-	assert(hlp->n_running >= hlp->n_to_start / 2);
 	if (hlp->n_running < hlp->n_to_start / 2)
 	    fatalf("Too few %s processes are running", hlp->id_name);
     }
