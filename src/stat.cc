ba781fe79304a37571ec97337b3e37f45bb0fa1b
wessels <>
1999-05-22 08:28:56 +0000

    Added log_type to 'active_requests' output

--- src/stat.cc
+++ src/stat.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: stat.cc,v 1.317 1999/05/04 21:58:38 wessels Exp $
+ * $Id: stat.cc,v 1.318 1999/05/22 02:28:56 wessels Exp $
  *
  * DEBUG: section 18    Cache Manager Statistics
  * AUTHOR: Harvest Derived
@@ -1332,6 +1332,7 @@ statClientRequests(StoreEntry * s)
 		conn->defer.n, conn->defer.until);
 	}
 	storeAppendPrintf(s, "uri %s\n", http->uri);
+	storeAppendPrintf(s, "log_type %s\n", log_tags[http->log_type]);
 	storeAppendPrintf(s, "out.offset %d, out.size %d\n",
 	    http->out.offset, http->out.size);
 	storeAppendPrintf(s, "req_sz %d\n", http->req_sz);
