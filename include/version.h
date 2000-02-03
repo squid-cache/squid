cddd0272cd93141fb09de397be31a304b2da8cf4
wessels <>
2000-02-03 15:04:01 +0000

    DW:  - oops, use DEVEL now instead of PRE

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 /*
- * $Id: version.h,v 1.128 2000/01/11 06:14:55 wessels Exp $
+ * $Id: version.h,v 1.129 2000/02/03 08:04:01 wessels Exp $
  *
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"2.4.PRE2"
+#define SQUID_VERSION	"2.4.DEVEL2"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
