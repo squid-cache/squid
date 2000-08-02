169d286299d1fbed88df51db86b15a922e00033a
wessels <Unknown>
2000-08-02 04:31:16 +0000

    DW:  - add some comments and ref about floating point math in C.

--- src/carp.cc
+++ src/carp.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: carp.cc,v 1.11 2000/08/01 22:01:29 wessels Exp $
+ * $Id: carp.cc,v 1.12 2000/08/01 22:31:16 wessels Exp $
  *
  * DEBUG: section 39    Cache Array Routing Protocol
  * AUTHOR: Eric Stern
@@ -59,7 +59,8 @@ carpInit(void)
     }
     /*
      * sum of carp-load-factor's for all cache_peer's in squid.conf
-     * must equal 1.0
+     * must equal 1.0.  If this doesn't work, see
+     * http://www.eskimo.com/~scs/C-faq/q14.4.html
      */
     assert(1000 == (int) (1000.0 * a));
     k = 1;
