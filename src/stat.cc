2506da2f0879b510cc0f0d7e563966ab5a2f9403
wessels <Unknown>
2000-10-06 05:01:40 +0000

    DW:  - Add counters for the number of calls to memPoolAlloc and memPoolFree.

--- src/stat.cc
+++ src/stat.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: stat.cc,v 1.336 2000/10/05 03:26:48 wessels Exp $
+ * $Id: stat.cc,v 1.337 2000/10/05 23:01:40 wessels Exp $
  *
  * DEBUG: section 18    Cache Manager Statistics
  * AUTHOR: Harvest Derived
@@ -86,6 +86,9 @@ StatCounters CountHist[N_COUNT_HIST];
 static int NCountHist = 0;
 static StatCounters CountHourHist[N_COUNT_HOUR_HIST];
 static int NCountHourHist = 0;
+
+extern unsigned int mem_pool_alloc_calls;
+extern unsigned int mem_pool_free_calls;
 
 static void
 statUtilization(StoreEntry * e)
@@ -587,6 +590,10 @@ info_get(StoreEntry * sentry)
     storeAppendPrintf(sentry, "Memory accounted for:\n");
     storeAppendPrintf(sentry, "\tTotal accounted:       %6d KB\n",
 	memTotalAllocated() >> 10);
+    storeAppendPrintf(sentry, "\tmemPoolAlloc calls: %d\n",
+	mem_pool_alloc_calls);
+    storeAppendPrintf(sentry, "\tmemPoolFree calls: %d\n",
+	mem_pool_free_calls);
 
     storeAppendPrintf(sentry, "File descriptor usage for %s:\n", appname);
     storeAppendPrintf(sentry, "\tMaximum number of file descriptors:   %4d\n",
