b8a5f2636590e243edc8535b442467815ed0a2e6
wessels <Unknown>
2000-10-06 11:00:24 +0000

    DW:  - These XXX comments are out-of-date.

--- src/fs/diskd/store_io_diskd.cc
+++ src/fs/diskd/store_io_diskd.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store_io_diskd.cc,v 1.15 2000/07/18 07:34:32 wessels Exp $
+ * $Id: store_io_diskd.cc,v 1.16 2000/10/06 05:00:24 wessels Exp $
  *
  * DEBUG: section 81    Squid-side DISKD I/O functions.
  * AUTHOR: Duane Wessels
@@ -61,8 +61,7 @@ storeDiskdOpen(SwapDir * SD, StoreEntry * e, STFNCB * file_callback,
     diskdinfo_t *diskdinfo = SD->fsdata;
     debug(81, 3) ("storeDiskdOpen: fileno %08X\n", f);
     /*
-     * XXX Eventually there should be an option here to fail on open()
-     * If there are too many requests queued.
+     * Fail on open() if there are too many requests queued.
      */
     if (diskdinfo->away > diskdinfo->magic1) {
 	debug(81, 3) ("storeDiskdOpen: FAILING, too many requests away\n");
@@ -118,8 +117,7 @@ storeDiskdCreate(SwapDir * SD, StoreEntry * e, STFNCB * file_callback,
     diskdinfo_t *diskdinfo = SD->fsdata;
     diskdstate_t *diskdstate;
     /*
-     * XXX Eventually there should be an option here to fail on open()
-     * If there are too many requests queued.
+     * Fail on open() if there are too many requests queued.
      */
     if (diskdinfo->away > diskdinfo->magic1) {
 	diskd_stats.open_fail_queue_len++;
