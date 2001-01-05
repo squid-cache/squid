0f76d3d589fbaef647111dadbcee22c92d737e32
hno <Unknown>
2001-01-05 05:34:18 +0000

    Backed out the change for including sys/vfs.h as Duane added the same thing in store_dir.c

--- src/squid.h
+++ src/squid.h
@@ -1,6 +1,6 @@
 
 /*
- * $Id: squid.h,v 1.205 2001/01/04 21:57:58 hno Exp $
+ * $Id: squid.h,v 1.206 2001/01/04 22:34:18 hno Exp $
  *
  * AUTHOR: Duane Wessels
  *
@@ -157,9 +157,6 @@
 #endif
 #if HAVE_SYS_STAT_H
 #include <sys/stat.h>
-#endif
-#if HAVE_SYS_VFS_H
-#include <sys/vfs.h>
 #endif
 #if HAVE_SYS_UN_H
 #include <sys/un.h>
