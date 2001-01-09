1ff94392d43514968ea44d3b5d6b42d399378f16
hno <Unknown>
2001-01-09 21:41:33 +0000

    login=*:password cache_peer option to pass the authenticated username to a upstream proxy in another adminstrative domain.

--- src/http.cc
+++ src/http.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: http.cc,v 1.375 2001/01/07 23:36:39 hno Exp $
+ * $Id: http.cc,v 1.376 2001/01/09 14:41:33 hno Exp $
  *
  * DEBUG: section 11    Hypertext Transfer Protocol (HTTP)
  * AUTHOR: Harvest Derived
@@ -789,9 +789,19 @@ httpBuildRequestHeader(request_t * request,
 	}
     }
     /* append Proxy-Authorization if configured for peer, and proxying */
-    if (!httpHeaderHas(hdr_out, HDR_PROXY_AUTHORIZATION)) {
-	if (request->flags.proxying && orig_request->peer_login &&
+    if (request->flags.proxying && orig_request->peer_login &&
+	    !httpHeaderHas(hdr_out, HDR_PROXY_AUTHORIZATION) &&
 	    strcmp(orig_request->peer_login, "PASS") != 0) {
+	if (*orig_request->peer_login == '*') {
+	    /* Special mode, to pass the username to the upstream cache */
+	    char loginbuf[256];
+	    char *username = "-";
+	    if (orig_request->auth_user_request)
+		username = authenticateUserRequestUsername(orig_request->auth_user_request);
+	    snprintf(loginbuf, sizeof(loginbuf), "%s%s", username, orig_request->peer_login+1);
+	    httpHeaderPutStrf(hdr_out, HDR_PROXY_AUTHORIZATION, "Basic %s",
+		base64_encode(loginbuf));
+	} else {
 	    httpHeaderPutStrf(hdr_out, HDR_PROXY_AUTHORIZATION, "Basic %s",
 		base64_encode(orig_request->peer_login));
 	}
