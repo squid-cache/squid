6380dd826780812d7d0d2f657a6ca8bd8d3546eb
hno <Unknown>
2001-01-30 17:06:48 +0000

    Cleanup of coredump_dir. No longer automatically uses the first cache_dir location, and can be commented out to use the current directory.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: main.cc,v 1.331 2001/01/12 00:37:19 wessels Exp $
+ * $Id: main.cc,v 1.332 2001/01/30 10:06:50 hno Exp $
  *
  * DEBUG: section 1     Startup and Main Loop
  * AUTHOR: Harvest Derived
@@ -430,21 +430,9 @@ mainSetCwd(void)
 	    debug(50, 0) ("chdir: %s: %s\n", Config.coredump_dir, xstrerror());
 	}
     }
-    if (!Config.effectiveUser) {
-	char *p = getcwd(NULL, 0);
-	debug(0, 1) ("Current Directory is %s\n", p);
-	xfree(p);
-	return;
-    }
-    /* we were probably started as root, so cd to a swap
-     * directory in case we dump core */
-    if (!chdir(storeSwapDir(0))) {
-	debug(0, 1) ("Set Current Directory to %s\n", storeSwapDir(0));
-	return;
-    } else {
-	debug(50, 0) ("%s: %s\n", storeSwapDir(0), xstrerror());
-	fatal_dump("Cannot cd to swap directory?");
-    }
+    debug(0, 1) ("Current Directory is %s\n", p);
+    xfree(p);
+    return;
 }
 
 static void
