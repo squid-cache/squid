d0d4950f303ca4f79150cd3e3e0839c71708c92c
hno <Unknown>
2001-05-19 23:56:41 +0000

    The ERROR macro is not used anywhere, and conflicts with a ERROR macro on WIN32 platforms.

--- include/snmp-internal.h
+++ include/snmp-internal.h
@@ -24,18 +24,12 @@
  * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
  * SOFTWARE.
  * 
- * $Id: snmp-internal.h,v 1.1 1998/02/22 11:48:41 kostas Exp $
+ * $Id: snmp-internal.h,v 1.2 2001/05/19 17:56:41 hno Exp $
  * 
  **********************************************************************/
 
 #define SNMP_PORT	    161
 #define SNMP_TRAP_PORT	    162
 #define SNMP_MAX_LEN	    484
-
-#ifdef DEBUG
-#define ERROR(string)	printf("%s(%d): %s\n",__FILE__, __LINE__, string);
-#else
-#define ERROR(string)
-#endif
 
 #endif /* _SNMP_INTERNAL_H_ */
