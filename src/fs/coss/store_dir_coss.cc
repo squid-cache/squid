0dbabb56eb90e2b10ef08f517bab89bff8f970b6
adrian <>
2001-08-13 04:18:35 +0000

    * return the load based on the aio queue depth * add a statistic line showing the number of pending disk operations

--- src/fs/coss/store_dir_coss.cc
+++ src/fs/coss/store_dir_coss.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store_dir_coss.cc,v 1.27 2001/08/12 10:25:00 adrian Exp $
+ * $Id: store_dir_coss.cc,v 1.28 2001/08/12 22:18:35 adrian Exp $
  *
  * DEBUG: section 81    Store COSS Directory Routines
  * AUTHOR: Eric Stern
@@ -680,13 +680,17 @@ storeCossDirShutdown(SwapDir * SD)
 int
 storeCossDirCheckObj(SwapDir * SD, const StoreEntry * e)
 {
+    CossInfo *cs = (CossInfo *) SD->fsdata;
+    int loadav;
+
     /* Check if the object is a special object, we can't cache these */
     if (EBIT_TEST(e->flags, ENTRY_SPECIAL))
 	return -1;
 
     /* Otherwise, we're ok */
-    /* Return 900 (90%) load */
-    return 900;
+    /* Return load, cs->aq.aq_numpending out of MAX_ASYNCOP */
+    loadav = cs->aq.aq_numpending * 1000 / MAX_ASYNCOP;
+    return loadav;
 }
 
 
@@ -720,6 +724,7 @@ storeCossDirStats(SwapDir * SD, StoreEntry * sentry)
 	SD->map->n_files_in_map, SD->map->max_n_files,
 	percent(SD->map->n_files_in_map, SD->map->max_n_files));
 #endif
+    storeAppendPrintf(sentry, "Pending operations: %d out of %d\n", cs->aq.aq_numpending, MAX_ASYNCOP);
     storeAppendPrintf(sentry, "Flags:");
     if (SD->flags.selected)
 	storeAppendPrintf(sentry, " SELECTED");
