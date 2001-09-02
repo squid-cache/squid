154b86f2ada7e84018845e6edce3a06ee1aee76a
robertc <Unknown>
2001-09-02 18:16:54 +0000

    Require version 1.5 of automake, and have bootstrap.sh provide basic error feedback.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,24 @@
 # configure has not been run, or if a Makefile.am in a non-configured directory
 # has been updated
 
-aclocal \
-&& autoheader \
-&& automake --foreign --add-missing \
-&& autoconf
+if ! ( aclocal ) ; then
+  echo "aclocal failed" 
+else
+  if ! ( autoheader ) ; then
+    echo "autoheader failed"
+  else
+    if ! ( automake --foreign --add-missing ) ; then
+      echo "automake failed"
+    else
+      if ! ( autoconf ) ; then
+	echo "autoconf failed"
+      else
+	echo "Autotool bootstrapping complete."
+	exit 0
+      fi
+    fi
+  fi
+fi
+
+echo "Autotool bootstrapping failed. You will need to investigate and correct" ;
+echo "before you can develop on this source tree" 
