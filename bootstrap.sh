96b8d5c3fb46a09d03e0334a4a5c74510ead5eba
hno <>
2001-09-04 04:15:07 +0000

    Major cleanup to separate error handling from normal flow
    Added creation of cfgaux if it does not exists

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,23 @@
 # configure has not been run, or if a Makefile.am in a non-configured directory
 # has been updated
 
-if ! ( aclocal ) ; then
-  echo "aclocal failed" 
-else
-  if ! ( autoheader ) ; then
-    echo "autoheader failed"
-  else
-    if ! ( automake --foreign --add-missing ) ; then
-      echo "automake failed"
-    else
-      if ! ( autoconf ) ; then
-	echo "autoconf failed"
-      else
-	echo "Autotool bootstrapping complete."
-	exit 0
-      fi
-    fi
-  fi
-fi
 
-echo "Autotool bootstrapping failed. You will need to investigate and correct" ;
-echo "before you can develop on this source tree" 
+bootstrap() {
+  if ! "$@"; then
+    echo "$1 failed"
+    echo "Autotool bootstrapping failed. You will need to investigate and correct" ;
+    echo "before you can develop on this source tree" 
+    exit 1
+  fi
+}
+
+# Make sure cfgaux exists
+mkdir -p cfgaux
+
+# Bootstrap the autotool subsystems
+bootstrap aclocal
+bootstrap autoheader
+bootstrap automake --foreign --add-missing
+bootstrap autoconf
+
+echo "Autotool bootstrapping complete."
