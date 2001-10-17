de896945cc5e746634ff40a7f92427aa352ad92c
hno <>
2001-10-17 18:33:34 +0000

    Minor cosmetic fix of squid_ldap_auth usage message

--- helpers/basic_auth/LDAP/squid_ldap_auth.c
+++ helpers/basic_auth/LDAP/squid_ldap_auth.c
@@ -235,7 +235,7 @@ main(int argc, char **argv)
 	fprintf(stderr, "\t-D binddn\t\tDN to bind as to perform searches\n");
 	fprintf(stderr, "\t-w bindpasswd\t\tpassword for binddn\n");
 	fprintf(stderr, "\t-h server\t\tLDAP server (defaults to localhost)\n");
-	fprintf(stderr, "\t-p port\t\tLDAP server port\n");
+	fprintf(stderr, "\t-p port\t\t\tLDAP server port\n");
 	fprintf(stderr, "\t-P\t\t\tpersistent LDAP connection\n");
 	fprintf(stderr, "\t-R\t\t\tdo not follow referrals\n");
 	fprintf(stderr, "\t-a never|always|search|find\n\t\t\t\twhen to dereference aliases\n");
