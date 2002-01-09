a4572994d0f211e9fab382cdf4da4f8e180c28fc
hno <>
2002-01-09 08:00:12 +0000

    Bootstrapped

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 revision"
+if [ $# -ne 1 -a $# -ne 2 ]; then
+	echo "Usage: $0 revision [destination]"
 	exit 1
 fi
 package=squid
 rev=`echo $1 | sed -e "s/^${package}-//"`
 name=${package}-${rev}
 tag=`echo ${name} | tr a-z.- A-Z__`
-startdir=$PWD
+startdir=$PWD/
+dst=${2:-$PWD}/
 
 tmpdir=$PWD/${name}-mkrelease
 
@@ -43,5 +44,9 @@ EOS
 make dist-all
 
 cd $startdir
-cp -p $tmpdir/${name}.tar.gz .
-cp -p $tmpdir/${name}.tar.bz2 .
+cp -p $tmpdir/${name}.tar.gz	$dst
+cp -p $tmpdir/${name}.tar.bz2	$dst
+cp -p $tmpdir/CONTRIBUTORS	$dst/CONTRIBUTORS.txt
+cp -p $tmpdir/COPYING		$dst/COPYING.txt
+cp -p $tmpdir/COPYRIGHT		$dst/COPYRIGHT.txt
+cp -p $tmpdir/CREDITS		$dst/CREDITS.txt
