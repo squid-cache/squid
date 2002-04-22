58bb546d96132da6ebb495d9d7ff2591246613c1
hno <Unknown>
2002-04-22 03:52:47 +0000

    Removed unused storeClientWaiting function

--- src/protos.h
+++ src/protos.h
@@ -1,6 +1,6 @@
 
 /*
- * $Id: protos.h,v 1.436 2002/04/21 21:23:15 hno Exp $
+ * $Id: protos.h,v 1.437 2002/04/21 21:52:47 hno Exp $
  *
  *
  * SQUID Web Proxy Cache          http://www.squid-cache.org/
@@ -886,7 +886,6 @@ extern StoreEntry *storeCreateEntry(const char *, const char *, request_flags, m
 extern void storeSetPublicKey(StoreEntry *);
 extern void storeComplete(StoreEntry *);
 extern void storeInit(void);
-extern int storeClientWaiting(const StoreEntry *);
 extern void storeAbort(StoreEntry *);
 extern void storeAppend(StoreEntry *, const char *, int);
 extern void storeLockObject(StoreEntry *);
