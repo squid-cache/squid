fb381327494f717919363c3df79c652ca0b6b57a
wessels <>
2002-06-20 04:54:14 +0000

    Some rewriting inspired by suggestions of overflow bugs and other problems with this code.
    denyusers.c and allowusers.c have a fair amount of nearly identical code.
    I moved these functions to a new file, called usersfile.[ch].  I didn't
    really want to add another source file, but....

---
+++ helpers/basic_auth/MSNT/usersfile.h
@@ -0,0 +1,15 @@
+
+
+typedef struct {
+    char *path;
+    char **names;
+    int Alloc;
+    int Inuse;
+    time_t LMT;
+} usersfile;
+
+
+
+int Read_usersfile(const char *path, usersfile * uf);
+int Check_userlist(usersfile * uf, char *User);
+void Check_forfilechange(usersfile * uf);
