0ef62b8a949bba6b7c2e30ee8e0cec716c9a265f
hno <Unknown>
2002-08-12 07:21:06 +0000

    Bogzilla #401: Killed the remaining (unused) references to sys_nerr to increase portability (MaxOS 10.2)

--- lib/util.c
+++ lib/util.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: util.c,v 1.85 2002/04/06 13:57:11 hno Exp $
+ * $Id: util.c,v 1.86 2002/08/12 01:21:06 hno Exp $
  *
  * DEBUG: 
  * AUTHOR: Harvest Derived
@@ -74,13 +74,6 @@ static void default_failure_notify(const char *);
 
 void (*failure_notify) (const char *) = default_failure_notify;
 static char msg[128];
-
-#if !defined(__CYGWIN__)
-extern int sys_nerr;
-#else
-#define sys_nerr _sys_nerr
-#endif
-
 
 #if MEM_GEN_TRACE
 
@@ -640,21 +633,6 @@ xstrerror(void)
         snprintf(xstrerror_buf, BUFSIZ, "(%d) Unknown", errno); 
     return xstrerror_buf;
 }
-
-#if NOT_NEEDED
-/*
- * xbstrerror with argument for late notification */
-
-const char *
-xbstrerror(int err)
-{
-    static char xbstrerror_buf[BUFSIZ];
-    if (err < 0 || err >= sys_nerr)
-	return ("Unknown");
-    snprintf(xbstrerror_buf, BUFSIZ, "(%d) %s", err, strerror(err));
-    return xbstrerror_buf;
-}
-#endif
 
 void
 Tolower(char *q)
