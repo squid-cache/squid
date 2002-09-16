2de96275b26acbb848f74eaf37612e5723271e85
robertc <>
2002-09-16 03:12:11 +0000

    make snmplib_debug's prototype match that used in snmplib_debug.c

--- include/snmp_debug.h
+++ include/snmp_debug.h
@@ -1,11 +1,16 @@
 /*
- * $Id: snmp_debug.h,v 1.10 2001/11/13 21:27:47 hno Exp $
+ * $Id: snmp_debug.h,v 1.11 2002/09/15 21:12:11 robertc Exp $
  */
 
 #ifndef SQUID_SNMP_DEBUG_H
 #define SQUID_SNMP_DEBUG_H
 
+#if STDC_HEADERS
 extern void 
 snmplib_debug(int, const char *,...) PRINTF_FORMAT_ARG2;
+#else
+extern void snmplib_debug (va_alist);
+#endif
+
 
 #endif /* SQUID_SNMP_DEBUG_H */
