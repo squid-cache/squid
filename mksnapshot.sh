3c337883286b0708aac659331bc5586f3c88c151
hno <Unknown>
2002-10-19 03:22:58 +0000

    Adjust for squid3 module

--- mksnapshot.sh
+++ mksnapshot.sh
@@ -3,12 +3,12 @@ if [ $# -gt 1 ]; then
 	echo "Usage: $0 [branch]"
 	exit 1
 fi
-package=squid
+module=squid3
 tag=${1:-HEAD}
 startdir=$PWD
 date=`env TZ=GMT date +%Y%m%d`
 
-tmpdir=$PWD/${package}-${tag}-mksnapshot
+tmpdir=$PWD/${module}-${tag}-mksnapshot
 
 CVSROOT=${CVSROOT:-/server/cvs-server/squid}
 export CVSROOT
@@ -17,9 +17,9 @@ rm -rf $tmpdir
 trap "rm -rf $tmpdir" 0
 
 rm -f ${tag}.out
-cvs -Q export -d $tmpdir -r $tag $package
+cvs -Q export -d $tmpdir -r $tag $module
 if [ ! -f $tmpdir/configure ]; then
-	echo "ERROR! Tag $tag not found in $package"
+	echo "ERROR! Tag $tag not found in $module"
 fi
 
 cd $tmpdir
@@ -47,6 +47,6 @@ echo ${PACKAGE}-${VERSION}-${date}.tar.bz2 >>${tag}.out
 
 if (echo $VERSION | grep PRE) || (echo $VERSION | grep STABLE); then
   echo "Differences from ${PACKAGE}-${VERSION} to ${PACKAGE}-${VERSION}-${date}" >${PACKAGE}-${VERSION}-${date}.diff
-  cvs -q rdiff -u -r SQUID_`echo $VERSION | tr .- __` -r $tag $package >>${PACKAGE}-${VERSION}-${date}.diff
+  cvs -q rdiff -u -r SQUID_`echo $VERSION | tr .- __` -r $tag $module >>${PACKAGE}-${VERSION}-${date}.diff
   echo ${PACKAGE}-${VERSION}-${date}.diff >>${tag}.out
 fi
