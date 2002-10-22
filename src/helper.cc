677ca55974425776cd26f64cf23de48df095d014
hno <Unknown>
2002-10-22 15:40:29 +0000

    Added a level 9 debug statement dumping the helper receive buffer to ease debugging of helper interactions

--- src/helper.cc
+++ src/helper.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: helper.cc,v 1.52 2002/10/21 22:17:32 hno Exp $
+ * $Id: helper.cc,v 1.53 2002/10/22 09:40:29 hno Exp $
  *
  * DEBUG: section 84    Helper process maintenance
  * AUTHOR: Harvest Derived?
@@ -722,6 +722,7 @@ helperHandleRead(int fd, char *buf, size_t len, comm_err_t flag, int xerrno, voi
     }
     srv->offset += len;
     srv->buf[srv->offset] = '\0';
+    debug(84, 9) ("helperHandleRead: '%s'\n", srv->rbuf);
     r = srv->request;
     if (r == NULL) {
 	/* someone spoke without being spoken to */
