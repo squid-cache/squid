f7628261592b18d3a15315b6269e73bb7dae9a0b
robertc <Unknown>
2002-10-25 09:57:30 +0000

    oops

---
+++ src/Generic.h
@@ -0,0 +1,60 @@
+
+/*
+ * $Id: Generic.h,v 1.2 2002/10/25 03:57:30 robertc Exp $
+ *
+ *
+ * SQUID Web Proxy Cache          http://www.squid-cache.org/
+ * ----------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from
+ *  the Internet community; see the CONTRIBUTORS file for full
+ *  details.   Many organizations have provided support for Squid's
+ *  development; see the SPONSORS file for full details.  Squid is
+ *  Copyrighted (C) 2001 by the Regents of the University of
+ *  California; see the COPYRIGHT file for full details.  Squid
+ *  incorporates software developed and/or copyrighted by other
+ *  sources; see the CREDITS file for full details.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ *
+ */
+
+#ifndef SQUID_GENERIC_H
+#define SQUID_GENERIC_H
+
+template <class _Arg, class _Result>
+struct unary_function {
+    typedef _Arg argument_type;
+    typedef _Result result_type;
+};
+
+template <class L, class T>
+T& for_each(L const &head, T& visitor)
+{
+    for (L const *node = &head; node; node=node->next)
+	visitor(*node);
+    return visitor;
+}
+
+
+template <class T>
+T& for_each(dlink_list const &collection, T& visitor)
+{
+    for (dlink_node const *node = collection.head; node; node=node->next)
+	visitor(*(typename T::argument_type const *)node->data);
+    return visitor;
+}
+
+#endif /* SQUID_GENERIC_H */
