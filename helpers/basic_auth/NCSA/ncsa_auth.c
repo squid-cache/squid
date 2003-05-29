363a8ce86aa89ce6105fe667163ff7e77fd6ae21
hno <>
2003-05-29 04:42:32 +0000

    Imported MD5 hash support for NCSA auth, supporting both shadow and Apache htpasswd MD5 hases (same algorithms, different permutation strings)

--- helpers/basic_auth/NCSA/ncsa_auth.c
+++ helpers/basic_auth/NCSA/ncsa_auth.c
@@ -42,6 +42,7 @@
 
 #include "util.h"
 #include "hash.h"
+#include "crypt_md5.h"
 
 static hash_table *hash = NULL;
 static HASHFREE my_free;
@@ -135,10 +136,12 @@ main(int argc, char **argv)
 	u = (user_data *)hash_lookup(hash, user);
 	if (u == NULL) {
 	    printf("ERR\n");
-	} else if (strcmp(u->passwd, (char *) crypt(passwd, u->passwd))) {
-	    printf("ERR\n");
-	} else {
+	} else if (strcmp(u->passwd, (char *) crypt(passwd, u->passwd)) == 0) {
 	    printf("OK\n");
+	} else if (strcmp(u->passwd, (char *) crypt_md5(passwd, u->passwd)) == 0) {
+	    printf("OK\n");
+	} else {
+	    printf("ERR\n");
 	}
     }
     exit(0);
