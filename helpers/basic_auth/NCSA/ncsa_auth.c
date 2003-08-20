d80d94ab2afde2415dc02bd6166860dec1d9df4b
hno <Unknown>
2003-08-20 18:59:24 +0000

    Bug #733: No explicit error message when ncsa_auth (squid user) can't access passwd file

--- helpers/basic_auth/NCSA/ncsa_auth.c
+++ helpers/basic_auth/NCSA/ncsa_auth.c
@@ -81,6 +81,10 @@ read_passwd_file(const char *passwdfile)
 	exit(1);
     }
     f = fopen(passwdfile, "r");
+    if (NULL == f) {
+	fprintf(stderr, "%s: %s\n", passwdfile, xstrerror());
+	exit(1);
+    }
     while (fgets(buf, 8192, f) != NULL) {
 	if ((buf[0] == '#') || (buf[0] == ' ') || (buf[0] == '\t') ||
 	    (buf[0] == '\n'))
