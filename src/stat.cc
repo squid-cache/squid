5bf47d994274e0bf471cb37efc7fd26e1fe9c64d
robertc <Unknown>
2004-12-21 01:48:58 +0000

    Bug #103: Use meaningful labels in the hit/miss statistics (via hno)

--- src/stat.cc
+++ src/stat.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: stat.cc,v 1.384 2004/12/20 16:30:36 robertc Exp $
+ * $Id: stat.cc,v 1.385 2004/12/20 18:48:58 robertc Exp $
  *
  * DEBUG: section 18    Cache Manager Statistics
  * AUTHOR: Harvest Derived
@@ -517,19 +517,19 @@ info_get(StoreEntry * sentry)
     storeAppendPrintf(sentry, "Cache information for %s:\n",
                       appname);
 
-    storeAppendPrintf(sentry, "\tRequest Hit Ratios:\t5min: %3.1f%%, 60min: %3.1f%%\n",
+    storeAppendPrintf(sentry, "\tHits as % of all requests:\t5min: %3.1f%%, 60min: %3.1f%%\n",
                       statRequestHitRatio(5),
                       statRequestHitRatio(60));
 
-    storeAppendPrintf(sentry, "\tByte Hit Ratios:\t5min: %3.1f%%, 60min: %3.1f%%\n",
+    storeAppendPrintf(sentry, "\tHits as % of bytes sent:\t5min: %3.1f%%, 60min: %3.1f%%\n",
                       statByteHitRatio(5),
                       statByteHitRatio(60));
 
-    storeAppendPrintf(sentry, "\tRequest Memory Hit Ratios:\t5min: %3.1f%%, 60min: %3.1f%%\n",
+    storeAppendPrintf(sentry, "\tMemory hits as % of hit requests:\t5min: %3.1f%%, 60min: %3.1f%%\n",
                       statRequestHitMemoryRatio(5),
                       statRequestHitMemoryRatio(60));
 
-    storeAppendPrintf(sentry, "\tRequest Disk Hit Ratios:\t5min: %3.1f%%, 60min: %3.1f%%\n",
+    storeAppendPrintf(sentry, "\tDisk hits as % of hit requests:\t5min: %3.1f%%, 60min: %3.1f%%\n",
                       statRequestHitDiskRatio(5),
                       statRequestHitDiskRatio(60));
 
