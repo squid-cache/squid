3df13e009462516b392c49cd66a5e1f2da7cd58c
hno <Unknown>
2005-03-19 00:17:51 +0000

    From ssl-2.5 2004/12/02 00:53:40
    %USER_CERTCHAIN external_acl_type tag, returning the complete client SSL
    certificate chain

--- src/ssl_support.cc
+++ src/ssl_support.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: ssl_support.cc,v 1.29 2005/03/18 17:12:34 hno Exp $
+ * $Id: ssl_support.cc,v 1.30 2005/03/18 17:17:51 hno Exp $
  *
  * AUTHOR: Benno Rice
  * DEBUG: section 83    SSL accelerator support
@@ -1016,6 +1016,44 @@ sslGetUserCertificatePEM(SSL *ssl)
     str[len] = '\0';
 
     X509_free(cert);
+
+    BIO_free(mem);
+
+    return str;
+}
+
+const char *
+sslGetUserCertificateChainPEM(SSL *ssl)
+{
+    STACK_OF(X509) *chain;
+    BIO *mem;
+    static char *str = NULL;
+    char *ptr;
+    long len;
+    int i;
+
+    safe_free(str);
+
+    if (!ssl)
+        return NULL;
+
+    chain = SSL_get_peer_cert_chain(ssl);
+
+    if (!chain)
+        return sslGetUserCertificatePEM(ssl);
+
+    mem = BIO_new(BIO_s_mem());
+
+    for (i = 0; i < sk_X509_num(chain); i++) {
+        X509 *cert = sk_X509_value(chain, i);
+        PEM_write_bio_X509(mem, cert);
+    }
+
+    len = BIO_get_mem_data(mem, &ptr);
+
+    str = (char *)xmalloc(len + 1);
+    memcpy(str, ptr, len);
+    str[len] = '\0';
 
     BIO_free(mem);
 
