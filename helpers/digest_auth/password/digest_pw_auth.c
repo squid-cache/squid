0fd205780bbcbdd47190c50e6aba3b71ec17b0df
serassio <>
2005-04-25 00:25:24 +0000

    Bug #1223: Make the use of the %m error page to return auth info messages
    This patch extends the helper protocols for Basic and Digest to provide
    some basic information in error responses, and makes use of the error
    response already included in the NTLM helper protocol, making these
    messages available as %m in error pages. Can be used if desired to
    indicate why a login failed. The exact messages returned is helper
    dependent.
    
    Forward port of 2.5 patch.

--- helpers/digest_auth/password/digest_pw_auth.c
+++ helpers/digest_auth/password/digest_pw_auth.c
@@ -54,7 +54,7 @@ OutputHHA1(RequestData *requestData)
     requestData->error = 0;
     GetHHA1(requestData);
     if (requestData->error) {    
-	printf("ERR\n");
+	printf("ERR No such user\n");
 	return;
     }
     printf("%s\n", requestData->HHA1);
