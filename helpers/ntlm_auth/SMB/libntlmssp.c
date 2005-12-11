ef78d119d1b8c6ad7352cd9ca9ddfb7701d53223
serassio <Unknown>
2005-12-11 17:00:47 +0000

    Bug #1022: bad credentials fetch (no user)
    This problem is triggered only when using the --enable-ntlm-fail-open
    configure option.

--- helpers/ntlm_auth/SMB/libntlmssp.c
+++ helpers/ntlm_auth/SMB/libntlmssp.c
@@ -178,14 +178,17 @@ fetch_credentials(ntlm_authenticate * auth, int auth_length)
     char *p = credentials;
     lstring tmp;
     tmp = ntlm_fetch_string((char *) auth, auth_length, &auth->domain);
+    *p = '\0';
     if (tmp.str == NULL)
 	return NULL;
     memcpy(p, tmp.str, tmp.l);
     p += tmp.l;
     *p++ = '\\';
+    *p = '\0';
     tmp = ntlm_fetch_string((char *) auth, auth_length, &auth->user);
     if (tmp.str == NULL)
 	return NULL;
+    memcpy(p, tmp.str, tmp.l);
     *(p + tmp.l) = '\0';
     return credentials;
 }
