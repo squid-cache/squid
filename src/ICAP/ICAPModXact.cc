7f2de71e5860f7ecab488d796630b6409d3be9e4
wessels <Unknown>
2005-12-23 04:29:22 +0000

    RFC 3507 says that there should be a space after comma in the Encapsulated list.

--- src/ICAP/ICAPModXact.cc
+++ src/ICAP/ICAPModXact.cc
@@ -958,7 +958,7 @@ void ICAPModXact::makeRequestHeaders(MemBuf &buf)
 void ICAPModXact::encapsulateHead(MemBuf &icapBuf, const char *section, MemBuf &httpBuf, const HttpMsg *head)
 {
     // update ICAP header
-    icapBuf.Printf("%s=%d,", section, (int) httpBuf.contentSize());
+    icapBuf.Printf("%s=%d, ", section, (int) httpBuf.contentSize());
 
     // pack HTTP head
     packHead(httpBuf, head);
