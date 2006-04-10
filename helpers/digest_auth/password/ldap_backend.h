faadb1b64436077d7f3b02e30cb9684b1c9ad075
hno <>
2006-04-10 14:44:52 +0000

    removed obsolete ldap files. These was moved to the ldap helper directory

--- helpers/digest_auth/password/ldap_backend.h
+++
@@ -1,9 +0,0 @@
-/*
- * text_backend.h
- *
- * AUTHOR: Flavio Pescuma. <flavio@marasystems.com>
- *
- */
-#include "digest_common.h"
-extern int LDAPArguments(int argc, char **argv);
-extern void LDAPHHA1(RequestData * requestData);
