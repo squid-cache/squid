bd0e1138e4a1a228affa8f8471f71a51b5b07ff4
robertc <Unknown>
2006-04-22 19:18:37 +0000

    More missing files.

---
+++ src/tests/testBoilerplate.cc
@@ -0,0 +1,13 @@
+#include "squid.h"
+#include <stdexcept>
+
+#include "testBoilerplate.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION( testBoilerplate );
+
+
+void
+testBoilerplate::testDemonstration()
+{
+    CPPUNIT_ASSERT(0 == 0);
+}
