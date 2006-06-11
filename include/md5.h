21532d2da3e9ca98bc007a167989b53e5e6440ae
hno <Unknown>
2006-06-11 19:29:00 +0000

    Bug #1490: GPL license issues with RSA MD5 implementation
    replaced the Squid provided MD5 with another public domain copy not
    encumbered by the RSA license.

--- include/md5.h
+++ include/md5.h
@@ -1,7 +1,3 @@
-/*
- * $Id: md5.h,v 1.15 2003/01/23 00:36:47 robertc Exp $
- */
-
 #ifndef SQUID_MD5_H
 #define SQUID_MD5_H
 
@@ -27,46 +23,47 @@
 
 #else /* USE_OPENSSL */
 
-/* MD5.H - header file for MD5C.C
+/*
+ * This is the header file for the MD5 message-digest algorithm.
+ * The algorithm is due to Ron Rivest.  This code was
+ * written by Colin Plumb in 1993, no copyright is claimed.
+ * This code is in the public domain; do with it what you wish.
+ *
+ * Equivalent code is available from RSA Data Security, Inc.
+ * This code has been tested against that, and is equivalent,
+ * except that you don't need to include two pages of legalese
+ * with every copy.
+ *
+ * To compute the message digest of a chunk of bytes, declare an
+ * MD5Context structure, pass it to MD5Init, call MD5Update as
+ * needed on buffers full of bytes, and then call MD5Final, which
+ * will fill a supplied 16-byte array with the digest.
+ *
+ * Changed so as no longer to depend on Colin Plumb's `usual.h'
+ * header definitions; now uses stuff from dpkg's config.h
+ *  - Ian Jackson <ian@chiark.greenend.org.uk>.
+ * Still in the public domain.
+ *
+ * Changed MD5Update to take a void * for easier use and some other
+ * minor cleanup. - Henrik Nordstrom <henrik@henriknordstrom.net>.
+ * Still in the public domain.
+ *
  */
 
-/* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
- * rights reserved.
- * 
- * License to copy and use this software is granted provided that it
- * is identified as the "RSA Data Security, Inc. MD5 Message-Digest
- * Algorithm" in all material mentioning or referencing this software
- * or this function.
- * 
- * License is also granted to make and use derivative works provided
- * that such works are identified as "derived from the RSA Data
- * Security, Inc. MD5 Message-Digest Algorithm" in all material
- * mentioning or referencing the derived work.
- * 
- * RSA Data Security, Inc. makes no representations concerning either
- * the merchantability of this software or the suitability of this
- * software for any particular purpose. It is provided "as is"
- * without express or implied warranty of any kind.
- * 
- * These notices must be retained in any copies of any part of this
- * documentation and/or software.
- */
+#include "squid_types.h"
 
-#include "config.h"
-
-/* MD5 context. */
-typedef struct {
-    u_int32_t state[4];		/* state (ABCD) */
-    u_int32_t count[2];		/* number of bits, modulo 2^64 (lsb first) */
-    unsigned char buffer[64];	/* input buffer */
+typedef struct MD5Context {
+    uint32_t buf[4];
+    uint32_t bytes[2];
+    uint32_t in[16];
 } MD5_CTX;
 
-SQUIDCEXTERN void MD5Init(MD5_CTX *);
-SQUIDCEXTERN void MD5Update(MD5_CTX *, const void *, unsigned long);
-SQUIDCEXTERN void MD5Final(unsigned char *, MD5_CTX *);
+SQUIDCEXTERN void MD5Init(struct MD5Context *context);
+SQUIDCEXTERN void MD5Update(struct MD5Context *context, const void *buf, unsigned len);
+SQUIDCEXTERN void MD5Final(uint8_t digest[16], struct MD5Context *context);
+SQUIDCEXTERN void MD5Transform(uint32_t buf[4], uint32_t const in[16]);
 
 #define MD5_DIGEST_CHARS         16
 
 #endif /* USE_OPENSSL */
-
 #endif /* SQUID_MD5_H */
