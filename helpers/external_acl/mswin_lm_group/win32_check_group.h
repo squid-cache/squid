e6b7397ea5226b451a10de3f7e96bcb0f981e7be
serassio <Unknown>
2006-07-05 21:26:47 +0000

    Ran indent

--- helpers/external_acl/mswin_lm_group/win32_check_group.h
+++ helpers/external_acl/mswin_lm_group/win32_check_group.h
@@ -50,14 +50,14 @@ extern char debug_enabled;
 #else /* __GNUC__ */
 extern char debug_enabled;
 static void
-debug(char *format,...) 
+debug(char *format,...)
 {
 #ifdef DEBUG
 #ifdef _SQUID_MSWIN_
     if (debug_enabled) {
 	va_list args;
 
-	va_start(args,format);
+	va_start(args, format);
 	fprintf(stderr, "%s[%d]: ", myname, mypid);
 	vfprintf(stderr, format, args);
 	fprintf(stderr, "\n");
