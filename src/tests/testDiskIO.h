ade1f679e00ac1e87397442e8ae79b8a50d0c0ba
robertc <Unknown>
2006-09-14 14:34:51 +0000

    Add missing files from bugfix for COSS config-failures.

---
+++ src/tests/testDiskIO.h
@@ -0,0 +1,24 @@
+
+#ifndef SQUID_SRC_TEST_DISKIO_H
+#define SQUID_SRC_TEST_DISKIO_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+/*
+ * test the DiskIO framework
+ */
+
+class testDiskIO : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE( testDiskIO );
+    CPPUNIT_TEST( testFindDefault );
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+protected:
+    void testFindDefault();
+};
+
+#endif
+
