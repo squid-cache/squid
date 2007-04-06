979f9f7fc6fd18d0315ca2b8ceac5747bb365d53
rousskov <Unknown>
2007-04-06 10:45:07 +0000

    - Added basic support for asynchronous calls. The calls are 	  implemented using Squid events, at least for now. The 	  provided functions and macros provide simple debugging at the 	  call scheduling and execution times. Macros are used to log 	  the location of the call and the call handler. For a simple 	  usage sketch, please see AsyncCall.cc.
    	  It may be possible to customize call scheduling and debugging
    	  and in a class context, but I have not tested that theory yet.
    
    	  The code was inspired by ICAP/MsgPipe implementation, that is
    	  being deleted for reasons unrelated to asynchronous calling.

---
+++ src/AsyncCall.cc
@@ -0,0 +1,47 @@
+#include "squid.h"
+#include "AsyncCall.h"
+
+void scheduleAsyncCall(int debugSection, int debugLevel,
+    const char *fileName, int fileLine, void *objectPtr, const char *callName,
+    EVH *wrapper)
+{
+    debugs(debugSection, debugLevel, fileName << "(" << fileLine <<
+        ") will call " << callName << '(' << objectPtr << ')');
+    eventAdd(callName, wrapper, objectPtr, 0.0, 0, true);
+}
+
+bool enterAsyncCallWrapper(int debugSection, int debugLevel,
+    void *objectPtr, const char *className, const char *methodName)
+{
+    assert(objectPtr);
+    debugs(debugSection, debugLevel, "entering " << className << "::" <<
+        methodName << '(' << objectPtr << ')');
+    return true;
+}
+
+void exitAsyncCallWrapper(int debugSection, int debugLevel,
+    void *objectPtr, const char *className, const char *methodName)
+{
+    debugs(debugSection, debugLevel, "exiting " << className << "::" <<
+        methodName << '(' << objectPtr << ')');
+}
+
+
+#if USAGE_SKETCH
+
+class TestClass {
+    public:
+        virtual ~TestClass();
+
+        virtual void testMethod(); // does not have to be virtual
+        AsyncCallWrapper(0,0, TestClass, testMethod) // define a wrapper
+
+    private:
+        CBDATA_CLASS2(TestClass);
+};
+
+void testCase(TestClass *c) {
+    AsyncCall(0,0, &c, TestClass::testMethod); // make an async call to c
+}
+
+#endif
