1b39caaa84db70bcd40fff82d3f98bcaf1715e61
rousskov <>
2007-04-06 10:48:25 +0000

    - Replaced BodyReader with BodyPipe. BodyReader was a 	  collection of function pointers augmented with body size 	  calculation logic. BodyReader was used to deliver request 	  body (of a known size) from the client side to the server 	  side. Reference counting was used to communicate abort 	  conditions to the other side (it did not work well because 	  decreasing the reference count does not have any side-effects 	  if the count remains positive). Direct calls between sides 	  sometimes resulted in a call-me-when-I-am-calling-you "loops" 	  and related bugs.
    	  BodyPipe is used to deliver request or response body (possibly
    	  of unknown size) from the body producer to the body consumer.
    	  A producer can be the client side (for virgin requests), the
    	  server side (for virgin replies), or the ICAP side (for
    	  adapted messages). A consumer can be the client side (for
    	  adapted responses, including responses in a request
    	  satisfaction mode), the server side (for adapted requests),
    	  and the ICAP side (for virgin requests and responses).
    
    	  BodyPipe uses asynchronous calls for communication between
    	  sides to avoid call-me-when-I-am-calling-you "loops".
    
    	  BodyPipe has methods to communicate normal termination and
    	  abort conditions to the other side. The use of those methods
    	  is mandatory. Reference counting is used only as a garbage
    	  collection mechanism.
    
    	  BodyPipe is used to read request bodies, including requests
    	  for which there is no consumer and the connection is in a
    	  'closing' state. BodyPipe can auto-consume body so that a
    	  'closing' connection does not have to rely on the body
    	  consumer presence when eating up remaining body data.
    
    	  If auto-consumption is turned on and the pipe starts
    	  consuming before a real consumer is attached to the pipe, the
    	  setConsumerIfNotLate call fails, and the real consumer has to
    	  handle the failure.
    
    	  The new BodyPipe approach should make support for HTTP/1.1
    	  chunked requests easier. Only a few places in the pipe-related
    	  code assume that the request size is known.
    
    	- Removed ClientBody as unused, replaced by BodyReader, then
    	  BodyPipe.

---
+++ src/BodyPipe.cc
@@ -0,0 +1,355 @@
+
+#include "squid.h"
+#include "BodyPipe.h"
+
+CBDATA_CLASS_INIT(BodyPipe);
+
+// inform the pipe that we are done and clear the Pointer
+void BodyProducer::stopProducingFor(RefCount<BodyPipe> &pipe, bool atEof)
+{
+	debugs(91,7, HERE << this << " will not produce for " << pipe <<
+		"; atEof: " << atEof);
+	assert(pipe != NULL); // be strict: the caller state may depend on this
+	pipe->clearProducer(atEof);
+	pipe = NULL;
+}
+
+// inform the pipe that we are done and clear the Pointer
+void BodyConsumer::stopConsumingFrom(RefCount<BodyPipe> &pipe)
+{
+	debugs(91,7, HERE << this << " will not consume from " << pipe);
+	assert(pipe != NULL); // be strict: the caller state may depend on this
+	pipe->clearConsumer();
+	pipe = NULL;
+}
+
+/* BodyPipe */
+
+BodyPipe::BodyPipe(Producer *aProducer): theBodySize(-1),
+	theProducer(aProducer), theConsumer(0),
+	thePutSize(0), theGetSize(0), mustAutoConsume(false), isCheckedOut(false)
+{
+	// TODO: teach MemBuf to start with zero minSize
+	// TODO: limit maxSize by theBodySize, when known?
+	theBuf.init(2*1024, MaxCapacity); 
+	debugs(91,7, HERE << "created BodyPipe" << status());
+}
+
+BodyPipe::~BodyPipe()
+{
+	debugs(91,7, HERE << "destroying BodyPipe" << status());
+	assert(!theProducer);
+	assert(!theConsumer);
+	theBuf.clean();
+}
+
+void BodyPipe::setBodySize(size_t aBodySize)
+{
+	assert(!bodySizeKnown());
+	assert(aBodySize >= 0);
+	assert(thePutSize <= aBodySize);
+
+	// If this assert fails, we need to add code to check for eof and inform
+	// the consumer about the eof condition via scheduleBodyEndNotification,
+	// because just setting a body size limit may trigger the eof condition.
+	assert(!theConsumer); 
+
+	theBodySize = aBodySize;
+	debugs(91,7, HERE << "set body size" << status());
+}
+
+size_t BodyPipe::bodySize() const
+{
+	assert(bodySizeKnown());
+	return static_cast<size_t>(theBodySize);
+}
+
+bool BodyPipe::expectMoreAfter(size_t offset) const
+{
+	assert(theGetSize <= offset);
+	return offset < thePutSize || // buffer has more now or
+		(!productionEnded() && mayNeedMoreData()); // buffer will have more
+}
+
+bool BodyPipe::exhausted() const
+{
+	return !expectMoreAfter(theGetSize);
+}
+
+size_t BodyPipe::unproducedSize() const
+{
+	return bodySize() - thePutSize; // bodySize() asserts that size is known
+}
+
+void
+BodyPipe::clearProducer(bool atEof)
+{
+	if (theProducer) {
+		debugs(91,7, HERE << "clearing BodyPipe producer" << status());
+		theProducer = NULL;
+		if (atEof) {
+			if (!bodySizeKnown())
+				theBodySize = thePutSize;
+			else
+			if (bodySize() != thePutSize)
+				debugs(91,1, HERE << "aborting on premature eof" << status());
+		} else {
+			// asserta that we can detect the abort if the consumer joins later
+			assert(!bodySizeKnown() || bodySize() != thePutSize);
+		}
+		scheduleBodyEndNotification();
+	}
+}
+
+size_t
+BodyPipe::putMoreData(const char *buf, size_t size)
+{
+	if (bodySizeKnown())
+		size = XMIN(size, unproducedSize());
+
+	const size_t spaceSize = static_cast<size_t>(theBuf.potentialSpaceSize());
+	if ((size = XMIN(size, spaceSize))) {
+		theBuf.append(buf, size);
+		postAppend(size);
+		return size;
+	}
+	return 0;
+}
+
+bool
+BodyPipe::setConsumerIfNotLate(Consumer *aConsumer)
+{
+	assert(!theConsumer);
+	assert(aConsumer);
+
+	// TODO: convert this into an exception and remove IfNotLate suffix
+	// If there is something consumed already, we are in an auto-consuming mode
+	// and it is too late to attach a real consumer to the pipe.
+	if (theGetSize > 0) {
+		assert(mustAutoConsume);
+		return false;
+	}
+
+	theConsumer = aConsumer;
+	debugs(91,7, HERE << "set consumer" << status());
+	if (theBuf.hasContent())
+		AsyncCall(91,5, this, BodyPipe::tellMoreBodyDataAvailable);
+	if (!theProducer)
+		scheduleBodyEndNotification();
+
+	return true;
+}
+
+void
+BodyPipe::clearConsumer() {
+	if (theConsumer) {
+		debugs(91,7, HERE << "clearing consumer" << status());
+		theConsumer = NULL;
+		if (!exhausted())
+			AsyncCall(91,5, this, BodyPipe::tellBodyConsumerAborted);
+	}
+}
+
+size_t
+BodyPipe::getMoreData(MemBuf &buf)
+{
+	if (!theBuf.hasContent())
+		return 0; // did not touch the possibly uninitialized buf
+
+	if (buf.isNull())
+		buf.init();
+	const size_t size = XMIN(theBuf.contentSize(), buf.potentialSpaceSize());
+	buf.append(theBuf.content(), size);
+	theBuf.consume(size);
+	postConsume(size);
+	return size; // cannot be zero if we called buf.init above
+}
+
+void
+BodyPipe::consume(size_t size)
+{
+	theBuf.consume(size);
+	postConsume(size);
+}
+
+void
+BodyPipe::enableAutoConsumption() {
+	mustAutoConsume = true;
+	debugs(91,5, HERE << "enabled auto consumption" << status());
+	if (!theConsumer && theBuf.hasContent())
+		AsyncCall(91,5, this, BodyPipe::tellMoreBodyDataAvailable);
+}
+
+MemBuf &
+BodyPipe::checkOut() {
+	assert(!isCheckedOut);
+	isCheckedOut = true;
+	return theBuf;	
+}
+
+void
+BodyPipe::checkIn(Checkout &checkout)
+{
+	assert(isCheckedOut);
+	isCheckedOut = false;
+	const size_t currentSize = theBuf.contentSize();
+	if (checkout.checkedOutSize > currentSize)
+		postConsume(checkout.checkedOutSize - currentSize);
+	else
+	if (checkout.checkedOutSize < currentSize)
+		postAppend(currentSize - checkout.checkedOutSize);
+}
+
+void
+BodyPipe::undoCheckOut(Checkout &checkout)
+{
+	assert(isCheckedOut);
+	const size_t currentSize = theBuf.contentSize();
+	// We can only undo if size did not change, and even that carries
+	// some risk. If this becomes a problem, the code checking out
+	// raw buffers should always check them in (possibly unchanged)
+	// instead of relying on the automated undo mechanism of Checkout.
+	// The code can always use a temporary buffer to accomplish that.
+	assert(checkout.checkedOutSize == currentSize);
+}
+
+// TODO: Optimize: inform consumer/producer about more data/space only if
+// they used the data/space since we notified them last time.
+
+void
+BodyPipe::postConsume(size_t size) {
+	assert(!isCheckedOut);
+	theGetSize += size;
+	debugs(91,7, HERE << "consumed " << size << " bytes" << status());
+	if (mayNeedMoreData())
+		AsyncCall(91,5, this, BodyPipe::tellMoreBodySpaceAvailable);
+}
+
+void
+BodyPipe::postAppend(size_t size) {
+	assert(!isCheckedOut);
+	thePutSize += size;
+	debugs(91,7, HERE << "added " << size << " bytes" << status());
+
+	// We should not consume here even if mustAutoConsume because the
+	// caller may not be ready for the data to be consumed during this call.
+	AsyncCall(91,5, this, BodyPipe::tellMoreBodyDataAvailable);
+
+	if (!mayNeedMoreData())
+		clearProducer(true); // reached end-of-body
+}
+
+
+void
+BodyPipe::scheduleBodyEndNotification()
+{
+	if (bodySizeKnown() && bodySize() == thePutSize)
+		AsyncCall(91,5, this, BodyPipe::tellBodyProductionEnded);
+	else
+		AsyncCall(91,5, this, BodyPipe::tellBodyProducerAborted);
+}
+
+void BodyPipe::tellMoreBodySpaceAvailable()
+{
+	if (theProducer != NULL)
+		theProducer->noteMoreBodySpaceAvailable(*this);
+}
+
+void BodyPipe::tellBodyConsumerAborted()
+{
+	if (theProducer != NULL)
+		theProducer->noteBodyConsumerAborted(*this);
+}
+
+void BodyPipe::tellMoreBodyDataAvailable()
+{
+	if (theConsumer != NULL)
+		theConsumer->noteMoreBodyDataAvailable(*this);
+	else
+	if (mustAutoConsume && theBuf.hasContent())
+		consume(theBuf.contentSize());
+}
+
+void BodyPipe::tellBodyProductionEnded()
+{
+	if (theConsumer != NULL)
+		theConsumer->noteBodyProductionEnded(*this);
+}
+
+void BodyPipe::tellBodyProducerAborted()
+{
+	if (theConsumer != NULL)
+		theConsumer->noteBodyProducerAborted(*this);
+}
+
+// a short temporary string describing buffer status for debugging
+const char *BodyPipe::status() const
+{
+    static MemBuf buf;
+    buf.reset();
+
+    buf.append(" [", 2);
+
+	buf.Printf("%d<=%d", (int)theGetSize, (int)thePutSize);
+    if (theBodySize >= 0)
+        buf.Printf("<=%d", (int)theBodySize);
+	else
+		buf.append("<=?", 3);
+
+	buf.Printf(" %d+%d", (int)theBuf.contentSize(), (int)theBuf.spaceSize());
+
+	buf.Printf(" pipe%p", this);
+    if (theProducer)
+        buf.Printf(" prod%p", theProducer);
+    if (theConsumer)
+        buf.Printf(" cons%p", theConsumer);
+
+	if (mustAutoConsume)
+		buf.append(" A", 2);
+	if (isCheckedOut)
+		buf.append(" L", 2); // Locked
+
+    buf.append("]", 1);
+
+    buf.terminate();
+
+    return buf.content();
+}
+
+
+/* BodyPipeCheckout */
+
+BodyPipeCheckout::BodyPipeCheckout(BodyPipe &aPipe): pipe(aPipe),
+	buf(aPipe.checkOut()), offset(aPipe.consumedSize()),
+	checkedOutSize(buf.contentSize()), checkedIn(false)
+{
+}
+
+BodyPipeCheckout::~BodyPipeCheckout()
+{
+	if (!checkedIn)
+		pipe.undoCheckOut(*this);
+}
+
+void
+BodyPipeCheckout::checkIn()
+{
+	assert(!checkedIn);
+	pipe.checkIn(*this);
+	checkedIn = true;
+}
+
+
+BodyPipeCheckout::BodyPipeCheckout(const BodyPipeCheckout &c): pipe(c.pipe),
+	buf(c.buf), offset(c.offset), checkedOutSize(c.checkedOutSize),
+	checkedIn(c.checkedIn)
+{
+	assert(false); // prevent copying
+}
+
+BodyPipeCheckout &
+BodyPipeCheckout::operator =(const BodyPipeCheckout &)
+{
+	assert(false); // prevent assignment
+	return *this;
+}
