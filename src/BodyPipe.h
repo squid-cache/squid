880587f5968169c1dd333d74e9cc0f9d006ff1ef
rousskov <Unknown>
2007-04-06 10:48:25 +0000

    - Replaced BodyReader with BodyPipe. BodyReader was a 	  collection of function pointers augmented with body size 	  calculation logic. BodyReader was used to deliver request 	  body (of a known size) from the client side to the server 	  side. Reference counting was used to communicate abort 	  conditions to the other side (it did not work well because 	  decreasing the reference count does not have any side-effects 	  if the count remains positive). Direct calls between sides 	  sometimes resulted in a call-me-when-I-am-calling-you "loops" 	  and related bugs.
    	  BodyPipe is used to deliver request or response body (possibly
    	  of unknown size) from the body producer to the body consumer.
    	  A producer can be the client side (for virgin requests), the
    	  server side (for virgin replies), or the ICAP side (for
    	  adapted messages). A consumer can be the client side (for
    	  adapted responses, including responses in a request
    	  satisfaction mode), the server side (for adapted requests),
    	  and the ICAP side (for virgin requests and responses).
    
    	  BodyPipe uses asynchronous calls for communication between
    	  sides to avoid call-me-when-I-am-calling-you "loops".
    
    	  BodyPipe has methods to communicate normal termination and
    	  abort conditions to the other side. The use of those methods
    	  is mandatory. Reference counting is used only as a garbage
    	  collection mechanism.
    
    	  BodyPipe is used to read request bodies, including requests
    	  for which there is no consumer and the connection is in a
    	  'closing' state. BodyPipe can auto-consume body so that a
    	  'closing' connection does not have to rely on the body
    	  consumer presence when eating up remaining body data.
    
    	  If auto-consumption is turned on and the pipe starts
    	  consuming before a real consumer is attached to the pipe, the
    	  setConsumerIfNotLate call fails, and the real consumer has to
    	  handle the failure.
    
    	  The new BodyPipe approach should make support for HTTP/1.1
    	  chunked requests easier. Only a few places in the pipe-related
    	  code assume that the request size is known.
    
    	- Removed ClientBody as unused, replaced by BodyReader, then
    	  BodyPipe.

---
+++ src/BodyPipe.h
@@ -0,0 +1,158 @@
+
+#ifndef SQUID_BODY_PIPE_H
+#define SQUID_BODY_PIPE_H
+
+#include "MemBuf.h"
+#include "AsyncCall.h"
+
+class BodyPipe;
+
+// Interface for those who want to produce body content for others.
+// BodyProducer is expected to create the BodyPipe.
+// One pipe cannot have more than one producer.
+class BodyProducer {
+	public:
+		virtual ~BodyProducer() {}
+
+		virtual void noteMoreBodySpaceAvailable(BodyPipe &bp) = 0;
+		virtual void noteBodyConsumerAborted(BodyPipe &bp) = 0;
+
+	protected:
+		void stopProducingFor(RefCount<BodyPipe> &pipe, bool atEof);
+};
+
+// Interface for those who want to consume body content from others.
+// BodyConsumer is expected to register with an existing BodyPipe
+// by calling BodyPipe::setConsumer().
+// One pipe cannot have more than one consumer.
+class BodyConsumer {
+	public:
+		virtual ~BodyConsumer() {}
+
+		virtual void noteMoreBodyDataAvailable(BodyPipe &bp) = 0;
+		virtual void noteBodyProductionEnded(BodyPipe &bp) = 0;
+		virtual void noteBodyProducerAborted(BodyPipe &bp) = 0;
+
+	protected:
+		void stopConsumingFrom(RefCount<BodyPipe> &pipe);
+};
+
+// Makes raw buffer checkin/checkout interface efficient and exception-safe.
+// Either append or consume operations can be performed on a checkedout buffer.
+class BodyPipeCheckout {
+	public:
+		friend class BodyPipe;
+
+	public:
+		BodyPipeCheckout(BodyPipe &pipe); // checks out
+		~BodyPipeCheckout(); // aborts checkout unless checkedIn
+
+		void checkIn();
+
+	public:
+		BodyPipe &pipe;
+		MemBuf &buf;
+		const size_t offset; // of current content, relative to the body start
+
+	protected:
+		const size_t checkedOutSize;
+		bool checkedIn;
+
+	private:
+		BodyPipeCheckout(const BodyPipeCheckout &); // prevent copying
+		BodyPipeCheckout &operator =(const BodyPipeCheckout &); // prevent assignment
+};
+
+// Connects those who produces message body content with those who
+// consume it. For example, connects ConnStateData with FtpStateData OR
+// ICAPModXact with HttpStateData.
+class BodyPipe: public RefCountable {
+	public:
+		typedef RefCount<BodyPipe> Pointer;
+		typedef BodyProducer Producer;
+		typedef BodyConsumer Consumer;
+		typedef BodyPipeCheckout Checkout;
+
+		enum { MaxCapacity = SQUID_TCP_SO_RCVBUF };
+
+		friend class BodyPipeCheckout;
+
+	public:
+		BodyPipe(Producer *aProducer);
+		~BodyPipe(); // asserts that producer and consumer are cleared
+
+		void setBodySize(size_t aSize); // set body size
+		bool bodySizeKnown() const { return theBodySize >= 0; }
+		size_t bodySize() const;
+		size_t consumedSize() const { return theGetSize; }
+		bool productionEnded() const { return !theProducer; }
+
+		// called by producers
+		void clearProducer(bool atEof); // aborts or sends eof
+		size_t putMoreData(const char *buf, size_t size);
+		bool mayNeedMoreData() const { return !bodySizeKnown() || needsMoreData(); }
+		bool needsMoreData() const { return bodySizeKnown() && unproducedSize() > 0; }
+		size_t unproducedSize() const; // size of still unproduced data
+
+		// called by consumers
+		bool setConsumerIfNotLate(Consumer *aConsumer);
+		void clearConsumer(); // aborts if still piping
+		size_t getMoreData(MemBuf &buf);
+		void consume(size_t size);
+		bool expectMoreAfter(size_t offset) const;
+		bool exhausted() const; // saw eof/abort and all data consumed
+
+		// start or continue consuming when there is no consumer
+		void enableAutoConsumption();
+
+		const MemBuf &buf() const { return theBuf; }
+
+		const char *status() const; // for debugging only
+
+	protected:
+		// lower-level interface used by Checkout
+		MemBuf &checkOut(); // obtain raw buffer
+		void checkIn(Checkout &checkout); // return updated raw buffer
+		void undoCheckOut(Checkout &checkout); // undo checkout efffect
+
+		void scheduleBodyEndNotification();
+
+		// keep counters in sync and notify producer or consumer
+		void postConsume(size_t size);
+		void postAppend(size_t size);
+
+	public: /* public to enable callbacks, but treat as private */
+
+		/* these methods are calling producer and sibscriber note*()
+		 * callbacks with this BodyPipe as a parameter, which allows
+		 * a single producer or consumer to support multiple pipes. */
+		 
+		void tellMoreBodySpaceAvailable();
+		void tellBodyConsumerAborted();
+		void tellMoreBodyDataAvailable();
+		void tellBodyProductionEnded();
+		void tellBodyProducerAborted();
+
+		AsyncCallWrapper(91,5, BodyPipe, tellMoreBodySpaceAvailable);
+		AsyncCallWrapper(91,5, BodyPipe, tellBodyConsumerAborted);
+		AsyncCallWrapper(91,5, BodyPipe, tellMoreBodyDataAvailable);
+		AsyncCallWrapper(91,5, BodyPipe, tellBodyProductionEnded);
+		AsyncCallWrapper(91,5, BodyPipe, tellBodyProducerAborted);
+
+	private:
+		ssize_t theBodySize;   // expected total content length, if known
+		Producer *theProducer; // content producer, if any
+		Consumer *theConsumer; // content consumer, if any
+
+		size_t thePutSize; // ever-increasing total
+		size_t theGetSize; // ever-increasing total
+
+		MemBuf theBuf; // produced but not yet consumed content, if any
+
+		bool mustAutoConsume; // consume when there is no consumer
+		bool isCheckedOut; // to keep track of checkout violations
+
+		CBDATA_CLASS2(BodyPipe);
+};
+
+#endif /* SQUID_BODY_PIPE_H */
