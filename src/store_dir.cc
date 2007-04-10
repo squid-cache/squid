b07b21cca5af07ddc4f92555f88e62b63bfe9a61
wessels <>
2007-04-10 06:45:10 +0000

    store_dirs_rebuilding should be initialized to 1
    store_dirs_rebuilding is initialized to _1_ as a hack so that
    storeDirWriteCleanLogs() doesn't try to do anything unless _all_
    cache_dirs have been read.  For example, without this hack, Squid
    will try to write clean log files if -kparse fails (becasue it
    calls fatal()).

--- src/store_dir.cc
+++ src/store_dir.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store_dir.cc,v 1.156 2006/08/21 00:50:41 robertc Exp $
+ * $Id: store_dir.cc,v 1.157 2007/04/10 00:45:10 wessels Exp $
  *
  * DEBUG: section 47    Store Directory Routines
  * AUTHOR: Duane Wessels
@@ -61,7 +61,14 @@
 static STDIRSELECT storeDirSelectSwapDirRoundRobin;
 static STDIRSELECT storeDirSelectSwapDirLeastLoad;
 
-int StoreController::store_dirs_rebuilding = 0;
+/*
+ * store_dirs_rebuilding is initialized to _1_ as a hack so that
+ * storeDirWriteCleanLogs() doesn't try to do anything unless _all_
+ * cache_dirs have been read.  For example, without this hack, Squid
+ * will try to write clean log files if -kparse fails (becasue it
+ * calls fatal()).
+ */
+int StoreController::store_dirs_rebuilding = 1;
 
 StoreController::StoreController() : swapDir (new StoreHashIndex())
 {}
