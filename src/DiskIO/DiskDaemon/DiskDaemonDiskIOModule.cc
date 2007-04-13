af00d03d5fea65f52ba6af1317e048e3707e9034
wessels <>
2007-04-13 01:37:23 +0000

    Comment out some debugging messages that are printed before cache_log is initialized because we see them on stderr for commands like squid -k reconfigure.

--- src/DiskIO/DiskDaemon/DiskDaemonDiskIOModule.cc
+++ src/DiskIO/DiskDaemon/DiskDaemonDiskIOModule.cc
@@ -1,6 +1,6 @@
 
 /*
- * $Id: DiskDaemonDiskIOModule.cc,v 1.3 2006/05/29 00:15:03 robertc Exp $
+ * $Id: DiskDaemonDiskIOModule.cc,v 1.4 2007/04/12 19:37:24 wessels Exp $
  *
  * SQUID Web Proxy Cache          http://www.squid-cache.org/
  * ----------------------------------------------------------
@@ -59,7 +59,15 @@ DiskDaemonDiskIOModule::init()
      */
     assert(!initialised);
     memset(&diskd_stats, '\0', sizeof(diskd_stats));
+#if 0
+    /*
+     * DPW 2007-04-12
+     * No debugging here please because this method is called before
+     * the debug log is configured and we'll get the message on
+     * stderr when doing things like 'squid -k reconfigure'
+     */
     debugs(47, 1, "diskd started");
+#endif
     initialised = true;
 }
 
