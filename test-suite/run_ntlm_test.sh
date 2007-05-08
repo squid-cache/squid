c10de90435c8cc917e62efc12bf5be987daf0853
hno <>
2007-05-08 05:37:47 +0000

    Updated NTLM/Negotiate test clients, explaining a bit of their use.
    Added a trivial Basic auth test helper.

--- test-suite/run_ntlm_test.sh
+++ test-suite/run_ntlm_test.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
+url=$1
+proxy=${2:-localhost}
+port=${3:-3128}
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 URL [server port]"
+    exit 1
+fi
+
+echo "blob		# partial message"
+echo "USER=...		# Success"
+echo "BAD..		# Login failure"
+echo "ERR..		# Failure"
+
 while read auth; do
-	echo "HEAD http://www.squid-cache.org/ HTTP/1.0"
+	echo "GET $url HTTP/1.0"
 	if [ -n "$auth" ]; then
 		echo "Proxy-Authorization: NTLM $auth"
 	fi
 	echo "Proxy-Connection: keep-alive"
 	echo
-done | tee -a /dev/fd/2 | nc localhost 3128
+done | tee -a /dev/fd/2 | nc $proxy $port
