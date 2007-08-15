4d883d1fa06364cb8bb95405c96abb0169a0083f
serassio <>
2007-08-15 01:17:43 +0000

    Fix build errors on HP Tru64 Unix and may be others
    - xusleep.c: on Tru64 timeval is defined in sys/time.h
    - move the definition of ISO C99 Standard printf() macros for 64 bit
      integers from squid.h to squid_types.h

--- include/squid_types.h
+++ include/squid_types.h
@@ -1,5 +1,5 @@
 /*
- * $Id: squid_types.h,v 1.8 2003/06/01 09:01:41 hno Exp $
+ * $Id: squid_types.h,v 1.9 2007/08/14 19:17:43 serassio Exp $
  *
  * * * * * * * * Legal stuff * * * * * * *
  *
@@ -72,6 +72,31 @@
 #endif
 #if HAVE_SYS_SELECT_H
 #include <sys/select.h>
+#endif
+
+/* 
+ * ISO C99 Standard printf() macros for 64 bit integers
+ * On some 64 bit platform, HP Tru64 is one, for printf must be used
+ * "%lx" instead of "%llx" 
+ */
+#ifndef PRId64
+#ifdef _SQUID_MSWIN_		/* Windows native port using MSVCRT */
+#define PRId64 "I64d"
+#elif SIZEOF_INT64_T > SIZEOF_LONG
+#define PRId64 "lld"
+#else
+#define PRId64 "ld"
+#endif
+#endif
+
+#ifndef PRIu64
+#ifdef _SQUID_MSWIN_		/* Windows native port using MSVCRT */
+#define PRIu64 "I64u"
+#elif SIZEOF_INT64_T > SIZEOF_LONG
+#define PRIu64 "llu"
+#else
+#define PRIu64 "lu"
+#endif
 #endif
 
 #endif /* SQUID_TYPES_H */
