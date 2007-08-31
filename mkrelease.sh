6cfa3e29a0f9ca8cb448014459fa3f46d7005dc8
wessels <Unknown>
2007-08-31 02:31:41 +0000

    Add a syntax check to the 'revision' arg of mkrelease.sh script

--- mkrelease.sh
+++ mkrelease.sh
@@ -12,6 +12,16 @@ startdir=$PWD/
 dst=${2:-$PWD}/
 RELEASE_TIME=`date +%s`
 
+# DPW 2007-08-30
+#
+# check that $rev has the right syntax
+#
+checkrev=`expr $rev : '\([0-9]\.[0-9]\.[A-Z0-9]*\)'`
+if test "$rev" != "$checkrev" ; then
+	echo "revision '$rev' has incorrect syntax.  Should be like '3.0.STABLE1'"
+	exit 1;
+fi
+
 tmpdir=${TMPDIR:-${PWD}}/${name}-mkrelease
 
 CVSROOT=${CVSROOT:-/server/cvs-server/squid}
