e11ffec305e6d947cbe8ed9dd766258f7793a020
serassio <>
2007-09-22 23:35:28 +0000

    Allow the build of digest_ldap_auth when ldap_start_tls_s() is not available

--- helpers/digest_auth/ldap/ldap_backend.c
+++ helpers/digest_auth/ldap/ldap_backend.c
@@ -359,10 +359,18 @@ ldapconnect(void)
 	    ldap_unbind(ld);
 	    ld = NULL;
 	}
-	if (use_tls && (version == LDAP_VERSION3) && (ldap_start_tls_s(ld, NULL, NULL) == LDAP_SUCCESS)) {
-	    fprintf(stderr, "Could not Activate TLS connection\n");
+	if (use_tls) {
+#ifdef LDAP_OPT_X_TLS
+	    if ((version == LDAP_VERSION3) && (ldap_start_tls_s(ld, NULL, NULL) == LDAP_SUCCESS)) {
+		fprintf(stderr, "Could not Activate TLS connection\n");
+		ldap_unbind(ld);
+		ld = NULL;
+	    }
+#else
+	    fprintf(stderr, "TLS not supported with your LDAP library\n");
 	    ldap_unbind(ld);
 	    ld = NULL;
+#endif
 	}
 #endif
 	squid_ldap_set_timelimit(timelimit);
