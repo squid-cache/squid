0e076fb12c54553d883e0fab09dd05c09f5a35db
amosjeffries <>
2007-12-14 12:03:25 +0000

    Import alternate RFC 3495 library functions from squid3-ipv6 branch.
    Some OS do not provide RFC 3495 libraries (socket extensions for IPv6).
    While these OS are expected to be built with --disable-ipv6 they still need
    squid to provide the library methods for IP-neutral code use of IPv4.
    
    TODO: This patch does not contain the autotools configure options to cause
     these functions to be built. That will come in the later IPv6-enabling patch

---
+++ include/getnameinfo.h
@@ -0,0 +1,30 @@
+#ifndef _getnameinfo_h
+#define _getnameinfo_h
+/*
+ * Reconstructed from KAME getnameinfo.c (in lib/)
+ *
+ * $Id: getnameinfo.h,v 1.1 2007/12/14 05:03:25 amosjeffries Exp $
+ */
+
+#include "config.h"
+
+#ifdef HAVE_GETNAMEINFO
+
+    /* These functions are provided by the OS */
+#define xgetnameinfo	getnameinfo
+
+#else /* !HAVE_GETNAMEINFO */
+
+/* RFC 2553 / Posix resolver */
+SQUIDCEXTERN int xgetnameinfo(const struct sockaddr *sa,
+        socklen_t salen,
+        char *host,
+        size_t hostlen,
+        char *serv,
+        size_t servlen,
+        int flags );
+
+
+#endif /* HAVE_GETNAMEINFO */
+
+#endif
