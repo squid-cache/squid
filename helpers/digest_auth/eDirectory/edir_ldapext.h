591c3246a8b067feb5dde44b29a0073c01e76dd9
hno <>
2007-12-22 05:50:31 +0000

    License cleanup to comply with GPLv2 or later.
    Replaced edir_ldapext.c with a cut down copy of pdb_nds.c from Samba about 3.0.24
    or so (CVS copy, just before Samba changed license to GPLv3)

--- helpers/digest_auth/eDirectory/edir_ldapext.h
+++ helpers/digest_auth/eDirectory/edir_ldapext.h
@@ -1,14 +1,1 @@
-/*
- * edir_ldapext.h
- *
- * AUTHOR: Guy Antony Halse <g.halse@ru.ac.za>
- *
- * stubs for FreeRadius's edir_ldapext.h
- *
- */
-#define UNIVERSAL_PASS_LEN    256
-#define NMAS_SUCCESS          0
-
-extern int berEncodePasswordData(struct berval **requestBV, char *objectDN, char *password, char *password2);
-extern int berDecodeLoginData(struct berval *replyBV, int *serverVersion, size_t * retDataLen, void *retData);
-extern int nmasldap_get_password(LDAP * ld, char *objectDN, size_t * pwdSize, char *pwd);
+int nds_get_password(LDAP *ld, char *object_dn, size_t * pwd_len, char *pwd);
