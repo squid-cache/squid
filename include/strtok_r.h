836f74e0a8b3d1c62d3520dcda8bee4ab6aac0b4
amosjeffries <Unknown>
2008-01-18 14:04:16 +0000

    Import strtok_r() library code for systems that do not provide it.
    This code has been written for the Free Software Foundation and
    falls under the GNU Public Lisence version 2 or later for use.

---
+++ include/strtok_r.h
@@ -0,0 +1,53 @@
+/* Split string into tokens
+   Copyright (C) 2004-2005 Free Software Foundation, Inc.
+   Written by Simon Josefsson.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+#ifndef STRTOK_R_H
+#define STRTOK_R_H
+
+/* Get strtok_r declaration, if available.  */
+#include <string.h>
+
+/* Parse S into tokens separated by characters in DELIM.
+   If S is NULL, the saved pointer in SAVE_PTR is used as
+   the next starting point.  For example:
+	char s[] = "-abc-=-def";
+	char *sp;
+	x = strtok_r(s, "-", &sp);	// x = "abc", sp = "=-def"
+	x = strtok_r(NULL, "-=", &sp);	// x = "def", sp = NULL
+	x = strtok_r(NULL, "=", &sp);	// x = NULL
+		// s = "abc\0-def\0"
+
+   This is a variant of strtok() that is multithread-safe.
+
+   For the POSIX documentation for this function, see:
+   http://www.opengroup.org/susv3xsh/strtok.html
+
+   Caveat: It modifies the original string.
+   Caveat: These functions cannot be used on constant strings.
+   Caveat: The identity of the delimiting character is lost.
+   Caveat: It doesn't work with multibyte strings unless all of the delimiter
+           characters are ASCII characters < 0x30.
+
+   See also strsep().
+*/
+#if defined HAVE_DECL_STRTOK_R && !HAVE_DECL_STRTOK_R
+extern char *strtok_r(char *restrict s, const char *restrict sep,
+		      char **restrict lasts);
+#endif
+
+#endif /* STRTOK_R_H */
