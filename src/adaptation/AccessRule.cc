bed1aa51813fdd2f5ed62c2cbdcd7738ada5a823
Alex Rousskov <rousskov@measurement-factory.com>
2008-04-03 17:11:40 -0600

    Create an "implicit" SingleService group if an access rule refers to a service rather than a service group.

--- src/adaptation/AccessRule.cc
+++ src/adaptation/AccessRule.cc
@@ -29,10 +29,20 @@ Adaptation::AccessRule::parse(ConfigParser &parser)
 void
 Adaptation::AccessRule::finalize()
 {
+    if (!group()) { // no explicit group 
+        debugs(93,7, HERE << "no service group: " << groupId);
+        // try to add a one-service group
+        if (FindService(groupId) != NULL) {
+            ServiceGroup *g = new SingleService(groupId);
+            g->finalize(); // explicit groups were finalized before rules
+            AllGroups().push_back(g);
+        }
+    }
+
     if (!group()) {
         debugs(93,0, "ERROR: Unknown adaptation service or group name: '" <<
             groupId << "'"); // TODO: fail on failures
-	}
+    }
 }
 
 Adaptation::ServiceGroup *
