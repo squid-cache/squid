34ec5c6298baf2055a16a03886d5590224a1e4c0
Amos Jeffries <squid3@treenet.co.nz>
2008-04-08 18:51:51 +1200

    Move TPROXY v2 method of setting the outgoing address to IPIntercept class

--- src/IPInterception.h
+++ src/IPInterception.h
@@ -46,6 +46,12 @@ class IPIntercept
 {
 public:
     int NatLookup(int fd, const IPAddress &me, const IPAddress &peer, IPAddress &dst);
+
+#if LINUX_TPROXY2
+    // only relevant to TPROXY v2 connections.
+    // which require the address be set specifically post-connect.
+    int SetTproxy2OutgoingAddr(int fd, const IPAddress &src);
+#endif
 }
 
 #if !defined(IP_TRANSPARENT)
