67d7d96a5526c0e1289529497d3d87483e016735
Amos Jeffries <squid3@treenet.co.nz>
2008-06-14 01:36:53 +1200

    Bug 2323 (again): basic auth leaks memory
    This round was caused by a few missing unlocks causing objects to be
    incorrectly identified as in-use during garbage collection.
    
    Future cleanups really need to make these objects true RefCounted.

--- src/ACLProxyAuth.cc
+++ src/ACLProxyAuth.cc
@@ -215,7 +215,7 @@ ACLProxyAuth::matchProxyAuth(ACLChecklist *checklist)
     /* check to see if we have matched the user-acl before */
     int result = cacheMatchAcl(&checklist->auth_user_request->user()->
                                proxy_match_cache, checklist);
-    checklist->auth_user_request = NULL;
+    AUTHUSERREQUESTUNLOCK(checklist->auth_user_request, "ACLChecklist via ACLProxyAuth");
     return result;
 }
 
