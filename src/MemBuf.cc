e589f36c2322534bb06043e84e7b2f695b839504
Amos Jeffries <squid3@treenet.co.nz>
2008-06-29 23:20:14 +1200

    MemBuf may be empty when items are append()'ed.
    - Fixes append() to handle the side case where the buffer is empty
      just after initialization and before any calls to Printf()
    
    This allows several performance optimisations to follow, since append()
    utilizes fast memcpy() and Printf() uses the slower snprintf()

--- src/MemBuf.cc
+++ src/MemBuf.cc
@@ -226,12 +226,18 @@ void MemBuf::consume(mb_size_t shiftSize)
     PROF_stop(MemBuf_consume);
 }
 
-// calls memcpy, appends exactly size bytes, extends buffer if needed
+/**
+ * calls memcpy, appends exactly size bytes,
+ * extends buffer or creates buffer if needed.
+ */
 void MemBuf::append(const char *newContent, mb_size_t sz)
 {
     assert(sz >= 0);
-    assert(buf);
     assert(!stolen); /* not frozen */
+
+    if(!buf) {
+        grow(sz+1, MEM_BUF_MAX_SIZE);
+    }
 
     PROF_start(MemBuf_append);
     if (sz > 0) {
@@ -247,7 +253,7 @@ void MemBuf::append(const char *newContent, mb_size_t sz)
     PROF_stop(MemBuf_append);
 }
 
-// updates content size after external append
+/// updates content size after external append
 void MemBuf::appended(mb_size_t sz)
 {
     assert(size + sz <= capacity);
