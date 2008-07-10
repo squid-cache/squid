2a4b3aa8a2d20e4e553b5179902b9965c8c551a0
Amos Jeffries <squid3@treenet.co.nz>
2008-07-11 01:22:19 +1200

    Author: Christos Tsantilas <chtsanti@users.sourceforge.net> Bug 2400: ICAP accept mechanism assumes okay result
    ACL checks produces an ACCESS_REQ_PROXY_AUTH result but ICAP
    result handler assumes any non-zero result means ACCESS_ALLOWED
    
    This can result in many requests being shunted to ICAP incorrently.

--- src/ICAP/ICAPConfig.cc
+++ src/ICAP/ICAPConfig.cc
@@ -218,7 +218,7 @@ ICAPAccessCheck::ICAPAccessCheckCallbackWrapper(int answer, void *data)
         debugs(93, 5, "ICAPAccessCheckCallbackWrapper matchedClass = " << ac->matchedClass.buf());
     }
 
-    if (!answer) {
+    if (answer!=ACCESS_ALLOWED) {
         ac->checkCandidates();
         return;
     }
