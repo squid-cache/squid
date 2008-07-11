bea1ee2ec732a80b229f095bdf228dce769f67bd
Amos Jeffries <squid3@treenet.co.nz>
2008-07-12 02:24:10 +1200

    TestBed: Add master test scripts and some control files
    This adds the ./test-builds.sh script which runs over an optional OS
    specific control build and a set of layered configurations.
    
     Usage:  bash ./test-builds.sh [os-name]
     Currently supported OS:  debian, ubuntu
    
    A sub-level build script is also added as the main engine of this system.
    It takes a pair of control files and attempts a clean build of squid.
    Logging as much of the build output as possible for manual oversight.
    
    The component control files are a set of files providing configure options
    and environment settings for the build. Their design and use is detailed in:
      http://wiki.squid-cache.org/Features/CodeTestBed

---
+++ test-suite/buildtest.sh
@@ -0,0 +1,48 @@
+# #!/bin/bash
+#
+# Configure and run a test build against any given set of configure options
+# or compile-time flags.
+#
+# Should be run from the source package root directory with paths relative to there.
+#
+
+dist="${1}"
+
+# Figure out where to log the test output
+log=`echo "${dist}" | sed s/..test-suite.buildtests.//g `
+
+# ... and send everything there...
+(
+
+if test -f ${dist/.opts}.opts ; then
+	echo "BUILD: ${dist/.opts}.opts"
+	. ./${dist/.opts}.opts
+else
+	echo "BUILD: DEFAULT"
+	OPTS=""
+fi
+
+if test -f ${dist/.opts/}.flags ; then
+#	echo "DEBUG: ${dist/.opts}.flags"
+	FLAGS=`cat ./${dist}.flags`
+# else nothing set for flags.
+fi
+
+#
+# empty all the existing code, reconfigure and builds test code
+
+make -k distclean
+
+#
+# above command currently encounters dependancy problems on cleanup.
+#
+rm -f -r src/fs/aufs/.deps src/fs/diskd/.deps &&
+	./bootstrap.sh &&
+	./configure ${OPTS} &&
+	make check &&
+	make
+
+
+) 2>&1 > ./buildtest_${log}.log
+
+# do not build any of the install's ...
