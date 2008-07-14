b707cdebbdb9898a3824686a7b6a007497061774
Francesco Chemolli <kinkie@squid-cache.org>
2008-07-14 19:08:55 +0200

    Fixed "make check".

--- src/tests/stub_cache_manager.cc
+++ src/tests/stub_cache_manager.cc
@@ -32,6 +32,8 @@
 #include "CacheManager.h"
 #include "squid.h"
 
+static CacheManager *cm=0;
+
 CacheManager::CacheManager()
 {
 }
@@ -39,32 +41,32 @@ CacheManager::CacheManager()
 void
 CacheManager::registerAction(char const * action, char const * desc, OBJH * handler, int pw_req_flag, int atomic)
 {
-	fatal("Not implemented");
+    return;
 }
 
 void
 CacheManager::registerAction(CacheManagerAction *anAction)
 {
-	fatal("Not implemented");
+    return;
 }
 
 CacheManagerAction *
 CacheManager::findAction(char const * action)
 {
-	fatal("Not implemented");
-	return 0; //notreached
+    return 0;
 }
 
 void
 CacheManager::Start(int fd, HttpRequest * request, StoreEntry * entry)
 {
-	fatal("Not implemented");
+    return;
 }
 
 CacheManager*
-CacheManager::GetInstance()
+CacheManager::GetInstance(void)
 {
-	fatal("Not implemented");
-	return 0; //notreached
+    if (!cm)
+        cm=new CacheManager();
+    return cm;
 }
 
