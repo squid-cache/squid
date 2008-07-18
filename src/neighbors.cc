f80286f6c0e74aec4d0b602ffb48f92f627fa168
Amos Jeffries <squid3@treenet.co.nz>
2008-07-18 23:24:16 +1200

    Correct HTCP stats. Slipped out with peerAlive conversion

--- src/neighbors.cc
+++ src/neighbors.cc
@@ -944,6 +944,7 @@ static void
 neighborAliveHtcp(peer * p, const MemObject * mem, const htcpReplyData * htcp)
 {
     peerAlive(p);
+    p->stats.pings_acked++;
     p->htcp.counts[htcp->hit ? 1 : 0]++;
     p->htcp.version = htcp->version;
 }
