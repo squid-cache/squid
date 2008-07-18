24913036145609cd5e588ef789d0eb4da26aa922
Henrik Nordstrom <henrik@henriknordstrom.net>
2008-07-19 00:13:57 +0200

    Author: Eugene Gladchenko <eugene@donpac.ru> Bug #2418: mgr:active_requests always returns "delay_pool 0"
    The delay pool indication in mgr:active_requests had not been updated
    when delay pools was refactored some years ago..

--- src/stat.cc
+++ src/stat.cc
@@ -1674,7 +1674,7 @@ statClientRequests(StoreEntry * s)
 
 #if DELAY_POOLS
 
-        storeAppendPrintf(s, "delay_pool %d\n", DelayId::DelayClient(http) >> 16);
+        storeAppendPrintf(s, "delay_pool %d\n", DelayId::DelayClient(http).pool());
 
 #endif
 
