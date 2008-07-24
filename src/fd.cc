86905e1f411161fe2dabb9f9b09d4dff991464a8
Amos Jeffries <squid3@treenet.co.nz>
2008-07-24 23:54:57 +1200

    Improve display on fd debug output

--- src/fd.cc
+++ src/fd.cc
@@ -269,7 +269,7 @@ fdDumpOpen(void)
         debugs(51, 1, "Open FD "<< std::left<< std::setw(10) << 
                (F->bytes_read && F->bytes_written ? "READ/WRITE" :
                 F->bytes_read ? "READING" : F->bytes_written ? "WRITING" : 
-                null_string)  <<
+                "UNSTARTED")  <<
                " "<< std::right << std::setw(4) << i  << " " << F->desc);
     }
 }
