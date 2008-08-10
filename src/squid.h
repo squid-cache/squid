b0c1c2bc47d2560870e1c722d12c8831ec5b8c7e
Amos Jeffries <amosjeffries@squid-cache.org>
2008-08-09 23:49:14 -0600

    Fix: cppunit tests broken by squid.h defines
    In order to promote safe coding and the use of internal accounting API
    Squid mangles certain common function definitions such as malloc/calloc.
    
    While this is a great idea for Squid internal code. It's not good when
    integrating external cppunit macro libraries. At least one of which in
    use performs its own allocation during testing.
    
    This adds an extra layer of protection to prevent redirection with certain
    unit-tests where the macros needed clash with Squid.

--- src/squid.h
+++ src/squid.h
@@ -425,6 +425,9 @@ max(A const & lhs, A const & rhs)
 #include "protos.h"
 #include "globals.h"
 
+/* Exclude CPPUnit tests from the below restriction. */
+/* BSD implementation uses these still */
+#if !defined(SQUID_UNIT_TEST)
 /*
  * Squid source files should not call these functions directly.
  * Use xmalloc, xfree, xcalloc, snprintf, and xstrdup instead.
@@ -435,7 +438,6 @@ max(A const & lhs, A const & rhs)
 #endif
 template <class V>
 void free(V x) { fatal("Do not use ::free()"); }
-
 #ifndef calloc
 #define calloc +
 #endif
@@ -445,6 +447,7 @@ void free(V x) { fatal("Do not use ::free()"); }
 #ifndef strdup
 #define strdup +
 #endif
+#endif /* !SQUID_UNIT_TEST */
 
 /*
  * Hey dummy, don't be tempted to move this to lib/config.h.in
