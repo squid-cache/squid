47177e61518b6aa75e92c5a0a963f6932fb5f9fb
Amos Jeffries <squid3@treenet.co.nz>
2008-09-13 18:00:54 +1200

    Updates auto-save

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -850,6 +850,17 @@ ErrorState::BuildHttpReply()
          * X-CACHE-MISS entry should tell us who.
          */
         httpHeaderPutStrf(&rep->header, HDR_X_SQUID_ERROR, "%s %d", name, xerrno);
+
+        /* add the Content-Language header according to RFC section 14.21 */
+        if(err_language) {
+            httpHeaderPutStrf(&rep->header, HDR_CONTENT_LANGUAGE, "%s", err_language);
+            /* TODO: do we need to modify Vary: header as well? */
+        }
+        else {
+            /* default templates are in English */
+            httpHeaderPutStrf(&rep->header, HDR_CONTENT_LANGUAGE, "en");
+        }
+
         httpBodySet(&rep->body, content);
         /* do not memBufClean() or delete the content, it was absorbed by httpBody */
     }
@@ -912,7 +923,11 @@ ErrorState::BuildContent()
                 debugs(4, 6, HERE << "Found language '" << reset << "', testing for available template in: '" << dir << "'");
                 m = errorTryLoadText( err_type_str[page_id], dir, false);
 
-                if(m) break; // FOUND IT!!
+                if(m) {
+                    /* store the language we found for the Content-Language reply header */
+                    err_language = xstrdup(reset);
+                    break;
+                }
 
 #if HAVE_GLOB
                 if( (dt - reset) == 2) {
