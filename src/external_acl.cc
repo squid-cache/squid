2170db3ef67ba8810c35fbfa3affa845deb88b3f
Amos Jeffries <squid3@treenet.co.nz>
2008-09-25 00:19:57 +1200

    Show request and reply headers new config via cachemgr dump

--- src/external_acl.cc
+++ src/external_acl.cc
@@ -492,15 +492,23 @@ dump_externalAclHelper(StoreEntry * sentry, const char *name, const external_acl
             switch (format->type) {
 
             case _external_acl_format::EXT_ACL_HEADER_REQUEST:
-
             case _external_acl_format::EXT_ACL_HEADER_REQUEST_ID:
-                storeAppendPrintf(sentry, " %%{%s}", format->header);
+                storeAppendPrintf(sentry, " %%>{%s}", format->header);
                 break;
 
             case _external_acl_format::EXT_ACL_HEADER_REQUEST_MEMBER:
-
             case _external_acl_format::EXT_ACL_HEADER_REQUEST_ID_MEMBER:
-                storeAppendPrintf(sentry, " %%{%s:%s}", format->header, format->member);
+                storeAppendPrintf(sentry, " %%>{%s:%s}", format->header, format->member);
+                break;
+
+            case _external_acl_format::EXT_ACL_HEADER_REPLY:
+            case _external_acl_format::EXT_ACL_HEADER_REPLY_ID:
+                storeAppendPrintf(sentry, " %%<{%s}", format->header);
+                break;
+
+            case _external_acl_format::EXT_ACL_HEADER_REPLY_MEMBER:
+            case _external_acl_format::EXT_ACL_HEADER_REPLY_ID_MEMBER:
+                storeAppendPrintf(sentry, " %%<{%s:%s}", format->header, format->member);
                 break;
 #define DUMP_EXT_ACL_TYPE(a) \
             case _external_acl_format::EXT_ACL_##a: \
