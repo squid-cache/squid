06b65426632901bcf715a431771bdc6e5f6c5f2e
Alex Rousskov <rousskov@measurement-factory.com>
2008-09-28 11:34:36 -0600

    When adding a body_pipe to adaptation message, do not forget to update the raw header as well.

--- src/eCAP/MessageRep.cc
+++ src/eCAP/MessageRep.cc
@@ -422,8 +422,10 @@ void
 Ecap::MessageRep::tieBody(Ecap::XactionRep *x)
 {
     Must(theBodyRep != NULL); // addBody must be called first
+    Must(!theMessage.header->body_pipe);
     Must(!theMessage.body_pipe);
-    theMessage.body_pipe = new BodyPipe(x);
+    theMessage.header->body_pipe = new BodyPipe(x);
+    theMessage.body_pipe = theMessage.header->body_pipe;
     theBodyRep->tie(theMessage.body_pipe);
 }
 
