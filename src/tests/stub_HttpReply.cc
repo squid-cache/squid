8bfcfe3a3d51ed1d41d94f64c5f2ddefa6ae69ad
Alex Rousskov <rousskov@measurement-factory.com>
2008-09-29 02:25:20 -0600

    Made "make check" happy.

--- src/tests/stub_HttpReply.cc
+++ src/tests/stub_HttpReply.cc
@@ -108,3 +108,10 @@ HttpReply::hdrCacheInit()
 {
     fatal ("Not implemented");
 }
+
+HttpReply *
+HttpReply::clone() const
+{
+    fatal("Not implemented");
+    return NULL;
+}
--- src/tests/stub_HttpRequest.cc
+++ src/tests/stub_HttpRequest.cc
@@ -94,6 +94,13 @@ HttpRequest::parseFirstLine(const char *start, const char *end)
     return false;
 }
 
+HttpRequest *
+HttpRequest::clone() const
+{
+    fatal("Not implemented");
+    return NULL;
+}
+
 /*
  * DO NOT MODIFY:
  * arch-tag: dd894aa8-63cc-4543-92d9-1079a18bee11
