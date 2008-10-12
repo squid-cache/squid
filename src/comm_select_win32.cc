9d026532fde230fbfe282dffb46c94e54c56647d
Guido Serassio <serassio@squid-cache.org>
2008-10-12 20:47:33 +0200

    Windows port: Fix build failure on Windows, rollback of wrong changes.
    The changes from this patch are absolutely wrong:
    http://www.squid-cache.org/Versions/v3/3.0/changesets/b8884.patch
    This is a 3.0 specific problem.

--- src/comm_select_win32.cc
+++ src/comm_select_win32.cc
@@ -684,15 +684,6 @@ comm_select_dns_incoming(void)
     statHistCount(&statCounter.comm_dns_incoming, nevents);
 }
 
-static void
-commSelectRegisterWithCacheManager(void)
-{
-    CacheManager::GetInstance()->
-            registerAction("comm_select_incoming",
-                           "comm_incoming() stats",
-                           commIncomingStats, 0, 1);
-}
-
 void
 comm_select_init(void)
 {
