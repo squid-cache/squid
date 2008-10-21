b2c251cca0a3e7ff597566d1f1ca40cef5d2f4f7
Amos Jeffries <squid3@treenet.co.nz>
2008-10-22 00:25:14 +1300

    Bug 2492: assertion failed: Server.cc:70: "!requestBodySource"
    Paranoid: check that swanSong has been called
    Extra paranoia: run before exiting swanSong. I really mean it. They MUST pass here.

--- src/Server.cc
+++ src/Server.cc
@@ -103,6 +103,14 @@ ServerStateData::swanSong()
     Initiator::swanSong();
     BodyProducer::swanSong();
 #endif
+
+    // paranoid: check that swanSong has been called
+    // extra paranoid: yeah, I really mean it. they MUST pass here.
+    assert(!requestBodySource);
+#if USE_ADAPTATION
+    assert(!virginBodyDestination);
+    assert(!adaptedBodySource);
+#endif
 }
 
 
