99697e8e104ac18aba958f4d261ba12fda8f90d1
Amos Jeffries <squid3@treenet.co.nz>
2008-11-12 17:55:12 +1300

    Correct debugs output on GetDenyInfo
    deny_info pages are expected to be redirects sometimes.
    It's only the particular lookup which can't use them.
    Also they may not be a match anyway since test was done after warning.

--- src/acl_noncore.cc
+++ src/acl_noncore.cc
@@ -52,20 +52,19 @@ aclGetDenyInfoPage(acl_deny_info_list ** head, const char *name, int redirect_al
 {
     acl_deny_info_list *A = NULL;
 
-    debugs(28, 9, "aclGetDenyInfoPage: got called for " << name);
-
+    debugs(28, 8, HERE << "got called for " << name);
 
     for (A = *head; A; A = A->next) {
         acl_name_list *L = NULL;
 
-        if (!redirect_allowed && strchr(A->err_page_name, ':')) {
-            debugs(28, 3, "aclGetDenyInfoPage: WARNING, unexpected codepath taken");
+        if (!redirect_allowed && strchr(A->err_page_name, ':') ) {
+            debugs(28, 8, HERE << "Skip '" << A->err_page_name << "' 30x redirects not allowed as response here.");
             continue;
         }
 
         for (L = A->acl_list; L; L = L->next) {
             if (!strcmp(name, L->name)) {
-                debugs(28, 8, "aclGetDenyInfoPage: match on " << name);
+                debugs(28, 8, HERE << "match on " << name);
                 return A->err_page_id;
             }
 
