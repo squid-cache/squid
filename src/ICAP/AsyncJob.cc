2bd535199ec3ad72b66da93f822ffbd15f05197b
Amos Jeffries <squid3@treenet.co.nz>
2008-11-29 00:51:11 +1300

    Author: Christos Tsantilas <chtsanti@users.sourceforge.net> Bug 2527: ICAP compile error with g++ 4.3.2

--- src/ICAP/AsyncJob.cc
+++ src/ICAP/AsyncJob.cc
@@ -155,7 +155,7 @@ const char *AsyncJob::status() const
     buf.append(" [", 2);
     if (stopReason != NULL) {
         buf.Printf("Stopped, reason:");
-        buf.Printf(stopReason);
+        buf.Printf("%s",stopReason);
     }
     buf.Printf(" job%d]", id);
     buf.terminate();
