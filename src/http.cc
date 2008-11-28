ab06ca30b9c9a056cac3088c728c2fe3c4b79e15
Amos Jeffries <squid3@treenet.co.nz>
2008-11-29 01:08:43 +1300

    Author: Henrik Nordstrom <henrik@henriknordstrom.net> Fix cache_peer forceddomainname when request has a Host header
    cache_peer forceddomainname=xxx only worked when the received request
    did not have a Host header.

--- src/http.cc
+++ src/http.cc
@@ -1563,8 +1563,9 @@ copyOneHeaderFromClientsideRequestToUpstreamRequest(const HttpHeaderEntry *e, St
          * went through our redirector and the admin configured
          * 'redir_rewrites_host' to be off.
          */
-
-        if (request->flags.redirected && !Config.onoff.redir_rewrites_host)
+	if (orig_request->peer_domain)
+            hdr_out->putStr(HDR_HOST, orig_request->peer_domain);
+        else if (request->flags.redirected && !Config.onoff.redir_rewrites_host)
             hdr_out->addEntry(e->clone());
         else {
             /* use port# only if not default */
