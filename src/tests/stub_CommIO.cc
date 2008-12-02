638b9bab165ac336ae2d6fc64f498f0f5d1b1dbb
Amos Jeffries <squid3@treenet.co.nz>
2008-12-03 03:03:13 +1300

    Various link errors in testDiskIO
    TODO:
       Fix undefined reference to CommIO members
       discover whether any of the calls now stub'ed need to be used
       may need to pull CommIO out of comm.cc

---
+++ src/tests/stub_CommIO.cc
@@ -0,0 +1,32 @@
+#include "squid.h"
+#include "CommIO.h"
+
+void
+CommIO::ResetNotifications()
+{
+    fatal("Not Implemented");
+}
+
+void
+CommIO::Initialise()
+{
+    fatal("Not Implemented");
+}
+
+void
+CommIO::NotifyIOClose()
+{
+    fatal("Not Implemented");
+}
+
+void
+CommIO::NULLFDHandler(int, void *)
+{
+    fatal("Not Implemented");
+}
+
+void
+CommIO::FlushPipe()
+{
+    fatal("Not Implemented");
+}
