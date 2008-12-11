3c02ae06b9618bdf8cdf4d73d8262f3a793c3517
Amos Jeffries <squid3@treenet.co.nz>
2008-12-11 13:09:16 +1300

    Author: Johnathan Conley <johnathan.conley@gmail.com> Bug 2547: ipv6 compile error in OSX

--- include/IPAddress.h
+++ include/IPAddress.h
@@ -70,8 +70,9 @@
  *   kernel macros commented as being 'non-standard'.
  *   We need to use them, much nicer than the OS-provided __u*_*[]
  * UPDATE: OpenBSD 4.3 has the same.
+ * UPDATE: MacOSX has the same.
  */
-#if USE_IPV6 && ( defined(_SQUID_FREEBSD_) || defined(_SQUID_OPENBSD_) )
+#if USE_IPV6 && ( defined(_SQUID_FREEBSD_) || defined(_SQUID_OPENBSD_) || defined(_SQUID_APPLE_))
 //#define s6_addr8  __u6_addr.__u6_addr8
 //#define s6_addr16 __u6_addr.__u6_addr16
 #define s6_addr32 __u6_addr.__u6_addr32
