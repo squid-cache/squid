8ee81e6ff109871fc9593952585511bd1a1fd8df
Amos Jeffries <squid3@treenet.co.nz>
2009-01-24 13:18:51 +1300

    Bug 2571: pt 1: Add DNS failover to use IPv4-only listen when IPv6 fails.
    TODO: there are other ports to check for this same behavior.

--- src/dns_internal.cc
+++ src/dns_internal.cc
@@ -1354,6 +1354,19 @@ idnsInit(void)
 
         debugs(78, 2, "idnsInit: attempt open DNS socket to: " << addr);
 
+#if USE_IPV6
+        if ( DnsSocket < 0 && addr.IsIPv6() && addr.SetIPv4() ) {
+            /* attempt to open this IPv4-only. */
+            DnsSocket = comm_open(SOCK_DGRAM,
+                                  IPPROTO_UDP,
+                                  addr,
+                                  COMM_NONBLOCKING,
+                                  "DNS Socket");
+
+            debugs(78, 2, "idnsInit: attempt open DNS socket to: " << addr);
+        }
+#endif
+
         if (DnsSocket < 0)
             fatal("Could not create a DNS socket");
 
