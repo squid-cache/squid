efc9d0c574288ab4988e778d80481e5c7d143d01
Amos Jeffries <squid3@treenet.co.nz>
2009-02-03 00:54:49 +1300

    Fix memory leak from logformat parsing

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -3362,6 +3362,7 @@ free_logformat(logformat ** definitions)
     while (*definitions) {
         logformat *format = *definitions;
         *definitions = format->next;
+        safe_free(format->name);
         accessLogFreeLogFormat(&format->format);
         xfree(format);
     }
