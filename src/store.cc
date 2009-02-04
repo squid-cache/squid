6c3b212b309afe1f2b3c3cd1724481b77d00f715
Francesco Chemolli <kinkie@squid-cache.org>
2009-02-05 00:25:42 +0100

    Fixed more previously-uncaught build errors.

--- src/store.cc
+++ src/store.cc
@@ -696,9 +696,9 @@ StoreEntry::setPublicKey()
 #if X_ACCELERATOR_VARY
             vary = mem_obj->getReply()->header.getList(HDR_X_ACCELERATOR_VARY);
 
-            if (vary.buf()) {
+            if (vary.unsafeBuf()) {
                 /* Again, we own this structure layout */
-                rep->header.putStr(HDR_X_ACCELERATOR_VARY, vary.buf());
+                rep->header.putStr(HDR_X_ACCELERATOR_VARY, vary.unsafeBuf());
                 vary.clean();
             }
 
