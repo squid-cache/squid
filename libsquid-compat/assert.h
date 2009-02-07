f189ba3e35890875cea09c39ca1d485b336c8911
Amos Jeffries <squid3@treenet.co.nz>
2009-02-08 02:59:04 +1300

    Trim squid_ from compat filenames

--- libsquid-compat/assert.h
+++ libsquid-compat/assert.h
@@ -45,6 +45,12 @@
 #else
 #define assert(EX)  ((EX)?((void)0):xassert("EX", __FILE__, __LINE__))
 #endif
-SQUIDCEXTERN void xassert(const char *, const char *, int);
+
+#ifdef __cplusplus
+extern "C" void
+#else
+extern void
+#endif
+xassert(const char *, const char *, int);
 
 #endif /* SQUID_ASSERT_H */
