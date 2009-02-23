16adba3f4975bfc643794f1018b5ec90a9d1023d
Amos Jeffries <squid3@treenet.co.nz>
2009-02-23 23:59:18 +1300

    TestBed: add distcheck to tested build procedures
    Also removed distclean filecount hack. With Henriks adjustments the log
    is no longer in the test build directory.

--- test-suite/buildtest.sh
+++ test-suite/buildtest.sh
@@ -21,9 +21,8 @@ fi
 #
 # empty all the existing code, reconfigure and builds test code
 # but skip if we have no files to remove.
-# AYJ: 1 because we already created the build log
 FILECOUNT=`ls -1 | grep -c .`
-if test "$FILECOUNT" != "1" ; then
+if test "$FILECOUNT" != "0" ; then
   make -k distclean || echo "distclean done. errors are unwanted but okay here."
 fi
 
@@ -33,6 +32,7 @@ fi
 rm -f -r src/fs/aufs/.deps src/fs/diskd/.deps &&
 	$base/../configure --silent ${OPTS} 2>&1 &&
 	make check 2>&1 &&
+	make distcheck 2>&1 &&
 	make 2>&1
 
 # do not build any of the install's ...
