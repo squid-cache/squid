fcfc884ec13df1d7e88955a1bf2a3a221adcd7e1
Christos Tsantilas <chtsanti@users.sourceforge.net>
2009-02-24 01:18:23 +0200

    Regression Fix: Bug 2586: Memory leaks on reconfigure
    The patch is not correct. Some of the objects freed here can be in use
    during reconfigure.

--- src/cache_manager.cc
+++ src/cache_manager.cc
@@ -74,10 +74,6 @@ void
 CacheManager::registerAction(char const * action, char const * desc, OBJH * handler, int pw_req_flag, int atomic)
 {
     debugs(16, 3, "CacheManager::registerAction: registering legacy " <<  action);
-    if (findAction(action) != NULL) {
-        debugs(16, 2, "CacheManager::registerAction: Duplicate '" << action << "'. Skipping.");
-        return;
-    }
     registerAction(new CacheManagerActionLegacy(action,desc,pw_req_flag,atomic,handler));
 }
 
