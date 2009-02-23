e08d498ff7b12acd94663bf43f8d86c15b460d5d
Amos Jeffries <squid3@treenet.co.nz>
2009-02-24 01:14:54 +1300

    Fix: appname is now APP_SHORTNAME

--- src/main.cc
+++ src/main.cc
@@ -1397,10 +1397,10 @@ sendSignal(void)
         }
     } else {
         if (opt_send_signal != SIGTERM) {
-            fprintf(stderr, "%s: ERROR: No running copy\n", appname);
+            fprintf(stderr, "%s: ERROR: No running copy\n", APP_SHORTNAME);
             exit(1);
         } else {
-            fprintf(stderr, "%s: No running copy\n", appname);
+            fprintf(stderr, "%s: No running copy\n", APP_SHORTNAME);
             exit(0);
         }
     }
