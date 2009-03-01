38e3f6d8c5164a507462e1978246eab31df2075a
Amos Jeffries <squid3@treenet.co.nz>
2009-03-01 16:06:42 +1300

    Author: Christos Tsantilas <chtsanti@users.sourceforge.net> Bug 2591: adaptation_access does not work
    An access checklist required for each service set, so we need only one
    AccessRule object for each adaptation service set
    
    This patch:
    - When parses an adaptation_access line, checks if an AccessRule object exists
      for the service set and if yes append the access checklist to this object else
      creates a new AccessRule object for this service set
    
    - The AccessRule constructor takes as argument now the reference service set
      name (groupId)
    
    - The new Adaptation::FindRuleByGroupId method created. This method returns
      the AccessRule object for a service set using its name (groupId)
    
    - When an AccessRule for a service group exist in AccessRules 
      list do not add it again in the list.
    
    - Fix adaptation service config parsing

--- src/adaptation/Config.cc
+++ src/adaptation/Config.cc
@@ -150,9 +150,14 @@ Adaptation::Config::DumpServiceSet(StoreEntry *entry, const char *name)
 void
 Adaptation::Config::ParseAccess(ConfigParser &parser)
 {
-    AccessRule *r = new AccessRule;
+    String groupId;
+    ConfigParser::ParseString(&groupId);
+    AccessRule *r;
+    if (!(r=FindRuleByGroupId(groupId))) {
+	r = new AccessRule(groupId);
+	AllRules().push_back(r);
+    }
     r->parse(parser);
-    AllRules().push_back(r);
 }
 
 void
