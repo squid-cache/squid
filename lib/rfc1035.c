d1f6f531ba78be193ba23ef559c1389ef9e935b2
Amos Jeffries <squid3@treenet.co.nz>
2009-04-02 00:49:17 +1300

    Purge last remnants of inet_ntoa
     - Now fully replaced by IPAddress::NtoA and inet_ntop()
     - No need for the third-party emulator and license either

--- lib/rfc1035.c
+++ lib/rfc1035.c
@@ -810,8 +810,9 @@ main(int argc, char *argv[])
                 for (i = 0; i < n; i++) {
                     if (answers[i].type == RFC1035_TYPE_A) {
                         struct in_addr a;
+                        char ipa_str[sizeof(a)];
                         memcpy(&a, answers[i].rdata, 4);
-                        printf("A\t%d\t%s\n", answers[i].ttl, inet_ntoa(a));
+                        printf("A\t%d\t%s\n", answers[i].ttl, inet_ntop(AF_INET,&a,tmp,sizeof(a)));
                     } else if (answers[i].type == RFC1035_TYPE_PTR) {
                         char ptr[128];
                         strncpy(ptr, answers[i].rdata, answers[i].rdlength);
