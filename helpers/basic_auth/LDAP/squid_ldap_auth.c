a16e17595eff753151553a4ceddf95368ff8ea0d
Amos Jeffries <squid3@treenet.co.nz>
2009-04-10 21:17:58 +1200

    Author: Guido Serassio <serassio@squid-cache.org> The debug mode option '-d' was not documented in LDAP helpers usage message

--- helpers/basic_auth/LDAP/squid_ldap_auth.c
+++ helpers/basic_auth/LDAP/squid_ldap_auth.c
@@ -544,6 +544,7 @@ main(int argc, char **argv)
 	fprintf(stderr, "\t-v 2|3\t\t\tLDAP version\n");
 	fprintf(stderr, "\t-Z\t\t\tTLS encrypt the LDAP connection, requires LDAP version 3\n");
 #endif
+	fprintf(stderr, "\t-d\t\t\tenable debug mode\n");
 	fprintf(stderr, "\n");
 	fprintf(stderr, "\tIf no search filter is specified, then the dn <userattr>=user,basedn\n\twill be used (same as specifying a search filter of '<userattr>=',\n\tbut quicker as as there is no need to search for the user DN)\n\n");
 	fprintf(stderr, "\tIf you need to bind as a user to perform searches then use the\n\t-D binddn -w bindpasswd or -D binddn -W secretfile options\n\n");
--- helpers/external_acl/ldap_group/squid_ldap_group.c
+++ helpers/external_acl/ldap_group/squid_ldap_group.c
@@ -431,6 +431,7 @@ main(int argc, char **argv)
 	fprintf(stderr, "\t-g\t\t\tfirst query parameter is base DN extension\n\t\t\t\tfor this query\n");
 	fprintf(stderr, "\t-S\t\t\tStrip NT domain from usernames\n");
 	fprintf(stderr, "\t-K\t\t\tStrip Kerberos realm from usernames\n");
+	fprintf(stderr, "\t-d\t\t\tenable debug mode\n");
 	fprintf(stderr, "\n");
 	fprintf(stderr, "\tIf you need to bind as a user to perform searches then use the\n\t-D binddn -w bindpasswd or -D binddn -W secretfile options\n\n");
 	exit(1);
