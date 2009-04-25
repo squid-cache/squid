f92aff03cdd602c6103a3f953a7fdb3fe256a07b
Amos Jeffries <squid3@treenet.co.nz>
2009-04-25 14:17:26 +1200

    Author: Hasso Tepper <hasso@estpak.ee> DragonFly Support for PF
    Received via NetBSD project.
    
    NetBSD Maintainer says:
     "pfvar.h part came from DragonFly people."
    
    
    Their CVS log attributes says, the fix is from hasso@estpak.ee:
    
    | Fix two problems reported by Hasso Tepper via PR pkg/39554.
    |
    | * DragonFly support.
    
    (other not relevant to this patch)

--- src/IPInterception.cc
+++ src/IPInterception.cc
@@ -81,7 +81,12 @@
 #include <sys/fcntl.h>
 #include <net/if.h>
 #include <netinet/in.h>
+#ifdef HAVE_NET_PF_PFVAR_H
+#include <net/pf/pfvar.h>
+#endif /* HAVE_NET_PF_PFVAR_H */
+#ifdef HAVE_NET_PFVAR_H
 #include <net/pfvar.h>
+#endif /* HAVE_NET_PFVAR_H */
 #endif
 
 #if LINUX_NETFILTER
