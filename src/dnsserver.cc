536fa9dd59c3d50c8fb0d196ce74ebaa3e4e5c05
Amos Jeffries <squid3@treenet.co.nz>
2009-05-20 21:01:21 +1200

    Fix compile errors after res_init polish

--- src/dnsserver.cc
+++ src/dnsserver.cc
@@ -427,7 +427,7 @@ squid_res_setservers(int reset)
     } else if ( ipa.IsIPv4() ) {
         if (_SQUID_RES_NSADDR_COUNT == MAXNS) {
             fprintf(stderr, "Too many -s options, only %d are allowed\n", MAXNS);
-            break;
+            return;
         }
         _SQUID_RES_NSADDR_LIST[_SQUID_RES_NSADDR_COUNT] = _SQUID_RES_NSADDR_LIST[0];
         ipa.GetInAddr(_SQUID_RES_NSADDR_LIST[_SQUID_RES_NSADDR_COUNT++].sin_addr);
@@ -436,7 +436,7 @@ squid_res_setservers(int reset)
          /* because things NEVER seem to resolve in tests without _res.nscount being a total. */
         if (_SQUID_RES_NSADDR_COUNT == MAXNS) {
             fprintf(stderr, "Too many -s options, only %d are allowed\n", MAXNS);
-            break;
+            return;
         }
         _SQUID_RES_NSADDR_COUNT++;
 
