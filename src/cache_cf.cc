bfaa62808660816132f6555a0d233664a2ea8c8d
Amos Jeffries <squid3@treenet.co.nz>
2009-06-06 12:11:44 +1200

    Author: Henrik Nordstrom <henrik@henriknordstrom.net> Bug #2407: Spelling error in http_port tcpkeepalive option
    One of the new parameters according to the docs is "keepalive". However, when
    using this option you'll get a "Bungled squid.conf in line ...". That's because
    when parsing the configuration Squid is looking for the keyword "tcpkeepalive"
    instead of "keepalive" as stated in the docs.
    
    Selected to fix the docs instead of code as having it named keepalive is too
    easily confused with HTTP keep-alive / persistent connections.
    
    
    2009-05-25: Also mistakes on spelling of config dump.

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -3179,9 +3179,9 @@ dump_generic_http_port(StoreEntry * e, const char *n, const http_port_list * s)
 
     if (s->tcp_keepalive.enabled) {
         if (s->tcp_keepalive.idle || s->tcp_keepalive.interval || s->tcp_keepalive.timeout) {
-            storeAppendPrintf(e, " tcp_keepalive=%d,%d,%d", s->tcp_keepalive.idle, s->tcp_keepalive.interval, s->tcp_keepalive.timeout);
+            storeAppendPrintf(e, " tcpkeepalive=%d,%d,%d", s->tcp_keepalive.idle, s->tcp_keepalive.interval, s->tcp_keepalive.timeout);
         } else {
-            storeAppendPrintf(e, " tcp_keepalive");
+            storeAppendPrintf(e, " tcpkeepalive");
         }
     }
 
