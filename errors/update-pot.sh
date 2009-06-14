5253238875a71e9cbfeb83c3b51211c3fcc5c476
Amos Jeffries <squid3@treenet.co.nz>
2009-06-14 18:18:11 +1200

    Update .pot and rename errpages.pot
    The old name dictionary.pot was picked semi-randomly based on my old
    experience in other translation arenas. It does not fit with the l18n
    requirements of Squid very well. This renaming adjusts to make things
    a bit clearer.

--- errors/update-pot.sh
+++ errors/update-pot.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
-# Update the core dictionary file from the basic templates
+# Update the core errpages.pot file from the basic templates
 # Useful if any template has altered.
+#
+# To be run during maintenance from the squid errors/ directory
 #
 
 # Make sure any existing temp stuff is gone from previous updates...
 rm -r -f ./pot
-rm -f dictionary.pot.new
-rm dictionary.pot
+rm -f errpages.pot.new
+rm errpages.pot
 
 # make a temp directory for all our workings...
 mkdir ./pot
@@ -19,22 +21,22 @@ for f in `ls -1 ./templates/`; do
 	fi
 done
 
-# merge and sort the per-page dictionaries into a single master
-msgcat ./pot/*.pot -s -o dictionary.pot.new &&
+# merge and sort the per-page .pot into a single master
+msgcat ./pot/*.pot -s -o errpages.pot.new &&
 	(
-	cat dictionary.pot.new | 
+	cat errpages.pot.new | 
 	sed s/PACKAGE\ VERSION/Squid-3/ |
 	sed s/LANGUAGE\ \<LL\@li\.org\>/Squid\ Developers\ \<squid-dev\@squid-cache\.org\>/
-	) >dictionary.pot
+	) >errpages.pot
 
 ## Update all existing dictionaries with the new content ...
 for f in `ls -1 ./*.po` ; do
 	echo -n "Update: ${f} ... "
-	msgmerge --verbose -s -o ${f}.new ${f} dictionary.pot
+	msgmerge --verbose -s -o ${f}.new ${f} errpages.pot
 	chown --reference=${f} ${f}.new
 	mv ${f}.new ${f}
 done
 
 # cleanup.
 rm -r -f ./pot
-rm -f dictionary.pot.new
+rm -f errpages.pot.new
