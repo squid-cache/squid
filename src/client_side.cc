e6540d66250498bf3fa85f0fb3ff2687b01580fa
Amos Jeffries <squid3@treenet.co.nz>
2009-06-17 11:32:41 +1200

    Fix Signed/Unsigned build error

--- src/client_side.cc
+++ src/client_side.cc
@@ -2141,7 +2141,7 @@ connKeepReadingIncompleteRequest(ConnStateData::Pointer & conn)
     // when we read chunked requests, the entire body is buffered
     // XXX: this check ignores header size and its limits.
     if (conn->in.dechunkingState == ConnStateData::chunkParsing)
-        return conn->in.notYetUsed < Config.maxChunkedRequestBodySize;
+        return ((int64_t)conn->in.notYetUsed) < Config.maxChunkedRequestBodySize;
 
     return conn->in.notYetUsed >= Config.maxRequestHeaderSize ? 0 : 1;
 }
