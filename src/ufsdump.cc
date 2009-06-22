b61a58df52fc37f31394bc81be426a576e0b75d2
Amos Jeffries <squid3@treenet.co.nz>
2009-06-23 01:06:09 +1200

    Make gcc 4.4 happy with 3.1 libraries
     - Fixes libecap build fubar
     - Fixes libbase circular dependencies for ufsdump

--- src/ufsdump.cc
+++ src/ufsdump.cc
@@ -87,9 +87,15 @@ void
 death(int sig)
 {
     std::cout << "Fatal: Signal " <<  sig;
-    exit (1);
+    exit(1);
 }
 
+void
+fatal(const char *message)
+{
+    fprintf(stderr, "FATAL: %s\n", message);
+    exit(1);
+}
 
 /* end stub functions */
 
