8fa1077d60e599ac1db0d52e8f01041460b13597
Amos Jeffries <squid3@treenet.co.nz>
2009-08-23 16:13:58 +1200

    Author: Guido Serassio <serassio@squid-cache.org> Windows port: fix mswin_negotiate_auth.exe crash when executing a LocalCall authentication with verbose debug enabled
    - Also ran source format

--- helpers/negotiate_auth/mswin_sspi/libnegotiatessp.c
+++ helpers/negotiate_auth/mswin_sspi/libnegotiatessp.c
@@ -22,7 +22,8 @@ typedef unsigned char uchar;
 #include <ctype.h>
 #endif
 
-void hex_dump(void *data, int size)
+void
+hex_dump(void *data, int size)
 {
     /* dumps size bytes of *data to stdout. Looks like:
      * [0000] 75 6E 6B 6E 6F 77 6E 20
@@ -39,39 +40,37 @@ void hex_dump(void *data, int size)
         int n;
         char bytestr[4] = {0};
         char addrstr[10] = {0};
-        char hexstr[ 16*3 + 5] = {0};
-        char charstr[16*1 + 5] = {0};
-        for (n=1;n<=size;n++) {
-            if (n%16 == 1) {
+        char hexstr[16 * 3 + 5] = {0};
+        char charstr[16 * 1 + 5] = {0};
+        for (n = 1; n <= size; n++) {
+            if (n % 16 == 1) {
                 /* store address for this line */
                 snprintf(addrstr, sizeof(addrstr), "%.4x",
-                         ((unsigned int)p-(unsigned int)data) );
+                         ((unsigned int) p - (unsigned int) data));
             }
-
             c = *p;
             if (xisalnum(c) == 0) {
                 c = '.';
             }
-
             /* store hex str (for left side) */
             snprintf(bytestr, sizeof(bytestr), "%02X ", *p);
-            strncat(hexstr, bytestr, sizeof(hexstr)-strlen(hexstr)-1);
+            strncat(hexstr, bytestr, sizeof(hexstr) - strlen(hexstr) - 1);
 
             /* store char str (for right side) */
             snprintf(bytestr, sizeof(bytestr), "%c", c);
-            strncat(charstr, bytestr, sizeof(charstr)-strlen(charstr)-1);
+            strncat(charstr, bytestr, sizeof(charstr) - strlen(charstr) - 1);
 
-            if (n%16 == 0) {
+            if (n % 16 == 0) {
                 /* line completed */
                 fprintf(stderr, "[%4.4s]   %-50.50s  %s\n", addrstr, hexstr, charstr);
                 hexstr[0] = 0;
                 charstr[0] = 0;
-            } else if (n%8 == 0) {
+            } else if (n % 8 == 0) {
                 /* half line: add whitespaces */
-                strncat(hexstr, "  ", sizeof(hexstr)-strlen(hexstr)-1);
-                strncat(charstr, " ", sizeof(charstr)-strlen(charstr)-1);
+                strncat(hexstr, "  ", sizeof(hexstr) - strlen(hexstr) - 1);
+                strncat(charstr, " ", sizeof(charstr) - strlen(charstr) - 1);
             }
-            p++; /* next byte */
+            p++;		/* next byte */
         }
 
         if (strlen(hexstr) > 0) {
@@ -80,4 +79,3 @@ void hex_dump(void *data, int size)
         }
     }
 }
-
