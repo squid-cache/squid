1db8047ab928247733e22327aaa4eb01dffe82d5
Amos Jeffries <squid3@treenet.co.nz>
2009-08-24 22:00:48 +1200

    Add concurrent support to shell re-writer

--- helpers/url_rewrite/fake/url_fake_rewrite.sh
+++ helpers/url_rewrite/fake/url_fake_rewrite.sh
@@ -7,11 +7,18 @@
 # squid under the same conditions as the main squid application.
 #
 
-if test "${1}" = "-d" ; then
-	echo "Usage: $0 [-h] [-d logfile]"
+if test "${1}" = "-h" ; then
+	echo "Usage: $0 [-h] [-c] [-d logfile]"
 	echo "  -h           Help: this help text"
+	echo "  -c           Accept concurrent request format"
 	echo "  -d logfile   Debug: log all data received to the named file"
 	exit 1
+fi
+
+concurrent=0
+if test "${1}" = "-c" ; then
+	concurrent=1
+	shift
 fi
 
 DEBUG=0
@@ -20,9 +27,18 @@ if test "${1}" = "-d" ; then
 	LOG="${2}"
 fi
 
-while read url rest; do
-	if test ${DEBUG} ; then
-		echo "$url $rest" >>${LOG}
-	fi
-	echo  # blank line for no change, or replace with another URL.
-done
+if test "$concurrent" = "1"; then
+	while read id url rest; do
+		if test ${DEBUG} ; then
+			echo "ID:$id URL:$url EXTRAS:$rest" >>${LOG}
+		fi
+		echo "${id} " # blank URL for no change, or replace with another URL.
+	done
+else
+	while read url rest; do
+		if test ${DEBUG} ; then
+			echo "URL:$url EXTRAS:$rest" >>${LOG}
+		fi
+		echo  # blank line/URL for no change, or replace with another URL.
+	done
+fi
