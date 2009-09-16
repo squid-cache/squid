7f20e4eb4aefb84fd4865f47a0b3f0d04187fbff
Amos Jeffries <squid3@treenet.co.nz>
2009-09-16 20:32:34 +1200

    Author: Henrik Nordstrom <hno@squid-cache.org> Bug 2510: digest_ldap_auth uses incorrect logic with TLS

--- helpers/digest_auth/ldap/ldap_backend.c
+++ helpers/digest_auth/ldap/ldap_backend.c
@@ -361,10 +361,12 @@ ldapconnect(void)
         }
         if (use_tls) {
 #ifdef LDAP_OPT_X_TLS
-            if ((version == LDAP_VERSION3) && (ldap_start_tls_s(ld, NULL, NULL) == LDAP_SUCCESS)) {
+            if (version != LDAP_VERSION3) {
+                fprintf(stderr, "TLS requires LDAP version 3\n");
+                exit(1);
+            } else if (ldap_start_tls_s(ld, NULL, NULL) != LDAP_SUCCESS) {
                 fprintf(stderr, "Could not Activate TLS connection\n");
-                ldap_unbind(ld);
-                ld = NULL;
+                exit(1);
             }
 #else
             fprintf(stderr, "TLS not supported with your LDAP library\n");
