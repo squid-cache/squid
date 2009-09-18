f7dd58d0cd1cb767b25a56c19f5a510b1cfee1bd
Henrik Nordstrom <henrik@henriknordstrom.net>
2009-09-19 01:46:33 +0200

    Make ESI behave reasonable when built but not used
    - Move (and extent/correct) hardcoded CC ignore to a new http_port
      option ignore-cc
    
    - Limit Surrogate-Capability header addition to accelerated requests.

--- src/client_side.cc
+++ src/client_side.cc
@@ -2370,6 +2370,7 @@ clientProcessRequest(ConnStateData *conn, HttpParser *hp, ClientSocketContext *c
     }
 
     request->flags.accelerated = http->flags.accel;
+    request->flags.ignore_cc = conn->port->ignore_cc;
     request->flags.no_direct = request->flags.accelerated ? !conn->port->allow_direct : 0;
 
     /** \par
