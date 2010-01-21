4b096e17cd1afc8c3474b2eb341975eed8fa2832
Amos Jeffries <squid3@treenet.co.nz>
2010-01-21 23:22:02 +1300

    Author: Wolfgang Nothdurft <wolfgang@linogate.de> Bug 2731: Add follow_x_forwarded_for support to ICAP
    Pass the indirect client address to the ICAP server using X-Client-IP.

--- src/adaptation/icap/ModXact.cc
+++ src/adaptation/icap/ModXact.cc
@@ -1250,8 +1250,17 @@ void Adaptation::Icap::ModXact::makeRequestHeaders(MemBuf &buf)
     }
 
     if (TheConfig.send_client_ip && request)
-        if (!request->client_addr.IsAnyAddr() && !request->client_addr.IsNoAddr())
-            buf.Printf("X-Client-IP: %s\r\n", request->client_addr.NtoA(ntoabuf,MAX_IPSTRLEN));
+    {
+        IpAddress client_addr;
+#if FOLLOW_X_FORWARDED_FOR
+	if (TheConfig.icap_uses_indirect_client) {
+	    client_addr = request->indirect_client_addr;
+	} else
+#endif
+	client_addr = request->client_addr;
+	if(!client_addr.IsAnyAddr() && !client_addr.IsNoAddr())
+	    buf.Printf("X-Client-IP: %s\r\n", client_addr.NtoA(ntoabuf,MAX_IPSTRLEN));
+    }
 
     if (TheConfig.send_client_username && request)
         makeUsernameHeader(request, buf);
