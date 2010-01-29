7c827d812283cec6826583805a72f7d9fbccdf29
Amos Jeffries <squid3@treenet.co.nz>
2010-01-30 02:16:35 +1300

    Display cache_peer name option in CacheMgr config

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -1593,12 +1593,13 @@ dump_peer(StoreEntry * entry, const char *name, peer * p)
     LOCAL_ARRAY(char, xname, 128);
 
     while (p != NULL) {
-        storeAppendPrintf(entry, "%s %s %s %d %d",
+        storeAppendPrintf(entry, "%s %s %s %d %d name=%s",
                           name,
                           p->host,
                           neighborTypeStr(p),
                           p->http_port,
-                          p->icp.port);
+                          p->icp.port,
+                          p->name);
         dump_peer_options(entry, p);
 
         for (d = p->peer_domain; d; d = d->next) {
