aa7558b937a12ea63741ec444e3e333ef036beec
Henrik Nordstrom <henrik@henriknordstrom.net>
2010-02-01 13:47:44 +0100

    Merge error when removing optional kerberos/spnegohelp/ library

--- helpers/negotiate_auth/squid_kerb_auth/squid_kerb_auth.c
+++ helpers/negotiate_auth/squid_kerb_auth/squid_kerb_auth.c
@@ -395,25 +395,8 @@ int main(int argc, char * const argv[])
 
 
     if (output_token.length) {
-#ifndef HAVE_SPNEGO
-      if (spnego_flag) {
-	if ((rc=makeNegTokenTarg (output_token.value,
-				  output_token.length,
-				  &spnegoToken,
-				  &spnegoTokenLength))!=0 ) {
-	  if (debug)
-	    fprintf(stderr, "%s| %s: makeNegTokenTarg failed with rc=%d\n", LogTime(), PROGRAM, rc);
-	  fprintf(stdout, "NA makeNegTokenTarg failed with rc=%d\n",rc);
-	  goto cleanup;
-	}
-      } else {
-	spnegoToken = output_token.value;
-	spnegoTokenLength = output_token.length;
-      }
-#else
       spnegoToken = output_token.value;
       spnegoTokenLength = output_token.length;
-#endif
       token = malloc(base64_encode_len(spnegoTokenLength));
       if (token == NULL) {
 	if (debug)
