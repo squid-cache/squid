33cb14ff660766694b80beea3a051eb7558ee146
Amos Jeffries <squid3@treenet.co.nz>
2010-02-14 18:36:46 +1300

    Author: Serassio Guido <serassio@squid-cache.org> Windows port: Update mswin_check_ad_group to version 2.0
    The global groups support was rewritten, now is based on ADSI.
    New Features:
     - support for Domain Local, Domain Global ad Universal groups
     - full group nesting support
    
    This helper, like the previous version, can be compiled only using
    Microsoft Visual Studio because some needed library are not available
    on MSYS+MinGW or Cygwin.

--- helpers/external_acl/mswin_ad_group/mswin_check_ad_group.h
+++ helpers/external_acl/mswin_ad_group/mswin_check_ad_group.h
@@ -39,29 +39,39 @@
 #include <unistd.h>
 static char *__foo;
 extern char debug_enabled;
+extern char *WIN32_ErrorMessage;
 #define debug(X...) if (debug_enabled) { \
                     fprintf(stderr,"%s[%d](%s:%d): ", myname, mypid, \
                     ((__foo=strrchr(__FILE__,'/'))==NULL?__FILE__:__foo+1),\
                     __LINE__);\
-                    fprintf(stderr,X); }
+                    fprintf(stderr,X); \
+                    if (WIN32_ErrorMessage != NULL){ \
+                    LocalFree(WIN32_ErrorMessage); \
+                    WIN32_ErrorMessage = NULL; \
+                    } }
 #else /* DEBUG */
 #define debug(X...)		/* */
 #endif /* DEBUG */
 #else /* __GNUC__ */
 extern char debug_enabled;
+extern char *WIN32_ErrorMessage;
 static void
 debug(char *format,...)
 {
 #ifdef DEBUG
 #ifdef _SQUID_MSWIN_
     if (debug_enabled) {
-        va_list args;
+	va_list args;
 
-        va_start(args, format);
-        fprintf(stderr, "%s[%d]: ", myname, mypid);
-        vfprintf(stderr, format, args);
-        fprintf(stderr, "\n");
-        va_end(args);
+	va_start(args, format);
+	fprintf(stderr, "%s[%d]: ", myname, mypid);
+	vfprintf(stderr, format, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+	if (WIN32_ErrorMessage != NULL) {
+	    LocalFree(WIN32_ErrorMessage);
+	    WIN32_ErrorMessage = NULL;
+	}
     }
 #endif /* _SQUID_MSWIN_ */
 #endif /* DEBUG */
