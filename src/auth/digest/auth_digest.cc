86399f85e360d293e58405807ec7a2ca3ce4848a
Henrik Nordstrom <henrik@henriknordstrom.net>
2010-02-14 01:09:05 +0100

    Undo unintended digest change in 10279 (will come back later in another change)

--- src/auth/digest/auth_digest.cc
+++ src/auth/digest/auth_digest.cc
@@ -1254,11 +1254,7 @@ AuthDigestConfig::decode(char const *proxy_auth)
     }
 
     /* we can't check the URI just yet. We'll check it in the
-     * authenticate phase, but needs to be given */
-    if (!digest_request->uri) {
-        debugs(29, 4, "authenticateDigestDecode: Missing URI field");
-        return authDigestLogUsername(username, digest_request);
-    }
+     * authenticate phase */
 
     /* is the response the correct length? */
 
