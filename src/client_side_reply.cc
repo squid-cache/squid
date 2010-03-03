054b579eab1d8d45248aadfa0362399a0ce9202f
Amos Jeffries <squid3@treenet.co.nz>
2010-03-03 22:38:49 +1300

    Full Surrogate/1.0 support
    This enables the Surrogate-Control header fields for HTTP reverse proxies
    in general. Not only those with ESI capability.
    
    Limited to use by reverse-proxies only, and some added security to strip
    Surrogate headers better at the border.

--- src/client_side_reply.cc
+++ src/client_side_reply.cc
@@ -1421,6 +1421,14 @@ clientReplyContext::buildReplyHeader()
 
 #endif
 
+    /* Surrogate-Control requires Surrogate-Capability from upstream to pass on */
+    if ( hdr->has(HDR_SURROGATE_CONTROL) ) {
+        if (!request->header.has(HDR_SURROGATE_CAPABILITY)) {
+            hdr->delById(HDR_SURROGATE_CONTROL);
+        }
+        /* TODO: else case: drop any controls intended specifically for our surrogate ID */
+    }
+
     httpHdrMangleList(hdr, request, ROR_REPLY);
 }
 
