2504e69c01cd335fd5bc7b041dc2758d8b370ef4
Amos Jeffries <squid3@treenet.co.nz>
2010-04-08 00:50:24 +1200

    Remove c++ comment from C code

--- lib/rfc3596.c
+++ lib/rfc3596.c
@@ -164,7 +164,6 @@ rfc3596BuildAAAAQuery(const char *hostname, char *buf, size_t sz, unsigned short
  * \return the size of the query
  */
 ssize_t
-
 rfc3596BuildPTRQuery4(const struct in_addr addr, char *buf, size_t sz, unsigned short qid, rfc1035_query * query)
 {
     static char rev[RFC1035_MAXHOSTNAMESZ];
@@ -181,13 +180,12 @@ rfc3596BuildPTRQuery4(const struct in_addr addr, char *buf, size_t sz, unsigned 
 }
 
 ssize_t
-
 rfc3596BuildPTRQuery6(const struct in6_addr addr, char *buf, size_t sz, unsigned short qid, rfc1035_query * query)
 {
     static char rev[RFC1035_MAXHOSTNAMESZ];
     const uint8_t* r = addr.s6_addr;
     char* p = rev;
-    int i; // NP: MUST allow signed for loop termination.
+    int i; /* NP: MUST allow signed for loop termination. */
 
     /* work from the raw addr field. anything else may have representation changes. */
     /* The sin6_port and sin6_addr members shall be in network byte order. */
