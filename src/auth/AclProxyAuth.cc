7d20e1402cd5f6b4b44956c9b1e3a1e27fb4dac9
Amos Jeffries <squid3@treenet.co.nz>
2010-05-06 00:25:14 +1200

    Make proxy_auth ACL fail on invalid auth credentials.

--- src/auth/AclProxyAuth.cc
+++ src/auth/AclProxyAuth.cc
@@ -208,19 +208,10 @@ int
 ACLProxyAuth::matchProxyAuth(ACLChecklist *cl)
 {
     ACLFilledChecklist *checklist = Filled(cl);
-    checkAuthForCaching(checklist);
+    if (!authenticateUserAuthenticated(Filled(checklist)->auth_user_request))
+        return 0;
     /* check to see if we have matched the user-acl before */
     int result = cacheMatchAcl(&checklist->auth_user_request->user()->proxy_match_cache, checklist);
     checklist->auth_user_request = NULL;
     return result;
 }
-
-void
-ACLProxyAuth::checkAuthForCaching(ACLChecklist *checklist)const
-{
-    /* for completeness */
-    /* consistent parameters ? */
-    assert(authenticateUserAuthenticated(Filled(checklist)->auth_user_request));
-    /* this check completed */
-}
-
