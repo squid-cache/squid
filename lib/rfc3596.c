42687bb299c004cd94424c85c9a0fa6bf38344bb
Henrik Nordstrom <henrik@henriknordstrom.net>
2010-05-09 16:21:38 +0200

    Kill rfc1035_errno & error_message globals. We always have the error code available anyway

--- lib/rfc3596.c
+++ lib/rfc3596.c
@@ -327,7 +327,7 @@ while (fgets(input, 512, stdin))
                                  &rid);
 
         if (n < 0) {
-            printf("ERROR %d\n", rfc1035_errno);
+            printf("ERROR %d\n", -n);
         } else if (rid != sid && rid != sidb) {
             printf("ERROR, ID mismatch (%#hx, %#hx)\n", sid, rid);
             printf("ERROR, ID mismatch (%#hx, %#hx)\n", sidb, rid);
