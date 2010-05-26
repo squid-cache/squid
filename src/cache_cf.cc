25579e97ef57f04eec8bd9d39f499d6e85416d90
Amos Jeffries <squid3@treenet.co.nz>
2010-05-26 15:49:37 +1200

    Dump refresh_pattern config as single lines not pairs

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -2228,7 +2228,7 @@ static void
 dump_refreshpattern(StoreEntry * entry, const char *name, refresh_t * head)
 {
     while (head != NULL) {
-        storeAppendPrintf(entry, "%s%s %s %d %d%% %d\n",
+        storeAppendPrintf(entry, "%s%s %s %d %d%% %d",
                           name,
                           head->flags.icase ? " -i" : null_string,
                           head->pattern,
