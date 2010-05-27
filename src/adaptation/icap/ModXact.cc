9b275e998811ce4a20792fe9fb6dba0c1764ba20
Amos Jeffries <squid3@treenet.co.nz>
2010-05-28 00:44:59 +1200

    Audit fixes

--- src/adaptation/icap/ModXact.cc
+++ src/adaptation/icap/ModXact.cc
@@ -1280,7 +1280,7 @@ void Adaptation::Icap::ModXact::makeRequestHeaders(MemBuf &buf)
 void Adaptation::Icap::ModXact::makeUsernameHeader(const HttpRequest *request, MemBuf &buf)
 {
     if (request->auth_user_request != NULL) {
-        char const *name = (request->auth_user_request)->username();
+        char const *name = request->auth_user_request->username();
         if (name) {
             const char *value = TheConfig.client_username_encode ? base64_encode(name) : name;
             buf.Printf("%s: %s\r\n", TheConfig.client_username_header, value);
