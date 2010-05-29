273802fda527a55d3f1edce2b43a27e4b7c2a9a7
Henrik Nordstrom <henrik@henriknordstrom.net>
2010-05-29 03:06:38 +0200

    Correct saved allocations statistics

--- lib/MemPoolMalloc.cc
+++ lib/MemPoolMalloc.cc
@@ -57,6 +57,7 @@ MemPoolMalloc::allocate()
     void *obj = freelist.pop();
     if (obj) {
 	memMeterDec(meter.idle);
+	saved_calls++;
     } else {
 	obj = xcalloc(1, obj_size);
 	memMeterInc(meter.alloc);
