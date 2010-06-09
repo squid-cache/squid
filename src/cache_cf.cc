31ef19cd9d84d9a304faf45a4826a3a540a04901
Amos Jeffries <squid3@treenet.co.nz>
2010-06-09 23:23:59 +1200

    Enforce connection re-try limit connect_retries
    We used to have a maximum_single_address_tries limit which as similar
    That is not dropped and the single-link retry used instead which applies
    to ALL links regardless of the host IP address count since we dont know
    that count any more.

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -526,12 +526,9 @@ configDoConfigure(void)
     else
         Config.appendDomainLen = 0;
 
-    if (Config.retry.maxtries > 10)
-        fatal("maximum_single_addr_tries cannot be larger than 10");
-
-    if (Config.retry.maxtries < 1) {
-        debugs(3, 0, "WARNING: resetting 'maximum_single_addr_tries to 1");
-        Config.retry.maxtries = 1;
+    if (Config.connect_retries > 10) {
+        debugs(0,DBG_CRITICAL, "WARNING: connect_retries cannot be larger than 10. Resetting to 10.");
+        Config.connect_retries = 10;
     }
 
     requirePathnameExists("MIME Config Table", Config.mimeTablePathname);
