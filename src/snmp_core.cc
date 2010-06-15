8e3a0e0899a553f5977bfc487919fda94917f595
Henrik Nordstrom <henrik@henriknordstrom.net>
2010-06-16 00:52:16 +0200

    free memory corruption introduced by the polishing of the SNMP tree generator.

--- src/snmp_core.cc
+++ src/snmp_core.cc
@@ -963,9 +963,10 @@ snmpCreateOidFromStr(const char *str, oid **name, int *nl)
     *name = NULL;
     *nl = 0;
     char *s = xstrdup(str);
+    char *s_ = s;
 
     /* Parse the OID string into oid bits */
-    while ( (p = strsep(&s, delim)) != NULL) {
+    while ( (p = strsep(&s_, delim)) != NULL) {
         *name = (oid*)xrealloc(*name, sizeof(oid) * ((*nl) + 1));
         (*name)[*nl] = atoi(p);
         (*nl)++;
