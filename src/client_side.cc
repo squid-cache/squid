08df15d3fba635bed477a411e01da1325736c48a
Alex Rousskov <rousskov@measurement-factory.com>
2010-07-01 15:20:38 -0600

    Removed comment-question. There are no default http_port entries.

--- src/client_side.cc
+++ src/client_side.cc
@@ -3517,7 +3517,7 @@ clientOpenListenSockets(void)
 #endif
 
     if (NHttpSockets < 1)
-        fatal("No HTTP or HTTPS ports configured"); // defaults prohibit this?
+        fatal("No HTTP or HTTPS ports configured");
 }
 
 void
