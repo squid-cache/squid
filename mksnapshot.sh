4e3173cf8fa231f386799575a7588626fd7f0675
Amos Jeffries <amosjeffries@squid-cache.org>
2010-07-06 21:00:05 -0600

    Maintenance: wrap all snapshot script variables

--- mksnapshot.sh
+++ mksnapshot.sh
@@ -15,24 +15,24 @@ BZRROOT=${BZRROOT:-/bzr}
 # name so we strip that.
 branchpath=${1:-trunk}
 tag=${2:-`basename $branchpath`}
-startdir=$PWD
+startdir=${PWD}
 date=`env TZ=GMT date +%Y%m%d`
 
 tmpdir=${TMPDIR:-${PWD}}/${module}-${tag}-mksnapshot
 
-rm -rf $tmpdir
-trap "rm -rf $tmpdir" 0
+rm -rf ${tmpdir}
+trap "rm -rf ${tmpdir}" 0
 
 rm -f ${tag}.out
-bzr export $tmpdir $BZRROOT/$module/$branchpath || exit 1
-if [ ! -f $tmpdir/configure ] && [ -f $tmpdir/configure.in ]; then
-	sh -c "cd $tmpdir && ./bootstrap.sh"
+bzr export ${tmpdir} ${BZRROOT}/${module}/${branchpath} || exit 1
+if [ ! -f ${tmpdir}/configure ] && [ -f{ $tmpdir}/configure.in ]; then
+	sh -c "cd ${tmpdir} && ./bootstrap.sh"
 fi
-if [ ! -f $tmpdir/configure ]; then
-	echo "ERROR! Tag $tag not found in $module"
+if [ ! -f ${tmpdir}/configure ]; then
+	echo "ERROR! Tag ${tag} not found in ${module}"
 fi
 
-cd $tmpdir
+cd ${tmpdir}
 eval `grep "^ *PACKAGE_VERSION=" configure | sed -e 's/-BZR//' | sed -e 's/PACKAGE_//'`
 eval `grep "^ *PACKAGE_TARNAME=" configure | sed -e 's/_TARNAME//'`
 ed -s configure.in <<EOS
@@ -56,52 +56,52 @@ echo "TMPDIR: ${tmpdir}"
 make -s dist-all
 
 webbase=/server/httpd/htdocs/squid-cache.org/content/
-basetarball=${webbase}/Versions/v`echo $VERSION | cut -d. -f1`/`echo $VERSION | cut -d. -f-2|cut -d- -f1`/${PACKAGE}-${VERSION}.tar.bz2
+basetarball=${webbase}/Versions/v`echo ${VERSION} | cut -d. -f1`/`echo ${VERSION} | cut -d. -f-2|cut -d- -f1`/${PACKAGE}-${VERSION}.tar.bz2
 
 echo "Building Tarball diff (${basetarball}) ..."
-if [ -f $basetarball ]; then
+if [ -f ${basetarball} ]; then
 	tar jxf ${PACKAGE}-${VERSION}-${date}.tar.bz2
-	tar jxf $basetarball
+	tar jxf ${basetarball}
 	echo "Differences from ${PACKAGE}-${VERSION} to ${PACKAGE}-${VERSION}-${date}" >${PACKAGE}-${VERSION}-${date}.diff
 	diff -ruN ${PACKAGE}-${VERSION} ${PACKAGE}-${VERSION}-${date} >>${PACKAGE}-${VERSION}-${date}.diff || true
 else
 	echo "Building Tarball diff ... skipped (no tarball exists)."
 fi
 
-cd $startdir
-echo "Preparing to publish: $tmpdir/${PACKAGE}-${VERSION}-${date}.tar.* ..."
+cd ${startdir}
+echo "Preparing to publish: ${tmpdir}/${PACKAGE}-${VERSION}-${date}.tar.* ..."
 #echo "LOCAL: " ; pwd
-#echo "BUILT TARS: " ; ls -1 $tmpdir/*.tar.* || true
+#echo "BUILT TARS: " ; ls -1 ${tmpdir}/*.tar.* || true
 
-cp -p $tmpdir/${PACKAGE}-${VERSION}-${date}.tar.gz .
+cp -p ${tmpdir}/${PACKAGE}-${VERSION}-${date}.tar.gz .
 echo ${PACKAGE}-${VERSION}-${date}.tar.gz >>${tag}.out
-cp -p $tmpdir/${PACKAGE}-${VERSION}-${date}.tar.bz2 .
+cp -p ${tmpdir}/${PACKAGE}-${VERSION}-${date}.tar.bz2 .
 echo ${PACKAGE}-${VERSION}-${date}.tar.bz2 >>${tag}.out
-if [ -f $tmpdir/${PACKAGE}-${VERSION}-${date}.diff ]; then
-    cp -p $tmpdir/${PACKAGE}-${VERSION}-${date}.diff .
+if [ -f ${tmpdir}/${PACKAGE}-${VERSION}-${date}.diff ]; then
+    cp -p ${tmpdir}/${PACKAGE}-${VERSION}-${date}.diff .
     echo ${PACKAGE}-${VERSION}-${date}.diff >>${tag}.out
 fi
 
-relnotes=$tmpdir/doc/release-notes/release-`echo $VERSION | cut -d. -f1,2 | cut -d- -f1`.html
-if [ -f $relnotes ]; then
-	cp -p $relnotes ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html
+relnotes=${tmpdir}/doc/release-notes/release-`echo ${VERSION} | cut -d. -f1,2 | cut -d- -f1`.html
+if [ -f ${relnotes} ]; then
+	cp -p ${relnotes} ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html
 	echo ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html >>${tag}.out
 	ed -s ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html <<EOF
 g/"ChangeLog"/ s//"${PACKAGE}-${VERSION}-${date}-ChangeLog.txt"/g
 w
 EOF
 fi
-cp -p $tmpdir/ChangeLog ${PACKAGE}-${VERSION}-${date}-ChangeLog.txt
+cp -p ${tmpdir}/ChangeLog ${PACKAGE}-${VERSION}-${date}-ChangeLog.txt
 echo ${PACKAGE}-${VERSION}-${date}-ChangeLog.txt >>${tag}.out
 
 # Generate Configuration Manual HTML
-if [ -x $tmpdir/scripts/www/build-cfg-help.pl ]; then
-	make -C $tmpdir/src cf.data
-	mkdir -p $tmpdir/doc/cfgman
-	$tmpdir/scripts/www/build-cfg-help.pl --version ${VERSION} -o $tmpdir/doc/cfgman $tmpdir/src/cf.data
-	sh -c "cd $tmpdir/doc/cfgman && tar -zcf $PWD/${PACKAGE}-${VERSION}-${date}-cfgman.tar.gz *"
+if [ -x ${tmpdir}/scripts/www/build-cfg-help.pl ]; then
+	make -C ${tmpdir}/src cf.data
+	mkdir -p ${tmpdir}/doc/cfgman
+	${tmpdir}/scripts/www/build-cfg-help.pl --version ${VERSION} -o ${tmpdir}/doc/cfgman ${tmpdir}/src/cf.data
+	sh -c "cd ${tmpdir}/doc/cfgman && tar -zcf ${PWD}/${PACKAGE}-${VERSION}-${date}-cfgman.tar.gz *"
 	echo ${PACKAGE}-${VERSION}-${date}-cfgman.tar.gz >>${tag}.out
-	$tmpdir/scripts/www/build-cfg-help.pl --version ${VERSION} -o ${PACKAGE}-${VERSION}-${date}-cfgman.html -f singlehtml $tmpdir/src/cf.data
+	${tmpdir}/scripts/www/build-cfg-help.pl --version ${VERSION} -o ${PACKAGE}-${VERSION}-${date}-cfgman.html -f singlehtml ${tmpdir}/src/cf.data
 	gzip -f -9 ${PACKAGE}-${VERSION}-${date}-cfgman.html
 	echo ${PACKAGE}-${VERSION}-${date}-cfgman.html.gz >>${tag}.out
 fi
@@ -125,6 +125,6 @@ fi
 # Generate language-pack tarballs
 # NP: Only to be done on trunk.
 if test "${tag}" = "trunk" ; then
-	sh -c "cd $tmpdir/errors && tar -zcf ${PWD}/${PACKAGE}-${VERSION}-${date}-langpack.tar.gz ./*/* ./alias* ./TRANSLATORS ./COPYRIGHT "
+	sh -c "cd ${tmpdir}/errors && tar -zcf ${PWD}/${PACKAGE}-${VERSION}-${date}-langpack.tar.gz ./*/* ./alias* ./TRANSLATORS ./COPYRIGHT "
 	echo ${PACKAGE}-${VERSION}-${date}-langpack.tar.gz >>${tag}.out
 fi
