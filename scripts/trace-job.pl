b7a5b9d0915b9a2ea43f482fc8611ea242432d85
Alex Rousskov <rousskov@measurement-factory.com>
2010-07-16 16:37:42 -0600

    Added debugging scripts that work with detailed cache.log
      scripts/find-alive.pl: pinpoint objects that are still alive, to find leaks
      scripts/trace-job.pl: find cache.log lines that correspond to a given job
      scripts/trace-master.pl: trace jobs related to a single master transaction
    
    The scripts require maintenance as the logging format changes, but
    they often simplify debugging by extracting relevant information from
    tons of poorly structured cache.log data.

---
+++ scripts/trace-job.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl -w
+
+# Reads cache.log and displays lines that correspond to a given async job.
+# 
+# If job entering/exiting line format changes, the script must be updated.
+# Keep the old RE around for a while because they may be handy for working
+# with folks running older Squids.
+
+use strict;
+use warnings;
+
+my $XactId = shift or die("usage: $0 <xaction id> [log file]\n");
+
+# Squid uses asyncNNN, jobNNN, icapxNNN for the same job/transaction
+# TODO: use jobNNN everywhere
+$XactId =~ s/^(?:async|job|icapx)(\d+)$/(async|job|icapx)$1/ and
+    warn("Replacing xaction ID with $XactId\n");
+
+my $inside = 0;
+
+my $entering;
+
+while (<>) {
+	$entering = $_ if !$inside && /\| entering\b/;
+	undef $entering if /\| leaving\b/;
+
+	# if (!$inside && /\bcalled\b.*\b$XactId\b/o) {
+	if (!$inside && /\bstatus in\b.*\b$XactId\b/o) {
+		print $entering if defined $entering;
+		$inside = 1;
+	}
+
+	my $external = !$inside && /\b$XactId\b/o;
+	
+	print $_ if $inside || $external;
+	print "\n" if $external;
+
+	next unless $inside;
+
+	# if (/\bended\b.*\b$XactId\b/o || /\bswan\s+sang\b.*\b$XactId\b/o) {
+	# if (/\bstatus out\b.*\b$XactId\b/o || /\bswan\s+sang\b.*\b$XactId\b/o ||
+	if (/\| leaving\b/) {
+		print "\n";
+		$inside = 0;
+	}
+}
+
+exit(0);
