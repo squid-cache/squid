3c999603b14322a7ea2ae26e15e2f262082ea32a
Amos Jeffries <amosjeffries@squid-cache.org>
2010-07-29 07:06:05 -0600

    Update helper debug macro for safe use inside unwrapped if...else statements

--- compat/debug.h
+++ compat/debug.h
@@ -30,7 +30,7 @@ SQUIDCEXTERN int debug_enabled;
                      if (debug_enabled) { \
                          fprintf(stderr, "%s(%d): pid=%ld :", __FILE__, __LINE__, (long)getpid() ); \
                          fprintf(stderr,X); \
-                     }
+                     } else (void)0
 
 #else /* __GNUC__ */
 
