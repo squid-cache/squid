0383747d92754ae56b7fcf091950b4db518a3d1c
Amos Jeffries <squid3@treenet.co.nz>
2010-08-13 23:54:31 +1200

    Permit rotate logs from cachemgr
    Given that reconfigure, shutdown and offline already have password-protected
    remote actions available it makes sense to permit the less critical rotate
    as well.

--- src/cache_manager.cc
+++ src/cache_manager.cc
@@ -56,6 +56,7 @@ CacheManager::CacheManager()
     registerAction(new OfflineToggleAction);
     registerAction(new ShutdownAction);
     registerAction(new ReconfigureAction);
+    registerAction(new RotateAction);
     registerAction(new MenuAction(this));
 }
 
@@ -371,6 +372,21 @@ CacheManager::ReconfigureAction::run(StoreEntry * sentry)
 }
 /// \ingroup CacheManagerInternal
 CacheManager::ReconfigureAction::ReconfigureAction() : CacheManagerAction("reconfigure","Reconfigure Squid", 1, 1) { }
+
+/// \ingroup CacheManagerInternal
+void
+CacheManager::RotateAction::run(StoreEntry * sentry)
+{
+    debugs(16, DBG_IMPORTANT, "Rotate Logs by Cache Manager command.");
+    storeAppendPrintf(sentry, "Rotating Squid Process Logs ....");
+#ifdef _SQUID_LINUX_THREADS_
+    rotate_logs(SIGQUIT);
+#else
+    rotate_logs(SIGUSR1);
+#endif
+}
+/// \ingroup CacheManagerInternal
+CacheManager::RotateAction::RotateAction() : CacheManagerAction("rotate","Rotate Squid Logs", 1, 1) { }
 
 /// \ingroup CacheManagerInternal
 void
