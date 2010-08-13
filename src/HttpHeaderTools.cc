90e8bd1f177ecfc10ddc3f9a1d3c4b5ef623db9a
Amos Jeffries <amosjeffries@squid-cache.org>
2010-08-13 01:53:08 -0600

    HTTP/1.1 compliance: Stop using Proxy-Connection header
    The Proxy-Connection header is not part of any HTTP standard. It was added
    by Netscape to differentiate persistent connections to intermediary proxies
    but that duty has been formally superceded by the Connection: header.
    
    This compliance update makes Squid stop sending Proxy-Connection on outbound
    requests. Starts consistently using Connection: header instead.
    
    The Proxy-Connection header is also ignored on HTTP-strict builds.
    For compatibility we must do a small violation and drop it as a hop-by-hop
    header despite strct-mode technically being required to pass it through.
    
    For origin server connections the non-strict builds will retain the
    status-quo: interpret it, but treat it as an HTTP/0.9 thing to be
    upgraded to HTTP/1.1 Connection:.
    
    squidclient is also fixed not to send it.

--- src/HttpHeaderTools.cc
+++ src/HttpHeaderTools.cc
@@ -145,18 +145,18 @@ int
 httpHeaderHasConnDir(const HttpHeader * hdr, const char *directive)
 {
     String list;
-    http_hdr_type ht;
     int res;
     /* what type of header do we have? */
 
+#if USE_HTTP_VIOLATIONS
     if (hdr->has(HDR_PROXY_CONNECTION))
-        ht = HDR_PROXY_CONNECTION;
-    else if (hdr->has(HDR_CONNECTION))
-        ht = HDR_CONNECTION;
+        list = hdr->getList(HDR_PROXY_CONNECTION);
+    else
+#endif
+    if (hdr->has(HDR_CONNECTION))
+        list = hdr->getList(HDR_CONNECTION);
     else
         return 0;
-
-    list = hdr->getList(ht);
 
     res = strListIsMember(&list, directive, ',');
 
