83f8d0e05f7b51c2b0c49359cfce1c60e81310aa
Automatic source maintenance <squidadm@squid-cache.org>
2010-08-20 18:12:42 -0600

    SourceFormat Enforcement

--- tools/purge/copyout.cc
+++ tools/purge/copyout.cc
@@ -234,7 +234,7 @@ copy_out( size_t filesize, size_t metasize, unsigned debug,
     // seek end of output file ...
     off_t position = lseek( out, filesize-metasize-1, SEEK_SET );
     if ( position == -1 ) {
-        fprintf( stderr, "lseek(%s,%lu): %s\n", filename, 
+        fprintf( stderr, "lseek(%s,%lu): %s\n", filename,
                  (unsigned long)filesize-metasize,
                  strerror(errno) );
         BAUTZ(false);
