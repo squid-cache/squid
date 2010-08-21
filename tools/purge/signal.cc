97193b5acfa4e82e394f03161ee46adf5b101f66
Automatic source maintenance <squidadm@squid-cache.org>
2010-08-20 18:12:42 -0600

    SourceFormat Enforcement

--- tools/purge/signal.cc
+++ tools/purge/signal.cc
@@ -115,19 +115,19 @@ sigChild( int signo )
     while ( (pid = waitpid( -1, &status, WNOHANG )) > 0 ) {
         if ( WIFEXITED(status) ) {
             fprintf( stderr, "child (pid=%ld) reaped, status %d\n%c",
-                      (long) pid, WEXITSTATUS(status), 0 );
+                     (long) pid, WEXITSTATUS(status), 0 );
         } else if ( WIFSIGNALED(status) ) {
             fprintf( stderr, "child (pid=%ld) died on signal %d%s\n%c",
-                      (long) pid, WTERMSIG(status),
+                     (long) pid, WTERMSIG(status),
 #ifdef WCOREDUMP
-                      WCOREDUMP(status) ? " (core generated)" : "",
+                     WCOREDUMP(status) ? " (core generated)" : "",
 #else
-                      "",
+                     "",
 #endif
-                      0 );
+                     0 );
         } else {
             fprintf( stderr, "detected dead child (pid=%ld), status %d\n%c",
-                      (long) pid, status, 0 );
+                     (long) pid, status, 0 );
         }
     }
     errno = saveerr;
