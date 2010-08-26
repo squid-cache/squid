0f87cb3229622d920632317055a25df5a2d7e6e7
Amos Jeffries <amosjeffries@squid-cache.org>
2010-08-26 06:14:58 -0600

    Author: Alex Rousskov <rousskov@measurement-factory.com> HTTP Compliance: remove Content-Length header if Transfer-Encoding is present.
    If after HTTP header parsing we have both "Transfer-Encoding: chunked"
    and Content-Length headers, remove the Content-Length entry. The
    adjusted behavior follows httpbis recommendations (ticket #95, part 2).
    
    The old client-side code forwarded the original Content-Length header
    which did not match the [dechunked] response, resulting in a malformed
    response.
    
    HttpHeader::chunked() method added to check if HTTP headers contain
    chunked Transfer-Encoding header. Use this method in code that checks
    for chunked encoding.
    
    Co-Advisor test cases: test_case/rfc2616/chunked-1p0-badClen-toClt
                           test_case/rfc2616/chunked-1p1-badClen-toClt

--- src/HttpRequest.cc
+++ src/HttpRequest.cc
@@ -498,7 +498,7 @@ HttpRequest::expectingBody(const HttpRequestMethod& unused, int64_t& theSize) co
         expectBody = Config.onoff.request_entities ? true : false;
     else if (method == METHOD_PUT || method == METHOD_POST)
         expectBody = true;
-    else if (header.hasListMember(HDR_TRANSFER_ENCODING, "chunked", ','))
+    else if (header.chunked())
         expectBody = true;
     else if (content_length >= 0)
         expectBody = true;
@@ -506,7 +506,7 @@ HttpRequest::expectingBody(const HttpRequestMethod& unused, int64_t& theSize) co
         expectBody = false;
 
     if (expectBody) {
-        if (header.hasListMember(HDR_TRANSFER_ENCODING, "chunked", ','))
+        if (header.chunked())
             theSize = -1;
         else if (content_length >= 0)
             theSize = content_length;
