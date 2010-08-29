784f46733cb6eacc1a63afd11231544300bc91c6
Automatic source maintenance <squidadm@squid-cache.org>
2010-08-29 00:15:38 -0600

    SourceFormat Enforcement

--- src/fs/ufs/store_io_ufs.cc
+++ src/fs/ufs/store_io_ufs.cc
@@ -454,8 +454,7 @@ UFSStoreState::kickReadQueue()
 
     if (cbdataReferenceValidDone(q->callback_data, &cbdata)) {
         read_(q->buf, q->size, q->offset, q->callback, cbdata);
-    }
-    else {
+    } else {
         debugs(79, 2, "UFSStoreState::kickReadQueue: this: " << this << " cbdataReferenceValidDone returned false." << " closing: " << closing << " flags.try_closing: " << flags.try_closing);
         delete q;
         return false;
