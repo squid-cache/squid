dffa13844b49e18afbf156bcc978ea11aa877019
Amos Jeffries <amosjeffries@squid-cache.org>
2010-08-29 19:34:43 -0600

    Libtool 1.x no longer work. autoconf 2.64 does

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,9 @@
 # Autotool versions preferred. To override either edit the script
 # to match the versions you want to use, or set the variables on
 # the command line like "env acver=.. amver=... ./bootstrap.sh"
-acversions="${acver:-2.63 2.62 2.61}"
+acversions="${acver:-2.64 2.63 2.62 2.61}"
 amversions="${amver:-1.11 1.10 1.9}"
-ltversions="${ltver:-2.2 1.5 1.4}"
+ltversions="${ltver:-2.2}"
 
 check_version()
 {
