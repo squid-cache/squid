b759992fa79f6bf10ae88b9ecc7675425b2a3c4a
Amos Jeffries <amosjeffries@squid-cache.org>
2010-09-03 03:20:33 -0600

    Remove rev10092 - missing dependent feature

--- src/tunnel.cc
+++ src/tunnel.cc
@@ -680,8 +680,6 @@ tunnelStart(ClientHttpRequest * http, int64_t * size_ptr, int *status_ptr)
         return;
     }
 
-    request->hier.peer_local_port = comm_local_port(sock); // for %<lp logging
-
     tunnelState = new TunnelStateData;
 #if DELAY_POOLS
 
