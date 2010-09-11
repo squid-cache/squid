9372d4aaff46a85641dc644957fcfe75b99b2c23
Alex Rousskov <rousskov@measurement-factory.com>
2010-09-10 22:46:32 -0600

    Fixed printf format(%llX)/parameter(mb_size_t==ssize_t) mismatch.
    Detected by GCC v4.4.1.

--- src/http.cc
+++ src/http.cc
@@ -2152,7 +2152,7 @@ HttpStateData::getMoreRequestBody(MemBuf &buf)
     // we may need to send: hex-chunk-size CRLF raw-data CRLF last-chunk
     buf.init(16 + 2 + rawDataSize + 2 + 5, raw.max_capacity);
 
-    buf.Printf("%"PRIX64"\r\n", rawDataSize);
+    buf.Printf("%x\r\n", static_cast<unsigned int>(rawDataSize));
     buf.append(raw.content(), rawDataSize);
     buf.Printf("\r\n");
 
