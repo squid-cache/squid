a7173732e35b75f3f98544cd34de02ba6184af1b
Amos Jeffries <amosjeffries@squid-cache.org>
2010-09-18 06:58:56 -0600

    Author: Alex Rousskov <rousskov@measurement-factory.com> Fixed a NetDB leak.

--- src/icmp/net_db.cc
+++ src/icmp/net_db.cc
@@ -252,6 +252,7 @@ netdbLookupAddr(const Ip::Address &addr)
     char *key = new char[MAX_IPSTRLEN];
     networkFromInaddr(addr).NtoA(key,MAX_IPSTRLEN);
     n = (netdbEntry *) hash_lookup(addr_table, key);
+    delete[] key;
     return n;
 }
 
