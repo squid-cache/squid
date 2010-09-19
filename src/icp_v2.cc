dd305054164fcff76c290e2e300e17480b3bc489
Amos Jeffries <squid3@treenet.co.nz>
2010-09-19 15:03:02 +1200

    Several leaks, nul-pointer dereferences and redundant checks
    Caught by cppcheck static analysis.

--- src/icp_v2.cc
+++ src/icp_v2.cc
@@ -235,15 +235,13 @@ void
 icpUdpSendQueue(int fd, void *unused)
 {
     icpUdpData *q;
-    int x;
-    int delay;
 
     while ((q = IcpQueueHead) != NULL) {
-        delay = tvSubUsec(q->queue_time, current_time);
+        int delay = tvSubUsec(q->queue_time, current_time);
         /* increment delay to prevent looping */
-        x = icpUdpSend(fd, q->address, (icp_common_t *) q->msg, q->logcode, ++delay);
+        const int x = icpUdpSend(fd, q->address, (icp_common_t *) q->msg, q->logcode, ++delay);
         IcpQueueHead = q->next;
-        safe_free(q);
+        xfree(q);
 
         if (x < 0)
             break;
