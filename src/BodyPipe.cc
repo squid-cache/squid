08c31d8b4b283b36ec07ce66f590ba31e7b652b2
Automatic source maintenance <squidadm@squid-cache.org>
2010-09-23 23:10:27 -0600

    SourceFormat Enforcement

--- src/BodyPipe.cc
+++ src/BodyPipe.cc
@@ -261,12 +261,12 @@ BodyPipe::expectNoConsumption()
 {
     Must(!theConsumer);
     if (!abortedConsumption && !exhausted()) {
-            AsyncCall::Pointer call= asyncCall(91, 7,
-                                               "BodyProducer::noteBodyConsumerAborted",
-                                               BodyProducerDialer(theProducer,
-                                                                  &BodyProducer::noteBodyConsumerAborted, this));
-            ScheduleCallHere(call);
-            abortedConsumption = true;
+        AsyncCall::Pointer call= asyncCall(91, 7,
+                                           "BodyProducer::noteBodyConsumerAborted",
+                                           BodyProducerDialer(theProducer,
+                                                              &BodyProducer::noteBodyConsumerAborted, this));
+        ScheduleCallHere(call);
+        abortedConsumption = true;
     }
 }
 
