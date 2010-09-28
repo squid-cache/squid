233eb84f2098007d771fb40006f02d2b67dd7723
Alex Rousskov <rousskov@measurement-factory.com>
2010-09-28 09:20:36 -0600

    HTTP Compliance: Reply with an error if required validation fails.
    RFC 2616 says that proxy MUST not use stale entries that have s-maxage,
    proxy-revalidate, or must-revalidate cache-directive.
    
    Add new fail_on_validation_err request flag to store result from
    refreshCheck().  It is needed to avoid refreshLimits() recalculation in
    clientReplyContext::handleIMSReply().
    
    Split LOG_TCP_REFRESH_FAIL into LOG_TCP_REFRESH_FAIL_OLD (stale reply sent)
    and LOG_TCP_REFRESH_FAIL_ERR (error forwarded). However, both are still logged
    as TCP_REFRESH_FAIL for backward-compatibility with external scripts and such.
    We may decide to start logging more detailed codes later.
    
    Co-Advisor test cases:
        test_case/rfc2616/noSrv-hit-must-reval-s-maxage-resp
        test_case/rfc2616/noSrv-hit-must-reval-proxy-revalidate-resp
        test_case/rfc2616/noSrv-hit-must-reval-must-revalidate-resp

--- src/refresh.cc
+++ src/refresh.cc
@@ -277,6 +277,7 @@ refreshCheck(const StoreEntry * entry, HttpRequest * request, time_t delta)
 #endif
        ) {
         debugs(22, 3, "refreshCheck: YES: Must revalidate stale response");
+        request->flags.fail_on_validation_err = 1;
         return STALE_MUST_REVALIDATE;
     }
 
