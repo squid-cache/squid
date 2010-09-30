55e051ff3967189be03a6735c8439beec502f09a
Amos Jeffries <amosjeffries@squid-cache.org>
2010-09-29 19:12:15 -0600

    Documentation: display default-if-none instead of 'none' defaults.
    Updates the parser to accept DEFAULT_IF_NONE: as a valid default value
    instead of requiring "DEFAULT: none" to be explicitly configured.
    
    This removes several cases from the docs where 'none' is displayed but
    means 'no default' rather than the text value of "none" being possible.

--- src/cf_gen.cc
+++ src/cf_gen.cc
@@ -562,27 +562,25 @@ gen_default(Entry * head, FILE * fp)
             continue;
         }
 
-        if (entry->default_value == NULL) {
+        if (entry->default_value == NULL && entry->default_if_none == NULL) {
             fprintf(stderr, "NO DEFAULT FOR %s\n", entry->name);
             rc |= 1;
             continue;
         }
 
-        assert(entry->default_value);
-
-        if (entry->ifdef)
-            fprintf(fp, "#if %s\n", entry->ifdef);
-
-        if (strcmp(entry->default_value, "none") == 0) {
+        if (entry->default_value == NULL || strcmp(entry->default_value, "none") == 0) {
             fprintf(fp, "\t/* No default for %s */\n", entry->name);
         } else {
+            if (entry->ifdef)
+                fprintf(fp, "#if %s\n", entry->ifdef);
+
             fprintf(fp, "\tdefault_line(\"%s %s\");\n",
                     entry->name,
                     entry->default_value);
-        }
 
-        if (entry->ifdef)
-            fprintf(fp, "#endif\n");
+            if (entry->ifdef)
+                fprintf(fp, "#endif\n");
+        }
     }
 
     fprintf(fp, "\tcfg_filename = NULL;\n");
