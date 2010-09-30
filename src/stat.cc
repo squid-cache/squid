126610d0085488afac3e7ff4b7a696f120bfa18b
Automatic source maintenance <squidadm@squid-cache.org>
2010-09-29 18:21:50 -0600

    SourceFormat Enforcement

--- src/stat.cc
+++ src/stat.cc
@@ -629,7 +629,7 @@ info_get(StoreEntry * sentry)
                       (long)(ms.bytes_total >> 10));
 
     storeAppendPrintf(sentry, "\tTotal free:            %6ld KB %d%%\n",
-                     (long)(ms.bytes_free >> 10), Math::intPercent(ms.bytes_free, ms.bytes_total));
+                      (long)(ms.bytes_free >> 10), Math::intPercent(ms.bytes_free, ms.bytes_total));
 
 #elif HAVE_MALLINFO && HAVE_STRUCT_MALLINFO
 
