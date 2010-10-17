624264c8a24dcc5ef3855277fa0c4e10d90763b6
Amos Jeffries <amosjeffries@squid-cache.org>
2010-10-16 23:10:55 -0600

    real fix: printf portability issue in EDNS display

--- src/dns_internal.cc
+++ src/dns_internal.cc
@@ -665,7 +665,7 @@ idnsStats(StoreEntry * sentry)
     }
 
     if (Config.dns.packet_max > 0)
-        storeAppendPrintf(sentry, "DNS jumbo-grams: %Z Bytes\n", Config.dns.packet_max);
+        storeAppendPrintf(sentry, "DNS jumbo-grams: %Zd Bytes\n", Config.dns.packet_max);
     else
         storeAppendPrintf(sentry, "DNS jumbo-grams: not working\n");
 
