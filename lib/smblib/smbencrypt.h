1e2ede428014a6874ca101555f2a8ad8d977e63b
Automatic source maintenance <squidadm@squid-cache.org>
2010-10-16 18:18:41 -0600

    SourceFormat Enforcement

--- lib/smblib/smbencrypt.h
+++ lib/smblib/smbencrypt.h
@@ -5,9 +5,9 @@
 extern "C" {
 #endif
 
-void SMBencrypt(unsigned char *passwd, unsigned char *c8, unsigned char *p24);
-void SMBNTencrypt(unsigned char *passwd, unsigned char *c8, unsigned char *p24);
-void nt_lm_owf_gen(char *pwd, char *nt_p16, char *p16);
+    void SMBencrypt(unsigned char *passwd, unsigned char *c8, unsigned char *p24);
+    void SMBNTencrypt(unsigned char *passwd, unsigned char *c8, unsigned char *p24);
+    void nt_lm_owf_gen(char *pwd, char *nt_p16, char *p16);
 
 #ifdef __cplusplus
 }
