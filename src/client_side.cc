73534e47c2e42e7d64964ed9ec39d876db81f7ab
Amos Jeffries <amosjeffries@squid-cache.org>
2010-10-20 00:04:52 -0600

    Author: Alex Rousskov <rousskov@measurement-factory.com> HTTP Compliance: do not remove ETag header from partial responses
    RFC 2616 section 10.2.7 says that partial responses MUST include ETag
    header if it would have been sent in a 200 response to the same request.
    
    Co-Advisor test cases:
        test_case/rfc2616/scHdrs-206-include-explicit-withVary-noIfs
        test_case/rfc2616/scHdrs-206-include-explicit-woutVary-noIfs
        test_case/rfc2616/scHdrs-206-include-other-If-Range-strong

--- src/client_side.cc
+++ src/client_side.cc
@@ -1161,8 +1161,6 @@ ClientSocketContext::buildRangeHeader(HttpReply * rep)
         debugs(33, 3, "clientBuildRangeHeader: range spec count: " <<
                spec_count << " virgin clen: " << rep->content_length);
         assert(spec_count > 0);
-        /* ETags should not be returned with Partial Content replies? */
-        hdr->delById(HDR_ETAG);
         /* append appropriate header(s) */
 
         if (spec_count == 1) {
