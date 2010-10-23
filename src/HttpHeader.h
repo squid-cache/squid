3130202d6c4ba48c31587826369a6c6279d68831
Amos Jeffries <amosjeffries@squid-cache.org>
2010-10-23 07:39:03 -0600

    Author: Alex Rousskov <rousskov@measurement-factory.com> HTTP Compliance: add appropriate Warnings if serving a stale hit.
    Per RFC 2616, we MUST add "110 Response is stale" Warning if serving a
    stale reply for any reason, including configured overrides. We MUST add
    "111 Revalidation failed" Warning if serving a stale reply because an
    attempt to revalidate the response failed, due to an inability to reach
    the server.
    
    The patch adds a new stale_if_hit request flag, which is set in
    refreshCheckHTTP() when entry freshness is calculated. refreshCheckHTTP()
    is now called in offline mode, to set stale_if_hit properly. We check for
    the offline mode before returning from refreshCheckHTTP() to preserve the
    original logic.
    
    refreshCheckHTTP() is no longer called for internal requests, to avoid
    setting of stale_if_hit flag. It did not do anything important for
    internal requests anyway.
    
    Co-Advisor test cases:
       test_case/rfc2616/noSrv-hit-stale-max-age-req
       test_case/rfc2616/ccReqDirMsg-max-stale-warning

--- src/HttpHeader.h
+++ src/HttpHeader.h
@@ -239,6 +239,7 @@ public:
     void putContRange(const HttpHdrContRange * cr);
     void putRange(const HttpHdrRange * range);
     void putSc(HttpHdrSc *sc);
+    void putWarning(const int code, const char *const text); ///< add a Warning header
     void putExt(const char *name, const char *value);
     int getInt(http_hdr_type id) const;
     int64_t getInt64(http_hdr_type id) const;
