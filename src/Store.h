38d604be55d19924063cfbe1e99c327e31b5f032
Amos Jeffries <squid3@treenet.co.nz>
2010-12-06 06:14:56 -0700

    Fix many C/C++ ABI warnings

--- src/Store.h
+++ src/Store.h
@@ -364,7 +364,7 @@ SQUIDCEXTERN int expiresMoreThan(time_t, time_t);
 SQUIDCEXTERN void storeAppendPrintf(StoreEntry *, const char *,...) PRINTF_FORMAT_ARG2;
 
 /// \ingroup StoreAPI
-SQUIDCEXTERN void storeAppendVPrintf(StoreEntry *, const char *, va_list ap);
+extern void storeAppendVPrintf(StoreEntry *, const char *, va_list ap);
 
 /// \ingroup StoreAPI
 SQUIDCEXTERN int storeTooManyDiskFilesOpen(void);
