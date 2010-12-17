8266857a4529660c7ba4c6bf5811d67785d5a43f
Amos Jeffries <squid3@treenet.co.nz>
2010-12-16 22:46:37 -0700

    Add docs and prevent parse loops on client_request_buffer_max

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -908,6 +908,14 @@ configDoConfigure(void)
     }
 
 #endif
+
+    // prevent infinite fetch loops in the request parser
+    // due to buffer full but not enough data recived to finish parse
+    if (Config.maxRequestBufferSize <= Config.maxRequestHeaderSize) {
+        fatalf("Client request buffer of %d bytes cannot hold a request with %d bytes of headers." \
+               " Change client_request_buffer_max or request_header_max_size limits.",
+               Config.maxRequestBufferSize, Config.maxRequestHeaderSize);
+    }
 }
 
 /** Parse a line containing an obsolete directive.
