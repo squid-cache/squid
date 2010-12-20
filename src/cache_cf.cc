8faddbd6b082da1601ba63e82af64fad9fa29f36
Amos Jeffries <squid3@treenet.co.nz>
2010-12-19 21:41:39 -0700

    Portage amendments to obsoletion processing

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -757,12 +757,6 @@ void
 parse_obsolete(const char *name)
 {
     // Directives which have been radically changed rather than removed
-    if (!strcmp(name, "url_rewrite_concurrency")) {
-        int cval;
-        parse_int(&cval);
-        debugs(3, DBG_CRITICAL, "WARNING: url_rewrite_concurrency upgrade overriding url_rewrite_children settings.");
-        Config.redirectChildren.concurrency = cval;
-    }
 }
 
 /* Parse a time specification from the config file.  Store the
