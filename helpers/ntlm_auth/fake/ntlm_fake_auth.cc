084edcefaa3ee5f7972dbe08dd62034bdc0af019
Francesco Chemolli <kinkie@squid-cache.org>
2011-01-25 22:30:11 +0100

    Fixed bug in NTLM auth helper which would show up on non-gcc compilers

--- helpers/ntlm_auth/fake/ntlm_fake_auth.cc
+++ helpers/ntlm_auth/fake/ntlm_fake_auth.cc
@@ -84,7 +84,7 @@
 #else
 /* no gcc, no debugging. varargs macros are a gcc extension */
 #define SEND2(X,Y) debug("sending '" X "' to squid\n",Y); printf(X "\n",Y);
-#define SEND4(X,Y,Z,W) debug("sending '" X "' to squid\n",Y,Z); printf(X "\n",Y,Z);
+#define SEND4(X,Y,Z,W) debug("sending '" X "' to squid\n",Y,Z,W); printf(X "\n",Y,Z,W);
 #endif
 
 const char *authenticate_ntlm_domain = "WORKGROUP";
