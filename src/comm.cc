b8897f4a7e6464050e744d8812104782b5b4f34b
Amos Jeffries <squid3@treenet.co.nz>
2011-01-26 16:47:13 +1300

    Bug 2581, Bug 3081, Bug 2948: various TCP socket connection problems
    Bug 3081:
      During conversion of listening socket handlers to AsyncCalls a violation
    of the AsyncCall API was introduced. Resulting in occasional crashes from
    invalid re-use of call objects.
    
    
    This implements a TcpAcceptor async job which receives a listening socket
    and a CallSubscription. For every connection attempt on the listener socket
    a new AsyncCall is spawned from the subscription template.
    
    Initial users are the HTTP and HTTPS listening sockets and FTP data channel.
    
    In order to implement this job in FTP the logics surrounding data channel
    handling had to be extended and reworked. Fixing bug 2948 and 2581 in the
    process.

--- src/comm.cc
+++ src/comm.cc
@@ -40,9 +40,9 @@
 #include "comm/AcceptLimiter.h"
 #include "comm/comm_internal.h"
 #include "comm/IoCallback.h"
-#include "comm/Write.h"
-#include "comm/ListenStateData.h"
 #include "comm/Loops.h"
+#include "comm/Write.h"
+#include "comm/TcpAcceptor.h"
 #include "CommIO.h"
 #include "CommRead.h"
 #include "ConnectionDetail.h"
@@ -144,7 +144,7 @@ fd_debug_t *fdd_table = NULL;
 bool
 isOpen(const int fd)
 {
-    return fd_table[fd].flags.open != 0;
+    return fd >= 0 && fd_table[fd].flags.open != 0;
 }
 
 /**
