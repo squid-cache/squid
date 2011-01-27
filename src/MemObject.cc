83af6fa2d157bff23080c87724c71c147d420395
Amos Jeffries <squid3@treenet.co.nz>
2011-01-28 01:23:25 +1300

    Author: Steven Lawrance <squid@moonlightdesign.org> Bug: 2968: Show the Vary: headers information in cachemgr objects report

--- src/MemObject.cc
+++ src/MemObject.cc
@@ -207,10 +207,12 @@ struct StoreClientStats : public unary_function<store_client, void> {
 };
 
 void
-MemObject::stat (MemBuf * mb) const
+MemObject::stat(MemBuf * mb) const
 {
     mb->Printf("\t%s %s\n",
                RequestMethodStr(method), log_url);
+    if (vary_headers)
+        mb->Printf("\tvary_headers: %s\n", vary_headers);
     mb->Printf("\tinmem_lo: %"PRId64"\n", inmem_lo);
     mb->Printf("\tinmem_hi: %"PRId64"\n", data_hdr.endOffset());
     mb->Printf("\tswapout: %"PRId64" bytes queued\n",
