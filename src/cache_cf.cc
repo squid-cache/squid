152bc2a2218712d96d63af8571f0d5e105d87dfe
Amos Jeffries <squid3@treenet.co.nz>
2011-01-28 21:11:10 -0700

    Windows: fix code wrappers for Cygwin and generic Win32 code. New MingW wrapper.
    _SQUID_WIN32_ was being used to wrap all code built specific for Windows
    but with any compiler on that platform.
     - rename to _SQUID_WINDOWS_ to match OS wrpper naming convention.
     - compact several macros using verbose test: (cygwin or Ming or any)
    
    Cleans up all affected tests to match current precompiler code style.
    
    Also cleans up all tests involving _SQUID_CYGWN_ to match code styles.
    
    Defines new wrapper macro for MinGW environment specific code.

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -419,10 +419,8 @@ parseOneConfigFile(const char *file_name, unsigned int depth)
     if (fp == NULL)
         fatalf("Unable to open configuration file: %s: %s", file_name, xstrerror());
 
-#ifdef _SQUID_WIN32_
-
+#if _SQUID_WINDOWS_
     setmode(fileno(fp), O_TEXT);
-
 #endif
 
     SetConfigFilename(file_name, bool(is_pipe));
--- src/mime.cc
+++ src/mime.cc
@@ -279,9 +279,8 @@ mimeInit(char *filename)
         return;
     }
 
-#ifdef _SQUID_WIN32_
+#if _SQUID_WINDOWS_
     setmode(fileno(fp), O_TEXT);
-
 #endif
 
     mimeFreeMemory();
