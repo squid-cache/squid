48af0f50d52835caeae353f56329cf437606eb59
Amos Jeffries <squid3@treenet.co.nz>
2011-01-28 19:43:16 -0700

    Author: Christos Tsantilas <chtsanti@users.sourceforge.net> Fix: Wrong value displayed for Storage Swap size in mgr::info page
    The store_swap_size/stats.store_swap_size holds kilobytes, it is wrong to
    divide by 1024 these values.

--- src/stat.cc
+++ src/stat.cc
@@ -749,7 +749,7 @@ DumpInfo(Mgr::InfoActionData& stats, StoreEntry* sentry)
                       stats.request_hit_disk_ratio60 / fct);
 
     storeAppendPrintf(sentry, "\tStorage Swap size:\t%.0f KB\n",
-                      stats.store_swap_size / 1024);
+                      stats.store_swap_size);
 
     storeAppendPrintf(sentry, "\tStorage Swap capacity:\t%4.1f%% used, %4.1f%% free\n",
                       Math::doublePercent(stats.store_swap_size, stats.store_swap_max_size),
