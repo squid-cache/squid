5521b73afee6443dc187e46a852b47cdecfc4831
Amos Jeffries <squid3@treenet.co.nz>
2011-01-31 19:15:16 -0700

    Fix external_acl_type grace= option
    Due to race conditions between concurrent requests this is still not a
    complete fix. But reduces the unwanted re-use of helper responses from
    all connections arriving in a whole second which match the ACL key down
    to just those that arrive within the reply lag time of the helper.
    
    The grace option has a resolution of 1 second.

--- src/external_acl.cc
+++ src/external_acl.cc
@@ -1051,7 +1051,7 @@ external_acl_grace_expired(external_acl * def, external_acl_entry * entry)
     ttl = entry->result == 1 ? def->ttl : def->negative_ttl;
     ttl = (ttl * (100 - def->grace)) / 100;
 
-    if (entry->date + ttl < squid_curtime)
+    if (entry->date + ttl <= squid_curtime)
         return 1;
     else
         return 0;
