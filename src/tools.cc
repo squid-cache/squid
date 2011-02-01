4bbf70548ca8b138ec77dc6f667b338d9ea780fb
Amos Jeffries <squid3@treenet.co.nz>
2011-01-31 18:47:36 -0700

    Fix Solaris getrusage wrapping

--- src/tools.cc
+++ src/tools.cc
@@ -235,13 +235,13 @@ squid_getrusage(struct rusage *r)
 
     memset(r, '\0', sizeof(struct rusage));
 #if HAVE_GETRUSAGE && defined(RUSAGE_SELF)
-#ifdef _SQUID_SOLARIS_
+#if _SQUID_SOLARIS_
     /* Solaris 2.5 has getrusage() permission bug -- Arjan de Vet */
     enter_suid();
 #endif
 
     getrusage(RUSAGE_SELF, r);
-#ifdef _SQUID_SOLARIS_
+#if _SQUID_SOLARIS_
 
     leave_suid();
 #endif
