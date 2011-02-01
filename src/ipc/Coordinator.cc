5ce457f640936b62a45df6afa3f84576689ba87e
Alex Rousskov <rousskov@measurement-factory.com>
2011-02-01 01:22:59 -0700

    Preserve old registration tag when updating registration info.
    Sometimes, tagless strand registers self only after its module (like
    IpcIoFile) supplies a tag. We need to keep the tag for future tag searches
    to succeed.

--- src/ipc/Coordinator.cc
+++ src/ipc/Coordinator.cc
@@ -48,10 +48,14 @@ void Ipc::Coordinator::registerStrand(const StrandCoord& strand)
 {
     debugs(54, 3, HERE << "registering kid" << strand.kidId <<
            ' ' << strand.tag);
-    if (StrandCoord* found = findStrand(strand.kidId))
+    if (StrandCoord* found = findStrand(strand.kidId)) {
+        const String oldTag = found->tag;
         *found = strand;
-    else
+        if (oldTag.size() && !strand.tag.size())
+            found->tag = oldTag; // keep more detailed info (XXX?)
+    } else {
         strands_.push_back(strand);
+    }
 
     // notify searchers waiting for this new strand, if any
     typedef Searchers::iterator SRI;
