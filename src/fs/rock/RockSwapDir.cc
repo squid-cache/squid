cc9cd36f126362e4df605013143cbd9961cd5c6b
Alex Rousskov <rousskov@measurement-factory.com>
2011-02-01 13:18:27 -0700

    Use Blocking DiskIO module when runnining in a no-daemon mode.
    We cannot use IpcIo module in no-daemon mode because there are no diskers
    to communicate with. If our implementation is correct, IpcIo module should
    contain no shared map or other rock-specific manipulations and, hence,
    should not be required for Rock Store to work.

--- src/fs/rock/RockSwapDir.cc
+++ src/fs/rock/RockSwapDir.cc
@@ -173,10 +173,15 @@ Rock::SwapDir::init()
         map = new DirMap(path, eAllowed);
     }
 
-    DiskIOModule *m = DiskIOModule::Find("IpcIo"); // TODO: configurable?
-    assert(m);
-    io = m->createStrategy();
-    io->init();
+    const char *ioModule = UsingSmp() ? "IpcIo" : "Blocking";
+    if (DiskIOModule *m = DiskIOModule::Find(ioModule)) {
+        debugs(47,2, HERE << "Using DiskIO module: " << ioModule);
+        io = m->createStrategy();
+        io->init();
+    } else {
+        debugs(47,1, "Rock store is missing DiskIO module: " << ioModule);
+        fatal("Rock Store missing a required DiskIO module");
+    }
 
     theFile = io->newFile(filePath);
     theFile->open(O_RDWR, 0644, this);
