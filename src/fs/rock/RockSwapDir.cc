9fbaf41e8607e8360942b7761185f418111e9c16
Alex Rousskov <rousskov@measurement-factory.com>
2011-02-03 10:13:39 -0700

    Avoid assertions when mgr:storedir is requested before cache_dir has a map.

--- src/fs/rock/RockSwapDir.cc
+++ src/fs/rock/RockSwapDir.cc
@@ -624,9 +624,13 @@ Rock::SwapDir::statfs(StoreEntry &e) const
     storeAppendPrintf(&e, "Current Size: %"PRIu64" KB %.2f%%\n", cur_size,
                       100.0 * cur_size / max_size);
 
-    storeAppendPrintf(&e, "Maximum entries: %9d\n", map->entryLimit());
-    storeAppendPrintf(&e, "Current entries: %9d %.2f%%\n",
-        map->entryCount(), (100.0 * map->entryCount() / map->entryLimit()));
+    if (map) {
+        const int limit = map->entryLimit();
+        storeAppendPrintf(&e, "Maximum entries: %9d\n", limit);
+        if (limit > 0)
+            storeAppendPrintf(&e, "Current entries: %9d %.2f%%\n",
+                map->entryCount(), (100.0 * map->entryCount() / limit));
+    }    
 
     storeAppendPrintf(&e, "Pending operations: %d out of %d\n",
         store_open_disk_fd, Config.max_open_disk_fds);
