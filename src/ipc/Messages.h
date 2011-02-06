3191ba78f3e6a096e52eaaaa8732955438d1bfd7
Christos Tsantilas <chtsanti@users.sourceforge.net>
2011-02-06 21:50:52 +0200

    SMP SNMP
    In the case the SQUID_SNMP is disabled the MessageType enum list ends with a ","
    The gcc compiler does not have any problem with this but SunStudio return a
    parse error.

--- src/ipc/Messages.h
+++ src/ipc/Messages.h
@@ -16,8 +16,9 @@ namespace Ipc
 /// message class identifier
 typedef enum { mtNone = 0, mtRegistration,
                mtSharedListenRequest, mtSharedListenResponse,
-               mtCacheMgrRequest, mtCacheMgrResponse,
+               mtCacheMgrRequest, mtCacheMgrResponse
 #if SQUID_SNMP
+               ,
                mtSnmpRequest, mtSnmpResponse
 #endif
              } MessageType;
