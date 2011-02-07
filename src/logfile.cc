dc5471410906f51344288146e90c098272327067
Amos Jeffries <squid3@treenet.co.nz>
2011-02-07 05:19:41 -0700

    Author: Phil Oester <kernel@linuxace.com> Bug 3115: logging segfaults if access_log is set to a directory

--- src/logfile.cc
+++ src/logfile.cc
@@ -128,6 +128,8 @@ logfileOpen(const char *path, size_t bufsz, int fatal_flag)
                        "\tThe parent directory must be writeable by the\n"
                        "\tuser '%s', which is the cache_effective_user\n"
                        "\tset in squid.conf.", path, Config.effectiveUser);
+            } else if (EISDIR == errno && fatal_flag) {
+                fatalf("Cannot open '%s' because it is a directory, not a file.\n", path);
             } else {
                 debugs(50, 1, "logfileOpen: " << path << ": " << xstrerror());
                 return NULL;
