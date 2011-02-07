05c66bbf867cc7f7570a3e0308e7a1f079e09974
Amos Jeffries <squid3@treenet.co.nz>
2011-02-06 21:32:37 -0700

    Author: Phil Oester <kernel@linuxace.com> Bug 3115: stdio: log module segfaults if access_log is set to a directory

--- src/log/ModStdio.cc
+++ src/log/ModStdio.cc
@@ -206,6 +206,8 @@ logfile_mod_stdio_open(Logfile * lf, const char *path, size_t bufsz, int fatal_f
                    "\tThe parent directory must be writeable by the\n"
                    "\tuser '%s', which is the cache_effective_user\n"
                    "\tset in squid.conf.", path, Config.effectiveUser);
+        } else if (EISDIR == errno && fatal_flag) {
+            fatalf("Cannot open '%s' because it is a directory, not a file.\n", path);
         } else {
             debugs(50, DBG_IMPORTANT, "logfileOpen (stdio): " <<  path << ": " << xstrerror());
             return 0;
