e6b8a084332828c8d2ca5deeed0a55ba292db9dd
Amos Jeffries <squid3@treenet.co.nz>
2011-02-07 03:54:46 -0700

    Author: Christos Tsantilas <chtsanti@users.sourceforge.net> Range::size() returned value type
    This patch allow the user of the Range template class to define the type of 
    the returned value of Range::size() method, adding a second template argument.
    The default type of the return value of Range::size() method is size_t, which
    in most cases is enough (but not always, eg  HttpRange).
    This patch will allow Range template to be used with non numeric types.

--- src/client_side.cc
+++ src/client_side.cc
@@ -1055,7 +1055,7 @@ ClientSocketContext::packRange(StoreIOBuffer const &source, MemBuf * mb)
              * intersection of "have" and "need" ranges must not be empty
              */
             assert(http->out.offset < i->currentSpec()->offset + i->currentSpec()->length);
-            assert(http->out.offset + available.size() > i->currentSpec()->offset);
+            assert(http->out.offset + (int64_t)available.size() > i->currentSpec()->offset);
 
             /*
              * put boundary and headers at the beginning of a range in a
@@ -1113,7 +1113,7 @@ ClientSocketContext::packRange(StoreIOBuffer const &source, MemBuf * mb)
         /* adjust for not to be transmitted bytes */
         http->out.offset = nextOffset;
 
-        if (available.size() <= skip)
+        if (available.size() <= (uint64_t)skip)
             return;
 
         available.start += skip;
