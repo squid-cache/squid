893c86b4648354ab8a8362c0fb5afccc7e8a21c7
Alex Rousskov <rousskov@measurement-factory.com>
2011-02-13 22:00:44 -0700

    Added an optimization TODO comment. No runtime changes.

--- src/stmem.cc
+++ src/stmem.cc
@@ -198,6 +198,7 @@ mem_hdr::internalAppend(const char *data, int len)
 mem_node *
 mem_hdr::getBlockContainingLocation (int64_t location) const
 {
+    // Optimize: do not create a whole mem_node just to store location
     mem_node target (location);
     target.nodeBuffer.length = 1;
     mem_node *const *result = nodes.find (&target, NodeCompare);
