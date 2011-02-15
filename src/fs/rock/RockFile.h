c728b6f9fff43c9e8a2c4af5bf4916dcb4c36fa9
Alex Rousskov <rousskov@measurement-factory.com>
2011-02-14 21:09:58 -0700

    * Removed old Rock Store hack to update swap_file_sz before it is packed into store entry disk "header". Prepend our own disk to each db cell. Eventually, the same header may be used to store "next page" or other info.
    Modern Squid code should not use packed swap_file_sz or object_sz because
    neither can be unknown at the packing time.  The storage code should either
    record its own size information (Rock Store does that now) or rely on
    3rd-party metadata (UFS uses fstat(2) system call).
    
    
    * Do not promise to read a being-written entry. Use peekAtReader() to check
    that we can read the presumably locked (by us) entry.
    
    It is probably possible to queue a reader somehow and wait for the entry to
    become readable or even to read initial pages as we write (when we start doing
    incremental writing) but it would be rather complex and expensive to
    synchronize such shared read/write access across workers.
    
    
    * Fixed ::StoreIOState::offset_ Rock maintenance. The old code probably
    misinterpret offset_ meaning and kept the disk offset there. It should have
    been just the distance from the beginning of the db cell (not db file)
    instead. It probably worked because we write everything at once and the
    offset_ values were usually big enough to pass "wrote everything" checks.
    
    
    * Extracted storeRebuildParseEntry from storeRebuildLoadEntry.
    
    This allows Rock Store to analyze and skip disk cell header before giving
    the packed store entry info to the common core code.
    
    
    * Report more disk stats. Needs more work, especially in SMP reporting case.
    Map stats are only reported for tiny maps because scanning the entire map may
    be too expensive for bigger maps (is it worth the overhead to maintain these
    stats all the time instead of computing them from scratch on-demand?).
    
    
    * Moved StoreEntryBasics into Rock namespace.
    
    
    * Be more consistent in setting Rock::IoState's swap_filen, swap_dirn,
    diskOffset, and payloadEnd.
    
    
    * Renamed vague (and confusing after the header was added to the db cell)
    entrySize to more specific payloadEnd.
    
    
    * Synced with r11228 changes (making Store work with SMP-shared max-size
    cache_dirs).

---
+++ src/fs/rock/RockFile.h
@@ -0,0 +1,24 @@
+#ifndef SQUID_FS_ROCK_DB_CELL_H
+#define SQUID_FS_ROCK_DB_CELL_H
+
+// XXX: rename to fs/rock/RockDbCell.{cc,h}
+
+namespace Rock {
+
+/// \ingroup Rock
+/// meta-information at the beginning of every db cell
+class DbCellHeader
+{
+public:
+    DbCellHeader(): payloadSize(0), reserved(0) {}
+
+    /// whether the freshly loaded header fields make sense
+    bool sane() const { return payloadSize >= 0 && reserved == 0; }
+
+    int64_t payloadSize; ///< cell contents size excluding this header
+    int64_t reserved; ///< reserved for future use (next cell pointer?)
+};
+
+} // namespace Rock
+
+#endif /* SQUID_FS_ROCK_DB_CELL_H */
