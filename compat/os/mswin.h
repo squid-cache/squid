7427ec379ce0230ca5fb1bab1ce4526f21b48d10
Amos Jeffries <squid3@treenet.co.nz>
2011-02-19 03:06:33 +1300

    Correct check for getpagesize definition

--- compat/os/mswin.h
+++ compat/os/mswin.h
@@ -825,7 +825,7 @@ SQUIDCEXTERN struct group * getgrnam(char *unused);
 #define getgid(X)   static_cast<gid_t>(100)
 #define setgid(X)   (void)0
 
-#if !defined(getpagesize)
+#if !HAVE_GETPAGESIZE
 /* Windows may lack getpagesize() prototype */
 SQUIDCEXTERN size_t getpagesize();
 #define HAVE_GETPAGESIZE 2
