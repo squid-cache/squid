57198694c7da5736709528df9e21fee9d056543d
Automatic source maintenance <squidadm@squid-cache.org>
2011-03-16 18:12:57 -0600

    SourceFormat Enforcement

--- src/ssl/ErrorDetail.cc
+++ src/ssl/ErrorDetail.cc
@@ -182,7 +182,7 @@ const char *Ssl::ErrorDetail::err_code() const
     static char tmpBuffer[64];
     const char *err = getErrorName(error_no);
     if (!err) {
-        snprintf(tmpBuffer, 64, "%d", (int)error_no); 
+        snprintf(tmpBuffer, 64, "%d", (int)error_no);
         err = tmpBuffer;
     }
     return err;
