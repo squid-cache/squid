e1177f493ae36c6c59535d1b85430b21fa58e365
Alex Rousskov <rousskov@measurement-factory.com>
2011-03-31 14:43:33 -0600

    Initial implementation of atomic container for free page numbers in a segment.

---
+++ src/ipc/mem/PageStack.cc
@@ -0,0 +1,95 @@
+/*
+ * $Id$
+ *
+ * DEBUG: section 54    Interprocess Communication
+ *
+ */
+
+#include "config.h"
+
+#include "base/TextException.h"
+#include "ipc/mem/PageStack.h"
+
+/// used to mark a stack slot available for storing free page offsets
+const Ipc::Mem::PageStack::Value Writable = 0;
+
+
+Ipc::Mem::PageStack::PageStack(const String &id, const unsigned int capacity):
+    shm(id.termedBuf())
+{
+    shm.create(sizeof(Shared::Item) * capacity + sizeof(Shared));
+    assert(shm.mem());
+    shared = new (shm.mem()) Shared(capacity);
+}
+
+Ipc::Mem::PageStack::PageStack(const String &id): shm(id.termedBuf())
+{
+    shm.open();
+    shared = reinterpret_cast<Shared *>(shm.mem());
+    assert(shared);
+}
+
+/*
+ * TODO: We currently rely on the theLastReadable hint during each
+ * loop iteration. We could also use hint just for the start position:
+ * (const Offset start = theLastReadable) and then scan the stack
+ * sequentially regardless of theLastReadable changes by others. Which
+ * approach is better? Same for push().
+ */
+bool
+Ipc::Mem::PageStack::pop(Value &value)
+{
+    // we may fail to dequeue, but be conservative to prevent long searches
+    --shared->theSize;
+
+    // find a Readable slot, starting with theLastReadable and going left
+    while (shared->theSize >= 0) {
+        const Offset idx = shared->theLastReadable;
+        // mark the slot at ids Writable while extracting its current value
+        value = shared->theItems[idx].fetchAndAnd(0); // works if Writable is 0
+        const bool popped = value != Writable;
+        // theItems[idx] is probably not Readable [any more]
+        // help others (if we popped) or go left ourselves (if we did not)
+        shared->theLastReadable.swap_if(idx, shared->prev(idx)); // may fail or lie
+        if (popped) {
+            // the slot we emptied may already be filled, but that is OK
+            shared->theFirstWritable = idx; // may lie
+            return true;
+        }
+        // TODO: report suspiciously long loops
+    }
+
+    ++shared->theSize;
+    return false;
+}
+
+void
+Ipc::Mem::PageStack::push(const Value value)
+{
+    Must(value != Writable);
+    // find a Writable slot, starting with theFirstWritable and going right
+    while (shared->theSize < shared->theCapacity) {
+        const Offset idx = shared->theFirstWritable;
+        const bool pushed = shared->theItems[idx].swap_if(Writable, value);
+        // theItems[idx] is probably not Writable [any more];
+        // help others (if we pushed) or go right ourselves (if we did not)
+        shared->theFirstWritable.swap_if(idx, shared->next(idx)); // may fail or lie
+        if (pushed) {
+            // the enqueued value may already by gone, but that is OK
+            shared->theLastReadable = idx; // may lie
+            ++shared->theSize;
+            return;
+        }
+        // TODO: report suspiciously long loops
+    }
+    Must(false); // the number of pages cannot exceed theCapacity
+}
+
+Ipc::Mem::PageStack::Shared::Shared(const unsigned int aCapacity):
+    theCapacity(aCapacity), theSize(theCapacity),
+    theLastReadable(prev(theSize)), theFirstWritable(next(theLastReadable))
+{
+    // initially, all pages are free
+    for (Offset i = 0; i < theSize; ++i)
+        theItems[i] = i + 1; // skip page number zero to keep numbers positive
+}
