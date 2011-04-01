341d4e507aed73f24e14623c18f795efb1b41d91
Alex Rousskov <rousskov@measurement-factory.com>
2011-04-01 08:39:24 -0600

    Log all transactions including those with uncertain status or no sent response.
    Excluding those transactions from access log hides valuable information.
    
    For example, it hides certain aborted client transactions which sometimes
    indicate Squid bugs or configuration problems that the administrator can fix.
    It also leads to wrong statistics reporting if the reporting tools are based
    on access logs information.
    
    The change just removes the logging guard and adds debugging. The logging
    code itself is unchanged except for indenting.

--- src/client_side.cc
+++ src/client_side.cc
@@ -615,73 +615,74 @@ prepareLogWithRequestDetails(HttpRequest * request, AccessLogEntry * aLogEntry)
 void
 ClientHttpRequest::logRequest()
 {
-    if (out.size || logType) {
-        al.icp.opcode = ICP_INVALID;
-        al.url = log_uri;
-        debugs(33, 9, "clientLogRequest: al.url='" << al.url << "'");
+    if (!out.size && !logType)
+        debugs(33, 5, HERE << "logging half-baked transaction: " << log_uri);
 
-        if (al.reply) {
-            al.http.code = al.reply->sline.status;
-            al.http.content_type = al.reply->content_type.termedBuf();
-        } else if (loggingEntry() && loggingEntry()->mem_obj) {
-            al.http.code = loggingEntry()->mem_obj->getReply()->sline.status;
-            al.http.content_type = loggingEntry()->mem_obj->getReply()->content_type.termedBuf();
-        }
+    al.icp.opcode = ICP_INVALID;
+    al.url = log_uri;
+    debugs(33, 9, "clientLogRequest: al.url='" << al.url << "'");
 
-        debugs(33, 9, "clientLogRequest: http.code='" << al.http.code << "'");
+    if (al.reply) {
+        al.http.code = al.reply->sline.status;
+        al.http.content_type = al.reply->content_type.termedBuf();
+    } else if (loggingEntry() && loggingEntry()->mem_obj) {
+        al.http.code = loggingEntry()->mem_obj->getReply()->sline.status;
+        al.http.content_type = loggingEntry()->mem_obj->getReply()->content_type.termedBuf();
+    }
 
-        if (loggingEntry() && loggingEntry()->mem_obj)
-            al.cache.objectSize = loggingEntry()->contentLen();
+    debugs(33, 9, "clientLogRequest: http.code='" << al.http.code << "'");
 
-        al.cache.caddr.SetNoAddr();
+    if (loggingEntry() && loggingEntry()->mem_obj)
+        al.cache.objectSize = loggingEntry()->contentLen();
 
-        if (getConn() != NULL) al.cache.caddr = getConn()->log_addr;
+    al.cache.caddr.SetNoAddr();
 
-        al.cache.requestSize = req_sz;
-        al.cache.requestHeadersSize = req_sz;
+    if (getConn() != NULL) al.cache.caddr = getConn()->log_addr;
 
-        al.cache.replySize = out.size;
-        al.cache.replyHeadersSize = out.headers_sz;
+    al.cache.requestSize = req_sz;
+    al.cache.requestHeadersSize = req_sz;
 
-        al.cache.highOffset = out.offset;
+    al.cache.replySize = out.size;
+    al.cache.replyHeadersSize = out.headers_sz;
 
-        al.cache.code = logType;
+    al.cache.highOffset = out.offset;
 
-        al.cache.msec = tvSubMsec(start_time, current_time);
+    al.cache.code = logType;
 
-        if (request)
-            prepareLogWithRequestDetails(request, &al);
+    al.cache.msec = tvSubMsec(start_time, current_time);
 
-        if (getConn() != NULL && getConn()->rfc931[0])
-            al.cache.rfc931 = getConn()->rfc931;
+    if (request)
+        prepareLogWithRequestDetails(request, &al);
+
+    if (getConn() != NULL && getConn()->rfc931[0])
+        al.cache.rfc931 = getConn()->rfc931;
 
 #if USE_SSL && 0
 
-        /* This is broken. Fails if the connection has been closed. Needs
-         * to snarf the ssl details some place earlier..
-         */
-        if (getConn() != NULL)
-            al.cache.ssluser = sslGetUserEmail(fd_table[getConn()->fd].ssl);
+    /* This is broken. Fails if the connection has been closed. Needs
+     * to snarf the ssl details some place earlier..
+     */
+    if (getConn() != NULL)
+        al.cache.ssluser = sslGetUserEmail(fd_table[getConn()->fd].ssl);
 
 #endif
 
-        ACLFilledChecklist *checklist = clientAclChecklistCreate(Config.accessList.log, this);
+    ACLFilledChecklist *checklist = clientAclChecklistCreate(Config.accessList.log, this);
 
-        if (al.reply)
-            checklist->reply = HTTPMSGLOCK(al.reply);
+    if (al.reply)
+        checklist->reply = HTTPMSGLOCK(al.reply);
 
-        if (!Config.accessList.log || checklist->fastCheck()) {
-            if (request)
-                al.adapted_request = HTTPMSGLOCK(request);
-            accessLogLog(&al, checklist);
-            updateCounters();
+    if (!Config.accessList.log || checklist->fastCheck()) {
+        if (request)
+            al.adapted_request = HTTPMSGLOCK(request);
+        accessLogLog(&al, checklist);
+        updateCounters();
 
-            if (getConn() != NULL)
-                clientdbUpdate(getConn()->peer, logType, AnyP::PROTO_HTTP, out.size);
-        }
-
-        delete checklist;
+        if (getConn() != NULL)
+            clientdbUpdate(getConn()->peer, logType, AnyP::PROTO_HTTP, out.size);
     }
+
+    delete checklist;
 
     accessLogFreeMemory(&al);
 }
