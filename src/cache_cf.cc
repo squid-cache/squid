4400a153642e311516ca1ac3fb53df5dbcbb08f4
Amos Jeffries <squid3@treenet.co.nz>
2011-04-06 01:04:11 +1200

    Display ERROR in cache.log for invalid configured paths
    The validator that checks system paths for files and directories in the
    configuration file sends error messages to stderr. It should send them to
    cache.log for the admin to see easily.
    
    Also, this makes the error display as FATAL ERROR when using -k parse to
    indicate that it is fatal to the startup. Other management signals where
    it is not necessarily fatal will only display as an ERROR.

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -4027,10 +4027,13 @@ requirePathnameExists(const char *name, const char *path)
     }
 
     if (stat(path, &sb) < 0) {
+        debugs(0, DBG_CRITICAL, (opt_parse_cfg_only?"FATAL ":"") << "ERROR: " << name << " " << path << ": " << xstrerror());
+        // keep going to find more issues if we are only checking the config file with "-k parse"
+        if (opt_parse_cfg_only)
+            return;
+        // this is fatal if it is found during startup or reconfigure
         if (opt_send_signal == -1 || opt_send_signal == SIGHUP)
             fatalf("%s %s: %s", name, path, xstrerror());
-        else
-            fprintf(stderr, "WARNING: %s %s: %s\n", name, path, xstrerror());
     }
 }
 
