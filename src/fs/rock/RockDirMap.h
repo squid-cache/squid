c8fa0ee2c9445ecec417bad28c36400903a3a455
Alex Rousskov <rousskov@measurement-factory.com>
2011-04-08 22:24:06 -0600

    Split Rock-only Rock::DirMap into Rock::DirMap and reusable Ipc pieces which a shared memory cache implementation can use:
    Ipc::StoreMap is responsible for maintaining a collection of lockable slots,
    each with readable/writeable/free state and a "waiting to be free" flag. Kids
    of this class can add more metadata (in parallel structures using the same
    index as primary slots). I tried extending the slots themselves, but that 
    turned out to be more complex/messy.
    
    Ipc::ReadWriteLock is a basic multiple readers, single writer lock.  Its
    earlier implementation inside Rock::DirMap mixed slot locking and slot
    state/flags. That simplified the caller code a little, but the current simpler
    class is easier to understand and reuse.
    
    Rock::DirMap now just adds Rock::DbCellHeader metadata to Ipc::StoreMap slots.
    
    Simplified mapping API by reducing the number of similar-but-different
    methods. For example, instead of putAt, the caller can use an
    openForWriting/closeForWriting pair. This helps with moving custom metadata
    manipulations outside of the reusable Ipc::StoreMap.
    
    It would be possible to split Ipc::StoreMap further by moving Store-specific
    bits outside of its slots. Currently, there is no need for that though.

--- src/fs/rock/RockDirMap.h
+++ src/fs/rock/RockDirMap.h
@@ -2,142 +2,33 @@
 #define SQUID_FS_ROCK_DIR_MAP_H
 
 #include "fs/rock/RockFile.h"
-#include "ipc/AtomicWord.h"
-#include "ipc/mem/Segment.h"
+#include "ipc/StoreMap.h"
 
 namespace Rock {
 
-class StoreEntryBasics {
-public:
-    void set(const DbCellHeader &aHeader, const StoreEntry &anEntry);
-
-    DbCellHeader header; ///< rock-specific entry metadata
-
-    /* START OF ON-DISK STORE_META_STD TLV field */
-    time_t timestamp;
-    time_t lastref;
-    time_t expires;
-    time_t lastmod;
-    uint64_t swap_file_sz;
-    u_short refcount;
-    u_short flags;
-    /* END OF ON-DISK STORE_META_STD */
-};
-
-/// aggregates basic map performance measurements; all numbers are approximate
-class MapStats {
-public:
-    MapStats();
-
-    void dump(StoreEntry &e) const;
-
-    int capacity; ///< the total number of slots in the map
-    int readable; ///< number of slots in Readable state
-    int writeable; ///< number of slots in Writeable state
-    int empty; ///< number of slots in Empty state
-    int readers; ///< sum of slot.readers
-    int writers; ///< sum of slot.writers
-    int marked; ///< number of slots marked for freeing
-};
-
-/// DirMap entry
-class Slot {
-public:
-    /// possible persistent states
-    typedef enum {
-        Empty, ///< ready for writing, with nothing of value
-        Writeable, ///< transitions from Empty to Readable
-        Readable, ///< ready for reading
-    } State;
-
-    void setKey(const cache_key *const aKey);
-    bool checkKey(const cache_key *const aKey) const;
-
-    bool sharedLock() const; ///< lock for reading or return false
-    bool exclusiveLock(); ///< lock for modification or return false
-    void releaseSharedLock() const; ///< undo successful sharedLock()
-    void releaseExclusiveLock(); ///< undo successful exclusiveLock()
-    void switchExclusiveToSharedLock(); ///< trade exclusive for shared access
-
-    /// adds approximate current stats to the supplied ones
-    void updateStats(MapStats &stats) const;
-
-public:
-    // we want two uint64_t, but older GCCs lack __sync_fetch_and_add_8
-    AtomicWordT<uint32_t> key_[4]; ///< MD5 entry key
-    StoreEntryBasics seBasics; ///< basic store entry data
-    AtomicWordT<uint8_t> state; ///< current state
-    AtomicWordT<uint8_t> waitingToBeFreed; ///< a state-independent mark
-
-private:
-    mutable AtomicWord readers; ///< number of users trying to read
-    AtomicWord writers; ///< number of writers trying to modify the slot
-};
-
 /// \ingroup Rock
 /// map of used db slots indexed by sfileno
-class DirMap
+class DirMap: public Ipc::StoreMap
 {
 public:
     DirMap(const char *const aPath, const int limit); ///< create a new shared DirMap
     DirMap(const char *const aPath); ///< open an existing shared DirMap
 
-    /// finds/reservers space for writing a new entry or returns nil
-    StoreEntryBasics *openForWriting(const cache_key *const key, sfileno &fileno);
-    /// successfully finish writing the entry, leaving it opened for reading
-    void closeForWriting(const sfileno fileno);
-
-    /// stores entry info at the requested slot or returns false
-    bool putAt(const DbCellHeader &header, const StoreEntry &e, const sfileno fileno);
-
-    /// only works on locked entries; returns nil unless the slot is readable
-    const StoreEntryBasics *peekAtReader(const sfileno fileno) const;
-
-    /// mark the slot as waiting to be freed and, if possible, free it
-    void free(const sfileno fileno);
-
-    /// open slot for reading, increments read level
-    const StoreEntryBasics *openForReading(const cache_key *const key, sfileno &fileno);
-    /// open slot for reading, increments read level
-    const StoreEntryBasics *openForReadingAt(const sfileno fileno);
-    /// close slot after reading, decrements read level
-    void closeForReading(const sfileno fileno);
-
-    /// called by lock holder to terminate either slot writing or reading
-    void abortIo(const sfileno fileno);
-
-    bool full() const; ///< there are no empty slots left
-    bool valid(int n) const; ///< whether n is a valid slot coordinate
-    int entryCount() const; ///< number of used slots
-    int entryLimit() const; ///< maximum number of slots that can be used
-
-    /// adds approximate current stats to the supplied ones
-    void updateStats(MapStats &stats) const;
+    /// write access to the cell header; call openForWriting() first!
+    DbCellHeader &header(const sfileno fileno);
+    /// read-only access to the cell header; call openForReading() first!
+    const DbCellHeader &header(const sfileno fileno) const;
 
     static int AbsoluteEntryLimit(); ///< maximum entryLimit() possible
 
 private:
-    struct Shared {
-        Shared(const int aLimit);
-
-        const AtomicWord limit; ///< maximum number of map slots
-        AtomicWord count; ///< current number of map slots
-
-        Slot slots[]; ///< slots storage
+    /// data shared by all DirMaps with the same path
+    class Shared {
+    public:
+        static size_t MemSize(int limit);
+        DbCellHeader headers[]; ///< DbCellHeaders storage
     };
 
-    int slotIdx(const cache_key *const key) const;
-    Slot &slot(const cache_key *const key);
-    const StoreEntryBasics *openForReading(Slot &s);
-    void abortWriting(const sfileno fileno);
-    void freeIfNeeded(Slot &s);
-    void freeLocked(Slot &s);
-    String sharedMemoryName();
-
-    static int SharedSize(const int limit);
-
-    const String path; ///< cache_dir path, used for logging
-    Ipc::Mem::Segment shm; ///< shared memory segment
     Shared *shared; ///< pointer to shared memory
 };
 
---
+++ src/ipc/StoreMap.h
@@ -0,0 +1,120 @@
+#ifndef SQUID_IPC_STORE_MAP_H
+#define SQUID_IPC_STORE_MAP_H
+
+#include "ipc/ReadWriteLock.h"
+#include "ipc/mem/Segment.h"
+
+namespace Ipc {
+
+/// a StoreMap element, holding basic shareable StoreEntry info
+class StoreMapSlot {
+public:
+    StoreMapSlot();
+
+    /// store StoreEntry key and basics
+    void set(const StoreEntry &anEntry);
+
+    void setKey(const cache_key *const aKey);
+    bool sameKey(const cache_key *const aKey) const;
+
+public:
+    mutable ReadWriteLock lock; ///< protects slot data below
+    AtomicWordT<uint8_t> waitingToBeFreed; ///< may be accessed w/o a lock
+
+    uint64_t key[2]; ///< StoreEntry key
+
+    // STORE_META_STD TLV field from StoreEntry
+    struct Basics {
+        time_t timestamp;
+        time_t lastref;
+        time_t expires;
+        time_t lastmod;
+        uint64_t swap_file_sz;
+        u_short refcount;
+        u_short flags;
+	} basics;
+
+    /// possible persistent states
+    typedef enum {
+        Empty, ///< ready for writing, with nothing of value
+        Writeable, ///< transitions from Empty to Readable
+        Readable, ///< ready for reading
+	} State;
+    State state; ///< current state
+};
+
+/// map of StoreMapSlots indexed by their keys, with read/write slot locking 
+/// kids extend to store custom data
+class StoreMap
+{
+public:
+    typedef StoreMapSlot Slot;
+
+    StoreMap(const char *const aPath, const int limit, size_t sharedSizeExtra); ///< create a new shared StoreMap
+    explicit StoreMap(const char *const aPath); ///< open an existing shared StoreMap
+
+    /// finds, reservers space for writing a new entry or returns nil
+    Slot *openForWriting(const cache_key *const key, sfileno &fileno);
+    /// successfully finish writing the entry
+    void closeForWriting(const sfileno fileno, bool lockForReading = false);
+
+    /// only works on locked entries; returns nil unless the slot is readable
+    const Slot *peekAtReader(const sfileno fileno) const;
+
+    /// mark the slot as waiting to be freed and, if possible, free it
+    void free(const sfileno fileno);
+
+    /// open slot for reading, increments read level
+    const Slot *openForReading(const cache_key *const key, sfileno &fileno);
+    /// open slot for reading, increments read level
+    const Slot *openForReadingAt(const sfileno fileno);
+    /// close slot after reading, decrements read level
+    void closeForReading(const sfileno fileno);
+
+    /// called by lock holder to terminate either slot writing or reading
+    void abortIo(const sfileno fileno);
+
+    bool full() const; ///< there are no empty slots left
+    bool valid(int n) const; ///< whether n is a valid slot coordinate
+    int entryCount() const; ///< number of used slots
+    int entryLimit() const; ///< maximum number of slots that can be used
+
+    /// adds approximate current stats to the supplied ones
+    void updateStats(ReadWriteLockStats &stats) const;
+
+protected:
+    class Shared {
+    public:
+        static size_t MemSize(int limit);
+
+        Shared(const int aLimit);
+
+        const AtomicWord limit; ///< maximum number of map slots
+        AtomicWord count; ///< current number of map slots
+
+        Slot slots[]; ///< slots storage
+    };
+
+protected:
+    const String path; ///< cache_dir path, used for logging
+    Ipc::Mem::Segment shm; ///< shared memory segment
+
+private:
+    int slotIndexByKey(const cache_key *const key) const;
+    Slot &slotByKey(const cache_key *const key);
+
+	Slot *openForReading(Slot &s);
+    void abortWriting(const sfileno fileno);
+    void freeIfNeeded(Slot &s);
+    void freeLocked(Slot &s, bool keepLocked);
+    String sharedMemoryName();
+
+    Shared *shared; ///< pointer to shared memory
+};
+
+} // namespace Ipc
+
+// We do not reuse struct _fileMap because we cannot control its size,
+// resulting in sfilenos that are pointing beyond the database.
+
+#endif /* SQUID_IPC_STORE_MAP_H */
