3a2ede14d7112a32d5d6d35a269e6e00f30cb3a0
Alex Rousskov <rousskov@measurement-factory.com>
2011-04-14 14:35:30 -0600

    Check that we do not overflow mmap(2) size argument.

--- src/ipc/mem/Segment.cc
+++ src/ipc/mem/Segment.cc
@@ -85,6 +85,10 @@ Ipc::Mem::Segment::attach()
     assert(theFD >= 0);
     assert(!theMem);
 
+    // mmap() accepts size_t for the size; we give it off_t which might
+    // be bigger; assert overflows until we support multiple mmap()s?
+    assert(theSize == static_cast<off_t>(static_cast<size_t>(theSize)));
+
     void *const p =
         mmap(NULL, theSize, PROT_READ | PROT_WRITE, MAP_SHARED, theFD, 0);
     if (p == MAP_FAILED) {
