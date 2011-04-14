bb5bb8273e98a66f065568aa3ca12635375f6a7c
Alex Rousskov <rousskov@measurement-factory.com>
2011-04-14 14:16:35 -0600

    Use off_t instead of int and size_t for segment sizes to be able to allocated larger than 4GB segments on some platforms.
    Double check the newly created segment size using fstat().

--- src/ipc/mem/Segment.h
+++ src/ipc/mem/Segment.h
@@ -21,11 +21,11 @@ public:
 
     /// Create a new shared memory segment. Fails if a segment with
     /// the same name already exists.
-    void create(const int aSize);
+    void create(const off_t aSize);
     void open(); ///< Open an existing shared memory segment.
 
     const String &name() { return theName; } ///< shared memory segment name
-    int size() { return theSize; } ///< shared memory segment size
+    off_t size() { return theSize; } ///< shared memory segment size
     void *mem() { return theMem; } ///< pointer to mmapped shared memory segment
     void *reserve(size_t chunkSize); ///< reserve and return the next chunk
     // TODO: convert most mem() calls to reserve()
@@ -33,14 +33,15 @@ public:
 private:
     void attach();
     void detach();
+    off_t statSize(const char *context) const;
 
     static String GenerateName(const char *id);
 
     const String theName; ///< shared memory segment file name
     int theFD; ///< shared memory segment file descriptor
     void *theMem; ///< pointer to mmapped shared memory segment
-    size_t theSize; ///< shared memory segment size
-    size_t theReserved; ///< the total number of reserve()d bytes
+    off_t theSize; ///< shared memory segment size
+    off_t theReserved; ///< the total number of reserve()d bytes
 };
 
 } // namespace Mem
