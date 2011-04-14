d5d78971c1f7591c267e51df1e45effe3fd1d268
Alex Rousskov <rousskov@measurement-factory.com>
2011-04-13 22:22:25 -0600

    Added RunnersRegistry, an API to register and, later, run a group of actions.
    Useful for keeping general initialization management code (e.g., main.cc)
    independent from specific initialization code (e.g., Ipc::Mem::Init) during
    staged initialization and cleaning.

---
+++ src/base/RunnersRegistry.h
@@ -0,0 +1,49 @@
+#ifndef SQUID_BASE_RUNNERSREGISTRY_H
+#define SQUID_BASE_RUNNERSREGISTRY_H
+
+/**
+ * This API allows different modules to register with a well-known registry,
+ * be activated by some central processor at some registry-specific time, and
+ * be deactiveated by some central processor at some registry-specific time.
+ *
+ * For example, main.cc may activate registered I/O modules after parsing
+ * squid.conf and deactivate them before exiting.
+ *
+ */
+
+/// well-known registries (currently, deactivation is not performed for these)
+typedef enum {
+    rrAfterConfig, ///< activated by main.cc after parsing squid.conf
+    rrEnd ///< not a real registry, just a label to mark the end of enum
+} RunnerRegistry;
+
+/// a runnable registrant API
+class RegisteredRunner {
+public:
+    // called when this runner's registry is deactivated
+    virtual ~RegisteredRunner() {}
+
+    // called when this runner's registry is activated
+    virtual void run(const RunnerRegistry &r) = 0;
+};
+
+
+/// registers a given runner with the given registry and returns registry count
+int RegisterRunner(const RunnerRegistry &registry, RegisteredRunner *rr);
+
+/// calls run() methods of all runners in the given registry
+int ActivateRegistered(const RunnerRegistry &registry);
+/// deletes all runners in the given registry
+void DeactivateRegistered(const RunnerRegistry &registry);
+
+
+/// convenience function to "use" an otherwise unreferenced static variable
+bool UseThisStatic(const void *);
+
+/// convenience macro: register one RegisteredRunner kid as early as possible
+#define RunnerRegistrationEntry(Registry, Who) \
+    static const bool Who ## _RegisteredWith_ ## Registry = \
+        RegisterRunner(Registry, new Who) > 0 && \
+        UseThisStatic(& Who ## _RegisteredWith_ ## Registry);
+
+#endif /* SQUID_BASE_RUNNERSREGISTRY_H */
