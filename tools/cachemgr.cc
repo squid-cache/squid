2db8f58a0169aca19695509b7c30a4d114e1ee06
Amos Jeffries <squid3@treenet.co.nz>
2011-04-18 06:06:47 -0600

    Manager: send User-Agent header from cachemgr.cgi
    Uses hard-coded string "cachemgr.cgi/" instead of progname to avoid
    complications from alternative names and when running under a browser.
    
    May be elided in transit, however the VERSION sent here will help the
    queried proxy respond appropriate to the CGI capabilities as we extend
    the types and content of reports coming back from the future releases.

--- tools/cachemgr.cc
+++ tools/cachemgr.cc
@@ -864,11 +864,13 @@ process_request(cachemgr_request * req)
 
     l = snprintf(buf, sizeof(buf),
                  "GET cache_object://%s/%s HTTP/1.0\r\n"
+                 "User-Agent: cachemgr.cgi/%s\r\n"
                  "Accept: */*\r\n"
                  "%s"			/* Authentication info or nothing */
                  "\r\n",
                  req->hostname,
                  req->action,
+                 VERSION,
                  make_auth_header(req));
     if (write(s, buf, l) < 0) {
         debug(1) fprintf(stderr, "ERROR: (%d) writing request: '%s'\n", errno, buf);
