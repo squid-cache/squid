089d4d2852b56f80e46491d4d11580ec23bb1fb3
Amos Jeffries <squid3@treenet.co.nz>
2011-04-18 06:01:28 -0600

    Bug 3183: Invalid URL accepted with url host part of only '@'.
    3.0 results in an ICAP segfault handling these URLs.
    
    Newer releases do not segfault as easily, but still accept the invalid
    URL and there may be other unknown side-effects.
    
    Makes the URL parser present ERR_INVALID_URL for this edge case.

--- src/url.cc
+++ src/url.cc
@@ -325,6 +325,12 @@ urlParse(const HttpRequestMethod& method, char *url, HttpRequest *request)
             }
         }
 
+        // Bug 3183 sanity check: If scheme is present, host must be too.
+        if (protocol != AnyP::PROTO_NONE && (host == NULL || *host == '\0')) {
+            debugs(23, DBG_IMPORTANT, "SECURITY WARNING: Missing hostname in URL '" << url << "'. see access.log for details.");
+            return NULL;
+        }
+
         if (t && *t == ':') {
             *t = '\0';
             t++;
