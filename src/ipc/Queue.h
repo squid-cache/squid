448bf7a6a94621004251106eb954a8151574b796
Alex Rousskov <rousskov@measurement-factory.com>
2011-04-18 22:31:53 -0600

    Optimized the number of "queue is no longer empty" IpcIo notifications.
    The original code relied on the writer (pusher) knowledge to decide when a
    notification is needed. That code was simpler but it resulted in many
    pointless notifications because the reader could have been busy processing the
    last popped item and would have checked the queue after that processing
    anyway. This would become especially wasteful when the reader pops multiple
    requests before processing them (e.g. to do "elevator" seek optimization).
    
    The intermediate implementation (not comitted) placed the reader state in
    each queue. That was still fairly simple and worked OK, but it was not
    addressing the needs of the disker readers. Diskers have many incoming
    queues. If at least one incoming queue has requests, the disker is not
    blocked and does not need a notification.
    
    The last implementation allows all incoming queues of a single disker to share
    the reader/disker state. The reader state is disassociated from the single
    queue.  There is still some wasteful state updates when multiple queues are
    iterated in FewToOneBiQueue::pop(), but their overheads should be very minor.
    We need to figure out whether a single shared reader state can also be used
    for workers though (each worker also has many incoming queues...).
    
    Also added debugging and a few XXXs/TODOs to mark future work items.

--- src/ipc/Queue.h
+++ src/ipc/Queue.h
@@ -7,16 +7,58 @@
 #define SQUID_IPC_QUEUE_H
 
 #include "Array.h"
+#include "base/InstanceId.h"
 #include "ipc/AtomicWord.h"
 #include "ipc/mem/Segment.h"
 #include "util.h"
 
 class String;
 
-/// Lockless fixed-capacity queue for a single writer and a single reader.
+/// State of the reading end of a queue (i.e., of the code calling pop()).
+/// Multiple queues attached to one reader share this state.
+class QueueReader {
+public:
+    QueueReader(); // the initial state is "blocked without a signal"
+
+    /// whether the reader is waiting for a notification signal
+    bool blocked() const { return popBlocked == 1; }
+
+    /// marks the reader as blocked, waiting for a notification signal
+    void block() { popBlocked.swap_if(0, 1); }
+
+    /// removes the block() effects
+    void unblock() { popBlocked.swap_if(1, 0); }
+
+    /// if reader is blocked and not notified, marks the notification signal
+    /// as sent and not received, returning true; otherwise, returns false
+    bool raiseSignal() { return blocked() && popSignal.swap_if(0,1); }
+
+    /// marks sent reader notification as received (also removes pop blocking)
+    void clearSignal() { unblock(); popSignal.swap_if(1,0); }
+
+private:
+    AtomicWord popBlocked; ///< whether the reader is blocked on pop()
+    AtomicWord popSignal; ///< whether writer has sent and reader has not received notification
+
+public:
+    /// unique ID for debugging which reader is used (works across processes)
+    const InstanceId<QueueReader> id;
+};
+
+
+/**
+ * Lockless fixed-capacity queue for a single writer and a single reader.
+ *
+ * If the queue is empty, the reader is considered "blocked" and needs
+ * an out-of-band notification message to notice the next pushed item.
+ *
+ * Current implementation assumes that the writer cannot get blocked: if the
+ * queue is full, the writer will just not push and come back later (with a
+ * different value). We can add support for blocked writers if needed.
+ */
 class OneToOneUniQueue {
 public:
-    class Empty {};
+    // pop() and push() exceptions; TODO: use TextException instead
     class Full {};
     class ItemTooLarge {};
 
@@ -33,10 +75,14 @@ public:
     static int Bytes2Items(const unsigned int maxItemSize, int size);
     static int Items2Bytes(const unsigned int maxItemSize, const int size);
 
-    template <class Value>
-    bool pop(Value &value); ///< returns true iff the queue was full
-    template <class Value>
-    bool push(const Value &value); ///< returns true iff the queue was empty
+    /// returns true iff the value was set; [un]blocks the reader as needed
+    template<class Value> bool pop(Value &value);
+
+    /// returns true iff the caller must notify the reader of the pushed item
+    template<class Value> bool push(const Value &value);
+
+    QueueReader &reader();
+    void reader(QueueReader *aReader);
 
 private:
     struct Shared {
@@ -54,12 +100,12 @@ private:
 
     Ipc::Mem::Segment shm; ///< shared memory segment
     Shared *shared; ///< pointer to shared memory
+    QueueReader *reader_; ///< the state of the code popping from this queue
 };
 
 /// Lockless fixed-capacity bidirectional queue for two processes.
 class OneToOneBiQueue {
 public:
-    typedef OneToOneUniQueue::Empty Empty;
     typedef OneToOneUniQueue::Full Full;
     typedef OneToOneUniQueue::ItemTooLarge ItemTooLarge;
 
@@ -67,12 +113,14 @@ public:
     OneToOneBiQueue(const String &id, const unsigned int maxItemSize, const int capacity);
     OneToOneBiQueue(const String &id); ///< Attach to existing shared queue.
 
-    template <class Value>
-    bool pop(Value &value) { return popQueue->pop(value); }
-    template <class Value>
-    bool push(const Value &value) { return pushQueue->push(value); }
+    void readers(QueueReader *r1, QueueReader *r2);
+    void clearReaderSignal();
 
-private:
+    /* wrappers to call the right OneToOneUniQueue method for this process */
+    template<class Value> bool pop(Value &value) { return popQueue->pop(value); }
+    template<class Value> bool push(const Value &value) { return pushQueue->push(value); }
+
+//private:
     OneToOneUniQueue *const popQueue; ///< queue to pop from for this process
     OneToOneUniQueue *const pushQueue; ///< queue to push to for this process
 };
@@ -87,7 +135,6 @@ private:
  */
 class FewToOneBiQueue {
 public:
-    typedef OneToOneBiQueue::Empty Empty;
     typedef OneToOneBiQueue::Full Full;
     typedef OneToOneBiQueue::ItemTooLarge ItemTooLarge;
 
@@ -98,15 +145,22 @@ public:
     bool validWorkerId(const int workerId) const;
     int workerCount() const { return theWorkerCount; }
 
-    template <class Value>
-    bool pop(int &workerId, Value &value); ///< returns false iff the queue was full
-    template <class Value>
-    bool push(const int workerId, const Value &value); ///< returns false iff the queue was empty
+    /// clears the reader notification received by the disker from worker
+    void clearReaderSignal(int workerId);
 
-private:
-    int theLastPopWorkerId; ///< the last worker ID we pop()ed from
-    Vector<OneToOneBiQueue *> biQueues; ///< worker queues
-    const int theWorkerCount; ///< number of worker processes
+    /// picks a worker and calls OneToOneUniQueue::pop() using its queue
+    template <class Value> bool pop(int &workerId, Value &value);
+
+    /// calls OneToOneUniQueue::push() using the given worker queue
+    template <class Value> bool push(const int workerId, const Value &value);
+
+//private: XXX: make private by moving pop/push debugging into pop/push
+    int theLastPopWorker; ///< the ID of the last worker we tried to pop() from
+    Vector<OneToOneBiQueue *> biQueues; ///< worker queues indexed by worker ID
+    const int theWorkerCount; ///< the total number of workers
+
+    Ipc::Mem::Segment shm; ///< shared memory segment to store the reader
+    QueueReader *reader; ///< the state of the code popping from all biQueues
 };
 
 
@@ -119,15 +173,23 @@ OneToOneUniQueue::pop(Value &value)
     if (sizeof(value) > shared->theMaxItemSize)
         throw ItemTooLarge();
 
-    if (empty())
-        throw Empty();
+    // A writer might push between the empty test and block() below, so we do
+    // not return false right after calling block(), but test again.
+    if (empty()) {
+        reader().block();
+        // A writer might push between the empty test and block() below,
+        // so we must test again as such a writer will not signal us.
+        if (empty())
+            return false;
+    }
 
-    const bool wasFull = full();
+    reader().unblock();
     const unsigned int pos =
-        shared->theOut++ % shared->theCapacity * shared->theMaxItemSize;
+        (shared->theOut++ % shared->theCapacity) * shared->theMaxItemSize;
     memcpy(&value, shared->theBuffer + pos, sizeof(value));
     --shared->theSize;
-    return wasFull;
+
+    return true;
 }
 
 template <class Value>
@@ -145,7 +207,8 @@ OneToOneUniQueue::push(const Value &value)
         shared->theIn++ % shared->theCapacity * shared->theMaxItemSize;
     memcpy(shared->theBuffer + pos, &value, sizeof(value));
     ++shared->theSize;
-    return wasEmpty;
+
+    return wasEmpty && reader().raiseSignal();
 }
 
 
@@ -155,16 +218,15 @@ template <class Value>
 bool
 FewToOneBiQueue::pop(int &workerId, Value &value)
 {
-    ++theLastPopWorkerId;
+    // iterate all workers, starting after the one we visited last
     for (int i = 0; i < theWorkerCount; ++i) {
-        theLastPopWorkerId = (theLastPopWorkerId + 1) % theWorkerCount;
-        try {
-            const bool wasFull = biQueues[theLastPopWorkerId]->pop(value);
-            workerId = theLastPopWorkerId;
-            return wasFull;
-        } catch (const Empty &) {}
+        theLastPopWorker = (theLastPopWorker + 1) % theWorkerCount;
+        if (biQueues[theLastPopWorker]->pop(value)) {
+            workerId = theLastPopWorker;
+            return true;
+        }
     }
-    throw Empty();
+    return false; // no worker had anything to pop
 }
 
 template <class Value>
