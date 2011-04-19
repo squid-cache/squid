bc9b2391964dc68213923191ec5d33b69c7bde32
Alex Rousskov <rousskov@measurement-factory.com>
2011-04-19 16:35:32 -0600

    Fixed compiler warnings about wrong printf() format for currentSize().

--- src/store_dir.cc
+++ src/store_dir.cc
@@ -349,7 +349,7 @@ StoreController::stat(StoreEntry &output) const
                       (unsigned long int)StoreEntry::inUseCount());
     storeAppendPrintf(&output, "Maximum Swap Size      : %"PRIu64" KB\n",
                       maxSize());
-    storeAppendPrintf(&output, "Current Store Swap Size: %8lu KB\n",
+    storeAppendPrintf(&output, "Current Store Swap Size: %"PRIu64" KB\n",
                       currentSize());
     storeAppendPrintf(&output, "Current Capacity       : %"PRId64"%% used, %"PRId64"%% free\n",
                       Math::int64Percent(currentSize(), maxSize()),
