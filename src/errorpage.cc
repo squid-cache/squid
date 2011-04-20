88d1e459293cf476300bba80d1fe6da05c862647
Amos Jeffries <squid3@treenet.co.nz>
2011-04-20 18:07:08 +1200

    Add errpages option %b for proxy listening port
    Use getMyPort() to insert the forward-proxy listening port into error
    pages and deny_info redirect URLs. This fixes the current port
    hard-coding assumption in ERR_AGENT_CONFIGURE.
    
    The %b option is added for this purpose as a temporary measure until the
    codes are merged with the more flexible log formatting set.
    
    This currently depends on squid.conf having a particular http_port
    ordering with the forward-proxy port listed first.

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -670,6 +670,11 @@ ErrorState::Convert(char token, bool building_deny_info_url, bool allowRecursion
 #endif
             p = "-";
         break;
+
+    case 'b':
+        mb.Printf("%d", getMyPort());
+        break;
+
     case 'B':
         if (building_deny_info_url) break;
         p = request ? ftpUrlWith2f(request) : "[no URL]";
