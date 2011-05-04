3e7083d39c7610ddbc77ac0ae79eb3767188da54
Amos Jeffries <squid3@treenet.co.nz>
2011-05-04 15:05:09 +1200

    Compile fixes for binutils-gold and gcc-4.6 support
    These two tools are much stricter about dependency linkages. We have already
    had to drop testAuth due to major dependency loops they dislike.
    This makes the remainder of the dependency changes needed.
    
    Also adds:
     - tests/STUB.h with macros for simpler stub file creation
     - stub_libmgr.cc for unit-test stub replacment of mgr/libmgr.la library.
       many API functions commented out, but sufficient for the current needs.

--- src/tests/testHttpReply.cc
+++ src/tests/testHttpReply.cc
@@ -12,19 +12,6 @@ struct SquidConfig Config;
 
 /* stub functions to link successfully */
 
-#include "Store.h"
-void
-StoreEntry::timestampsSet()
-{
-    fatal("StoreEntry::timestampsSet. Not implemented.");
-}
-
-void
-StoreEntry::setPublicKey()
-{
-    fatal("StoreEntry::setPulicKey. Not implemented.");
-}
-
 #include "MemObject.h"
 int64_t
 MemObject::endOffset() const
