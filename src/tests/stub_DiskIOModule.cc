ca0039db7c91deb632ce7346067c826e48c106f4
Amos Jeffries <squid3@treenet.co.nz>
2011-05-07 04:16:45 +1200

    Implicit Dependency removal for gcc-4.6.1
    GCC 4.6.1 is stricter than 4.6.0. It does not by default include implicit
    dependencies. This adds several unit tests .cc files which were implicitly
    linked before.
    
    Also adds tests/stub_DiskIOModule.cc to short-circuit the DiskIOModule API

---
+++ src/tests/stub_DiskIOModule.cc
@@ -0,0 +1,13 @@
+#include "config.h"
+
+#define STUB_API "DiskIOModule.cc"
+#include "tests/STUB.h"
+
+#include "DiskIO/DiskIOModule.h"
+void DiskIOModule::SetupAllModules() STUB
+void DiskIOModule::ModuleAdd(DiskIOModule &) STUB
+void DiskIOModule::FreeAllModules() STUB
+void DiskIOModule::PokeAllModules() STUB
+DiskIOModule *DiskIOModule::Find(char const *) STUB_RETVAL(NULL)
+DiskIOModule *DiskIOModule::FindDefault() STUB_RETVAL(NULL)
+Vector<DiskIOModule*> const &DiskIOModule::Modules() STUB_RETSTATREF(Vector<DiskIOModule*>)
