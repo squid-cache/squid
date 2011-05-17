a2f77337f972cf5af6e47651b5d6dc1b3b7e5d9c
Amos Jeffries <squid3@treenet.co.nz>
2011-05-17 18:43:45 +1200

    Add stub for libicmp

---
+++ src/tests/stub_libicmp.cc
@@ -0,0 +1,31 @@
+#include "config.h"
+#define STUB_API "icmp/libicmp.la"
+#include "STUB.h"
+
+#include "icmp/IcmpSquid.h"
+//IcmpSquid::IcmpSquid() STUB
+//IcmpSquid::~IcmpSquid() STUB
+int IcmpSquid::Open() STUB_RETVAL(-1)
+void IcmpSquid::Close() STUB
+void IcmpSquid::DomainPing(Ip::Address &to, const char *domain) STUB
+#if USE_ICMP
+void IcmpSquid::SendEcho(Ip::Address &to, int opcode, const char* payload, int len) STUB
+void IcmpSquid::Recv(void) STUB
+#endif
+//IcmpSquid icmpEngine;
+
+#include "icmp/net_db.h"
+void netdbInit(void) STUB
+void netdbHandlePingReply(const Ip::Address &from, int hops, int rtt) STUB
+void netdbPingSite(const char *hostname) STUB
+void netdbDump(StoreEntry *) STUB
+void netdbFreeMemory(void) STUB
+int netdbHostHops(const char *host) STUB_RETVAL(-1)
+int netdbHostRtt(const char *host) STUB_RETVAL(-1)
+void netdbUpdatePeer(HttpRequest *, peer * e, int rtt, int hops) STUB
+void netdbDeleteAddrNetwork(Ip::Address &addr) STUB
+void netdbBinaryExchange(StoreEntry *) STUB
+void netdbExchangeStart(void *) STUB
+void netdbExchangeUpdatePeer(Ip::Address &, peer *, double, double) STUB
+peer *netdbClosestParent(HttpRequest *) STUB_RETVAL(NULL)
+void netdbHostData(const char *host, int *samp, int *rtt, int *hops) STUB
