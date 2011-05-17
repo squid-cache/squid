633a78c1c800163f9232adfc9735335c9b888bbf
Christos Tsantilas <chtsanti@users.sourceforge.net>
2011-05-17 06:35:41 -0600

    Squid-to-origin SNI for ssl-bump
    This patch adds Squid-to-server SSL Server Name Indication (SNI) support to the
    outgoing connections in Squid.
    
    This is a Measurement Factory project

--- src/ssl/support.cc
+++ src/ssl/support.cc
@@ -1237,4 +1237,23 @@ bool Ssl::verifySslCertificateDate(SSL_CTX * sslContext)
     return ret;
 }
 
+bool
+Ssl::setClientSNI(SSL *ssl, const char *fqdn)
+{
+    //The SSL_CTRL_SET_TLSEXT_HOSTNAME is a openssl macro which indicates
+    // if the TLS servername extension (SNI) is enabled in openssl library.
+#if defined(SSL_CTRL_SET_TLSEXT_HOSTNAME)
+    if (!SSL_set_tlsext_host_name(ssl, fqdn)) {
+        const int ssl_error = ERR_get_error();
+        debugs(83, 3,  "WARNING: unable to set TLS servername extension (SNI): " <<
+               ERR_error_string(ssl_error, NULL) << "\n");
+        return false;
+    }
+    return true;
+#else
+    debugs(83, 7,  "no support for TLS servername extension (SNI)\n");
+    return false;
+#endif
+}
+
 #endif /* USE_SSL */
