1c16ab00f7687d39ba95606ab38cfcbb59ebae94
Alex Rousskov <rousskov@measurement-factory.com>
2011-05-24 21:14:15 -0600

    Detect support for __sync_add_and_fetch() and friends, setting HAVE_ATOMIC_OPS accordingly.
    Disable shared memory caching by default if atomic operations are not
    supported. Prohibit shared memory caching if atomic operations are not
    supported.
    
    Needs more testing.

--- src/MemStore.cc
+++ src/MemStore.cc
@@ -361,7 +361,12 @@ void MemStoreRr::run(const RunnerRegistry &)
 {
     // decide whether to use a shared memory cache if the user did not specify
     if (!Config.memShared.configured()) {
-        Config.memShared.configure(UsingSmp() && Config.memMaxSize > 0);
+        Config.memShared.configure(AtomicOperationsSupported &&
+            UsingSmp() && Config.memMaxSize > 0);
+    } else
+    if (Config.memShared && !AtomicOperationsSupported) {
+        // bail if the user wants shared memory cache but we cannot support it
+        fatal("memory_cache_shared is on, but no support for atomic operations detected");
     }
 
     if (!Config.memShared)
