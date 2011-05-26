6a933a810cf1ab5f0eae39d354d1f6e587e7a928
Francesco Chemolli <kinkie@squid-cache.org>
2011-05-26 13:28:19 +0200

    Portability fix for OpenBSD in time quota external acl helper.

--- helpers/external_acl/time_quota/ext_time_quota_acl.cc
+++ helpers/external_acl/time_quota/ext_time_quota_acl.cc
@@ -96,7 +96,8 @@ static void vlog(const char *level, const char *format, va_list args)
 {
     time_t now = time(NULL);
 
-    fprintf(logfile, "%ld %s| %s: ", now, program_name, level);
+    fprintf(logfile, "%ld %s| %s: ", static_cast<long int>(now), 
+              program_name, level);
     vfprintf (logfile, format, args);
     fflush(logfile);
 }
