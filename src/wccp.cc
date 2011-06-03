9055f14b229471043f9c43559cff9cfdc309b5e2
Amos Jeffries <squid3@treenet.co.nz>
2011-06-03 05:27:18 -0600

    Fix rev11470 missing casts

--- src/wccp.cc
+++ src/wccp.cc
@@ -168,13 +168,13 @@ wccpConnectionOpen(void)
 
     struct sockaddr_in router;
     Config.Wccp.router.GetSockAddr(router);
-    if (connect(theWccpConnection, &router, sizeof(router)))
+    if (connect(theWccpConnection, (struct sockaddr*)&router, sizeof(router)))
         fatal("Unable to connect WCCP out socket");
 
     struct sockaddr_in local;
     memset(&local, '\0', sizeof(local));
     socklen_t slen = sizeof(local);
-    if (getsockname(theWccpConnection, &local, &slen))
+    if (getsockname(theWccpConnection, (struct sockaddr*)&local, &slen))
         fatal("Unable to getsockname on WCCP out socket");
 
     local_ip = local;
