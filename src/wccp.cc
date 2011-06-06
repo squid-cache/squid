31db2786233aae0acc8929336b715a1f65ebb045
Amos Jeffries <squid3@treenet.co.nz>
2011-06-06 02:17:28 -0600

    Optimization: revert WCCP setup to using sockaddr_in
    Replaces a some malloc/free with local stack variables and a memset

--- src/wccp.cc
+++ src/wccp.cc
@@ -130,7 +130,6 @@ wccpInit(void)
 void
 wccpConnectionOpen(void)
 {
-    struct addrinfo *router = NULL, *local = NULL;
     debugs(80, 5, "wccpConnectionOpen: Called");
 
     if (Config.Wccp.router.IsAnyAddr()) {
@@ -164,21 +163,21 @@ wccpConnectionOpen(void)
 
     debugs(80, 1, "Accepting WCCPv1 messages on " << Config.Wccp.address << ", FD " << theWccpConnection << ".");
 
-    Config.Wccp.router.GetAddrInfo(router,AF_INET);
 
-    if (connect(theWccpConnection, router->ai_addr, router->ai_addrlen))
+    // Sadly WCCP only does IPv4
+
+    struct sockaddr_in router;
+    Config.Wccp.router.GetSockAddr(router);
+    if (connect(theWccpConnection, (struct sockaddr*)&router, sizeof(router)))
         fatal("Unable to connect WCCP out socket");
 
-    Config.Wccp.router.FreeAddrInfo(router);
-
-    Config.Wccp.address.InitAddrInfo(local);
-
-    if (getsockname(theWccpConnection, local->ai_addr, &local->ai_addrlen))
+    struct sockaddr_in local;
+    memset(&local, '\0', sizeof(local));
+    socklen_t slen = sizeof(local);
+    if (getsockname(theWccpConnection, (struct sockaddr*)&local, &slen))
         fatal("Unable to getsockname on WCCP out socket");
 
-    local_ip = *local;
-
-    Config.Wccp.address.FreeAddrInfo(local);
+    local_ip = local;
 }
 
 
