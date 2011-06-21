feab43cb76f0b10f4c22ec20e6794135b39465d7
Dmitry Kurochkin <dmitry.kurochkin@measurement-factory.com>
2011-06-21 18:35:48 +0400

    Add shared memory page reserve for Rock and check page availability before IO.
    The patch adds a shared memory page reserve for Rock (any IPC IO in
    general) that can not be used by the memory cache.  Also, Rock checks
    if there are free pages available before trying to do IPC IO (both
    read and write).  This allows to fail early for cache reads and go
    MISS route.

--- src/ipc/mem/Pages.h
+++ src/ipc/mem/Pages.h
@@ -26,14 +26,35 @@ char *PagePointer(const PageId &page);
 
 /* Limits and statistics */
 
-/// the total number of shared memory bytes that can be in use at any time
-size_t Limit();
+/// the total number of shared memory pages that can be in use at any time
+size_t PageLimit();
 
-/// approximate total number of shared memory bytes used now
-size_t Level();
+/// the total number of shared memory pages for memory cache that can be in
+/// use at any time
+size_t CachePageLimit();
 
-/// approximate total number of shared memory bytes we can allocate now
-inline size_t Available() { return Limit() - Level(); }
+/// the total number of shared memory pages for IPC IO that can be in
+/// use at any time
+size_t IoPageLimit();
+
+/// approximate total number of shared memory pages used now
+size_t PageLevel();
+
+/// approximate total number of shared memory pages for memory cache used now
+size_t CachePageLevel();
+
+/// approximate total number of shared memory pages for IPC IO used now
+size_t IoPageLevel();
+
+/// approximate total number of shared memory pages we can allocate now
+inline size_t PagesAvailable() { return PageLimit() - PageLevel(); }
+
+/// approximate total number of shared memory pages for memory cache we can
+/// allocate now
+inline size_t CachePagesAvailable() { return CachePageLimit() - CachePageLevel(); }
+
+/// approximate total number of shared memory pages for IPC IO we can allocate now
+inline size_t IoPagesAvailable() { return IoPageLimit() - IoPageLevel(); }
 
 /// returns page size in bytes; all pages are assumed to be the same size
 size_t PageSize();
