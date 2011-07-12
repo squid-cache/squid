04bd87ac3ac5ad99c507718283691bf5ab7dee7b
Christos Tsantilas <chtsanti@users.sourceforge.net>
2011-07-12 22:02:48 +0300

    Author: Amos Jeffries <squid3@treenet.co.nz>, Christos Tsantilas <chtsanti@users.sourceforge.net> Add DNS lookup step to ICAP connect
    This patch add an async DNS lookup step to the ICAP connection setup process.
    As a side effect it adds a little bit of tcp_outgoing_address support to ICAP. 
    Its limited to "dst" ACL at present. So outgoing ACL selections depending on 
    HTTP request details wont work. Which makes sense since this connection may 
    be reused for multiple requests.
    
    More than one IP result are not used  since there seems to be no sane place 
    to store multiple IPs between connect attempts. It currently relies on ipcache
    MarkGood/MarkBad keeping a good usable IP at the top of the IP set.       
    
    This patch also try to make Max-Connections ICAP feature cooperate well with 
    the new DNS lookup code

--- src/adaptation/icap/ServiceRep.h
+++ src/adaptation/icap/ServiceRep.h
@@ -113,6 +113,7 @@ public:
     Comm::ConnectionPointer getConnection(bool isRetriable, bool &isReused);
     void putConnection(const Comm::ConnectionPointer &conn, bool isReusable, const char *comment);
     void noteConnectionUse(const Comm::ConnectionPointer &conn);
+    void noteConnectionFailed(const char *comment);
 
     void noteFailure(); // called by transactions to report service failure
 
@@ -160,7 +161,7 @@ private:
     int theMaxConnections; ///< the maximum allowed connections to the service
     // TODO: use a better type like the FadingCounter for connOverloadReported
     mutable bool connOverloadReported; ///< whether we reported exceeding theMaxConnections
-    IdleConnList theIdleConns; ///< idle persistent connection pool
+    IdleConnList *theIdleConns; ///< idle persistent connection pool
 
     FadingCounter theSessionFailures;
     const char *isSuspended; // also stores suspension reason for debugging
