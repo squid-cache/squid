64b56a46795d662b88192290ff73d410b0770ce7
Pierre LALET <pierre.lalet@cea.fr>
2011-07-20 23:01:24 +1200

    Bug 3248: login=NEGOTIATE sends wrong auth header to origin peers

--- src/http.cc
+++ src/http.cc
@@ -1603,7 +1603,7 @@ httpFixupAuthentication(HttpRequest * request, const HttpHeader * hdr_in, HttpHe
         }
         Token = peer_proxy_negotiate_auth(PrincipalName, request->peer_host);
         if (Token) {
-            httpHeaderPutStrf(hdr_out, HDR_PROXY_AUTHORIZATION, "Negotiate %s",Token);
+            httpHeaderPutStrf(hdr_out, header, "Negotiate %s",Token);
         }
         return;
     }
