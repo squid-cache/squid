91987403dfaafd493aa506c9ab628ef00e66bd92
Christos Tsantilas <chtsanti@users.sourceforge.net>
2011-07-21 05:21:58 -0600

    Bug 3273: assertion comm.cc:775: Comm::IsConnOpen(conn)

--- src/CommCalls.cc
+++ src/CommCalls.cc
@@ -76,7 +76,7 @@ CommIoCbParams::syncWithComm()
 {
     // change parameters if the call was scheduled before comm_close but
     // is being fired after comm_close
-    if (conn->fd >= 0 && fd_table[conn->fd].closing() && flag != COMM_ERR_CLOSING) {
+    if ((conn->fd < 0 || fd_table[conn->fd].closing()) && flag != COMM_ERR_CLOSING) {
         debugs(5, 3, HERE << "converting late call to COMM_ERR_CLOSING: " << conn);
         flag = COMM_ERR_CLOSING;
     }
