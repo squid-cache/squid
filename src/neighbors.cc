9260d17fd2a308770af1e61f42c8675b166261d7
Jean-Gabriel Dick <jean-gabriel.dick@curie.fr>
2011-07-22 08:42:14 -0600

    Bug 1842: Optimize order of tests in peerWouldBePinged() and peerHTTPOkay()
    The peerAllowedToUse() function may be time consuming, especially on sites that
    have lots of acls.

--- src/neighbors.cc
+++ src/neighbors.cc
@@ -214,13 +214,10 @@ peerAllowedToUse(const peer * p, HttpRequest * request)
 static int
 peerWouldBePinged(const peer * p, HttpRequest * request)
 {
-    if (!peerAllowedToUse(p, request))
+    if (p->icp.port == 0)
         return 0;
 
     if (p->options.no_query)
-        return 0;
-
-    if (p->options.background_ping && (squid_curtime - p->stats.last_query < Config.backgroundPingRate))
         return 0;
 
     if (p->options.mcast_responder)
@@ -229,7 +226,7 @@ peerWouldBePinged(const peer * p, HttpRequest * request)
     if (p->n_addresses == 0)
         return 0;
 
-    if (p->icp.port == 0)
+    if (p->options.background_ping && (squid_curtime - p->stats.last_query < Config.backgroundPingRate))
         return 0;
 
     /* the case below seems strange, but can happen if the
@@ -237,6 +234,9 @@ peerWouldBePinged(const peer * p, HttpRequest * request)
     if (p->type == PEER_SIBLING)
         if (!request->flags.hierarchical)
             return 0;
+
+    if (!peerAllowedToUse(p, request))
+        return 0;
 
     /* Ping dead peers every timeout interval */
     if (squid_curtime - p->stats.last_query > Config.Timeout.deadPeer)
@@ -252,15 +252,15 @@ peerWouldBePinged(const peer * p, HttpRequest * request)
 int
 peerHTTPOkay(const peer * p, HttpRequest * request)
 {
+    if (p->max_conn)
+        if (p->stats.conn_open >= p->max_conn)
+            return 0;
+
     if (!peerAllowedToUse(p, request))
         return 0;
 
     if (!neighborUp(p))
         return 0;
-
-    if (p->max_conn)
-        if (p->stats.conn_open >= p->max_conn)
-            return 0;
 
     return 1;
 }
