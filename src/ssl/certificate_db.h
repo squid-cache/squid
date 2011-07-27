4736709f30c7f2fb6d05893fcf0ec1bcb2d99cc6
Automatic source maintenance <squidadm@squid-cache.org>
2011-07-26 18:12:59 -0600

    SourceFormat Enforcement

--- src/ssl/certificate_db.h
+++ src/ssl/certificate_db.h
@@ -117,7 +117,7 @@ private:
     static unsigned long index_serial_LHASH_HASH(const void *a) {
         return index_serial_hash((const char **)a);
     }
-    static int index_serial_LHASH_COMP(const void *arg1, const void *arg2){
+    static int index_serial_LHASH_COMP(const void *arg1, const void *arg2) {
         return index_serial_cmp((const char **)arg1, (const char **)arg2);
     }
     static unsigned long index_name_LHASH_HASH(const void *a) {
