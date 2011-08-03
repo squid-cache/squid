96f6f33b443412f7e8075d18c800ee7b560d5134
Automatic source maintenance <squidadm@squid-cache.org>
2011-08-02 18:16:42 -0600

    SourceFormat Enforcement

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -2250,21 +2250,21 @@ parse_peer(peer ** head)
             p->options.carp_key.set=1;
             char *nextkey=token+strlen("carp-key="), *key=nextkey;
             for (; key; key = nextkey) {
-            	nextkey=strchr(key,',');
-            	if (nextkey) ++nextkey; // skip the comma, any
-            	if (0==strncasecmp(key,"scheme",6)) {
-            		p->options.carp_key.scheme=1;
-            	} else if (0==strncasecmp(key,"host",4)) {
-            		p->options.carp_key.host=1;
-            	} else if (0==strncasecmp(key,"port",4)) {
-            		p->options.carp_key.port=1;
-            	} else if (0==strncasecmp(key,"path",4)) {
-            		p->options.carp_key.path=1;
-            	} else if (0==strncasecmp(key,"params",6)) {
-            		p->options.carp_key.params=1;
-            	} else {
-            		fatalf("invalid carp-key '%s'",key);
-            	}
+                nextkey=strchr(key,',');
+                if (nextkey) ++nextkey; // skip the comma, any
+                if (0==strncasecmp(key,"scheme",6)) {
+                    p->options.carp_key.scheme=1;
+                } else if (0==strncasecmp(key,"host",4)) {
+                    p->options.carp_key.host=1;
+                } else if (0==strncasecmp(key,"port",4)) {
+                    p->options.carp_key.port=1;
+                } else if (0==strncasecmp(key,"path",4)) {
+                    p->options.carp_key.path=1;
+                } else if (0==strncasecmp(key,"params",6)) {
+                    p->options.carp_key.params=1;
+                } else {
+                    fatalf("invalid carp-key '%s'",key);
+                }
             }
         } else if (!strcasecmp(token, "userhash")) {
 #if USE_AUTH
