d5bcbd4d21472021678a6a888c8e0eaead9dbab0
Automatic source maintenance <squidadm@squid-cache.org>
2011-08-07 18:12:19 -0600

    SourceFormat Enforcement

--- src/ip/Address.cc
+++ src/ip/Address.cc
@@ -285,7 +285,7 @@ Ip::Address::IsNoAddr() const
 {
     // IFF the address == 0xff..ff (all ones)
     return IN6_ARE_ADDR_EQUAL( &m_SocketAddr.sin6_addr, &v6_noaddr )
-        || IN6_ARE_ADDR_EQUAL( &m_SocketAddr.sin6_addr, &v4_noaddr );
+           || IN6_ARE_ADDR_EQUAL( &m_SocketAddr.sin6_addr, &v4_noaddr );
 }
 
 void
