42ffba3e799aa990dcb5e5b1e66b7dca14f0c344
Amos Jeffries <squid3@treenet.co.nz>
2011-08-14 02:55:47 +1200

    Fix signed/unsigend conversion error

--- tools/purge/purge.cc
+++ tools/purge/purge.cc
@@ -292,7 +292,7 @@ log_extended( const char* fn, int code, long size, const SquidMetaList* meta )
                   (unsigned long)temp.timestamp, (unsigned long)temp.lastref,
                   (unsigned long)temp.expires, (unsigned long)temp.lastmod, temp.flags, temp.refcount );
     } else {
-        unsigned long ul = -1;  // Match type of StoreMetaTLV fields
+        unsigned long ul = ULONG_MAX;  // Match type of StoreMetaTLV fields
         unsigned short hu = 0;  // Match type of StoreMetaTLV refcount fields
         snprintf( timeb, sizeof(timeb), "%08lx %08lx %08lx %08lx %04x %5d ", ul, ul, ul, ul, 0, hu);
     }
