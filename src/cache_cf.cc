49920e6b6a5ce3cb4beeb410b900a0aeafe3f7ef
Amos Jeffries <squid3@treenet.co.nz>
2011-08-13 00:20:18 -0600

    Kill one goto

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -4082,7 +4082,11 @@ parse_access_log(customlog ** logs)
 
     if (strcmp(filename, "none") == 0) {
         cl->type = Log::Format::CLF_NONE;
-        goto done;
+        aclParseAclList(LegacyParser, &cl->aclList);
+        while (*logs)
+            logs = &(*logs)->next;
+        *logs = cl;
+        return;
     }
 
     if ((logdef_name = strtok(NULL, w_space)) == NULL)
@@ -4130,7 +4134,6 @@ parse_access_log(customlog ** logs)
         return;
     }
 
-done:
     aclParseAclList(LegacyParser, &cl->aclList);
 
     while (*logs)
