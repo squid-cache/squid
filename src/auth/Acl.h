8d8fb23cdf92eb0c879160e52a6ada3d07745135
Amos Jeffries <squid3@treenet.co.nz>
2011-08-14 06:18:57 -0600

    Convert AuthenticateAcl() to use new ACL states

--- src/auth/Acl.h
+++ src/auth/Acl.h
@@ -3,13 +3,15 @@
 
 #if USE_AUTH
 
+#include "acl/Acl.h"
+
 // ACL-related code used by authentication-related code. This code is not in
 // auth/Gadgets to avoid making auth/libauth dependent on acl/libstate because
 // acl/libstate already depends on auth/libauth.
 
 class ACLChecklist;
 /// \ingroup AuthAPI
-extern int AuthenticateAcl(ACLChecklist *ch);
+extern allow_t AuthenticateAcl(ACLChecklist *ch);
 
 #endif /* USE_AUTH */
 #endif /* SQUID_AUTH_ACL_H */
