6b355e5a838f0228536fb83cbdb1be17fe7d81df
Amos Jeffries <squid3@treenet.co.nz>
2011-08-21 12:50:02 +1200

    Bug 3213: https sites (CONNECT) not open when using NTLM

--- src/client_side_request.cc
+++ src/client_side_request.cc
@@ -792,6 +792,7 @@ ClientRequestContext::clientAccessCheckDone(const allow_t &answer)
 #else
                                     NULL);
 #endif
+        http->getConn()->flags.readMore = true; // resume any pipeline reads.
         node = (clientStreamNode *)http->client_stream.tail->data;
         clientStreamRead(node, http, node->readBuffer);
         return;
@@ -1781,7 +1782,7 @@ ClientHttpRequest::handleAdaptationFailure(int errDetail, bool bypassable)
 #endif
 
     request->detailError(ERR_ICAP_FAILURE, errDetail);
-
+    c->flags.reaMore = true;
     node = (clientStreamNode *)client_stream.tail->data;
     clientStreamRead(node, this, node->readBuffer);
 }
