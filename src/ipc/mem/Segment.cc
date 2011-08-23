020cb4567c54a501c39ce8b82dade32da841d40e
Dmitry Kurochkin <dmitry.kurochkin@measurement-factory.com>
2011-08-24 00:08:05 +0400

    Fix warning in Ipc::Mem::Segment::reserve() on systems with unsigned off_t.

--- src/ipc/mem/Segment.cc
+++ src/ipc/mem/Segment.cc
@@ -129,7 +129,8 @@ Ipc::Mem::Segment::reserve(size_t chunkSize)
 {
     Must(theMem);
     // check for overflows
-    assert(static_cast<off_t>(chunkSize) >= 0);
+    // chunkSize >= 0 may result in warnings on systems where off_t is unsigned
+    assert(!chunkSize && static_cast<off_t>(chunkSize) > 0);
     assert(static_cast<off_t>(chunkSize) <= theSize);
     assert(theReserved <= theSize - static_cast<off_t>(chunkSize));
     void *result = reinterpret_cast<char*>(theMem) + theReserved;
