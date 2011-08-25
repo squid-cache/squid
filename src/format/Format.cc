bbe167486433963cf82616f1510c2994bee2fefa
Amos Jeffries <squid3@treenet.co.nz>
2011-08-26 00:32:02 +1200

    Bug 3310: regression: %<pt translates as %<p
    Separate the format tags into sub-groups for size-oriented checks.
    
    Taking the opportunity, separate the section along the namespace borders.

--- src/format/Format.cc
+++ src/format/Format.cc
@@ -226,12 +226,7 @@ Format::Format::dump(StoreEntry * entry, const char *name)
                 if (arg)
                     storeAppendPrintf(entry, "{%s}", arg);
 
-                for (struct TokenTableEntry *te = TokenTable; te->config != NULL; te++) {
-                    if (te->token_type == type) {
-                        storeAppendPrintf(entry, "%s", te->config);
-                        break;
-                    }
-                }
+                storeAppendPrintf(entry, "%s", t->label);
 
                 if (t->space)
                     entry->append(" ", 1);
