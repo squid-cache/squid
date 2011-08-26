6761161dcfb5c9f72368030bc9e755731811c8fc
Amos Jeffries <squid3@treenet.co.nz>
2011-08-26 17:09:04 +1200

    Portability: avoid htons() on 8-bit values. Little Endians complain.

--- src/ip/Address.cc
+++ src/ip/Address.cc
@@ -276,8 +276,8 @@ Ip::Address::IsSiteLocal6() const
 bool
 Ip::Address::IsSlaac() const
 {
-    return m_SocketAddr.sin6_addr.s6_addr[10] == htons(0xff) &&
-           m_SocketAddr.sin6_addr.s6_addr[11] == htons(0xfe);
+    return m_SocketAddr.sin6_addr.s6_addr[10] == static_cast<uint8_t>(0xff) &&
+           m_SocketAddr.sin6_addr.s6_addr[11] == static_cast<uint8_t>(0xfe);
 }
 
 bool
