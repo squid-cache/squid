4951d45059729e70a9c6624b11a7f148f2d0a824
Alex Rousskov <rousskov@measurement-factory.com>
2011-08-26 16:34:48 -0600

    Fixed overflow testing assertion.

--- src/ipc/mem/Segment.cc
+++ src/ipc/mem/Segment.cc
@@ -130,7 +130,7 @@ Ipc::Mem::Segment::reserve(size_t chunkSize)
     Must(theMem);
     // check for overflows
     // chunkSize >= 0 may result in warnings on systems where off_t is unsigned
-    assert(!chunkSize && static_cast<off_t>(chunkSize) > 0);
+    assert(!chunkSize || static_cast<off_t>(chunkSize) > 0);
     assert(static_cast<off_t>(chunkSize) <= theSize);
     assert(theReserved <= theSize - static_cast<off_t>(chunkSize));
     void *result = reinterpret_cast<char*>(theMem) + theReserved;
