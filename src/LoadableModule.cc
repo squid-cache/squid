e8004619989bde737c0e3392fe3d6b5725b8a002
Alex Rousskov <rousskov@measurement-factory.com>
2011-08-27 06:34:04 -0600

    Polished unused code.
    The unused dlopen() call is actually useful to enable when lt_dlopen() reports
    "file not found" errors for loadable modules that do exist but that Libtool
    cannot load successfully due to undefined symbols or other errors.
    
    This inability to correctly report a library loading error is a long-standing
    Libtool bug, stemming from Libtool's desire to try and load several
    differently named library files until one succeeds, losing true error
    information in the process.

--- src/LoadableModule.cc
+++ src/LoadableModule.cc
@@ -69,7 +69,7 @@ void *LoadableModule::openModule(int mode)
 #	if XSTD_USE_LIBLTDL
     return lt_dlopen(theName.termedBuf());
 #	else
-    return dlopen(theName.c_str(),
+    return dlopen(theName.termedBuf(),
                   mode == lmNow ? RTLD_NOW : RTLD_LAZY);
 #	endif
 }
