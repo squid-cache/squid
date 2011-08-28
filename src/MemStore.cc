c975f53276eb34b4ea0e9db626d98bbd605531d5
Dmitry Kurochkin <dmitry.kurochkin@measurement-factory.com>
2011-08-28 21:11:19 +0400

    Disable Rock store and shared memory cache if shared memory is not available.
    The patch adds support for building on systems without POSIX shared
    memory (e.g. OpenBSD).  IpcIo DiskIO module and Rock store is not
    enabled by default if shared memory support is not available.  If IpcIo
    or Rock store is explicitly enabled, but no shared memory support is
    available, fail with error during configure.  Fail with error at
    runtime if shared memory cache is enabled but no shared memory support
    is detected.

--- src/MemStore.cc
+++ src/MemStore.cc
@@ -362,11 +362,15 @@ void MemStoreRr::run(const RunnerRegistry &)
     // decide whether to use a shared memory cache if the user did not specify
     if (!Config.memShared.configured()) {
         Config.memShared.configure(AtomicOperationsSupported &&
-            UsingSmp() && Config.memMaxSize > 0);
+            Ipc::Mem::Segment::Enabled() && UsingSmp() &&
+            Config.memMaxSize > 0);
     } else
     if (Config.memShared && !AtomicOperationsSupported) {
         // bail if the user wants shared memory cache but we cannot support it
         fatal("memory_cache_shared is on, but no support for atomic operations detected");
+    } else
+    if (Config.memShared && !Ipc::Mem::Segment::Enabled()) {
+        fatal("memory_cache_shared is on, but no support for shared memory detected");
     }
 
     if (!Config.memShared)
