e557e9df40cfaf55f9441d0e4346076c1195ef66
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-04 12:51:47 -0600

    Minimize differences with trunk.

--- src/DiskIO/Blocking/BlockingFile.cc
+++ src/DiskIO/Blocking/BlockingFile.cc
@@ -217,7 +217,7 @@ void
 BlockingFile::writeDone(int rvfd, int errflag, size_t len)
 {
     assert (rvfd == fd);
-    debugs(79,3, HERE << "FD " << fd << ", len " << len);
+    debugs(79, 3, HERE << "FD " << fd << ", len " << len);
 
     WriteRequest::Pointer result = writeRequest;
     writeRequest = NULL;
