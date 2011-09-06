68caf6d9957d2445774b82671ee30fed7bcca838
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-06 11:37:29 -0600

    Log ICAP_ERR_GONE ICAP transaction outcome when ICAP initiator disappears early.
    In this case, "early" means "before the end of the ICAP transaction".
    Previously, "catch all" ICAP_ERR_UNKNOWN outcome was logged into the
    adaptation log in such cases, raising red flags that there is something wrong
    with the ICAP service or Squid.

--- src/adaptation/icap/Xaction.cc
+++ src/adaptation/icap/Xaction.cc
@@ -476,8 +476,10 @@ void Adaptation::Icap::Xaction::noteInitiatorAborted()
 {
 
     if (theInitiator.set()) {
+        debugs(93,4, HERE << "Initiator gone before ICAP transaction ended");
         clearInitiator();
         detailError(ERR_DETAIL_ICAP_INIT_GONE);
+        setOutcome(xoGone);
         mustStop("initiator aborted");
     }
 
