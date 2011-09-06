7c15a69ac905602442275a7f6b48719237f663f8
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-06 12:28:11 -0600

    Support maximum field width for string access.log fields.
    Some standard command-line and some log processing tools have trouble
    handling URLs or other logged fields exceeding 8KB in length. Moreover,
    Squid violates its own log line format and truncates the entire log line
    if, for example, the URL is 8KB long. By supporting .precision format
    argument, we allow the administrator to specify logged URL size and
    avoid these problems.
    
    Limiting logged field width has no effect on traffic on the wire, with
    the exception of log records if they are sent over the network, of course.
    
    TODO: The name comes from the printf(3) "precision" format part. It may
    be a good idea to rename our "precision" into max_width or similar,
    especially if we do not support floating point precision logging.
    
    TODO: Old code used chars to store user-configured field width and
    precision. That does not work for URLs, headers, and other entries
    longer than 256 characters. This patch changes the storage type to int.
    The code should probably be polished further to remove unsigned->signed
    conversions.

--- src/format/Format.cc
+++ src/format/Format.cc
@@ -1034,11 +1034,18 @@ Format::Format::assemble(MemBuf &mb, AccessLogEntry *al, int logSequenceNumber) 
                 }
             }
 
-            if (fmt->width) {
+            // enforce width limits if configured
+            const bool haveMaxWidth = fmt->precision && !doint && !dooff;
+            if (haveMaxWidth || fmt->width) {
+                const int minWidth = fmt->width ?
+                                     static_cast<int>(fmt->width) : 0;
+                const int maxWidth = haveMaxWidth ?
+                                     static_cast<int>(fmt->precision) : strlen(out);
+
                 if (fmt->left)
-                    mb.Printf("%-*s", (int) fmt->width, out);
+                    mb.Printf("%-*.*s", minWidth, maxWidth, out);
                 else
-                    mb.Printf("%*s", (int) fmt->width, out);
+                    mb.Printf("%*.*s", minWidth, maxWidth, out);
             } else
                 mb.append(out, strlen(out));
         } else {
