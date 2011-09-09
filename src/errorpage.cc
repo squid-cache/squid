495218b17badeedc524b4be045d44d9394e98419
Christos Tsantilas <chtsanti@users.sourceforge.net>
2011-09-09 04:16:43 -0600

    Regression: %I shows hostname on SSL error page
    The %I must print the server ip address, but currently displays the host name of
    the server on squid error pages. This patch fixes %I to print the server ip 
    address in the server or "[unknown]" otherwise.
    
    This is a Measurement Factory project

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -892,8 +892,8 @@ ErrorState::Convert(char token, bool building_deny_info_url, bool allowRecursion
         break;
 
     case 'I':
-        if (request && request->hier.host[0] != '\0') // if non-empty string
-            mb.Printf("%s", request->hier.host);
+        if (request && request->hier.tcpServer != NULL)
+            p = request->hier.tcpServer->remote.NtoA(ntoabuf,MAX_IPSTRLEN);
         else if (!building_deny_info_url)
             p = "[unknown]";
         break;
