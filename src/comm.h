7efa7b994a400d33ddc398987657a293a3278cce
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-09 04:11:34 -0600

    Cleanup: Marked comm_reset_close() parameter as const because it is.
    This allows const methods to call this Comm function with their conn member.

--- src/comm.h
+++ src/comm.h
@@ -19,7 +19,7 @@ SQUIDCEXTERN void commSetTcpKeepalive(int fd, int idle, int interval, int timeou
 extern void _comm_close(int fd, char const *file, int line);
 #define comm_close(x) (_comm_close((x), __FILE__, __LINE__))
 SQUIDCEXTERN void old_comm_reset_close(int fd);
-SQUIDCEXTERN void comm_reset_close(Comm::ConnectionPointer &conn);
+SQUIDCEXTERN void comm_reset_close(const Comm::ConnectionPointer &conn);
 #if LINGERING_CLOSE
 SQUIDCEXTERN void comm_lingering_close(int fd);
 #endif
