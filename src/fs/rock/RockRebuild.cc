462c68bc789f8aea59e41accb241894f28853356
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-10 13:38:15 -0600

    Polished debugging, #includes. And mkdir() no longer needs _SQUID_MSWIN_.

--- src/fs/rock/RockRebuild.cc
+++ src/fs/rock/RockRebuild.cc
@@ -193,8 +193,8 @@ Rock::Rebuild::failure(const char *msg, int errNo)
            dbOffset << " <= " << dbSize);
 
     if (errNo)
-        debugs(47,0, "Rock cache_dir rebuild failure: " << xstrerr(errNo));
-    debugs(47,0, "Do you need to run 'squid -z' to initialize storage?");
+        debugs(47, DBG_CRITICAL, "ERROR: Rock cache_dir rebuild failure: " << xstrerr(errNo));
+    debugs(47, DBG_CRITICAL, "Do you need to run 'squid -z' to initialize storage?");
 
     assert(sd);
     fatalf("Rock cache_dir[%d] rebuild of %s failed: %s.",
