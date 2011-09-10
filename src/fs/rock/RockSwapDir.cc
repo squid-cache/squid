a0d9ebfcfbab60e53db2cdb9bfff5bdbb46e19a2
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-09 19:25:27 -0600

    Moved squid.conf global disk_io_timeout to cache_dir-local swap-timeout.
    The I/O timeout option belongs to cache_dir because not all cache_dir types
    support it and because different cache_dirs may need different timeout values,
    especially if some of them handle very large or otherwise unusual files.
    
    To propagate the knowledge of the option down to DiskIO/IpcIoFile I decided
    to add a DiskFile::Config class and DiskFile::configure() method. At first
    glance that API does not belong to DiskFile because only IpcIoFile supports
    it. However, DiskFile may be a better location for it because
    
      * Other specific DiskIO files may want to support the same configuration
        API.
    
      * Placing API in IpcIoFile would require either making Rock Store dependent
        on IpcIoFile (in terms of linking and in terms of availability) or more
        complex API with multiple inheritance, dynamic casting and such. We can
        introduce the "more complex API" mentioned above later if needed.
    
    Renamed "disk_io" to "swap" timeout because the option is about the whole swap
    out/in delay (something an admin may care about) and not individual I/O
    (something only low-level code should know about).

--- src/fs/rock/RockSwapDir.cc
+++ src/fs/rock/RockSwapDir.cc
@@ -10,6 +10,7 @@
 #include "MemObject.h"
 #include "SquidMath.h"
 #include "base/RunnersRegistry.h"
+#include "ConfigOption.h"
 #include "DiskIO/DiskIOModule.h"
 #include "DiskIO/DiskIOStrategy.h"
 #include "DiskIO/ReadRequest.h"
@@ -226,6 +227,7 @@ Rock::SwapDir::init()
     }
 
     theFile = io->newFile(filePath);
+    theFile->configure(fileConfig);
     theFile->open(O_RDWR, 0644, this);
 
     // Increment early. Otherwise, if one SwapDir finishes rebuild before
@@ -279,6 +281,55 @@ Rock::SwapDir::parseSize()
     if (i < 0)
         fatal("negative Rock cache_dir size value");
     max_size = static_cast<uint64_t>(i) << 20; // MBytes to Bytes
+}
+
+ConfigOption *
+Rock::SwapDir::getOptionTree() const
+{
+    ConfigOptionVector *vector = dynamic_cast<ConfigOptionVector*>(::SwapDir::getOptionTree());
+    assert(vector);
+    vector->options.push_back(new ConfigOptionAdapter<SwapDir>(*const_cast<SwapDir *>(this), &SwapDir::parseTimeOption, &SwapDir::dumpTimeOption));
+    return vector;
+}
+
+/// parses time-specific options; mimics ::SwapDir::optionObjectSizeParse()
+bool
+Rock::SwapDir::parseTimeOption(char const *option, const char *value, int reconfiguring)
+{
+    // TODO: ::SwapDir or, better, Config should provide time-parsing routines,
+    // including time unit handling. Same for size.
+
+    time_msec_t *storedTime;
+    if (strcmp(option, "swap-timeout") == 0)
+        storedTime = &fileConfig.ioTimeout;
+    else
+        return false;
+
+    if (!value)
+        self_destruct();
+
+    const time_msec_t newTime = strtoll(value, NULL, 10);
+
+    if (newTime < 0) {
+        debugs(3, DBG_CRITICAL, "FATAL: cache_dir " << path << ' ' << option << " must not be negative but is: " << newTime);
+        self_destruct();
+    }
+
+    if (reconfiguring && *storedTime != newTime)
+        debugs(3, DBG_IMPORTANT, "cache_dir " << path << ' ' << option << " is now " << newTime);
+
+    *storedTime = newTime;
+
+    return true;
+}
+
+/// reports time-specific options; mimics ::SwapDir::optionObjectSizeDump()
+void
+Rock::SwapDir::dumpTimeOption(StoreEntry * e) const
+{
+    if (fileConfig.ioTimeout)
+        storeAppendPrintf(e, " swap-timeout=%"PRId64,
+                          static_cast<int64_t>(fileConfig.ioTimeout));
 }
 
 /// check the results of the configuration; only level-0 debugging works here
