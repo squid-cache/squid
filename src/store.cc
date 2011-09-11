41ed4ee6f80065eff896bdae57427c58a1fe985b
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-11 16:08:15 -0600

    Polished debugging: Use DBG_CRITICAL for level-0 messages.

--- src/store.cc
+++ src/store.cc
@@ -1236,7 +1236,7 @@ StoreController::maintain()
 
     if (Store::Root().currentSize() > Store::Root().maxSize()) {
         if (squid_curtime - last_warn_time > 10) {
-            debugs(20, 0, "WARNING: Disk space over limit: "
+            debugs(20, DBG_CRITICAL, "WARNING: Disk space over limit: "
                    << Store::Root().currentSize() / 1024.0 << " KB > "
                    << (Store::Root().maxSize() >> 10) << " KB");
             last_warn_time = squid_curtime;
