26406f192fb1eca0e6c030e1a0d095ad97c5eca9
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-14 00:28:54 -0600

    Add RunnersRegistry, an API to register and, later, run a group of actions.
    Useful for keeping general initialization/cleanup management code (e.g.,
    main.cc) independent from specific initialization/cleanup code (e.g., 
    Store file systems or memory cache) during staged initialization and
    cleaning.
    
    Designed with Rock Store needs in mind. Currently unused. Should eventually be
    used for most modules initialization and cleanup, removing main.cc dependency
    on those modules and perfecting [de]initialization order.

---
+++ src/base/RunnersRegistry.h
@@ -0,0 +1,61 @@
+#ifndef SQUID_BASE_RUNNERSREGISTRY_H
+#define SQUID_BASE_RUNNERSREGISTRY_H
+
+/**
+ * This API allows virtually any module to register with a well-known registry,
+ * be activated by some central processor at some registry-specific time, and
+ * be deactiveated by some central processor at some registry-specific time.
+ *
+ * For example, main.cc may activate registered I/O modules after parsing
+ * squid.conf and deactivate them before exiting.
+ *
+ * A module in this context is code providing a functionality or service to the
+ * rest of Squid, such as src/DiskIO/Blocking, src/fs/ufs, or Cache Manager. A
+ * module must declare a RegisteredRunner child class to implement activation and
+ * deactivation logic using the run() method and destructor, respectively.
+ *
+ * This API allows the registry to determine the right [de]activation time for
+ * each group of similar modules, without knowing any module specifics.
+ *
+ */
+
+/// well-known registries
+typedef enum {
+    /// managed by main.cc; activated after parsing squid.conf and
+    /// deactivated before freeing configuration-related memory or exit()-ing
+    rrAfterConfig,
+
+    rrEnd ///< not a real registry, just a label to mark the end of enum
+} RunnerRegistry;
+
+/// a runnable registrant API
+class RegisteredRunner
+{
+public:
+    // called when this runner's registry is deactivated
+    virtual ~RegisteredRunner() {}
+
+    // called when this runner's registry is activated
+    virtual void run(const RunnerRegistry &r) = 0;
+};
+
+
+/// registers a given runner with the given registry and returns registry count
+int RegisterRunner(const RunnerRegistry &registry, RegisteredRunner *rr);
+
+/// calls run() methods of all runners in the given registry
+int ActivateRegistered(const RunnerRegistry &registry);
+/// deletes all runners in the given registry
+void DeactivateRegistered(const RunnerRegistry &registry);
+
+
+/// convenience function to "use" an otherwise unreferenced static variable
+bool UseThisStatic(const void *);
+
+/// convenience macro: register one RegisteredRunner kid as early as possible
+#define RunnerRegistrationEntry(Registry, Who) \
+    static const bool Who ## _RegisteredWith_ ## Registry = \
+        RegisterRunner(Registry, new Who) > 0 && \
+        UseThisStatic(& Who ## _RegisteredWith_ ## Registry);
+
+#endif /* SQUID_BASE_RUNNERSREGISTRY_H */
