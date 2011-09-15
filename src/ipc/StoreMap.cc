70288a5cb51aee3db3d4fd85f4292b79c2f6e5fb
Alex Rousskov <rousskov@measurement-factory.com>
2011-09-15 08:54:06 -0600

    Removed "pointless comparison of unsigned integer with 0" to make ICC happier.
    Besides making ICC compiler happier, we should not check parameter values that
    are just passed to other Squid code. Let the recepient code check them.

--- src/ipc/StoreMap.cc
+++ src/ipc/StoreMap.cc
@@ -13,7 +13,6 @@ Ipc::StoreMap::Owner *
 Ipc::StoreMap::Init(const char *const path, const int limit, const size_t extrasSize)
 {
     assert(limit > 0); // we should not be created otherwise
-    assert(extrasSize >= 0);
     Owner *const owner = shm_new(Shared)(path, limit, extrasSize);
     debugs(54, 5, HERE << "new map [" << path << "] created: " << limit);
     return owner;
