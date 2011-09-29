c397963f33267ed9051eea7042cc1d72a1df8d91
Francesco Chemolli <kinkie@squid-cache.org>
2011-09-29 08:53:34 +0200

    Optimized HttpHdrCc::packInto and clarified its behaviour in comments Negative values to Cache-Control directives are now explicitly discarded Clarified documentation

--- src/HttpHdrCc.cc
+++ src/HttpHdrCc.cc
@@ -223,22 +223,30 @@ HttpHdrCc::packInto(Packer * p) const
     for (flag = CC_PUBLIC; flag < CC_ENUM_END; ++flag) {
         if (isSet(flag) && flag != CC_OTHER) {
 
-            /* print option name */
+            /* print option name for all options */
             packerPrintf(p, (pcount ? ", %s": "%s") , CcAttrs[flag].name);
 
-            /* handle options with values */
-
-            if (flag == CC_MAX_AGE)
+            /* for all options having values, "=value" after the name */
+            switch (flag) {
+            case CC_MAX_AGE:
                 packerPrintf(p, "=%d", (int) maxAge());
-
-            if (flag == CC_S_MAXAGE)
+                break;
+            case CC_S_MAXAGE:
                 packerPrintf(p, "=%d", (int) sMaxAge());
-
-            if (flag == CC_MAX_STALE && maxStale()!=MAX_STALE_ANY)
-                packerPrintf(p, "=%d", (int) maxStale());
-
-            if (flag == CC_MIN_FRESH)
+                break;
+            case CC_MAX_STALE:
+                /* max-stale's value is optional.
+                  If we didn't receive it, don't send it */
+                if (maxStale()!=MAX_STALE_ANY)
+                    packerPrintf(p, "=%d", (int) maxStale());
+                break;
+            case CC_MIN_FRESH:
                 packerPrintf(p, "=%d", (int) minFresh());
+                break;
+            default:
+                /* do nothing, directive was already printed */
+                break;
+            }
 
             ++pcount;
         }
