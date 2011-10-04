f5480a9e690773150236ab3f001d9a5c6d946b74
Dmitry Kurochkin <dmitry.kurochkin@measurement-factory.com>
2011-10-04 21:26:58 +0400

    Add base class for registered runners that work with shared memory.
    Shared memory runners do two main things:
    
    * create shared memory segments
    * open shared memory segments created earlier
    
    Each runner has to decide whether it needs to create and/or open
    shared memory segments.  Common runners share (some) logic in making
    this decisions.  The purpose of this patch is to implement this logic
    in a single place to allow easy reuse and avoid duplication.

--- src/ipc/mem/Segment.cc
+++ src/ipc/mem/Segment.cc
@@ -256,3 +256,23 @@ Ipc::Mem::Segment::checkSupport(const char *const context)
 }
 
 #endif // HAVE_SHM
+
+void
+Ipc::Mem::RegisteredRunner::run(const RunnerRegistry &r)
+{
+    // If Squid is built with real segments, we create() real segments
+    // in the master process only.  Otherwise, we create() fake
+    // segments in each worker process.  We assume that only workers
+    // need and can work with fake segments.
+#if HAVE_SHM
+    if (IamMasterProcess())
+#else
+    if (IamWorker())
+#endif
+        create(r);
+
+    // we assume that master process does not need shared segments
+    // unless it is also a worker
+    if (!InDaemonMode() || !IamMasterProcess())
+        open(r);
+}
