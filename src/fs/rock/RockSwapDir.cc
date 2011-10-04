277055913c5da9cf937ea771668803d92ab482b3
Dmitry Kurochkin <dmitry.kurochkin@measurement-factory.com>
2011-10-04 21:29:09 +0400

    Remove entryLimitAllowed() TODO from RockSwapDirRr::run().
    RockSwapDirRr::run() calls sd->entryLimitAllowed() when sd does not
    have a map yet, which causes entryLimitAllowed() to use zero for the
    lower limit.  But that OK so we can remove the TODO.

--- src/fs/rock/RockSwapDir.cc
+++ src/fs/rock/RockSwapDir.cc
@@ -833,7 +833,6 @@ void RockSwapDirRr::create(const RunnerRegistry &)
     Must(owners.empty());
     for (int i = 0; i < Config.cacheSwap.n_configured; ++i) {
         if (const Rock::SwapDir *const sd = dynamic_cast<Rock::SwapDir *>(INDEXSD(i))) {
-            // TODO: check whether entryLimitAllowed() has map here
             Rock::SwapDir::DirMap::Owner *const owner =
                 Rock::SwapDir::DirMap::Init(sd->path, sd->entryLimitAllowed());
             owners.push_back(owner);
