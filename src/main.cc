51d61a86b334618ba00fc8516b2618cbcd651335
Stefan Fritsch <sf@sfritsch.de>
2011-10-09 00:02:17 -0600

    Bug 3368: Unhandled exceptions are not logged (workaround)

--- src/main.cc
+++ src/main.cc
@@ -1176,9 +1176,11 @@ SquidMainSafe(int argc, char **argv)
         return SquidMain(argc, argv);
     } catch (const std::exception &e) {
         std::cerr << "dying from an unhandled exception: " << e.what() << std::endl;
+        debugs(1, DBG_CRITICAL, "FATAL: dying from an unhandled exception: " << e.what());
         throw;
     } catch (...) {
         std::cerr << "dying from an unhandled exception." << std::endl;
+        debugs(1, DBG_CRITICAL, "FATAL: dying from an unhandled exception.");
         throw;
     }
     return -1; // not reached
