fd86b48bbac5c3734b39ec93583f9bbd00339bcc
Alex Rousskov <rousskov@measurement-factory.com>
2011-10-14 09:53:02 -0600

    Add basic "make check" tests for Rock Store, based on UFS and COSS tests.

--- src/tests/stub_tools.cc
+++ src/tests/stub_tools.cc
@@ -72,7 +72,10 @@ bool
 IamMasterProcess()
 {
     fprintf(stderr, "Not implemented");
-    return false;
+    // Since most tests run as a single process, this is the best default.
+    // TODO: If some test case uses multiple processes and cares about
+    // its role, we may need to parameterize or remove this stub.
+    return true;
 }
 
 bool
