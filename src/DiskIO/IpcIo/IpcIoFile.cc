043abaa9171d908b775db1686ce044933a71caef
Alex Rousskov <rousskov@measurement-factory.com>
2011-10-23 20:29:21 -0600

    Account for max-swap-rate in swap-timeout handling for Rock.
    Current swap-timeout code does not know about max-swap-rate.  It
    simply finds the longest-waiting I/O in disker queues (incoming and
    outgoing) and then assumes that the new I/O will wait at least that
    long.  The assumption is likely to be wrong when the queue contains
    lots of freshly queued requests to disker: Those requests have not
    waited long yet, but a max-swap-rate limit will slow them down
    shortly.
    
    The patch changes the swap-timeout code to account for max-swap-rate
    when dealing with the workers-to-disker queue: If there are N requests
    pending, the new one will wait at least N/max-swap-rate seconds.  Also
    expected wait time is adjusted based on the queue "balance" member, in
    case we have been borrowing time against future I/O already.

--- src/DiskIO/IpcIo/IpcIoFile.cc
+++ src/DiskIO/IpcIo/IpcIoFile.cc
@@ -360,7 +360,21 @@ IpcIoFile::canWait() const
     if (!queue->peek(diskId, oldestIo) || oldestIo.start.tv_sec <= 0)
         return true; // we cannot estimate expected wait time; assume it is OK
 
-    const int expectedWait = tvSubMsec(oldestIo.start, current_time);
+    const int oldestWait = tvSubMsec(oldestIo.start, current_time);
+
+    int rateWait = -1; // time in millisecons
+    const Ipc::QueueReader::Rate::Value ioRate = queue->rateLimit(diskId);
+    if (ioRate > 0) {
+        // if there are N requests pending, the new one will wait at
+        // least N/max-swap-rate seconds
+        rateWait = static_cast<int>(1e3 * queue->outSize(diskId) / ioRate);
+        // adjust N/max-swap-rate value based on the queue "balance"
+        // member, in case we have been borrowing time against future
+        // I/O already
+        rateWait += queue->balance(diskId);
+    }
+
+    const int expectedWait = max(oldestWait, rateWait);
     if (expectedWait < 0 ||
             static_cast<time_msec_t>(expectedWait) < config.ioTimeout)
         return true; // expected wait time is acceptible
