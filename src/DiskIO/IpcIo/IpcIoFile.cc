3ce26b09910a9918ea711883e42d1712365d7eee
Automatic source maintenance <squidadm@squid-cache.org>
2011-10-29 16:45:17 -0600

    SourceFormat Enforcement

--- src/DiskIO/IpcIo/IpcIoFile.cc
+++ src/DiskIO/IpcIo/IpcIoFile.cc
@@ -866,7 +866,7 @@ void
 IpcIoClaimMemoryNeedsRr::run(const RunnerRegistry &)
 {
     const int itemsCount = Ipc::FewToFewBiQueue::MaxItemsCount(
-        ::Config.workers, ::Config.cacheSwap.n_strands, QueueCapacity);
+                               ::Config.workers, ::Config.cacheSwap.n_strands, QueueCapacity);
     // the maximum number of shared I/O pages is approximately the
     // number of queue slots, we add a fudge factor to that to account
     // for corner cases where I/O pages are created before queue
