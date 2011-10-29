2016e5a9f1d586a4233b1a211f1d5bdda77f7863
Dmitry Kurochkin <dmitry.kurochkin@measurement-factory.com>
2011-10-29 05:49:34 -0600

    Provide "fake" AtomicWordT implementation for non-SMP configurations.
    While we can not provide real AtomicWordT implementation on the systems where
    atomic operations are not available, we can use a "fake" one if Squid is
    running in non-SMP mode.  Before the change, the "fake" implementation was
    always asserting, which is too restrictive and leads to test failures on
    systems without atomic operations.
    
    The new implementation works under conditions similar to "fake" shared memory
    segments and allows SMP-using code (e.g. Rock store) to work in non-SMP mode.
    In particular, it allows tests to pass on such systems.
    
    AtomicWordT was renamed to WordT and moved to Ipc::Atomic namespace to allow
    Ipc::Atomic::Enabled() to be declared outside of the AtomicWordT template
    class.  This lets us define the Enabled() method in AtomicWord.cc which avoids
    dragging protos.h #include into the AtomicWord.h header.

--- src/MemStore.cc
+++ src/MemStore.cc
@@ -397,10 +397,10 @@ void MemStoreRr::run(const RunnerRegistry &r)
 {
     // decide whether to use a shared memory cache if the user did not specify
     if (!Config.memShared.configured()) {
-        Config.memShared.configure(AtomicOperationsSupported &&
+        Config.memShared.configure(Ipc::Atomic::Enabled() &&
                                    Ipc::Mem::Segment::Enabled() && UsingSmp() &&
                                    Config.memMaxSize > 0);
-    } else if (Config.memShared && !AtomicOperationsSupported) {
+    } else if (Config.memShared && !Ipc::Atomic::Enabled()) {
         // bail if the user wants shared memory cache but we cannot support it
         fatal("memory_cache_shared is on, but no support for atomic operations detected");
     } else if (Config.memShared && !Ipc::Mem::Segment::Enabled()) {
--- src/ipc/AtomicWord.h
+++ src/ipc/AtomicWord.h
@@ -6,17 +6,27 @@
 #ifndef SQUID_IPC_ATOMIC_WORD_H
 #define SQUID_IPC_ATOMIC_WORD_H
 
+namespace Ipc
+{
+
+namespace Atomic
+{
+
+/// Whether atomic operations support is available
+bool Enabled();
+
 #if HAVE_ATOMIC_OPS
+
 /// Supplies atomic operations for an integral Value in memory shared by kids.
 /// Used to implement non-blocking shared locks, queues, tables, and pools.
 template <class ValueType>
-class AtomicWordT
+class WordT
 {
 public:
     typedef ValueType Value;
 
-    AtomicWordT() {} // leave value unchanged
-    AtomicWordT(Value aValue): value(aValue) {} // XXX: unsafe
+    WordT() {} // leave value unchanged
+    WordT(Value aValue): value(aValue) {} // XXX: unsafe
 
     Value operator +=(int delta) { return __sync_add_and_fetch(&value, delta); }
     Value operator -=(int delta) { return __sync_sub_and_fetch(&value, delta); }
@@ -25,64 +35,67 @@ public:
     Value operator ++(int) { return __sync_fetch_and_add(&value, 1); }
     Value operator --(int) { return __sync_fetch_and_sub(&value, 1); }
 
-    bool swap_if(const int comparand, const int replacement) { return __sync_bool_compare_and_swap(&value, comparand, replacement); }
+    bool swap_if(const Value comparand, const Value replacement) { return __sync_bool_compare_and_swap(&value, comparand, replacement); }
 
     /// v1 = value; value &= v2; return v1;
     Value fetchAndAnd(const Value v2) { return __sync_fetch_and_and(&value, v2); }
 
     // TODO: no need for __sync_bool_compare_and_swap here?
-    bool operator ==(int v2) { return __sync_bool_compare_and_swap(&value, v2, value); }
+    bool operator ==(const Value v2) { return __sync_bool_compare_and_swap(&value, v2, value); }
 
     // TODO: no need for __sync_fetch_and_add here?
     Value get() const { return __sync_fetch_and_add(const_cast<Value*>(&value), 0); }
     operator Value () const { return get(); }
 
 private:
+
     Value value;
 };
 
-enum { AtomicOperationsSupported = 1 };
-
 #else
-/// A wrapper to provide AtomicWordT API (and asserting implementation)
+
+/// A wrapper to provide AtomicWordT API (and implementation asserting in SMP mode)
 /// where we do not support atomic operations. This avoids ifdefs in core code.
 template <class ValueType>
-class AtomicWordT
+class WordT
 {
 public:
     typedef ValueType Value;
 
-    AtomicWordT() {} // leave value unchanged
-    AtomicWordT(Value aValue): value(aValue) {} // XXX: unsafe
+    WordT() {} // leave value unchanged
+    WordT(Value aValue): value(aValue) {} // XXX: unsafe
 
-    Value operator +=(int) { assert(false); return *this; }
+    Value operator +=(int delta) { assert(Enabled()); return value += delta; }
     Value operator ++() { return *this += 1; }
     Value operator --() { return *this += -1; }
-    Value operator ++(int) { assert(false); return *this; }
-    Value operator --(int) { assert(false); return *this; }
+    Value operator ++(int) { assert(Enabled()); return value++; }
+    Value operator --(int) { assert(Enabled()); return value--; }
 
-    bool swap_if(const int comparand, const int replacement)
-    { assert(false); return false; }
+    bool swap_if(const Value comparand, const Value replacement)
+    { assert(Enabled()); return value == comparand ? value = replacement, true : false; }
 
     /// v1 = value; value &= v2; return v1;
     Value fetchAndAnd(const Value v2)
-    { assert(false); return value; }
+    { assert(Enabled()); const Value v1 = value; value &= v2; return v1; }
 
     // TODO: no need for __sync_bool_compare_and_swap here?
-    bool operator ==(int v2) { assert(false); return false; }
+    bool operator ==(const Value v2) { assert(Enabled()); return value == v2; }
 
     // TODO: no need for __sync_fetch_and_add here?
-    Value get() const { assert(false); return value; }
+    Value get() const { assert(Enabled()); return value; }
     operator Value () const { return get(); }
 
 private:
+
     Value value;
 };
 
-enum { AtomicOperationsSupported = 0 };
-
 #endif /* HAVE_ATOMIC_OPS */
 
-typedef AtomicWordT<int> AtomicWord;
+typedef WordT<int> Word;
+
+} // namespace Atomic
+
+} // namespace Ipc
 
 #endif // SQUID_IPC_ATOMIC_WORD_H
