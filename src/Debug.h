d8b2269703fa22a63e2a4b73ddfa596f75f728b4
Amos Jeffries <squid3@treenet.co.nz>
2011-11-05 19:27:52 -0600

    Bug 3406: SSL Log Error in debug

--- src/Debug.h
+++ src/Debug.h
@@ -137,8 +137,8 @@ inline std::ostream& operator <<(std::ostream &os, const uint8_t d)
 }
 
 /* Legacy debug style. Still used in some places. needs to die... */
-#define do_debug(SECTION, LEVEL)   ((Debug::level = (LEVEL)) > Debug::Levels[SECTION])
-#define old_debug(SECTION, LEVEL)  if( (Debug::level=(LEVEL)) <= Debug::Levels[SECTION] ) _db_print
+#define do_debug(SECTION, LEVEL)   ((Debug::level = (LEVEL)) <= Debug::Levels[SECTION])
+#define old_debug(SECTION, LEVEL)  if do_debug((SECTION), (LEVEL)) _db_print
 
 /* Legacy debug function definitions */
 extern void _db_init(const char *logfile, const char *options);
