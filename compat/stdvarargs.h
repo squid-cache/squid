49217c2ec9cd262ac599ae8f5df861c8a02d9f08
Amos Jeffries <squid3@treenet.co.nz>
2011-11-06 01:16:18 -0600

    Portability: add cstdarg as source of var_arg when available

--- compat/stdvarargs.h
+++ compat/stdvarargs.h
@@ -7,28 +7,35 @@
 
 /*
  * va_* variables come from various places on different platforms.
- * We provide a clean set of wrappers for the variosu operations
+ * We provide a clean set of wrappers for the various operations
  * Depending on what is available and needed.
  */
-#if defined(HAVE_STDARG_H)
+#if HAVE_CSTDARG && defined(__cplusplus)
+#include <cstdarg>
+
+#else
+#if HAVE_STDARG_H
 #include <stdarg.h>
 #define HAVE_STDARGS            /* let's hope that works everywhere (mj) */
 #define VA_LOCAL_DECL va_list ap;
 #define VA_START(f) va_start(ap, f)
 #define VA_SHIFT(v,t) ;         /* no-op for ANSI */
 #define VA_END va_end(ap)
+
 #else
-#if defined(HAVE_VARARGS_H)
+#if HAVE_VARARGS_H
 #include <varargs.h>
 #undef HAVE_STDARGS
 #define VA_LOCAL_DECL va_list ap;
 #define VA_START(f) va_start(ap)        /* f is ignored! */
 #define VA_SHIFT(v,t) v = va_arg(ap,t)
 #define VA_END va_end(ap)
+
 #else
 #error XX **NO VARARGS ** XX
-#endif
-#endif
+#endif /* HAVE_VARARGS_H */
+#endif /* HAVE_STDARG_H */
+#endif /* HAVE_CSTDARG */
 
 /* Make sure syslog goes after stdarg/varargs */
 #ifdef HAVE_SYSLOG_H
