97b32442a889576ad39ece31dce4910c0a6def88
Amos Jeffries <squid3@treenet.co.nz>
2011-11-08 03:54:37 -0700

    Document and alter the pconn idle timeout directives.
    Alters the directive names to clarify what they do and adds some more
    description to the config file documentation.
    
    Alters the internal config variables to match the new directive names.
    
    Also alters the well known messages in mgr:filedescriptors report a little
    to indicate client/server type and adds a standard "Idle " prefix for
    easy automated scanning.

--- src/client_side.cc
+++ src/client_side.cc
@@ -1485,14 +1485,14 @@ ConnStateData::readNextRequest()
 {
     debugs(33, 5, HERE << clientConnection << " reading next req");
 
-    fd_note(clientConnection->fd, "Waiting for next request");
+    fd_note(clientConnection->fd, "Idle client: Waiting for next request");
     /**
      * Set the timeout BEFORE calling clientReadRequest().
      */
     typedef CommCbMemFunT<ConnStateData, CommTimeoutCbParams> TimeoutDialer;
     AsyncCall::Pointer timeoutCall = JobCallback(33, 5,
                                      TimeoutDialer, this, ConnStateData::requestTimeout);
-    commSetConnTimeout(clientConnection, Config.Timeout.persistent_request, timeoutCall);
+    commSetConnTimeout(clientConnection, Config.Timeout.clientIdlePconn, timeoutCall);
 
     readSomeData();
     /** Please don't do anything with the FD past here! */
