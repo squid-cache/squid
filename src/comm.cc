7cec8f6f908335d09d528cfcb0d695b489a57897
Amos Jeffries <squid3@treenet.co.nz>
2011-11-24 03:44:38 -0700

    Bug 3366: assertion comm.cc:1276: isOpen(fd) via CompositePoolNode::kickReads
    Check Deferred read FD is still active before re-scheduling the read.

--- src/comm.cc
+++ src/comm.cc
@@ -2047,6 +2047,19 @@ DeferredReadManager::popHead(CbDataListContainer<DeferredRead> &deferredReads)
     assert (!deferredReads.empty());
 
     DeferredRead &read = deferredReads.head->element;
+
+    // NOTE: at this point the connection has been paused/stalled for an unknown
+    //       amount of time. We must re-validate that it is active and usable.
+
+    // If the connection has been closed already. Cancel this read.
+    if (!Comm::IsConnOpen(read.theRead.conn)) {
+        if (read.closer != NULL) {
+            read.closer->cancel("Connection closed before.");
+            read.closer = NULL;
+        }
+        read.markCancelled();
+    }
+
     if (!read.cancelled) {
         comm_remove_close_handler(read.theRead.conn->fd, read.closer);
         read.closer = NULL;
