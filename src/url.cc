3a039cbe7bc09c6a0b0db08e5086cb98451382d1
Amos Jeffries <squid3@treenet.co.nz>
2011-11-24 00:04:58 -0700

    Polish: convert one SECURITY WARNING to ALERT

--- src/url.cc
+++ src/url.cc
@@ -327,7 +327,7 @@ urlParse(const HttpRequestMethod& method, char *url, HttpRequest *request)
 
         // Bug 3183 sanity check: If scheme is present, host must be too.
         if (protocol != AnyP::PROTO_NONE && (host == NULL || *host == '\0')) {
-            debugs(23, DBG_IMPORTANT, "SECURITY WARNING: Missing hostname in URL '" << url << "'. see access.log for details.");
+            debugs(23, DBG_IMPORTANT, "SECURITY ALERT: Missing hostname in URL '" << url << "'. see access.log for details.");
             return NULL;
         }
 
