03de457e36d89eabd405f2f89e305987a8039373
yabuki <yabuki@sraoss.co.jp>
2011-11-27 14:34:50 -0700

    Bug 3432: Crash logging FTP errors

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -779,7 +779,7 @@ ErrorState::Dump(MemBuf * mb)
 
     if (ftp.request) {
         str.Printf("FTP Request: %s\r\n", ftp.request);
-        str.Printf("FTP Reply: %s\r\n", ftp.reply);
+        str.Printf("FTP Reply: %s\r\n", (ftp.reply? ftp.reply:"[none]"));
         str.Printf("FTP Msg: ");
         wordlistCat(ftp.server_msg, &str);
         str.Printf("\r\n");
