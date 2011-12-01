5cf4e671950487c6232a9f31b093bbfdb44cc73b
Jan Klemkow <j.klemkow@wemelug.de>
2011-12-02 02:16:08 +1300

    Bug 3422: Buffer overflow in recv-announce

--- src/recv-announce.cc
+++ src/recv-announce.cc
@@ -86,7 +86,7 @@ main(int argc, char *argv[])
     socklen_t len;
 
     struct hostent *hp = NULL;
-    char logfile[BUFSIZ];
+    const char *logfile;
     char ip[4];
 
     for (len = 0; len < 32; len++) {
@@ -95,9 +95,9 @@ main(int argc, char *argv[])
 
 
     if (argc > 1)
-        strcpy(logfile, argv[1]);
+        logfile = argv[1];
     else
-        strcpy(logfile, "/tmp/recv-announce.log");
+        logfile = "/tmp/recv-announce.log";
 
     close(1);
 
