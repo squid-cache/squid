d73139bd3a96420871e4a42729a04e55949beb92
kinkie@squid-cache.org <>
2011-12-03 18:05:32 +0100

    Fix arguments to swaplog renaming calls

--- src/fs/ufs/store_dir_ufs.cc
+++ src/fs/ufs/store_dir_ufs.cc
@@ -743,14 +743,14 @@ UFSSwapDir::closeTmpSwapLog()
 
     if (xrename(new_path, swaplog_path) < 0) {
         debugs(50, DBG_IMPORTANT, "ERROR: " << swaplog_path << ": " << xstrerror());
-        fatalf("Failed to rename log file %s with .new", swaplog_path);
+        fatalf("Failed to rename log file %s to %s.new", swaplog_path, swaplog_path);
     }
 
     fd = file_open(swaplog_path, O_WRONLY | O_CREAT | O_BINARY);
 
     if (fd < 0) {
         debugs(50, DBG_IMPORTANT, "ERROR: " << swaplog_path << ": " << xstrerror());
-        fatalf("Failed to open swap log ", swaplog_path);
+        fatalf("Failed to open swap log %s", swaplog_path);
     }
 
     safe_free(swaplog_path);
