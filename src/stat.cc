143690d21ce142d5628bf21e5dce94fa1c302816
Francesco Chemolli <kinkie@squid-cache.org>
2011-12-06 06:32:23 +0100

    Refactored StatHist::clean

--- src/stat.cc
+++ src/stat.cc
@@ -1515,19 +1515,19 @@ static void
 statCountersClean(StatCounters * C)
 {
     assert(C);
-    statHistClean(&C->client_http.all_svc_time);
-    statHistClean(&C->client_http.miss_svc_time);
-    statHistClean(&C->client_http.nm_svc_time);
-    statHistClean(&C->client_http.nh_svc_time);
-    statHistClean(&C->client_http.hit_svc_time);
-    statHistClean(&C->icp.query_svc_time);
-    statHistClean(&C->icp.reply_svc_time);
-    statHistClean(&C->dns.svc_time);
-    statHistClean(&C->cd.on_xition_count);
-    statHistClean(&C->comm_icp_incoming);
-    statHistClean(&C->comm_dns_incoming);
-    statHistClean(&C->comm_http_incoming);
-    statHistClean(&C->select_fds_hist);
+    C->client_http.all_svc_time.clear();
+    C->client_http.miss_svc_time.clear();
+    C->client_http.nm_svc_time.clear();
+    C->client_http.nh_svc_time.clear();
+    C->client_http.hit_svc_time.clear();
+    C->icp.query_svc_time.clear();
+    C->icp.reply_svc_time.clear();
+    C->dns.svc_time.clear();
+    C->cd.on_xition_count.clear();
+    C->comm_icp_incoming.clear();
+    C->comm_dns_incoming.clear();
+    C->comm_http_incoming.clear();
+    C->select_fds_hist.clear();
 }
 
 /* add special cases here as they arrive */
