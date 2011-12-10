82146cc800aa12d238a4bf3343303598fe2cc267
Francesco Chemolli <kinkie@squid-cache.org>
2011-12-10 22:34:36 +0100

    Fixed some more dead references to stats.

--- src/comm/ModDevPoll.cc
+++ src/comm/ModDevPoll.cc
@@ -174,10 +174,9 @@ comm_update_fd(int fd, int events)
 
 static void commIncomingStats(StoreEntry *sentry)
 {
-    StatCounters *f = &statCounter;
     storeAppendPrintf(sentry, "Total number of devpoll loops: %ld\n", statCounter.select_loops);
     storeAppendPrintf(sentry, "Histogram of returned filedescriptors\n");
-    statHistDump(&f->select_fds_hist, sentry, statHistIntDumper);
+    statCounter.select_fds_hist.dump(sentry, statHistIntDumper);
 }
 
 
