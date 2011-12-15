9ae4c1b4afeeeb7bacc1cfbc97168fec955b9c8c
Francesco Chemolli <kinkie@squid-cache.org>
2011-12-15 08:01:07 +0100

    Updated (and made use of) stub_StatHist

--- src/tests/stub_StatHist.cc
+++ src/tests/stub_StatHist.cc
@@ -1,10 +1,24 @@
-#include "squid.h"
-
-// for StatHist definitions
+#include "config.h"
+#include "STUB.h"
 #include "StatHist.h"
 
+#define STUB_API "StatHist.cc"
+
 void
-statHistEnumInit(StatHist * H, int last_enum)
-{
-//NO-OP    fatal("statHistEnumInit: Not implemented");
-}
+StatHist::init(int capacity_, hbase_f * val_in_, hbase_f * val_out_, double min_, double max_)
+{}
+
+StatHist::~StatHist()
+{}
+
+void
+StatHist::enumInit(int last_enum)
+{}
+
+void
+StatHist::count(double val)
+{}
+
+void
+StatHist::dump(StoreEntry * sentry, StatHistBinDumper * bd) const
+{}
