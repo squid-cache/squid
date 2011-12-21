7879b3a343567b4641a0e83b6f089fb248ce4753
Alex Rousskov <rousskov@measurement-factory.com>
2011-12-20 18:44:04 -0700

    Cherry-picked trunk r11900: Bug 3433: Segfault closing SNMP

--- src/snmp_core.cc
+++ src/snmp_core.cc
@@ -358,13 +358,13 @@ snmpPortOpened(const Comm::ConnectionPointer &conn, int errNo)
 void
 snmpConnectionClose(void)
 {
-    if (!Comm::IsConnOpen(snmpIncomingConn)) {
+    if (Comm::IsConnOpen(snmpIncomingConn)) {
         debugs(49, DBG_IMPORTANT, "Closing SNMP receiving port " << snmpIncomingConn->local);
         snmpIncomingConn->close();
     }
     snmpIncomingConn = NULL;
 
-    if (!Comm::IsConnOpen(snmpOutgoingConn) && snmpIncomingConn != snmpOutgoingConn) {
+    if (Comm::IsConnOpen(snmpOutgoingConn) && snmpIncomingConn != snmpOutgoingConn) {
         // Perform OUT port closure so as not to step on IN port when sharing a conn.
         debugs(49, DBG_IMPORTANT, "Closing SNMP sending port " << snmpOutgoingConn->local);
         snmpOutgoingConn->close();
