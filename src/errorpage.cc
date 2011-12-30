d7228daf2ffc5aa7890527326db203890e2e7fe5
Amos Jeffries <squid3@treenet.co.nz>
2011-12-31 05:01:37 +1300

    Cache Manager migration support
     * Add a little bit of XHR script to the CGI cachemgr front page which
    probes each of the managed proxies for http:// and https:// capabilities
    and produces web links to their internal managers.
    
     * Reserve the template name MGR_INDEX for use by cachemgr scripts.
    But do not distribute any preset template. This allows manager apps to
    provide their own static template with linked scripts and objects.
    
     * The error page system is updated to create a blanket message
    indicating missing template instead of aborting Squid if a template is
    not even installed.

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -298,8 +298,11 @@ TemplateFile::loadDefault()
     }
 
     /* giving up if failed */
-    if (!loaded())
-        fatal("failed to find or read error text file.");
+    if (!loaded()) {
+        debugs(1, DBG_CRITICAL, "WARNING: failed to find or read error text file " << templateName);
+        parse("Internal Error: Missing Template ", 33, '\0');
+        parse(templateName.termedBuf(), templateName.size(), '\0');
+    }
 
     return true;
 }
