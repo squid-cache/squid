510dc9b309a03a61179fd0b04b905315a01a8ea8
Alex Rousskov <rousskov@measurement-factory.com>
2012-02-09 17:01:17 -0700

    Do not swap out swapped out objects.
    I noticed that sometimes Squid would start swapping out an entry that was
    recently loaded from disk and was still on disk. That wastes disk
    resources (at best).
    
    The old StoreEntry::mayStartSwapOut() code assumed that when swap_status is
    not SWAPOUT_NONE it is SWAPOUT_WRITING, but SWAPOUT_WRITING is impossible
    after recent StoreEntry::swapOut() modifications because mayStartSwapOut() is
    only called when we are not swappingOut() already. SWAPOUT_DONE is possible.

--- src/store_swapout.cc
+++ src/store_swapout.cc
@@ -364,6 +364,7 @@ StoreEntry::mayStartSwapOut()
 
     // must be checked in the caller
     assert(!EBIT_TEST(flags, ENTRY_ABORTED));
+    assert(!swappingOut());
 
     if (!Config.cacheSwap.n_configured)
         return false;
@@ -383,11 +384,11 @@ StoreEntry::mayStartSwapOut()
         return true;
     }
 
-    // if we are swapping out already, do not repeat same checks
-    if (swap_status != SWAPOUT_NONE) {
-        debugs(20, 3,  HERE << " already started");
-        decision = MemObject::SwapOut::swPossible;
-        return true;
+    // if we swapped out already, do not start over
+    if (swap_status == SWAPOUT_DONE) {
+        debugs(20, 3,  HERE << "already did");
+        decision = MemObject::SwapOut::swImpossible;
+        return false;
     }
 
     if (!checkCachable()) {
