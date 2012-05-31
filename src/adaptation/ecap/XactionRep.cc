9ab6273f320870284e1fda773f86a3b73ca15dfb
Automatic source maintenance <squidadm@squid-cache.org>
2012-05-30 18:20:44 -0600

    Bootstrapped

--- src/adaptation/ecap/XactionRep.cc
+++ src/adaptation/ecap/XactionRep.cc
@@ -145,7 +145,7 @@ Adaptation::Ecap::XactionRep::usernameValue() const
         if (char const *name = request->auth_user_request->username())
             return libecap::Area::FromTempBuffer(name, strlen(name));
         else if (request->extacl_user.defined() && request->extacl_user.size())
-            return libecap::Area::FromTempBuffer(request->extacl_user.rawBuf(), 
+            return libecap::Area::FromTempBuffer(request->extacl_user.rawBuf(),
                                                  request->extacl_user.size());
     }
 #endif
--- src/adaptation/icap/ModXact.cc
+++ src/adaptation/icap/ModXact.cc
@@ -1345,11 +1345,11 @@ void Adaptation::Icap::ModXact::makeRequestHeaders(MemBuf &buf)
         String vh=virgin.header->header.getByName("Proxy-Authorization");
         buf.Printf("Proxy-Authorization: " SQUIDSTRINGPH "\r\n", SQUIDSTRINGPRINT(vh));
     } else if (request->extacl_user.defined() && request->extacl_user.size() && request->extacl_passwd.defined() && request->extacl_passwd.size()) {
-      char loginbuf[256];
-      snprintf(loginbuf, sizeof(loginbuf), SQUIDSTRINGPH ":" SQUIDSTRINGPH,
-              SQUIDSTRINGPRINT(request->extacl_user),
-              SQUIDSTRINGPRINT(request->extacl_passwd));
-      buf.Printf("Proxy-Authorization: Basic %s\r\n", old_base64_encode(loginbuf));
+        char loginbuf[256];
+        snprintf(loginbuf, sizeof(loginbuf), SQUIDSTRINGPH ":" SQUIDSTRINGPH,
+                 SQUIDSTRINGPRINT(request->extacl_user),
+                 SQUIDSTRINGPRINT(request->extacl_passwd));
+        buf.Printf("Proxy-Authorization: Basic %s\r\n", old_base64_encode(loginbuf));
     }
 
     // share the cross-transactional database records if needed
