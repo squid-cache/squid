be41d9b52e9cddd005ce35dd39e15596469d3fea
Automatic source maintenance <squidadm@squid-cache.org>
2012-05-30 19:09:59 -0600

    SourceFormat Enforcement

--- src/HttpHeader.cc
+++ src/HttpHeader.cc
@@ -711,14 +711,14 @@ HttpHeader::packInto(Packer * p, bool mask_sensitive_info) const
             continue;
         }
         switch (e->id) {
-            case HDR_AUTHORIZATION:
-            case HDR_PROXY_AUTHORIZATION:
-                packerAppend(p, e->name.rawBuf(), e->name.size());
-                packerAppend(p, ": ** NOT DISPLAYED **\r\n", 23);
-                break;
-            default:
-                e->packInto(p);
-                break;
+        case HDR_AUTHORIZATION:
+        case HDR_PROXY_AUTHORIZATION:
+            packerAppend(p, e->name.rawBuf(), e->name.size());
+            packerAppend(p, ": ** NOT DISPLAYED **\r\n", 23);
+            break;
+        default:
+            e->packInto(p);
+            break;
         }
     }
     /* Pack in the "special" entries */
