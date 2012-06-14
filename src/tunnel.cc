6df4ecaeef5494b2e325c3ecc7483ac98f158bbc
Christos Tsantilas <chtsanti@users.sourceforge.net>
2012-06-15 00:48:10 +0300

    Non-bumping broken for intercept
    When an intercepted SSL connection matches "ssl_bump none" in
    squid.conf, Squid correctly refuses to bump it and establishes a TCP
    tunnel using a fake CONNECT request. Unfortunately, the HTTP client
    terminates with an "unknown protocol" SSL error.
    
    Also the client_dst_passthru does not work as expected for intercepted requests

--- src/tunnel.cc
+++ src/tunnel.cc
@@ -523,7 +523,7 @@ tunnelConnected(const Comm::ConnectionPointer &server, void *data)
     TunnelStateData *tunnelState = (TunnelStateData *)data;
     debugs(26, 3, HERE << server << ", tunnelState=" << tunnelState);
 
-    if (tunnelState->request && tunnelState->request->flags.spoof_client_ip)
+    if (tunnelState->request && (tunnelState->request->flags.spoof_client_ip || tunnelState->request->flags.intercepted))
         tunnelStartShoveling(tunnelState); // ssl-bumped connection, be quiet
     else {
         AsyncCall::Pointer call = commCbCall(5,5, "tunnelConnectedWriteDone",
