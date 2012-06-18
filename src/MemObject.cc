fdbcae3b1e54da76a299341cce9e492d6163c084
Alex Rousskov <rousskov@measurement-factory.com>
2012-06-18 17:13:05 -0600

    Account for Store disk client quota when bandwidth-limiting the server.
    It is not clear why the store client type matters when
    MemObject::mostBytesAllowed() is trying to find the maximum delay pool
    quota for reading from the next hop HTTP server.  Whether the client(s)
    are reading from disk or RAM, the corresponding server-side bandwidth
    ought to be limited.
    
    This code was removed as a part of bug 3462 investigation, but it is not
    needed to fix bug 3462.

--- src/MemObject.cc
+++ src/MemObject.cc
@@ -477,10 +477,6 @@ MemObject::mostBytesAllowed() const
 
 #endif
 
-        if (sc->getType() != STORE_MEM_CLIENT)
-            /* reading off disk */
-            continue;
-
         j = sc->delayId.bytesWanted(0, sc->copyInto.length);
 
         if (j > jmax) {
