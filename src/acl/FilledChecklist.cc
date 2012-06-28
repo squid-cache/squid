d5cbce971f0761a3fc59074b5b1683c595b5caa9
Alex Rousskov <rousskov@measurement-factory.com>
2012-06-28 12:26:44 -0600

    Removed FilledChecklist::checkCallback() as harmful and not needed.
    The method was resetting the authentication state (auth_user_request) of a
    connection just before notifying the caller of the async ACL check result. The
    reset restarts authentication sequence, creating a 407 "loop" for auth schemes
    that require more than one step (e.g., NTLM).
    
    The method is no longer needed because we do not need to explicitly "unlock"
    auth_user_request anymore. It is refcounted now.
    
    There are other cases where connection authentication state () is reset from
    within the ACL code. Some of those cases are not needed and some might even
    cause similar bugs, but I did not risk misclassifying them in this commit.

--- src/acl/FilledChecklist.cc
+++ src/acl/FilledChecklist.cc
@@ -12,31 +12,6 @@
 
 CBDATA_CLASS_INIT(ACLFilledChecklist);
 
-void
-ACLFilledChecklist::checkCallback(allow_t answer)
-{
-    debugs(28, 5, HERE << this << " answer=" << answer);
-
-#if USE_AUTH
-    /* During reconfigure, we can end up not finishing call
-     * sequences into the auth code */
-
-    if (auth_user_request != NULL) {
-        /* the filled_checklist lock */
-        auth_user_request = NULL;
-        // It might have been connection based
-        // In the case of sslBump we need to preserve authentication info
-        // XXX: need to re-evaluate this. ACL tests should not be playing with
-        // XXX: wider scoped TCP connection state, even if the helper lookup is stuck.
-        if (conn() && !conn()->switchedToHttps()) {
-            conn()->auth_user_request = NULL;
-        }
-    }
-#endif
-
-    ACLChecklist::checkCallback(answer); // may delete us
-}
-
 
 void *
 ACLFilledChecklist::operator new (size_t size)
