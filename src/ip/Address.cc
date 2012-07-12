54fd48faec817389a1908dadca650ccd046286d6
Francesco Chemolli <kinkie@squid-cache.org>
2012-07-12 16:10:53 +0200

    Changed increment/decrement operators from postfix to prefix form.

--- src/ip/Address.cc
+++ src/ip/Address.cc
@@ -58,7 +58,7 @@
 	if(!(b)){	printf("assert \"%s\" at line %d\n", a, __LINE__); \
 		printf("Ip::Address invalid? with IsIPv4()=%c, IsIPv6()=%c\n",(IsIPv4()?'T':'F'),(IsIPv6()?'T':'F')); \
 		printf("ADDRESS:"); \
-		for(unsigned int i = 0; i < sizeof(m_SocketAddr.sin6_addr); i++) { \
+		for(unsigned int i = 0; i < sizeof(m_SocketAddr.sin6_addr); ++i) { \
 			printf(" %x", m_SocketAddr.sin6_addr.s6_addr[i]); \
 		} printf("\n"); assert(b); \
 	}
