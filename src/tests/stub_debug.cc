dd274aad92a645a8839ab0be97d72dd0e6c266ec
Francesco Chemolli <kinkie@squid-cache.org>
2012-07-25 14:31:05 +0200

    Remove unnecessary todo marker in stub_debug.cc

--- src/tests/stub_debug.cc
+++ src/tests/stub_debug.cc
@@ -73,9 +73,6 @@ _db_print(const char *format,...)
 static void
 _db_print_stderr(const char *format, va_list args)
 {
-    /* FIXME? */
-    // if (opt_debug_stderr < Debug::level)
-
     if (1 < Debug::level)
         return;
 
