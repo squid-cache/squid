5fe1f1c4311d7511d66d1168f7d105aa436fc635
Automatic source maintenance <squidadm@squid-cache.org>
2012-08-01 18:12:45 -0600

    SourceFormat Enforcement

--- src/tests/stub_libsslsquid.cc
+++ src/tests/stub_libsslsquid.cc
@@ -31,7 +31,7 @@ void Ssl::LocalContextStorage::remove(char const * host_name) STUB
 //Ssl::GlobalContextStorage::~GlobalContextStorage() STUB
 void Ssl::GlobalContextStorage::addLocalStorage(Ip::Address const & address, size_t size_of_store) STUB
 Ssl::LocalContextStorage & Ssl::GlobalContextStorage::getLocalStorage(Ip::Address const & address)
-    { fatal(STUB_API " required"); static Ssl::LocalContextStorage v(0); return v; }
+{ fatal(STUB_API " required"); static Ssl::LocalContextStorage v(0); return v; }
 void Ssl::GlobalContextStorage::reconfigureStart() STUB
 //Ssl::GlobalContextStorage Ssl::TheGlobalContextStorage;
 
