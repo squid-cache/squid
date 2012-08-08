d35465280a5a5857e411e61b487ed05158bef89e
Francesco Chemolli <kinkie@squid-cache.org>
2012-08-08 11:24:40 +0200

    converted stub_StatHist.cc stub_main_cc.cc stub_mem.cc stub_store_rebuild.cc to stub API

--- src/tests/stub_main_cc.cc
+++ src/tests/stub_main_cc.cc
@@ -35,15 +35,10 @@
 
 #include "squid.h"
 
-void
-shut_down(int)
-{ /* stub */ }
+#define STUB_API "stub_main_cc.cc"
+#include "tests/STUB.h"
 
-void
-reconfigure(int)
-{ /* stub */ }
-
-void
-rotate_logs(int)
-{ /* stub */ }
+void shut_down(int) STUB
+void reconfigure(int) STUB
+void rotate_logs(int) STUB
 
