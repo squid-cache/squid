6367408640ba6990c63ffc53cec10592a6cd8f30
Automatic source maintenance <squidadm@squid-cache.org>
2012-08-09 18:12:23 -0600

    SourceFormat Enforcement

--- src/tests/stub_HttpRequest.cc
+++ src/tests/stub_HttpRequest.cc
@@ -6,15 +6,15 @@
 #include "tests/STUB.h"
 
 HttpRequest::HttpRequest() : HttpMsg(hoRequest) STUB
-HttpRequest::HttpRequest(const HttpRequestMethod& method, AnyP::ProtocolType protocol, const char *aUrlpath) : HttpMsg(hoRequest) STUB
-HttpRequest::~HttpRequest() STUB
-void HttpRequest::packFirstLineInto(Packer * p, bool full_uri) const STUB
-bool HttpRequest::sanityCheckStartLine(MemBuf *buf, const size_t hdr_len, http_status *error) STUB_RETVAL(false)
-void HttpRequest::hdrCacheInit() STUB
-void HttpRequest::reset() STUB
-bool HttpRequest::expectingBody(const HttpRequestMethod& unused, int64_t&) const STUB_RETVAL(false)
-void HttpRequest::initHTTP(const HttpRequestMethod& aMethod, AnyP::ProtocolType aProtocol, const char *aUrlpath) STUB
-bool HttpRequest::parseFirstLine(const char *start, const char *end) STUB_RETVAL(false)
-HttpRequest * HttpRequest::clone() const STUB_RETVAL(NULL)
-bool HttpRequest::inheritProperties(const HttpMsg *aMsg) STUB_RETVAL(false)
-int64_t HttpRequest::getRangeOffsetLimit() STUB_RETVAL(0)
+        HttpRequest::HttpRequest(const HttpRequestMethod& method, AnyP::ProtocolType protocol, const char *aUrlpath) : HttpMsg(hoRequest) STUB
+        HttpRequest::~HttpRequest() STUB
+        void HttpRequest::packFirstLineInto(Packer * p, bool full_uri) const STUB
+        bool HttpRequest::sanityCheckStartLine(MemBuf *buf, const size_t hdr_len, http_status *error) STUB_RETVAL(false)
+        void HttpRequest::hdrCacheInit() STUB
+        void HttpRequest::reset() STUB
+        bool HttpRequest::expectingBody(const HttpRequestMethod& unused, int64_t&) const STUB_RETVAL(false)
+        void HttpRequest::initHTTP(const HttpRequestMethod& aMethod, AnyP::ProtocolType aProtocol, const char *aUrlpath) STUB
+        bool HttpRequest::parseFirstLine(const char *start, const char *end) STUB_RETVAL(false)
+        HttpRequest * HttpRequest::clone() const STUB_RETVAL(NULL)
+        bool HttpRequest::inheritProperties(const HttpMsg *aMsg) STUB_RETVAL(false)
+        int64_t HttpRequest::getRangeOffsetLimit() STUB_RETVAL(0)
