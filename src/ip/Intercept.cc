5afac208b5ef21984c2d5ebbbb7d5ba72d81dca3
Amos Jeffries <squid3@treenet.co.nz>
2012-08-16 01:11:05 -0600

    Fix ip/Intercept header ordering and documentation

--- src/ip/Intercept.cc
+++ src/ip/Intercept.cc
@@ -89,13 +89,14 @@
 #include <net/pfvar.h>
 #endif /* HAVE_NET_PFVAR_H */
 #endif /* PF_TRANSPARENT required headers */
-/* must be before including netfilter_ipv4.h */
+
+#if LINUX_NETFILTER
 #if HAVE_LIMITS_H
+/* must be before including netfilter_ipv4.h */
 #include <limits.h>
 #endif
-#if LINUX_NETFILTER
 #include <linux/netfilter_ipv4.h>
-#endif
+#endif /* LINUX_NETFILTER required headers */
 
 // single global instance for access by other components.
 Ip::Intercept Ip::Interceptor;
