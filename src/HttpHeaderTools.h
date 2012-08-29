b41e99c16f1b443266d082e72a70f99aea47650c
Francesco Chemolli <kinkie@squid-cache.org>
2012-08-29 13:37:22 +0200

    Moved httpHdrMangleList prototype to HttpHeaderTools.h

--- src/HttpHeaderTools.h
+++ src/HttpHeaderTools.h
@@ -96,6 +96,8 @@ extern int httpHeaderParseOffset(const char *start, int64_t * off);
 
 class HttpHeaderFieldInfo;
 class String;
+class HttpHeader;
+class HttpRequest;
 
 extern HttpHeaderFieldInfo *httpHeaderBuildFieldsInfo(const HttpHeaderFieldAttrs * attrs, int count);
 extern void httpHeaderDestroyFieldsInfo(HttpHeaderFieldInfo * info, int count);
@@ -107,5 +109,8 @@ extern int httpHeaderParseInt(const char *start, int *val);
 extern void httpHeaderPutStrf(HttpHeader * hdr, http_hdr_type id, const char *fmt,...) PRINTF_FORMAT_ARG3;
 
 extern const char *getStringPrefix(const char *str, const char *end);
+
+extern void httpHdrMangleList(HttpHeader *, HttpRequest *, int req_or_rep);
+
 
 #endif
