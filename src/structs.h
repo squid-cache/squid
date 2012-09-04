b743682e291731dbff060cecbd80ac7c963e354f
Francesco Chemolli <kinkie@squid-cache.org>
2012-09-04 13:13:13 +0200

    Moved YesNoNone to own header and implementation file.

--- src/structs.h
+++ src/structs.h
@@ -146,29 +146,6 @@ struct PortCfg;
 }
 class SwapDir;
 
-/// Used for boolean enabled/disabled options with complex default logic.
-/// Allows Squid to compute the right default after configuration.
-/// Checks that not-yet-defined option values are not used.
-class YesNoNone
-{
-// TODO: generalize to non-boolean option types
-public:
-    YesNoNone(): option(0) {}
-
-    /// returns true iff enabled; asserts if the option has not been configured
-    operator void *() const; // TODO: use a fancy/safer version of the operator
-
-    /// enables or disables the option;
-    void configure(bool beSet);
-
-    /// whether the option was enabled or disabled, by user or Squid
-    bool configured() const { return option != 0; }
-
-private:
-    enum { optUnspecified = -1, optDisabled = 0, optEnabled = 1 };
-    int option; ///< configured value or zero
-};
-
 struct SquidConfig2 {
     struct {
         int enable_purge;
