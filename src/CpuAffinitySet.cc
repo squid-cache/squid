0ec84481458772c7f1e9105936053576f78b9c61
Dmitry Kurochkin <dmitry.kurochkin@measurement-factory.com>
2012-09-09 04:03:38 -0600

    Make CpuAffinitySet::applied() method non-const.
    According to CPU_SET(3) and, apparently, on some systems (e.g.,
    OpenSuSE 10.3) CPU_COUNT macro expects a non-const argument.  The
    patch fixes build error on these systems.

--- src/CpuAffinitySet.cc
+++ src/CpuAffinitySet.cc
@@ -68,8 +68,11 @@ CpuAffinitySet::undo()
 }
 
 bool
-CpuAffinitySet::applied() const
+CpuAffinitySet::applied()
 {
+    // NOTE: cannot be const.
+    // According to CPU_SET(3) and, apparently, on some systems (e.g.,
+    // OpenSuSE 10.3) CPU_COUNT macro expects a non-const argument.
     return (CPU_COUNT(&theOrigCpuSet) > 0);
 }
 
