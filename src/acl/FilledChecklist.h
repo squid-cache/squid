65d0d8a37b543c869dee113d4af7c986d21ba0e9
Christos Tsantilas <chtsanti@users.sourceforge.net>
2012-09-25 16:42:54 +0300

    Polish the ACLFilledChecklist::fd related documentation

--- src/acl/FilledChecklist.h
+++ src/acl/FilledChecklist.h
@@ -27,14 +27,15 @@ public:
     ~ACLFilledChecklist();
 
 public:
+    /// The client connection manager
     ConnStateData * conn() const;
 
-    /// uses conn() if available
+    /// The client side fd. It uses conn() if available
     int fd() const;
 
     /// set either conn
     void conn(ConnStateData *);
-    /// set FD
+    /// set the client side FD
     void fd(int aDescriptor);
 
     //int authenticated();
