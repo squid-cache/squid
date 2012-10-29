45bb03812cb68af775325092731082b5485e9dce
Amos Jeffries <squid3@treenet.co.nz>
2012-10-28 20:17:18 -0600

    Bug 3189: AIO thread race on pipe() initialization

--- src/DiskIO/DiskThreads/CommIO.cc
+++ src/DiskIO/DiskThreads/CommIO.cc
@@ -41,13 +41,16 @@
 void
 CommIO::Initialise()
 {
+    if (CommIO::Initialised)
+        return;
+
     /* Initialize done pipe signal */
     int DonePipe[2];
     if (pipe(DonePipe)) {}
     DoneFD = DonePipe[1];
     DoneReadFD = DonePipe[0];
-    fd_open(DoneReadFD, FD_PIPE, "async-io completetion event: main");
-    fd_open(DoneFD, FD_PIPE, "async-io completetion event: threads");
+    fd_open(DoneReadFD, FD_PIPE, "async-io completion event: main");
+    fd_open(DoneFD, FD_PIPE, "async-io completion event: threads");
     commSetNonBlocking(DoneReadFD);
     commSetNonBlocking(DoneFD);
     Comm::SetSelect(DoneReadFD, COMM_SELECT_READ, NULLFDHandler, NULL, 0);
