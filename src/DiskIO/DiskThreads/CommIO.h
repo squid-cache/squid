412044643d14d71684cd90d9d9d02e8b7ea0fb6d
Amos Jeffries <squid3@treenet.co.nz>
2012-10-28 20:17:18 -0600

    Bug 3189: AIO thread race on pipe() initialization

--- src/DiskIO/DiskThreads/CommIO.h
+++ src/DiskIO/DiskThreads/CommIO.h
@@ -22,12 +22,13 @@ private:
     static int DoneReadFD;
 };
 
-/* Inline code. TODO: make structued approach to inlining */
+/* Inline code. TODO: make structured approach to inlining */
 void
 CommIO::NotifyIOCompleted()
 {
-    if (!Initialised)
-        Initialise();
+    if (!Initialised) {
+        fatalf("Disk Threads I/O pipes not initialized before first use.");
+    }
 
     if (!DoneSignalled) {
         DoneSignalled = true;
