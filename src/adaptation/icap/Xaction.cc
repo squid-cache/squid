aeeb50e1ba0002e13af26b923622f109d75b52fa
Automatic source maintenance <squidadm@squid-cache.org>
2012-12-01 18:13:09 -0700

    SourceFormat Enforcement

--- src/adaptation/icap/Xaction.cc
+++ src/adaptation/icap/Xaction.cc
@@ -511,12 +511,11 @@ void Adaptation::Icap::Xaction::setOutcome(const Adaptation::Icap::XactOutcome &
 void Adaptation::Icap::Xaction::swanSong()
 {
     // kids should sing first and then call the parent method.
-    if (cs)
-    {
-       debugs(93,6, HERE << id << " about to notify ConnOpener!");
-       CallJobHere(93, 3, cs, Comm::ConnOpener, noteAbort);
-       cs = NULL;
-       service().noteConnectionFailed("abort");
+    if (cs) {
+        debugs(93,6, HERE << id << " about to notify ConnOpener!");
+        CallJobHere(93, 3, cs, Comm::ConnOpener, noteAbort);
+        cs = NULL;
+        service().noteConnectionFailed("abort");
     }
 
     closeConnection(); // TODO: rename because we do not always close
