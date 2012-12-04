97f37a6408cddff123c21e3c80cd119da2512181
Christos Tsantilas <chtsanti@users.sourceforge.net>
2012-12-04 14:49:01 +0200

    minor fix to build after latest merge from trunk

--- src/ssl/helper.cc
+++ src/ssl/helper.cc
@@ -181,8 +181,9 @@ void Ssl::CertValidationHelper::sslSubmit(CrtdMessage const & message, HLPCB * c
         if (squid_curtime - first_warn > 3 * 60)
             fatal("ssl_crtvd queue being overloaded for long time");
         debugs(83, DBG_IMPORTANT, "WARNING: ssl_crtvd queue overload, rejecting");
-        const char *errMsg = "BH error 45 Temporary network problem, please retry later";
-        HelperReply failReply(errMsg,strlen(errMsg));
+        HelperReply failReply;
+        failReply.result = HelperReply::BrokenHelper;
+        failReply.notes.add("message", "error 45 Temporary network problem, please retry later");
         callback(data, failReply);
         return;
     }
