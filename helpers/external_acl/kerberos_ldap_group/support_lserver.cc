96072b376333a025988af61c5cdb5429b32d4ce9
Amos Jeffries <squid3@treenet.co.nz>
2012-12-13 22:52:56 -0700

    ext_kerberos_ldap_group_acl: Fix various bugs in rev.12512
    Two issues added by patch hunk mis-alignment in rev.12512.
    and correct patch for issue 740423.
    
     Detected by Coverity Scan. Issues 751097, 740423, 751096

--- helpers/external_acl/kerberos_ldap_group/support_lserver.cc
+++ helpers/external_acl/kerberos_ldap_group/support_lserver.cc
@@ -88,6 +88,11 @@ create_ls(struct main_args *margs)
                 free_ls(lssp);
                 return (1);
             }
+            if (dp) {  /* end of domain name - twice */
+                debug((char *) "%s| %s: @ is not allowed in server name %s@%s\n",LogTime(), PROGRAM,np,dp);
+                free_ls(lssp);
+                return(1);
+            }
             *p = '\0';
             ++p;
             lssp = init_ls();
@@ -99,11 +104,6 @@ create_ls(struct main_args *margs)
                 debug((char *) "%s| %s: DEBUG: No ldap servers defined for domain %s\n", LogTime(), PROGRAM, p);
                 free_ls(lssp);
                 return (1);
-            }
-            if (dp) {  /* end of domain name - twice */
-                debug((char *) "%s| %s: @ is not allowed in server name %s@%s\n",LogTime(), PROGRAM,np,dp);
-                free_ls(lssp);
-                return(1);
             }
             *p = '\0';
             ++p;
