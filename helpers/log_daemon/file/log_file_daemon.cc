faf6157f21d4bf7236d6743ba5abe106a5c66ebf
Markus Rietzler <markus.rietzler@rzf.fin-nrw.de>
2012-12-26 19:58:57 -0700

    Bug 3712: Rotating logs overwrites the previous log

--- helpers/log_daemon/file/log_file_daemon.cc
+++ helpers/log_daemon/file/log_file_daemon.cc
@@ -61,8 +61,8 @@ rotate(const char *path, int rotate_count)
             fprintf(stderr, "WARNING: remove '%s' failure: %s\n", to, xstrerror());
         }
 #endif
-        if (rename(path, to) < 0 && errno != ENOENT) {
-            fprintf(stderr, "WARNING: rename '%s' to '%s' failure: %s\n", path, to, xstrerror());
+        if (rename(from, to) < 0 && errno != ENOENT) {
+            fprintf(stderr, "WARNING: rename '%s' to '%s' failure: %s\n", from, to, xstrerror());
         }
     }
     if (rotate_count > 0) {
