b7f9912feea44d829638a7ff7ecc84abaa54e943
Amos Jeffries <squid3@treenet.co.nz>
2013-01-03 12:40:49 +1300

    Bug 3676: Fix various shadowed variables
    Also, fixes logformat dumper to output 'logformat' instead of 'format'.
    'format' directive is the planned future name but is not yet parsed.
    
     Detected by GCC 4.7 with -Wshadow

--- src/format/Format.cc
+++ src/format/Format.cc
@@ -79,16 +79,16 @@ Format::Format::parse(const char *def)
 }
 
 void
-Format::Format::dump(StoreEntry * entry, const char *name)
+Format::Format::dump(StoreEntry * entry, const char *directiveName)
 {
     debugs(46, 4, HERE);
 
     // loop rather than recursing to conserve stack space.
-    for (Format *format = this; format; format = format->next) {
-        debugs(46, 3, HERE << "Dumping format definition for " << format->name);
-        storeAppendPrintf(entry, "format %s ", format->name);
+    for (Format *fmt = this; fmt; fmt = fmt->next) {
+        debugs(46, 3, HERE << "Dumping format definition for " << fmt->name);
+        storeAppendPrintf(entry, "%s %s ", directiveName, fmt->name);
 
-        for (Token *t = format->format; t; t = t->next) {
+        for (Token *t = fmt->format; t; t = t->next) {
             if (t->type == LFT_STRING)
                 storeAppendPrintf(entry, "%s", t->data.string);
             else {
