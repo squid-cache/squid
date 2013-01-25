9409b3d558a35eee40f6af95055073daefc17243
Amos Jeffries <squid3@treenet.co.nz>
2013-01-25 16:44:49 -0700

    Fix NULL-dereference added in rev.12611
    * strto*() may be called without a value parameter when used simply to
      verify the content of a string is a valid conversion.
    
      Detected by Coverity Scan. Issue 970939.

--- compat/xstrto.cc
+++ compat/xstrto.cc
@@ -85,11 +85,12 @@ xstrtoui(const char *s, char **end, unsigned int *value,
     bool ret;
 
     ret = xstrtoul(s, end, &v, min, max);
-    if (value != NULL)
+    if (value != NULL) {
         *value = v;
 
-    if (v != static_cast<unsigned long>(*value)) {
-        return false;
+        if (v != static_cast<unsigned long>(*value)) {
+            return false;
+        }
     }
 
     return ret;
