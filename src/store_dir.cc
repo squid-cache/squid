91eb5a99829ca804efb522137494c5cbf8c7b093
Tianyin Xu <tixu@cs.ucsd.edu>
2013-01-27 21:33:00 -0700

    Bug 3728: Improve debug for cache_dir

--- src/store_dir.cc
+++ src/store_dir.cc
@@ -907,6 +907,10 @@ StoreHashIndex::callback()
 void
 StoreHashIndex::create()
 {
+    if (Config.cacheSwap.n_configured == 0) {
+        debugs(0, DBG_PARSE_NOTE(DBG_CRITICAL), "No cache_dir stores are configured.");
+    }
+
     for (int i = 0; i < Config.cacheSwap.n_configured; ++i) {
         if (dir(i).active())
             store(i)->create();
