864ee44cb5d7980bfbfc20fbdd0ba81550f85c92
Tianyin Xu <tixu@cs.ucsd.edu>
2013-01-28 04:40:12 -0700

    Bug 3736: Floating point exception due to divide by zero

--- src/store_dir.cc
+++ src/store_dir.cc
@@ -938,6 +938,12 @@ StoreHashIndex::get(String const key, STOREGETCLIENT aCallback, void *aCallbackD
 void
 StoreHashIndex::init()
 {
+    if (Config.Store.objectsPerBucket <= 0)
+        fatal("'store_objects_per_bucket' should be larger than 0.");
+
+    if (Config.Store.avgObjectSize <= 0)
+        fatal("'store_avg_object_size' should be larger than 0.");
+
     /* Calculate size of hash table (maximum currently 64k buckets).  */
     /* this is very bogus, its specific to the any Store maintaining an
      * in-core index, not global */
