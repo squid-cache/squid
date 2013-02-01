ce99a63e612da37e7c4d2d26ad7c5132088a4060
Amos Jeffries <squid3@treenet.co.nz>
2013-01-31 21:46:16 -0700

    Fix ipv6 enabled pinger.
    Was failing with assertion failure due to ipv6 support not being probed
    automatically.

--- src/icmp/pinger.cc
+++ src/icmp/pinger.cc
@@ -70,6 +70,7 @@
 #include "Icmp4.h"
 #include "Icmp6.h"
 #include "IcmpPinger.h"
+#include "ip/tools.h"
 
 #if _SQUID_MSWIN_
 
@@ -146,6 +147,9 @@ main(int argc, char *argv[])
         debug_args = xstrdup(t);
 
     getCurrentTime();
+
+    // determine IPv4 or IPv6 capabilities before using sockets.
+    Ip::ProbeTransport();
 
     _db_init(NULL, debug_args);
 
