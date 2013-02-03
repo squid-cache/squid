d724f01ad4c73a59f505b43d80ef7b2fcbca6a00
Amos Jeffries <squid3@treenet.co.nz>
2013-02-03 07:05:11 -0700

    Bug 3515: crash in FtpStateData::ftpTimeout
    Since revision squid-3.2-11174 restructured teh TCO connection handling
    in FTP the data connection has had separate 'opener' connection.
    Meaning the conn->fd state is always NULL when the setup times out.
    
    This cleans up the data connection opener state and allows the error
    message generation to take place.

--- src/ftp.cc
+++ src/ftp.cc
@@ -681,10 +681,17 @@ FtpStateData::ftpTimeout(const CommTimeoutCbParams &io)
     if (abortOnBadEntry("entry went bad while waiting for a timeout"))
         return;
 
-    if (SENT_PASV == state && io.conn->fd == data.conn->fd) {
-        /* stupid ftp.netscape.com */
+    if (SENT_PASV == state) {
+        /* stupid ftp.netscape.com, of FTP server behind stupid firewall rules */
         flags.pasv_supported = false;
         debugs(9, DBG_IMPORTANT, "ftpTimeout: timeout in SENT_PASV state" );
+
+        // cancel the data connection setup.
+        if (data.opener != NULL) {
+            data.opener->cancel("timeout");
+            data.opener = NULL;
+        }
+        data.close();
     }
 
     failed(ERR_READ_TIMEOUT, 0);
