9d205e0e8c59cdf2eda379baddc810b2e98f5d4d
Amos Jeffries <squid3@treenet.co.nz>
2013-02-09 00:04:42 -0700

    Polish debugs on more obsolete options

--- src/SwapDir.cc
+++ src/SwapDir.cc
@@ -288,6 +288,10 @@ SwapDir::optionReadOnlyParse(char const *option, const char *value, int isaRecon
     if (strcmp(option, "no-store") != 0 && strcmp(option, "read-only") != 0)
         return false;
 
+    if (strcmp(option, "read-only") == 0) {
+        debugs(3, DBG_PARSE_NOTE(3), "UPGRADE WARNING: Replace cache_dir option 'read-only' with 'no-store'.");
+    }
+
     int read_only = 0;
 
     if (value)
