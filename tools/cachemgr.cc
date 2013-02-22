b9674d65f843dd2e1fb61b547f4c267623c03bf2
Reinhard Sojka <reinhard.sojka@parlament.gv.at>
2013-02-22 04:13:25 -0700

    Bug 3790: cachemgr.cgi crash with authentication

--- tools/cachemgr.cc
+++ tools/cachemgr.cc
@@ -1162,7 +1162,6 @@ make_auth_header(const cachemgr_request * req)
 {
     static char buf[1024];
     size_t stringLength = 0;
-    const char *str64;
 
     if (!req->passwd)
         return "";
@@ -1171,15 +1170,12 @@ make_auth_header(const cachemgr_request * req)
              req->user_name ? req->user_name : "",
              req->passwd);
 
-    str64 = base64_encode(buf);
-
-    stringLength += snprintf(buf, sizeof(buf), "Authorization: Basic %s\r\n", str64);
+    stringLength += snprintf(buf, sizeof(buf), "Authorization: Basic %s\r\n", base64_encode(buf));
 
     assert(stringLength < sizeof(buf));
 
-    snprintf(&buf[stringLength], sizeof(buf) - stringLength, "Proxy-Authorization: Basic %s\r\n", str64);
+    snprintf(&buf[stringLength], sizeof(buf) - stringLength, "Proxy-Authorization: Basic %s\r\n", base64_encode(buf));
 
-    xxfree(str64);
     return buf;
 }
 
