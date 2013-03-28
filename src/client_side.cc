86e04dd564ee58dc3a5d72da99489518b5497ca3
Amos Jeffries <squid3@treenet.co.nz>
2013-03-27 21:16:05 -0600

    Fix memory leaks in ConnStateData pinning
    ConnStateData does not cleanup any of the pinning child structure fields.
    This results in a leak of pinned host and peer details.
    
    Run the full un-pin cleanup operation from ConnStateData::swanSong in
    order to clean up all the pinning state instead of just the release the
    Comm::Connection.

--- src/client_side.cc
+++ src/client_side.cc
@@ -809,9 +809,7 @@ ConnStateData::swanSong()
     }
 #endif
 
-    if (Comm::IsConnOpen(pinning.serverConnection))
-        pinning.serverConnection->close();
-    pinning.serverConnection = NULL;
+    unpinConnection();
 
     if (Comm::IsConnOpen(clientConnection))
         clientConnection->close();
