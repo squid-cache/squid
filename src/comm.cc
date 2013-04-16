b219204255c110294c25fbf4031fb1892e4d0ba1
Amos Jeffries <squid3@treenet.co.nz>
2013-04-15 20:21:47 -0600

    Polish TPROXY support for OpenBSD and FreeBSD
    Current OpenBSD implementation of PF divert-to works similarly to TPROXY
    and only requires a getsockname() lookup to locate the TCP packet
    original destination.
    
    We can use the same PF configuration to preform "intercept" option but
    the old PF transparent code does lookups on /dev/pf which fails badly
    on the new PF versions. getsockname() is what is really required and
    already performed by TcpAcceptor on all incoming connections, so there
    is no need for a special PF lookup code now.
    
    Add a new ./configure option --with-nat-devpf to enable the old /dev/pf
    NAT lookup code in a backward-compatible way for older OS versions and 
    OpenBSD based distros which have not yet ported the new PF code. The
    option is disabled by default since the systems requiring it are fairly
    old now.
    
    Also remove the getsockname() lookup in the IPFW lookup implementation
    which is redundant behind TcpAcceptor.

--- src/comm.cc
+++ src/comm.cc
@@ -495,35 +495,46 @@ comm_set_v6only(int fd, int tos)
 }
 
 /**
- * Set the socket IP_TRANSPARENT option for Linux TPROXY v4 support,
- * or set the socket SO_BINDANY option for BSD divert-to support.
+ * Set the socket option required for TPROXY spoofing for:
+ * - Linux TPROXY v4 support,
+ * - OpenBSD divert-to support,
+ * - FreeBSD IPFW TPROXY v4 support.
  */
 void
 comm_set_transparent(int fd)
 {
-#if _SQUID_LINUX_ && defined(IP_TRANSPARENT)
-    int tos = 1;
-    if (setsockopt(fd, SOL_IP, IP_TRANSPARENT, (char *) &tos, sizeof(int)) < 0) {
-        debugs(50, DBG_IMPORTANT, "comm_open: setsockopt(IP_TRANSPARENT) on FD " << fd << ": " << xstrerror());
-    } else {
-        /* mark the socket as having transparent options */
-        fd_table[fd].flags.transparent = true;
-    }
+    bool doneSuid = false;
+#if _SQUID_LINUX_ && defined(IP_TRANSPARENT) // Linux
+# define soLevel SOL_IP
+# define soFlag  IP_TRANSPARENT
 
-#elif defined(SO_BINDANY)
-    int tos = 1;
+#elif defined(SO_BINDANY) // OpenBSD 4.7+ and NetBSD with PF
+# define soLevel SOL_SOCKET
+# define soFlag  SO_BINDANY
     enter_suid();
-    if (setsockopt(fd, SOL_SOCKET, SO_BINDANY, (char *) &tos, sizeof(int)) < 0) {
-        debugs(50, DBG_IMPORTANT, "comm_open: setsockopt(SO_BINDANY) on FD " << fd << ": " << xstrerror());
-    } else {
-        /* mark the socket as having transparent options */
-        fd_table[fd].flags.transparent = true;
-    }
-    leave_suid();
+    doneSuid = true;
+
+#elif defined(IP_BINDANY) // FreeBSD with IPFW
+# define soLevel IPPROTO_IP
+# define soFlag  IP_BINDANY
+    enter_suid();
+    doneSuid = true;
 
 #else
-    debugs(50, DBG_CRITICAL, "WARNING: comm_open: setsockopt(IP_TRANSPARENT) not supported on this platform");
+    debugs(50, DBG_CRITICAL, "WARNING: comm_open: setsockopt(TPROXY) not supported on this platform");
 #endif /* sockopt */
+
+#if defined(soLevel) && defined(soFlag)
+    int tos = 1;
+    if (setsockopt(fd, soLevel, soFlag, (char *) &tos, sizeof(int)) < 0) {
+        debugs(50, DBG_IMPORTANT, "comm_open: setsockopt(TPROXY) on FD " << fd << ": " << xstrerror());
+    } else {
+        /* mark the socket as having transparent options */
+        fd_table[fd].flags.transparent = true;
+    }
+    if (doneSuid)
+        leave_suid();
+#endif
 }
 
 /**
