659861c5e81956d541a40604b640102770fa9a46
Amos Jeffries <squid3@treenet.co.nz>
2013-04-22 00:22:27 -0600

    Docs: Polish configuration file documentation
    * Mostly adding DEFAULT_DOC directive to hide strange default values.
      This wll help us move to different internal values for no-limit etc
      at some point in the future.
    
    * Migrates some access controls to "DEFAULT: none" instead of "deny all".
     This reduces run-time CPU cycles running useless ACL tests.
     NP: some access controls have been left unchanged due to complexity in
         the code testing them (ie icap_access).
    
    * added documentation for several directives which were missing it
    
    * corrected buffered_logs documentation (text by Alex Rousskov)
    
    * Updated cf_gen tool to produce more descriptive error messages.

--- src/client_side.cc
+++ src/client_side.cc
@@ -2619,12 +2619,13 @@ bool ConnStateData::serveDelayedError(ClientSocketContext *context)
             debugs(33, 2, "SQUID_X509_V_ERR_DOMAIN_MISMATCH: Certificate " <<
                    "does not match domainname " << request->GetHost());
 
-            ACLFilledChecklist check(Config.ssl_client.cert_error, request, dash_str);
+            if (Config.ssl_client.cert_error) {
+                ACLFilledChecklist check(Config.ssl_client.cert_error, request, dash_str);
+                const bool allowDomainMismatch = (check.fastCheck() == ACCESS_ALLOWED);
+                delete check.sslErrors;
+                check.sslErrors = NULL;
+            }
             check.sslErrors = new Ssl::Errors(SQUID_X509_V_ERR_DOMAIN_MISMATCH);
-            const bool allowDomainMismatch =
-                check.fastCheck() == ACCESS_ALLOWED;
-            delete check.sslErrors;
-            check.sslErrors = NULL;
 
             if (!allowDomainMismatch) {
                 quitAfterError(request);
