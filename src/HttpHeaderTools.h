a3e093c172854e76aecf42de0938cf02b230fefa
Automatic source maintenance <squidadm@squid-cache.org>
2013-05-16 18:13:27 -0600

    SourceFormat Enforcement

--- src/HttpHeaderTools.h
+++ src/HttpHeaderTools.h
@@ -64,7 +64,8 @@ public:
 private:
     /// Case-insensitive std::string "less than" comparison functor.
     /// Fast version recommended by Meyers' "Effective STL" for ASCII c-strings.
-    class NoCaseLessThan: public std::binary_function<std::string, std::string, bool> {
+    class NoCaseLessThan: public std::binary_function<std::string, std::string, bool>
+    {
     public:
         bool operator()(const std::string &lhs, const std::string &rhs) const {
             return strcasecmp(lhs.c_str(), rhs.c_str()) < 0;
