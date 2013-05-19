125063d744e442558bef9c038afd02b65c64a447
Alex Rousskov <rousskov@measurement-factory.com>
2013-05-18 21:44:04 -0600

    Fixed leaking configurable SSL error details.
    Trunk r11496 "Configurable SSL error details messages" correctly disabled
    collection of HTTP statistics for non-HTTP header fields, such as configurable
    SSL error details. However, it also incorrectly disabled deletion of those
    non-HTTP header fields.
    
    Configurable SSL error details are only created during [re]configuration time,
    so the leak went unnoticed since 2011-06-17, but the same bug caused a major
    runtime annotation leak later (r12413) until the new annotation code was
    redesigned to avoid using HttpHeader (r12779).

--- src/HttpHeader.cc
+++ src/HttpHeader.cc
@@ -433,37 +433,37 @@ HttpHeader::clean()
 
     PROF_start(HttpHeaderClean);
 
-    /*
-     * An unfortunate bug.  The entries array is initialized
-     * such that count is set to zero.  httpHeaderClean() seems to
-     * be called both when 'hdr' is created, and destroyed.  Thus,
-     * we accumulate a large number of zero counts for 'hdr' before
-     * it is ever used.  Can't think of a good way to fix it, except
-     * adding a state variable that indicates whether or not 'hdr'
-     * has been used.  As a hack, just never count zero-sized header
-     * arrays.
-     */
-
     if (owner <= hoReply) {
+        /*
+         * An unfortunate bug.  The entries array is initialized
+         * such that count is set to zero.  httpHeaderClean() seems to
+         * be called both when 'hdr' is created, and destroyed.  Thus,
+         * we accumulate a large number of zero counts for 'hdr' before
+         * it is ever used.  Can't think of a good way to fix it, except
+         * adding a state variable that indicates whether or not 'hdr'
+         * has been used.  As a hack, just never count zero-sized header
+         * arrays.
+         */
         if (0 != entries.count)
             HttpHeaderStats[owner].hdrUCountDistr.count(entries.count);
 
         ++ HttpHeaderStats[owner].destroyedCount;
 
         HttpHeaderStats[owner].busyDestroyedCount += entries.count > 0;
-
-        while ((e = getEntry(&pos))) {
-            /* tmp hack to try to avoid coredumps */
-
-            if (e->id < 0 || e->id >= HDR_ENUM_END) {
-                debugs(55, 0, "HttpHeader::clean BUG: entry[" << pos << "] is invalid (" << e->id << "). Ignored.");
-            } else {
-                HttpHeaderStats[owner].fieldTypeDistr.count(e->id);
-                /* yes, this deletion leaves us in an inconsistent state */
-                delete e;
-            }
-        }
     } // if (owner <= hoReply)
+
+    while ((e = getEntry(&pos))) {
+        /* tmp hack to try to avoid coredumps */
+
+        if (e->id < 0 || e->id >= HDR_ENUM_END) {
+            debugs(55, DBG_CRITICAL, "HttpHeader::clean BUG: entry[" << pos << "] is invalid (" << e->id << "). Ignored.");
+        } else {
+            if (owner <= hoReply)
+                HttpHeaderStats[owner].fieldTypeDistr.count(e->id);
+            /* yes, this deletion leaves us in an inconsistent state */
+            delete e;
+        }
+    }
     entries.clean();
     httpHeaderMaskInit(&mask, 0);
     len = 0;
