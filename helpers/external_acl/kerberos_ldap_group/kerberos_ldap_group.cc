bb178d353957a0e750ec5cd0e881e345344e20bd
Amos Jeffries <squid3@treenet.co.nz>
2013-05-20 00:02:24 -0600

    Revert rev.11818 - not applicable to 3.2.

--- helpers/external_acl/kerberos_ldap_group/kerberos_ldap_group.cc
+++ helpers/external_acl/kerberos_ldap_group/kerberos_ldap_group.cc
@@ -404,7 +404,6 @@ main(int argc, char *const argv[])
         }
         if (!domain && margs.ddomain) {
             domain = xstrdup(margs.ddomain);
-            dp = xstrdup(rfc1738_escape(domain));
             if (debug_enabled)
                 debug((char *) "%s| %s: INFO: Got User: %s set default domain: %s\n", LogTime(), PROGRAM, user, domain);
             else
