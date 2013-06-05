155bf685c2d4a45079a7906f395ead0d1c0d609b
Automatic source maintenance <squidadm@squid-cache.org>
2013-06-04 18:13:22 -0600

    SourceFormat Enforcement

--- src/main.cc
+++ src/main.cc
@@ -962,7 +962,7 @@ mainSetCwd(void)
         chrooted = true;
 
         if (chroot(Config.chroot_dir) != 0);
-            fatal("failed to chroot");
+        fatal("failed to chroot");
 
         strncpy(pathbuf, "/", sizeof(pathbuf)-1);
     }
