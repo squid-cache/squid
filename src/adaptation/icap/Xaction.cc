3535a5d5fb244c1524c2e72c3795739c0cb1d507
Christos Tsantilas <chtsanti@users.sourceforge.net>
2013-06-11 08:52:01 -0600

    Support HTTP reply ACLs in icap_log and log_icap
    The HTTP reply related acls does not check for icap_log

--- src/adaptation/icap/Xaction.cc
+++ src/adaptation/icap/Xaction.cc
@@ -549,7 +549,11 @@ void Adaptation::Icap::Xaction::tellQueryAborted()
 void Adaptation::Icap::Xaction::maybeLog()
 {
     if (IcapLogfileStatus == LOG_ENABLE) {
-        ACLChecklist *checklist = new ACLFilledChecklist(::Config.accessList.icap, al.request, dash_str);
+        ACLFilledChecklist *checklist = new ACLFilledChecklist(::Config.accessList.icap, al.request, dash_str);
+        if (al.reply) {
+            checklist->reply = al.reply;
+            HTTPMSGLOCK(checklist->reply);
+        }
         if (!::Config.accessList.icap || checklist->fastCheck() == ACCESS_ALLOWED) {
             finalizeLogInfo();
             icapLogLog(alep, checklist);
