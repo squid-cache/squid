f54986adf0906890917175e2558ec1d37e176199
Alex Rousskov <rousskov@measurement-factory.com>
2013-06-27 15:09:02 -0600

    Improved debugging.

--- src/CollapsedForwarding.cc
+++ src/CollapsedForwarding.cc
@@ -53,7 +53,7 @@ CollapsedForwarding::Broadcast(const StoreEntry &e)
 
     if (!e.mem_obj || e.mem_obj->xitTable.index < 0 ||
         !Store::Root().transientReaders(e)) {
-        debugs(17, 7, "not broadcasting " << e << " changes to nobody");
+        debugs(17, 7, "nobody reads " << e);
         return;
     }
 
