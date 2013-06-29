a01fe97eee577a1df88766af6e624dfd88756c24
Alexis Robert <alexis.robert@gmail.com>
2013-06-29 02:23:49 -0600

    Fix Ip::Address::operator =(sockaddr_storage)
    The memcpy() for AF_INET6 is using a length of sizeof(sockaddr_in) instead
    of sizeof(sockaddr_in6), so squid was trying to connect to truncatured IPv6
    addresses with strange ports.

--- src/ip/Address.cc
+++ src/ip/Address.cc
@@ -451,7 +451,7 @@ Ip::Address::operator =(const struct sockaddr_storage &s)
 {
     /* some AF_* magic to tell socket types apart and what we need to do */
     if (s.ss_family == AF_INET6) {
-        memcpy(&m_SocketAddr, &s, sizeof(struct sockaddr_in));
+        memcpy(&m_SocketAddr, &s, sizeof(struct sockaddr_in6));
     } else { // convert it to our storage mapping.
         struct sockaddr_in *sin = (struct sockaddr_in*)&s;
         m_SocketAddr.sin6_port = sin->sin_port;
