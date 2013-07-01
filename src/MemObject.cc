a5e14a6f2e4472e819c03c47899493fd85b3ecfa
Alex Rousskov <rousskov@measurement-factory.com>
2013-06-30 20:26:35 -0600

    Add mem-cache and transient store entry details in mgr:objects output.

--- src/MemObject.cc
+++ src/MemObject.cc
@@ -259,6 +259,17 @@ MemObject::stat(MemBuf * mb) const
         mb->Printf("\tswapout: %" PRId64 " bytes written\n",
                    (int64_t) swapout.sio->offset());
 
+    if (xitTable.index >= 0)
+        mb->Printf("\ttransient index: %d state: %d\n",
+                   xitTable.index, xitTable.io);
+    if (memCache.index >= 0)
+        mb->Printf("\tmem-cache index: %d state: %d offset: %" PRId64 "\n",
+                   memCache.index, memCache.io, memCache.offset);
+    if (object_sz >= 0)
+        mb->Printf("\tobject_sz: %" PRId64 "\n", object_sz);
+    if (smpCollapsed)
+        mb->Printf("\tsmp-collapsed\n");
+
     StoreClientStats statsVisitor(mb);
 
     for_each<StoreClientStats>(clients, statsVisitor);
