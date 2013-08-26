849c387f0cc38c27f3b0e706604b71f07c4b6879
Alex Rousskov <rousskov@measurement-factory.com>
2013-08-26 16:43:42 -0600

    Marked where HDR_FTP_ARGUMENTS for FTP gw PASS may need to be hidden.

--- src/HttpHeader.cc
+++ src/HttpHeader.cc
@@ -725,6 +725,8 @@ HttpHeader::packInto(Packer * p, bool mask_sensitive_info) const
             continue;
         }
         switch (e->id) {
+        // TODO: When FTP gw command wrappers may end up in error pages and
+        // other sensitive places, hide HDR_FTP_ARGUMENTS for FTP PASS command.
         case HDR_AUTHORIZATION:
         case HDR_PROXY_AUTHORIZATION:
             packerAppend(p, e->name.rawBuf(), e->name.size());
