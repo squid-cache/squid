1e1a902194d5b12804135b07f340a48fef9cee59
Amos Jeffries <squid3@treenet.co.nz>
2013-09-03 21:05:02 +1200

    Windows: fix collision between POSIX wrappers and DiskIO class methods
    The POSIX function shutdown() is #define'd on Windows. This causes
    compiler issues with the DiskIO method of same name. Rename the method
    to gracefulShutdown() instead.
    
    Also, Bug 3189 was incomplete and missed a change to DiskThreads
    Windows initialization method.
    
      This is an iCelero project.

--- src/DiskIO/DiskIOModule.cc
+++ src/DiskIO/DiskIOModule.cc
@@ -95,7 +95,7 @@ DiskIOModule::FreeAllModules()
     while (GetModules().size()) {
         DiskIOModule *fs = GetModules().back();
         GetModules().pop_back();
-        fs->shutdown();
+        fs->gracefulShutdown();
     }
 }
 
