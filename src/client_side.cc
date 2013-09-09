957fa03172a85db8f57e100af6c8c0d885f63aef
Christos Tsantilas <chtsanti@users.sourceforge.net>
2013-09-08 19:51:04 -0600

    Bug 3849: Duplicate certificate sent when using https_port
    The certificate file given with the "cert=" option it may contain a list of
    certificates to be chained to the SSL client, for example intermediate
    certificates.
    
    The bug caused because in the certificates  chain we are storing also the
    certificate of the port. This is works well for SSL-bump because squid
    generates a certificate which uses the port certificate as CA certificate.
    But in the case of https_port without bumping the port certificate is sent
    twice, one as SSL server certificate and one as chained certificate.
    
    This patch try to chain port certificate only when the sslbump is used.
    
    
    This is a Measurement Factory project

--- src/client_side.cc
+++ src/client_side.cc
@@ -3938,8 +3938,18 @@ ConnStateData::getSslContextDone(SSL_CTX * sslContext, bool isNew)
     // Try to add generated ssl context to storage.
     if (port->generateHostCertificates && isNew) {
 
-        if (signAlgorithm == Ssl::algSignTrusted)
+        if (signAlgorithm == Ssl::algSignTrusted) {
+            // Add signing certificate to the certificates chain
+            X509 *cert = port->signingCert.get();
+            if (SSL_CTX_add_extra_chain_cert(sslContext, cert)) {
+                // increase the certificate lock
+                CRYPTO_add(&(cert->references),1,CRYPTO_LOCK_X509);
+            } else {
+                const int ssl_error = ERR_get_error();
+                debugs(33, DBG_IMPORTANT, "WARNING: can not add signing certificate to SSL context chain: " << ERR_error_string(ssl_error, NULL));
+            }
             Ssl::addChainToSslContext(sslContext, port->certsToChain.get());
+        }
         //else it is self-signed or untrusted do not attrach any certificate
 
         Ssl::LocalContextStorage & ssl_ctx_cache(Ssl::TheGlobalContextStorage.getLocalStorage(port->s));
