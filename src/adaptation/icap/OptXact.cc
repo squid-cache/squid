2a41ad13732adead358527dd239d6ef428d9b814
Nathan Hoad <nathan@getoffmalawn.com>
2013-10-18 17:52:10 -0600

    Append "Connection: close" to OPTIONS requests when icap_persistent_connections is off.
    This brings OPTIONS requests behavior inline with REQMOD and RESPMOD.

--- src/adaptation/icap/OptXact.cc
+++ src/adaptation/icap/OptXact.cc
@@ -51,6 +51,10 @@ void Adaptation::Icap::OptXact::makeRequest(MemBuf &buf)
     buf.Printf("OPTIONS " SQUIDSTRINGPH " ICAP/1.0\r\n", SQUIDSTRINGPRINT(uri));
     const String host = s.cfg().host;
     buf.Printf("Host: " SQUIDSTRINGPH ":%d\r\n", SQUIDSTRINGPRINT(host), s.cfg().port);
+
+    if (!TheConfig.reuse_connections)
+        buf.Printf("Connection: close\r\n");
+
     if (TheConfig.allow206_enable)
         buf.Printf("Allow: 206\r\n");
     buf.append(ICAP::crlf, 2);
