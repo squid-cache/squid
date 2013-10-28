ff86fee22db9121158f19918ac62dc040ed48238
Alex Rousskov <rousskov@measurement-factory.com>
2013-10-28 15:23:33 -0600

    End "227 Entering Passive Mode (...)" response with CRLF as required by FTP.

--- src/client_side.cc
+++ src/client_side.cc
@@ -5289,7 +5289,7 @@ FtpHandlePasvReply(ClientSocketContext *context, const HttpReply *reply, StoreIO
 
     // Do not use "227 =a,b,c,d,p1,p2" format or omit parens: some nf_ct_ftp
     // versions block responses that use those alternative syntax rules!
-    mb.Printf("227 Entering Passive Mode (%s,%i,%i).\n",
+    mb.Printf("227 Entering Passive Mode (%s,%i,%i).\r\n",
               addr,
               static_cast<int>(port / 256),
               static_cast<int>(port % 256));
