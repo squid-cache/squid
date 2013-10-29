79a90a506222bd3c3d5843518f2717c2f3799e18
Amos Jeffries <squid3@treenet.co.nz>
2013-10-28 19:24:29 -0700

    Cleanup transaction message size logging
    - Add class MessageCounters to track all size properties of a
      transaction message. HTTP-centric for now.
    
    - replaced AccessLogEntry::cache.requestHeadersSize with
      AccessLogEntry::http.clientRequest.headerSz
    
    - replaced AccessLogEntry::cache.requestSize with
      AccessLogEntry::http.clientRequest (total() of all counters)
    
    - replaced AccessLogEntry::cache.replyHeadersSize with
      AccessLogEntry::http.adaptedReply.headerSz
    
    - replaced AccessLogEntry::cache.replySize with
      AccessLogEntry::http.adaptedReply (total() of all counters)
    
    - Rename the logformat byte codes for message size counters to
      form a logical set of details by message type
    
    - reorganize and document logformat option codes more clearly
    
    There are no logic changes in this patch. Several accounting
    bugs have been identified and marked for fixing later.

--- src/adaptation/icap/ModXact.cc
+++ src/adaptation/icap/ModXact.cc
@@ -1289,7 +1289,8 @@ void Adaptation::Icap::ModXact::finalizeLogInfo()
         al.cache.ssluser = h->ssluser.termedBuf();
 #endif
     al.cache.code = h->logType;
-    al.cache.requestSize = h->req_sz;
+    // XXX: should use icap-specific counters instead ?
+    al.http.clientRequest.payloadDataSz = h->req_sz;
 
     // leave al.icap.bodyBytesRead negative if no body
     if (replyHttpHeaderSize >= 0 || replyHttpBodySize >= 0) {
@@ -1302,7 +1303,9 @@ void Adaptation::Icap::ModXact::finalizeLogInfo()
         al.http.code = reply_->sline.status();
         al.http.content_type = reply_->content_type.termedBuf();
         if (replyHttpBodySize >= 0) {
-            al.cache.replySize = replyHttpBodySize + reply_->hdr_sz;
+            // XXX: should use icap-specific counters instead ?
+            al.http.adaptedReply.payloadDataSz = replyHttpBodySize;
+            al.http.adaptedReply.headerSz =  reply_->hdr_sz;
             al.cache.highOffset = replyHttpBodySize;
         }
         //don't set al.cache.objectSize because it hasn't exist yet
