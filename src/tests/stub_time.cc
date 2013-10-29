2e4e915c1369978ee9d0527913501a3bc481885e
Amos Jeffries <squid3@treenet.co.nz>
2013-10-29 02:27:45 -0700

    Fix stub_time definitions

--- src/tests/stub_time.cc
+++ src/tests/stub_time.cc
@@ -8,7 +8,7 @@ struct timeval current_time;
 double current_dtime;
 time_t squid_curtime = 0;
 
-time_t getCurrentTime(void) STUB
-int tvSubMsec(struct timeval, struct timeval) STUB
-const char * Time::FormatStrf(time_t ) STUB
-const char * Time::FormatHttpd(time_t ) STUB
+time_t getCurrentTime(void) STUB__RETVAL(0)
+int tvSubMsec(struct timeval, struct timeval) STUB_RETVAL(0)
+const char * Time::FormatStrf(time_t ) STUB_RETVAL("")
+const char * Time::FormatHttpd(time_t ) STUB_RETVAL("")
