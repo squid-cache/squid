e478bceac9f3e9482ea18cf80ac6baf9e3d95dbd
Francesco Chemolli <kinkie@squid-cache.org>
2013-10-30 19:37:05 +0100

    Added some more stubs

--- src/tests/stub_time.cc
+++ src/tests/stub_time.cc
@@ -10,4 +10,5 @@ time_t squid_curtime = 0;
 
 time_t getCurrentTime(void) STUB
 int tvSubMsec(struct timeval, struct timeval) STUB
-
+const char * Time::FormatStrf(time_t ) STUB
+const char * Time::FormatHttpd(time_t ) STUB
