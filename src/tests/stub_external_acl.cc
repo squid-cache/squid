a583eb88027048331cd51a46dfe54d1a0cb5cbf9
Francesco Chemolli <kinkie@squid-cache.org>
2013-11-07 03:59:49 +0100

    Fix stub_external_acl on icc

--- src/tests/stub_external_acl.cc
+++ src/tests/stub_external_acl.cc
@@ -10,14 +10,14 @@ void parse_externalAclHelper(external_acl ** ) STUB
 void dump_externalAclHelper(StoreEntry *, const char *, const external_acl *) STUB
 void free_externalAclHelper(external_acl **) STUB
 void ACLExternal::parse() STUB
-bool ACLExternal::valid () const STUB
-bool ACLExternal::empty () const STUB
-int ACLExternal::match(ACLChecklist *) STUB
-wordlist * ACLExternal::dump() const STUB
+bool ACLExternal::valid () const STUB_RETVAL(false)
+bool ACLExternal::empty () const STUB_RETVAL(false)
+int ACLExternal::match(ACLChecklist *) STUB_RETVAL(0)
+wordlist * ACLExternal::dump() const STUB_RETVAL(NULL)
 void ACLExternal::ExternalAclLookup(ACLChecklist *, ACLExternal *) STUB
 void ExternalACLLookup::Start(ACLChecklist *, external_acl_data *, bool) STUB
 void externalAclInit(void) STUB_NOP
 void externalAclShutdown(void) STUB_NOP
-ExternalACLLookup * ExternalACLLookup::Instance() STUB
+ExternalACLLookup * ExternalACLLookup::Instance() STUB_RETVAL(NULL)
 void ExternalACLLookup::checkForAsync(ACLChecklist *) const STUB
 void ExternalACLLookup::LookupDone(void *, void *) STUB
