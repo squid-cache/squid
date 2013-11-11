a4caeef76c403b2f3ae33ca97e1a81bc0fce15e2
Automatic source maintenance <squidadm@squid-cache.org>
2013-11-10 21:24:58 -0700

    SourceFormat Enforcement

--- src/ipc/TypedMsgHdr.cc
+++ src/ipc/TypedMsgHdr.cc
@@ -170,10 +170,10 @@ Ipc::TypedMsgHdr::putRaw(const void *raw, size_t size)
 bool
 Ipc::TypedMsgHdr::hasFd() const
 {
-     struct cmsghdr *cmsg = CMSG_FIRSTHDR(this);
-     return cmsg &&
-         cmsg->cmsg_level == SOL_SOCKET &&
-         cmsg->cmsg_type == SCM_RIGHTS;
+    struct cmsghdr *cmsg = CMSG_FIRSTHDR(this);
+    return cmsg &&
+           cmsg->cmsg_level == SOL_SOCKET &&
+           cmsg->cmsg_type == SCM_RIGHTS;
 }
 
 void
