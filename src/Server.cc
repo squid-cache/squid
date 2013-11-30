404ea3b2aa8d7a2fe50aee68be1d380a52c9716d
Amos Jeffries <squid3@treenet.co.nz>
2013-11-30 02:06:57 -0700

    Fix mistake in porting rev.12660

--- src/Server.cc
+++ src/Server.cc
@@ -526,7 +526,7 @@ ServerStateData::haveParsedReplyHeaders()
 
     // adaptation may overwrite old offset computed using the virgin response
     const bool partial = theFinalReply->content_range &&
-                         theFinalReply->sline.status() == Http::scPartialContent;
+                         theFinalReply->sline.status() == HTTP_PARTIAL_CONTENT;
     currentOffset = partial ? theFinalReply->content_range->spec.offset : 0;
 }
 
