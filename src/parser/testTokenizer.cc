2c1716e735ab201cd8d91af29001b06eff589f46
Francesco Chemolli <kinkie@squid-cache.org>
2013-12-12 21:27:03 +0100

    Fist stub of tokenizer unit tests

---
+++ src/parser/testTokenizer.cc
@@ -0,0 +1,26 @@
+#include "testTokenizer.h"
+#include "Tokenizer.h"
+
+void
+testTokenizer::testTokenizerPrefix()
+{
+
+}
+
+void
+testTokenizer::testTokenizerSkip()
+{
+
+}
+
+void
+testTokenizer::testTokenizerToken()
+{
+
+}
+
+void
+testTokenizer::testCharacterSet()
+{
+
+}
