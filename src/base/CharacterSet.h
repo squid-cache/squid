f830e249fdb38413bcb115cd53711960e133ce1c
Francesco Chemolli <kinkie@squid-cache.org>
2013-12-15 12:47:07 +0100

    Implemented CharacterSet-based SBuf::find_first_of and find_first_not_of

---
+++ src/base/CharacterSet.h
@@ -0,0 +1,44 @@
+#ifndef _SQUID_SRC_PARSER_CHARACTERSET_H
+#define _SQUID_SRC_PARSER_CHARACTERSET_H
+
+#include <vector>
+
+class CharacterSet
+{
+public:
+    CharacterSet(const char *label, const char * const c) : name(label), chars_(std::vector<bool>(256,false)) {
+        size_t clen = strlen(c);
+        for (size_t i = 0; i < clen; ++i)
+            chars_[static_cast<uint8_t>(c[i])] = true;
+    }
+
+    /// whether a given character exists in the set
+    bool operator[](unsigned char c) const {return chars_[static_cast<uint8_t>(c)];}
+
+    /// add a given char to the character set. c must be >= 0.
+    CharacterSet & add(const unsigned char c) {chars_[static_cast<uint8_t>(c)] = true; return *this; }
+
+    /// add all characters from the given CharacterSet to this one
+    const CharacterSet &operator +=(const CharacterSet &src) {
+        //precondition: src.chars_.size() == chars_.size()
+        std::vector<bool>::const_iterator s = src.chars_.begin();
+        const std::vector<bool>::const_iterator e = src.chars_.end();
+        std::vector<bool>::iterator d = chars_.begin();
+        while (s != e) {
+            if (*s)
+                *d = true;
+            ++s;
+            ++d;
+        }
+        return *this;
+    }
+
+    /// name of this character set
+    const char * name;
+
+private:
+    /// characters defined in this set
+    std::vector<bool> chars_; //std::vector<bool> is optimized
+};
+
+#endif /* _SQUID_SRC_PARSER_CHARACTERSET_H */
