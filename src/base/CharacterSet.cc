d40a1e0b8e0bb90db9c4fc2ddf9c96fd538857a2
Francesco Chemolli <kinkie@squid-cache.org>
2013-12-17 16:58:52 +0100

    Clarified documentatio for CharacterSet; moved CharacterSet::add and constructor to .cc file

--- src/base/CharacterSet.cc
+++ src/base/CharacterSet.cc
@@ -5,17 +5,29 @@
 const CharacterSet &
 CharacterSet::operator +=(const CharacterSet &src)
 {
-    if (src.chars_.size() > chars_.size())
-        chars_.reserve(src.chars_.size());
-
     vector_type::const_iterator s = src.chars_.begin();
     const vector_type::const_iterator e = src.chars_.end();
     vector_type::iterator d = chars_.begin();
     while (s != e) {
         if (*s)
-            *d = true;
+            *d = 1;
         ++s;
         ++d;
     }
     return *this;
+}
+
+CharacterSet &
+CharacterSet::add(const unsigned char c)
+{
+    chars_[static_cast<uint8_t>(c)] = 1;
+    return *this;
+}
+
+CharacterSet::CharacterSet(const char *label, const char * const c)
+: name(label == NULL ? "anonymous" : label), chars_(vector_type(256,0))
+{
+    const size_t clen = strlen(c);
+    for (size_t i = 0; i < clen; ++i)
+        add(c[i]);
 }
