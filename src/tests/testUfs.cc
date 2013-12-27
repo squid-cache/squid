02c267b034c557181e21f1c0adab04c5286eb2d8
Alex Rousskov <rousskov@measurement-factory.com>
2013-12-27 11:37:26 -0700

    Updated tests/testRock and the store rebuild stubs it needs. Synced tests/testUfs after updating the store rebuild stubs.
    Store uses StoreController::store_dirs_rebuilding to decide whether the entry
    release should be delayed. Thus, storeRebuildComplete() must update it. Also
    synced the corresponding CPPUNIT_ASSERT_EQUAL() statements to expect a zero
    value after the rebuild is completed.
    
    Do not create an entry just to get its key: Creating forces a public key which
    necessarily invalidates the previously cached entry with the same key, if any.
    
    Unlock unused entries. This helps with their release later (and adds more
    realism to the test case).
    
    Fixed entry #5 test case to account for the fact that creating a second entry
    with the same public key invalidates the first entry.
    
    Release instead of just unlinking entries. StoreEntry::release() is the public
    interface which updates internal tables as needed. StoreEntry::unlink() is an
    internal call for updating the disk cache-related state; it does not update
    the the in-transit index.

--- src/tests/testUfs.cc
+++ src/tests/testUfs.cc
@@ -129,7 +129,7 @@ testUfs::testUfsSearch()
     /* rebuild is a scheduled event */
     StockEventLoop loop;
 
-    while (StoreController::store_dirs_rebuilding > 1)
+    while (StoreController::store_dirs_rebuilding)
         loop.runOnce();
 
     /* cannot use loop.run(); as the loop will never idle: the store-dir
@@ -137,7 +137,7 @@ testUfs::testUfsSearch()
      */
 
     /* nothing left to rebuild */
-    CPPUNIT_ASSERT_EQUAL(1, StoreController::store_dirs_rebuilding);
+    CPPUNIT_ASSERT_EQUAL(0, StoreController::store_dirs_rebuilding);
 
     /* add an entry */
     {
