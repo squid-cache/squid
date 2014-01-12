09831610517176b6c21e602d526ab6d0b6d38395
Susant Sahani <ssahani@redhat.com>
2014-01-11 19:09:32 -0700

    Bug 3996: Malformed DNS reply leads to crash

--- src/dns_internal.cc
+++ src/dns_internal.cc
@@ -1504,6 +1504,12 @@ idnsReadVCHeader(const Comm::ConnectionPointer &conn, char *buf, size_t len, com
 
     vc->msglen = ntohs(vc->msglen);
 
+    if (!vc->msglen) {
+        if (Comm::IsConnOpen(conn))
+            conn->close();
+        return;
+    }
+
     vc->msg->init(vc->msglen, vc->msglen);
     AsyncCall::Pointer call = commCbCall(5,4, "idnsReadVC",
                                          CommIoCbPtrFun(idnsReadVC, vc));
