1ee292b7b9744bc535e69fc5a00118763a9fd339
Amos Jeffries <squid3@treenet.co.nz>
2014-01-26 22:27:41 -0700

    Bug 3608: part 1 - per-service name for workers UDS sockets
    Separate the UDS socket names used by SMP workers by the -n service_name
    label assigned to the Squid instance being run.
    
    TODO: separate shared memory blocks per-service.

--- src/mgr/Action.cc
+++ src/mgr/Action.cc
@@ -79,7 +79,7 @@ Mgr::Action::sendResponse(unsigned int requestId)
     Response response(requestId, this);
     Ipc::TypedMsgHdr message;
     response.pack(message);
-    Ipc::SendMessage(Ipc::coordinatorAddr, message);
+    Ipc::SendMessage(Ipc::Port::CoordinatorAddr(), message);
 }
 
 void
