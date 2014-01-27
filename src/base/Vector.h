cd7be71b4da399d11bb0383dd0b3ab561c1e363c
Unknown FreeBSD Contributor <Unknown>
2014-01-26 19:11:08 -0700

    Bug 3954: compile failure in CpuAffinity.cc
    Patch written by 'dim [1]' contributor to FreeBSD and imported to Squid
    under FreeBSD license. see
    http://svnweb.freebsd.org/ports/head/www/squid33/files/patch-include__Array.h

--- src/base/Vector.h
+++ src/base/Vector.h
@@ -42,12 +42,18 @@
 #include "compat/assert.h"
 
 /* iterator support */
+#include <iterator>
 
 template <class C>
 class VectorIteratorBase
 {
-
 public:
+    typedef typename C::value_type value_type;
+    typedef std::forward_iterator_tag iterator_category;
+    typedef typename C::pointer pointer;
+    typedef typename C::reference reference;
+    typedef typename C::difference_type difference_type;
+
     VectorIteratorBase();
     VectorIteratorBase(C &);
     VectorIteratorBase(size_t, C &);
@@ -75,12 +81,13 @@ private:
 template<class E>
 class Vector
 {
-
 public:
     typedef E value_type;
     typedef E* pointer;
+    typedef E& reference;
     typedef VectorIteratorBase<Vector<E> > iterator;
     typedef VectorIteratorBase<Vector<E> const> const_iterator;
+    typedef ptrdiff_t difference_type;
 
     void *operator new (size_t);
     void operator delete (void *);
