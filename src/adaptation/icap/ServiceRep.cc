81481ec0087aad9e3a85b5614a1e8ca9ccecd680
Francesco Chemolli <kinkie@squid-cache.org>
2014-02-10 13:58:49 +0100

    Migrate some users of Vector to std::vector

--- src/adaptation/icap/ServiceRep.cc
+++ src/adaptation/icap/ServiceRep.cc
@@ -470,7 +470,7 @@ void Adaptation::Icap::ServiceRep::checkOptions()
     if (!theOptions->methods.empty()) {
         bool method_found = false;
         String method_list;
-        Vector <ICAP::Method>::iterator iter = theOptions->methods.begin();
+        std::vector <ICAP::Method>::iterator iter = theOptions->methods.begin();
 
         while (iter != theOptions->methods.end()) {
 
