36332557faee3b6800d4d93cc0d3415a14681a5f
Amos Jeffries <squid3@treenet.co.nz>
2014-02-21 12:33:29 +1300

    Fix compile issues in rev.13288

--- tools/squidclient/Ping.cc
+++ tools/squidclient/Ping.cc
@@ -3,13 +3,12 @@
 #include "tools/squidclient/Parameters.h"
 #include "tools/squidclient/Ping.h"
 
+#include <climits>
+#include <csignal>
 #include <iostream>
 
 #if HAVE_GETOPT_H
 #include <getopt.h>
-#endif
-#if HAVE_SIGNAL_H
-#include <signal.h>
 #endif
 
 namespace Ping
@@ -101,7 +100,7 @@ Ping::TimerStop(size_t fsize)
     t2s = tv2.tv_sec;
     tmp = localtime(&t2s);
     char tbuf[4096];
-    snprintf(tbuf, sizeof(tbuf)-1, "%d-%02d-%02d %02d:%02d:%02d [%d]: %ld.%03ld secs, %f KB/s",
+    snprintf(tbuf, sizeof(tbuf)-1, "%d-%02d-%02d %02d:%02d:%02d [%ld]: %ld.%03ld secs, %f KB/s",
              tmp->tm_year + 1900, tmp->tm_mon + 1, tmp->tm_mday,
              tmp->tm_hour, tmp->tm_min, tmp->tm_sec, stats.counted + 1,
              elapsed_msec / 1000, elapsed_msec % 1000,
@@ -135,7 +134,8 @@ Ping::DisplayStats()
 {
     if (Ping::Config.enable && stats.counted) {
         long mean = stats.sum / stats.counted;
-        std::cerr << stats.counted << " requests, round-trip (secs) min/avg/max = "
+        std::cerr << std::endl
+                  << stats.counted << " requests, round-trip (secs) min/avg/max = "
                   << (stats.pMin/1000) << "." << (stats.pMin%1000)
                   << "/" << (mean/1000) << "." << (mean%1000)
                   << "/" << (stats.pMax/1000) << "." << (stats.pMax%1000)
