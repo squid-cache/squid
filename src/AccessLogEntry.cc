230114fea4a1b7052a4f31fc5ef87b817e9e0104
Amos Jeffries <squid3@treenet.co.nz>
2014-02-19 18:27:16 -0700

    Regression Bug 3769: client_netmask not evaluated since Comm redesign

--- src/AccessLogEntry.cc
+++ src/AccessLogEntry.cc
@@ -15,17 +15,30 @@ AccessLogEntry::SslDetails::SslDetails(): user(NULL), bumpMode(::Ssl::bumpEnd)
 void
 AccessLogEntry::getLogClientIp(char *buf, size_t bufsz) const
 {
+    Ip::Address log_ip;
+
 #if FOLLOW_X_FORWARDED_FOR
     if (Config.onoff.log_uses_indirect_client && request)
-        request->indirect_client_addr.NtoA(buf, bufsz);
+        log_ip = request->indirect_client_addr;
     else
 #endif
         if (tcpClient != NULL)
-            tcpClient->remote.NtoA(buf, bufsz);
-        else if (cache.caddr.IsNoAddr()) // e.g., ICAP OPTIONS lack client
+            log_ip = tcpClient->remote;
+        else if (cache.caddr.IsNoAddr()) { // e.g., ICAP OPTIONS lack client
             strncpy(buf, "-", bufsz);
-        else
-            cache.caddr.NtoA(buf, bufsz);
+            return;
+        } else
+            log_ip = cache.caddr;
+
+    // Apply so-called 'privacy masking' to IPv4 clients
+    // - localhost IP is always shown in full
+    // - IPv4 clients masked with client_netmask
+    // - IPv6 clients use 'privacy addressing' instead.
+
+    if (!log_ip.IsLocalhost() && log_ip.IsIPv4())
+        log_ip.ApplyMask(Config.Addrs.client_netmask);
+
+    log_ip.NtoA(buf, bufsz);
 }
 
 AccessLogEntry::~AccessLogEntry()
