c9d7b37fbbc5abcb0ca0120bc0f345ca141b7200
Amos Jeffries <squid3@treenet.co.nz>
2014-03-05 20:55:41 -0700

    Copyright: Relicense helpers by Treehouse Networks Ltd.
    Update the license on helper code designed and authored by myself using
    the BSD 2-clause license. This makes the example helper code and license
    more legally acceptible for use as a basis of proprietary helpers while
    remaining compatible with GPL for distribution with Squid.

--- helpers/url_rewrite/fake/fake.cc
+++ helpers/url_rewrite/fake/fake.cc
@@ -1,15 +1,37 @@
 /*
- * AUTHOR: Amos Jeffries <squid3@treenet.co.nz>
+ * Copyright (c) 2009-2014, Treehouse Networks Ltd. New Zealand
+ * All rights reserved.
  *
- * Example url re-writer program for Squid.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Example URL re-writer program for Squid.
  *
  * This code gets the url and returns it. No re-writing is done.
  * It is intended for testing use and as a base for further implementation.
- *
- *
- * This code is copyright (C) 2009 by Treehouse Networks Ltd
- * of New Zealand. It is published and Licensed as an extension of
- * squid under the same conditions as the main squid application.
  */
 
 #include "squid.h"
