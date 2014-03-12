b11724bb229abea57b43e9747c4a12bfbfb61ac5
Christos Tsantilas <chtsanti@users.sourceforge.net>
2014-03-12 18:46:27 +0200

    Add url_rewrite_extras and store_id_extras for redirector and store_id helpers
    The url_rewrite_extras/store_id_extras is a "quoted string" with logformat
    %macro support. It is used to modify the request line for redirector and
    storeId helpers.
    
    The url rewrite and store_id helpers request format now is:
           url [<SP> extras]
    and the default value for extras is:
      "%>a/%>A %un %>rm myip=%la myport=%lp"
    
    Example usage:
       url_rewrite_extras "Note1=%{Note1}note Note2=%{Note2}note"
    
    This is a Measurement Factory project.

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -3033,6 +3033,21 @@ parse_eol(char *volatile *var)
 #define free_eol free_string
 
 static void
+parse_TokenOrQuotedString(char **var)
+{
+    char *token = ConfigParser::NextQuotedToken();
+    safe_free(*var);
+
+    if (token == NULL)
+        self_destruct();
+
+    *var = xstrdup(token);
+}
+
+#define dump_TokenOrQuotedString dump_string
+#define free_TokenOrQuotedString free_string
+
+static void
 dump_time_t(StoreEntry * entry, const char *name, time_t var)
 {
     storeAppendPrintf(entry, "%s %d seconds\n", name, (int) var);
--- src/client_side_request.cc
+++ src/client_side_request.cc
@@ -897,7 +897,7 @@ void
 ClientRequestContext::clientRedirectStart()
 {
     debugs(33, 5, HERE << "'" << http->uri << "'");
-
+    (void)SyncNotes(*http->al, *http->request);
     if (Config.accessList.redirector) {
         acl_checklist = clientAclChecklistCreate(Config.accessList.redirector, http);
         acl_checklist->nonBlockingCheck(clientRedirectAccessCheckDone, this);
