1c8f8c938299e5d09446efe49df91b82550f4596
Amos Jeffries <squid3@treenet.co.nz>
2014-03-22 06:03:18 -0600

    Portability: Basic auth NIS helper depends on crypt()
    Detect crypt.h before permitting the helper to build.
    
    Also, if crypt() function is missing build the helper to output BH code
    with an error message instead of failing to build.

--- helpers/basic_auth/NIS/basic_nis_auth.cc
+++ helpers/basic_auth/NIS/basic_nis_auth.cc
@@ -65,9 +65,15 @@ main(int argc, char **argv)
         if (!nispasswd) {
             /* User does not exist */
             printf("ERR No such user\n");
+#if HAVE_CRYPT
         } else if (strcmp(nispasswd, (char *) crypt(passwd, nispasswd)) == 0) {
             /* All ok !, thanks... */
             printf("OK\n");
+#else
+        } else {
+            /* Password incorrect */
+            printf("BH message=\"Missing crypto capability\"\n");
+#endif
         } else {
             /* Password incorrect */
             printf("ERR Wrong password\n");
