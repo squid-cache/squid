5ecaae99b90a544d52b14f0de6a97b132c6f421b
Amos Jeffries <squid3@treenet.co.nz>
2014-03-22 06:58:52 -0600

    Fix if-else mismatch in rev.13319

--- helpers/basic_auth/NIS/basic_nis_auth.cc
+++ helpers/basic_auth/NIS/basic_nis_auth.cc
@@ -69,14 +69,14 @@ main(int argc, char **argv)
         } else if (strcmp(nispasswd, (char *) crypt(passwd, nispasswd)) == 0) {
             /* All ok !, thanks... */
             printf("OK\n");
+        } else {
+            /* Password incorrect */
+            printf("ERR Wrong password\n");
 #else
         } else {
             /* Password incorrect */
             printf("BH message=\"Missing crypto capability\"\n");
 #endif
-        } else {
-            /* Password incorrect */
-            printf("ERR Wrong password\n");
         }
     }
     exit(0);
