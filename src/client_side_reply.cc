f11c8e2f5b6f5e70d6fbd45526491efeed7cb5b7
Amos Jeffries <squid3@treenet.co.nz>
2014-03-30 23:57:27 -0700

    HTTP/1.1: Update registered status codes from IANA registry
    IETF HTTPbis WG has created an IANA registry for HTTP status codes at
    http://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
    
    Update our registered codes and strings to match the registry entries.

--- src/client_side_reply.cc
+++ src/client_side_reply.cc
@@ -710,8 +710,8 @@ clientReplyContext::processOnlyIfCachedMiss()
 {
     debugs(88, 4, "clientProcessOnlyIfCachedMiss: '" <<
            RequestMethodStr(http->request->method) << " " << http->uri << "'");
-    http->al->http.code = Http::scGateway_Timeout;
-    ErrorState *err = clientBuildError(ERR_ONLY_IF_CACHED_MISS, Http::scGateway_Timeout, NULL,
+    http->al->http.code = Http::scGatewayTimeout;
+    ErrorState *err = clientBuildError(ERR_ONLY_IF_CACHED_MISS, Http::scGatewayTimeout, NULL,
                                        http->getConn()->clientConnection->remote, http->request);
     removeClientStoreReference(&sc, http);
     startError(err);
