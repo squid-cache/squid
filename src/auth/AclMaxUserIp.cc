bb54459746038e8998666fcc3abf48ca7da02865
Francesco Chemolli <kinkie@squid-cache.org>
2014-04-12 16:50:51 +0200

    Fix broken acl_access dumper

--- src/auth/AclMaxUserIp.cc
+++ src/auth/AclMaxUserIp.cc
@@ -153,7 +153,7 @@ ACLMaxUserIP::dump() const
     if (!maximum)
         return sl;
     SBuf s;
-    s.Printf("%lu", (unsigned long int) maximum);
+    s.Printf("%d", maximum);
     sl.push_back(s);
     return sl;
 }
