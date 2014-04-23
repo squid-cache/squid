07266c4c6801a8b8703207535107f2180d6b4989
Amos Jeffries <squid3@treenet.co.nz>
2014-04-22 22:15:49 -0700

    Fix outgoing peer type when using ORIGINAL_DST instead of DIRECT

--- src/peer_select.cc
+++ src/peer_select.cc
@@ -242,7 +242,7 @@ peerSelectDnsPaths(ps_state *psstate)
             // construct a "result" adding the ORIGINAL_DST to the set instead of DIRECT
             Comm::ConnectionPointer p = new Comm::Connection();
             p->remote = req->clientConnectionManager->clientConnection->local;
-            p->peerType = fs->code;
+            p->peerType = ORIGINAL_DST; // fs->code is DIRECT. This fixes the display.
             p->setPeer(fs->_peer);
 
             // check for a configured outgoing address for this destination...
