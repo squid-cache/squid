e4ae074cecfad815a379ad8d9fd493c9bf361bf9
Alex Rousskov <rousskov@measurement-factory.com>
2014-05-08 22:45:54 -0600

    Temporary fix for segmentation faults in FwdState::serverClosed debugging.
    See r13404 for details.

--- src/FwdState.cc
+++ src/FwdState.cc
@@ -619,9 +619,10 @@ FwdState::checkRetriable()
 void
 FwdState::serverClosed(int fd)
 {
+    // XXX: fd is often -1 here
     debugs(17, 2, "FD " << fd << " " << entry->url() << " after " <<
-           fd_table[fd].pconn.uses << " requests");
-    if (fd >= 0 && serverConnection()->fd == fd) // XXX: fd is often -1 here
+           (fd >= 0 ? fd_table[fd].pconn.uses : -1) << " requests");
+    if (fd >= 0 && serverConnection()->fd == fd)
         fwdPconnPool->noteUses(fd_table[fd].pconn.uses);
     retryOrBail();
 }
