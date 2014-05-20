6d0613b273a03d7c388a97a70cb62dbc0adbd853
Amos Jeffries <squid3@treenet.co.nz>
2014-05-19 20:45:00 -0700

    Cleanup debug output
    * fix debug display of sections parsed, hiding followup bytes in buffer.
    
    * removes several duplicate dumps of message bytes.
    
    * add mimeHeader() accessor to reduce c_str() usage when accessing mime
      header content.

--- src/http/Http1Parser.h
+++ src/http/Http1Parser.h
@@ -69,6 +69,9 @@ public:
     int64_t messageHeaderSize() const {return firstLineSize() + headerBlockSize();}
 
     /// buffer containing HTTP mime headers, excluding message first-line.
+    const SBuf mimeHeader() const {return mimeHeaderBlock_;}
+
+    /// char* version of mimeHeader()
     const char *rawHeaderBuf() {return mimeHeaderBlock_.c_str();}
 
     /// attempt to parse a message from the buffer
