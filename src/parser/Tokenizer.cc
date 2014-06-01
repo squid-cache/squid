06af457a81d8dc077b24a79cfd1460312d720a71
Amos Jeffries <squid3@treenet.co.nz>
2014-06-01 01:57:43 -0700

    Import Tokenizer updates

--- src/parser/Tokenizer.cc
+++ src/parser/Tokenizer.cc
@@ -1,25 +1,27 @@
 #include "squid.h"
-#include "Tokenizer.h"
-
-namespace Parser {
+#include "parser/Tokenizer.h"
 
 bool
-Tokenizer::token(SBuf &returnedToken, const CharacterSet &whitespace)
+Parser::Tokenizer::token(SBuf &returnedToken, const CharacterSet &delimiters)
 {
-    const SBuf::size_type endOfPreWhiteSpace = buf_.findFirstNotOf(whitespace);
-    const SBuf::size_type endOfToken = buf_.findFirstOf(whitespace, endOfPreWhiteSpace);
-    if (endOfToken == SBuf::npos)
+    SBuf savebuf(buf_);
+    skip(delimiters);
+    SBuf::size_type tokenLen = buf_.findFirstOf(delimiters); // not found = npos => consume to end
+    if (tokenLen == SBuf::npos && !delimiters['\0']) {
+        // no delimiter found, nor is NUL/EOS/npos acceptible as one
+        buf_ = savebuf;
         return false;
-    buf_.consume(endOfPreWhiteSpace);
-    returnedToken = buf_.consume(endOfToken - endOfPreWhiteSpace);
-    skip(whitespace);
+    }
+    SBuf retval = buf_.consume(tokenLen);
+    skip(delimiters);
+    returnedToken = retval;
     return true;
 }
 
 bool
-Tokenizer::prefix(SBuf &returnedToken, const CharacterSet &tokenChars)
+Parser::Tokenizer::prefix(SBuf &returnedToken, const CharacterSet &tokenChars, const SBuf::size_type limit)
 {
-    SBuf::size_type prefixLen = buf_.findFirstNotOf(tokenChars);
+    SBuf::size_type prefixLen = buf_.substr(0,limit).findFirstNotOf(tokenChars);
     if (prefixLen == 0)
         return false;
     returnedToken = buf_.consume(prefixLen);
@@ -27,7 +29,7 @@ Tokenizer::prefix(SBuf &returnedToken, const CharacterSet &tokenChars)
 }
 
 bool
-Tokenizer::skip(const CharacterSet &tokenChars)
+Parser::Tokenizer::skip(const CharacterSet &tokenChars)
 {
     SBuf::size_type prefixLen = buf_.findFirstNotOf(tokenChars);
     if (prefixLen == 0)
@@ -37,7 +39,7 @@ Tokenizer::skip(const CharacterSet &tokenChars)
 }
 
 bool
-Tokenizer::skip(const SBuf &tokenToSkip)
+Parser::Tokenizer::skip(const SBuf &tokenToSkip)
 {
     if (buf_.startsWith(tokenToSkip)) {
         buf_.consume(tokenToSkip.length());
@@ -47,7 +49,7 @@ Tokenizer::skip(const SBuf &tokenToSkip)
 }
 
 bool
-Tokenizer::skip(const char tokenChar)
+Parser::Tokenizer::skip(const char tokenChar)
 {
     if (buf_[0] == tokenChar) {
         buf_.consume(1);
@@ -55,4 +57,78 @@ Tokenizer::skip(const char tokenChar)
     }
     return false;
 }
-} /* namespace Parser */
+
+/* reworked from compat/strtoll.c */
+bool
+Parser::Tokenizer::int64(int64_t & result, int base)
+{
+    if (buf_.isEmpty())
+        return false;
+
+    //fixme: account for buf_.size()
+    bool neg = false;
+    const char *s = buf_.rawContent();
+    const char *end = buf_.rawContent() + buf_.length();
+
+    if (*s == '-') {
+        neg = true;
+        ++s;
+    } else if (*s == '+') {
+        ++s;
+    }
+    if (s >= end) return false;
+    if (( base == 0 || base == 16) && *s == '0' && (s+1 <= end ) &&
+                    tolower(*(s+1)) == 'x') {
+        s += 2;
+        base = 16;
+    }
+    if (base == 0) {
+        if ( *s == '0') {
+            base = 8;
+            ++s;
+        } else {
+            base = 10;
+        }
+    }
+    if (s >= end) return false;
+
+    uint64_t cutoff;
+
+    cutoff = neg ? -static_cast<uint64_t>(INT64_MIN) : INT64_MAX;
+    int cutlim = cutoff % static_cast<int64_t>(base);
+    cutoff /= static_cast<uint64_t>(base);
+
+    int any = 0, c;
+    int64_t acc = 0;
+    for (c = *s++; s <= end; c = *s++) {
+        if (xisdigit(c)) {
+            c -= '0';
+        } else if (xisalpha(c)) {
+            c -= xisupper(c) ? 'A' - 10 : 'a' - 10;
+        } else {
+            break;
+        }
+        if (c >= base)
+            break;
+        if (any < 0 || static_cast<uint64_t>(acc) > cutoff || (static_cast<uint64_t>(acc) == cutoff && c > cutlim))
+            any = -1;
+        else {
+            any = 1;
+            acc *= base;
+            acc += c;
+        }
+    }
+
+    if (any == 0) // nothing was parsed
+        return false;
+    if (any < 0) {
+        acc = neg ? INT64_MIN : INT64_MAX;
+        errno = ERANGE;
+        return false;
+    } else if (neg)
+        acc = -acc;
+
+    result = acc;
+    buf_.consume(s - buf_.rawContent() -1);
+    return true;
+}
