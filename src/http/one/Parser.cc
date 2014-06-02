8e5b5bef53eddac42ea29c2d4cba27739d2f90f2
Amos Jeffries <squid3@treenet.co.nz>
2014-06-02 04:49:56 -0700

    Cleanup: rewrite Http::One::Parser::getHeaderField() using Tokenizer
    Fixes performance regression in SBuf usage.
    
    Also, fixes one nasty bug where it would return the value of a line
    containign the named header despite obs-fold and quoting. This was
    particualarly bad as this method is used primarily to retrieve Host:
    header before full aprsing of the mime block takes place.
    
    The bug causes "foo.invalid" to be detected as hostname in:
    
      GET / HTTP/1.1
      Foo: bar="\r  Host: fake.invalid\n  "
      Host: example.com

--- src/http/one/Parser.cc
+++ src/http/one/Parser.cc
@@ -1,6 +1,7 @@
 #include "squid.h"
 #include "Debug.h"
 #include "http/one/Parser.h"
+#include "parser/Tokenizer.h"
 
 /// RFC 7230 section 2.6 - 7 magic octets
 const SBuf Http::One::Parser::Http1magic("HTTP/1.");
@@ -17,60 +18,53 @@ Http::One::Parser::clear()
 // arbitrary maximum-length for headers which can be found by Http1Parser::getHeaderField()
 #define GET_HDR_SZ	1024
 
+// BUG: returns only the first header line with given name,
+//      ignores multi-line headers and obs-fold headers
 char *
 Http::One::Parser::getHeaderField(const char *name)
 {
-    LOCAL_ARRAY(char, header, GET_HDR_SZ);
-    const char *p = NULL;
-    char *q = NULL;
-    char got = 0;
-    const int namelen = name ? strlen(name) : 0;
-
     if (!headerBlockSize() || !name)
         return NULL;
 
+    LOCAL_ARRAY(char, header, GET_HDR_SZ);
+    const int namelen = name ? strlen(name) : 0;
+
     debugs(25, 5, "looking for '" << name << "'");
 
-    for (p = mimeHeader().c_str(); *p; p += strcspn(p, "\n\r")) {
-        if (strcmp(p, "\r\n\r\n") == 0 || strcmp(p, "\n\n") == 0)
-            return NULL;
+    ::Parser::Tokenizer tok(mimeHeaderBlock_);
+    SBuf p;
+    const SBuf crlf("\r\n");
 
-        while (xisspace(*p))
-            ++p;
+    // while we can find more LF in the SBuf
+    while (tok.prefix(p, CharacterSet::LF)) {
+        tok.skip(CharacterSet::LF); // move tokenizer past the LF
 
-        if (strncasecmp(p, name, namelen))
+        // header lines must start with the name (case insensitive)
+        if (p.substr(0, namelen).caseCmp(name, namelen))
             continue;
 
-        if (!xisspace(p[namelen]) && p[namelen] != ':')
+        // then a COLON
+        if (p[namelen] != ':')
             continue;
 
-        int l = strcspn(p, "\n\r") + 1;
+        // drop any trailing *CR sequence
+        p.trim(crlf, false, true);
 
-        if (l > GET_HDR_SZ)
-            l = GET_HDR_SZ;
+        debugs(25, 5, "checking " << p);
+        p.consume(namelen + 1);
 
-        xstrncpy(header, p, l);
+        // TODO: optimize SBuf::trim to take CharacterSet directly
+        ::Parser::Tokenizer t(p);
+        t.skip(CharacterSet::WSP);
+        p = t.remaining();
 
-        debugs(25, 5, "checking '" << header << "'");
+        // prevent buffer overrun on char header[];
+        p.chop(0, sizeof(header)-1);
 
-        q = header;
-
-        q += namelen;
-
-        if (*q == ':') {
-            ++q;
-            got = 1;
-        }
-
-        while (xisspace(*q)) {
-            ++q;
-            got = 1;
-        }
-
-        if (got) {
-            debugs(25, 5, "returning '" << q << "'");
-            return q;
-        }
+        // return the header field-value
+        xstrncpy(header, p.rawContent(), p.length());
+        debugs(25, 5, "returning: " << header);
+        return header;
     }
 
     return NULL;
