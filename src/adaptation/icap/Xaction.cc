69a0f1a009a7b5ee4185003b3e86fcf2111a29e5
Amos Jeffries <squid3@treenet.co.nz>
2014-06-05 02:32:50 -0700

    Cleanup: remove comm_read_cancel(AsyncCall) API
    Converting remaining users of the old API wrapper to Comm::ReadCancel.

--- src/adaptation/icap/Xaction.cc
+++ src/adaptation/icap/Xaction.cc
@@ -428,7 +428,7 @@ void Adaptation::Icap::Xaction::cancelRead()
 {
     if (reader != NULL) {
         Must(haveConnection());
-        comm_read_cancel(connection->fd, reader);
+        Comm::ReadCancel(connection->fd, reader);
         reader = NULL;
     }
 }
