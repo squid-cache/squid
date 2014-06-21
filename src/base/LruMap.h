0e39d542c3f68044fa191d23461e1521f2f32d50
Christos Tsantilas <chtsanti@users.sourceforge.net>
2014-06-20 22:21:13 -0600

    Assure that when LruMap::memLimit_ is set to 0 no entries stored on LruMap
    Changes:
     - Do not add new entries to LruMap if memLimit is 0
     - Remove all entries in LruMap::trim if memLimit set to 0

--- src/base/LruMap.h
+++ src/base/LruMap.h
@@ -143,6 +143,10 @@ LruMap<EntryValue, EntryCost>::add(const char *key, EntryValue *t)
 
     del(key);
     trim();
+
+    if (memLimit() == 0)
+        return false;
+
     index.push_front(new Entry(key, t));
     storage.insert(MapPair(key, index.begin()));
 
@@ -188,7 +192,7 @@ template <class EntryValue, size_t EntryCost>
 void
 LruMap<EntryValue, EntryCost>::trim()
 {
-    while (memLimit() > 0 && size() >= memLimit()) {
+    while (size() >= memLimit()) {
         QueueIterator i = index.end();
         --i;
         if (i != index.end()) {
