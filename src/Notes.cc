2db163d27601e33be4359a5c9c170dede7dc8c66
Automatic source maintenance <squidadm@squid-cache.org>
2014-07-12 18:14:38 -0600

    SourceFormat Enforcement

--- src/Notes.cc
+++ src/Notes.cc
@@ -207,7 +207,7 @@ void
 NotePairs::remove(const char *key)
 {
     std::vector<NotePairs::Entry *>::iterator i = entries.begin();
-    while(i != entries.end()) {
+    while (i != entries.end()) {
         if ((*i)->name.cmp(key) == 0) {
             delete *i;
             i = entries.erase(i);
