21e820721b226797e2317c62013e268d9bd6097d
Amos Jeffries <squid3@treenet.co.nz>
2014-07-14 05:58:03 -0700

    Validate -n service name parameter value
    Service name is used for path prefixes in SHM/UDS sockets, Windows
    service name, Unix/Linux/BSD daemon names, and file path segments.
    
    Restrict service names to a max 32 character alphanumeric value in order
    to accommodate as widely portable names as possible in all these cases.

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -358,7 +358,7 @@ SubstituteMacro(char*& line, int& len, const char* macroName, const char* substS
 static void
 ProcessMacros(char*& line, int& len)
 {
-    SubstituteMacro(line, len, "${service_name}", service_name);
+    SubstituteMacro(line, len, "${service_name}", service_name.c_str());
     SubstituteMacro(line, len, "${process_name}", TheKidName);
     SubstituteMacro(line, len, "${process_number}", xitoa(KidIdentifier));
 }
