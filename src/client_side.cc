9d29f845e40a7b8fe6fad5458d9b7a6e4d3e66e8
Amos Jeffries <squid3@treenet.co.nz>
2014-07-24 18:46:09 -0700

    Log PROXY errors to cache.log

--- src/client_side.cc
+++ src/client_side.cc
@@ -2931,8 +2931,10 @@ ConnStateData::proxyProtocolValidateClient()
 bool
 ConnStateData::proxyProtocolError(const char *msg)
 {
-    if (msg)
+    if (msg) {
+        debugs(33, 2, msg << " from " << clientConnection);
         mustStop(msg);
+    }
     return false;
 }
 
@@ -2994,7 +2996,7 @@ ConnStateData::parseProxy10()
         // else, no LF found
 
         // protocol error only if there are more than 107 bytes prefix header
-        return proxyProtocolError(in.buf.length() > 107? "PROXY error: missing CRLF":NULL);
+        return proxyProtocolError(in.buf.length() > 107? "PROXY/1.0 error: missing CRLF":NULL);
 
     } else if (!tcpVersion.cmp("TCP",3)) {
 
