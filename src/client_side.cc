d3d92daafd6ebda0b0969b0f715af49e6b399d92
Amos Jeffries <squid3@treenet.co.nz>
2014-08-05 07:39:35 -0700

    Updated configuration options
    * Replace proxy-surrogate fag with require-proxy-header
    
    * Revert follow_x_forwarded_for linkage with PROXY protocol.
      Add proxy_protocol_access directive for PROXY ACLs instead.
    
    * set fast default for proxy_protocol_access when none configured.

--- src/client_side.cc
+++ src/client_side.cc
@@ -2905,14 +2905,17 @@ ConnStateData::concurrentRequestQueueFilled() const
 }
 
 /**
- * Perform forwarded_access ACL tests on the client which
+ * Perform proxy_protocol_access ACL tests on the client which
  * connected to PROXY protocol port to see if we trust the
  * sender enough to accept their PROXY header claim.
  */
 bool
 ConnStateData::proxyProtocolValidateClient()
 {
-    ACLFilledChecklist ch(Config.accessList.followXFF, NULL, clientConnection->rfc931);
+    if (!Config.accessList.proxyProtocol)
+        return proxyProtocolError("PROXY client not permitted by default ACL");
+
+    ACLFilledChecklist ch(Config.accessList.proxyProtocol, NULL, clientConnection->rfc931);
     ch.src_addr = clientConnection->remote;
     ch.my_addr = clientConnection->local;
     ch.conn(this);
