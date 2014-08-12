265e1f455745032f9a03b0712cf3674831ba7ac0
Amos Jeffries <squid3@treenet.co.nz>
2014-08-11 22:24:05 -0700

    Performance optimizations and polish

--- src/client_side.h
+++ src/client_side.h
@@ -405,7 +405,7 @@ private:
     bool parseProxyProtocolHeader();
     bool parseProxy1p0();
     bool parseProxy2p0();
-    bool proxyProtocolError(const char *reason = NULL);
+    bool proxyProtocolError(const char *reason);
 
     /// whether PROXY protocol header is still expected
     bool needProxyProtocolHeader_;
