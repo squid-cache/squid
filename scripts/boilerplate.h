133578a783205143c7c7b69d687a3e4e6ffc3479
Amos Jeffries <squid3@treenet.co.nz>
2014-08-24 21:20:56 -0700

    Update Squid Software Foundation blurb dates

--- scripts/boilerplate.h
+++ scripts/boilerplate.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1996-2013 The Squid Software Foundation and contributors
+ * Copyright (C) 1996-2014 The Squid Software Foundation and contributors
  *
  * Squid software is distributed under GPLv2+ license and includes
  * contributions from numerous individuals and organizations.
