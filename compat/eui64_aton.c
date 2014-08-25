7d02e5cb335b9e5000ee73b71b1d6a8a0c39427b
Amos Jeffries <squid3@treenet.co.nz>
2014-08-25 07:08:02 -0700

    Boilerplate: update copyright on compat/ library sources
    * add Squid Software Foundation boilerplate
    
    * update CONTRIBUTORS with authors not already mentioned.
    
    * update CREDITS with missing licenses.

--- compat/eui64_aton.c
+++ compat/eui64_aton.c
@@ -1,4 +1,12 @@
 /*
+ * Copyright (C) 1996-2014 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+/*
  * Squid Change History:
  *
  *  2009-10-16 : import eui64_aton() function from NetBSD eui64.c
