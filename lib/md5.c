45e00fb01f4130843e9e82c0cd34f3ff1652db75
Amos Jeffries <squid3@treenet.co.nz>
2014-08-28 09:10:38 -0700

    Boilerplate: update copyright blurbs on libmisc* libraries
    * add Squid Software Foundation blurbs
    
    * update CONTRIBUTORS with missing authors
    
    * update CREDITS to match current reality

--- lib/md5.c
+++ lib/md5.c
@@ -1,4 +1,12 @@
 /*
+ * Copyright (C) 1996-2014 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+/*
  * This code implements the MD5 message-digest algorithm.
  * The algorithm is due to Ron Rivest.  This code was
  * written by Colin Plumb in 1993, no copyright is claimed.
