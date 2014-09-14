332fd2670cce31908d94e40e207cda0bccadae8b
Amos Jeffries <squid3@treenet.co.nz>
2014-09-13 20:11:33 -0700

    Boilerplate: update copyright reference for kerberos_ldap_group/support_ldap.cc

--- helpers/external_acl/kerberos_ldap_group/support_ldap.cc
+++ helpers/external_acl/kerberos_ldap_group/support_ldap.cc
@@ -30,6 +30,20 @@
  * -----------------------------------------------------------------------------
  */
 
+/* get_attributes is partly from OpenLDAP Software <http://www.openldap.org/>.
+ *
+ * Copyright 1998-2009 The OpenLDAP Foundation.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the OpenLDAP
+ * Public License.
+ *
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.OpenLDAP.org/license.html>.
+ */
+
 #include "squid.h"
 #include "util.h"
 
