ba687e7081c71dd75fd6d634ec428fa7a95d8e00
Amos Jeffries <squid3@treenet.co.nz>
2014-09-15 01:15:59 -0700

    3.4.8

--- src/client_side_reply.cc
+++ src/client_side_reply.cc
@@ -1401,7 +1401,7 @@ clientReplyContext::buildReplyHeader()
                     if (!request->flags.accelerated && !request->flags.intercepted) {
                         httpHeaderPutStrf(hdr, HDR_PROXY_SUPPORT, "Session-Based-Authentication");
                         /*
-                          We send "[Proxy-]Connection: Proxy-Support" header to mark
+                          We send "Connection: Proxy-Support" header to mark
                           Proxy-Support as a hop-by-hop header for intermediaries that do not
                           understand the semantics of this header. The RFC should have included
                           this recommendation.
