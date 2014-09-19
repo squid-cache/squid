f67e6b47cb55236cdef61c4a72d1211e483a9354
Amos Jeffries <squid3@treenet.co.nz>
2014-09-19 10:52:22 -0700

    Portability: add shim for memrchr() and detect direct.h
    Windows requires direct.h to define the chdir(), chroot(), cwd() and
    related system calls.
    
    memrchr() is not always defined but required by SBuf. Provide a local
    implementation for systems that need it.

--- compat/compat_shared.h
+++ compat/compat_shared.h
@@ -230,6 +230,9 @@ extern "C" {
  */
 #include "compat/strtoll.h"
 
+// memrchr() is a GNU extension. MinGW in particular does not define it.
+#include "compat/memrchr.h"
+
 #if !HAVE_MEMCPY
 #if HAVE_BCOPY
 #define memcpy(d,s,n) bcopy((s),(d),(n))
