615540cd6b5cf323733e3bfd5eead37f8358d478
Amos Jeffries <squid3@treenet.co.nz>
2014-10-02 05:07:26 -0700

    Portability: provide xstatvfs() shim for system call statvfs()
    statfs() and struct statfs have been deprecated for years. Hoewver the
    POSIX statvfs replacements are still not universally available.
    
    Remove deprecated statfs() and struct statfs usage from Squid. Although
    they may still be used by the xstatvfs() compatibility wrapper if they
    are the only available API.

--- compat/mswindows.cc
+++ compat/mswindows.cc
@@ -127,42 +127,6 @@ gettimeofday(struct timeval *pcur_time, void *tzp)
 
 #if !_SQUID_MINGW_
 int
-statfs(const char *path, struct statfs *sfs)
-{
-    char drive[4];
-    DWORD spc, bps, freec, totalc;
-    DWORD vsn, maxlen, flags;
-
-    if (!sfs) {
-        errno = EINVAL;
-        return -1;
-    }
-    strncpy(drive, path, 2);
-    drive[2] = '\0';
-    strcat(drive, "\\");
-
-    if (!GetDiskFreeSpace(drive, &spc, &bps, &freec, &totalc)) {
-        errno = ENOENT;
-        return -1;
-    }
-    if (!GetVolumeInformation(drive, NULL, 0, &vsn, &maxlen, &flags, NULL, 0)) {
-        errno = ENOENT;
-        return -1;
-    }
-    sfs->f_type = flags;
-    sfs->f_bsize = spc * bps;
-    sfs->f_blocks = totalc;
-    sfs->f_bfree = sfs->f_bavail = freec;
-    sfs->f_files = -1;
-    sfs->f_ffree = -1;
-    sfs->f_fsid = vsn;
-    sfs->f_namelen = maxlen;
-    return 0;
-}
-#endif
-
-#if !_SQUID_MINGW_
-int
 WIN32_ftruncate(int fd, off_t size)
 {
     HANDLE hfile;
