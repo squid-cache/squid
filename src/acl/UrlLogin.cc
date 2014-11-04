92d6986ddb6efce85370f6bcf446cdfc53046e9f
Amos Jeffries <squid3@treenet.co.nz>
2014-11-04 00:47:03 -0800

    Bug 1961 part 2: redesign of URL handling.
    Move the HttpRequest::login detail into class URL. Renaming to userInfo
    as per the RFC 3986 defined name for this URI piece.
    
    Convert the details to SBuf gaining several minor str*() removals in
    the process and a simpler FTP login parser based on SBuf capabilities.
    
    Also, updated the base64 encoder API used for converting between URI
    userInfo and Basic authentication header token. Gaining better control
    over the length of maximum token size and a small speedup from pre-known
    input length.

--- src/acl/UrlLogin.cc
+++ src/acl/UrlLogin.cc
@@ -16,13 +16,19 @@
 #include "rfc1738.h"
 
 int
-ACLUrlLoginStrategy::match (ACLData<char const *> * &data, ACLFilledChecklist *checklist, ACLFlags &)
+ACLUrlLoginStrategy::match(ACLData<char const *> * &data, ACLFilledChecklist *checklist, ACLFlags &)
 {
-    char *esc_buf = xstrdup(checklist->request->login);
-    rfc1738_unescape(esc_buf);
-    int result = data->match(esc_buf);
-    safe_free(esc_buf);
-    return result;
+    if (checklist->request->url.userInfo().isEmpty()) {
+        debugs(28, 5, "URL has no user-info details. cannot match");
+        return 0; // nothing can match
+    }
+
+    static char str[MAX_URL]; // should be big enough for a single URI segment
+
+    const SBuf::size_type len = checklist->request->url.userInfo().copy(str, sizeof(str)-1);
+    str[len] = '\0';
+    rfc1738_unescape(str);
+    return data->match(str);
 }
 
 ACLUrlLoginStrategy *
