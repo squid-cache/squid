7d2e9c88354ec39bba5f17a1478214bb86149791
Amos Jeffries <squid3@treenet.co.nz>
2014-11-09 07:02:19 -0800

    Add HTTP Response parser

--- src/http/one/Parser.cc
+++ src/http/one/Parser.cc
@@ -9,6 +9,7 @@
 #include "squid.h"
 #include "Debug.h"
 #include "http/one/Parser.h"
+#include "mime_header.h"
 #include "parser/Tokenizer.h"
 
 /// RFC 7230 section 2.6 - 7 magic octets
@@ -21,6 +22,44 @@ Http::One::Parser::clear()
     buf_ = NULL;
     msgProtocol_ = AnyP::ProtocolVersion();
     mimeHeaderBlock_.clear();
+}
+
+bool
+Http::One::Parser::findMimeBlock(const char *which, size_t limit)
+{
+    if (msgProtocol_.major == 1) {
+        /* NOTE: HTTP/0.9 messages do not have a mime header block.
+         *       So the rest of the code will need to deal with '0'-byte headers
+         *       (ie, none, so don't try parsing em)
+         */
+        int64_t mimeHeaderBytes = 0;
+        // XXX: c_str() reallocates. performance regression.
+        if ((mimeHeaderBytes = headersEnd(buf_.c_str(), buf_.length())) == 0) {
+            if (buf_.length()+firstLineSize() >= limit) {
+                debugs(33, 5, "Too large " << which);
+                parseStatusCode = Http::scHeaderTooLarge;
+                parsingStage_ = HTTP_PARSE_DONE;
+            } else
+                debugs(33, 5, "Incomplete " << which << ", waiting for end of headers");
+            return false;
+        }
+        mimeHeaderBlock_ = buf_.consume(mimeHeaderBytes);
+        debugs(74, 5, "mime header (0-" << mimeHeaderBytes << ") {" << mimeHeaderBlock_ << "}");
+
+    } else
+        debugs(33, 3, "Missing HTTP/1.x identifier");
+
+    // NP: we do not do any further stages here yet so go straight to DONE
+    parsingStage_ = HTTP_PARSE_DONE;
+
+    // Squid could handle these headers, but admin does not want to
+    if (messageHeaderSize() >= limit) {
+        debugs(33, 5, "Too large " << which);
+        parseStatusCode = Http::scHeaderTooLarge;
+        return false;
+    }
+
+    return true;
 }
 
 // arbitrary maximum-length for headers which can be found by Http1Parser::getHeaderField()
---
+++ src/http/one/ResponseParser.cc
@@ -0,0 +1,211 @@
+#include "squid.h"
+#include "Debug.h"
+#include "http/one/ResponseParser.h"
+#include "http/ProtocolVersion.h"
+#include "parser/Tokenizer.h"
+#include "profiler/Profiler.h"
+#include "SquidConfig.h"
+
+const SBuf Http::One::ResponseParser::IcyMagic("ICY ");
+
+Http1::Parser::size_type
+Http::One::ResponseParser::firstLineSize() const
+{
+    Http1::Parser::size_type result = 0;
+
+    switch (msgProtocol_.protocol)
+    {
+    case AnyP::PROTO_HTTP:
+        result += Http1magic.length();
+        break;
+    case AnyP::PROTO_ICY:
+        result += IcyMagic.length();
+        break;
+    default: // no other protocols supported
+        return result;
+    }
+    // NP: the parser does not accept >2 DIGIT for version numbers
+    if (msgProtocol_.minor >10)
+        result += 2;
+    else
+        result += 1;
+
+    result += 5; /* 5 octets in: SP status SP */
+    result += reasonPhrase_.length();
+    return result;
+}
+
+// NP: we found the protocol version and consumed it already.
+// just need the status code and reason phrase
+const int
+Http::One::ResponseParser::parseResponseStatusAndReason()
+{
+    if (buf_.isEmpty())
+        return 0;
+
+    ::Parser::Tokenizer tok(buf_);
+
+    if (!completedStatus_) {
+        SBuf status;
+        // status code is 3 DIGIT octets
+        if(!tok.prefix(status, CharacterSet::DIGIT, 3))
+            return -1; // invalid status
+        // NOTE: multiple SP or non-SP bytes between version and status code are invalid.
+        if (tok.atEnd())
+            return 0; // need more to be sure we have it all
+        if(!tok.skip(' '))
+            return -1; // invalid status, a single SP terminator required
+        // NOTE: any whitespace after the single SP is part of the reason phrase.
+
+        // get the actual numeric value of the 0-3 digits we found
+        ::Parser::Tokenizer t2(status);
+        int64_t statusValue;
+        if (!t2.int64(statusValue))
+            return -1; // ouch. digits not forming a valid number?
+        if (statusValue < 0 || statusValue > 999)
+            return -1; // ouch. digits not within valid status code range.
+
+        statusCode_ = static_cast<Http::StatusCode>(statusValue);
+
+        buf_ = tok.remaining(); // resume checkpoint
+        completedStatus_ = true;
+    }
+
+    if (tok.atEnd())
+        return 0; // need more to be sure we have it all
+
+    /* RFC 7230 says we SHOULD ignore the reason phrase content
+     * but it has a definite valid vs invalid character set.
+     * We interpret the SHOULD as ignoring absence and syntax, but
+     * producing an error if it contains an invalid octet.
+     */
+
+    // if we got here we are still looking for reason-phrase bytes
+    static const CharacterSet phraseChars = CharacterSet::WSP + CharacterSet::VCHAR + CharacterSet::OBSTEXT;
+    tok.prefix(reasonPhrase_, phraseChars); // optional, no error if missing
+    tok.skip('\r'); // optional trailing CR
+
+    if (tok.atEnd())
+        return 0; // need more to be sure we have it all
+
+    // LF existence matters
+    if (!tok.skip('\n')) {
+        reasonPhrase_.clear();
+        return -1; // found invalid characters in the phrase
+    }
+
+    buf_ = tok.remaining(); // resume checkpoint
+    return 1;
+}
+
+const int
+Http::One::ResponseParser::parseResponseFirstLine()
+{
+    ::Parser::Tokenizer tok(buf_);
+
+    if (msgProtocol_.protocol != AnyP::PROTO_NONE) {
+        // we already found the magic, but not the full line. keep going.
+        return parseResponseStatusAndReason();
+
+    } else if (tok.skip(Http1magic)) {
+        // HTTP Response status-line parse
+
+        // magic contains major version, still need to find minor
+        SBuf verMinor;
+        // NP: we limit to 2-digits for speed, there really is no limit
+        // XXX: the protocols we accept dont have valid versions > 10 anyway
+        if (!tok.prefix(verMinor, CharacterSet::DIGIT, 2))
+            return -1; // invalid version minor code
+        if (tok.atEnd())
+            return 0; // need more to be sure we have it all
+        if(!tok.skip(' '))
+            return -1; // invalid version, a single SP terminator required
+
+        // get the actual numeric value of the 0-3 digits we found
+        ::Parser::Tokenizer t2(verMinor);
+        int64_t tvm = 0;
+        if (!t2.int64(tvm))
+            return -1; // ouch. digits not forming a valid number?
+        msgProtocol_.minor = static_cast<unsigned int>(tvm);
+
+        msgProtocol_.protocol = AnyP::PROTO_HTTP;
+        msgProtocol_.major = 1;
+        buf_ = tok.remaining(); // resume checkpoint
+        return parseResponseStatusAndReason();
+
+    } else if (tok.skip(IcyMagic)) {
+        // ICY Response status-line parse (same as HTTP/1 after the magic version)
+        msgProtocol_.protocol = AnyP::PROTO_ICY;
+        // NP: ICY has no /major.minor details
+        buf_ = tok.remaining(); // resume checkpoint
+        return parseResponseStatusAndReason();
+
+    } else if (buf_.length() > Http1magic.length() && buf_.length() > IcyMagic.length()) {
+        // found something that looks like an HTTP/0.9 response
+        msgProtocol_ = Http::ProtocolVersion(1,1);
+        // XXX: probably should use version 0.9 here and upgrade on output,
+        // but the old code did 1.1 transformation now.
+        statusCode_ = Http::scOkay;
+        static const SBuf gatewayPhrase("Gatewaying");
+        reasonPhrase_ = gatewayPhrase;
+        static const SBuf fakeHttpMimeBlock("X-Transformed-From: HTTP/0.9\r\n"
+                                            /* Server: visible_appname_string */
+                                            "Mime-Version: 1.0\r\n"
+                                            /* Date: squid_curtime */
+                                            "Expires: -1\r\n\r\n");
+        return 1; // no more parsing
+    }
+
+    return 0; // need more to parse anything.
+}
+
+bool
+Http::One::ResponseParser::parse(const SBuf &aBuf)
+{
+    buf_ = aBuf;
+    debugs(74, DBG_DATA, "Parse buf={length=" << aBuf.length() << ", data='" << aBuf << "'}");
+
+    // stage 1: locate the status-line
+    if (parsingStage_ == HTTP_PARSE_NONE) {
+        // RFC 7230 explicitly states whether garbage whitespace is to be handled
+        // at each point of the message framing boundaries.
+        // It omits mentioning garbage prior to HTTP Responses.
+        // Therefore, if we receive anything at all treat it as Response message.
+        if (!buf_.isEmpty())
+            parsingStage_ = HTTP_PARSE_FIRST;
+        else
+            return false;
+    }
+
+    // stage 2: parse the status-line
+    if (parsingStage_ == HTTP_PARSE_FIRST) {
+        PROF_start(HttpParserParseReplyLine);
+
+        int retcode = parseResponseFirstLine();
+
+        // first-line (or a look-alike) found successfully.
+        if (retcode > 0)
+            parsingStage_ = HTTP_PARSE_MIME;
+        debugs(74, 5, "status-line: retval " << retcode);
+        debugs(74, 5, "status-line: proto " << msgProtocol_);
+        debugs(74, 5, "status-line: status-code " << statusCode_);
+        debugs(74, 5, "status-line: reason-phrase " << reasonPhrase_);
+        debugs(74, 5, "Parser: bytes processed=" << (aBuf.length()-buf_.length()));
+        PROF_stop(HttpParserParseReplyLine);
+
+        // syntax errors already
+        if (retcode < 0) {
+            parsingStage_ = HTTP_PARSE_DONE;
+            statusCode_ = scInvalidHeader;
+            return false;
+        }
+    }
+
+    // stage 3: locate the mime header block
+    if (parsingStage_ == HTTP_PARSE_MIME) {
+        if (!findMimeBlock("Response", Config.maxReplyHeaderSize))
+            return false;
+    }
+
+    return !needsMoreData();
+}
