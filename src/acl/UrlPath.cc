e5ef8fb2c154eed3d4fab17c7f181fad93ef5748
Amos Jeffries <squid3@treenet.co.nz>
2014-11-13 00:19:06 -0800

    Fix segmentation fault in ACLUrlPathStrategy::match
    CONNECT requests do not contain a path segment. If tested with a
    urlpath_regex ACL will crash Squid via xstrdup() NULL protection.

--- src/acl/UrlPath.cc
+++ src/acl/UrlPath.cc
@@ -42,6 +42,9 @@
 int
 ACLUrlPathStrategy::match (ACLData<char const *> * &data, ACLFilledChecklist *checklist, ACLFlags &)
 {
+    if (!checklist->request->urlpath.size())
+        return -1;
+
     char *esc_buf = xstrdup(checklist->request->urlpath.termedBuf());
     rfc1738_unescape(esc_buf);
     int result = data->match(esc_buf);
