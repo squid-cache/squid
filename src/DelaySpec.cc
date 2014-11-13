ed29d4dec41578608bcc44e29947d5a5c650526e
Amos Jeffries <squid3@treenet.co.nz>
2014-11-12 23:45:12 -0800

    Fix delay_parameters parsing
    Squid-3.4 parser changes resulted in always getting a BUNGLED error when
    parsing delay_parameters. Convert to the new ConfigParser engine.
    
    Add ability to specify 'none' for no-limit instead of -n/*

--- src/DelaySpec.cc
+++ src/DelaySpec.cc
@@ -41,19 +41,31 @@ DelaySpec::dump (StoreEntry *entry) const
 void
 DelaySpec::parse()
 {
-    int r;
-    char *token;
-    token = strtok(NULL, "/");
-
+    // get the token.
+    char *token = ConfigParser::NextToken();
     if (token == NULL)
         self_destruct();
 
-    if (sscanf(token, "%d", &r) != 1)
+    // no-limit value
+    if (strcmp(token, "none") == 0 || token[0] == '-') {
+        restore_bps = -1;
+        max_bytes = -1;
+        return;
+    }
+
+    // parse the first digits into restore_bps
+    const char *p = NULL;
+    if (!StringToInt(token, restore_bps, &p, 10) && *p != '/') {
+        debugs(77, DBG_CRITICAL, "ERROR: invalid delay rate '" << token << "'. Expecting restore/max or 'none'.");
         self_destruct();
+    }
+    p++; // increment past the '/'
 
-    restore_bps = r;
-
-    max_bytes = GetInteger64();
+    // parse the rest into max_bytes
+    if (!StringToInt64(p, max_bytes, NULL, 10)) {
+        debugs(77, DBG_CRITICAL, "ERROR: restore rate in '" << token << "' is not a number.");
+        self_destruct();
+    }
 }
 
 #endif
