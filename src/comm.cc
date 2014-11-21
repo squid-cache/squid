2b9cf0dce3fe3beefff6956cf1ceacffdef9bcef
Amos Jeffries <squid3@treenet.co.nz>
2014-11-21 02:07:05 -0800

    Bug 4146: workaround SSL Bump crash on Linux

--- src/comm.cc
+++ src/comm.cc
@@ -102,9 +102,14 @@ static void
 comm_empty_os_read_buffers(int fd)
 {
 #if _SQUID_LINUX_
+#if USE_OPENSSL
+    // Bug 4146: SSL-Bump BIO does not release sockets on close.
+    if (fd_table[fd].ssl)
+        return;
+#endif
+
     /* prevent those nasty RST packets */
     char buf[SQUID_TCP_SO_RCVBUF];
-
     if (fd_table[fd].flags.nonblocking) {
         while (FD_READ_METHOD(fd, buf, SQUID_TCP_SO_RCVBUF) > 0) {};
     }
