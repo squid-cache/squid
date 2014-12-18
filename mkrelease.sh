412df829714ac03552887d0bf93adc45b92d1063
Amos Jeffries <squid3@treenet.co.nz>
2014-12-18 04:43:32 -0800

    Maintenance: check release notes on packaging

--- mkrelease.sh
+++ mkrelease.sh
@@ -61,6 +61,11 @@ if [ ${name} != ${PACKAGE}-${VERSION} ]; then
 	exit 1
 fi
 RELEASE=`echo $VERSION | cut -d. -f1,2 | cut -d- -f1`
+NOTES_VERSION=`grep "$VERSION" doc/release-notes/release-${RELEASE}.html`
+if test "x$NOTES_VERSION" = "x"; then
+	echo "ERROR! Release Notes HTML version numbers do not match!"
+	exit 1
+fi
 ed -s configure.ac <<EOS
 g/${VERSION}-BZR/ s//${VERSION}/
 w
