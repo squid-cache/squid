edb05a4229f6e26f9cbc44da075c4df17b2a5b47
Markus Moeller <huaraz@moeller.plus.com>
2014-12-19 14:16:42 -0800

    negotiate_kerberos_auth: MEMORY keytab and replay cache support
    1) Checks for MEMORY: keytab support and reads the keytab from disk into
       MEMORY to improve performance (i.e. read keytab only at startup and
       nerver again)
    
    2) Add option for replay cache type. Allows to set replay cache to none
       to improve performance ( may reduce security a bit )
    
    3) Add option for replay cache directory.  If /var/tmp is not the best
       location you can choose a different location.

--- helpers/negotiate_auth/kerberos/negotiate_kerberos.h
+++ helpers/negotiate_auth/kerberos/negotiate_kerberos.h
@@ -116,13 +116,13 @@ static const unsigned char ntlmProtocol[] = {'N', 'T', 'L', 'M', 'S', 'S', 'P', 
 inline const char *
 LogTime()
 {
-    struct tm *tm;
     struct timeval now;
     static time_t last_t = 0;
     static char buf[128];
 
     gettimeofday(&now, NULL);
     if (now.tv_sec != last_t) {
+        struct tm *tm;
         tm = localtime((time_t *) & now.tv_sec);
         strftime(buf, 127, "%Y/%m/%d %H:%M:%S", tm);
         last_t = now.tv_sec;
