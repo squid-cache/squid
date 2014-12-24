6a515ac4393087dd054efca0a7f09d120377d83e
Source Maintenance <squidadm@squid-cache.org>
2014-12-24 12:12:04 +0000

    SourceFormat Enforcement

--- src/comm/Connection.cc
+++ src/comm/Connection.cc
@@ -112,3 +112,4 @@ Comm::Connection::timeLeft(const time_t idleTimeout) const
     const time_t lifeTimeLeft = lifeTime() < Config.Timeout.pconnLifetime ? Config.Timeout.pconnLifetime - lifeTime() : 1;
     return min(lifeTimeLeft, idleTimeout);
 }
+
