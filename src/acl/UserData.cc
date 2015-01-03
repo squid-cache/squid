54f83c1905f2c299c9da90351f33c88b180cd899
Source Maintenance <squidadm@squid-cache.org>
2015-01-03 12:12:04 +0000

    SourceFormat Enforcement

--- src/acl/UserData.cc
+++ src/acl/UserData.cc
@@ -71,7 +71,7 @@ ACLUserData::match(char const *user)
 struct UserDataAclDumpVisitor {
     SBufList contents;
     void operator() (char * const & node_data) {
-       contents.push_back(SBuf(node_data));
+        contents.push_back(SBuf(node_data));
     }
 };
 
