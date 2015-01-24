b06c45a54fc81ab2644cd2165f7b11ba56b85c73
Francesco Chemolli <kinkie@squid-cache.org>
2015-01-24 21:09:41 +0100

    several clang versions do not seem to offer std::vector::cbegin and cend

--- src/acl/Eui64.cc
+++ src/acl/Eui64.cc
@@ -108,7 +108,7 @@ SBufList
 ACLEui64::dump() const
 {
     SBufList sl;
-    for (auto i = eui64Data.cbegin(); i != eui64Data.end(); ++i) {
+    for (auto i = eui64Data.begin(); i != eui64Data.end(); ++i) {
         static char buf[48];
         i->encode(buf,48);
         sl.push_back(SBuf(buf));
