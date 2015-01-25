524f5ff607c606f9dadee6d6194d0e77df5eb4d1
Amos Jeffries <squid3@treenet.co.nz>
2015-01-24 20:53:40 -0800

    Older Clang versions do not seem to offer full C++11 std::splice API

--- src/acl/NoteData.cc
+++ src/acl/NoteData.cc
@@ -64,9 +64,13 @@ ACLNoteData::dump() const
 {
     SBufList sl;
     sl.push_back(SBuf(name));
+#if __cplusplus >= 201103L
+    sl.splice(sl.end(), values->dump());
+#else
     // temp is needed until c++11 move constructor
     SBufList temp = values->dump();
     sl.splice(sl.end(), temp);
+#endif
     return sl;
 }
 
