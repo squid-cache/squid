8e08543ac2fd592cde2bf778674da57b9e4fa061
Amos Jeffries <squid3@treenet.co.nz>
2015-01-31 06:10:25 -0800

    Stop emitting (Proxy-)Authentication-Info for Negotiate
    This header is not defined for use by RFC 4559, and there seem to
    be no clients actually using it.
    
    The syntax Squid was using to emit the details was also clashing
    with the syntax defined for use in Digest which is becoming the
    standardized ABNF syntax for the header in general.

--- src/auth/negotiate/UserRequest.cc
+++ src/auth/negotiate/UserRequest.cc
@@ -390,23 +390,3 @@ Auth::Negotiate::UserRequest::HandleReply(void *data, const Helper::Reply &reply
     r->handler(r->data);
     delete r;
 }
-
-void
-Auth::Negotiate::UserRequest::addAuthenticationInfoHeader(HttpReply * rep, int accel)
-{
-    http_hdr_type type;
-
-    if (!server_blob)
-        return;
-
-    /* don't add to authentication error pages */
-    if ((!accel && rep->sline.status() == Http::scProxyAuthenticationRequired)
-            || (accel && rep->sline.status() == Http::scUnauthorized))
-        return;
-
-    type = accel ? HDR_AUTHENTICATION_INFO : HDR_PROXY_AUTHENTICATION_INFO;
-    httpHeaderPutStrf(&rep->header, type, "Negotiate %s", server_blob);
-
-    safe_free(server_blob);
-}
-
