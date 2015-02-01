f6df4b86ecb259e5794a4a3a4b22753ace41b585
Amos Jeffries <squid3@treenet.co.nz>
2015-02-01 01:42:12 -0800

    Remove dst ACL dependency on HTTP request message existence
    The ACL checklist dst_addr member can be used in cases where the HTTP
    message is not provided. Such as ssl_bump, ICAP outgoing IP, or peer
    selection probes.

--- src/acl/DestinationIp.h
+++ src/acl/DestinationIp.h
@@ -58,7 +58,6 @@ public:
     ACLDestinationIP(): ACLIP(ACLDestinationIP::SupportedFlags) {}
     virtual char const *typeString() const;
     virtual int match(ACLChecklist *checklist);
-    virtual bool requiresRequest() const {return true;}
 
     virtual ACL *clone()const;
 
