bdfd6358d79d056729dca37af463ab31772ab04b
Amos Jeffries <squid3@treenet.co.nz>
2015-02-02 10:51:25 -0800

    Cleanup: migrate acl_tos and acl_nfmark to CBDATA_CLASS API
    Also a little documentation and syntax polishing.

--- src/ip/QosConfig.h
+++ src/ip/QosConfig.h
@@ -24,21 +24,28 @@
 class fde;
 
 // TODO: move to new ACL framework
-// not integrated in namespace, as this class uses a strange CBDATA definition
-// POD
 class acl_tos
 {
+    CBDATA_CLASS(acl_tos);
+
 public:
+    acl_tos() : next(NULL), aclList(NULL) {}
+    ~acl_tos();
+
     acl_tos *next;
     ACLList *aclList;
     tos_t tos;
 };
+
 // TODO: move to new ACL framework
-// not integrated in namespace, as this class uses a strange CBDATA definition
-// POD
 class acl_nfmark
 {
+    CBDATA_CLASS(acl_nfmark);
+
 public:
+    acl_nfmark() : next(NULL), aclList(NULL) {}
+    ~acl_nfmark();
+
     acl_nfmark *next;
     ACLList *aclList;
     nfmark_t nfmark;
