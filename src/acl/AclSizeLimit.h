2ed55c2fa4bf7fc0dde31cd8bd2199f53bea5caf
Source Maintenance <squidadm@squid-cache.org>
2015-02-03 00:12:07 +0000

    SourceFormat Enforcement

--- src/acl/AclSizeLimit.h
+++ src/acl/AclSizeLimit.h
@@ -19,7 +19,7 @@ class AclSizeLimit
 
 public:
     AclSizeLimit() : next(NULL), aclList(NULL), size(0) {}
-   ~AclSizeLimit();
+    ~AclSizeLimit();
 
     AclSizeLimit *next;
     ACLList *aclList;
