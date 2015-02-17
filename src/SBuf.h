fc86e0486f161080fe4aad3750994780bcb076da
Francesco Chemolli <kinkie@squid-cache.org>
2015-02-17 18:46:12 +0100

    Interim: implement move constructor and tryto rework SBuf::compare

--- src/SBuf.h
+++ src/SBuf.h
@@ -135,8 +135,7 @@ public:
     /// create an empty (zero-size) SBuf
     SBuf();
     SBuf(const SBuf &S);
-    SBuf(SBuf&& S) {
-        store_=S.store_; off_=S.off_; len_=S.len_;
+    SBuf(SBuf&& S) : store_(std::move(S.store_)), off_(S.off_), len_(S.len_) {
         S.store_=GetStorePrototype(); S.off_=0; S.len_=0;
     }
 
