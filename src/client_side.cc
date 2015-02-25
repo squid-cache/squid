29177d830d7445b0ac59d895c752d16a48609f9f
Christos Tsantilas <chtsanti@users.sourceforge.net>
2015-02-25 18:22:15 +0200

    Fake CONNECT exceeds concurrent requests limit part2: wrap Openssl related code inside "#if USE_OPENSSL ..."

--- src/client_side.cc
+++ src/client_side.cc
@@ -2741,7 +2741,11 @@ ConnStateData::concurrentRequestQueueFilled() const
 
     // default to the configured pipeline size.
     // add 1 because the head of pipeline is counted in concurrent requests and not prefetch queue
+#if USE_OPENSSL
     const int internalRequest = (transparent() && sslBumpMode == Ssl::bumpSplice) ? 1 : 0;
+#else
+   const int internalRequest = 0;
+#endif
     const int concurrentRequestLimit = pipelinePrefetchMax() + 1 + internalRequest;
 
     // when queue filled already we cant add more.
