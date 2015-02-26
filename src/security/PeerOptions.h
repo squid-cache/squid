dc28c059a7fee4661db7cfae1b27847d33525561
Source Maintenance <squidadm@squid-cache.org>
2015-02-26 12:12:11 +0000

    SourceFormat Enforcement

--- src/security/PeerOptions.h
+++ src/security/PeerOptions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1996-2014 The Squid Software Foundation and contributors
+ * Copyright (C) 1996-2015 The Squid Software Foundation and contributors
  *
  * Squid software is distributed under GPLv2+ license and includes
  * contributions from numerous individuals and organizations.
@@ -61,3 +61,4 @@ void parse_securePeerOptions(Security::PeerOptions *);
 #define dump_securePeerOptions(e,n,x) // not supported yet
 
 #endif /* SQUID_SRC_SECURITY_PEEROPTIONS_H */
+
