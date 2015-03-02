f66a8e356b4067be009cbd62251d2890b5caea58
Source Maintenance <squidadm@squid-cache.org>
2015-03-02 18:12:08 +0000

    SourceFormat Enforcement

--- src/ssl/bio.cc
+++ src/ssl/bio.cc
@@ -151,11 +151,11 @@ bool
 Ssl::ClientBio::isClientHello(int state)
 {
     return (
-            state == SSL3_ST_SR_CLNT_HELLO_A ||
-            state == SSL23_ST_SR_CLNT_HELLO_A ||
-            state == SSL23_ST_SR_CLNT_HELLO_B ||
-            state == SSL3_ST_SR_CLNT_HELLO_B ||
-            state == SSL3_ST_SR_CLNT_HELLO_C
+               state == SSL3_ST_SR_CLNT_HELLO_A ||
+               state == SSL23_ST_SR_CLNT_HELLO_A ||
+               state == SSL23_ST_SR_CLNT_HELLO_B ||
+               state == SSL3_ST_SR_CLNT_HELLO_B ||
+               state == SSL3_ST_SR_CLNT_HELLO_C
            );
 }
 
