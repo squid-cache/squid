0af01cb474bd4d4628598b709e87933e09217f90
Amos Jeffries <squid3@treenet.co.nz>
2015-03-03 08:51:03 -0800

    Rename MemBuf::vPrintf to MemBuf::vappendf
    ... inline with the Packable API method name

--- src/tests/stub_MemBuf.cc
+++ src/tests/stub_MemBuf.cc
@@ -25,7 +25,7 @@ void MemBuf::clean() STUB
 void MemBuf::reset() STUB
 int MemBuf::isNull() STUB_RETVAL(1)
 void MemBuf::Printf(const char *fmt,...) STUB
-void MemBuf::vPrintf(const char *fmt, va_list ap) STUB
+void MemBuf::vappendf(const char *fmt, va_list ap) STUB
 FREE *MemBuf::freeFunc() STUB_RETVAL(NULL)
 
 void memBufReport(MemBuf * mb) STUB
