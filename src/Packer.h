8fa4dcaec0fb528eecdeb949be38d03c75a092b6
Amos Jeffries <squid3@treenet.co.nz>
2015-03-02 23:46:18 -0800

    Shuffle packerPrintf() to be Packer::Printf method

--- src/Packer.h
+++ src/Packer.h
@@ -24,15 +24,20 @@ class Packer
 
 public:
     virtual ~Packer();
+
     virtual void append(const char *buf, int size);
+
+    /*
+     * \note  we use Printf instead of printf so the compiler won't
+     *        think we're calling the libc printf()
+     */
+    virtual void Printf(const char *fmt,...) PRINTF_FORMAT_ARG2;
 
     /* protected, use interface functions instead */
     append_f append_;
     vprintf_f packer_vprintf;
     void *real_handler;     /* first parameter to real append and vprintf */
 };
-
-void packerPrintf(Packer * p, const char *fmt,...) PRINTF_FORMAT_ARG2;
 
 #endif /* SQUID_PACKER_H */
 
