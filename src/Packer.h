8afb977555bee6988a0781f46fef748f91cf7ab5
Amos Jeffries <squid3@treenet.co.nz>
2015-03-03 06:06:31 -0800

    Update Packable API to implement vappendf() method
    The backend classes actually implement the vappendf()
    base function receiving va_list args object.
    
    The Printf() implementations are all duplicate code. So
    provide that function in terms of a virtual vappendf().
    
    vappendf() is incidentally the name used by SBuf API for
    this operation and avoids the global vprintf() libc
    definition.

--- src/Packer.h
+++ src/Packer.h
@@ -29,7 +29,7 @@ public:
 
     /* Packable API */
     virtual void append(const char *buf, int size);
-    virtual void Printf(const char *fmt,...) PRINTF_FORMAT_ARG2;
+    virtual void vappendf(const char *fmt, va_list ap);
 
     /* protected, use interface functions instead */
     append_f append_;
