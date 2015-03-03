2e159d85b5ca5842fb939a15ea221bed2e204158
Christos Tsantilas <chtsanti@users.sourceforge.net>
2015-03-03 06:45:57 -0800

    Fake CONNECT exceeds concurrent requests limit.
    Squid closes the SSL client connection with "Failed to start fake CONNECT 
    request for ssl spliced connection". This happens especially often when
    the pipeline_prefetch configuration parameter is set to "0" (i.e., default).
    
    When a transparent SSL connection is peeked and then spliced in step2, we are
    generating a fake CONNECT request. The fake CONNECT request is counted as a
    new pipelined request and may exceed the configured limit. This patch solves
    this problem by raising the limit for that request.
    
    Needs more work to better identify the requests that need a different limit.
    
    This is a Measurement Factory project.

--- src/client_side.cc
+++ src/client_side.cc
@@ -2874,7 +2874,12 @@ ConnStateData::concurrentRequestQueueFilled() const
 
     // default to the configured pipeline size.
     // add 1 because the head of pipeline is counted in concurrent requests and not prefetch queue
-    const int concurrentRequestLimit = pipelinePrefetchMax() + 1;
+#if USE_OPENSSL
+    const int internalRequest = (transparent() && sslBumpMode == Ssl::bumpSplice) ? 1 : 0;
+#else
+    const int internalRequest = 0;
+#endif
+    const int concurrentRequestLimit = pipelinePrefetchMax() + 1 + internalRequest;
 
     // when queue filled already we cant add more.
     if (existingRequestCount >= concurrentRequestLimit) {
