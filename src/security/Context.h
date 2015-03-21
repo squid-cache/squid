ad5c60427a1609a1b1755ef3e2a17c802430e004
Amos Jeffries <squid3@treenet.co.nz>
2015-03-21 04:58:32 -0700

    Define Security::ContextPointer for GnuTLS

--- src/security/Context.h
+++ src/security/Context.h
@@ -10,14 +10,24 @@
 #define SQUID_SRC_SECURITY_CONTEXT_H
 
 #if USE_OPENSSL
-#include "ssl/gadgets.h"
+#if HAVE_OPENSSL_SSL_H
+#include <openssl/ssl.h>
+#endif
+#endif
+
+#if USE_GNUTLS
+#if HAVE_GNUTLS_GNUTLS_H
+#include <gnutls/gnutls.h>
+#endif
 #endif
 
 namespace Security {
 
 #if USE_OPENSSL
-// XXX: make this a SSL_CTX_Pointer
 typedef SSL_CTX* ContextPointer;
+
+#elif USE_GNUTLS
+typedef gnutls_certificate_credentials_t* ContextPointer;
 
 #else
 // use void* so we can check against NULL
