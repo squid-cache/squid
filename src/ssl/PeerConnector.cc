07e6d76ea0db1d2376afa03d7c487aacf08a4354
Amos Jeffries <squid3@treenet.co.nz>
2015-04-27 04:16:32 -0700

    Update libsquidssl.la MemBuf use

--- src/ssl/PeerConnector.cc
+++ src/ssl/PeerConnector.cc
@@ -734,12 +734,12 @@ Ssl::PeerConnector::status() const
     // id and stop reason reporting duplication.
     buf.append(" [", 2);
     if (stopReason != NULL) {
-        buf.Printf("Stopped, reason:");
-        buf.Printf("%s",stopReason);
+        buf.append("Stopped, reason:", 16);
+        buf.appendf("%s",stopReason);
     }
     if (serverConn != NULL)
-        buf.Printf(" FD %d", serverConn->fd);
-    buf.Printf(" %s%u]", id.Prefix, id.value);
+        buf.appendf(" FD %d", serverConn->fd);
+    buf.appendf(" %s%u]", id.Prefix, id.value);
     buf.terminate();
 
     return buf.content();
