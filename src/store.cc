3e1ddfcec159d87adfcfe9d1cada58bc6a2fde93
Amos Jeffries <squid3@treenet.co.nz>
2015-04-27 07:39:04 -0700

    Add vsnprintf() protection for vargs

--- src/store.cc
+++ src/store.cc
@@ -872,11 +872,26 @@ StoreEntry::vappendf(const char *fmt, va_list vargs)
     *buf = 0;
     int x;
 
+#ifdef VA_COPY
+    va_args ap;
+    /* Fix of bug 753r. The value of vargs is undefined
+     * after vsnprintf() returns. Make a copy of vargs
+     * incase we loop around and call vsnprintf() again.
+     */
+    VA_COPY(ap,vargs);
+    errno = 0;
+    if ((x = vsnprintf(buf, sizeof(buf), fmt, ap)) < 0) {
+        fatalf( xstrerror(errno));
+        return;
+    }
+    va_end(ap);
+#else /* VA_COPY */
     errno = 0;
     if ((x = vsnprintf(buf, sizeof(buf), fmt, vargs)) < 0) {
         fatalf( xstrerror(errno));
         return;
     }
+#endif /*VA_COPY*/
 
     if (x < sizeof(buf)) {
         append(buf, x);
