0f7f4cfc3198f4ad71c241c958a63cd554144cf8
Amos Jeffries <amosjeffries@squid-cache.org>
2015-04-30 23:31:01 -0700

    Fix X509 server certificate domain matching
    The X509 certificate domain fields may contain non-ASCII encodings.
    Ensure the domain match algorithm is only passed UTF-8 ASCII-compatible
    strings.

--- src/acl/ServerName.cc
+++ src/acl/ServerName.cc
@@ -71,7 +71,13 @@ check_cert_domain( void *check_data, ASN1_STRING *cn_data)
     if (cn_data->length > (int)sizeof(cn) - 1)
         return 1; // ignore data that does not fit our buffer
 
-    memcpy(cn, cn_data->data, cn_data->length);
+    char *s = reinterpret_cast<char *>(cn_data->data);
+    char *d = cn;
+    for (int i = 0; i < cn_data->length; ++i, ++d, ++s) {
+        if (*s == '\0')
+            return 1; // always a domain mismatch. contains 0x00
+        *d = *s;
+    }
     cn[cn_data->length] = '\0';
     debugs(28, 4, "Verifying certificate name/subjectAltName " << cn);
     if (data->match(cn))
