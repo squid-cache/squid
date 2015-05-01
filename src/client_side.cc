076711e595b18e71ab5fc74e7bf4cdce1f0d7ea5
Christos Tsantilas <chtsanti@users.sourceforge.net>
2015-04-30 23:43:48 -0700

    Bug 3775: Disable HTTP/1.1 pipeline feature for pinned connections

--- src/client_side.cc
+++ src/client_side.cc
@@ -2860,6 +2860,9 @@ connStripBufferWhitespace (ConnStateData * conn)
 int
 ConnStateData::pipelinePrefetchMax() const
 {
+    // TODO: Support pipelined requests through pinned connections.
+    if (pinning.pinned)
+        return 0;
     return Config.pipeline_max_prefetch;
 }
 
