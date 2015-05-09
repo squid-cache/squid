feef21bbfd1fc09431458abc1f40c0733898d77f
Amos Jeffries <squid3@treenet.co.nz>
2015-05-09 04:43:14 -0700

    CacheMgr: display 'client_db off' instead of 0 clients accessing cache
    ... to clarify why there is no record of even the mgr request happening.
    The client_db mechanism needs to be enabled and measuring traffic for
    any useful client counter value to exist.

--- src/stat.cc
+++ src/stat.cc
@@ -620,8 +620,10 @@ DumpInfo(Mgr::InfoActionData& stats, StoreEntry* sentry)
 
     storeAppendPrintf(sentry, "Connection information for %s:\n",APP_SHORTNAME);
 
-    storeAppendPrintf(sentry, "\tNumber of clients accessing cache:\t%.0f\n",
-                      stats.client_http_clients);
+    if (Config.onoff.client_db)
+        storeAppendPrintf(sentry, "\tNumber of clients accessing cache:\t%.0f\n", stats.client_http_clients);
+    else
+        sentry->append("\tNumber of clients accessing cache:\t(client_db off)\n", 52);
 
     storeAppendPrintf(sentry, "\tNumber of HTTP requests received:\t%.0f\n",
                       stats.client_http_requests);
