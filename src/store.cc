bc611eac8a64314f72f12477d1b079d1c31f2a10
Amos Jeffries <squid3@treenet.co.nz>
2015-05-18 08:51:14 -0700

    Fix build errors in rev.14057 and rev.14058

--- src/store.cc
+++ src/store.cc
@@ -881,19 +881,19 @@ StoreEntry::vappendf(const char *fmt, va_list vargs)
     VA_COPY(ap,vargs);
     errno = 0;
     if ((x = vsnprintf(buf, sizeof(buf), fmt, ap)) < 0) {
-        fatalf( xstrerror(errno));
+        fatal(xstrerr(errno));
         return;
     }
     va_end(ap);
 #else /* VA_COPY */
     errno = 0;
     if ((x = vsnprintf(buf, sizeof(buf), fmt, vargs)) < 0) {
-        fatalf( xstrerror(errno));
+        fatal(xstrerr(errno));
         return;
     }
 #endif /*VA_COPY*/
 
-    if (x < sizeof(buf)) {
+    if (x < static_cast<int>(sizeof(buf))) {
         append(buf, x);
         return;
     }
