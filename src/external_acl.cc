93d4ce749e69a0deaf84b203b8bfee2a60dcdc33
Nathan Hoad <nathan@getoffmalawn.com>
2015-05-21 21:26:17 -0700

    Fix missing external ACL helper notes
    external ACL helper notes are only added onto the HTTP request that
    kicked off the external ACL lookup, and not cached ACL responses.
    This means if you set notes from an external ACL that are used for
    some processing in other ACLs, or post-processing on logs, things
    may be missed.

--- src/external_acl.cc
+++ src/external_acl.cc
@@ -706,6 +706,9 @@ copyResultsFromEntry(HttpRequest *req, const ExternalACLEntryPointer &entry)
 
         if (entry->message.size())
             req->extacl_message = entry->message;
+
+        // attach the helper kv-pair to the transaction
+        UpdateRequestNotes(req->clientConnectionManager.get(), *req, entry->notes);
     }
 }
 
@@ -1532,18 +1535,6 @@ ExternalACLLookup::LookupDone(void *data, const ExternalACLEntryPointer &result)
 {
     ACLFilledChecklist *checklist = Filled(static_cast<ACLChecklist*>(data));
     checklist->extacl_entry = result;
-
-    // attach the helper kv-pair to the transaction
-    if (checklist->extacl_entry != NULL) {
-        if (HttpRequest * req = checklist->request) {
-            // XXX: we have no access to the transaction / AccessLogEntry so cant SyncNotes().
-            // workaround by using anything already set in HttpRequest
-            // OR use new and rely on a later Sync copying these to AccessLogEntry
-
-            UpdateRequestNotes(checklist->conn(), *req, checklist->extacl_entry->notes);
-        }
-    }
-
     checklist->resumeNonBlockingCheck(ExternalACLLookup::Instance());
 }
 
