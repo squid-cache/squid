f680026f939cb3031a49d83b578972b451275609
Source Maintenance <squidadm@squid-cache.org>
2015-05-26 18:12:08 +0000

    SourceFormat Enforcement

--- src/urn.cc
+++ src/urn.cc
@@ -368,9 +368,9 @@ urnHandleReply(void *data, StoreIOBuffer result)
     mb = new MemBuf;
     mb->init();
     mb->appendf( "<TITLE>Select URL for %s</TITLE>\n"
-                "<STYLE type=\"text/css\"><!--BODY{background-color:#ffffff;font-family:verdana,sans-serif}--></STYLE>\n"
-                "<H2>Select URL for %s</H2>\n"
-                "<TABLE BORDER=\"0\" WIDTH=\"100%%\">\n", e->url(), e->url());
+                 "<STYLE type=\"text/css\"><!--BODY{background-color:#ffffff;font-family:verdana,sans-serif}--></STYLE>\n"
+                 "<H2>Select URL for %s</H2>\n"
+                 "<TABLE BORDER=\"0\" WIDTH=\"100%%\">\n", e->url(), e->url());
 
     for (i = 0; i < urlcnt; ++i) {
         u = &urls[i];
