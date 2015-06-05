7161073e16b945ac81e791c09e2a39607bf970c9
Amos Jeffries <squid3@treenet.co.nz>
2015-06-05 16:45:06 -0700

    Cleanup after rev.14102

--- src/ssl/ErrorDetail.cc
+++ src/ssl/ErrorDetail.cc
@@ -433,8 +433,7 @@ const char  *Ssl::ErrorDetail::subject() const
 {
     if (broken_cert.get()) {
         static char tmpBuffer[256]; // A temporary buffer
-        if (X509_NAME_oneline(X509_get_subject_name(broken_cert.get()), tmpBuffer,
-                              sizeof(tmpBuffer)))
+        if (X509_NAME_oneline(X509_get_subject_name(broken_cert.get()), tmpBuffer, sizeof(tmpBuffer)))
             return tmpBuffer;
     }
     return "[Not available]";
