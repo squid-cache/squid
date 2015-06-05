59aac415fa8e02704a8cdde9ec01772c1d8fb12e
Amos Jeffries <squid3@treenet.co.nz>
2015-06-04 22:56:36 -0700

    SourceLayout: convert helper stats display to Packable API
    Requires unifying the classes Helper::Request queues which incidentally
    also brings stateful helpers closer to concurrency support

--- src/external_acl.cc
+++ src/external_acl.cc
@@ -1464,7 +1464,8 @@ externalAclStats(StoreEntry * sentry)
     for (external_acl *p = Config.externalAclHelperList; p; p = p->next) {
         storeAppendPrintf(sentry, "External ACL Statistics: %s\n", p->name);
         storeAppendPrintf(sentry, "Cache size: %d\n", p->cache->count);
-        helperStats(sentry, p->theHelper);
+        assert(p->theHelper);
+        p->theHelper->packStatsInto(sentry);
         storeAppendPrintf(sentry, "\n");
     }
 }
