45cc995f05157a8e5b6d963e610e7b8dccfd52e3
Francesco Chemolli <kinkie@squid-cache.org>
2015-07-09 12:00:01 +0200

    Add default constructor for StoreRebuildData (CID 740560)

--- src/store_rebuild.h
+++ src/store_rebuild.h
@@ -15,6 +15,12 @@
 class StoreRebuildData
 {
 public:
+    StoreRebuildData() :
+        objcount(0), expcount(0), scancount(0), clashcount(0),
+        dupcount(0), cancelcount(0), invalid(0), badflags(0),
+        bad_log_op(0), zero_object_sz(0)
+    {}
+
     int objcount;       /* # objects successfully reloaded */
     int expcount;       /* # objects expired */
     int scancount;      /* # entries scanned or read from state file */
