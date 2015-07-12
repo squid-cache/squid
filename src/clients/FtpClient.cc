bc0e26f5201d99d90442ff3d5b3c139f5c958b9c
Francesco Chemolli <kinkie@squid-cache.org>
2015-07-12 19:43:26 +0200

    Comment out dead code in FtpClient.cc (CID 1231357)

--- src/clients/FtpClient.cc
+++ src/clients/FtpClient.cc
@@ -509,8 +509,9 @@ Ftp::Client::handleEpsvReply(Ip::Address &remoteAddr)
             debugs(9, DBG_IMPORTANT, "WARNING: Server at " << ctrl.conn->remote << " sent unknown protocol negotiation hint: " << buf);
             return sendPassive();
         }
-        failed(ERR_FTP_FAILURE, 0);
-        return false;
+        /* not reached */
+        // failed(ERR_FTP_FAILURE, 0);
+        // return false;
     }
 
     /*  229 Entering Extended Passive Mode (|||port|) */
