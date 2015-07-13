6b38abef1c669d5935a52e7a55ab10dcaad23591
Source Maintenance <squidadm@squid-cache.org>
2015-07-13 12:12:09 +0000

    SourceFormat Enforcement

--- src/LogTags.cc
+++ src/LogTags.cc
@@ -11,30 +11,30 @@
 
 // old deprecated tag strings
 const char * LogTags::Str_[] = {
-	"TAG_NONE",
-	"TCP_HIT",
-	"TCP_MISS",
-	"TCP_REFRESH_UNMODIFIED",
-	"TCP_REFRESH_FAIL_OLD",
-	"TCP_REFRESH_FAIL_ERR",
-	"TCP_REFRESH_MODIFIED",
-	"TCP_CLIENT_REFRESH_MISS",
-	"TCP_IMS_HIT",
-	"TCP_SWAPFAIL_MISS",
-	"TCP_NEGATIVE_HIT",
-	"TCP_MEM_HIT",
-	"TCP_DENIED",
-	"TCP_DENIED_REPLY",
-	"TCP_OFFLINE_HIT",
-	"TCP_REDIRECT",
-	"TCP_TUNNEL",
-	"UDP_HIT",
-	"UDP_MISS",
-	"UDP_DENIED",
-	"UDP_INVALID",
-	"UDP_MISS_NOFETCH",
-	"ICP_QUERY",
-	"TYPE_MAX"
+    "TAG_NONE",
+    "TCP_HIT",
+    "TCP_MISS",
+    "TCP_REFRESH_UNMODIFIED",
+    "TCP_REFRESH_FAIL_OLD",
+    "TCP_REFRESH_FAIL_ERR",
+    "TCP_REFRESH_MODIFIED",
+    "TCP_CLIENT_REFRESH_MISS",
+    "TCP_IMS_HIT",
+    "TCP_SWAPFAIL_MISS",
+    "TCP_NEGATIVE_HIT",
+    "TCP_MEM_HIT",
+    "TCP_DENIED",
+    "TCP_DENIED_REPLY",
+    "TCP_OFFLINE_HIT",
+    "TCP_REDIRECT",
+    "TCP_TUNNEL",
+    "UDP_HIT",
+    "UDP_MISS",
+    "UDP_DENIED",
+    "UDP_INVALID",
+    "UDP_MISS_NOFETCH",
+    "ICP_QUERY",
+    "TYPE_MAX"
 };
 
 /*
@@ -75,3 +75,4 @@ LogTags::isTcpHit() const
         (oldType == LOG_TCP_MEM_HIT) ||
         (oldType == LOG_TCP_OFFLINE_HIT);
 }
+
