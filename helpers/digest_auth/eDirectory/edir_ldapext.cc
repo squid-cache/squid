dd177d6441f37887ed62a0c10f554abe85b91c98
Francesco Chemolli <kinkie@squid-cache.org>
2015-07-26 22:24:16 +0200

    Implemented review change requests to edir_ldapext

--- helpers/digest_auth/eDirectory/edir_ldapext.cc
+++ helpers/digest_auth/eDirectory/edir_ldapext.cc
@@ -155,9 +155,6 @@ static int berEncodeLoginData(
     size_t   putDataLen,
     void     *putData)
 {
-    int presult = 0;
-    BerElement *requestBer = NULL;
-
     unsigned int elemCnt = methodIDLen / sizeof(unsigned int);
 
     char    *utf8ObjPtr=NULL;
@@ -173,48 +170,46 @@ static int berEncodeLoginData(
     utf8TagSize = strlen(utf8TagPtr)+1;
 
     /* Allocate a BerElement for the request parameters. */
-    requestBer = ber_alloc();
-    if (requestBer == NULL)
+    BerElement *requestBer = ber_alloc();
+    if (!requestBer)
         return LDAP_ENCODING_ERROR;
 
     /* BER encode the NMAS Version and the objectDN */
-    presult = ber_printf(requestBer, "{io", NMAS_LDAP_EXT_VERSION, utf8ObjPtr, utf8ObjSize);
-    if (presult < 0) {
+    if (ber_printf(requestBer, "{io", NMAS_LDAP_EXT_VERSION, utf8ObjPtr, utf8ObjSize) < 0) {
         ber_free(requestBer, 1);
         return LDAP_ENCODING_ERROR;
     }
 
     /* BER encode the MethodID Length and value */
-    presult = ber_printf(requestBer, "{i{", methodIDLen);
-    if (presult < 0) {
+    if (ber_printf(requestBer, "{i{", methodIDLen) < 0) {
         ber_free(requestBer, 1);
         return LDAP_ENCODING_ERROR;
     }
 
     for (unsigned int i = 0; i < elemCnt; ++i) {
-        presult = ber_printf(requestBer, "i", methodID[i]);
-        if (presult < 0) {
+        if (ber_printf(requestBer, "i", methodID[i]) < 0) {
             ber_free(requestBer, 1);
             return LDAP_ENCODING_ERROR;
         }
     }
 
-    presult = ber_printf(requestBer, "}}", 0);
-    if (presult < 0) {
+    if (ber_printf(requestBer, "}}", 0) < 0) {
         ber_free(requestBer, 1);
         return LDAP_ENCODING_ERROR;
     }
 
     if (putData) {
         /* BER Encode the the tag and data */
-        presult = ber_printf(requestBer, "oio}", utf8TagPtr, utf8TagSize, putDataLen, putData, putDataLen);
+        if (ber_printf(requestBer, "oio}", utf8TagPtr, utf8TagSize, putDataLen, putData, putDataLen) < 0) {
+            ber_free(requestBer, 1);
+            return LDAP_ENCODING_ERROR;
+        }
     } else {
         /* BER Encode the the tag */
-        presult = ber_printf(requestBer, "o}", utf8TagPtr, utf8TagSize);
-    }
-    if (presult < 0) {
-        ber_free(requestBer, 1);
-        return LDAP_ENCODING_ERROR;
+        if (ber_printf(requestBer, "o}", utf8TagPtr, utf8TagSize) < 0) {
+            ber_free(requestBer, 1);
+            return LDAP_ENCODING_ERROR;
+        }
     }
 
     /* Convert the BER we just built to a berval that we'll send with the extended request. */
@@ -224,7 +219,6 @@ static int berEncodeLoginData(
     }
 
     ber_free(requestBer, 1);
-
     return 0; /* no error */
 }
 
