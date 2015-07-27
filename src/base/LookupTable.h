498598f761e9e64cb78fcc6ac8cebe8c76cb3d9a
Francesco Chemolli <kinkie@squid-cache.org>
2015-07-27 16:53:51 +0200

    Migrate to generic LookupTable, document it and clean it up

--- src/base/LookupTable.h
+++ src/base/LookupTable.h
@@ -6,12 +6,26 @@
 #include <map>
 
 /**
- * SBuf -> enum lookup table.
+ * SBuf -> enum lookup table
+ *
+ * How to use:
+ * enum enum_type { ... };
+ * static const LookupTable<enum_type>::Record initializerTable[] {
+ *   {"key1", ENUM_1}, {"key2", ENUM_2}, ... {nullptr, ENUM_INVALID_VALUE}
+ * };
+ * LookupTable<enum_type> lookupTableInstance(ENUM_INVALID_VALUE, initializerTable);
+ *
+ * then in the code:
+ * SBuf s(string_to_lookup);
+ * enum_type item = lookupTableInstance.lookup(s);
+ * if (item != ENUM_INVALID_VALUE) { // do stuff }
+ *
  */
-template<class EnumType>
+template<typename EnumType>
 class LookupTable
 {
 public:
+    /// element of the lookup table initialization list
     typedef struct {
         const char *name;
         EnumType id;
