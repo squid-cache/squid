31ca1e650a129a07782d0c78c6169ed57242edb1
Francesco Chemolli <kinkie@squid-cache.org>
2015-08-06 20:55:29 +0200

    Remove always-true checks in HttpHdrCc (CID 1315405, 1315406)

--- src/HttpHdrCc.cc
+++ src/HttpHdrCc.cc
@@ -293,7 +293,7 @@ std::ostream &
 operator<< (std::ostream &s, HttpHdrCcType c)
 {
     const unsigned char ic = static_cast<int>(c);
-    if (c >= HttpHdrCcType::CC_PUBLIC && c < HttpHdrCcType::CC_ENUM_END)
+    if (c < HttpHdrCcType::CC_ENUM_END)
         s << CcAttrs[ic].name << '[' << ic << ']' ;
     else
         s << "*invalid hdrcc* [" << ic << ']';
