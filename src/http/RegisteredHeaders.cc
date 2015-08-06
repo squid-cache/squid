921c8e9a35dc1345c0509129f55dc2cb3ac82bf6
Source Maintenance <squidadm@squid-cache.org>
2015-08-06 12:12:11 +0000

    SourceFormat Enforcement

--- src/http/RegisteredHeaders.cc
+++ src/http/RegisteredHeaders.cc
@@ -124,9 +124,10 @@ const LookupTable<Http::HdrType, HeaderTableRecord> HeaderLookupTable(Http::HdrT
 std::ostream&
 operator<< (std::ostream &s, Http::HdrType id)
 {
-	if (Http::any_HdrType_enum_value(id))
-		s << Http::HeaderTable[id].name << '[' << static_cast<int>(id) << ']';
-	else
-		s << "Invalid-Header[" << static_cast<int>(id) << ']';
-	return s;
+    if (Http::any_HdrType_enum_value(id))
+        s << Http::HeaderTable[id].name << '[' << static_cast<int>(id) << ']';
+    else
+        s << "Invalid-Header[" << static_cast<int>(id) << ']';
+    return s;
 }
+
