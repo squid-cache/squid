c1677ab199a367399e275935e7446341b8929391
Source Maintenance <squidadm@squid-cache.org>
2015-08-13 00:12:11 +0000

    SourceFormat Enforcement

--- src/parser/Tokenizer.cc
+++ src/parser/Tokenizer.cc
@@ -194,7 +194,7 @@ Parser::Tokenizer::skipAllTrailing(const CharacterSet &skippable)
 {
     const SBuf::size_type prefixEnd = buf_.findLastNotOf(skippable);
     const SBuf::size_type prefixLen = prefixEnd == SBuf::npos ?
-        0 : (prefixEnd + 1);
+                                      0 : (prefixEnd + 1);
     const SBuf::size_type suffixLen = buf_.length() - prefixLen;
     if (suffixLen == 0) {
         debugs(24, 8, "no match when trying to skip " << skippable.name);
