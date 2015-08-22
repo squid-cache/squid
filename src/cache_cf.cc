e3074758b0136489e2d934733b8cc0f99805c907
Amos Jeffries <squid3@treenet.co.nz>
2015-08-22 12:06:46 -0700

    TLS: failure of https:// context non-fatal for non-OpenSSL builds
    Only OpenSSL library is 'guaranteed' to produce a TLS context at this point
    in the conversion to library-agnostic security. Any others may produce
    nothing.
    
    Match the DBG_IMPORTANT used for debug level of the 'initializing' message.

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -869,8 +869,11 @@ configDoConfigure(void)
         debugs(3, DBG_IMPORTANT, "Initializing https:// proxy context");
         Config.ssl_client.sslContext = Security::ProxyOutgoingConfig.createClientContext(false);
         if (!Config.ssl_client.sslContext) {
-            debugs(3, DBG_CRITICAL, "ERROR: Could not initialize https:// proxy context");
-            self_destruct();
+#if USE_OPENSSL
+            fatal("ERROR: Could not initialize https:// proxy context");
+#else
+            debugs(3, DBG_IMPORTANT, "ERROR: proxying https:// currently still requires --with-openssl");
+#endif
         }
     }
 
