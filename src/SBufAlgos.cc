78e5e8039777583aa762fedd392d5e12bbbe002e
Francesco Chemolli <kinkie@squid-cache.org>
2015-08-24 19:49:50 +0200

    mempools-nozero part 3: debug
    Make Debug::OutStream to be a MEMPROXY_CLASS user, rather than rolling own
    operator new and delete.

--- src/SBufAlgos.cc
+++ src/SBufAlgos.cc
@@ -9,17 +9,30 @@
 #include "squid.h"
 #include "SBufAlgos.h"
 
-std::size_t std::hash<SBuf>::operator() (const SBuf & sbuf) const noexcept
+static std::size_t
+lowSBufHash (const SBuf & sbuf, bool caseInsensitive = false) noexcept
 {
     //ripped and adapted from hash_string
     const char *s = sbuf.rawContent();
     size_t rv = 0;
     SBuf::size_type len=sbuf.length();
     while (len != 0) {
-        rv ^= 271 * *s;
+        rv ^= 271 * (caseInsensitive? xtolower(*s) : *s);
         ++s;
         --len;
     }
     return rv ^ (sbuf.length() * 271);
+}
+
+std::size_t
+std::hash<SBuf>::operator() (const SBuf & sbuf) const noexcept
+{
+    return lowSBufHash(sbuf);
+}
+
+std::size_t
+CaseInsensitiveSBufHash::operator() (const SBuf & sbuf) const noexcept
+{
+    return lowSBufHash(sbuf, true);
 }
 
