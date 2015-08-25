cd754ef8bf1b43d698bcc63a6477af3127d402c0
Francesco Chemolli <kinkie@squid-cache.org>
2015-08-25 16:36:54 +0200

    Portability fix: detect c++11 random support and implement fallbacks if not available

--- helpers/basic_auth/RADIUS/basic_radius_auth.cc
+++ helpers/basic_auth/RADIUS/basic_radius_auth.cc
@@ -207,7 +207,7 @@ static void
 random_vector(char *aVector)
 {
     static std::mt19937 mt(time(0));
-    static std::uniform_int_distribution<uint8_t> dist;
+    static xuniform_int_distribution<uint8_t> dist;
 
     for (int i = 0; i < AUTH_VECTOR_LEN; ++i)
         aVector[i] = static_cast<char>(dist(mt) & 0xFF);
