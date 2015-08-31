5f5f70bc39bb4c966e69c737b55d6f1eb6ad0010
Amos Jeffries <squid3@treenet.co.nz>
2015-08-31 02:38:51 -0700

    Packable API: Rename StoreEntryStream to PackableStream
    PackableStream has some implicit new properties different from
    the original StoreEntryStream type:
    
    * lack of Store.h dependency
    
    * ability to stream into a MemBuf if its creator desires that
    
    Meaning PackableStream can be used in a wider range of the code
    without increasing library dependencies.

--- src/SBufStatsAction.cc
+++ src/SBufStatsAction.cc
@@ -7,12 +7,12 @@
  */
 
 #include "squid.h"
+#include "base/PackableStream.h"
 #include "ipc/Messages.h"
 #include "ipc/TypedMsgHdr.h"
 #include "mgr/Registration.h"
 #include "SBufDetailedStats.h"
 #include "SBufStatsAction.h"
-#include "StoreEntryStream.h"
 
 SBufStatsAction::SBufStatsAction(const Mgr::CommandPointer &cmd_):
     Action(cmd_)
@@ -53,7 +53,7 @@ statHistSBufDumper(StoreEntry * sentry, int, double val, double size, int count)
 void
 SBufStatsAction::dump(StoreEntry* entry)
 {
-    StoreEntryStream ses(entry);
+    PackableStream ses(*entry);
     ses << "\n\n\nThese statistics are experimental; their format and contents "
         "should not be relied upon, they are bound to change as "
         "the SBuf feature is evolved\n";
