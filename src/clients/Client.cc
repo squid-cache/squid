1e2a8cf892c3d31f0e8a8b04f1afd932689113f3
Amos Jeffries <squid3@treenet.co.nz>
2015-09-01 11:40:41 -0700

    Fix FreeBSD Clang-3.5 build error

--- src/clients/Client.cc
+++ src/clients/Client.cc
@@ -800,7 +800,7 @@ void Client::handleAdaptedBodyProducerAborted()
     if (abortOnBadEntry("entry went bad while waiting for the now-aborted adapted body"))
         return;
 
-    Must(adaptedBodySource != nullptr);
+    Must(adaptedBodySource != NULL);
     if (!adaptedBodySource->exhausted()) {
         debugs(11,5, "waiting to consume the remainder of the aborted adapted body");
         return; // resumeBodyStorage() should eventually consume the rest
