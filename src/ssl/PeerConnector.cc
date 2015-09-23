3057acf9c7d24390e26243e3b8c64b3529d79e76
Source Maintenance <squidadm@squid-cache.org>
2015-09-23 18:11:57 +0000

    SourceFormat Enforcement

--- src/ssl/PeerConnector.cc
+++ src/ssl/PeerConnector.cc
@@ -221,8 +221,8 @@ void
 Ssl::PeekingPeerConnector::checkForPeekAndSpliceDone(allow_t answer)
 {
     const Ssl::BumpMode finalAction = (answer.code == ACCESS_ALLOWED) ?
-        static_cast<Ssl::BumpMode>(answer.kind):
-        checkForPeekAndSpliceGuess();
+                                      static_cast<Ssl::BumpMode>(answer.kind):
+                                      checkForPeekAndSpliceGuess();
     checkForPeekAndSpliceMatched(finalAction);
 }
 
