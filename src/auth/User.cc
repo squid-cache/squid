0caa282007102ca0fd2ee97178e222fadc1b3f8c
Amos Jeffries <squid3@treenet.co.nz>
2015-09-25 02:54:31 -0700

    Display username cache key in cache manager report

--- src/auth/User.cc
+++ src/auth/User.cc
@@ -246,20 +246,21 @@ Auth::User::CredentialsCacheStats(StoreEntry *output)
 {
     auto userlist = authenticateCachedUsersList();
     storeAppendPrintf(output, "Cached Usernames: %d", static_cast<int32_t>(userlist.size()));
-    storeAppendPrintf(output, "\n%-15s %-9s %-9s %-9s %s\n",
+    storeAppendPrintf(output, "\n%-15s %-9s %-9s %-9s %s\t%s\n",
                       "Type",
                       "State",
                       "Check TTL",
                       "Cache TTL",
-                      "Username");
+                      "Username", "Key");
     storeAppendPrintf(output, "--------------- --------- --------- --------- ------------------------------\n");
     for ( auto auth_user : userlist ) {
-        storeAppendPrintf(output, "%-15s %-9s %-9d %-9d %s\n",
+        storeAppendPrintf(output, "%-15s %-9s %-9d %-9d %s\t" SQUIDSBUFPH "\n",
                           Auth::Type_str[auth_user->auth_type],
                           CredentialState_str[auth_user->credentials()],
                           auth_user->ttl(),
                           static_cast<int32_t>(auth_user->expiretime - squid_curtime + ::Config.authenticateTTL),
-                          auth_user->username()
+                          auth_user->username(),
+                          SQUIDSBUFPRINT(auth_user->userKey())
                          );
     }
 }
