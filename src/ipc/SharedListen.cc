d7a44f20006b88dc8544ea6bf9f3b280100f3c4f
Source Maintenance <squidadm@squid-cache.org>
2015-09-25 06:12:10 +0000

    SourceFormat Enforcement

--- src/ipc/SharedListen.cc
+++ src/ipc/SharedListen.cc
@@ -150,7 +150,7 @@ Ipc::JoinSharedListen(const OpenListenerParams &params, AsyncCall::Pointer &cb)
     const DelayedSharedListenRequests::size_type concurrencyLimit = 1;
     if (TheSharedListenRequestMap.size() >= concurrencyLimit) {
         debugs(54, 3, "waiting for " << TheSharedListenRequestMap.size() <<
-           " active + " << TheDelayedRequests.size() << " delayed requests");
+               " active + " << TheDelayedRequests.size() << " delayed requests");
         TheDelayedRequests.push_back(por);
     } else {
         SendSharedListenRequest(por);
