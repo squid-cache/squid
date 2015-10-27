4b77ea6b5c768413df3a2707ae8d7fb810a30c5e
Alex Rousskov <rousskov@measurement-factory.com>
2015-10-26 21:45:40 -0600

    Connection stats, including %<lp, missing for persistent connections.
    The code reusing a pconn was missing a hier.note() call, resulting in 0
    values logged for %<lp (local port number of the last server or peer
    connection) and probably other missing stats.
    
    Also refactored poorly copied statistics collection code to remove
    duplication and always update to-server connection stats when the actual
    connection becomes available.
    
    Positive side effect: Upon setsockopt(2) failures, the tos and nfmark
    fields of a pinned connection were set to the desired (but not actually
    applied) values, while persistent connection fields were left intact
    (and, hence, stale). Both fields are now reset to zero on failures, for
    both types of connections.

--- src/FwdState.h
+++ src/FwdState.h
@@ -120,6 +120,8 @@ private:
     /// stops monitoring server connection for closure and updates pconn stats
     void closeServerConnection(const char *reason);
 
+    void syncWithServerConn(const char *host);
+
 public:
     StoreEntry *entry;
     HttpRequest *request;
