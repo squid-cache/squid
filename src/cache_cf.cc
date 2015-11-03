fe89b8bbb2c0d467448d07bf02d30419e4ec48ea
Amos Jeffries <squid3@treenet.co.nz>
2015-11-03 06:42:13 -0800

    Make FATAL messages have a consistent prefix

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -3842,7 +3842,7 @@ requirePathnameExists(const char *name, const char *path)
     }
 
     if (stat(path, &sb) < 0) {
-        debugs(0, DBG_CRITICAL, (opt_parse_cfg_only?"FATAL ":"") << "ERROR: " << name << " " << path << ": " << xstrerror());
+        debugs(0, DBG_CRITICAL, (opt_parse_cfg_only?"FATAL: ":"ERROR: ") << name << " " << path << ": " << xstrerror());
         // keep going to find more issues if we are only checking the config file with "-k parse"
         if (opt_parse_cfg_only)
             return;
