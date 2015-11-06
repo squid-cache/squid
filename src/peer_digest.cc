6fbd1d41ba6f1e7b7d21407c66dbbb59ef7e6554
Amos Jeffries <squid3@treenet.co.nz>
2015-11-05 23:34:54 -0800

    Convert cacheDigestBitUtil to a method
    Also, convert from int output to double. Avoiding implicit cast wrap,
    overflow and signed/unsigned data inaccuracy.

--- src/peer_digest.cc
+++ src/peer_digest.cc
@@ -1024,12 +1024,11 @@ static int
 peerDigestUseful(const PeerDigest * pd)
 {
     /* TODO: we should calculate the prob of a false hit instead of bit util */
-    const int bit_util = cacheDigestBitUtil(pd->cd);
+    const auto bit_util = pd->cd->usedMaskPercent();
 
-    if (bit_util > 65) {
+    if (bit_util > 65.0) {
         debugs(72, DBG_CRITICAL, "Warning: " << pd->host <<
-               " peer digest has too many bits on (" << bit_util << "%%).");
-
+               " peer digest has too many bits on (" << bit_util << "%).");
         return 0;
     }
 
