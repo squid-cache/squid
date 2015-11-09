737ace5a4ba345cb4be39947b182bcfc0a680205
Source Maintenance <squidadm@squid-cache.org>
2015-11-09 18:12:10 +0000

    SourceFormat Enforcement

--- src/client_side.cc
+++ src/client_side.cc
@@ -4768,7 +4768,7 @@ ConnStateData::handleIdleClientPinnedTlsRead()
     switch(const int error = SSL_get_error(ssl, readResult)) {
     case SSL_ERROR_WANT_WRITE:
         debugs(83, DBG_IMPORTANT, pinning.serverConnection << " TLS SSL_ERROR_WANT_WRITE request for idle pinned connection");
-        // fall through to restart monitoring, for now
+    // fall through to restart monitoring, for now
     case SSL_ERROR_NONE:
     case SSL_ERROR_WANT_READ:
         startPinnedConnectionMonitoring();
