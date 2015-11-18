61551ae4fed03af30d98f1c98e8218e1970bdc0b
Alex Rousskov <rousskov@measurement-factory.com>
2015-11-18 13:04:35 -0700

    Use "override" more consistently.
    Some broken compilers may not like it when only some overridden
    methods in a class declaration have the "override" specifier.

--- src/tests/TestSwapDir.h
+++ src/tests/TestSwapDir.h
@@ -19,24 +19,21 @@ public:
 
     bool statsCalled;
 
-    virtual uint64_t maxSize() const;
-    virtual uint64_t currentSize() const;
-    virtual uint64_t currentCount() const;
-    virtual void stat(StoreEntry &) const; /* output stats to the provided store entry */
-    virtual void swappedOut(const StoreEntry &e) {}
-
-    virtual void reconfigure();
-    virtual void init();
-    virtual bool unlinkdUseful() const;
-    virtual bool canStore(const StoreEntry &e, int64_t diskSpaceNeeded, int &load) const;
-    virtual StoreIOState::Pointer createStoreIO(StoreEntry &, StoreIOState::STFNCB *, StoreIOState::STIOCB *, void *);
-    virtual StoreIOState::Pointer openStoreIO(StoreEntry &, StoreIOState::STFNCB *, StoreIOState::STIOCB *, void *);
-    virtual void parse(int, char*);
-
+    /* Store::Disk API */
+    virtual uint64_t maxSize() const override;
+    virtual uint64_t currentSize() const override;
+    virtual uint64_t currentCount() const override;
+    virtual void stat(StoreEntry &) const override;
+    virtual void swappedOut(const StoreEntry &e) override {}
+    virtual void reconfigure() override;
+    virtual void init() override;
+    virtual bool unlinkdUseful() const override;
+    virtual bool canStore(const StoreEntry &e, int64_t diskSpaceNeeded, int &load) const override;
+    virtual StoreIOState::Pointer createStoreIO(StoreEntry &, StoreIOState::STFNCB *, StoreIOState::STIOCB *, void *) override;
+    virtual StoreIOState::Pointer openStoreIO(StoreEntry &, StoreIOState::STFNCB *, StoreIOState::STIOCB *, void *) override;
+    virtual void parse(int, char*) override;
     virtual void markForUnlink(StoreEntry &) override {}
     virtual void unlink(StoreEntry &) override {}
-    virtual bool updateCollapsed(StoreEntry &) override { return false; }
-    virtual bool anchorCollapsed(StoreEntry &, bool &) override { return false; }
 };
 
 typedef RefCount<TestSwapDir> TestSwapDirPointer;
