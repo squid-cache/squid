1ce592c667421cbe204e7083849bc1c1510fc499
Alex Rousskov <rousskov@measurement-factory.com>
2015-11-18 21:08:41 -0800

    Stop using dangling pointers for eCAP-set custom HTTP reason phrases.
    Squid still does not support [external] custom reason phrases and,
    hence, cannot reliably support eCAP API that sets the reason phrase to
    the one supplied by the adapter.  This and r14398 changes fix [known]
    regression bugs introduced by r12728 ("SourceLayout").

--- src/http/StatusLine.h
+++ src/http/StatusLine.h
@@ -34,8 +34,11 @@ public:
     void clean();
 
     /// set this status-line to the given values
-    /// when reason is NULL the default message text for this StatusCode will be used
+    /// when reason is not NULL, it must not point to a dynamically allocated value
     void set(const Http::ProtocolVersion &newVersion, Http::StatusCode newStatus, const char *newReason = NULL);
+
+    /// reset the reason phrase to its default status code-derived value
+    void resetReason() { reason_ = NULL; }
 
     /// retrieve the status code for this status line
     Http::StatusCode status() const { return status_; }
