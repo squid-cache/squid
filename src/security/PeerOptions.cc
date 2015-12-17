91e078bf0f29c1ea9667809b3d315b73ccdb8f98
Amos Jeffries <squid3@treenet.co.nz>
2015-12-18 00:54:50 +1300

    Typo in rev.14448

--- src/security/PeerOptions.cc
+++ src/security/PeerOptions.cc
@@ -503,7 +503,7 @@ Security::PeerOptions::parseFlags()
             fatalf("Unknown TLS flag '" SQUIDSBUFPH "'", SQUIDSBUFPRINT(tok.remaining()));
         if (found == SSL_FLAG_NO_DEFAULT_CA) {
             debugs(83, DBG_PARSE_NOTE(2), "UPGRADE WARNING: flags=NO_DEFAULT_CA is deprecated. Use tls-no-default-ca instead.");
-            flags.tlDefaultCa = false;
+            flags.tlsDefaultCa = false;
         } else
             fl |= found;
     } while (tok.skipOne(delims));
