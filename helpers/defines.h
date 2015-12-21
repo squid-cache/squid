f3eeb2e60cc045e64944d8867248744459651941
Francesco Chemolli <kinkie@squid-cache.org>
2015-12-21 11:22:45 +0100

    C++-ify basic_fake_auth

--- helpers/defines.h
+++ helpers/defines.h
@@ -46,16 +46,16 @@
 #define HELPER_INPUT_BUFFER 8196
 
 /* send OK result to Squid with a string parameter. */
-#define SEND_OK(x)  fprintf(stdout, "OK %s\n",x)
+#define SEND_OK(x)  std::cout << "OK " << x << std::endl
 
 /* send ERR result to Squid with a string parameter. */
-#define SEND_ERR(x) fprintf(stdout, "ERR %s\n",x)
+#define SEND_ERR(x) std::cout << "ERR " << x << std::endl
 
 /* send ERR result to Squid with a string parameter. */
-#define SEND_BH(x)  fprintf(stdout, "BH %s\n",x)
+#define SEND_BH(x)  std::cout << "BH " << x << std::endl
 
 /* send TT result to Squid with a string parameter. */
-#define SEND_TT(x)  fprintf(stdout, "TT %s\n",x)
+#define SEND_TT(x)  std::cout << "TT " << x << std::endl
 
 #endif /* __SQUID_HELPERS_DEFINES_H */
 
