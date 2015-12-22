b64a472847ad3c73dd945f3a641f17a6c7a15c8a
Amos Jeffries <squid3@treenet.co.nz>
2015-12-22 15:59:42 +1300

    Fix clang build error after rev.13961

--- src/ssl/support.cc
+++ src/ssl/support.cc
@@ -1795,7 +1795,8 @@ findCertByIssuerFast(X509_STORE_CTX *ctx, Ssl::CertsIndexedList &list, X509 *cer
     else
         return NULL;
 
-    const auto ret = list.equal_range(SBuf(buffer));
+    typedef std::pair<Ssl::CertsIndexedList::iterator, Ssl::CertsIndexedList::iterator> Pair;
+    const Pair ret = list.equal_range(SBuf(buffer));
     for (Ssl::CertsIndexedList::iterator it = ret.first; it != ret.second; ++it) {
         X509 *issuer = it->second;
         if (ctx->check_issued(ctx, cert, issuer)) {
