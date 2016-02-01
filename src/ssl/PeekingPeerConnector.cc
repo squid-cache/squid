0ba8b2add7f59e780116c1dbce994d4daa994c2d
Amos Jeffries <squid3@treenet.co.nz>
2016-02-01 17:09:11 +1300

    Add missing include after rev.14518

--- src/ssl/PeekingPeerConnector.cc
+++ src/ssl/PeekingPeerConnector.cc
@@ -14,6 +14,7 @@
 #include "errorpage.h"
 #include "fde.h"
 #include "HttpRequest.h"
+#include "http/Stream.h"
 #include "security/NegotiationHistory.h"
 #include "SquidConfig.h"
 #include "ssl/bio.h"
