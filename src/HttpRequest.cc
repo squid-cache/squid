d7ef451f7e351b0ccb09bea66dfa9408b03f0df0
Francesco Chemolli <kinkie@squid-cache.org>
2016-02-25 17:44:46 +0100

    avoid unnecessary SBuf copies

--- src/HttpRequest.cc
+++ src/HttpRequest.cc
@@ -661,7 +661,7 @@ HttpRequest::storeId()
 {
     if (store_id.size() != 0) {
         debugs(73, 3, "sent back store_id: " << store_id);
-        return SBuf(StringToSBuf(store_id));
+        return StringToSBuf(store_id);
     }
     debugs(73, 3, "sent back effectiveRequestUrl: " << effectiveRequestUri());
     return effectiveRequestUri();
