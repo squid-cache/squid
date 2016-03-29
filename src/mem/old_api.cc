3b1439d03a1e603521ad95474483002238762eb2
Amos Jeffries <squid3@treenet.co.nz>
2016-03-29 20:58:21 +1300

    Use Mem::Init() to initialize for GetPool()

--- src/mem/old_api.cc
+++ src/mem/old_api.cc
@@ -70,6 +70,9 @@ GetPool(size_t type)
     if (!initialized) {
         memset(pools, '\0', sizeof(pools));
         initialized = true;
+        // Mem::Init() makes use of GetPool(type) to initialize
+        // the actual pools. So must come after the flag is true
+        Mem::Init();
     }
 
     return pools[type];
