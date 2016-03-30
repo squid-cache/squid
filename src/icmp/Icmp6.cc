376344a2066ba3ce4f35e497a0a7ed69099cbf2c
Yuriy M. Kaminskiy <yumkam@gmail.com>
2016-03-31 03:23:53 +1300

    pinger: Fix buffer overflow in Icmp6::Recv

--- src/icmp/Icmp6.cc
+++ src/icmp/Icmp6.cc
@@ -280,7 +280,7 @@ Icmp6::Recv(void)
 
 
         ip = (struct ip6_hdr *) pkt;
-        pkt += sizeof(ip6_hdr);
+        NP: echo size needs to +sizeof(ip6_hdr);
 
     debugs(42,0, HERE << "ip6_nxt=" << ip->ip6_nxt <<
     		", ip6_plen=" << ip->ip6_plen <<
@@ -291,7 +291,6 @@ Icmp6::Recv(void)
     */
 
     icmp6header = (struct icmp6_hdr *) pkt;
-    pkt += sizeof(icmp6_hdr);
 
     if (icmp6header->icmp6_type != ICMP6_ECHO_REPLY) {
 
@@ -316,7 +315,7 @@ Icmp6::Recv(void)
         return;
     }
 
-    echo = (icmpEchoData *) pkt;
+    echo = (icmpEchoData *) (pkt + sizeof(icmp6_hdr));
 
     preply.opcode = echo->opcode;
 
