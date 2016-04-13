7f6e3bc5061a2727fdf209c5a503b3a73dcf6b81
Alex Rousskov <rousskov@measurement-factory.com>
2016-04-12 23:36:05 -0600

    The "hold write" conditions are not important enough for debug level 1.
    ... and are already reported at lower levels in ServerBio.
    
    I was getting one of this messages for every(?) transaction in Polygraph
    tests.

--- src/ssl/PeekingPeerConnector.cc
+++ src/ssl/PeekingPeerConnector.cc
@@ -270,7 +270,6 @@ Ssl::PeekingPeerConnector::noteWantWrite()
     Ssl::ServerBio *srvBio = static_cast<Ssl::ServerBio *>(b->ptr);
 
     if ((srvBio->bumpMode() == Ssl::bumpPeek || srvBio->bumpMode() == Ssl::bumpStare) && srvBio->holdWrite()) {
-        debugs(81, DBG_IMPORTANT, "hold write on SSL connection on FD " << fd);
         checkForPeekAndSplice();
         return;
     }
