aad871e34f048a7365b4479b7354b25ee2582c00
Dan Searle <dan@censornet.com>
2016-05-13 00:37:50 +1200

    Bug 4492: chunked parser needs to accept BWS after chunk size

--- src/http/one/TeChunkedParser.cc
+++ src/http/one/TeChunkedParser.cc
@@ -117,6 +117,11 @@ Http::One::TeChunkedParser::parseChunkSize(Http1::Tokenizer &tok)
 bool
 Http::One::TeChunkedParser::parseChunkExtension(Http1::Tokenizer &tok, bool skipKnown)
 {
+    // Bug 4492: IBM_HTTP_Server sends SP padding
+    if (auto n = tok.skipAll(CharacterSet::SP)) {
+        debugs(94, 3, "skipping " << n << " spurious whitespace at start of chunk extension");
+    }
+
     SBuf ext;
     SBuf value;
     while (tok.skip(';') && tok.prefix(ext, CharacterSet::TCHAR)) {
