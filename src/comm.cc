e3c37fc292e0cb5f75809d732b878c8920c9a679
Alex Rousskov <rousskov@measurement-factory.com>
2016-06-15 09:37:44 -0600

    Do not make bogus recvmsg(2) calls when closing UDS sockets.
    comm_empty_os_read_buffers() assumes that all non-blocking
    FD_READ_METHODs can read into an opaque buffer filled with random
    characters. That assumption is wrong for UDS sockets that require an
    initialized msghdr structure. Feeding random data to recvmsg(2) leads to
    confusing errors, at best. Squid does not log those errors, but they
    are visible in, for example, strace:
    
      recvmsg(17, 0x7fffbb, MSG_DONTWAIT) = -1 EMSGSIZE (Message too long)
    
    comm_empty_os_read_buffers() is meant to prevent TCP RST packets. The
    function now ignores UDS sockets that are not used for TCP.
    
    TODO: Useless reads may also exist for UDP and some TCP sockets.

--- src/comm.cc
+++ src/comm.cc
@@ -110,7 +110,7 @@ comm_empty_os_read_buffers(int fd)
 
     /* prevent those nasty RST packets */
     char buf[SQUID_TCP_SO_RCVBUF];
-    if (fd_table[fd].flags.nonblocking) {
+    if (fd_table[fd].flags.nonblocking && fd_table[fd].type != FD_MSGHDR) {
         while (FD_READ_METHOD(fd, buf, SQUID_TCP_SO_RCVBUF) > 0) {};
     }
 #endif
