19e652c645ad830aa2695ec5decb0ff9f95590d5
Amos Jeffries <squid3@treenet.co.nz>
2016-07-28 09:16:35 +1200

    Cleanup: sort stub_libsecurity

--- src/tests/stub_libsecurity.cc
+++ src/tests/stub_libsecurity.cc
@@ -14,30 +14,21 @@
 #define STUB_API "security/libsecurity.la"
 #include "tests/STUB.h"
 
+#include "security/BlindPeerConnector.h"
+CBDATA_NAMESPACED_CLASS_INIT(Security, BlindPeerConnector);
+namespace Security
+{
+bool BlindPeerConnector::initializeTls(Security::SessionPointer &) STUB_RETVAL(false)
+Security::ContextPtr BlindPeerConnector::getSslContext() STUB_RETVAL(nullptr)
+void BlindPeerConnector::noteNegotiationDone(ErrorState *) STUB
+}
 #include "security/EncryptorAnswer.h"
 Security::EncryptorAnswer::~EncryptorAnswer() {}
 std::ostream &Security::operator <<(std::ostream &os, const Security::EncryptorAnswer &) STUB_RETVAL(os)
 
-#include "security/PeerOptions.h"
-Security::PeerOptions Security::ProxyOutgoingConfig;
-void Security::PeerOptions::parse(char const*) STUB
-Security::ContextPtr Security::PeerOptions::createClientContext(bool) STUB_RETVAL(NULL)
-void Security::PeerOptions::updateTlsVersionLimits() STUB
-Security::ContextPtr Security::PeerOptions::createBlankContext() const STUB
-void Security::PeerOptions::updateContextCa(Security::ContextPtr &) STUB
-void Security::PeerOptions::updateContextCrl(Security::ContextPtr &) STUB
-void Security::PeerOptions::dumpCfg(Packable*, char const*) const STUB
-long Security::PeerOptions::parseOptions() STUB_RETVAL(0)
-long Security::PeerOptions::parseFlags() STUB_RETVAL(0)
-void parse_securePeerOptions(Security::PeerOptions *) STUB
-
-#include "security/ServerOptions.h"
-//Security::ServerOptions::ServerOptions(const Security::ServerOptions &) STUB
-void Security::ServerOptions::parse(const char *) STUB
-void Security::ServerOptions::dumpCfg(Packable *, const char *) const STUB
-Security::ContextPtr Security::ServerOptions::createBlankContext() const STUB
-bool Security::ServerOptions::createStaticServerContext(AnyP::PortCfg &) STUB_RETVAL(false)
-void Security::ServerOptions::updateContextEecdh(Security::ContextPtr &) STUB
+#include "security/Handshake.h"
+Security::HandshakeParser::HandshakeParser() STUB
+bool Security::HandshakeParser::parseHello(const SBuf &) STUB_RETVAL(false)
 
 #include "security/NegotiationHistory.h"
 Security::NegotiationHistory::NegotiationHistory() STUB
@@ -45,10 +36,6 @@ void Security::NegotiationHistory::retrieveNegotiatedInfo(Security::SessionPtr) 
 void Security::NegotiationHistory::retrieveParsedInfo(Security::TlsDetails::Pointer const &) STUB
 const char *Security::NegotiationHistory::cipherName() const STUB
 const char *Security::NegotiationHistory::printTlsVersion(AnyP::ProtocolVersion const &v) const STUB
-
-#include "security/Handshake.h"
-Security::HandshakeParser::HandshakeParser() STUB
-bool Security::HandshakeParser::parseHello(const SBuf &) STUB_RETVAL(false)
 
 #include "security/PeerConnector.h"
 CBDATA_NAMESPACED_CLASS_INIT(Security, PeerConnector);
@@ -78,12 +65,24 @@ void PeerConnector::callBack() STUB
 void PeerConnector::recordNegotiationDetails() STUB
 }
 
-#include "security/BlindPeerConnector.h"
-CBDATA_NAMESPACED_CLASS_INIT(Security, BlindPeerConnector);
-namespace Security
-{
-bool BlindPeerConnector::initializeTls(Security::SessionPointer &) STUB_RETVAL(false)
-Security::ContextPtr BlindPeerConnector::getSslContext() STUB_RETVAL(nullptr)
-void BlindPeerConnector::noteNegotiationDone(ErrorState *) STUB
-}
+#include "security/PeerOptions.h"
+Security::PeerOptions Security::ProxyOutgoingConfig;
+void Security::PeerOptions::parse(char const*) STUB
+Security::ContextPtr Security::PeerOptions::createClientContext(bool) STUB_RETVAL(NULL)
+void Security::PeerOptions::updateTlsVersionLimits() STUB
+Security::ContextPtr Security::PeerOptions::createBlankContext() const STUB
+void Security::PeerOptions::updateContextCa(Security::ContextPtr &) STUB
+void Security::PeerOptions::updateContextCrl(Security::ContextPtr &) STUB
+void Security::PeerOptions::dumpCfg(Packable*, char const*) const STUB
+long Security::PeerOptions::parseOptions() STUB_RETVAL(0)
+long Security::PeerOptions::parseFlags() STUB_RETVAL(0)
+void parse_securePeerOptions(Security::PeerOptions *) STUB
+
+#include "security/ServerOptions.h"
+//Security::ServerOptions::ServerOptions(const Security::ServerOptions &) STUB
+void Security::ServerOptions::parse(const char *) STUB
+void Security::ServerOptions::dumpCfg(Packable *, const char *) const STUB
+Security::ContextPtr Security::ServerOptions::createBlankContext() const STUB
+bool Security::ServerOptions::createStaticServerContext(AnyP::PortCfg &) STUB_RETVAL(false)
+void Security::ServerOptions::updateContextEecdh(Security::ContextPtr &) STUB
 
