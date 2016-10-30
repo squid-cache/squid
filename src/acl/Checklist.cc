b85a2faa325f784466625b1b272a8aea46fd4dfc
Garri Djavadyan <garryd@comnet.uz>
2016-10-30 22:40:25 +1300

    Fix debug message in ACLChecklist::bannedAction()

--- src/acl/Checklist.cc
+++ src/acl/Checklist.cc
@@ -397,7 +397,7 @@ bool
 ACLChecklist::bannedAction(const allow_t &action) const
 {
     const bool found = std::find(bannedActions_.begin(), bannedActions_.end(), action) != bannedActions_.end();
-    debugs(28, 5, "Action '" << action << "/" << action.kind << (found ? " is " : "is not") << " banned");
+    debugs(28, 5, "Action '" << action << "/" << action.kind << (found ? "' is " : "' is not") << " banned");
     return found;
 }
 
