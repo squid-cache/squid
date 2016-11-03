a7378e715141a56527221d8212901c35c8360a95
Alex Rousskov <rousskov@measurement-factory.com>
2016-11-04 02:57:02 +1300

    Optimize appending to empty SBufs, a common use case.
    Most appends start with an empty buffer. Some append only once. There
    is no reason to allocate new memory for that first append or force the
    appender to write code to optimize that first append.
    
    Why also check that SBuf has never been configured or altered then? To
    preserve pre-allocated SBufs, such as those [ab]used as I/O buffers.

--- src/sbuf/SBuf.cc
+++ src/sbuf/SBuf.cc
@@ -187,6 +187,9 @@ SBuf::clear()
 SBuf&
 SBuf::append(const SBuf &S)
 {
+    if (isEmpty() && store_ == GetStorePrototype())
+        return (*this = S); // optimization: avoid needless copying
+
     const Locker blobKeeper(this, S.buf());
     return lowAppend(S.buf(), S.length());
 }
