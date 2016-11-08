28e1d824b2709692b463f590f8e3fd4d6b944652
Amos Jeffries <squid3@treenet.co.nz>
2016-11-08 19:33:38 +1300

    Make key= before cert= a fatal error instead of quietly hiding the issue

--- src/security/PeerOptions.cc
+++ src/security/PeerOptions.cc
@@ -59,7 +59,7 @@ Security::PeerOptions::parse(const char *token)
         certs.emplace_back(t);
     } else if (strncmp(token, "key=", 4) == 0) {
         if (certs.empty() || certs.back().certFile.isEmpty()) {
-            debugs(3, DBG_PARSE_NOTE(1), "ERROR: cert= option must be set before key= is used.");
+            fatal("cert= option must be set before key= is used.");
             return;
         }
         KeyData &t = certs.back();
