58e1b950c27bfb0245134f5108ea507e9e90a40a
Amos Jeffries <squid3@treenet.co.nz>
2016-12-22 17:08:55 +1300

    Bug 3940 pt2: Make 'cache deny' do what is documented
    Instead of overriding whatever cacheability was previously set to
    (including changing non-cacheables to be cacheable) actually
    prevent both cache read and write.

--- src/client_side_request.cc
+++ src/client_side_request.cc
@@ -1399,7 +1399,10 @@ void
 ClientRequestContext::checkNoCacheDone(const allow_t &answer)
 {
     acl_checklist = NULL;
-    http->request->flags.cachable = (answer == ACCESS_ALLOWED);
+    if (answer == ACCESS_DENIED) {
+        http->request->flags.noCache = true; // dont read reply from cache
+        http->request->flags.cachable = false; // dont store reply into cache
+    }
     http->doCallouts();
 }
 
