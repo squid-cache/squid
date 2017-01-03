11e14d639b6979de936aad35a04be4cf82c6519b
Amos Jeffries <squid3@treenet.co.nz>
2017-01-03 16:29:53 +1300

    Tests: shuffle libmem Pool unit test to src/
    Refactor for cppunit framework instead of custom code.
    
    Duplicate the basic create-free-realloc cycle test for both Pool and MemProxy
    but do not add missing tests for other API calls at this time.

---
+++ src/tests/testMem.cc
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 1996-2017 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+#include "mem/forward.h"
+#include "mem/Pool.h"
+#include "tests/testMem.h"
+#include "unitTestMain.h"
+
+#include <iostream>
+#include <stdexcept>
+
+CPPUNIT_TEST_SUITE_REGISTRATION( testMem );
+
+class SomethingToAlloc
+{
+public:
+    int aValue;
+};
+
+class MoreToAlloc
+{
+    MEMPROXY_CLASS(MoreToAlloc);
+
+public:
+    int aValue = 0;
+};
+
+void
+testMem::testMemPool()
+{
+    MemAllocator *Pool = memPoolCreate("Test Pool", sizeof(SomethingToAlloc));
+    CPPUNIT_ASSERT(Pool);
+
+    auto *something = static_cast<SomethingToAlloc *>(Pool->alloc());
+    CPPUNIT_ASSERT(something);
+    CPPUNIT_ASSERT_EQUAL(something->aValue, 0);
+    something->aValue = 5;
+    Pool->freeOne(something);
+
+    // Pool should use the FreeList to allocate next object
+    auto *otherthing = static_cast<SomethingToAlloc *>(Pool->alloc());
+    CPPUNIT_ASSERT_EQUAL(otherthing, something);
+    CPPUNIT_ASSERT_EQUAL(otherthing->aValue, 0);
+    Pool->freeOne(otherthing);
+
+    delete Pool;
+}
+
+void
+testMem::testMemProxy()
+{
+    auto *something = new MoreToAlloc;
+    CPPUNIT_ASSERT(something);
+    CPPUNIT_ASSERT_EQUAL(something->aValue, 0);
+    something->aValue = 5;
+    delete something;
+
+    // The MEMPROXY pool should use its FreeList to allocate next object
+    auto *otherthing = new MoreToAlloc;
+    CPPUNIT_ASSERT_EQUAL(otherthing, something);
+    CPPUNIT_ASSERT_EQUAL(otherthing->aValue, 0);
+}
+
