3965c8fc6b66b217abdcade876101f0a6fc0b2b9
Amos Jeffries <squid3@treenet.co.nz>
2017-01-28 02:24:30 +1300

    Bug 4636: assertion failed: client_side.cc:1448: 'byteCount > 0 && byteCount <= inBuf.length()'

--- src/client_side.cc
+++ src/client_side.cc
@@ -1321,7 +1321,8 @@ parseHttpRequest(ConnStateData *csd, const Http1::RequestParserPointer &hp)
             auto result = csd->abortRequestParsing(
                               tooBig ? "error:request-too-large" : "error:invalid-request");
             // assume that remaining leftovers belong to this bad request
-            csd->consumeInput(csd->inBuf.length());
+            if (!csd->inBuf.isEmpty())
+                csd->consumeInput(csd->inBuf.length());
             return result;
         }
     }
