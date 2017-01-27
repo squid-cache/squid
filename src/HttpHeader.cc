3e1e155b351e73f7369c895eec9e96365b4a0713
Alex Rousskov <rousskov@measurement-factory.com>
2017-01-28 02:28:31 +1300

    Complain about malformed 64bit header values, not the valid ones.
    Added this regression in r14819, apparently confused by my own
    unfortunate "bad is true" httpHeaderNoteParsedEntry(error) API.

--- src/HttpHeader.cc
+++ src/HttpHeader.cc
@@ -1467,7 +1467,7 @@ HttpHeaderEntry::getInt64() const
 {
     int64_t val = -1;
     const bool ok = httpHeaderParseOffset(value.termedBuf(), &val);
-    httpHeaderNoteParsedEntry(id, value, ok);
+    httpHeaderNoteParsedEntry(id, value, !ok);
     return val; // remains -1 if !ok (XXX: bad method API)
 }
 
