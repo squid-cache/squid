dfe70a1b18a237a6a619d44470cc042d2b94fced
Amos Jeffries <squid3@treenet.co.nz>
2017-02-01 04:30:19 +1300

    Add debugs for loading the system CA set

--- src/security/PeerOptions.cc
+++ src/security/PeerOptions.cc
@@ -621,6 +621,7 @@ Security::PeerOptions::updateContextNpn(Security::ContextPointer &ctx)
 static const char *
 loadSystemTrustedCa(Security::ContextPointer &ctx)
 {
+    debugs(83, 8, "Setting default system Trusted CA. ctx=" << (void*)ctx.get());
 #if USE_OPENSSL
     if (SSL_CTX_set_default_verify_paths(ctx.get()) == 0)
         return Security::ErrorString(ERR_get_error());
