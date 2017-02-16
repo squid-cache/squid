6c28dd4a379a9ecaf632ca8147cf2b7642560f92
Source Maintenance <squidadm@squid-cache.org>
2017-02-16 00:12:17 +0000

    SourceFormat Enforcement

--- src/adaptation/ecap/MessageRep.cc
+++ src/adaptation/ecap/MessageRep.cc
@@ -34,8 +34,8 @@ Adaptation::Ecap::HeaderRep::hasAny(const Name &name) const
 {
     const Http::HdrType squidId = TranslateHeaderId(name);
     return squidId == Http::HdrType::OTHER ?
-        theHeader.hasNamed(name.image().c_str(), name.image().size()) :
-        static_cast<bool>(theHeader.has(squidId));
+           theHeader.hasNamed(name.image().c_str(), name.image().size()) :
+           static_cast<bool>(theHeader.has(squidId));
 }
 
 Adaptation::Ecap::HeaderRep::Value
