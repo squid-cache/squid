e2ec8348a093a61242baddd5e147fb80c5a13981
Amos Jeffries <squid3@treenet.co.nz>
2017-02-20 17:29:18 +1300

    Bug 4663: GCC compile errors with optimization level -O3

--- src/security/Handshake.cc
+++ src/security/Handshake.cc
@@ -545,6 +545,10 @@ Security::HandshakeParser::ParseCertificate(const SBuf &raw, Security::CertPoint
     pCert.resetWithoutLocking(x509);
     Must(x509); // successfully parsed
     Must(x509Pos == x509Start + raw.length()); // no leftovers
+#else
+    // workaround GCC -O3 error with unused variables. see bug 4663.
+    (void)pCert;
+    debugs(83, 2, "TLS parsing is not supported without OpenSSL. " << raw);
 #endif
 }
 
