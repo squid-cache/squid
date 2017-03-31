05465d0f0720c1f8237e24f4745645cdb431931b
Source Maintenance <squidadm@squid-cache.org>
2017-03-31 12:14:21 +0000

    SourceFormat Enforcement

--- src/MemObject.cc
+++ src/MemObject.cc
@@ -463,12 +463,12 @@ void
 MemObject::delayRead(DeferredRead const &aRead)
 {
 #if USE_DELAY_POOLS
-        if (readAheadPolicyCanRead()) {
-            if (DelayId mostAllowedId = mostBytesAllowed()) {
-                mostAllowedId.delayRead(aRead);
-                return;
-            }
+    if (readAheadPolicyCanRead()) {
+        if (DelayId mostAllowedId = mostBytesAllowed()) {
+            mostAllowedId.delayRead(aRead);
+            return;
         }
+    }
 #endif
     deferredReads.delayRead(aRead);
 }
