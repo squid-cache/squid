40ab18576e0d90eb04d40c9c53a000e734906f45
Amos Jeffries <>
2017-04-12 12:07:55 +1200

    Cleanup: convert ACLIntRange loops to range-for

--- src/acl/IntRange.cc
+++ src/acl/IntRange.cc
@@ -55,10 +55,8 @@ bool
 ACLIntRange::match(int i)
 {
     RangeType const toFind(i, i+1);
-    for (std::list<RangeType>::const_iterator iter = ranges.begin(); iter != ranges.end(); ++iter) {
-        const RangeType & element = *iter;
+    for (const auto &element : ranges) {
         RangeType result = element.intersection(toFind);
-
         if (result.size())
             return true;
     }
@@ -82,9 +80,8 @@ SBufList
 ACLIntRange::dump() const
 {
     SBufList sl;
-    for (std::list<RangeType>::const_iterator iter = ranges.begin(); iter != ranges.end(); ++iter) {
+    for (const auto &element : ranges) {
         SBuf sb;
-        const RangeType & element = *iter;
 
         if (element.size() == 1)
             sb.Printf("%d", element.start);
