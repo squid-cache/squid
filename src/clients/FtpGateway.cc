b1e321fcbb035d4679029a1359ad7ed6dc60f7c0
Rainer Tammer <rainer.tammer@schulergroup.com>
2017-05-16 18:48:07 +1200

    Bug 3772: message from FTP server gets mangled

--- src/clients/FtpGateway.cc
+++ src/clients/FtpGateway.cc
@@ -1513,7 +1513,7 @@ ftpReadCwd(Ftp::Gateway * ftpState)
         /* Reset cwd_message to only include the last message */
         ftpState->cwd_message.reset("");
         for (wordlist *w = ftpState->ctrl.message; w; w = w->next) {
-            ftpState->cwd_message.append(' ');
+            ftpState->cwd_message.append('\n');
             ftpState->cwd_message.append(w->key);
         }
         ftpState->ctrl.message = NULL;
