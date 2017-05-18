c6f9a0ff17398f9570d95ee01120c06ec8885075
Christos Tsantilas <chtsanti@users.sourceforge.net>
2017-05-18 20:21:45 +0300

    Bug 4653 - %st lies about tunneled traffic volumes
    Squid-5 and squid-4 does not count the "HTTP/1.1 200 Connection Established"
    header size for %<st formatting code.
    
    This is a Measurement Factory project

--- src/tunnel.cc
+++ src/tunnel.cc
@@ -899,7 +899,7 @@ tunnelStartShoveling(TunnelStateData *tunnelState)
  * Call the tunnelStartShoveling to start the blind pump.
  */
 static void
-tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *, size_t, Comm::Flag flag, int, void *data)
+tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *, size_t len, Comm::Flag flag, int, void *data)
 {
     TunnelStateData *tunnelState = (TunnelStateData *)data;
     debugs(26, 3, HERE << conn << ", flag=" << flag);
@@ -909,6 +909,11 @@ tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *, size_t, Co
         *tunnelState->status_ptr = Http::scInternalServerError;
         tunnelErrorComplete(conn->fd, data, 0);
         return;
+    }
+
+    if (auto http = tunnelState->http.get()) {
+        http->out.headers_sz += len;
+        http->out.size += len;
     }
 
     tunnelStartShoveling(tunnelState);
