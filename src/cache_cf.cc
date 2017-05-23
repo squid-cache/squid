41cb7fe5b01bc5d242be88ddce76abb291da0d00
Amos Jeffries <squid3@treenet.co.nz>
2017-05-23 22:35:48 +1200

    Use Http::ProtocolVersion in config parsing

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -3511,7 +3511,7 @@ parsePortProtocol(const SBuf &value)
     // HTTP/1.0 not supported because we are version 1.1 which contains a superset of 1.0
     // and RFC 2616 requires us to upgrade 1.0 to 1.1
     if (value.cmp("HTTP") == 0 || value.cmp("HTTP/1.1") == 0)
-        return AnyP::ProtocolVersion(AnyP::PROTO_HTTP, 1,1);
+        return Http::ProtocolVersion(1,1);
 
     if (value.cmp("HTTPS") == 0 || value.cmp("HTTPS/1.1") == 0)
         return AnyP::ProtocolVersion(AnyP::PROTO_HTTPS, 1,1);
