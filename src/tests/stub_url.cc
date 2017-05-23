3c849cc50dd59d550b31b475158c9995f26cfbdc
Amos Jeffries <squid3@treenet.co.nz>
2017-05-23 18:17:35 +1200

    Tests: Add stub_url.cc

---
+++ src/tests/stub_url.cc
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 1996-2014 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+
+#define STUB_API "url.cc"
+#include "tests/STUB.h"
+
+#include "URL.h"
+URL::URL(AnyP::UriScheme const &) {STUB}
+void URL::touch() STUB
+void URL::host(const char *) STUB
+static SBuf nil;
+const SBuf &URL::path() const STUB_RETVAL(nil)
+const SBuf &URL::SlashPath()
+{
+    static SBuf slash("/");
+    return slash;
+}
+const SBuf &URL::Asterisk()
+{
+    static SBuf asterisk("*");
+    return asterisk;
+}
+SBuf &URL::authority(bool) const STUB_RETVAL(nil)
+SBuf &URL::absolute() const STUB_RETVAL(nil)
+void urlInitialize() STUB
+HttpRequest *urlParse(const HttpRequestMethod&, char *, HttpRequest *) STUB_RETVAL(nullptr)
+char *urlCanonicalClean(const HttpRequest *) STUB_RETVAL(nullptr)
+const char *urlCanonicalFakeHttps(const HttpRequest *) STUB_RETVAL(nullptr)
+bool urlIsRelative(const char *) STUB_RETVAL(false)
+char *urlMakeAbsolute(const HttpRequest *, const char *)STUB_RETVAL(nullptr)
+char *urlRInternal(const char *, unsigned short, const char *, const char *) STUB_RETVAL(nullptr)
+char *urlInternal(const char *, const char *) STUB_RETVAL(nullptr)
+int matchDomainName(const char *, const char *, uint) STUB_RETVAL(0)
+int urlCheckRequest(const HttpRequest *) STUB_RETVAL(0)
+char *urlHostname(const char *) STUB_RETVAL(nullptr)
+void urlExtMethodConfigure() STUB
