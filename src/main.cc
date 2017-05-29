750335196a4775f97abec9f8c2b863a9d5dc86b5
Amos Jeffries <squid3@treenet.co.nz>
2017-05-29 15:19:47 +1200

    Add OpenSSL library details to -v output
    This is partially to meet the OpenSSL copyright requirement that binaries
    mention when they are using the library, and partially for admin to see
    which library their Squid is using when multiple are present in the system.

--- src/main.cc
+++ src/main.cc
@@ -663,6 +663,10 @@ mainParseOptions(int argc, char *argv[])
             printf("Service Name: " SQUIDSBUFPH "\n", SQUIDSBUFPRINT(service_name));
             if (strlen(SQUID_BUILD_INFO))
                 printf("%s\n",SQUID_BUILD_INFO);
+#if USE_OPENSSL
+            printf("\nThis binary uses %s. ", SSLeay_version(SSLEAY_VERSION));
+            printf("For legal restrictions on distribution see https://www.openssl.org/source/license.html\n\n");
+#endif
             printf( "configure options: %s\n", SQUID_CONFIGURE_OPTIONS);
 
 #if USE_WIN32_SERVICE
