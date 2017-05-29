a22ed3972f54fa07167f452deb99e7949104894e
Amos Jeffries <squid3@treenet.co.nz>
2017-05-30 00:57:48 +1200

    Revert r14161
    Wrong patch and commit message.

--- src/tunnel.cc
+++ src/tunnel.cc
@@ -836,7 +836,7 @@ tunnelStartShoveling(TunnelStateData *tunnelState)
  * Call the tunnelStartShoveling to start the blind pump.
  */
 static void
-tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *, size_t len, Comm::Flag flag, int, void *data)
+tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *buf, size_t size, Comm::Flag flag, int xerrno, void *data)
 {
     TunnelStateData *tunnelState = (TunnelStateData *)data;
     debugs(26, 3, HERE << conn << ", flag=" << flag);
@@ -846,11 +846,6 @@ tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *, size_t len
         *tunnelState->status_ptr = Http::scInternalServerError;
         tunnelErrorComplete(conn->fd, data, 0);
         return;
-    }
-
-    if (ClientHttpRequest *http = tunnelState->http.get()) {
-        http->out.headers_sz += len;
-        http->out.size += len;
     }
 
     tunnelStartShoveling(tunnelState);
