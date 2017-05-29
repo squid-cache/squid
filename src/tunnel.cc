121ddaafc905321969e60b836863b28e8e65ee33
Christos Tsantilas <chtsanti@users.sourceforge.net>
2017-05-29 17:33:59 +1200

    Bug 4682: ignoring http_access deny when client-first bumping mode is used
    Squid fails to identify HTTP requests which are tunneled inside an already
    established client-first bumped tunnel, and this is results in ignoring
    http_access denied for these requests.
    
    This is a Measurement Factory project

--- src/tunnel.cc
+++ src/tunnel.cc
@@ -836,7 +836,7 @@ tunnelStartShoveling(TunnelStateData *tunnelState)
  * Call the tunnelStartShoveling to start the blind pump.
  */
 static void
-tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *buf, size_t size, Comm::Flag flag, int xerrno, void *data)
+tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *, size_t len, Comm::Flag flag, int, void *data)
 {
     TunnelStateData *tunnelState = (TunnelStateData *)data;
     debugs(26, 3, HERE << conn << ", flag=" << flag);
@@ -846,6 +846,11 @@ tunnelConnectedWriteDone(const Comm::ConnectionPointer &conn, char *buf, size_t 
         *tunnelState->status_ptr = Http::scInternalServerError;
         tunnelErrorComplete(conn->fd, data, 0);
         return;
+    }
+
+    if (ClientHttpRequest *http = tunnelState->http.get()) {
+        http->out.headers_sz += len;
+        http->out.size += len;
     }
 
     tunnelStartShoveling(tunnelState);
