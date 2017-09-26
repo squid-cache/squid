squid-cache/squid

commit 1967a1bef22dead074d4d94105ce2d2c0843099c

Public StoreEntries must have Transients.
This is a preliminary fix for future adjustments, trying to address
different race condition problems when SMP workers purge cache
attached/unattached entries. Since now, we state that every local
public StoreEntry has a corresponding Transients entry (if Transients is
on).

src/MemStore.cc
@@ -307,13 +307,13 @@ MemStore::dereference(StoreEntry &)
 }
 
 StoreEntry *
-MemStore::get(const cache_key *key)
+MemStore::get(const Store::CacheKey &cacheKey)
 {
     if (!map)
         return NULL;
 
     sfileno index;
-    const Ipc::StoreMapAnchor *const slot = map->openForReading(key, index);
+    const Ipc::StoreMapAnchor *const slot = map->openForReading(cacheKey.key, index);
     if (!slot)
         return NULL;
 
@@ -330,7 +330,7 @@ MemStore::get(const cache_key *key)
     const bool copied = copyFromShm(*e, index, *slot);
 
     if (copied) {
-        e->hashInsert(key);
+        e->insertPublicKey(cacheKey);
         return e;
     }
 

src/Store.h
@@ -84,6 +84,7 @@ public:
     void abort();
     void makePublic(const KeyScope keyScope = ksDefault);
     void makePrivate(const bool shareable);
+    void insertPublicKey(const Store::CacheKey &);
     /// A low-level method just resetting "private key" flags.
     /// To avoid key inconsistency please use forcePublicKey()
     /// or similar instead.
@@ -364,7 +365,7 @@ StoreEntry *storeCreateEntry(const char *, const char *, const RequestFlags &, c
 
 /// \ingroup StoreAPI
 /// Creates a new StoreEntry with mem_obj and sets initial flags/states.
-StoreEntry *storeCreatePureEntry(const char *storeId, const char *logUrl, const RequestFlags &, const HttpRequestMethod&);
+StoreEntry *storeCreatePureEntry(const char *storeId, const char *logUrl, const HttpRequestMethod&);
 
 /// \ingroup StoreAPI
 void storeInit(void);

src/client_side_reply.cc
@@ -913,7 +913,8 @@ purgeEntriesByUrl(HttpRequest * req, const char *url)
         if (m.respMaybeCacheable()) {
             const cache_key *key = storeKeyPublic(url, m);
             debugs(88, 5, m << ' ' << url << ' ' << storeKeyText(key));
-            if (StoreEntry *entry = Store::Root().get(key)) {
+            Store::CacheKey cacheKey(key, SBuf(url), m);
+            if (StoreEntry *entry = Store::Root().get(cacheKey)) {
                 entry->lock("purgeEntriesByUrl");
 #if USE_HTCP
                 neighborsHtcpClear(entry, url, req, m, HTCP_CLR_INVALIDATION);

src/fs/rock/RockSwapDir.cc
@@ -54,13 +54,13 @@ Rock::SwapDir::~SwapDir()
 
 // called when Squid core needs a StoreEntry with a given key
 StoreEntry *
-Rock::SwapDir::get(const cache_key *key)
+Rock::SwapDir::get(const Store::CacheKey &cacheKey)
 {
     if (!map || !theFile || !theFile->canRead())
         return NULL;
 
     sfileno filen;
-    const Ipc::StoreMapAnchor *const slot = map->openForReading(key, filen);
+    const Ipc::StoreMapAnchor *const slot = map->openForReading(cacheKey.key, filen);
     if (!slot)
         return NULL;
 
@@ -68,7 +68,7 @@ Rock::SwapDir::get(const cache_key *key)
     StoreEntry *e = new StoreEntry();
     anchorEntry(*e, filen, *slot);
 
-    e->hashInsert(key);
+    e->insertPublicKey(cacheKey);
     trackReferences(*e);
 
     return e;

src/fs/ufs/UFSSwapDir.cc
@@ -818,7 +818,7 @@ Fs::Ufs::UFSSwapDir::addDiskRestore(const cache_key * key,
     mapBitSet(e->swap_filen);
     cur_size += fs.blksize * sizeInBlocks(e->swap_file_sz);
     ++n_disk_objects;
-    e->hashInsert(key); /* do it after we clear KEY_PRIVATE */
+    e->insertPublicKey(Store::CacheKey(key)); /* do it after we clear KEY_PRIVATE */
     replacementAdd (e);
     return e;
 }

src/store.cc
@@ -161,6 +161,16 @@ StoreEntry::clearPrivate()
 }
 
 void
+StoreEntry::insertPublicKey(const Store::CacheKey &cacheKey)
+{
+    debugs(20, 3, "Inserting public key " << *this << " key '" << storeKeyText(cacheKey.key) << "'");
+    hashInsert(cacheKey.key);
+    if (Store::Root().transientsAvailable() && !hasTransients() &&
+            !Store::Root().createTransientsEntry(this, cacheKey))
+        setPrivateKey(true, false);
+}
+
+void
 StoreEntry::cacheNegatively()
 {
     /* This object may be negatively cached */
@@ -532,13 +542,15 @@ StoreEntry::getPublic (StoreClient *aClient, const char *uri, const HttpRequestM
 StoreEntry *
 storeGetPublic(const char *uri, const HttpRequestMethod& method)
 {
-    return Store::Root().get(storeKeyPublic(uri, method));
+    const Store::CacheKey cacheKey(storeKeyPublic(uri, method), SBuf(uri), method);
+    return Store::Root().get(cacheKey);
 }
 
 StoreEntry *
 storeGetPublicByRequestMethod(HttpRequest * req, const HttpRequestMethod& method, const KeyScope keyScope)
 {
-    return Store::Root().get(storeKeyPublicByRequestMethod(req, method, keyScope));
+    const Store::CacheKey cacheKey(storeKeyPublicByRequestMethod(req, method, keyScope), req->storeId(), method);
+    return Store::Root().get(cacheKey);
 }
 
 StoreEntry *
@@ -576,6 +588,7 @@ getKeyCounter(void)
 void
 StoreEntry::setPrivateKey(const bool shareable, const bool permanent)
 {
+    debugs(20, 3, " private key " << *this);
     if (permanent)
         EBIT_SET(flags, RELEASE_REQUEST); // may already be set
     if (!shareable)
@@ -607,6 +620,7 @@ StoreEntry::setPrivateKey(const bool shareable, const bool permanent)
 void
 StoreEntry::setPublicKey(const KeyScope scope)
 {
+    debugs(20, 3, "public key " << *this);
     if (key && !EBIT_TEST(flags, KEY_PRIVATE))
         return;                 /* is already public */
 
@@ -654,6 +668,9 @@ StoreEntry::clearPublicKeyScope()
 void
 StoreEntry::forcePublicKey(const cache_key *newkey)
 {
+    debugs(20, 3, "key " << *this << " key " << storeKeyText(newkey));
+    assert(mem_obj);
+
     if (StoreEntry *e2 = (StoreEntry *)hash_lookup(store_table, newkey)) {
         assert(e2 != this);
         debugs(20, 3, "releasing clashing " << *e2);
@@ -667,7 +684,7 @@ StoreEntry::forcePublicKey(const cache_key *newkey)
 
     clearPrivate();
 
-    hashInsert(newkey);
+    insertPublicKey(Store::CacheKey(newkey, SBuf(mem_obj->storeId()), mem_obj->method));
 
     if (hasDisk())
         storeDirSwapLog(this, SWAP_LOG_ADD);
@@ -756,7 +773,7 @@ StoreEntry::adjustVary()
 }
 
 StoreEntry *
-storeCreatePureEntry(const char *url, const char *log_url, const RequestFlags &, const HttpRequestMethod& method)
+storeCreatePureEntry(const char *url, const char *log_url, const HttpRequestMethod& method)
 {
     StoreEntry *e = NULL;
     debugs(20, 3, "storeCreateEntry: '" << url << "'");
@@ -776,7 +793,7 @@ storeCreatePureEntry(const char *url, const char *log_url, const RequestFlags &,
 StoreEntry *
 storeCreateEntry(const char *url, const char *logUrl, const RequestFlags &flags, const HttpRequestMethod& method)
 {
-    StoreEntry *e = storeCreatePureEntry(url, logUrl, flags, method);
+    StoreEntry *e = storeCreatePureEntry(url, logUrl, method);
     e->lock("storeCreateEntry");
 
     if (neighbors_do_private_keys || !flags.hierarchical || !flags.cachable)

src/store/Controller.cc
@@ -309,9 +309,9 @@ Store::Controller::hasReadableDiskEntry(const StoreEntry &e) const
 }
 
 StoreEntry *
-Store::Controller::get(const cache_key *key)
+Store::Controller::get(const CacheKey &cacheKey)
 {
-    if (StoreEntry *e = find(key)) {
+    if (StoreEntry *e = find(cacheKey)) {
         // this is not very precise: some get()s are not initiated by clients
         e->touch();
         referenceBusy(*e);
@@ -323,16 +323,16 @@ Store::Controller::get(const cache_key *key)
 /// Internal method to implements the guts of the Store::get() API:
 /// returns an in-transit or cached object with a given key, if any.
 StoreEntry *
-Store::Controller::find(const cache_key *key)
+Store::Controller::find(const CacheKey &cacheKey)
 {
-    debugs(20, 3, storeKeyText(key));
+    debugs(20, 3, storeKeyText(cacheKey.key));
 
-    if (markedForDeletion(key)) {
-        debugs(20, 3, "ignoring marked " << storeKeyText(key));
+    if (markedForDeletion(cacheKey.key)) {
+        debugs(20, 3, "ignoring marked " << storeKeyText(cacheKey.key));
         return nullptr;
     }
 
-    if (StoreEntry *e = static_cast<StoreEntry*>(hash_lookup(store_table, key))) {
+    if (StoreEntry *e = static_cast<StoreEntry*>(hash_lookup(store_table, cacheKey.key))) {
         // TODO: ignore and maybe handleIdleEntry() unlocked intransit entries
         // because their backing store slot may be gone already.
         debugs(20, 3, HERE << "got in-transit entry: " << *e);
@@ -341,7 +341,7 @@ Store::Controller::find(const cache_key *key)
 
     // Must search transients before caches because we must sync those we find.
     if (transients) {
-        if (StoreEntry *e = transients->get(key)) {
+        if (StoreEntry *e = transients->get(cacheKey)) {
             debugs(20, 3, "got shared in-transit entry: " << *e);
             bool inSync = false;
             const bool found = anchorCollapsed(*e, inSync);
@@ -354,20 +354,20 @@ Store::Controller::find(const cache_key *key)
     }
 
     if (memStore) {
-        if (StoreEntry *e = memStore->get(key)) {
+        if (StoreEntry *e = memStore->get(cacheKey)) {
             debugs(20, 3, HERE << "got mem-cached entry: " << *e);
             return e;
         }
     }
 
     if (swapDir) {
-        if (StoreEntry *e = swapDir->get(key)) {
+        if (StoreEntry *e = swapDir->get(cacheKey)) {
             debugs(20, 3, "got disk-cached entry: " << *e);
             return e;
         }
     }
 
-    debugs(20, 4, "cannot locate " << storeKeyText(key));
+    debugs(20, 4, "cannot locate " << storeKeyText(cacheKey.key));
     return nullptr;
 }
 
@@ -563,10 +563,21 @@ Store::Controller::allowCollapsing(StoreEntry *e, const RequestFlags &reqFlags,
 {
     const KeyScope keyScope = reqFlags.refresh ? ksRevalidation : ksDefault;
     e->makePublic(keyScope); // this is needed for both local and SMP collapsing
-    if (transients)
-        transients->startWriting(e, reqFlags, reqMethod);
     debugs(20, 3, "may " << (transients && e->hasTransients() ?
                              "SMP-" : "locally-") << "collapse " << *e);
+}
+
+bool
+Store::Controller::createTransientsEntry(StoreEntry *e, const CacheKey &cacheKey)
+{
+    if (!transients)
+        return false;
+    if (e->hasTransients())
+        return true;
+    if (!cacheKey.hasUris())
+        return false;
+
+    return transients->startWriting(e, cacheKey.method);
 }
 
 void

src/store/Controller.h
@@ -28,7 +28,7 @@ public:
     /* Storage API */
     virtual void create() override;
     virtual void init() override;
-    virtual StoreEntry *get(const cache_key *) override;
+    virtual StoreEntry *get(const CacheKey &key) override;
     virtual uint64_t maxSize() const override;
     virtual uint64_t minSize() const override;
     virtual uint64_t currentSize() const override;
@@ -74,6 +74,10 @@ public:
     /// makes the entry available for collapsing future requests
     void allowCollapsing(StoreEntry *, const RequestFlags &, const HttpRequestMethod &);
 
+    /// For each public StoreEntry creates corresponding Transients entry.
+    /// Makes the StoreEntry private on Transients entry creation error.
+    bool createTransientsEntry(StoreEntry *e, const CacheKey &cacheKey);
+
     /// marks the entry completed for collapsed requests
     void transientsCompleteWriting(StoreEntry &);
 
@@ -98,6 +102,9 @@ public:
     /// \returns an iterator for all Store entries
     StoreSearch *search();
 
+    /// whether Transients table exists
+    bool transientsAvailable() const { return bool(transients); }
+
     /// the number of cache_dirs being rebuilt; TODO: move to Disks::Rebuilding
     static int store_dirs_rebuilding;
 
@@ -107,7 +114,7 @@ private:
     /// dereference() an idle entry and return true if the entry should be deleted
     bool dereferenceIdle(StoreEntry &, bool wantsLocalMemory);
 
-    StoreEntry *find(const cache_key *key);
+    StoreEntry *find(const CacheKey &);
     bool keepForLocalMemoryCache(StoreEntry &e) const;
     bool anchorCollapsed(StoreEntry &, bool &inSync);
 

commit e9a21e7dfa23d75a979e377d59734ced94b8fc3c

Public StoreEntries must have Transients (pt. 2).
Previous attempt addressed the problem, creating Transients when
every public StoreEntry was placed into store_table and dropped the key
to private if creation was unsuccessful. This change addresses problems
caused by 1967a1be:

* Making entry private on possible Transients collision causes
  undesirable releasing the entry from stores.

* For now, we consider entries used in http transactions
  (Transients is on) for SMP mode, skipping different 'special' cases,
  e.g., 'ENTRY_SPECIAL' or ones, used within Store manager.

src/MemStore.cc
@@ -329,9 +329,11 @@ MemStore::get(const Store::CacheKey &cacheKey)
 
     const bool copied = copyFromShm(*e, index, *slot);
 
-    if (copied) {
-        e->insertPublicKey(cacheKey);
+    if (copied && e->preparePublicEntry(cacheKey)) {
         return e;
+    } else {
+        e->lock("MemStore::get");
+        e->unlock("MemStore::get");
     }
 
     debugs(20, 3, HERE << "mem-loading failed; freeing " << index);

src/Store.h
@@ -84,7 +84,10 @@ public:
     void abort();
     void makePublic(const KeyScope keyScope = ksDefault);
     void makePrivate(const bool shareable);
-    void insertPublicKey(const Store::CacheKey &);
+    /// Creates Transients entry for this entry if needed.
+    /// Use before placing the entry into a public SMP cache.
+    bool preparePublicEntry(const Store::CacheKey &);
+    bool preparePublicEntry();
     /// A low-level method just resetting "private key" flags.
     /// To avoid key inconsistency please use forcePublicKey()
     /// or similar instead.

src/client_side_reply.cc
@@ -316,10 +316,8 @@ clientReplyContext::processExpired()
                                  http->log_uri, http->request->flags, http->request->method);
         /* NOTE, don't call StoreEntry->lock(), storeCreateEntry() does it */
 
-        if (collapsingAllowed) {
+        if (collapsingAllowed && Store::Root().allowCollapsing(entry, http->request->flags, http->request->method)) {
             debugs(88, 5, "allow other revalidation requests to collapse on " << *entry);
-            Store::Root().allowCollapsing(entry, http->request->flags,
-                                          http->request->method);
             collapsedRevalidation = crInitiator;
         } else {
             collapsedRevalidation = crNone;

src/fs/rock/RockSwapDir.cc
@@ -68,10 +68,16 @@ Rock::SwapDir::get(const Store::CacheKey &cacheKey)
     StoreEntry *e = new StoreEntry();
     anchorEntry(*e, filen, *slot);
 
-    e->insertPublicKey(cacheKey);
-    trackReferences(*e);
+    if (e->preparePublicEntry(cacheKey)) {
+        e->hashInsert(cacheKey.key);
+        trackReferences(*e);
+        return e;
+    } else {
+        e->lock("MemStore::get");
+        e->unlock("MemStore::get");
+    }
 
-    return e;
+    return nullptr;
     // the disk entry remains open for reading, protected from modifications
 }
 

src/fs/ufs/UFSSwapDir.cc
@@ -818,7 +818,7 @@ Fs::Ufs::UFSSwapDir::addDiskRestore(const cache_key * key,
     mapBitSet(e->swap_filen);
     cur_size += fs.blksize * sizeInBlocks(e->swap_file_sz);
     ++n_disk_objects;
-    e->insertPublicKey(Store::CacheKey(key)); /* do it after we clear KEY_PRIVATE */
+    e->hashInsert(key); /* do it after we clear KEY_PRIVATE */
     replacementAdd (e);
     return e;
 }

src/http.cc
@@ -959,6 +959,8 @@ HttpStateData::haveParsedReplyHeaders()
 
         case ReuseDecision::cachePositively:
             entry->makePublic();
+            if (!entry->preparePublicEntry())
+                entry->makePrivate(true);
             break;
 
         case ReuseDecision::cacheNegatively:

src/store.cc
@@ -160,16 +160,23 @@ StoreEntry::clearPrivate()
     shareableWhenPrivate = false;
 }
 
-void
-StoreEntry::insertPublicKey(const Store::CacheKey &cacheKey)
+bool
+StoreEntry::preparePublicEntry(const Store::CacheKey &cacheKey)
 {
-    debugs(20, 3, "Inserting public key " << *this << " key '" << storeKeyText(cacheKey.key) << "'");
-    hashInsert(cacheKey.key);
-    if (!EBIT_TEST(flags, ENTRY_SPECIAL)) {
-        if (Store::Root().transientsAvailable() && !hasTransients() &&
-                !Store::Root().createTransientsEntry(this, cacheKey))
-            setPrivateKey(true, true);
-    }
+    if (EBIT_TEST(flags, ENTRY_SPECIAL))
+        return true;
+
+    if (!Store::Root().transientsAvailable() || hasTransients())
+        return true;
+
+    return Store::Root().createTransientsEntry(this, cacheKey);
+}
+
+bool
+StoreEntry::preparePublicEntry()
+{
+    return preparePublicEntry(Store::CacheKey(reinterpret_cast<cache_key*>(key),
+			SBuf(mem_obj->storeId()), mem_obj->method));
 }
 
 void
@@ -687,7 +694,7 @@ StoreEntry::forcePublicKey(const cache_key *newkey)
     clearPrivate();
 
     assert(mem_obj->hasUris());
-    insertPublicKey(Store::CacheKey(newkey, SBuf(mem_obj->storeId()), mem_obj->method));
+    hashInsert(newkey);
 
     if (hasDisk())
         storeDirSwapLog(this, SWAP_LOG_ADD);

src/store/Controller.cc
@@ -575,21 +575,25 @@ Store::Controller::updateOnNotModified(StoreEntry *old, const StoreEntry &newer)
         swapDir->updateHeaders(old);
 }
 
-void
+bool
 Store::Controller::allowCollapsing(StoreEntry *e, const RequestFlags &reqFlags,
                                    const HttpRequestMethod &reqMethod)
 {
     const KeyScope keyScope = reqFlags.refresh ? ksRevalidation : ksDefault;
     e->makePublic(keyScope); // this is needed for both local and SMP collapsing
+    if (!e->preparePublicEntry()) {
+        e->makePrivate(true);
+        return false;
+    }
     debugs(20, 3, "may " << (transients && e->hasTransients() ?
-                             "SMP-" : "locally-") << "collapse " << *e);
+                "SMP-" : "locally-") << "collapse " << *e);
+    return true;
 }
 
 bool
 Store::Controller::createTransientsEntry(StoreEntry *e, const CacheKey &cacheKey)
 {
-    if (!transients)
-        return false;
+    assert(transients);
     if (e->hasTransients())
         return true;
 

src/store/Controller.h
@@ -72,7 +72,7 @@ public:
     void updateOnNotModified(StoreEntry *old, const StoreEntry &newer);
 
     /// makes the entry available for collapsing future requests
-    void allowCollapsing(StoreEntry *, const RequestFlags &, const HttpRequestMethod &);
+    bool allowCollapsing(StoreEntry *, const RequestFlags &, const HttpRequestMethod &);
 
     /// For each public StoreEntry creates corresponding Transients entry.
     /// Makes the StoreEntry private on Transients entry creation error.