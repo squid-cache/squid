squid-cache/squid

commit e4d9b3f1cc6c15934ab7729b906c5323753e081c

Adjusted entry purging code.
... according to changes, introduced in 1967a1be.
The work is not completed/fully tested yet.

src/Transients.cc
@@ -219,7 +219,7 @@ Transients::findCollapsed(const sfileno index)
 }
 
 bool
-Transients::startWriting(StoreEntry *e, const HttpRequestMethod &reqMethod)
+Transients::startWriting(StoreEntry *e, const Store::CacheKey &cacheKey)
 {
     assert(e);
     assert(e->mem_obj);
@@ -238,7 +238,7 @@ Transients::startWriting(StoreEntry *e, const HttpRequestMethod &reqMethod)
     }
 
     try {
-        if (copyToShm(*e, index, reqMethod)) {
+        if (copyToShm(*e, index, cacheKey)) {
             slot->set(*e);
             e->mem_obj->xitTable.io = MemObject::ioWriting;
             e->mem_obj->xitTable.index = index;
@@ -255,7 +255,7 @@ Transients::startWriting(StoreEntry *e, const HttpRequestMethod &reqMethod)
                ' ' << *e << ": " << x.what());
         // fall through to the error handling code
     }
-
+    debugs(20, 1, "returning false " << *e);
     map->abortWriting(index);
     return false;
 }
@@ -263,18 +263,16 @@ Transients::startWriting(StoreEntry *e, const HttpRequestMethod &reqMethod)
 /// copies all relevant local data to shared memory
 bool
 Transients::copyToShm(const StoreEntry &e, const sfileno index,
-                      const HttpRequestMethod &reqMethod)
+        const Store::CacheKey &cacheKey)
 {
     TransientsMapExtras::Item &extra = extras->items[index];
 
-    const char *url = e.url();
-    const size_t urlLen = strlen(url);
-    Must(urlLen < sizeof(extra.url)); // we have space to store it all, plus 0
-    strncpy(extra.url, url, sizeof(extra.url));
+    Must(cacheKey.storeId.length() < sizeof(extra.url)); // we have space to store it all, plus 0
+    const int urlLen = cacheKey.storeId.copy(&extra.url[0], sizeof(extra.url));
     extra.url[urlLen] = '\0';
 
-    Must(reqMethod != Http::METHOD_OTHER);
-    extra.reqMethod = reqMethod.id();
+    Must(cacheKey.method != Http::METHOD_OTHER);
+    extra.reqMethod = cacheKey.method.id();
 
     return true;
 }
@@ -318,9 +316,8 @@ Transients::completeWriting(const StoreEntry &e)
         // there will be no more updates from us after this, so we must prevent
         // future readers from joining. Making the entry complete() is sufficient
         // because Transients::get() does not return completed entries.
-        map->closeForWriting(e.mem_obj->xitTable.index);
-        e.mem_obj->xitTable.index = -1;
-        e.mem_obj->xitTable.io = MemObject::ioDone;
+        map->closeForWriting(e.mem_obj->xitTable.index, true);
+        e.mem_obj->xitTable.io = MemObject::ioReading;
     }
 }
 

src/client_side_reply.cc
@@ -905,38 +905,16 @@ clientReplyContext::purgeRequestFindObjectToPurge()
 void
 purgeEntriesByUrl(HttpRequest * req, const char *url)
 {
-#if USE_HTCP
-    bool get_or_head_sent = false;
-#endif
-
     for (HttpRequestMethod m(Http::METHOD_NONE); m != Http::METHOD_ENUM_END; ++m) {
         if (m.respMaybeCacheable()) {
             const cache_key *key = storeKeyPublic(url, m);
             debugs(88, 5, m << ' ' << url << ' ' << storeKeyText(key));
-            Store::CacheKey cacheKey(key, SBuf(url), m);
-            if (StoreEntry *entry = Store::Root().get(cacheKey)) {
-                entry->lock("purgeEntriesByUrl");
 #if USE_HTCP
-                neighborsHtcpClear(entry, url, req, m, HTCP_CLR_INVALIDATION);
-                if (m == Http::METHOD_GET || m == Http::METHOD_HEAD) {
-                    get_or_head_sent = true;
-                }
+            neighborsHtcpClear(nullptr, url, req, m, HTCP_CLR_INVALIDATION);
 #endif
-                // entry->release() notifies waiting workers, including this one
-                entry->release(true);
-
-                entry->unlock("purgeEntriesByUrl");
-            } else {
-                Store::Root().unlinkByKeyIfFound(key); // does not broadcast but there were no waiting workers
-            }
+            Store::Root().unlinkByKeyIfFound(key);
         }
     }
-
-#if USE_HTCP
-    if (!get_or_head_sent) {
-        neighborsHtcpClear(NULL, url, req, HttpRequestMethod(Http::METHOD_GET), HTCP_CLR_INVALIDATION);
-    }
-#endif
 }
 
 void

src/store.cc
@@ -165,9 +165,11 @@ StoreEntry::insertPublicKey(const Store::CacheKey &cacheKey)
 {
     debugs(20, 3, "Inserting public key " << *this << " key '" << storeKeyText(cacheKey.key) << "'");
     hashInsert(cacheKey.key);
-    if (Store::Root().transientsAvailable() && !hasTransients() &&
-            !Store::Root().createTransientsEntry(this, cacheKey))
-        setPrivateKey(true, false);
+    if (!EBIT_TEST(flags, ENTRY_SPECIAL)) {
+        if (Store::Root().transientsAvailable() && !hasTransients() &&
+                !Store::Root().createTransientsEntry(this, cacheKey))
+            setPrivateKey(true, true);
+    }
 }
 
 void
@@ -684,6 +686,7 @@ StoreEntry::forcePublicKey(const cache_key *newkey)
 
     clearPrivate();
 
+    assert(mem_obj->hasUris());
     insertPublicKey(Store::CacheKey(newkey, SBuf(mem_obj->storeId()), mem_obj->method));
 
     if (hasDisk())

src/store/Controller.cc
@@ -320,6 +320,26 @@ Store::Controller::get(const CacheKey &cacheKey)
     return nullptr;
 }
 
+// TODO: partially duplicates Controller::find().
+StoreEntry*
+Store::Controller::intransitEntry(const CacheKey &cacheKey)
+{
+    if (markedForDeletion(cacheKey.key)) {
+        debugs(20, 3, "ignoring marked " << storeKeyText(cacheKey.key));
+        return nullptr;
+    }
+
+    if (StoreEntry *e = static_cast<StoreEntry*>(hash_lookup(store_table, cacheKey.key))) {
+        assert(e->hasTransients());
+        return e;
+    }
+
+    if (transients)
+        return transients->get(cacheKey);
+
+    return nullptr;
+}
+
 /// Internal method to implements the guts of the Store::get() API:
 /// returns an in-transit or cached object with a given key, if any.
 StoreEntry *
@@ -343,6 +363,8 @@ Store::Controller::find(const CacheKey &cacheKey)
     if (transients) {
         if (StoreEntry *e = transients->get(cacheKey)) {
             debugs(20, 3, "got shared in-transit entry: " << *e);
+            if (!e->mem_obj->smpCollapsed)
+                return e;
             bool inSync = false;
             const bool found = anchorCollapsed(*e, inSync);
             if (!found || inSync)
@@ -392,15 +414,11 @@ Store::Controller::markForUnlink(StoreEntry &e)
 void
 Store::Controller::unlinkByKeyIfFound(const cache_key *key)
 {
-    if (StoreEntry *entry = static_cast<StoreEntry*>(hash_lookup(store_table, key))) {
-        debugs(20, 3, "got in-transit entry: " << *entry);
-        // unlink all Store entries with entry->key, even unattached ones
-        entry->release(true);
-        return;
+    if (StoreEntry *entry = intransitEntry(CacheKey(key))) {
+        assert(entry->hasTransients());
+        transients->markForUnlink(*entry);
     }
 
-    if (transients)
-        transients->unlinkByKeyIfFound(key);
     if (memStore)
         memStore->unlinkByKeyIfFound(key);
     if (swapDir)
@@ -574,10 +592,8 @@ Store::Controller::createTransientsEntry(StoreEntry *e, const CacheKey &cacheKey
         return false;
     if (e->hasTransients())
         return true;
-    if (!cacheKey.hasUris())
-        return false;
 
-    return transients->startWriting(e, cacheKey.method);
+    return transients->startWriting(e, cacheKey);
 }
 
 void
@@ -604,13 +620,11 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
     bool waitingToBeFreed = false;
     transients->status(*collapsed, abortedByWriter, waitingToBeFreed);
 
-    if (waitingToBeFreed) {
+    if (waitingToBeFreed && transients->collapsedWriter(*collapsed)) {
         debugs(20, 3, "will release " << *collapsed << " due to waitingToBeFreed");
         collapsed->release(true); // may already be marked
+        return;
     }
-
-    if (transients->collapsedWriter(*collapsed))
-        return; // readers can only change our waitingToBeFreed flag
 
     assert(transients->collapsedReader(*collapsed));
 
@@ -647,6 +661,8 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
     if (inSync) {
         debugs(20, 5, "synced " << *collapsed);
         collapsed->invokeHandlers();
+        if (waitingToBeFreed)
+            collapsed->release(true);
         return;
     }
 
@@ -655,6 +671,9 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
         collapsed->abort();
         return;
     }
+
+    if (waitingToBeFreed)
+        collapsed->release(true);
 
     // the entry is still not in one of the caches
     debugs(20, 7, "waiting " << *collapsed);

src/store/Controller.h
@@ -78,6 +78,8 @@ public:
     /// Makes the StoreEntry private on Transients entry creation error.
     bool createTransientsEntry(StoreEntry *e, const CacheKey &cacheKey);
 
+    StoreEntry *intransitEntry(const CacheKey &);
+
     /// marks the entry completed for collapsed requests
     void transientsCompleteWriting(StoreEntry &);
 

src/Transients.h
@@ -40,7 +40,7 @@ public:
     StoreEntry *findCollapsed(const sfileno xitIndex);
 
     /// add an in-transit entry suitable for collapsing future requests
-    bool startWriting(StoreEntry *e, const HttpRequestMethod &reqMethod);
+    bool startWriting(StoreEntry *e, const Store::CacheKey &cacheKey);
 
     /// called when the in-transit entry has been successfully cached
     void completeWriting(const StoreEntry &e);
@@ -91,7 +91,7 @@ public:
 
 protected:
     StoreEntry *copyFromShm(const sfileno index);
-    bool copyToShm(const StoreEntry &e, const sfileno index, const HttpRequestMethod &reqMethod);
+    bool copyToShm(const StoreEntry &e, const sfileno index, const Store::CacheKey &cacheKey);
 
     // Ipc::StoreMapCleaner API
     virtual void noteFreeMapSlice(const Ipc::StoreMapSliceId sliceId) override;

commit 548f9a8bace95c713d3abf81c7bd8a87e50793d3

Public StoreEntries must have Transients (pt. 3).
Fixed several problems:

* Disk/Rock code did not close Transients entry for writing. This caused
  unexpected writer entry aborting and overall collapsing fail. The
  problem affects configurations with cache_mem=0.

* Create transients entry for situation, when storeCreateEntry() returns
  public entry, except when collapsing is allowed.

* Do not mark for deletion just created private entries. This may happen,
  e.g., when there is a race between two concurrent clients which is not a
  reason for clearing shared cache.

* Create memory object within Rock::SwapDir::get(), required when
  creating transients entry.

* Fixed an infinite loop when StoreEntry::checkDisk() fails.

* Avoid problems with unlocked entries within syncCollapsed(): simply
  destroy them.

* Fixed memory leak in MemStore::get(), destroying useless StoreEntry.

* A typo within Transients::abandon(), caused by e4d9b3f1.

src/MemStore.cc
@@ -329,15 +329,14 @@ MemStore::get(const Store::CacheKey &cacheKey)
 
     const bool copied = copyFromShm(*e, index, *slot);
 
-    if (copied && e->preparePublicEntry(cacheKey)) {
+    if (copied && e->preparePublicEntry(cacheKey))
         return e;
-    } else {
-        e->lock("MemStore::get");
-        e->unlock("MemStore::get");
-    }
+    else
+        destroyStoreEntry(static_cast<hash_link *>(e));
 
     debugs(20, 3, HERE << "mem-loading failed; freeing " << index);
     map->freeEntry(index); // do not let others into the same trap
+
     return NULL;
 }
 

src/Store.h
@@ -101,7 +101,8 @@ public:
     /// Either fills this entry with private key or changes the existing key
     /// from public to private.
     /// \param permanent whether this entry should be private forever.
-    void setPrivateKey(const bool shareable, const bool permanent);
+    void setPrivateKey(const bool shareable, const bool permanent, const bool shouldMark = true);
+
     void expireNow();
     /// Makes the StoreEntry private and marks the corresponding entry
     /// for eventual removal from the Store.

src/Transients.cc
@@ -288,7 +288,7 @@ Transients::abandon(const StoreEntry &e)
 {
     assert(e.mem_obj && map);
     // avoid useless broadcasts
-    if (map->freeEntry(e.mem_obj->xitTable.index)); // just marks the locked entry
+    if (map->freeEntry(e.mem_obj->xitTable.index)) // just marks the locked entry
         CollapsedForwarding::Broadcast(e, true);
     // We do not unlock the entry now because the problem is most likely with
     // the server resource rather than a specific cache writer, so we want to
@@ -311,14 +311,13 @@ Transients::status(const StoreEntry &entry, bool &aborted, bool &waitingToBeFree
 void
 Transients::completeWriting(const StoreEntry &e)
 {
-    if (e.hasTransients()) {
-        assert(collapsedWriter(e));
-        // there will be no more updates from us after this, so we must prevent
-        // future readers from joining. Making the entry complete() is sufficient
-        // because Transients::get() does not return completed entries.
-        map->closeForWriting(e.mem_obj->xitTable.index, true);
-        e.mem_obj->xitTable.io = MemObject::ioReading;
-    }
+    assert(e.hasTransients());
+    assert(collapsedWriter(e));
+    // there will be no more updates from us after this, so we must prevent
+    // future readers from joining. Making the entry complete() is sufficient
+    // because Transients::get() does not return completed entries.
+    map->closeForWriting(e.mem_obj->xitTable.index, true);
+    e.mem_obj->xitTable.io = MemObject::ioReading;
 }
 
 int

src/client_side_reply.cc
@@ -316,11 +316,17 @@ clientReplyContext::processExpired()
                                  http->log_uri, http->request->flags, http->request->method);
         /* NOTE, don't call StoreEntry->lock(), storeCreateEntry() does it */
 
-        if (collapsingAllowed && Store::Root().allowCollapsing(entry, http->request->flags, http->request->method)) {
-            debugs(88, 5, "allow other revalidation requests to collapse on " << *entry);
-            collapsedRevalidation = crInitiator;
-        } else {
-            collapsedRevalidation = crNone;
+        if (collapsingAllowed)
+            Store::Root().allowCollapsing(entry, http->request->flags, http->request->method);
+
+        collapsedRevalidation = crNone;
+        if (!EBIT_TEST(entry->flags, KEY_PRIVATE)) {
+            if (!entry->preparePublicEntry())
+                entry->setPrivateKey(false, !http->request->flags.cachable, false);
+            else if (collapsingAllowed) {
+                debugs(88, 5, "allow other revalidation requests to collapse on " << *entry);
+                collapsedRevalidation = crInitiator;
+            }
         }
     }
 
@@ -2283,6 +2289,9 @@ clientReplyContext::createStoreEntry(const HttpRequestMethod& m, RequestFlags re
         // make the entry available for future requests now
         Store::Root().allowCollapsing(e, reqFlags, m);
     }
+
+    if (!EBIT_TEST(e->flags, KEY_PRIVATE) && !e->preparePublicEntry())
+        e->setPrivateKey(false, !reqFlags.cachable, false);
 
     sc = storeClientListAdd(e, this);
 

src/fs/rock/RockSwapDir.cc
@@ -66,15 +66,16 @@ Rock::SwapDir::get(const Store::CacheKey &cacheKey)
 
     // create a brand new store entry and initialize it with stored basics
     StoreEntry *e = new StoreEntry();
+    e->createMemObject();
     anchorEntry(*e, filen, *slot);
 
+    e->hashInsert(cacheKey.key);
     if (e->preparePublicEntry(cacheKey)) {
-        e->hashInsert(cacheKey.key);
         trackReferences(*e);
         return e;
     } else {
-        e->lock("MemStore::get");
-        e->unlock("MemStore::get");
+        e->hashDelete();
+        destroyStoreEntry(static_cast<hash_link *>(e));
     }
 
     return nullptr;

src/store.cc
@@ -595,7 +595,7 @@ getKeyCounter(void)
  * concept'.
  */
 void
-StoreEntry::setPrivateKey(const bool shareable, const bool permanent)
+StoreEntry::setPrivateKey(const bool shareable, const bool permanent, const bool shouldMark)
 {
     debugs(20, 3, " private key " << *this);
     if (permanent)
@@ -607,7 +607,8 @@ StoreEntry::setPrivateKey(const bool shareable, const bool permanent)
         return;
 
     if (key) {
-        Store::Root().markForUnlink(*this);  // all caches/workers will know
+        if (shouldMark)
+            Store::Root().markForUnlink(*this);  // all caches/workers will know
 
         // TODO: move into SwapDir::markForUnlink() already called by Root()
         if (hasDisk())
@@ -2141,6 +2142,10 @@ std::ostream &operator <<(std::ostream &os, const StoreEntry &e)
         os << 't' << e.mem_obj->xitTable.index;
     if (e.hasMemStore())
         os << 'm' << e.mem_obj->memCache.index;
+    // It is not possible to use e.hasDisk() here because
+    // it may end up with infinite loop.
+    if (e.swap_filen > -1 || e.swap_dirn > -1)
+        os << 'd' << e.swap_filen << '@' << e.swap_dirn;
     if (e.hasDisk())
         os << 'd' << e.swap_filen << '@' << e.swap_dirn;
 

src/store/Controller.cc
@@ -499,7 +499,7 @@ Store::Controller::transientsAbandon(StoreEntry &e)
 void
 Store::Controller::transientsCompleteWriting(StoreEntry &e)
 {
-    if (transients && e.hasTransients())
+    if (transients && e.hasTransients() && transients->collapsedWriter(e))
         transients->completeWriting(e);
 }
 
@@ -575,19 +575,14 @@ Store::Controller::updateOnNotModified(StoreEntry *old, const StoreEntry &newer)
         swapDir->updateHeaders(old);
 }
 
-bool
+void
 Store::Controller::allowCollapsing(StoreEntry *e, const RequestFlags &reqFlags,
                                    const HttpRequestMethod &reqMethod)
 {
     const KeyScope keyScope = reqFlags.refresh ? ksRevalidation : ksDefault;
     e->makePublic(keyScope); // this is needed for both local and SMP collapsing
-    if (!e->preparePublicEntry()) {
-        e->makePrivate(true);
-        return false;
-    }
     debugs(20, 3, "may " << (transients && e->hasTransients() ?
                 "SMP-" : "locally-") << "collapse " << *e);
-    return true;
 }
 
 bool
@@ -611,6 +606,13 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
         return;
     }
 
+    if (!collapsed->locked()) {
+        debugs(20, 3, "will release unlocked " << *collapsed);
+        // should destroy unlocked entry
+        collapsed->release();
+        return;
+    }
+
     assert(collapsed->mem_obj);
 
     if (EBIT_TEST(collapsed->flags, ENTRY_ABORTED)) {
@@ -624,11 +626,13 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
     bool waitingToBeFreed = false;
     transients->status(*collapsed, abortedByWriter, waitingToBeFreed);
 
-    if (waitingToBeFreed && transients->collapsedWriter(*collapsed)) {
+    if (waitingToBeFreed) {
         debugs(20, 3, "will release " << *collapsed << " due to waitingToBeFreed");
         collapsed->release(true); // may already be marked
-        return;
     }
+
+    if (transients->collapsedWriter(*collapsed))
+        return; // readers can only change our waitingToBeFreed flag
 
     assert(transients->collapsedReader(*collapsed));
 
@@ -665,8 +669,6 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
     if (inSync) {
         debugs(20, 5, "synced " << *collapsed);
         collapsed->invokeHandlers();
-        if (waitingToBeFreed)
-            collapsed->release(true);
         return;
     }
 
@@ -675,9 +677,6 @@ Store::Controller::syncCollapsed(const sfileno xitIndex)
         collapsed->abort();
         return;
     }
-
-    if (waitingToBeFreed)
-        collapsed->release(true);
 
     // the entry is still not in one of the caches
     debugs(20, 7, "waiting " << *collapsed);

src/store/Controller.h
@@ -72,7 +72,7 @@ public:
     void updateOnNotModified(StoreEntry *old, const StoreEntry &newer);
 
     /// makes the entry available for collapsing future requests
-    bool allowCollapsing(StoreEntry *, const RequestFlags &, const HttpRequestMethod &);
+    void allowCollapsing(StoreEntry *, const RequestFlags &, const HttpRequestMethod &);
 
     /// For each public StoreEntry creates corresponding Transients entry.
     /// Makes the StoreEntry private on Transients entry creation error.

src/store_swapout.cc
@@ -333,6 +333,7 @@ storeSwapOutFileClosed(void *data, int errflag, StoreIOState::Pointer self)
         ++statCounter.swap.outs;
     }
 
+    Store::Root().transientsCompleteWriting(*e);
     debugs(20, 3, "storeSwapOutFileClosed: " << __FILE__ << ":" << __LINE__);
     mem->swapout.sio = NULL;
     e->unlock("storeSwapOutFileClosed");