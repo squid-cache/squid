squid-cache/squid

commit d81657759f3ac9f3e688b4b2e8051166a23f01e1

Fixed %<Hs, %<pt, %<tt, %<bs calculation bugs for error responses (#79)
This change started as a %<Hs fix described in the first bullet but
subsequent testing exposed the bug described in the second bullet,
fixing which resulted in other related fixes/improvements:

* Fixed %<Hs for received CONNECT errors: Correctly store the response
  status code for %<Hs logging (instead of misplacing it in the %>Hs
  location that was later overwritten with the correct to-client value).

* Fixed %<pt and %<tt for received CONNECT errors: Squid tunneling code
  was missing message I/O timing maintenance required for those %codes.

* Probably fixed %<bs logging bug on forwarding retries: Squid did not
  clear the (bodyBytesRead) counter between retries.

* Possibly improved %<Hs, %<pt, %<tt, %<bs logging on SslBump errors:
  request->hier member did not copy all the sslServerBump->request->hier
  details when generating an error response in
  ConnStateData::serveDelayedError().

* Probably fixed a server.all.kbytes_out and server.other.kbytes_out
  statistics bug: Squid missed written CONNECT request header bytes.

Also improved HierarchyLogEntry-related code: Reduced code duplication,
removed unnecessary destructor, and described the class. Removed
peer_http_request_sent timestamp because it ought to be equal to the
last request write time (now peer_last_write_).

TODO: Relay (expected) peer CONNECT error responses to users (instead of
hiding them behind a Squid-generated ERR_CONNECT_FAIL) and support %<h.

src/store.cc
@@ -1553,10 +1553,10 @@ StoreEntry::timestampsSet()
 
     // compensate for Squid-to-server and server-to-Squid delays
     if (mem_obj && mem_obj->request) {
-        const time_t request_sent =
-            mem_obj->request->hier.peer_http_request_sent.tv_sec;
-        if (0 < request_sent && request_sent < squid_curtime)
-            served_date -= (squid_curtime - request_sent);
+        struct timeval responseTime;
+        if (mem_obj->request->hier.peerResponseTime(responseTime) &&
+            responseTime.tv_sec < squid_curtime)
+            served_date -= (squid_curtime - responseTime.tv_sec);
     }
 
     time_t exp = 0;

src/FwdState.cc
@@ -850,9 +850,9 @@ void
 FwdState::syncHierNote(const Comm::ConnectionPointer &server, const char *host)
 {
     if (request)
-        request->hier.note(server, host);
+        request->hier.resetPeerNotes(server, host);
     if (al)
-        al->hier.note(server, host);
+        al->hier.resetPeerNotes(server, host);
 }
 
 /**

src/HierarchyLogEntry.h
@@ -17,16 +17,25 @@
 #include "PingData.h"
 #include "rfc2181.h"
 
+/// Maintains peer selection details and peer I/O stats.
+/// Here, "peer" is an origin server or CachePeer.
 class HierarchyLogEntry
 {
 
 public:
     HierarchyLogEntry();
-    ~HierarchyLogEntry() { tcpServer = NULL; };
 
-    /// Record details from a new server connection.
-    /// call this whenever the destination server changes.
-    void note(const Comm::ConnectionPointer &server, const char *requestedHost);
+    /// Start recording new origin server or cache peer connection details.
+    /// Call this when trying to connect to a peer.
+    void resetPeerNotes(const Comm::ConnectionPointer &server, const char *requestedHost);
+
+    /// Account for a TCP peer read. Maintains peer response time stats (%<pt).
+    /// Call this after each successful peer socket read(2).
+    void notePeerRead();
+
+    /// Account for a TCP peer write. Maintains peer response time stats (%<pt).
+    /// Call this after each peer socket write(2), including failed ones.
+    void notePeerWrite();
 
     /// Start recording total time spent communicating with peers
     void startPeerClock();
@@ -36,8 +45,13 @@ public:
      */
     void stopPeerClock(const bool force);
 
-    /// Return the total time spent communicating with peers
-    void totalResponseTime(struct timeval &responseTime);
+    /// Estimates response generation and sending delay at the last peer.
+    /// \returns whether the estimate (stored in `responseTime`) is available.
+    bool peerResponseTime(struct timeval &responseTime);
+
+    /// Estimates the total time spent communicating with peers.
+    /// \returns whether the estimate (stored in `responseTime`) is available.
+    bool totalResponseTime(struct timeval &responseTime);
 
 public:
     hier_code code;
@@ -53,13 +67,15 @@ public:
     struct timeval store_complete_stop;
 
     Http::StatusCode peer_reply_status; ///< last HTTP status code received
-    timeval peer_http_request_sent; ///< last peer finished writing req
-    struct timeval peer_response_time; ///< last peer response delay
     Comm::ConnectionPointer tcpServer; ///< TCP/IP level details of the last peer/server connection
     int64_t bodyBytesRead;  ///< number of body bytes received from the next hop or -1
 
 private:
+    void clearPeerNotes();
+
     timeval firstConnStart_; ///< first connection use among all peers
+    struct timeval peer_last_read_; ///< time of the last read from the last peer
+    struct timeval peer_last_write_; ///< time of the last write to the last peer
     struct timeval totalResponseTime_; ///< cumulative for all peers
 };
 

src/client_side.cc
@@ -1537,9 +1537,7 @@ bool ConnStateData::serveDelayedError(Http::Stream *context)
                 clientReplyContext *repContext = dynamic_cast<clientReplyContext *>(node->data.getRaw());
                 assert (repContext);
 
-                // Fill the server IP and hostname for error page generation.
-                HttpRequest::Pointer const & peekerRequest = sslServerBump->request;
-                request->hier.note(peekerRequest->hier.tcpServer, request->url.host());
+                request->hier = sslServerBump->request->hier;
 
                 // Create an error object and fill it
                 ErrorState *err = new ErrorState(ERR_SECURE_CONNECT_FAIL, Http::scServiceUnavailable, request);

src/clients/Client.cc
@@ -359,6 +359,9 @@ Client::sentRequestBody(const CommIoCbParams &io)
         return; // do nothing;
     }
 
+    // both successful and failed writes affect response times
+    request->hier.notePeerWrite();
+
     if (io.flag) {
         debugs(11, DBG_IMPORTANT, "sentRequestBody error: FD " << io.fd << ": " << xstrerr(io.xerrno));
         ErrorState *err;

src/format/Format.cc
@@ -592,17 +592,17 @@ Format::Format::assemble(MemBuf &mb, const AccessLogEntry::Pointer &al, int logS
             break;
 
         case LFT_PEER_RESPONSE_TIME:
-            if (al->hier.peer_response_time.tv_sec != -1) {
-                outtv = al->hier.peer_response_time;
+            struct timeval peerResponseTime;
+            if (al->hier.peerResponseTime(peerResponseTime)) {
+                outtv = peerResponseTime;
                 doMsec = 1;
             }
             break;
 
         case LFT_TOTAL_SERVER_SIDE_RESPONSE_TIME: {
-            timeval total_response_time;
-            al->hier.totalResponseTime(total_response_time);
-            if (total_response_time.tv_sec != -1) {
-                outtv = total_response_time;
+            struct timeval totalResponseTime;
+            if (al->hier.totalResponseTime(totalResponseTime)) {
+                outtv = totalResponseTime;
                 doMsec = 1;
             }
         }

src/http.cc
@@ -97,10 +97,6 @@ HttpStateData::HttpStateData(FwdState *theFwdState) :
     surrogateNoStore = false;
     serverConnection = fwd->serverConnection();
 
-    // reset peer response time stats for %<pt
-    request->hier.peer_http_request_sent.tv_sec = 0;
-    request->hier.peer_http_request_sent.tv_usec = 0;
-
     if (fwd->serverConnection() != NULL)
         _peer = cbdataReference(fwd->serverConnection()->getPeer());         /* might be NULL */
 
@@ -1189,12 +1185,7 @@ HttpStateData::readReply(const CommIoCbParams &io)
 
         ++ IOStats.Http.read_hist[bin];
 
-        // update peer response time stats (%<pt)
-        const timeval &sent = request->hier.peer_http_request_sent;
-        if (sent.tv_sec)
-            tvSub(request->hier.peer_response_time, sent, current_time);
-        else
-            request->hier.peer_response_time.tv_sec = -1;
+        request->hier.notePeerRead();
     }
 
         /* Continue to process previously read data */
@@ -1581,6 +1572,9 @@ HttpStateData::wroteLast(const CommIoCbParams &io)
     entry->mem_obj->checkUrlChecksum();
 #endif
 
+    // XXX: Keep in sync with Client::sentRequestBody().
+    // TODO: Extract common parts.
+
     if (io.size > 0) {
         fd_bytes(io.fd, io.size, FD_WRITE);
         statCounter.server.all.kbytes_out += io.size;
@@ -1589,6 +1583,9 @@ HttpStateData::wroteLast(const CommIoCbParams &io)
 
     if (io.flag == Comm::ERR_CLOSING)
         return;
+
+    // both successful and failed writes affect response times
+    request->hier.notePeerWrite();
 
     if (io.flag) {
         ErrorState *err = new ErrorState(ERR_WRITE_ERROR, Http::scBadGateway, fwd->request);
@@ -1620,7 +1617,6 @@ HttpStateData::sendComplete()
 
     commSetConnTimeout(serverConnection, Config.Timeout.read, timeoutCall);
     flags.request_sent = true;
-    request->hier.peer_http_request_sent = current_time;
 }
 
 void

src/log/access_log.cc
@@ -237,11 +237,7 @@ HierarchyLogEntry::HierarchyLogEntry() :
     store_complete_stop.tv_sec =0;
     store_complete_stop.tv_usec =0;
 
-    peer_http_request_sent.tv_sec = 0;
-    peer_http_request_sent.tv_usec = 0;
-
-    peer_response_time.tv_sec = -1;
-    peer_response_time.tv_usec = 0;
+    clearPeerNotes();
 
     totalResponseTime_.tv_sec = -1;
     totalResponseTime_.tv_usec = 0;
@@ -251,8 +247,10 @@ HierarchyLogEntry::HierarchyLogEntry() :
 }
 
 void
-HierarchyLogEntry::note(const Comm::ConnectionPointer &server, const char *requestedHost)
+HierarchyLogEntry::resetPeerNotes(const Comm::ConnectionPointer &server, const char *requestedHost)
 {
+    clearPeerNotes();
+
     tcpServer = server;
     if (tcpServer == NULL) {
         code = HIER_NONE;
@@ -267,6 +265,31 @@ HierarchyLogEntry::note(const Comm::ConnectionPointer &server, const char *reque
             xstrncpy(host, requestedHost, sizeof(host));
         }
     }
+}
+
+/// forget previous notePeerRead() and notePeerWrite() calls (if any)
+void
+HierarchyLogEntry::clearPeerNotes()
+{
+    peer_last_read_.tv_sec = 0;
+    peer_last_read_.tv_usec = 0;
+
+    peer_last_write_.tv_sec = 0;
+    peer_last_write_.tv_usec = 0;
+
+    bodyBytesRead = -1;
+}
+
+void
+HierarchyLogEntry::notePeerRead()
+{
+    peer_last_read_ = current_time;
+}
+
+void
+HierarchyLogEntry::notePeerWrite()
+{
+    peer_last_write_ = current_time;
 }
 
 void
@@ -290,7 +313,34 @@ HierarchyLogEntry::stopPeerClock(const bool force)
         tvSub(totalResponseTime_, firstConnStart_, current_time);
 }
 
-void
+bool
+HierarchyLogEntry::peerResponseTime(struct timeval &responseTime)
+{
+    // no I/O whatsoever
+    if (peer_last_write_.tv_sec <= 0 && peer_last_read_.tv_sec <= 0)
+        return false;
+
+    // accommodate read without (completed) write
+    const auto last_write = peer_last_write_.tv_sec > 0 ?
+        peer_last_write_ : peer_last_read_;
+
+    // accommodate write without (completed) read
+    const auto last_read = peer_last_read_.tv_sec > 0 ?
+        peer_last_read_ : peer_last_write_;
+
+    tvSub(responseTime, last_write, last_read);
+    // The peer response time (%<pt) stopwatch is currently defined to start
+    // when we wrote the entire request. Thus, if we wrote something after the
+    // last read, report zero peer response time.
+    if (responseTime.tv_sec < 0) {
+        responseTime.tv_sec = 0;
+        responseTime.tv_usec = 0;
+    }
+
+    return true;
+}
+
+bool
 HierarchyLogEntry::totalResponseTime(struct timeval &responseTime)
 {
     // This should not really happen, but there may be rare code
@@ -300,6 +350,7 @@ HierarchyLogEntry::totalResponseTime(struct timeval &responseTime)
         stopPeerClock(false);
 
     responseTime = totalResponseTime_;
+    return responseTime.tv_sec >= 0 && responseTime.tv_usec >= 0;
 }
 
 static void

commit bd2fc42bd8a5ede3b7569322229cca1ea36d5278

Restored Squid ability to serve pure hits broken by d816577.
Since PR #79, most incoming objects got wrong (zero) served_date,
becoming immediately stale and usually requiring revalidation.

src/store.cc
@@ -1554,9 +1554,8 @@ StoreEntry::timestampsSet()
     // compensate for Squid-to-server and server-to-Squid delays
     if (mem_obj && mem_obj->request) {
         struct timeval responseTime;
-        if (mem_obj->request->hier.peerResponseTime(responseTime) &&
-                responseTime.tv_sec < squid_curtime)
-            served_date -= (squid_curtime - responseTime.tv_sec);
+        if (mem_obj->request->hier.peerResponseTime(responseTime))
+            served_date -= responseTime.tv_sec;
     }
 
     time_t exp = 0;