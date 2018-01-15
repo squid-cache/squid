46391e02f72f5afd96d64d81a3b3eff2c8a10a50
Amos Jeffries <amosjeffries@squid-cache.org>
2018-01-16 02:22:27 +1300

    ESI: remove custom parser
    Alex Rousskov:
      let's consider removing the custom ESI parser from Squid. It is of
    terrible quality and "nobody" is testing ESI code when things change. Is
    the CVE risk worth supporting few platforms that do not have the right
    parser libraries?

--- src/esi/Module.cc
+++ src/esi/Module.cc
@@ -7,14 +7,12 @@
  */
 
 #include "squid.h"
-#include "esi/CustomParser.h"
 #include "esi/Libxml2Parser.h"
 #include "esi/Module.h"
 /* include for esi/ExpatParser.h must follow esi/Libxml2Parser.h */
 /* do not remove this comment, as it acts as barrier for the autmatic sorting */
 #include "esi/ExpatParser.h"
 
-static ESIParser::Register *prCustom = 0;
 #if HAVE_LIBXML2
 static ESIParser::Register *prLibxml = 0;
 #endif
@@ -24,34 +22,29 @@ static ESIParser::Register *prExpat = 0;
 
 void Esi::Init()
 {
-    assert(!prCustom); // we should be called once
-
-    prCustom = new ESIParser::Register("custom", &ESICustomParser::NewParser);
-
 #if HAVE_LIBXML2
+    assert(!prLibxml); // we should be called once
     prLibxml = new ESIParser::Register("libxml2", &ESILibxml2Parser::NewParser);
 #endif
 
 #if HAVE_LIBEXPAT
+    assert(!prLibxml); // we should be called once
     prExpat = new ESIParser::Register("expat", &ESIExpatParser::NewParser);
 #endif
 }
 
 void Esi::Clean()
 {
-    assert(prCustom); // we should be called once, and only after Init()
-
 #if HAVE_LIBEXPAT
+    assert(prExpat); // we should be called once, and only after Init()
     delete prExpat;
     prExpat = NULL;
 #endif
 
 #if HAVE_LIBXML2
+    assert(prLibxml); // we should be called once, and only after Init()
     delete prLibxml;
     prLibxml = NULL;
 #endif
-
-    delete prCustom;
-    prCustom = NULL;
 }
 
