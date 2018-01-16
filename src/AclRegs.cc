653d992747bfda4f6eb757354c1cc674492a21de
Andrey <rybakovandrey85@gmail.com>
2018-01-17 02:28:59 +0300

    Added clientside_mark ACL for checking CONNMARK (#111)
    Matches CONNMARK of accepted connections. Takes into account
    clientside_mark and qos_flows mark changes (because Squid-set marks are
    cached by Squid in conn->nfmark). Ignores 3rd-party marks set after
    Squid has accepted the connection from a client (because Squid never
    re-queries the connection to update/sync conn->nfmark).
    
    Also added a debugs()-friendly API to print hex values.

--- src/AclRegs.cc
+++ src/AclRegs.cc
@@ -33,6 +33,9 @@
 #include "acl/DestinationDomain.h"
 #include "acl/DestinationIp.h"
 #include "acl/DomainData.h"
+#if USE_LIBNETFILTERCONNTRACK
+#include "acl/ConnMark.h"
+#endif
 #if USE_AUTH
 #include "acl/ExtUser.h"
 #endif
@@ -157,6 +160,10 @@ Acl::Init()
     RegisterMaker("annotate_transaction", [](TypeName name)->ACL* { return new ACLStrategised<NotePairs::Entry*>(new ACLAnnotationData, new ACLAnnotateTransactionStrategy, name); });
     RegisterMaker("has", [](TypeName name)->ACL* {return new ACLStrategised<ACLChecklist *>(new ACLHasComponentData, new ACLHasComponentStrategy, name); });
     RegisterMaker("transaction_initiator", [](TypeName name)->ACL* {return new TransactionInitiator(name);});
+
+#if USE_LIBNETFILTERCONNTRACK
+    RegisterMaker("clientside_mark", [](TypeName name)->ACL* { return new Acl::ConnMark; });
+#endif
 
 #if USE_OPENSSL
     RegisterMaker("ssl_error", [](TypeName name)->ACL* { return new ACLStrategised<const Security::CertErrors *>(new ACLSslErrorData, new ACLSslErrorStrategy, name); });
