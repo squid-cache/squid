799b66d1964c2ad925f9e6cb71b1c287da3e16d7
Amos Jeffries <yadij@users.noreply.github.com>
2018-01-19 09:54:50 +1300

    ESI: remove custom parser (#128)
    Alex Rousskov:
      let's consider removing the custom ESI parser from Squid. It is of
    terrible quality and "nobody" is testing ESI code when things change. Is
    the CVE risk worth supporting few platforms that do not have the right
    parser libraries?
    
    * Fixed configure.ac tests for ESI libraries

--- src/esi/Module.cc
+++ src/esi/Module.cc
@@ -7,14 +7,12 @@
  */
 
 #include "squid.h"
-#include "esi/CustomParser.h"
 #include "esi/Libxml2Parser.h"
 #include "esi/Module.h"
 /* include for esi/ExpatParser.h must follow esi/Libxml2Parser.h */
 /* do not remove this comment, as it acts as barrier for the autmatic sorting */
 #include "esi/ExpatParser.h"
 
-static ESIParser::Register *prCustom = 0;
 #if HAVE_LIBXML2
 static ESIParser::Register *prLibxml = 0;
 #endif
@@ -24,23 +22,21 @@ static ESIParser::Register *prExpat = 0;
 
 void Esi::Init()
 {
-    assert(!prCustom); // we should be called once
-
-    prCustom = new ESIParser::Register("custom", &ESICustomParser::NewParser);
-
-#if HAVE_LIBXML2
-    prLibxml = new ESIParser::Register("libxml2", &ESILibxml2Parser::NewParser);
+    // register in reverse order of preference.
+    // The latest registered parser will be used as default.
+#if HAVE_LIBEXPAT
+    assert(!prExpat); // we should be called once
+    prExpat = new ESIParser::Register("expat", &ESIExpatParser::NewParser);
 #endif
 
-#if HAVE_LIBEXPAT
-    prExpat = new ESIParser::Register("expat", &ESIExpatParser::NewParser);
+#if HAVE_LIBXML2
+    assert(!prLibxml); // we should be called once
+    prLibxml = new ESIParser::Register("libxml2", &ESILibxml2Parser::NewParser);
 #endif
 }
 
 void Esi::Clean()
 {
-    assert(prCustom); // we should be called once, and only after Init()
-
 #if HAVE_LIBEXPAT
     delete prExpat;
     prExpat = NULL;
@@ -50,8 +46,5 @@ void Esi::Clean()
     delete prLibxml;
     prLibxml = NULL;
 #endif
-
-    delete prCustom;
-    prCustom = NULL;
 }
 
