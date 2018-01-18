squid-cache/squid

commit 957e7c621b0ef311f1bc4966f567679b1f0b40c3

Fixed RockIoState.cc "shutting_down || !writeableAnchor_" assertion
... probably introduced by my branch commit 7f0486e. That commit
correctly stopped disconnect()ing in-use/locked entries from their disks
upon release(), but that reader protection had an unfortunate side
effect of not cleaning up writeableAnchor_ of aborted swapouts: Dirty
RockIoState asserted upon self `destruction` while exiting from
storeSwapOutFileClosed().

Since master commit 4976925, RockIoState preserves writeableAnchor_ (and
the associated write lock on the disk entry) upon discovering a swapout
problem because Store swapout error handling code aparently requires the
StoreEntry to be still associated with its disk. That means Store itself
must clear writeableAnchor_ while handling those swapout errors. That
clearing is now done via the new Disk::finalizeSwapoutFailure() method.

src/fs/rock/RockSwapDir.h
@@ -45,7 +45,8 @@ public:
     virtual uint64_t currentSize() const;
     virtual uint64_t currentCount() const;
     virtual bool doReportStat() const;
-    virtual void swappedOut(const StoreEntry &e);
+    virtual void finalizeSwapoutSuccess(const StoreEntry &) override;
+    virtual void finalizeSwapoutFailure(StoreEntry &) override;
     virtual void create();
     virtual void parse(int index, char *path);
     virtual bool smpAware() const { return true; }

src/fs/rock/RockSwapDir.cc
@@ -127,6 +127,8 @@ void Rock::SwapDir::disconnect(StoreEntry &e)
 {
     assert(e.hasDisk(index));
 
+    ignoreReferences(e);
+
     // do not rely on e.swap_status here because there is an async delay
     // before it switches from SWAPOUT_WRITING to SWAPOUT_DONE.
 
@@ -170,9 +172,16 @@ Rock::SwapDir::doReportStat() const
 }
 
 void
-Rock::SwapDir::swappedOut(const StoreEntry &)
+Rock::SwapDir::finalizeSwapoutSuccess(const StoreEntry &)
 {
-    // stats are not stored but computed when needed
+    // nothing to do
+}
+
+void
+Rock::SwapDir::finalizeSwapoutFailure(StoreEntry &entry)
+{
+    debugs(47, 5, entry);
+    disconnect(entry); // calls abortWriting() to free the disk entry
 }
 
 int64_t
@@ -880,7 +889,7 @@ Rock::SwapDir::writeCompleted(int errflag, size_t, RefCount< ::WriteRequest> r)
 
         writeError(sio);
         sio.finishedWriting(errflag);
-        // and hope that Core will call disconnect() to close the map entry
+        // and wait for the finalizeSwapoutFailure() call to close the map entry
     }
 
     if (sio.touchingStoreEntry())
@@ -984,10 +993,8 @@ Rock::SwapDir::evictCached(StoreEntry &e)
     if (e.hasDisk(index)) {
         if (map->freeEntry(e.swap_filen))
             CollapsedForwarding::Broadcast(e);
-        if (!e.locked()) {
-            ignoreReferences(e);
+        if (!e.locked())
             disconnect(e);
-        }
     } else if (const auto key = e.publicKey()) {
         evictIfFound(key);
     }

src/fs/ufs/UFSSwapDir.cc
@@ -1287,10 +1287,18 @@ Fs::Ufs::UFSSwapDir::sync()
 }
 
 void
-Fs::Ufs::UFSSwapDir::swappedOut(const StoreEntry &e)
+Fs::Ufs::UFSSwapDir::finalizeSwapoutSuccess(const StoreEntry &e)
 {
     cur_size += fs.blksize * sizeInBlocks(e.swap_file_sz);
     ++n_disk_objects;
+}
+
+void
+Fs::Ufs::UFSSwapDir::finalizeSwapoutFailure(StoreEntry &entry)
+{
+    debugs(47, 5, entry);
+    // rely on the expected subsequent StoreEntry::release(), evictCached(), or
+    // a similar call to call unlink(), detachFromDisk(), etc. for the entry.
 }
 
 void

src/fs/ufs/UFSSwapDir.h
@@ -67,7 +67,8 @@ public:
     virtual void reconfigure() override;
     virtual int callback() override;
     virtual void sync() override;
-    virtual void swappedOut(const StoreEntry &e) override;
+    virtual void finalizeSwapoutSuccess(const StoreEntry &) override;
+    virtual void finalizeSwapoutFailure(StoreEntry &) override;
     virtual uint64_t currentSize() const override { return cur_size; }
     virtual uint64_t currentCount() const override { return n_disk_objects; }
     virtual ConfigOption *getOptionTree() const override;

src/store/Disk.h
@@ -69,8 +69,10 @@ public:
     /// called when the entry is about to forget its association with cache_dir
     virtual void disconnect(StoreEntry &) {}
 
-    /// called when entry swap out is complete
-    virtual void swappedOut(const StoreEntry &e) = 0;
+    /// finalize the successful swapout that has been already noticed by Store
+    virtual void finalizeSwapoutSuccess(const StoreEntry &) = 0;
+    /// abort the failed swapout that has been already noticed by Store
+    virtual void finalizeSwapoutFailure(StoreEntry &) = 0;
 
     /// whether this cache dir has an entry with `e.key`
     virtual bool hasReadableEntry(const StoreEntry &e) const = 0;

src/store_swapout.cc
@@ -304,6 +304,7 @@ storeSwapOutFileClosed(void *data, int errflag, StoreIOState::Pointer self)
             storeConfigure();
         }
 
+        e->disk().finalizeSwapoutFailure(*e);
         e->releaseRequest(); // TODO: Keep the memory entry (if any)
     } else {
         /* swapping complete */
@@ -315,7 +316,7 @@ storeSwapOutFileClosed(void *data, int errflag, StoreIOState::Pointer self)
 
         e->swap_file_sz = e->objectLen() + mem->swap_hdr_sz;
         e->swap_status = SWAPOUT_DONE;
-        e->disk().swappedOut(*e);
+        e->disk().finalizeSwapoutSuccess(*e);
 
         // XXX: For some Stores, it is pointless to re-check cachability here
         // and it leads to double counts in store_check_cachable_hist. We need

src/tests/TestSwapDir.h
@@ -24,7 +24,8 @@ public:
     virtual uint64_t currentSize() const override;
     virtual uint64_t currentCount() const override;
     virtual void stat(StoreEntry &) const override;
-    virtual void swappedOut(const StoreEntry &e) override {}
+    virtual void finalizeSwapoutSuccess(const StoreEntry &) override {}
+    virtual void finalizeSwapoutFailure(StoreEntry &) override {}
     virtual void reconfigure() override;
     virtual void init() override;
     virtual bool unlinkdUseful() const override;

commit 1c85f6efab5e8fce41305a5c5551c26207151dd2

Fix an 'inconsistent override' compiler error caused by 957e7c6

src/fs/rock/RockSwapDir.h
@@ -45,8 +45,8 @@ public:
     virtual uint64_t currentSize() const;
     virtual uint64_t currentCount() const;
     virtual bool doReportStat() const;
-    virtual void finalizeSwapoutSuccess(const StoreEntry &) override;
-    virtual void finalizeSwapoutFailure(StoreEntry &) override;
+    virtual void finalizeSwapoutSuccess(const StoreEntry &);
+    virtual void finalizeSwapoutFailure(StoreEntry &);
     virtual void create();
     virtual void parse(int index, char *path);
     virtual bool smpAware() const { return true; }