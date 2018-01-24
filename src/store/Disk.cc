8c533e3b173a4f0d7d6b97c0c1deea0b78ad3189
Eduard Bagdasaryan <dictiano@gmail.com>
2018-01-24 16:43:44 +0300

    Remove an unused method argument

--- src/store/Disk.cc
+++ src/store/Disk.cc
@@ -390,7 +390,7 @@ Store::Disk::optionObjectSizeDump(StoreEntry * e) const
 
 // some SwapDirs may maintain their indexes and be able to lookup an entry key
 StoreEntry *
-Store::Disk::get(const cache_key *key)
+Store::Disk::get(const cache_key *)
 {
     return NULL;
 }
