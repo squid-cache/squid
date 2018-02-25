6dee4e334dfbbfdef489407deff336196e1d35b8
Amos Jeffries <amosjeffries@squid-cache.org>
2018-02-26 02:40:00 +1300

    Auto-detect certtool and openssl command line tools. Pass the result to test-suite/mk-tls-test-certs.sh.
    Also, reformat test-suite/mk-tls-test-certs.sh and add exit code.

--- test-suite/mk-tls-test-certs.sh
+++ test-suite/mk-tls-test-certs.sh
@@ -4,80 +4,89 @@
 # test certificates and keys
 #
 
-if test -x /usr/bin/certtool; then
+CERTTOOL="$1"
+OPENSSL="$2"
 
-echo "
-organization = \"Example.Org\"
-cn = \"Test CA. Do not Trust\"
-dc = \"example.org\"
-expiration_days = 700
-ca
-cert_signing_key
-crl_signing_key
-tls_www_server
-" >example.org-ca
+if test "x$CERTTOOL" != "x" -a -x $CERTTOOL; then
 
-#
-# self-signed root CA
-#
-/usr/bin/certtool --generate-privkey --rsa --outfile ca-root-rsa.pkey
+	#
+	# self-signed root CA
+	#
+	echo "
+		organization = \"Example.Org\"
+		cn = \"Test CA. Do not Trust\"
+		dc = \"example.org\"
+		expiration_days = 365
+		ca
+		cert_signing_key
+		crl_signing_key
+		tls_www_server
+		" > example.org-ca
 
-/usr/bin/certtool --generate-self-signed --template example.org-ca \
-	--rsa --load-privkey ca-root-rsa.pkey --outfile ca-root-rsa.crt
+	$CERTTOOL --generate-privkey --rsa --outfile ca-root-rsa.pkey
 
-rm example.org-ca
+	$CERTTOOL --generate-self-signed --template example.org-ca \
+		--rsa --load-privkey ca-root-rsa.pkey --outfile ca-root-rsa.crt
 
-#
-# intermediary CA, signed by root
-#
-echo "
-organization = \"Example.Net\"
-cn = \"Test CA. Do not Trust\"
-dc = \"example.net\"
-expiration_days = 700
-ca
-cert_signing_key
-crl_signing_key
-tls_www_server
-" >example.net-ca
+	rm example.org-ca
 
-/usr/bin/certtool --generate-privkey --rsa --outfile ca-mid-rsa.pkey
+	#
+	# intermediary CA, signed by root
+	#
+	echo "
+	organization = \"Example.Net\"
+	cn = \"Test CA. Do not Trust\"
+	dc = \"example.net\"
+	expiration_days = 365
+	ca
+	cert_signing_key
+	crl_signing_key
+	tls_www_server
+	" > example.net-ca
 
-/usr/bin/certtool --generate-certificate --load-privkey ca-mid-rsa.pkey \
-	--load-ca-certificate ca-root-rsa.crt --load-ca-privkey ca-root-rsa.pkey \
-	--template example.net-ca --outfile ca-mid-rsa.crt
+	$CERTTOOL --generate-privkey --rsa --outfile ca-mid-rsa.pkey
 
-rm example.net-ca
+	$CERTTOOL --generate-certificate --load-privkey ca-mid-rsa.pkey \
+		--load-ca-certificate ca-root-rsa.crt --load-ca-privkey ca-root-rsa.pkey \
+		--template example.net-ca --outfile ca-mid-rsa.crt
 
-#
-# Standard leaf / non-CA certificate
-#
+	rm example.net-ca
 
-echo "
-organization = \"Example.Com\"
-cn = \"Test CA. Do not Trust\"
-dc = \"example.com\"
-expiration_days = 700
-tls_www_server
-" >example.com-leaf
+	#
+	# Standard leaf / non-CA certificate
+	#
 
-/usr/bin/certtool --generate-privkey --rsa --outfile leaf-rsa.pkey
+	echo "
+	organization = \"Example.Com\"
+	cn = \"Test CA. Do not Trust\"
+	dc = \"example.com\"
+	expiration_days = 365
+	tls_www_server
+	" >example.com-leaf
 
-/usr/bin/certtool --generate-certificate --load-privkey leaf-rsa.pkey \
-	--load-ca-certificate ca-root-rsa.crt --load-ca-privkey ca-root-rsa.pkey \
-	--template example.com-leaf --outfile leaf-rsa.crt
+	$CERTTOOL --generate-privkey --rsa --outfile leaf-rsa.pkey
 
-rm example.com-leaf
+	$CERTTOOL --generate-certificate --load-privkey leaf-rsa.pkey \
+		--load-ca-certificate ca-root-rsa.crt --load-ca-privkey ca-root-rsa.pkey \
+		--template example.com-leaf --outfile leaf-rsa.crt
 
-#
-# PEM files with CA chain
-#
-cat ca-root-rsa.crt ca-root-rsa.pkey >ca-root-rsa.pem
-cat ca-mid-rsa.crt ca-mid-rsa.pkey >ca-mid-rsa.pem
-cat ca-mid-rsa.crt ca-mid-rsa.pkey ca-root-rsa.crt >ca-chain-rsa.pem
-cat leaf-rsa.crt leaf-rsa.pkey ca-root-rsa.crt >leaf-chain-rsa.pem
+	rm example.com-leaf
 
+	#
+	# PEM files with CA chain
+	#
+	cat ca-root-rsa.crt ca-root-rsa.pkey >ca-root-rsa.pem
+	cat ca-mid-rsa.crt ca-mid-rsa.pkey >ca-mid-rsa.pem
+	cat ca-mid-rsa.crt ca-mid-rsa.pkey ca-root-rsa.crt >ca-chain-rsa.pem
+	cat leaf-rsa.crt leaf-rsa.pkey ca-root-rsa.crt >leaf-chain-rsa.pem
+
+elif test "x$OPENSSL" != "x" -a -x $OPENSSL; then
+
+	# TODO: generate certificates with openssl's tool
+	echo "ERROR: still missing OpenSSL certificate creation"
+	exit 1
 
 else
-    echo "ERROR: canont find /usr/bin/certtool"
+	echo "ERROR: cannot find a tool to generate certificates"
+	exit 1
 fi
