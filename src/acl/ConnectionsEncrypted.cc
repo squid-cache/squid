695b65ec5938a50576ed407f826e9ffac8ae84c7
Amos Jeffries <yadij@users.noreply.github.com>
2018-04-30 22:14:17 +0000

    Documentation for several ACL types (#189)
    These ACL types have been available for some time but have not
    yet had a mention in the squid.conf docs.

--- src/acl/ConnectionsEncrypted.cc
+++ src/acl/ConnectionsEncrypted.cc
@@ -47,7 +47,7 @@ void
 Acl::ConnectionsEncrypted::parse()
 {
     if (ConfigParser::strtokFile()) {
-        debugs(89, DBG_CRITICAL, "WARNING: connections_encrypted does not accepts any value.");
+        debugs(89, DBG_CRITICAL, "WARNING: connections_encrypted does not accept any value.");
     }
 }
 
