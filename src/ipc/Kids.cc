d84142e6283f24f3d08be290f36093edce73e2f2
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2018-04-30 16:57:53 +0000

    Do not abuse argv[0] to supply roles and IDs to SMP kids (#176)
    Use a newly added "--kid role-ID" command line option instead. Just like
    argv[0], the new option is not meant for direct/human use.
    
    This change allows exec(3)-wrapping tools like Valgrind to work with SMP
    Squid: When launching kid processes, Valgrind does not pass Squid-formed
    argv[0] to kid processes, breaking old kid role and ID detection code.
    
    This change does not alter argv[0] of Squid processes. There is nothing
    wrong with Squid-formed argv[0] values for Squid kids.
    
    Also added a CommandLine class to support command line parsing without
    code duplication. Squid needs to handle the new --kid option way before
    the old mainParseOptions() handles the other options. The new class also
    encapsulates argv manipulations, reducing main.cc pollution.

--- src/ipc/Kids.cc
+++ src/ipc/Kids.cc
@@ -16,7 +16,7 @@
 #include "tools.h"
 
 Kids TheKids;
-KidName TheKidName;
+SBuf TheKidName;
 
 Kids::Kids()
 {
@@ -29,25 +29,16 @@ void Kids::init()
 
     storage.reserve(NumberOfKids());
 
-    char kid_name[32];
-
-    // add Kid records for all workers
-    for (int i = 0; i < Config.workers; ++i) {
-        snprintf(kid_name, sizeof(kid_name), "(squid-%d)", (int)(storage.size()+1));
-        storage.push_back(Kid(kid_name));
-    }
+    for (int i = 0; i < Config.workers; ++i)
+        storage.emplace_back("squid", storage.size() + 1);
 
     // add Kid records for all disk processes
-    for (int i = 0; i < Config.cacheSwap.n_strands; ++i) {
-        snprintf(kid_name, sizeof(kid_name), "(squid-disk-%d)", (int)(storage.size()+1));
-        storage.push_back(Kid(kid_name));
-    }
+    for (int i = 0; i < Config.cacheSwap.n_strands; ++i)
+        storage.emplace_back("squid-disk", storage.size() + 1);
 
     // if coordination is needed, add a Kid record for Coordinator
-    if (storage.size() > 1) {
-        snprintf(kid_name, sizeof(kid_name), "(squid-coord-%d)", (int)(storage.size()+1));
-        storage.push_back(Kid(kid_name));
-    }
+    if (storage.size() > 1)
+        storage.emplace_back("squid-coord", storage.size() + 1);
 
     Must(storage.size() == static_cast<size_t>(NumberOfKids()));
 }
