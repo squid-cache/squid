7168ddf4b5b292c79579a0e8648614483ba09361
squidadm <squidadm@users.noreply.github.com>
2018-05-05 11:58:34 +0000

    Revert incorrect changes in PR189 (#199)
    Commit a85f0df5d9226a613c14219a26c53f9a9e6c3a5f was supposed
    to be documentation only, but somehow included changes from
    another PR branch. Remove so the other PR can be applied with
    correct commit details.

--- src/acl/UserData.cc
+++ src/acl/UserData.cc
@@ -17,13 +17,6 @@
 #include "sbuf/Algorithms.h"
 #include "util.h"
 
-const Acl::ParameterFlags &
-ACLUserData::supportedFlags() const
-{
-    static const Acl::ParameterFlags theFlagNames = { "-i", "+i" };
-    return theFlagNames;
-}
-
 bool
 ACLUserData::match(char const *user)
 {
