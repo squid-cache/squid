eccf7e8eb5771c0bec34feab44cc63df432106d6
Eduard Bagdasaryan <dictiano@gmail.com>
2018-05-22 15:12:52 +0300

    Bug 4223: Fixed retries of failed re-forwardable transactions.
    This change fixes Store to delay writing (and, therefore, sharing) of
    re-triable errors (e.g., 504 Gateway Timeout responses) to clients:
    once we start sharing the response with a client, we cannot re-try the
    transaction. Since ENTRY_FWD_HDR_WAIT flag purpose is to delay response
    sharing with Store clients, this patch fixes and clarifies its usage:
    
    1. Removes unconditional clearing from startWriting().
    2. Adds a conditional clearing to StoreEntry::write().
    3. Sets it only for may-be-rejected responses.
    
    (2) adds ENTRY_FWD_HDR_WAIT clearing to detect responses that filled the
    entire read buffer and must be shared now with the clients because
    they can no longer be retried with the current re-forwarding mechanisms
    (which rely on completing the bad response transaction first) and will
    get stuck. Such large re-triable error responses (>32KB with default
    read_ahead_gap) should be uncommon. They were getting stuck prior to
    trunk r12501.1.48. That revision started clearing ENTRY_FWD_HDR_WAIT in
    StoreEntry startWriting() unconditionally, allowing all errors to be
    sent to Store clients without a delay, and effectively disabling
    retries.
    
    Mgr::Forwarder was always setting ENTRY_FWD_HDR_WAIT, probably mimicking
    similarly aggressive FwdState behavior that we are now removing. Since the
    forwarder never rewrites the entry content, it should not need to set that
    flag. The forwarder and associated Server classes must not communicate
    with the mgr client during the client-Squid connection descriptor
    handoff to Coordinator, but ENTRY_FWD_HDR_WAIT is not the right
    mechanism to block such Squid-client communication. The flag does not
    work well for this purpose because those Server classes may (and do?)
    substitute the "blocked" StoreEntry with another one to write an error
    message to the client.
    
    Also moved ENTRY_FWD_HDR_WAIT clearance from many StoreEntry::complete()
    callers to that method itself. StoreEntry::complete() is meant to be the
    last call when forming the entry. Any post-complete entry modifications
    such as retries are prohibited.

--- src/ipc/Forwarder.cc
+++ src/ipc/Forwarder.cc
@@ -87,8 +87,10 @@ Ipc::Forwarder::handleRemoteAck()
 {
     debugs(54, 3, HERE);
     request->requestId = 0;
-    // Do not clear ENTRY_FWD_HDR_WAIT or do entry->complete() because
-    // it will trigger our client side processing. Let job cleanup close.
+    // Do not do entry->complete() because it will trigger our client side
+    // processing when we no longer own the client-Squid connection.
+    // Let job cleanup close the client-Squid connection that Coordinator
+    // now owns.
 }
 
 /// Ipc::Forwarder::requestTimedOut wrapper
