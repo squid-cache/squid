squid-cache/squid

commit 4310f8b00dd574542dcec4208112bb89ef403528

Bug 4505: SMP caches sometimes do not purge entries (#46)
When Squid finds a requested entry in the memory cache, it does not
check whether the same entry is also stored in a cache_dir. The
StoreEntry object may become associated with its store entry in the
memory cache but not with its store entry on disk. This inconsistency
causes two known problems:

1. Squid may needlessly swap out the memory hit to disk, either
   overwriting an existing (and identical) disk entry or, worse,
   creating a duplicate entry on another disk. In the second case, the
   two disk entries are not synchronized and may eventually start to
   differ if one of them is removed or updated.

2. Squid may not delete a stale disk entry when needed, violating
   various HTTP MUSTs, and eventually serving stale [disk] cache entries
   to clients.

Another purging problem is not caused by the above inconsistency:

3. A DELETE request or equivalent may come for the entry which is still
   locked for writing. Squid fails to get a lock for such an entry (in
   order to purge it) and the entry remains in disk and/or memory cache.

To solve the first two problems:

* StoreEntry::mayStartSwapout() now avoids needless swapouts by checking
  whether StoreEntry was fully loaded, is being loaded, or could have
  been loaded from disk. To be able to reject swapouts in the last case,
  we now require that the newer (disk) entries explicitly delete their
  older variants instead of relying on the Store to overwrite the older
  (unlocked) variant. That explicit delete should already be happening
  in higher-level code (that knows which entry is newer and must mark
  any stale entries for deletion anyway).

To fix problem #3:

* A new Store::Controller::evictIfFound(key) method purges (or marks for
  deletion if purging is impossible) all the matching store entries,
  without loading the StoreEntry information from stores. Avoiding

src/neighbors.cc
@@ -593,7 +593,7 @@ neighborsUdpPing(HttpRequest * request,
     if (Config.peers == NULL)
         return 0;
 
-    assert(entry->swap_status == SWAPOUT_NONE);
+    assert(!entry->hasDisk());
 
     mem->start_ping = current_time;
 
@@ -984,7 +984,7 @@ neighborsUdpAck(const cache_key * key, icp_common_t * header, const Ip::Address 
 
     debugs(15, 6, "neighborsUdpAck: opcode " << opcode << " '" << storeKeyText(key) << "'");
 
-    if (NULL != (entry = Store::Root().get(key)))
+    if ((entry = Store::Root().findCallback(key)))
         mem = entry->mem_obj;
 
     if ((p = whichPeer(from)))
@@ -1694,7 +1694,7 @@ dump_peers(StoreEntry * sentry, CachePeer * peers)
 void
 neighborsHtcpReply(const cache_key * key, HtcpReplyData * htcp, const Ip::Address &from)
 {
-    StoreEntry *e = Store::Root().get(key);
+    StoreEntry *e = Store::Root().findCallback(key);
     MemObject *mem = NULL;
     CachePeer *p;
     peer_t ntype = PEER_NONE;

src/store/Controller.h
@@ -28,7 +28,6 @@ public:
     /* Storage API */
     virtual void create() override;
     virtual void init() override;
-    virtual StoreEntry *get(const cache_key *) override;
     virtual uint64_t maxSize() const override;
     virtual uint64_t minSize() const override;
     virtual uint64_t currentSize() const override;
@@ -38,10 +37,39 @@ public:
     virtual void stat(StoreEntry &) const override;
     virtual void sync() override;
     virtual void maintain() override;
-    virtual void markForUnlink(StoreEntry &) override;
-    virtual void unlink(StoreEntry &) override;
+    virtual void evictCached(StoreEntry &) override;
+    virtual void evictIfFound(const cache_key *) override;
     virtual int callback() override;
     virtual bool smpAware() const override;
+
+    /// \returns a locally indexed and SMP-tracked matching StoreEntry (or nil)
+    /// Slower than peek() but does not restrict StoreEntry use and storage.
+    /// Counts as an entry reference from the removal policy p.o.v.
+    StoreEntry *find(const cache_key *);
+
+    /// \returns a matching StoreEntry not suitable for long-term use (or nil)
+    /// Faster than find() but the returned entry may not receive updates, may
+    /// lack information from some of the Stores, and should not be updated
+    /// except that purging peek()ed entries is supported.
+    /// Does not count as an entry reference from the removal policy p.o.v.
+    StoreEntry *peek(const cache_key *);
+
+    /// \returns matching StoreEntry associated with local ICP/HTCP transaction
+    /// Warning: The returned StoreEntry is not synced and may be marked for
+    /// deletion. Use it only for extracting transaction callback details.
+    /// TODO: Group and return just that callback-related data instead?
+    StoreEntry *findCallback(const cache_key *);
+
+    /// Whether a transient entry with the given public key exists and (but) was
+    /// marked for removal some time ago; get(key) returns nil in such cases.
+    bool markedForDeletion(const cache_key *key) const;
+
+    /// markedForDeletion() with no readers
+    /// this is one method because the two conditions must be checked in the right order
+    bool markedForDeletionAndAbandoned(const StoreEntry &) const;
+
+    /// whether there is a disk entry with e.key
+    bool hasReadableDiskEntry(const StoreEntry &) const;
 
     /// Additional unknown-size entry bytes required by Store in order to
     /// reduce the risk of selecting the wrong disk cache for the growing entry.
@@ -53,14 +81,33 @@ public:
     /// called when the entry is no longer needed by any transaction
     void handleIdleEntry(StoreEntry &);
 
+    /// Evict memory cache entries to free at least `spaceRequired` bytes.
+    /// Should be called via storeGetMemSpace().
+    /// Unreliable: Fails if enough victims cannot be found fast enough.
+    void freeMemorySpace(const int spaceRequired);
+
     /// called to get rid of no longer needed entry data in RAM, if any
     void memoryOut(StoreEntry &, const bool preserveSwappable);
 
     /// update old entry metadata and HTTP headers using a newer entry
     void updateOnNotModified(StoreEntry *old, const StoreEntry &newer);
 
-    /// makes the entry available for collapsing future requests
-    void allowCollapsing(StoreEntry *, const RequestFlags &, const HttpRequestMethod &);
+    /// tries to make the entry available for collapsing future requests
+    bool allowCollapsing(StoreEntry *, const RequestFlags &, const HttpRequestMethod &);
+
+    /// register a being-read StoreEntry (to optimize concurrent cache reads
+    /// and to receive remote DELETE events)
+    void addReading(StoreEntry *, const cache_key *);
+
+    /// register a being-written StoreEntry (to support concurrent cache reads
+    /// and to receive remote DELETE events)
+    void addWriting(StoreEntry *, const cache_key *);
+
+    /// whether the entry is in "reading from Transients" I/O state
+    bool transientsReader(const StoreEntry &) const;
+
+    /// whether the entry is in "writing to Transients" I/O state
+    bool transientsWriter(const StoreEntry &) const;
 
     /// marks the entry completed for collapsed requests
     void transientsCompleteWriting(StoreEntry &);
@@ -68,17 +115,14 @@ public:
     /// Update local intransit entry after changes made by appending worker.
     void syncCollapsed(const sfileno);
 
-    /// calls Root().transients->abandon() if transients are tracked
-    void transientsAbandon(StoreEntry &);
+    /// stop any current (and prevent any future) SMP sharing of the given entry
+    void stopSharing(StoreEntry &);
 
     /// number of the transient entry readers some time ago
     int transientReaders(const StoreEntry &) const;
 
     /// disassociates the entry from the intransit table
-    void transientsDisconnect(MemObject &);
-
-    /// removes the entry from the memory cache
-    void memoryUnlink(StoreEntry &);
+    void transientsDisconnect(StoreEntry &);
 
     /// disassociates the entry from the memory cache, preserving cached data
     void memoryDisconnect(StoreEntry &);
@@ -90,14 +134,21 @@ public:
     static int store_dirs_rebuilding;
 
 private:
+    bool memoryCacheHasSpaceFor(const int pagesRequired) const;
+
     /// update reference counters of the recently touched entry
     void referenceBusy(StoreEntry &e);
     /// dereference() an idle entry and return true if the entry should be deleted
     bool dereferenceIdle(StoreEntry &, bool wantsLocalMemory);
 
-    StoreEntry *find(const cache_key *key);
+    void allowSharing(StoreEntry &, const cache_key *);
+    StoreEntry *peekAtLocal(const cache_key *);
+
+    void memoryEvictCached(StoreEntry &);
+    void transientsUnlinkByKeyIfFound(const cache_key *);
     bool keepForLocalMemoryCache(StoreEntry &e) const;
-    bool anchorCollapsed(StoreEntry &, bool &inSync);
+    bool anchorToCache(StoreEntry &e, bool &inSync);
+    void checkTransients(const StoreEntry &) const;
 
     Disks *swapDir; ///< summary view of all disk caches
     Memory *memStore; ///< memory cache
@@ -106,6 +157,9 @@ private:
     /// will belong to a memory cache, a disk cache, or will be uncachable
     /// when the response header comes. Used for SMP collapsed forwarding.
     Transients *transients;
+
+    /// Hack: Relays page shortage from freeMemorySpace() to handleIdleEntry().
+    int memoryPagesDebt_ = 0;
 };
 
 /// safely access controller singleton

src/CollapsedForwarding.cc
@@ -53,27 +53,34 @@ CollapsedForwarding::Init()
 }
 
 void
-CollapsedForwarding::Broadcast(const StoreEntry &e)
+CollapsedForwarding::Broadcast(const StoreEntry &e, const bool includingThisWorker)
 {
-    if (!queue.get())
-        return;
-
-    if (!e.mem_obj || e.mem_obj->xitTable.index < 0 ||
+    if (!e.hasTransients() ||
             !Store::Root().transientReaders(e)) {
         debugs(17, 7, "nobody reads " << e);
         return;
     }
 
+    debugs(17, 5, e);
+    Broadcast(e.mem_obj->xitTable.index, includingThisWorker);
+}
+
+void
+CollapsedForwarding::Broadcast(const sfileno index, const bool includingThisWorker)
+{
+    if (!queue.get())
+        return;
+
     CollapsedForwardingMsg msg;
     msg.sender = KidIdentifier;
-    msg.xitIndex = e.mem_obj->xitTable.index;
+    msg.xitIndex = index;
 
-    debugs(17, 5, e << " to " << Config.workers << "-1 workers");
+    debugs(17, 7, "entry " << index << " to " << Config.workers << (includingThisWorker ? "" : "-1") << " workers");
 
     // TODO: send only to workers who are waiting for data
     for (int workerId = 1; workerId <= Config.workers; ++workerId) {
         try {
-            if (workerId != KidIdentifier && queue->push(workerId, msg))
+            if ((workerId != KidIdentifier || includingThisWorker) && queue->push(workerId, msg))
                 Notify(workerId);
         } catch (const Queue::Full &) {
             debugs(17, DBG_IMPORTANT, "ERROR: Collapsed forwarding " <<

src/CollapsedForwarding.h
@@ -13,6 +13,7 @@
 
 #include "ipc/forward.h"
 #include "ipc/Queue.h"
+#include "store/forward.h"
 
 #include <memory>
 
@@ -26,7 +27,11 @@ public:
     static void Init();
 
     /// notify other workers about changes in entry state (e.g., new data)
-    static void Broadcast(const StoreEntry &e);
+    static void Broadcast(const StoreEntry &e, const bool includingThisWorker = false);
+
+    /// notify other workers about state changes in Transient entry at the given xitTable.index
+    /// use Broadcast(StoreEntry) variant if you have a StoreEntry object
+    static void Broadcast(const sfileno index, const bool includingThisWorker);
 
     /// kick worker with empty IPC queue
     static void Notify(const int workerId);

src/MemObject.cc
@@ -98,7 +98,6 @@ MemObject::setUris(char const *aStoreId, char const *aLogUri, const HttpRequestM
 MemObject::MemObject() :
     inmem_lo(0),
     nclients(0),
-    smpCollapsed(false),
     ping_reply_callback(nullptr),
     ircb_data(nullptr),
     id(0),
@@ -220,8 +219,6 @@ MemObject::stat(MemBuf * mb) const
         mb->appendf("\tmem-cache index: %d state: %d offset: %" PRId64 "\n", memCache.index, memCache.io, memCache.offset);
     if (object_sz >= 0)
         mb->appendf("\tobject_sz: %" PRId64 "\n", object_sz);
-    if (smpCollapsed)
-        mb->appendf("\tsmp-collapsed\n");
 
     StoreClientStats statsVisitor(mb);
 

src/MemObject.h
@@ -15,6 +15,7 @@
 #include "RemovalPolicy.h"
 #include "SquidString.h"
 #include "stmem.h"
+#include "store/forward.h"
 #include "StoreIOBuffer.h"
 #include "StoreIOState.h"
 #include "typedefs.h" //for IRCB
@@ -123,8 +124,12 @@ public:
 
     SwapOut swapout;
 
-    /// cache "I/O" direction and status
-    typedef enum { ioUndecided, ioWriting, ioReading, ioDone } Io;
+    /* TODO: Remove this change-minimizing hack */
+    using Io = Store::IoStatus;
+    static constexpr Io ioUndecided = Store::ioUndecided;
+    static constexpr Io ioReading = Store::ioReading;
+    static constexpr Io ioWriting = Store::ioWriting;
+    static constexpr Io ioDone = Store::ioDone;
 
     /// State of an entry with regards to the [shared] in-transit table.
     class XitTable
@@ -149,8 +154,6 @@ public:
         Io io; ///< current I/O state
     };
     MemCache memCache; ///< current [shared] memory caching state for the entry
-
-    bool smpCollapsed; ///< whether this entry gets data from another worker
 
     /* Read only - this reply must be preserved by store clients */
     /* The original reply. possibly with updated metadata. */

src/MemStore.cc
@@ -321,7 +321,7 @@ MemStore::get(const cache_key *key)
     StoreEntry *e = new StoreEntry();
 
     // XXX: We do not know the URLs yet, only the key, but we need to parse and
-    // store the response for the Root().get() callers to be happy because they
+    // store the response for the Root().find() callers to be happy because they
     // expect IN_MEMORY entries to already have the response headers and body.
     e->createMemObject();
 
@@ -329,13 +329,12 @@ MemStore::get(const cache_key *key)
 
     const bool copied = copyFromShm(*e, index, *slot);
 
-    if (copied) {
-        e->hashInsert(key);
+    if (copied)
         return e;
-    }
 
-    debugs(20, 3, HERE << "mem-loading failed; freeing " << index);
+    debugs(20, 3, "failed for " << *e);
     map->freeEntry(index); // do not let others into the same trap
+    destroyStoreEntry(static_cast<hash_link *>(e));
     return NULL;
 }
 
@@ -403,46 +402,41 @@ MemStore::updateHeadersOrThrow(Ipc::StoreMapUpdate &update)
 }
 
 bool
-MemStore::anchorCollapsed(StoreEntry &collapsed, bool &inSync)
+MemStore::anchorToCache(StoreEntry &entry, bool &inSync)
 {
     if (!map)
         return false;
 
     sfileno index;
     const Ipc::StoreMapAnchor *const slot = map->openForReading(
-            reinterpret_cast<cache_key*>(collapsed.key), index);
+            reinterpret_cast<cache_key*>(entry.key), index);
     if (!slot)
         return false;
 
-    anchorEntry(collapsed, index, *slot);
-    inSync = updateCollapsedWith(collapsed, index, *slot);
+    anchorEntry(entry, index, *slot);
+    inSync = updateAnchoredWith(entry, index, *slot);
     return true; // even if inSync is false
 }
 
 bool
-MemStore::updateCollapsed(StoreEntry &collapsed)
+MemStore::updateAnchored(StoreEntry &entry)
 {
-    assert(collapsed.mem_obj);
-
-    const sfileno index = collapsed.mem_obj->memCache.index;
-
-    // already disconnected from the cache, no need to update
-    if (index < 0)
-        return true;
-
     if (!map)
         return false;
 
+    assert(entry.mem_obj);
+    assert(entry.hasMemStore());
+    const sfileno index = entry.mem_obj->memCache.index;
     const Ipc::StoreMapAnchor &anchor = map->readableEntry(index);
-    return updateCollapsedWith(collapsed, index, anchor);
+    return updateAnchoredWith(entry, index, anchor);
 }
 
-/// updates collapsed entry after its anchor has been located
+/// updates Transients entry after its anchor has been located
 bool
-MemStore::updateCollapsedWith(StoreEntry &collapsed, const sfileno index, const Ipc::StoreMapAnchor &anchor)
+MemStore::updateAnchoredWith(StoreEntry &entry, const sfileno index, const Ipc::StoreMapAnchor &anchor)
 {
-    collapsed.swap_file_sz = anchor.basics.swap_file_sz;
-    const bool copied = copyFromShm(collapsed, index, anchor);
+    entry.swap_file_sz = anchor.basics.swap_file_sz;
+    const bool copied = copyFromShm(entry, index, anchor);
     return copied;
 }
 
@@ -450,15 +444,8 @@ MemStore::updateCollapsedWith(StoreEntry &collapsed, const sfileno index, const 
 void
 MemStore::anchorEntry(StoreEntry &e, const sfileno index, const Ipc::StoreMapAnchor &anchor)
 {
-    const Ipc::StoreMapAnchor::Basics &basics = anchor.basics;
-
-    e.swap_file_sz = basics.swap_file_sz;
-    e.lastref = basics.lastref;
-    e.timestamp = basics.timestamp;
-    e.expires = basics.expires;
-    e.lastModified(basics.lastmod);
-    e.refcount = basics.refcount;
-    e.flags = basics.flags;
+    assert(!e.hasDisk()); // no conflict with disk entry basics
+    anchor.exportInto(e);
 
     assert(e.mem_obj);
     if (anchor.complete()) {
@@ -470,11 +457,7 @@ MemStore::anchorEntry(StoreEntry &e, const sfileno index, const Ipc::StoreMapAnc
         assert(e.mem_obj->object_sz < 0);
         e.setMemStatus(NOT_IN_MEMORY);
     }
-    assert(e.swap_status == SWAPOUT_NONE); // set in StoreEntry constructor
-    e.ping_status = PING_NONE;
 
-    EBIT_CLR(e.flags, RELEASE_REQUEST);
-    e.clearPrivate();
     EBIT_SET(e.flags, ENTRY_VALIDATED);
 
     MemObject::MemCache &mc = e.mem_obj->memCache;
@@ -910,25 +893,29 @@ MemStore::completeWriting(StoreEntry &e)
 }
 
 void
-MemStore::markForUnlink(StoreEntry &e)
+MemStore::evictCached(StoreEntry &e)
 {
-    assert(e.mem_obj);
-    if (e.mem_obj->memCache.index >= 0)
-        map->freeEntry(e.mem_obj->memCache.index);
+    debugs(47, 5, e);
+    if (e.hasMemStore()) {
+        if (map->freeEntry(e.mem_obj->memCache.index))
+            CollapsedForwarding::Broadcast(e);
+        if (!e.locked()) {
+            disconnect(e);
+            e.destroyMemObject();
+        }
+    } else if (const auto key = e.publicKey()) {
+        // the entry may have been loaded and then disconnected from the cache
+        evictIfFound(key);
+        if (!e.locked())
+            e.destroyMemObject();
+    }
 }
 
 void
-MemStore::unlink(StoreEntry &e)
+MemStore::evictIfFound(const cache_key *key)
 {
-    if (e.mem_obj && e.mem_obj->memCache.index >= 0) {
-        map->freeEntry(e.mem_obj->memCache.index);
-        disconnect(e);
-    } else if (map) {
-        // the entry may have been loaded and then disconnected from the cache
-        map->freeEntryByKey(reinterpret_cast<cache_key*>(e.key));
-    }
-
-    e.destroyMemObject(); // XXX: but it may contain useful info such as a client list. The old code used to do that though, right?
+    if (map)
+        map->freeEntryByKey(key);
 }
 
 void
@@ -936,12 +923,12 @@ MemStore::disconnect(StoreEntry &e)
 {
     assert(e.mem_obj);
     MemObject &mem_obj = *e.mem_obj;
-    if (mem_obj.memCache.index >= 0) {
+    if (e.hasMemStore()) {
         if (mem_obj.memCache.io == MemObject::ioWriting) {
             map->abortWriting(mem_obj.memCache.index);
             mem_obj.memCache.index = -1;
             mem_obj.memCache.io = MemObject::ioDone;
-            Store::Root().transientsAbandon(e); // broadcasts after the change
+            Store::Root().stopSharing(e); // broadcasts after the change
         } else {
             assert(mem_obj.memCache.io == MemObject::ioReading);
             map->closeForReading(mem_obj.memCache.index);

commit 80d0fe086af2f9493d4fee384ad5ab070b75357a

Bug 4855: re-enable querying private entries for HTCP/ICP (#214)
This was broken since 4310f8b: HTCP/ICP misused Store as a storage of
private entries during queries (e.g., see
neighborsUdpPing()/neighborsUdpAck()). A smarter HTCP/ICP
implementation would maintain its own StoreEntry cache for this purpose
(just like the existing queried_keys array for cache keys). However,
fixing this is beyond this issue scope.

src/neighbors.cc
@@ -1005,7 +1005,7 @@ neighborsUdpAck(const cache_key * key, icp_common_t * header, const Ip::Address 
 
     debugs(15, 6, "neighborsUdpAck: opcode " << opcode << " '" << storeKeyText(key) << "'");
 
-    if ((entry = Store::Root().findCallback(key)))
+    if ((entry = Store::Root().findCallbackXXX(key)))
         mem = entry->mem_obj;
 
     if ((p = whichPeer(from)))
@@ -1721,7 +1721,7 @@ dump_peers(StoreEntry * sentry, CachePeer * peers)
 void
 neighborsHtcpReply(const cache_key * key, HtcpReplyData * htcp, const Ip::Address &from)
 {
-    StoreEntry *e = Store::Root().findCallback(key);
+    StoreEntry *e = Store::Root().findCallbackXXX(key);
     MemObject *mem = NULL;
     CachePeer *p;
     peer_t ntype = PEER_NONE;

src/store/Controller.cc
@@ -349,14 +349,16 @@ Store::Controller::allowSharing(StoreEntry &entry, const cache_key *key)
 }
 
 StoreEntry *
-Store::Controller::findCallback(const cache_key *key)
+Store::Controller::findCallbackXXX(const cache_key *key)
 {
     // We could check for mem_obj presence (and more), moving and merging some
     // of the duplicated neighborsUdpAck() and neighborsHtcpReply() code here,
     // but that would mean polluting Store with HTCP/ICP code. Instead, we
     // should encapsulate callback-related data in a protocol-neutral MemObject
     // member or use an HTCP/ICP-specific index rather than store_table.
-    return peekAtLocal(key);
+
+    // cannot reuse peekAtLocal() because HTCP/ICP callbacks may use private keys
+    return static_cast<StoreEntry*>(hash_lookup(store_table, key));
 }
 
 /// \returns either an existing local reusable StoreEntry object or nil

src/store/Controller.h
@@ -56,9 +56,9 @@ public:
 
     /// \returns matching StoreEntry associated with local ICP/HTCP transaction
     /// Warning: The returned StoreEntry is not synced and may be marked for
-    /// deletion. Use it only for extracting transaction callback details.
-    /// TODO: Group and return just that callback-related data instead?
-    StoreEntry *findCallback(const cache_key *);
+    /// deletion. It can only be used for extracting transaction callback details.
+    /// New code should be designed to avoid this deprecated API.
+    StoreEntry *findCallbackXXX(const cache_key *);
 
     /// Whether a transient entry with the given public key exists and (but) was
     /// marked for removal some time ago; get(key) returns nil in such cases.