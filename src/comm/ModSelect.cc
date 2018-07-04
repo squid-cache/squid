168f80584e04dc73419d084e897d202fbe649783
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2018-07-04 19:09:33 +0300

    SetSelect() assertion should also check for zero timeout (461ce1f fix)

--- src/comm/ModSelect.cc
+++ src/comm/ModSelect.cc
@@ -124,7 +124,7 @@ Comm::SetSelect(int fd, unsigned int type, PF * handler, void *client_data, time
 {
     fde *F = &fd_table[fd];
     assert(fd >= 0);
-    assert(F->flags.open || (!handler && !client_data));
+    assert(F->flags.open || (!handler && !client_data && !timeout));
     debugs(5, 5, HERE << "FD " << fd << ", type=" << type <<
            ", handler=" << handler << ", client_data=" << client_data <<
            ", timeout=" << timeout);
