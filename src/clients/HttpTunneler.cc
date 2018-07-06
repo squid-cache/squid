0c514151cc85f03ad4db181f08054d05eb0e16d3
Christos Tsantilas <christos@chtsanti.net>
2018-07-06 13:48:58 +0300

    Fix HttpTunneler response when receives a malformed response
    When the HttpTunneler receives a valid HTTP status line but the headers
    are malformed:
      - If the HTTP status code is "200 OK" return an squid error page
      - If the Http status code is not "200 OK" return truncated the original peer
        response.

--- src/clients/HttpTunneler.cc
+++ src/clients/HttpTunneler.cc
@@ -270,7 +270,7 @@ Http::Tunneler::handleResponse(const bool eof)
     HttpReply::Pointer rep = new HttpReply;
     rep->sources |= HttpMsg::srcHttp;
     rep->sline.set(hp->messageProtocol(), hp->messageStatus());
-    if (!rep->parseHeader(*hp)) {
+    if (!rep->parseHeader(*hp) && rep->sline.status() == Http::scOkay) {
         bailOnResponseError("malformed CONNECT response from peer", nullptr);
         return;
     }
