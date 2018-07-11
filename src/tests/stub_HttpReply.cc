e11ad242c1c65a00b8ddc21db3658dee702fb5e7
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2018-07-11 08:26:48 +0300

    Bug 2821: Ignore Content-Range in non-206 responses (#250)
    Squid used to honor Content-Range header in HTTP 200 OK (and possibly
    other non-206) responses, truncating (and possibly enlarging) some
    response bodies. RFC 7233 declares Content-Range meaningless for
    standard HTTP status codes other than 206 and 416. Squid now relays
    meaningless Content-Range as is, without using its value.
    
    Why not just strip a meaningless Content-Range header? Squid does not
    really know whether it is the status code or the header that is "wrong".
    Let the client figure it out while the server remains responsible.
    
    Also ignore Content-Range in 416 (Range Not Satisfiable) responses
    because that header does not apply to the response body.
    
    Also fixed body corruption of (unlikely) multipart 206 responses to
    single-part Range requests. Valid multipart responses carry no
    Content-Range (in the primary header), which confused Squid.

--- src/tests/stub_HttpReply.cc
+++ src/tests/stub_HttpReply.cc
@@ -13,8 +13,8 @@
 #include "tests/STUB.h"
 
 HttpReply::HttpReply() : HttpMsg(hoReply), date (0), last_modified (0),
-    expires (0), surrogate_control (NULL), content_range (NULL), keep_alive (0),
-    protoPrefix("HTTP/"), do_clean(false), bodySizeMax(-2)
+    expires(0), surrogate_control(nullptr), keep_alive(0),
+    protoPrefix("HTTP/"), do_clean(false), bodySizeMax(-2), content_range(nullptr)
     STUB_NOP
     HttpReply::~HttpReply() STUB
     void HttpReply::setHeaders(Http::StatusCode status, const char *reason, const char *ctype, int64_t clen, time_t lmt, time_t expires_) STUB
@@ -29,4 +29,5 @@ HttpReply::HttpReply() : HttpMsg(hoReply), date (0), last_modified (0),
     HttpReply * HttpReply::clone() const STUB_RETVAL(NULL)
     bool HttpReply::inheritProperties(const HttpMsg *aMsg) STUB_RETVAL(false)
     int64_t HttpReply::bodySize(const HttpRequestMethod&) const STUB_RETVAL(0)
+    const HttpHdrContRange *HttpReply::contentRange() const STUB_RETVAL(nullptr)
 
