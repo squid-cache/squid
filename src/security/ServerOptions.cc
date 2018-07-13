7c1d6d82b4e27340bfdfa0707421f202f4e00c77
Alex Rousskov <rousskov@measurement-factory.com>
2018-07-12 22:58:02 -0600

    Restored support for the https_port clientca option
    Probably missing since commit 621f429.

--- src/security/ServerOptions.cc
+++ src/security/ServerOptions.cc
@@ -61,7 +61,9 @@ Security::ServerOptions::parse(const char *token)
     }
 
     // parse the server-only options
-    if (strncmp(token, "dh=", 3) == 0) {
+    if (strncmp(token, "clientca=", 9) == 0) {
+        clientCaFile = SBuf(token + 9);
+    } else if (strncmp(token, "dh=", 3) == 0) {
         // clear any previous Diffi-Helman configuration
         dh.clear();
         dhParamsFile.clear();
