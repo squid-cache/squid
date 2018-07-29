3567a7e73a28063580b2584c737ff7a5bc1c3189
Brightside56 <o.gumbar@papakaya.com>
2018-07-29 19:55:07 +0300

    libressl compatibility

--- src/ssl/bio.cc
+++ src/ssl/bio.cc
@@ -76,7 +76,11 @@ Ssl::Bio::Create(const int fd, Security::Io::Type type)
         BIO_meth_set_create(SquidMethods, squid_bio_create);
         BIO_meth_set_destroy(SquidMethods, squid_bio_destroy);
     }
+#if defined(LIBRESSL_VERSION_NUMBER)
+    BIO_METHOD *useMethod = SquidMethods;
+#elif !defined(LIBRESSL_VERSION_NUMBER)
     const BIO_METHOD *useMethod = SquidMethods;
+#endif
 #else
     BIO_METHOD *useMethod = &SquidMethods;
 #endif
