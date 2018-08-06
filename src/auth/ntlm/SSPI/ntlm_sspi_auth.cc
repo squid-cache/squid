3635ac8c9b0d3f183aa385a95814cafce2e1740f
Amos Jeffries <amosjeffries@squid-cache.org>
2018-08-06 18:30:19 +1200

    Update SSPI auth helpers to latest base64 encoder API
    This was missed when updating Nettle library.

--- src/auth/ntlm/SSPI/ntlm_sspi_auth.cc
+++ src/auth/ntlm/SSPI/ntlm_sspi_auth.cc
@@ -418,7 +418,7 @@ token_decode(size_t *decodedLen, uint8_t decoded[], const char *buf)
 {
     struct base64_decode_ctx ctx;
     base64_decode_init(&ctx);
-    if (!base64_decode_update(&ctx, decodedLen, decoded, strlen(buf), reinterpret_cast<const uint8_t*>(buf)) ||
+    if (!base64_decode_update(&ctx, decodedLen, decoded, strlen(buf), buf) ||
             !base64_decode_final(&ctx)) {
         SEND_BH("message=\"base64 decode failed\"");
         fprintf(stderr, "ERROR: base64 decoding failed for: '%s'\n", buf);
