668f774c4f21c69aee6210457b8e17be5747b83b
Amos Jeffries <amosjeffries@squid-cache.org>
2018-08-17 23:44:03 +1200

    Revert fde to slow allocate-copy-deallocate
    ... on every FD opening event via clear().

--- src/fde.h
+++ src/fde.h
@@ -59,11 +59,6 @@ public:
         readMethod_ = nullptr;
         writeMethod_ = nullptr;
     }
-    // no copying, just move-construct for clear()
-    fde(const fde &) = delete;
-    fde &operator =(const fde &) = delete;
-    fde(fde &&) = default;
-    fde &operator =(fde &&) = default;
     ~fde() {
         xfree(desc);
     }
