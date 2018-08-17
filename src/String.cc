36ba250bdb4a0637cec62980725be5f30bdf77e7
Amos Jeffries <amosjeffries@squid-cache.org>
2018-08-18 00:05:48 +1200

    Add space to clarify +1 documentation is not a divisor

--- src/String.cc
+++ src/String.cc
@@ -163,7 +163,7 @@ String::append( char const *str, int len)
     assert(str && len >= 0);
 
     PROF_start(StringAppend);
-    if (len_ + len + 1/*'\0'*/ < size_) {
+    if (len_ + len + 1 /*'\0'*/ < size_) {
         xstrncpy(buf_+len_, str, len+1);
         len_ += len;
     } else {
