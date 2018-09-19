squid-cache/squid

commit e2cc8c07bcfb7ef139107764a060ab68081709ff

Bug 4680: Memory leak in v5 r15076
Convrts the Http::Message lock/unlock macros to inline functions so the compiler can
catch this type of regression in future Pointer updates

src/http/Message.h
@@ -140,8 +140,22 @@ protected:
 
 } // namespace Http
 
-#define HTTPMSGUNLOCK(a) if (a) { if ((a)->unlock() == 0) delete (a); (a)=NULL; }
-#define HTTPMSGLOCK(a) (a)->lock()
+inline void
+HTTPMSGUNLOCK(Http::Message *a)
+{
+    if (a) {
+        if (a->unlock() == 0)
+            delete a;
+       a = nullptr;
+    }
+}
+
+inline void
+HTTPMSGLOCK(Http::Message *a)
+{
+    if (a)
+        a->lock();
+}
 
 #endif /* SQUID_HTTPMSG_H */
 

src/FwdState.cc
@@ -343,7 +343,6 @@ FwdState::Start(const Comm::ConnectionPointer &clientConn, StoreEntry *entry, Ht
      * Might want to assert that request is NULL at this point
      */
     entry->mem_obj->request = request;
-    HTTPMSGLOCK(entry->mem_obj->request);
 #if URL_CHECKSUM_DEBUG
 
     entry->mem_obj->checkUrlChecksum();
@@ -426,10 +425,8 @@ FwdState::fail(ErrorState * errorState)
     delete err;
     err = errorState;
 
-    if (!errorState->request) {
+    if (!errorState->request)
         errorState->request = request;
-        HTTPMSGLOCK(errorState->request);
-    }
 
     if (err->type != ERR_ZERO_SIZE_OBJECT)
         return;

src/mime.cc
@@ -402,15 +402,14 @@ MimeIcon::created(StoreEntry *newEntry)
     EBIT_SET(e->flags, ENTRY_SPECIAL);
     e->setPublicKey();
     e->buffer();
-    HttpRequest *r = HttpRequest::CreateFromUrl(url_);
 
-    if (NULL == r)
+    HttpRequestPointer r(HttpRequest::CreateFromUrl(url_));
+    if (!r)
         fatalf("mimeLoadIcon: cannot parse internal URL: %s", url_);
 
     e->mem_obj->request = r;
-    HTTPMSGLOCK(e->mem_obj->request);
 
-    HttpReply *reply = new HttpReply;
+    HttpReplyPointer reply(new HttpReply);
 
     if (status == Http::scNoContent)
         reply->setHeaders(status, NULL, NULL, 0, -1, -1);
@@ -419,7 +418,7 @@ MimeIcon::created(StoreEntry *newEntry)
     reply->cache_control = new HttpHdrCc();
     reply->cache_control->maxAge(86400);
     reply->header.putCc(reply->cache_control);
-    e->replaceHttpReply(reply);
+    e->replaceHttpReply(reply.getRaw());
 
     if (status == Http::scOkay) {
         /* read the file into the buffer and append it to store */

src/neighbors.cc
@@ -1367,7 +1367,6 @@ peerCountMcastPeersStart(void *data)
     psstate->ping.start = current_time;
     mem = fake->mem_obj;
     mem->request = psstate->request;
-    HTTPMSGLOCK(mem->request);
     mem->start_ping = current_time;
     mem->ping_reply_callback = peerCountHandleIcpReply;
     mem->ircb_data = psstate;

src/peer_digest.cc
@@ -530,10 +530,8 @@ peerDigestFetchReply(void *data, char *buf, ssize_t size)
             /* our old entry is fine */
             assert(fetch->old_entry);
 
-            if (!fetch->old_entry->mem_obj->request) {
+            if (!fetch->old_entry->mem_obj->request)
                 fetch->old_entry->mem_obj->request = fetch->entry->mem_obj->request;
-                HTTPMSGLOCK(fetch->old_entry->mem_obj->request);
-            }
 
             assert(fetch->old_entry->mem_obj->request);
 

src/store_digest.cc
@@ -421,9 +421,7 @@ storeDigestRewriteStart(void *datanotused)
     assert(e);
     sd_state.rewrite_lock = e;
     debugs(71, 3, "storeDigestRewrite: url: " << url << " key: " << e->getMD5Text());
-    HttpRequest *req = HttpRequest::CreateFromUrl(url);
-    e->mem_obj->request = req;
-    HTTPMSGLOCK(e->mem_obj->request);
+    e->mem_obj->request = HttpRequest::CreateFromUrl(url);
     /* wait for rebuild (if any) to finish */
 
     if (sd_state.rebuild_lock) {

commit 854f929d944772466370c674453316bed3b7ae29

Restored message nullification in HTTPMSGUNLOCK(), broken by e2cc8c0
Also restored correct clientReplyContext::cloneReply() assertion,
improperly removed in 91955d1.

src/HttpReply.h
@@ -161,12 +161,5 @@ protected:
     virtual bool parseFirstLine(const char *start, const char *end);
 };
 
-inline void
-HTTPMSGUNLOCK(HttpReply *&rep)
-{
-    if (Http::Message::Destroy(rep))
-        rep = nullptr;
-}
-
 #endif /* SQUID_HTTPREPLY_H */
 

src/HttpRequest.h
@@ -271,12 +271,5 @@ void UpdateRequestNotes(ConnStateData *csd, HttpRequest &request, NotePairs cons
 /// nil parameter(s) indicate missing caller information and are handled safely
 const Ip::Address *FindListeningPortAddress(const HttpRequest *, const AccessLogEntry *);
 
-inline void
-HTTPMSGUNLOCK(HttpRequest *&req)
-{
-    if (Http::Message::Destroy(req))
-        req = nullptr;
-}
-
 #endif /* SQUID_HTTPREQUEST_H */
 

src/client_side_reply.cc
@@ -1644,6 +1644,8 @@ clientReplyContext::buildReplyHeader()
 void
 clientReplyContext::cloneReply()
 {
+    assert(reply == NULL);
+
     reply = http->storeEntry()->getReply()->clone();
     HTTPMSGLOCK(reply);
 

src/http/Message.h
@@ -16,6 +16,7 @@
 #include "http/ProtocolVersion.h"
 #include "http/StatusCode.h"
 #include "HttpHeader.h"
+#include <type_traits>
 
 namespace Http
 {
@@ -65,11 +66,6 @@ public:
      * Factors other than the headers may result in connection closure.
      */
     bool persistent() const;
-
-    /// Decrements the message lock count, deletes the message
-    /// when the lock count is zero.
-    /// \returns true if the message was deleted (or is nil)
-    static inline bool Destroy(Message *);
 
 public:
     /// HTTP-Version field in the first line of the message.
@@ -146,29 +142,25 @@ protected:
     bool parseHeader(Http1::Parser &, Http::ContentLengthInterpreter &); // TODO move this function to the parser
 };
 
-inline bool
-Message::Destroy(Message *m)
-{
-    if (m && m->unlock() > 0)
-        return false;
-    delete m;
-    return true;
-}
-
 } // namespace Http
+
+template <class T>
+void
+HTTPMSGUNLOCK(T *&a)
+{
+    static_assert(std::is_base_of<Http::Message, T>::value, "T must inherit from Http::Message");
+    if (a) {
+        if (a->unlock() == 0)
+            delete a;
+        a = nullptr;
+    }
+}
 
 inline void
 HTTPMSGLOCK(Http::Message *a)
 {
     if (a)
         a->lock();
-}
-
-inline void
-HTTPMSGUNLOCK(Http::Message *&m)
-{
-    if (Http::Message::Destroy(m))
-        m = nullptr;
 }
 
 #endif /* SQUID_HTTPMSG_H */