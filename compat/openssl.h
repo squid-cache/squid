079bc702aa782187f54411e5030fcdd8f0c06e82
Eneas U de Queiroz <cote2004-github@yahoo.com>
2018-10-08 15:59:12 -0300

    Adapt compat/openssl.h to astyle
    astyle enforced the indentation of the functions under the extern "C"
    braces.
    
    Signed-off-by: Eneas U de Queiroz <cote2004-github@yahoo.com>

--- compat/openssl.h
+++ compat/openssl.h
@@ -48,74 +48,74 @@
 extern "C" {
 
 #if !HAVE_LIBCRYPTO_ASN1_STRING_GET0_DATA
-inline const unsigned char *
-ASN1_STRING_get0_data(const ASN1_STRING *x)
-{
-    return x->data;
-}
+    inline const unsigned char *
+    ASN1_STRING_get0_data(const ASN1_STRING *x)
+    {
+        return x->data;
+    }
 #endif
 
 #if !HAVE_LIBCRYPTO_BIO_GET_DATA
-inline void *
-BIO_get_data(BIO *table)
-{
-    return table->ptr;
-}
+    inline void *
+    BIO_get_data(BIO *table)
+    {
+        return table->ptr;
+    }
 
-inline void
-BIO_set_data(BIO *table, void *data)
-{
-   table->ptr = data;
-}
+    inline void
+    BIO_set_data(BIO *table, void *data)
+    {
+        table->ptr = data;
+    }
 
-inline void
-BIO_set_init(BIO *table, int init)
-{
-   table->init = init;
-}
+    inline void
+    BIO_set_init(BIO *table, int init)
+    {
+        table->init = init;
+    }
 #endif
 
 #if !HAVE_LIBCRYPTO_BIO_GET_INIT
-inline int
-BIO_get_init(BIO *table)
-{
-    return table->init;
-}
+    inline int
+    BIO_get_init(BIO *table)
+    {
+        return table->init;
+    }
 #endif
 
 #if !HAVE_LIBCRYPTO_DH_UP_REF // OpenSSL 1.1 API
 #if defined(CRYPTO_LOCK_DH) // OpenSSL 1.0 API
-inline int
-DH_up_ref(DH *t)
-{
-    if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_DH) > 1))
-        return 1;
-    return 0;
-}
+    inline int
+    DH_up_ref(DH *t)
+    {
+        if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_DH) > 1))
+            return 1;
+        return 0;
+    }
 #else
 #error missing both OpenSSL API features DH_up_ref (v1.1) and CRYPTO_LOCK_DH (v1.0)
 #endif /* OpenSSL 1.0 CRYPTO_LOCK_DH */
 #endif /* OpenSSL 1.1 DH_up_ref */
 
 #if !HAVE_LIBCRYPTO_EVP_PKEY_GET0_RSA
-inline RSA *
-EVP_PKEY_get0_RSA(EVP_PKEY *pkey)
-{
-    if (pkey->type != EVP_PKEY_RSA)
-        return nullptr;
-    return pkey->pkey.rsa;
-}
+    inline RSA *
+    EVP_PKEY_get0_RSA(EVP_PKEY *pkey)
+    {
+        if (pkey->type != EVP_PKEY_RSA)
+            return nullptr;
+        return pkey->pkey.rsa;
+    }
 #endif
 
 #if !HAVE_LIBCRYPTO_EVP_PKEY_UP_REF
 #if defined(CRYPTO_LOCK_EVP_PKEY) // OpenSSL 1.0
-inline int
-EVP_PKEY_up_ref(EVP_PKEY *t)
-{
-    if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_EVP_PKEY)) > 1)
-        return 1;
-    return 0;
-}
+    inline int
+    EVP_PKEY_up_ref(EVP_PKEY *t)
+    {
+        if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_EVP_PKEY)) > 1)
+            return 1;
+        return 0;
+    }
 
 #else
 #error missing both OpenSSL API features EVP_PKEY_up_ref (v1.1) and CRYPTO_LOCK_EVP_PKEY (v1.0)
@@ -127,18 +127,18 @@ EVP_PKEY_up_ref(EVP_PKEY *t)
 #define OPENSSL_LH_strhash lh_strhash
 #endif
 
-inline void
-SQUID_OPENSSL_init_ssl(void)
-{
+    inline void
+    SQUID_OPENSSL_init_ssl(void)
+    {
 #if HAVE_LIBSSL_OPENSSL_INIT_SSL
-    // OpenSSL will properly auto-initialize itself (in Squid context).
-    // No explicit initialization is required.
-    //OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, nullptr);
+        // OpenSSL will properly auto-initialize itself (in Squid context).
+        // No explicit initialization is required.
+        //OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, nullptr);
 #else
-    SSL_load_error_strings();
-    SSLeay_add_ssl_algorithms();
+        SSL_load_error_strings();
+        SSLeay_add_ssl_algorithms();
 #endif
-}
+    }
 
 #if !defined OPENSSL_VERSION
 #define OPENSSL_VERSION SSLEAY_VERSION
@@ -146,21 +146,21 @@ SQUID_OPENSSL_init_ssl(void)
 #endif
 
 #if !HAVE_LIBSSL_SSL_CIPHER_FIND
-inline const SSL_CIPHER *
-SSL_CIPHER_find(SSL *ssl, const unsigned char *ptr)
-{
-    return ssl->method->get_cipher_by_char(ptr);
-}
+    inline const SSL_CIPHER *
+    SSL_CIPHER_find(SSL *ssl, const unsigned char *ptr)
+    {
+        return ssl->method->get_cipher_by_char(ptr);
+    }
 #endif
 
 #if !HAVE_LIBSSL_SSL_SESSION_GET_ID
-inline const unsigned char *
-SSL_SESSION_get_id(const SSL_SESSION *s, unsigned int *len)
-{
-    if (len)
-        *len = s->session_id_length;
-    return s->session_id;
-}
+    inline const unsigned char *
+    SSL_SESSION_get_id(const SSL_SESSION *s, unsigned int *len)
+    {
+        if (len)
+            *len = s->session_id_length;
+        return s->session_id;
+    }
 #endif
 
 #if !HAVE_OPENSSL_TLS_CLIENT_METHOD
@@ -173,43 +173,43 @@ SSL_SESSION_get_id(const SSL_SESSION *s, unsigned int *len)
 
 #if !HAVE_LIBCRYPTO_X509_CRL_UP_REF // OpenSSL 1.1 API
 #if defined(CRYPTO_LOCK_X509_CRL) // OpenSSL 1.0 API
-inline int
-X509_CRL_up_ref(X509_CRL *t)
-{
-    if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_X509_CRL) > 1))
-        return 1;
-    return 0;
-}
+    inline int
+    X509_CRL_up_ref(X509_CRL *t)
+    {
+        if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_X509_CRL) > 1))
+            return 1;
+        return 0;
+    }
 #else
 #error missing both OpenSSL API features X509_up_ref (v1.1) and CRYPTO_LOCK_X509 (v1.0)
 #endif /* CRYPTO_LOCK_X509_CRL */
 #endif /* X509_CRL_up_ref */
 
 #if !HAVE_LIBCRYPTO_X509_GET0_SIGNATURE
-inline void
-X509_get0_signature(ASN1_BIT_STRING **psig, X509_ALGOR **palg, const X509 *x)
-{
-    if (psig)
-        *psig = (ASN1_BIT_STRING *)&x->signature;
-    if (palg)
-        *palg = (X509_ALGOR *)&x->sig_alg;
-}
+    inline void
+    X509_get0_signature(ASN1_BIT_STRING **psig, X509_ALGOR **palg, const X509 *x)
+    {
+        if (psig)
+            *psig = (ASN1_BIT_STRING *)&x->signature;
+        if (palg)
+            *palg = (X509_ALGOR *)&x->sig_alg;
+    }
 #endif
 
 #if !HAVE_LIBCRYPTO_X509_STORE_CTX_GET0_CERT
-inline X509 *
-X509_STORE_CTX_get0_cert(X509_STORE_CTX *ctx)
-{
-    return ctx->cert;
-}
+    inline X509 *
+    X509_STORE_CTX_get0_cert(X509_STORE_CTX *ctx)
+    {
+        return ctx->cert;
+    }
 #endif
 
 #if !HAVE_LIBCRYPTO_X509_STORE_CTX_GET0_UNTRUSTED
-inline STACK_OF(X509) *
-X509_STORE_CTX_get0_untrusted(X509_STORE_CTX *ctx)
-{
-    return ctx->untrusted;
-}
+    inline STACK_OF(X509) *
+    X509_STORE_CTX_get0_untrusted(X509_STORE_CTX *ctx)
+    {
+        return ctx->untrusted;
+    }
 
 /// Note that all of the calls in this next group were renamed, or had the new
 /// name added at the same time as X509_STORE_CTX_get0_untrusted was implemented,
@@ -223,26 +223,26 @@ X509_STORE_CTX_get0_untrusted(X509_STORE_CTX *ctx)
 
 #if !HAVE_LIBCRYPTO_X509_UP_REF // OpenSSL 1.1 API
 #if defined(CRYPTO_LOCK_X509) // OpenSSL 1.0 API
-inline int
-X509_up_ref(X509 *t)
-{
-    if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_X509)) > 1)
-        return 1;
-    return 0;
-}
+    inline int
+    X509_up_ref(X509 *t)
+    {
+        if (t && (CRYPTO_add(&t->references, 1, CRYPTO_LOCK_X509)) > 1)
+            return 1;
+        return 0;
+    }
 #else
 #error missing both OpenSSL API features X509_up_ref (v1.1) and CRYPTO_LOCK_X509 (v1.0)
 #endif /* CRYPTO_LOCK_X509 */
 #endif /* X509_up_ref */
 
-
 #if !HAVE_LIBCRYPTO_X509_VERIFY_PARAM_GET_DEPTH
-inline int
-X509_VERIFY_PARAM_get_depth(const X509_VERIFY_PARAM *param)
-{
-    return param->depth;
-}
+    inline int
+    X509_VERIFY_PARAM_get_depth(const X509_VERIFY_PARAM *param)
+    {
+        return param->depth;
+    }
 #endif
 
 } /* extern "C" */
 #endif /* OPENSSL_COMPAT_H */
+
