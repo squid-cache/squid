5f3a3ebd2699ad0d000c96d0859e653ab0c295ed
Daris A Nevil <dnevil@intrusion.com>
2018-11-15 20:40:00 +0000

    Sync prototype of logfileWrite() with definition.

--- src/log/File.h
+++ src/log/File.h
@@ -66,7 +66,7 @@ public:
 Logfile *logfileOpen(const char *path, size_t bufsz, int);
 void logfileClose(Logfile * lf);
 void logfileRotate(Logfile * lf, int16_t rotateCount);
-void logfileWrite(Logfile * lf, char *buf, size_t len);
+void logfileWrite(Logfile * lf, const char *buf, size_t len);
 void logfileFlush(Logfile * lf);
 void logfilePrintf(Logfile * lf, const char *fmt,...) PRINTF_FORMAT_ARG2;
 void logfileLineStart(Logfile * lf);
