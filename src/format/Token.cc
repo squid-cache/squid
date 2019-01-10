squid-cache/squid

commit 3f2759891bb1a445350753354be928ba00cbc2da

A few polishing fixes requested during review

src/format/Token.cc
@@ -491,10 +491,9 @@ Format::Token::parse(const char *def, Quoting *quoting)
 
         if (data.string) {
             char *header = data.string;
-            // HTTP header field names cannot have ':' while
-            // PROXY protocol pseudo headers start with it.
-            const auto proxyPseudoHeader = type == LFT_PROXY_PROTOCOL_RECEIVED_HEADER && header[0] == ':';
-            char *cp = strchr(proxyPseudoHeader ? header+1 : header, ':');
+
+            const auto pseudoHeader = header[0] == ':';
+            char *cp = strchr(pseudoHeader ? header+1 : header, ':');
 
             if (cp) {
                 *cp = '\0';
@@ -545,8 +544,11 @@ Format::Token::parse(const char *def, Quoting *quoting)
             if (!*header)
                 throw TexcHere(ToSBuf("Can't parse configuration token: '", def, "': missing header name"));
 
-            if (proxyPseudoHeader)
-                data.headerId = ProxyProtocol::HeaderNameToHeaderType(SBuf(header));
+            if (pseudoHeader) {
+                if (type == LFT_PROXY_PROTOCOL_RECEIVED_HEADER)
+                    data.headerId = ProxyProtocol::HeaderNameToHeaderType(SBuf(header));
+                throw TexcHere(ToSBuf("Can't parse configuration token: '", def, "': wrong header name"));
+            }
 
             data.header.header = header;
         } else {

src/AccessLogEntry.cc
@@ -40,7 +40,7 @@ AccessLogEntry::getLogClientIp(char *buf, size_t bufsz) const
     // - IPv4 clients masked with client_netmask
     // - IPv6 clients use 'privacy addressing' instead.
 
-    (void)log_ip.applyClientMask(Config.Addrs.client_netmask);
+    log_ip.applyClientMask(Config.Addrs.client_netmask);
 
     log_ip.toStr(buf, bufsz);
 }

src/client_side.cc
@@ -2200,7 +2200,7 @@ ConnStateData::ConnStateData(const MasterXaction::Pointer &xact) :
 
     // store the details required for creating more MasterXaction objects as new requests come in
     log_addr = xact->tcpClient->remote;
-    (void)log_addr.applyClientMask(Config.Addrs.client_netmask);
+    log_addr.applyClientMask(Config.Addrs.client_netmask);
 
     // register to receive notice of Squid signal events
     // which may affect long persisting client connections

src/format/Token.h
@@ -10,6 +10,7 @@
 #define _SQUID_FORMAT_TOKEN_H
 
 #include "format/ByteCode.h"
+#include "proxyp/Elements.h"
 
 /*
  * Squid configuration allows users to define custom formats in
@@ -52,7 +53,7 @@ public:
         char *string;
         // TODO: Add ID caching for protocols other than PROXY protocol.
         /// the cached ID of the parsed header or zero
-        uint32_t headerId;
+        ProxyProtocol::Two::HeaderType headerId;
 
         struct {
             char *header;

src/ip/Address.cc
@@ -101,12 +101,11 @@ Ip::Address::applyMask(Ip::Address const &mask_addr)
     return changes;
 }
 
-int
+void
 Ip::Address::applyClientMask(const Address &mask)
 {
     if (!isLocalhost() && isIPv4())
-        return applyMask(mask);
-    return 0;
+        (void)applyMask(mask);
 }
 
 bool

src/ip/Address.h
@@ -193,7 +193,7 @@ public:
     /// Apply so-called 'privacy masking' to IPv4 addresses,
     /// except localhost IP.
     /// IPv6 clients use 'privacy addressing' instead.
-    int applyClientMask(const Address &mask);
+    void applyClientMask(const Address &mask);
 
     /** Return the ASCII equivalent of the address
      *  Semantically equivalent to the IPv4 inet_ntoa()

src/proxyp/Message.cc
@@ -50,7 +50,7 @@ ProxyProtocol::Message::getValues(const uint32_t headerType, const char sep) con
         if (!hasAddresses())
             return SBuf();
         auto logAddr = sourceAddress;
-        (void)logAddr.applyClientMask(Config.Addrs.client_netmask);
+        logAddr.applyClientMask(Config.Addrs.client_netmask);
         char ipBuf[MAX_IPSTRLEN];
         return SBuf(logAddr.toStr(ipBuf, sizeof(ipBuf)));
     }

commit a4dbcde16dd574c0770d07a681b64c034b75b6f9

Fixed parsing of configured integer PROXY protocol TLV types like {224}
This was caused by 3f27598. HeaderNameToHeaderType() should not be
limited to pseudo headers.

src/format/Token.cc
@@ -544,11 +544,10 @@ Format::Token::parse(const char *def, Quoting *quoting)
             if (!*header)
                 throw TexcHere(ToSBuf("Can't parse configuration token: '", def, "': missing header name"));
 
-            if (pseudoHeader) {
-                if (type == LFT_PROXY_PROTOCOL_RECEIVED_HEADER)
-                    data.headerId = ProxyProtocol::HeaderNameToHeaderType(SBuf(header));
-                throw TexcHere(ToSBuf("Can't parse configuration token: '", def, "': wrong header name"));
-            }
+            if (type == LFT_PROXY_PROTOCOL_RECEIVED_HEADER)
+                data.headerId = ProxyProtocol::HeaderNameToHeaderType(SBuf(header));
+            else if (pseudoHeader)
+                throw TexcHere(ToSBuf("Pseudo headers are not supported in this context; got: '", def, "'"));
 
             data.header.header = header;
         } else {