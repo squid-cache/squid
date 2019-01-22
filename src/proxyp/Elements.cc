d9f9458461b4334acad1866b32954e64af7d7869
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2019-01-22 13:00:43 +0300

    Refactored ProxyProtocol::IntegerToHeaderType()

--- src/proxyp/Elements.cc
+++ src/proxyp/Elements.cc
@@ -50,10 +50,19 @@ ProxyProtocol::IntegerToHeaderType(const SBuf &rawInteger)
     int64_t tlvType = 0;
 
     Parser::Tokenizer ptok(rawInteger);
-    const auto parsed = ptok.skip('0') || ptok.int64(tlvType, 10, false);
-    if (!parsed || !ptok.atEnd())
-        throw TexcHere(ToSBuf("Invalid PROXY protocol TLV type value. ",
-                              "Expected a decimal integer but got '", rawInteger, "'"));
+    if (ptok.skip('0')) {
+        if (!ptok.atEnd())
+            throw TexcHere(ToSBuf("Invalid PROXY protocol TLV type value. ",
+                                  "Expected a decimal integer without leading zeros but got '",
+                                  rawInteger, "'")); // e.g., 077, 0xFF, or 0b101
+        // tlvType stays zero
+    } else {
+        Must(ptok.int64(tlvType, 10, false)); // the first character is a DIGIT
+        if (!ptok.atEnd())
+            throw TexcHere(ToSBuf("Invalid PROXY protocol TLV type value. ",
+                                  "Trailing garbage after ", tlvType, " in '",
+                                  rawInteger, "'")); // e.g., 1.0 or 5e0
+    }
 
     const auto limit = std::numeric_limits<uint8_t>::max();
     if (tlvType > limit)
