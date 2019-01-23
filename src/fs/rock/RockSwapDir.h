a12f1a4c6ca6e058d1bc10113a59be7fa6ee5c2d
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2019-01-23 04:30:40 +0000

    Fail Rock swapout if the disk dropped some of the write requests (#352)
    Detecting dropped writes earlier is more than a TODO: If the last entry
    write was successful, the whole entry becomes available for hits
    immediately.  IpcIoFile::checkTimeouts() that runs every 7 seconds
    (IpcIoFile::Timeout) would eventually notify Rock about the timeout,
    allowing Rock to release the failed entry, but that notification may
    be too late.
    
    The precise outcome of hitting an entry with a missing on-disk slice is
    unknown (because the bug was detected by temporary hit validation code
    that turned such hits into misses), but SWAPFAIL is the best we could
    hope for.

--- src/fs/rock/RockSwapDir.h
+++ src/fs/rock/RockSwapDir.h
@@ -138,6 +138,9 @@ protected:
 
 private:
     void createError(const char *const msg);
+    void handleWriteCompletionSuccess(const WriteRequest &request);
+    void handleWriteCompletionProblem(const int errflag, const WriteRequest &request);
+    bool droppedEarlierRequest(const WriteRequest &request) const;
 
     DiskIOStrategy *io;
     RefCount<DiskFile> theFile; ///< cache storage for this cache_dir
