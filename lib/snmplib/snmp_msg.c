c0773d3bd12e9247c49ac76dba909068fe322c73
flozilla <fishyflow@gmail.com>
2019-02-19 12:41:00 +0000

    Fix stack-based buffer-overflow when parsing SNMP messages (#319)
    Fortunately, this off-by-one bug seems to have no runtime effect.

--- lib/snmplib/snmp_msg.c
+++ lib/snmplib/snmp_msg.c
@@ -272,12 +272,16 @@ snmp_msg_Decode(u_char * Packet, int *PacketLenP,
         snmplib_debug(4, "snmp_msg_Decode:Error decoding SNMP Message Header (Version)!\n");
         ASN_PARSE_ERROR(NULL);
     }
+    int terminatorPos = *CommLenP - 1;
     bufp = asn_parse_string(bufp, PacketLenP, &type, Community, CommLenP);
     if (bufp == NULL) {
         snmplib_debug(4, "snmp_msg_Decode:Error decoding SNMP Message Header (Community)!\n");
         ASN_PARSE_ERROR(NULL);
     }
-    Community[*CommLenP] = '\0';
+    if (*CommLenP < terminatorPos) {
+        terminatorPos = *CommLenP;
+    }
+    Community[terminatorPos] = '\0';
 
     if ((*Version != SNMP_VERSION_1) &&
             (*Version != SNMP_VERSION_2)) {
