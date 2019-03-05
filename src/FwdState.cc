8bdbdd7d038b9756535c9209a66ad0d9e443a860
Alex Rousskov <rousskov@measurement-factory.com>
2019-03-04 23:27:03 -0700

    Polished comments to tie them closer to the Must() they describe
    ... while still explaining why we are receiving more paths even though
    we have found (and are using) an open one.

--- src/FwdState.cc
+++ src/FwdState.cc
@@ -585,8 +585,8 @@ FwdState::noteDestination(Comm::ConnectionPointer path)
     destinations->addPath(path);
 
     if (Comm::IsConnOpen(serverConn)) {
-        // We are already using a previously opened connection but also
-        // receiving destinations in case we need to re-forward.
+        // We are already using a previously opened connection, so we cannot be
+        // waiting for connOpener. We still receive destinations for backup.
         Must(!opening());
         return;
     }
@@ -625,8 +625,8 @@ FwdState::noteDestinationsEnd(ErrorState *selectionError)
     Must(!selectionError); // finding at least one path means selection succeeded
 
     if (Comm::IsConnOpen(serverConn)) {
-        // We are already using a previously opened connection but also
-        // receiving destinations in case we need to re-forward.
+        // We are already using a previously opened connection, so we cannot be
+        // waiting for connOpener. We were receiving destinations for backup.
         Must(!opening());
         return;
     }
