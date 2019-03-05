12fa4efdb82c14f4cfad49567374d81d3c2f3554
Christos Tsantilas <christos@chtsanti.net>
2019-03-05 13:11:40 +0200

    Set ErrorState::response_ to passed HttpReply object
    This is lost with the commit 3265fd78 where the ErrorPage constructors fixes.
    As a result squid sent back the status code read from server but reported
    "Internal Error: Missing Template ERR_RELAY_REMOTE" (produced while tried to
    build a new HttpReply object).

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -698,6 +698,7 @@ ErrorState::ErrorState(HttpRequest * req, HttpReply *errorReply) :
     ErrorState(ERR_RELAY_REMOTE)
 {
     Must(errorReply);
+    response_ = errorReply;
     httpStatus = errorReply->sline.status();
 
     if (req) {
