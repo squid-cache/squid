9573169fe92d3eb9bbcf53a8c651bb4099f93c8f
Amos Jeffries <amosjeffries@squid-cache.org>
2019-05-17 02:00:48 +1200

    RFC 7230 forbids user-info on https:// URL

--- src/anyp/Uri.cc
+++ src/anyp/Uri.cc
@@ -496,7 +496,7 @@ AnyP::Uri::absolute() const
         if (getScheme() != AnyP::PROTO_URN) {
             absolute_.append("//", 2);
             const bool omitUserInfo = getScheme() == AnyP::PROTO_HTTP ||
-                                      getScheme() != AnyP::PROTO_HTTPS ||
+                                      getScheme() == AnyP::PROTO_HTTPS ||
                                       userInfo().isEmpty();
             if (!omitUserInfo) {
                 absolute_.append(userInfo());
