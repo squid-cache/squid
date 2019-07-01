9541c13ac72c7e34cc385f9a3d52122177a06ece
Christos Tsantilas <christos@chtsanti.net>
2019-07-01 18:10:09 +0300

    Shrunk GeneratingCONNECT to focus on CONNECT generation
    For two major reasons:
      - To make the GeneratingCONNECT match with what its name says: only while
        generating the CONNECT request.
      - Correctly managing  a MasterXaction::connectingToPeer flag looks more
        tricky than initially believed. Requires to update the flag:
          - before the Http::Tunneler started
          - After the Http::Tunneler creator receives the callback
          - In connection comm close handlers because of the Http::Tunneler
            gone for an unexpected reason the Tunneler creator will not be
            called through callback
       Not a huge update, but looks that currently we do not need it.

--- src/clients/HttpTunneler.cc
+++ src/clients/HttpTunneler.cc
@@ -148,6 +148,7 @@ Http::Tunneler::writeRequest()
 
     debugs(11, 2, "Tunnel Server REQUEST: " << connection <<
            ":\n----------\n" << mb.buf << "\n----------");
+    request->masterXaction->generatingConnect = false;
     fd_note(connection->fd, "Tunnel Server CONNECT");
 
     typedef CommCbMemFunT<Http::Tunneler, CommIoCbParams> Dialer;
@@ -341,7 +342,6 @@ Http::Tunneler::callBack()
     debugs(83, 5, connection << status());
     Must(request);
     Must(request->masterXaction);
-    request->masterXaction->generatingConnect = false;
     auto cb = callback;
     callback = nullptr;
     ScheduleCallHere(cb);
