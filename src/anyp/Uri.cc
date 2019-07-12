595b2b4576a9e8bd1c5c0b91c035b508ee303d9b
Amos Jeffries <squid3@treenet.co.nz>
2019-07-12 14:57:07 +1200

    Reviewer requested update

--- src/anyp/Uri.cc
+++ src/anyp/Uri.cc
@@ -180,8 +180,8 @@ urlAppendDomain(char *host)
     /* For IPv4 addresses check for a dot */
     /* For IPv6 addresses also check for a colon */
     if (Config.appendDomain && !strchr(host, '.') && !strchr(host, ':')) {
-        const int64_t dlen = strlen(host);
-        const int64_t want = dlen + Config.appendDomainLen;
+        const uint64_t dlen = strlen(host);
+        const uint64_t want = dlen + Config.appendDomainLen;
         if (want > SQUIDHOSTNAMELEN - 1) {
             debugs(23, 2, "URL domain too large (" << dlen << " bytes)");
             return false;
