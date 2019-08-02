189de970fa6f5ec35278f4bd8283a501d54279d3
Vadim Salavatov <xremmargorpx@gmail.com>
2019-08-02 17:10:31 +0300

    Fix: .c_str() called on a temporary string

--- src/adaptation/ecap/MessageRep.cc
+++ src/adaptation/ecap/MessageRep.cc
@@ -204,8 +204,8 @@ Adaptation::Ecap::RequestLineRep::uri(const Area &aUri)
 {
     // TODO: if method is not set, AnyP::Uri::parse will assume it is not connect;
     // Can we change AnyP::Uri::parse API to remove the method parameter?
-    const char *buf = aUri.toString().c_str();
-    const bool ok = theMessage.url.parse(theMessage.method, buf);
+    auto uriBuffer = aUri.toString();
+    const bool ok = theMessage.url.parse(theMessage.method, uriBuffer.c_str());
     Must(ok);
 }
 
