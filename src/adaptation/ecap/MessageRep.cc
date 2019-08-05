467175e77e3c79ae715648c9efa935dcefcf2dd3
Alex Rousskov <rousskov@measurement-factory.com>
2019-08-05 09:39:21 -0400

    Improve temporary/copy elimination chances
    This change also removes the need for naming toString() result and and makes the
    code a bit more consistent with similar expressions in MessageRep.cc.

--- src/adaptation/ecap/MessageRep.cc
+++ src/adaptation/ecap/MessageRep.cc
@@ -204,8 +204,7 @@ Adaptation::Ecap::RequestLineRep::uri(const Area &aUri)
 {
     // TODO: if method is not set, AnyP::Uri::parse will assume it is not connect;
     // Can we change AnyP::Uri::parse API to remove the method parameter?
-    auto uriBuffer = aUri.toString();
-    const bool ok = theMessage.url.parse(theMessage.method, uriBuffer.c_str());
+    const auto ok = theMessage.url.parse(theMessage.method, aUri.toString().c_str());
     Must(ok);
 }
 
