18102f7de14e3ba35d2b4e9457b3611a178bf8b9
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2019-08-07 12:04:30 +0000

    Fix rock disk entry contamination related to aborted swapouts (#444)
    Also probably fixed hit response corruption related to aborted rock
    swapins.
    
    The following disk entry contamination sequence was observed in detailed
    cache logs during high-load Polygraph tests. Some of the observed
    level-1 errors matched those in real-world deployments.
    
    1. Worker A schedules AX – a request to write a piece of entry A to disk
       slot X. The disker is busy writing and reading other slots for worker
       A and other workers so AX stays in the A-to-Disker queue for a while.
    2. Entry A aborts swapout (for any reason, including network errors
       while receiving the being-stored response). Squid makes disk slot X
       available for other entries to use. AX stays queued.
    3. Another worker B picks up disk slot X (from the shared free disk slot
       pool) and schedules BX, a request to write a piece of entry B to disk
       slot X. BX gets queued in an B-to-Disker queue. AX stays queued.
    4. The disker satisfies write request BX. Disk slot X contains entry B
       contents now. AX stays queued.
    5. The disker satisfies write request AX. Disk slot X is a part of entry
       B slot chain but contains former entry A contents now! HTTP requests
       for entry B now read entry A leftovers from disk and complain about
       metadata mismatches (at best) or get wrong response contents (at
       worst).
    
    To prevent premature disk slot reuse, we now keep disk slots reserved
    while they are in the disk queue, even if the corresponding cache entry
    is long gone: Individual disk write requests now "own" the slot they are
    writing. The Rock::IoState object owns reserved but not yet used slots
    so that they can be freed when the object is gone. The disk entry owns
    the (successfully written) slots added to its chain in the map.
    
    The new slot ownership scheme required changes in what metadata the
    writing code has to maintain. For example, we now keep the address of
    the previous slot in the entry chain so that we can update its .next
    field after a successful disk write. Also, the old code detecting
    dropped write requests could no longer rely on the now-empty .next field
    in the previous map entry. The rewritten code numbers I/O transactions
    so that out-of-order replies can be detected without using the map.
    
    I tried to simplify the metadata maintenance code by shortening
    reservation lifetimes and using just-in-time [first] slot reservations.
    The new code may also leak fewer slots when facing C++ exceptions.
    
    As for reading, I realized that we had no protection from dropped rock
    read requests. If the first read request is dropped, the metadata
    decoding would probably fail but if subsequent reads are skipped, the
    client may be fed with data that is missing those skipped blocks. I did
    not try to reproduce these problems, but they are now fixed using the
    same I/O transaction numbering mechanism that the writing code now uses.
    Negative length checks in store_client.cc treat dropped reads as errors.
    
    I also removed commented out "partial writing" code because IoState
    class member changes should expose any dangerous merge problems.

--- src/fs/rock/RockSwapDir.cc
+++ src/fs/rock/RockSwapDir.cc
@@ -845,16 +845,15 @@ Rock::SwapDir::readCompleted(const char *, int rlen, int errflag, RefCount< ::Re
     ReadRequest *request = dynamic_cast<Rock::ReadRequest*>(r.getRaw());
     assert(request);
     IoState::Pointer sio = request->sio;
-
-    if (errflag == DISK_OK && rlen > 0)
-        sio->offset_ += rlen;
-
-    sio->callReaderBack(r->buf, rlen);
+    sio->handleReadCompletion(*request, rlen, errflag);
 }
 
 void
 Rock::SwapDir::writeCompleted(int errflag, size_t, RefCount< ::WriteRequest> r)
 {
+    // TODO: Move details into IoState::handleWriteCompletion() after figuring
+    // out how to deal with map access. See readCompleted().
+
     Rock::WriteRequest *request = dynamic_cast<Rock::WriteRequest*>(r.getRaw());
     assert(request);
     assert(request->sio !=  NULL);
@@ -863,7 +862,7 @@ Rock::SwapDir::writeCompleted(int errflag, size_t, RefCount< ::WriteRequest> r)
     // quit if somebody called IoState::close() while we were waiting
     if (!sio.stillWaiting()) {
         debugs(79, 3, "ignoring closed entry " << sio.swap_filen);
-        noteFreeMapSlice(request->sidNext);
+        noteFreeMapSlice(request->sidCurrent);
         return;
     }
 
@@ -871,7 +870,7 @@ Rock::SwapDir::writeCompleted(int errflag, size_t, RefCount< ::WriteRequest> r)
 
     if (errflag != DISK_OK)
         handleWriteCompletionProblem(errflag, *request);
-    else if (droppedEarlierRequest(*request))
+    else if (!sio.expectedReply(request->id))
         handleWriteCompletionProblem(DISK_ERROR, *request);
     else
         handleWriteCompletionSuccess(*request);
@@ -888,15 +887,24 @@ Rock::SwapDir::handleWriteCompletionSuccess(const WriteRequest &request)
     sio.splicingPoint = request.sidCurrent;
     // do not increment sio.offset_ because we do it in sio->write()
 
-    // finalize the shared slice info after writing slice contents to disk
+    assert(sio.writeableAnchor_);
+    if (sio.writeableAnchor_->start < 0) { // wrote the first slot
+        Must(request.sidPrevious < 0);
+        sio.writeableAnchor_->start = request.sidCurrent;
+    } else {
+        Must(request.sidPrevious >= 0);
+        map->writeableSlice(sio.swap_filen, request.sidPrevious).next = request.sidCurrent;
+    }
+
+    // finalize the shared slice info after writing slice contents to disk;
+    // the chain gets possession of the slice we were writing
     Ipc::StoreMap::Slice &slice =
         map->writeableSlice(sio.swap_filen, request.sidCurrent);
     slice.size = request.len - sizeof(DbCellHeader);
-    slice.next = request.sidNext;
+    Must(slice.next < 0);
 
     if (request.eof) {
         assert(sio.e);
-        assert(sio.writeableAnchor_);
         if (sio.touchingStoreEntry()) {
             sio.e->swap_file_sz = sio.writeableAnchor_->basics.swap_file_sz =
                                       sio.offset_;
@@ -915,7 +923,7 @@ Rock::SwapDir::handleWriteCompletionProblem(const int errflag, const WriteReques
 {
     auto &sio = *request.sio;
 
-    noteFreeMapSlice(request.sidNext);
+    noteFreeMapSlice(request.sidCurrent);
 
     writeError(sio);
     sio.finishedWriting(errflag);
@@ -934,23 +942,6 @@ Rock::SwapDir::writeError(StoreIOState &sio)
     // else noop: a fresh entry update error does not affect stale entry readers
 
     // All callers must also call IoState callback, to propagate the error.
-}
-
-/// whether the disk has dropped at least one of the previous write requests
-bool
-Rock::SwapDir::droppedEarlierRequest(const WriteRequest &request) const
-{
-    const auto &sio = *request.sio;
-    assert(sio.writeableAnchor_);
-    const Ipc::StoreMapSliceId expectedSliceId = sio.splicingPoint < 0 ?
-            sio.writeableAnchor_->start :
-            map->writeableSlice(sio.swap_filen, sio.splicingPoint).next;
-    if (expectedSliceId != request.sidCurrent) {
-        debugs(79, 3, "yes; expected " << expectedSliceId << ", but got " << request.sidCurrent);
-        return true;
-    }
-
-    return false;
 }
 
 void
