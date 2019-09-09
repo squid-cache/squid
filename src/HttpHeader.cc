a94dc12fb39d2011ecb7b8c4ea9936fe82d2fee4
Alex Rousskov <rousskov@measurement-factory.com>
2019-09-09 15:36:24 -0400

    Exclude Vary response headers from 304 updates
    RFC 7234 Section 4.3.4 requires that Squid updates cached Vary. However,
    reacting to changed variance requires either forgetting all cached
    entries attached to the old Vary mark entry (bad for caching) or
    re-keying all those entries using a new variance and a new Vary mark.
    Re-keying requires both maintaining a list of cached Vary-controlled
    entries and caching _request_ headers for every such entry!
    
    Whether HTTP compliance is worth the complexity is debatable. Perhaps
    origins should not return 304s to change variance? FWIW, Fastly folks
    decided that it is _not_ worth it for them; look for the "Side note" in
    https://www.smashingmagazine.com/2017/11/understanding-vary-header/

--- src/HttpHeader.cc
+++ src/HttpHeader.cc
@@ -283,8 +283,12 @@ HttpHeader::updateWarnings()
 bool
 HttpHeader::skipUpdateHeader(const Http::HdrType id) const
 {
-    // RFC 7234, section 4.3.4: use fields other from Warning for update
-    return id == Http::HdrType::WARNING;
+    return
+        // RFC 7234, section 4.3.4: use header fields other than Warning
+        (id == Http::HdrType::WARNING) ||
+        // TODO: Consider updating Vary headers after comparing the magnitude of
+        // the required changes (and/or cache losses) with compliance gains.
+        (id == Http::HdrType::VARY);
 }
 
 void
