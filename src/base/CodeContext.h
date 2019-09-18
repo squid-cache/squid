6de8d197e3f10644292aa9be1e92767962fd2b6f
Alex Rousskov <rousskov@measurement-factory.com>
2019-09-18 18:16:46 -0400

    Preserve code context across IpcIoFile I/O (e.g., rock disk hits)
    Discovered two primary subcases of context switching inside low-level
    "services" code such as IpcIoFile and ModEpoll. Added CallBack() and
    poorly named CallService() helpers for them.
    
    ModEpoll should CallBack() for its callbacks, but it is unclear whether
    refactoring ModEpoll code (a little) is worthwhile -- very little
    non-callback in-service work happens while calling epoll callbacks.

--- src/base/CodeContext.h
+++ src/base/CodeContext.h
@@ -32,6 +32,9 @@ public:
 
     virtual ~CodeContext() {}
 
+    // TODO: This should probably become some kind of <char*,id> tuple so that
+    // we can preserve context gist across Reset()-triggered destruction _and_
+    // pass (master transaction?) context gist to SMP workers (e.g., disker).
     /// writes a word or two to help identify code context in debug messages
     virtual std::ostream &briefCodeContext(std::ostream &os) const = 0;
 
@@ -42,6 +45,51 @@ public:
 /* convenience context-reporting wrappers that also reduce linking problems */
 std::ostream &CurrentCodeContextBrief(std::ostream &os);
 std::ostream &CurrentCodeContextDetail(std::ostream &os);
+
+/// Convenience class that automatically restores the current/outer CodeContext
+/// when leaving the scope of the new-context following/inner code. \see Run().
+class CodeContextGuard
+{
+public:
+    CodeContextGuard(const CodeContext::Pointer &newContext): savedCodeContext(CodeContext::Current()) { CodeContext::Reset(newContext); }
+    ~CodeContextGuard() { CodeContext::Reset(savedCodeContext); }
+
+    // no copying of any kind (for simplicity and to prevent accidental copies)
+    CodeContextGuard(CodeContextGuard &&) = delete;
+
+    CodeContext::Pointer savedCodeContext;
+};
+
+/// Executes service `callback` in `callbackContext`. If an exception occurs,
+/// the callback context is preserved, so that the exception is associated with
+/// the callback that triggered them (rather than with the service).
+///
+/// Service code running in its own service context should use this function.
+template <typename Fun>
+inline void
+CallBack(const CodeContext::Pointer &callbackContext, Fun &&callback)
+{
+    // TODO: Consider catching exceptions and letting CodeContext handle them.
+    const auto savedCodeContext(CodeContext::Current());
+    CodeContext::Reset(callbackContext);
+    callback();
+    CodeContext::Reset(savedCodeContext);
+}
+
+/// Executes `service` in `serviceContext` but due to automatic caller context
+/// restoration, service exceptions are associated with the caller that suffered
+/// from (and/or caused) them (rather than with the service itself).
+///
+/// Service code running in caller's context should use this function to escape
+/// into service context (e.g., for submitting caller-agnostic requests).
+template <typename Fun>
+inline void
+CallService(const CodeContext::Pointer &serviceContext, Fun &&service)
+{
+    // TODO: Consider catching exceptions and letting CodeContext handle them.
+    CodeContextGuard guard(serviceContext);
+    service();
+}
 
 #endif
 
