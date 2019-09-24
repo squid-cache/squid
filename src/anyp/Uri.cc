091213e61e8fe047cef022a3867afff1feb0c28c
Christos Tsantilas <christos@chtsanti.net>
2019-09-24 10:04:05 +0000

    Bug 4987: restore support for the "cache_object://" URI scheme (#475)
    Broken by commit 6c880a1.

--- src/anyp/Uri.cc
+++ src/anyp/Uri.cc
@@ -142,8 +142,15 @@ uriParseScheme(Parser::Tokenizer &tok)
      * Scheme names consist of a sequence of characters beginning with a
      * letter and followed by any combination of letters, digits, plus
      * ("+"), period ("."), or hyphen ("-").
+     *
+     * The underscore ("_") required to match "cache_object://" squid
+     * special URI scheme.
      */
-    static const auto schemeChars = CharacterSet("scheme", "+.-") + CharacterSet::ALPHA + CharacterSet::DIGIT;
+    static const auto schemeChars =
+#if USE_HTTP_VIOLATIONS
+        CharacterSet("special", "_") +
+#endif
+        CharacterSet("scheme", "+.-") + CharacterSet::ALPHA + CharacterSet::DIGIT;
 
     SBuf str;
     if (tok.prefix(str, schemeChars, 16) && tok.skip(':') && CharacterSet::ALPHA[str.at(0)]) {
