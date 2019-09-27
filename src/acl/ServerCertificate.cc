3c2da0634be927fc0e7aa18ba424b9fc2d949a14
Christos Tsantilas <christos@chtsanti.net>
2019-09-27 23:59:52 +0300

    Fix a check for nil to a simpler form

--- src/acl/ServerCertificate.cc
+++ src/acl/ServerCertificate.cc
@@ -27,7 +27,7 @@ ACLServerCertificateStrategy::match(ACLData<MatchType> * &data, ACLFilledCheckli
     else if (checklist->al && Comm::IsConnOpen(checklist->al->hier.tcpServer)) {
         const auto ssl = fd_table[checklist->al->hier.tcpServer->fd].ssl.get();
         cert.resetWithoutLocking(SSL_get_peer_certificate(ssl));
-    } else if (checklist->conn() != NULL && checklist->conn()->serverBump())
+    } else if (checklist->conn() && checklist->conn()->serverBump())
         cert = checklist->conn()->serverBump()->serverCert;
 
     if (!cert)
