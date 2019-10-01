4dc3c5997fd9276229960c6e989f2bfd1a060fc4
Christos Tsantilas <christos@chtsanti.net>
2019-10-01 13:45:19 +0300

    Report as BUG the case a request is missing a master transaction

--- src/acl/AtStep.cc
+++ src/acl/AtStep.cc
@@ -42,7 +42,7 @@ ACLAtStepStrategy::match(ACLData<XactionStep> * &data, ACLFilledChecklist *check
             return 0; // we have warned about the missing request earlier
 
         if (!checklist->request->masterXaction) {
-            debugs(28, DBG_IMPORTANT, "at_step GeneratingCONNECT ACL is missing master transaction info. Assuming mismatch.");
+            debugs(28, DBG_IMPORTANT, "BUG: at_step GeneratingCONNECT ACL is missing master transaction info. Assuming mismatch.");
             return 0;
         }
 
