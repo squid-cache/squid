squid-cache/squid

commit 8f212b54d62c6412c0b8df7d635df567b464a3d8

Avoid throwing from acl checks if HttpRequest or MasterXaction object is missing
It is better to just warn the user about and return a missmatch

Also check first for ServerBump steps and then for other steps. The
SslBump is widely used.

src/acl/AtStep.cc
@@ -20,17 +20,25 @@
 int
 ACLAtStepStrategy::match (ACLData<XactionStep> * &data, ACLFilledChecklist *checklist)
 {
-    Must(checklist->request);
-    Must(checklist->request->masterXaction);
-    if (checklist->request->masterXaction->generatingConnect && data->match(xstepGeneratingConnect))
-        return 1;
-
-#if USE_OPENSSL
+    #if USE_OPENSSL
     Ssl::ServerBump *bump = NULL;
     if (checklist->conn() != NULL && (bump = checklist->conn()->serverBump()))
         return data->match(bump->step);
     else
         return data->match(xstepTlsBump1);
 #endif
+
+    if (data->match(xstepGeneratingConnect)) {
+        if (!checklist->request)
+            return 0; // we have warned about the missing request earlier
+
+        if (!checklist->request->masterXaction) {
+            debugs(28, DBG_IMPORTANT, "missing MasterXaction object, treating as a mismatch");
+            return 0;
+        }
+
+        return checklist->request->masterXaction->generatingConnect ? 1 : 0;
+    }
+
     return 0;
 }

commit 3d2d2d7175516aca4ea489ce910fec5a3872f6e3

Rearrange ACLAtStepStrategy::match to always check for bumping steps first
The phrase "However the check for bumping step1 has to do be the last"
at commit with title "Commit 8f212b54 broke GeneratingCONNECT step matching"
was wrong. This commit fixes ACLAtStepStrategy::match to always check
the bumping stps before check for GeneratingCONNECT step.

src/acl/AtStep.cc
@@ -26,22 +26,28 @@ ACLAtStepStrategy::match(ACLData<XactionStep> * &data, ACLFilledChecklist *check
         (bump = checklist->conn()->serverBump()) &&
         data->match(bump->step))
         return 1;
-#endif
 
-    if (checklist->request && data->match(XactionStep::generatingConnect)) {
-        if (!checklist->request->masterXaction)
-            debugs(28, DBG_IMPORTANT, "at_step GeneratingCONNECT ACL is missing master transaction info. Assuming mismatch.");
-        else if (checklist->request->masterXaction->generatingConnect)
-            return 1;
-    }
-
-#if USE_OPENSSL
     // We need the following to cover the case of bumping at SslBump1 step
     // where the connStateData::serverBump() is not build yet.
     // The following also has the meaning that if no bumping preformed
     // or a client-first bumping is applied then the request is remaining
     // at SslBump1 bumping processing step.
-    return data->match(XactionStep::tlsBump1);
+    if (data->match(XactionStep::tlsBump1))
+        return 1;
 #endif
+
+    if (data->match(XactionStep::generatingConnect)) {
+        if (!checklist->request)
+            return 0; // we have warned about the missing request earlier
+
+        if (!checklist->request->masterXaction) {
+            debugs(28, DBG_IMPORTANT, "at_step GeneratingCONNECT ACL is missing master transaction info. Assuming mismatch.");
+            return 0;
+        }
+
+        return checklist->request->masterXaction->generatingConnect ? 1 : 0;
+    }
+
+    return 0;
 }
 