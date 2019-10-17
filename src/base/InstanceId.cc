a4ed2fcb5cd3adab8a56659266a601f6759dd482
Alex Rousskov <rousskov@measurement-factory.com>
2019-10-17 10:51:10 -0400

    Fix build after ccfbe8f (Report context of cache.log messages)
    In some environments (e.g., Ubuntu 14 without libcppunit-dev), GCC
    correctly complained that it does not know how to print "[unknown]",
    probably because none of the other headers imported <ostream> before
    importing base/InstanceId.h.

---
+++ src/base/InstanceId.cc
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 1996-2019 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+#include "base/InstanceId.h"
+
+#include <iostream>
+
+std::ostream&
+operator <<(std::ostream &os, const ScopedId &id)
+{
+    if (id.value)
+        os << id.scope << id.value;
+    else if (id.scope)
+        os << id.scope;
+    else
+        os << "[unknown]";
+    return os;
+}
+
