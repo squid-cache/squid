acad0c1f62a2128fe138ed25715234337114ee97
aaron-costello <56684862+aaron-costello@users.noreply.github.com>
2019-10-19 00:03:04 +0100

    Revert "Improved handling of cachemgr.cgi errors"
    This reverts commit 696a59ce9ff6f268074ba9aa310ad47ad47f9056.

--- tools/cachemgr.cc
+++ tools/cachemgr.cc
@@ -381,7 +381,6 @@ auth_html(const char *host, int port, const char *user_name)
 static void
 error_html(const char *msg)
 {
-    printf("X-XSS-Protection: 1\r\n");
     printf("Content-Type: text/html\r\n\r\n");
     printf("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
     printf("<HTML><HEAD><TITLE>Cache Manager Error</TITLE>\n");
