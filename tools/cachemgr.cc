14563d224ea0dd2bcfa1fac06d223dfe05e3453f
aaron-costello <56684862+aaron-costello@users.noreply.github.com>
2019-10-19 01:30:23 +0100

    Revert "Improve handling for cachemgr.cgi parameters and errors"
    This reverts commit 086fda7303bcd1ffba084123be0465a6910d68b3.

--- tools/cachemgr.cc
+++ tools/cachemgr.cc
@@ -245,7 +245,6 @@ auth_html(const char *host, int port, const char *user_name)
     if (fp == NULL)
         printf("X-Error: message=\"Unable to open config %s\"", DEFAULT_CACHEMGR_CONFIG);
 
-    printf("X-XSS-Protection: 1\r\n");
     printf("Content-Type: text/html\r\n\r\n");
 
     printf("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
@@ -381,7 +380,6 @@ auth_html(const char *host, int port, const char *user_name)
 static void
 error_html(const char *msg)
 {
-    printf("X-XSS-Protection: 1\r\n");
     printf("Content-Type: text/html\r\n\r\n");
     printf("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
     printf("<HTML><HEAD><TITLE>Cache Manager Error</TITLE>\n");
@@ -649,7 +647,6 @@ read_reply(int s, cachemgr_request * req)
         case isHeaders:
             /* forward header field */
             if (!strcmp(buf, "\r\n")) {     /* end of headers */
-                fputs("X-XSS-Protection: 1\r\n", stdout); /* add XSS Protection for supported browsers */
                 fputs("Content-Type: text/html\r\n", stdout);   /* add our type */
                 istate = isBodyStart;
             }
