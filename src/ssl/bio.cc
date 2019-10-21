5aff6fdb7e068682a8058852754ce0211d388e46
Christos Tsantilas <christos@chtsanti.net>
2019-10-21 17:25:07 +0300

    Wrap SSL_OP_NO_TLSv1_3 related code inside an '#if defined(SSL_OP_NO_TLSv1_3)'

--- src/ssl/bio.cc
+++ src/ssl/bio.cc
@@ -722,8 +722,11 @@ applyTlsDetailsToSSL(SSL *ssl, Security::TlsDetails::Pointer const &details, Ssl
     if (!details->compressionSupported)
         SSL_set_options(ssl, SSL_OP_NO_COMPRESSION);
 #endif
+
+#if defined(SSL_OP_NO_TLSv1_3)
     if (details->tlsSupportedVersion != AnyP::ProtocolVersion(AnyP::PROTO_TLS, 1, 3))
         SSL_set_options(ssl, SSL_OP_NO_TLSv1_3);
+#endif
 
 #if defined(TLSEXT_STATUSTYPE_ocsp)
     if (details->tlsStatusRequest)
