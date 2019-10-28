efbc650ad1655f5d806ad442f675704dd1219884
James DeFelice <james.defelice@gmail.com>
2019-10-28 06:53:33 -0400

    Fix calculation for 3 year cert expiration

--- src/ssl/gadgets.cc
+++ src/ssl/gadgets.cc
@@ -504,7 +504,7 @@ static bool buildCertificate(Security::CertPointer & cert, Ssl::CertificatePrope
     if (aTime) {
         if (!X509_set1_notAfter(cert.get(), aTime))
             return false;
-    } else if (!X509_gmtime_adj(X509_getm_notAfter(cert.get()), 60*60*24*356*3))
+    } else if (!X509_gmtime_adj(X509_getm_notAfter(cert.get()), 60*60*24*365*3))
         return false;
 
     int addedExtensions = 0;
