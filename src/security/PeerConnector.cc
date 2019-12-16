cd4dd61871d55a1023ca27b1beccf8ba7a450909
Christos Tsantilas <christos@chtsanti.net>
2019-12-16 18:35:56 +0200

    Fixes to allow compile if openssl is not used

--- src/security/PeerConnector.cc
+++ src/security/PeerConnector.cc
@@ -87,7 +87,9 @@ Security::PeerConnector::commTimeoutHandler(const CommTimeoutCbParams &)
 {
     debugs(83, 5, serverConnection() << " timedout. this=" << (void*)this);
     const auto err = new ErrorState(ERR_SECURE_CONNECT_FAIL, Http::scGatewayTimeout, request.getRaw(), al);
+#if USE_OPENSSL
     err->detail = new Ssl::ErrorDetail(SQUID_ERR_SSL_HANDSHAKE, nullptr, nullptr);
+#endif
     bail(err);
 }
 
@@ -96,7 +98,9 @@ Security::PeerConnector::connectionClosed(const char *reason)
 {
     debugs(83, 5, reason << " socket closed/closing. this=" << (void*)this);
     const auto err = new ErrorState(ERR_SECURE_CONNECT_FAIL, Http::scServiceUnavailable, request.getRaw(), al);
+#if USE_OPENSSL
     err->detail = new Ssl::ErrorDetail(SQUID_ERR_SSL_HANDSHAKE, nullptr, nullptr);
+#endif
     bail(err);
 }
 
