b3c558da710b6f9d2e4a19edd3ac2748226fdcb3
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2019-12-17 17:40:33 +0300

    Fixed memory size calculation for shared memory pages on 64-bit systems
    Due to code duplication, the wrong type (uint32 instead of size_t) was
    used to calculate the PagePool::theLevels size. theLevels memory
    (positioned by different and correct code) did not overlap with the raw
    pages buffer, but the raw pages buffer could, in some cases, be 32 bits
    short -- the last 4 bytes of the last page were out of allocated memory.
    
    In practice, shared memory allocations are page-aligned, and the
    difference in 4 bytes was probably compensated by the extra allocated
    bytes in most (or perhaps even all) cases.

--- src/ipc/mem/PageStack.cc
+++ src/ipc/mem/PageStack.cc
@@ -122,7 +122,7 @@ Ipc::Mem::PageStack::sharedMemorySize() const
 size_t
 Ipc::Mem::PageStack::SharedMemorySize(const uint32_t, const unsigned int capacity, const size_t pageSize)
 {
-    const size_t levelsSize = PageId::maxPurpose * sizeof(std::atomic<Ipc::Mem::PageStack::Value>);
+    const size_t levelsSize = PageId::maxPurpose * sizeof(Levels_t);
     const size_t pagesDataSize = capacity * pageSize;
     return StackSize(capacity) + LevelsPaddingSize(capacity) + levelsSize + pagesDataSize;
 }
