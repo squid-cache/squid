d1d3b4dce2e25663aa0565dc630b2c97aa199065
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2020-01-15 15:57:14 +0000

    Do not stall if xactions overwrite a recently active cache entry (#516)
    After the last transaction that cached or read the reply R1 ended, its
    Transients entry T1 was not freed. Subsequent requests (with different
    public keys) could occupy the same shared memory and rock slots (purging
    unlocked R1), preventing Squid from attaching a T1-derived StoreEntry to
    the cache. Another request for R1 would receive T1 and stall because its
    worker W1 kept waiting for a notification from another worker W2,
    incorrectly assuming that W2 exists and is going to fetch R1 for W1.
    That request was aborted after a timeout.
    
    A Transients entry represents active transaction(s). Broadcasts stop
    when there are no transactions to inform. We must remove idle (i.e.
    unlocked) Transients entries to avoid feeding new transactions with
    stale info. We now do that when unlocking a Transients entry and also
    double check that a found unattached Transients entry has a writer.

--- src/ipc/StoreMap.cc
+++ src/ipc/StoreMap.cc
@@ -247,10 +247,18 @@ Ipc::StoreMap::peekAtReader(const sfileno fileno) const
     const Anchor &s = anchorAt(fileno);
     if (s.reading())
         return &s; // immediate access by lock holder so no locking
+    assert(s.writing()); // must be locked for reading or writing
+    return nullptr;
+}
+
+const Ipc::StoreMap::Anchor *
+Ipc::StoreMap::peekAtWriter(const sfileno fileno) const
+{
+    const Anchor &s = anchorAt(fileno);
     if (s.writing())
-        return NULL; // the caller is not a read lock holder
-    assert(false); // must be locked for reading or writing
-    return NULL;
+        return &s; // immediate access by lock holder so no locking
+    assert(s.reading()); // must be locked for reading or writing
+    return nullptr;
 }
 
 const Ipc::StoreMap::Anchor &
@@ -440,6 +448,23 @@ Ipc::StoreMap::closeForReading(const sfileno fileno)
     assert(s.reading());
     s.lock.unlockShared();
     debugs(54, 5, "closed entry " << fileno << " for reading " << path);
+}
+
+void
+Ipc::StoreMap::closeForReadingAndFreeIdle(const sfileno fileno)
+{
+    auto &s = anchorAt(fileno);
+    assert(s.reading());
+
+    if (!s.lock.unlockSharedAndSwitchToExclusive()) {
+        debugs(54, 5, "closed entry " << fileno << " for reading " << path);
+        return;
+    }
+
+    assert(s.writing());
+    assert(!s.reading());
+    freeChain(fileno, s, false);
+    debugs(54, 5, "closed idle entry " << fileno << " for reading " << path);
 }
 
 bool
