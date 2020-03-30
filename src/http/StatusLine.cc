c684272657bb911f311da299e0d1dbb44860c188
Christos Tsantilas <christos@chtsanti.net>
2020-03-30 18:46:49 +0300

    Avoid sending zero status code because of a (future or undiscovered) bug
    ... and also report it in cache.log file. The error message in log will
    apear up to 100 times. Squid will return an "500 Internal server error"
    status to the client.

--- src/http/StatusLine.cc
+++ src/http/StatusLine.cc
@@ -47,6 +47,21 @@ Http::StatusLine::packInto(Packable * p) const
 {
     assert(p);
 
+    Http::StatusCode useStatus;
+    const char *useReason;
+
+    if (!status()) {
+        static int zeroStatusCount = 0;
+        if (++zeroStatusCount < 100) {
+            debugs(57, DBG_IMPORTANT, "BUG: Generated response lacks status code");
+        }
+        useStatus = Http::scInternalServerError;
+        useReason = Http::StatusCodeString(useStatus);
+    } else {
+        useStatus = status();
+        useReason = reason();
+    }
+
     /* local constants */
     /* AYJ: see bug 2469 - RFC2616 confirms stating 'SP characters' plural! */
     static const char *Http1StatusLineFormat = "HTTP/%d.%d %3d %s\r\n";
@@ -56,15 +71,15 @@ Http::StatusLine::packInto(Packable * p) const
     if (protocol == AnyP::PROTO_ICY) {
         debugs(57, 9, "packing sline " << this << " using " << p << ":");
         debugs(57, 9, "FORMAT=" << IcyStatusLineFormat );
-        debugs(57, 9, "ICY " << status() << " " << reason());
-        p->appendf(IcyStatusLineFormat, status(), reason());
+        debugs(57, 9, "ICY " << useStatus << " " << useReason);
+        p->appendf(IcyStatusLineFormat, useStatus, useReason);
         return;
     }
 
     debugs(57, 9, "packing sline " << this << " using " << p << ":");
     debugs(57, 9, "FORMAT=" << Http1StatusLineFormat );
-    debugs(57, 9, "HTTP/" << version.major << "." << version.minor << " " << status() << " " << reason());
-    p->appendf(Http1StatusLineFormat, version.major, version.minor, status(), reason());
+    debugs(57, 9, "HTTP/" << version.major << "." << version.minor << " " << useStatus << " " << useReason);
+    p->appendf(Http1StatusLineFormat, version.major, version.minor, useStatus, useReason);
 }
 
 /*
