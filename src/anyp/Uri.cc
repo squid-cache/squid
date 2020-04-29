bc01cc98d0ef5d9079ab1575e78aeb871fa0f16f
Alex Rousskov <rousskov@measurement-factory.com>
2020-04-29 13:08:58 +1200

    Replace simple byte-wise encoder with Tokenizer scanning encoder

--- src/anyp/Uri.cc
+++ src/anyp/Uri.cc
@@ -57,15 +57,28 @@ AnyP::Uri::Encode(const SBuf &buf, const CharacterSet &encode)
     if (buf.isEmpty())
         return buf;
 
-    SBuf output;
-    output.reserveSpace(buf.length()*3); // worst-case every byte is encoded
+    Parser::Tokenizer tk(buf);
+    const CharacterSet goodChars = encode.complement("ignore-chars");
 
-    for (const auto ch: buf) {
-        if (encode[ch])
-            output.appendf("%%%02X", static_cast<unsigned int>(ch)); // TODO: Optimize using a table
-        else
-            output.append(ch);
+    SBuf goodSection;
+    // optimization for the arguably common "no encoding necessary" case
+    if (tk.prefix(goodSection, goodChars) && tk.atEnd())
+        return buf;
+
+    SBuf output;
+    output.reserveSpace(buf.length() * 3); // worst case: encode all chars
+    output.append(goodSection); // may be empty
+
+    while (!tk.atEnd()) {
+        // TODO: Add Tokenizer::parseOne(void).
+        const auto ch = tk.remaining()[0];
+        output.appendf("%%%02X", static_cast<unsigned int>(ch)); // TODO: Optimize using a table
+        (void)tk.skip(ch);
+
+        if (tk.prefix(goodSection, goodChars))
+            output.append(goodSection);
     }
+
     return output;
 }
 
