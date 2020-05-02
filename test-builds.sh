f1aa71477f7b73587fed13bedf322bf049f4f19d
Francesco Chemolli <kinkie@squid-cache.org>
2020-05-02 21:50:38 +0100

    test-builds.sh: in case of error dump full log
    Change behaviour of the test-builds script to
    dump the full output of the current layer's log in
    case of error. This will help better diagnose errors
    as the root cause of an error may have exited the 20-lines
    buffer we currently use

--- test-builds.sh
+++ test-builds.sh
@@ -139,8 +139,8 @@ buildtest() {
 	fi
     else
         if test "${verbose}" != "yes" ; then
-            echo "Build Failed. Last log lines are:"
-            tail -20 ${log}
+            echo "Build Failed. Full log dump:"
+            cat ${log}
         else
             echo "Build FAILED."
         fi
