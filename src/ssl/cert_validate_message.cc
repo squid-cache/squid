3cb853b1eb07dfa4f130a80c574aec35d895afc4
Christos Tsantilas <christos@chtsanti.net>
2020-05-14 17:11:41 +0300

    Undo Ssl::CertValidationMsg::composeRequest changes  to minimise patch changes

--- src/ssl/cert_validate_message.cc
+++ src/ssl/cert_validate_message.cc
@@ -65,8 +65,7 @@ Ssl::CertValidationMsg::composeRequest(CertValidationRequest const &vcert)
         int i = 0;
         for (const Security::CertErrors *err = vcert.errors; err; err = err->next, ++i) {
             body +="\n";
-            const char *errName = GetErrorName(err->element.code);
-            body = body + param_error_name + xitoa(i) + "=" + errName + "\n";
+            body = body + param_error_name + xitoa(i) + "=" + GetErrorName(err->element.code) + "\n";
             int errorCertPos = -1;
             if (err->element.cert.get())
                 errorCertPos = sk_X509_find(peerCerts, err->element.cert.get());
