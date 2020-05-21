f72037a133b0e0eaeaf9a83d411352e80821c109
Amos Jeffries <yadij@users.noreply.github.com>
2020-05-21 14:42:02 +0000

    Add flexible RFC 3986 URI encoder (#617)
    Use AnyP::Uri namespace to self-document encoder scope and
    coding type.
    
    Use SBuf and CharacterSet for more flexible input and actions
    than previous RFC 1738 encoder. Allowing callers to trivially
    determine which characters are encoded.

--- src/tests/stub_StatHist.cc
+++ src/tests/stub_StatHist.cc
@@ -16,7 +16,7 @@ class StoreEntry;
 
 void StatHist::dump(StoreEntry * sentry, StatHistBinDumper * bd) const STUB
 void StatHist::enumInit(unsigned int i) STUB_NOP
-void StatHist::count(double d) STUB_NOP
+void StatHist::count(double) {/* STUB_NOP */}
 double statHistDeltaMedian(const StatHist & A, const StatHist & B) STUB_RETVAL(0.0)
 double statHistDeltaPctile(const StatHist & A, const StatHist & B, double pctile) STUB_RETVAL(0.0)
 void StatHist::logInit(unsigned int i, double d1, double d2) STUB
