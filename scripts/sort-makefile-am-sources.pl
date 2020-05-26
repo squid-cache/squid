660f17d0d06b87fd56c187cb72d7b862f344c5a2
Francesco Chemolli <kinkie@squid-cache.org>
2020-05-26 07:33:35 +0000

    Moved to case-sensitive sorting, stylistic fixes, warn about duplicate files

--- scripts/sort-makefile-am-sources.pl
+++ scripts/sort-makefile-am-sources.pl
@@ -10,28 +10,33 @@
 use strict;
 use warnings;
 
+my $current_source_section='';
 while (<>) {
+    s/@([A-Z0-9_]+)@/\$($1)/g;  # @VARNAME -> $(VARNAME)
     if (m!^#!) {
         print;
         next;
     }
     if (/^(\S+_SOURCES)\s*=\s*\\$/) {
+        $current_source_section=$1;
         print "$1 = \\\n";
     } else {
         print;
         next;
     }
     # accumulate files and prep for sorting
-    my %files;
+    my %files = ();
     while (<>) {
-        my $prefix='';
-        my $filename='';
 
         chomp;
         m!\s*(tests/stub_|tests/test)?(\S+)(\s+\\\s*)?$! || die "no parse";
-        $prefix=$1 if (defined $1);
-        $filename=$2 if (defined $2);
+        my $prefix = (defined $1) ? $1 : '';
+        my $filename = (defined $2) ? $2 : '';
         
+        print STDERR "WARNING: duplicate $prefix$filename ".
+            "detected in $current_source_section"
+            if (exists($files{"$filename.$prefix"}));
+
         $files{"$filename.$prefix"}="$prefix$filename";
         if (! /\\$/ ) {  # last line in the list
             &print_files(\%files);
@@ -40,12 +45,14 @@ while (<>) {
     }
 }
 
+exit 0;
+
 # arg is hash ref, print values in order of key
 sub print_files
 {
     my %files=%{$_[0]};
     my @q=();
-    foreach my $k (sort {lc $a cmp lc $b} keys %files) {
+    foreach my $k (sort keys %files) {
         push @q, "\t".$files{$k};
     }
     print join(" \\\n", @q)."\n";
