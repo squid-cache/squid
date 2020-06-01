ed2222cc0b84f665f43239660e7cbbe52baeb2ad
SquidAdm <noc@lists.squid-cache.org>
2020-06-01 13:43:10 +0000

    Revert "Fixed prohibitively slow search for new SMP shm pages (#523)"
    This reverts commit 21200fe204f039aeaee337b79040d3560c78fb89.

--- src/ipc/mem/PageStack.cc
+++ src/ipc/mem/PageStack.cc
@@ -15,115 +15,112 @@
 #include "ipc/mem/Page.h"
 #include "ipc/mem/PageStack.h"
 
-/* Ipc::Mem::PageStackStorageSlot */
+/// used to mark a stack slot available for storing free page offsets
+const Ipc::Mem::PageStack::Value Writable = 0;
 
-static_assert(sizeof(Ipc::Mem::PageStackStorageSlot::Pointer) ==
-    sizeof(decltype(Ipc::Mem::PageId::number)), "page indexing types are consistent");
-
-void
-Ipc::Mem::PageStackStorageSlot::take()
+Ipc::Mem::PageStack::PageStack(const uint32_t aPoolId, const unsigned int aCapacity, const size_t aPageSize):
+    thePoolId(aPoolId), theCapacity(aCapacity), thePageSize(aPageSize),
+    theSize(theCapacity),
+    theLastReadable(prev(theSize)), theFirstWritable(next(theLastReadable)),
+    theItems(aCapacity)
 {
-    const auto nxt = nextOrMarker.exchange(TakenPage);
-    assert(nxt != TakenPage);
-}
-
-void
-Ipc::Mem::PageStackStorageSlot::put(const PointerOrMarker expected, const Pointer nxt)
-{
-    assert(nxt != TakenPage);
-    const auto old = nextOrMarker.exchange(nxt);
-    assert(old == expected);
-}
-
-/* Ipc::Mem::PageStack */
-
-Ipc::Mem::PageStack::PageStack(const uint32_t aPoolId, const PageCount aCapacity, const size_t aPageSize):
-    thePoolId(aPoolId), capacity_(aCapacity), thePageSize(aPageSize),
-    size_(0),
-    head_(Slot::NilPtr),
-    slots_(aCapacity)
-{
-    assert(capacity_ < Slot::TakenPage);
-    assert(capacity_ < Slot::NilPtr);
-
     // initially, all pages are free
-    if (capacity_) {
-        const auto lastIndex = capacity_-1;
-        // FlexibleArray cannot construct its phantom elements so, technically,
-        // all slots (except the very first one) are uninitialized until now.
-        for (Slot::Pointer i = 0; i < lastIndex; ++i)
-            (void)new(&slots_[i])Slot(i+1);
-        (void)new(&slots_[lastIndex])Slot(Slot::NilPtr);
-        size_ = capacity_;
-        head_ = 0;
-    }
+    for (Offset i = 0; i < theSize; ++i)
+        theItems[i] = i + 1; // skip page number zero to keep numbers positive
 }
 
+/*
+ * TODO: We currently rely on the theLastReadable hint during each
+ * loop iteration. We could also use hint just for the start position:
+ * (const Offset start = theLastReadable) and then scan the stack
+ * sequentially regardless of theLastReadable changes by others. Which
+ * approach is better? Same for push().
+ */
 bool
 Ipc::Mem::PageStack::pop(PageId &page)
 {
-    assert(!page);
+    Must(!page);
 
-    Slot::Pointer current = head_.load();
+    // we may fail to dequeue, but be conservative to prevent long searches
+    --theSize;
 
-    auto nextFree = Slot::NilPtr;
-    do {
-        if (current == Slot::NilPtr)
-            return false;
-        nextFree = slots_[current].next();
-    } while (!head_.compare_exchange_weak(current, nextFree));
+    // find a Readable slot, starting with theLastReadable and going left
+    while (theSize >= 0) {
+        Offset idx = theLastReadable;
+        // mark the slot at ids Writable while extracting its current value
+        const Value value = theItems[idx].fetch_and(0); // works if Writable is 0
+        const bool popped = value != Writable;
+        // theItems[idx] is probably not Readable [any more]
 
-    // must decrement after removing the page to avoid underflow
-    const auto newSize = --size_;
-    assert(newSize < capacity_);
+        // Whether we popped a Readable value or not, we should try going left
+        // to maintain the index (and make progress).
+        // We may fail if others already updated the index, but that is OK.
+        theLastReadable.compare_exchange_weak(idx, prev(idx)); // may fail or lie
 
-    slots_[current].take();
-    page.number = current + 1;
-    page.pool = thePoolId;
-    debugs(54, 8, page << " size: " << newSize);
-    return true;
+        if (popped) {
+            // the slot we emptied may already be filled, but that is OK
+            theFirstWritable = idx; // may lie
+            page.pool = thePoolId;
+            page.number = value;
+            debugs(54, 9, page << " at " << idx << " size: " << theSize);
+            return true;
+        }
+        // TODO: report suspiciously long loops
+    }
+
+    ++theSize;
+    return false;
 }
 
 void
 Ipc::Mem::PageStack::push(PageId &page)
 {
-    debugs(54, 8, page);
-    assert(page);
-    assert(pageIdIsValid(page));
+    debugs(54, 9, page);
 
-    const auto pageIndex = page.number - 1;
-    auto &slot = slots_[pageIndex];
+    if (!page)
+        return;
 
-    // must increment before inserting the page to avoid underflow in pop()
-    const auto newSize = ++size_;
-    assert(newSize <= capacity_);
+    Must(pageIdIsValid(page));
+    // find a Writable slot, starting with theFirstWritable and going right
+    while (theSize < theCapacity) {
+        Offset idx = theFirstWritable;
+        auto isWritable = Writable;
+        const bool pushed = theItems[idx].compare_exchange_strong(isWritable, page.number);
+        // theItems[idx] is probably not Writable [any more];
 
-    auto current = head_.load();
-    auto expected = Slot::TakenPage;
-    do {
-        slot.put(expected, current);
-        expected = current;
-    } while (!head_.compare_exchange_weak(current, pageIndex));
+        // Whether we pushed the page number or not, we should try going right
+        // to maintain the index (and make progress).
+        // We may fail if others already updated the index, but that is OK.
+        theFirstWritable.compare_exchange_weak(idx, next(idx)); // may fail or lie
 
-    debugs(54, 8, page << " size: " << newSize);
-    page = PageId();
+        if (pushed) {
+            // the enqueued value may already by gone, but that is OK
+            theLastReadable = idx; // may lie
+            ++theSize;
+            debugs(54, 9, page << " at " << idx << " size: " << theSize);
+            page = PageId();
+            return;
+        }
+        // TODO: report suspiciously long loops
+    }
+    Must(false); // the number of pages cannot exceed theCapacity
 }
 
 bool
 Ipc::Mem::PageStack::pageIdIsValid(const PageId &page) const
 {
-    return page.pool == thePoolId &&
-           0 < page.number && page.number <= capacity();
+    return page.pool == thePoolId && page.number != Writable &&
+           page.number <= capacity();
 }
 
 size_t
 Ipc::Mem::PageStack::sharedMemorySize() const
 {
-    return SharedMemorySize(thePoolId, capacity_, thePageSize);
+    return SharedMemorySize(thePoolId, theCapacity, thePageSize);
 }
 
 size_t
-Ipc::Mem::PageStack::SharedMemorySize(const uint32_t, const PageCount capacity, const size_t pageSize)
+Ipc::Mem::PageStack::SharedMemorySize(const uint32_t, const unsigned int capacity, const size_t pageSize)
 {
     const auto levelsSize = PageId::maxPurpose * sizeof(Levels_t);
     const size_t pagesDataSize = capacity * pageSize;
@@ -131,19 +128,19 @@ Ipc::Mem::PageStack::SharedMemorySize(const uint32_t, const PageCount capacity, 
 }
 
 size_t
-Ipc::Mem::PageStack::StackSize(const PageCount capacity)
+Ipc::Mem::PageStack::StackSize(const unsigned int capacity)
 {
-    return sizeof(PageStack) + capacity * sizeof(Slot);
+    return sizeof(PageStack) + capacity * sizeof(Item);
 }
 
 size_t
 Ipc::Mem::PageStack::stackSize() const
 {
-    return StackSize(capacity_);
+    return StackSize(theCapacity);
 }
 
 size_t
-Ipc::Mem::PageStack::LevelsPaddingSize(const PageCount capacity)
+Ipc::Mem::PageStack::LevelsPaddingSize(const unsigned int capacity)
 {
     const auto displacement = StackSize(capacity) % alignof(Levels_t);
     return displacement ? alignof(Levels_t) - displacement : 0;
