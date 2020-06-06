e2f6cffa2bc31dc8d8a7893f03ece6638cfadf50
Francesco Chemolli <kinkie@squid-cache.org>
2020-06-06 11:40:32 +0000

    ClientRequestContext: make two wrappers static

--- src/ClientRequestContext.h
+++ src/ClientRequestContext.h
@@ -83,10 +83,5 @@ public:
     bool readNextRequest = false; ///< whether Squid should read after error handling
 };
 
-void
-clientStoreIdDoneWrapper(void *data, const Helper::Reply &result);
-void
-clientRedirectDoneWrapper(void *data, const Helper::Reply &result);
-
 #endif /* SQUID_CLIENTREQUESTCONTEXT_H */
 
