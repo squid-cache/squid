929669104af98f3b46753c0a8a0ce248c339b45e
Amos Jeffries <amosjeffries@squid-cache.org>
2020-07-27 20:37:20 +1200

    Remove unused aclCacheMatchFlush()

--- src/acl/Acl.cc
+++ src/acl/Acl.cc
@@ -356,24 +356,6 @@ ACL::cacheMatchAcl(dlink_list * cache, ACLChecklist *checklist)
     return auth_match->matchrv;
 }
 
-void
-aclCacheMatchFlush(dlink_list * cache)
-{
-    acl_proxy_auth_match_cache *auth_match;
-    dlink_node *link, *tmplink;
-    link = cache->head;
-
-    debugs(28, 8, "aclCacheMatchFlush called for cache " << cache);
-
-    while (link) {
-        auth_match = (acl_proxy_auth_match_cache *)link->data;
-        tmplink = link;
-        link = link->next;
-        dlinkDelete(tmplink, cache);
-        delete auth_match;
-    }
-}
-
 bool
 ACL::requiresAle() const
 {
