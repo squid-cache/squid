19a584710ea03c8c89bd8002188577634600ed85
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-07-27 14:23:46 +0300

    Update tools.cc

--- src/tools.cc
+++ src/tools.cc
@@ -911,7 +911,6 @@ void
 parseEtcHosts(void)
 {
     char buf[1024];
-    char buf2[512];
     char *nt = buf;
     char *lt = buf;
 
@@ -971,17 +970,8 @@ parseEtcHosts(void)
             *nt = '\0';
             debugs(1, 5, "etc_hosts: got hostname '" << lt << "'");
 
-            /* For IPV6 addresses also check for a colon */
-            if (Config.appendDomain && strlen(Config.appendDomain)<sizeof(buf2)-2 && !strchr(lt, '.') && !strchr(lt, ':')) {
-                /* I know it's ugly, but it's only at reconfig */
-                memset(buf2,0,sizeof(buf2));
-                strncpy(buf2, lt, sizeof(buf2)-strlen(Config.appendDomain)-2);
-                strncat(buf2, Config.appendDomain,  sizeof(buf2) - strlen(buf2) );
-                host = buf2;
-            } else {
-                host = lt;
-            }
-
+            (void)urlAppendDomain(lt);
+            host = lt;
             if (ipcacheAddEntryFromHosts(host, addr) != 0) {
                 /* invalid address, continuing is useless */
                 hosts.clear();
