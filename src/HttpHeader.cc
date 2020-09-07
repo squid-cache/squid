0c60a4f7c2be9b791c1bada3b5b595bd8008c721
Alex Rousskov <rousskov@measurement-factory.com>
2020-09-07 12:26:32 -0400

    Resume honoring non-lowercase Transfer-Encoding values
    ... after "Improve Transfer-Encoding handling" commit f6dd87e.

--- src/HttpHeader.cc
+++ src/HttpHeader.cc
@@ -551,9 +551,9 @@ HttpHeader::parse(const char *header_start, size_t hdrLen, Http::ContentLengthIn
         delById(Http::HdrType::CONTENT_LENGTH);
         // and clen state becomes irrelevant
 
-        if (rawTe == "chunked") {
+        if (rawTe.caseCmp("chunked")) {
             ; // leave header present for chunked() method
-        } else if (rawTe == "identity") { // deprecated. no coding
+        } else if (rawTe.caseCmp("identity")) { // deprecated. no coding
             delById(Http::HdrType::TRANSFER_ENCODING);
         } else {
             // This also rejects multiple encodings until we support them properly.
