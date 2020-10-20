90c46733847ffc5e19ad241389f10336635acddf
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2020-10-20 23:59:43 +0300

    Cleanup

--- src/client_side_reply.cc
+++ src/client_side_reply.cc
@@ -802,8 +802,7 @@ clientReplyContext::processOnlyIfCachedMiss()
     debugs(88, 4, http->request->method << ' ' << http->uri);
     http->al->http.code = Http::scGatewayTimeout;
     ErrorState *err = clientBuildError(ERR_ONLY_IF_CACHED_MISS, Http::scGatewayTimeout, NULL,
-                                       http->getConn() ? http->getConn() : nullptr,
-                                       http->request, http->al);
+                                       http->getConn(), http->request, http->al);
     removeClientStoreReference(&sc, http);
     startError(err);
 }
