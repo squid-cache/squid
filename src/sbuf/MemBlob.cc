67b4a95efbbbb4a15712862865f123afc0b78a0a
Alex Rousskov <rousskov@measurement-factory.com>
2020-11-04 09:26:33 -0500

    fixup: De-inlined syncSize() to avoid exposing API users to Must()
    Also made explicit and enforced the "cannot enlarge" restriction.
    
    Also added debugging. The method is no longer a cheap assignment and
    it is useful to record this important MemBlob state change.

--- src/sbuf/MemBlob.cc
+++ src/sbuf/MemBlob.cc
@@ -125,6 +125,15 @@ MemBlob::append(const char *source, const size_type n)
 }
 
 void
+MemBlob::syncSize(const size_type n)
+{
+    debugs(MEMBLOB_DEBUGSECTION, 7, n << " was: " << size);
+    Must(LockCount() <= 1);
+    Must(n <= size);
+    size = n;
+}
+
+void
 MemBlob::consume(const size_type rawN)
 {
     if (rawN && size) {
--- src/sbuf/MemBlob.h
+++ src/sbuf/MemBlob.h
@@ -97,7 +97,8 @@ public:
     void clear() { size = 0; }
 
     /// keep the first n bytes and forget the rest of data
-    void syncSize(const size_type n) { Must(LockCount() <= 1); size = n; }
+    /// cannot be used to increase our size; use append*() methods for that
+    void syncSize(const size_type n);
 
     /// forget the first n bytes, moving the rest of data (if any) to the start
     /// forgets all data (i.e. empties the buffer) if n exceeds size
