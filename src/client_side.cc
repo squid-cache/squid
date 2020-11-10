24b51f7946d372294e6a0a184308d46569636d1b
Alex Rousskov <rousskov@measurement-factory.com>
2020-11-10 21:42:18 +0000

    Transactions exceeding client_lifetime are logged as _ABORTED (#748)
    ... rather than timed out (_TIMEOUT).
    
    To record the right cause of death, we have to call terminateAll()
    rather than setting logType.err.timedout directly. Otherwise, when
    ConnStateData::swanSong() calls terminateAll(0), it overwrites our
    direct setting.

--- src/client_side.cc
+++ src/client_side.cc
@@ -2347,7 +2347,8 @@ clientLifetimeTimeout(const CommTimeoutCbParams &io)
     ClientHttpRequest *http = static_cast<ClientHttpRequest *>(io.data);
     debugs(33, DBG_IMPORTANT, "WARNING: Closing client connection due to lifetime timeout");
     debugs(33, DBG_IMPORTANT, "\t" << http->uri);
-    http->logType.err.timedout = true;
+    if (const auto conn = http->getConn())
+        conn->pipeline.terminateAll(ETIMEDOUT);
     if (Comm::IsConnOpen(io.conn))
         io.conn->close();
 }
