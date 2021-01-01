4b745cc54ed81a0bc43cbbc6f66a4ab5038dcc1f
SquidAdm <noc@lists.squid-cache.org>
2021-01-01 00:15:17 +0000

    Source Format Enforcement

--- scripts/sort-includes.pl
+++ scripts/sort-includes.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-## Copyright (C) 1996-2020 The Squid Software Foundation and contributors
+## Copyright (C) 1996-2021 The Squid Software Foundation and contributors
 ##
 ## Squid software is distributed under GPLv2+ license and includes
 ## contributions from numerous individuals and organizations.
