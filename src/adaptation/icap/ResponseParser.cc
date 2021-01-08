cbd9323d7b1096f1898aae55f018bad649b00468
Amos Jeffries <yadij@users.noreply.github.com>
2021-01-08 13:28:04 +1300

    Update ResponseParser.cc

--- src/adaptation/icap/ResponseParser.cc
+++ src/adaptation/icap/ResponseParser.cc
@@ -17,15 +17,13 @@ Adaptation::Icap::ResponseParser::parseResponseFirstLine()
 {
     Tokenizer tok(buf_);
 
-    const CharacterSet &WspDelim = DelimiterCharacters();
-
     if (msgProtocol_.protocol != AnyP::PROTO_NONE) {
         debugs(74, 6, "continue incremental parse for " << msgProtocol_);
         debugs(74, DBG_DATA, "parse remaining buf={length=" << tok.remaining().length() << ", data='" << tok.remaining() << "'}");
         // we already found the magic, but not the full line. keep going.
-        return parseResponseStatusAndReason(tok, WspDelim);
+        return parseResponseStatusAndReason(tok);
 
-    } else if (tok.skip(Icap1magic) && tok.skipOne(WspDelim)) {
+    } else if (tok.skip(Icap1magic) && tok.skipOne(DelimiterCharacters())) {
         debugs(74, 6, "found prefix magic " << Icap1magic);
         msgProtocol_.protocol = AnyP::PROTO_ICAP;
         msgProtocol_.major = 1;
@@ -34,7 +32,7 @@ Adaptation::Icap::ResponseParser::parseResponseFirstLine()
         // ICAP/1.0 Response status-line parse
         debugs(74, DBG_DATA, "parse remaining buf={length=" << tok.remaining().length() << ", data='" << tok.remaining() << "'}");
         buf_ = tok.remaining(); // resume checkpoint
-        return parseResponseStatusAndReason(tok, WspDelim);
+        return parseResponseStatusAndReason(tok);
 
     } else if (buf_.length() < Icap1magic.length() && Icap1magic.startsWith(buf_)) {
         debugs(74, 7, Raw("valid ICAP/1 prefix", buf_.rawContent(), buf_.length()));
