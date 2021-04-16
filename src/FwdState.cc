8478756c6cc5280d895ffb9a0b88e1f15b98a1a1
Joshua Rogers <jrogers@opera.com>
2021-04-16 12:33:17 +0000

    Fix incorrect level-2 logging (#804)
    Print debugging information where available rather than their pointer
    addresses.

--- src/FwdState.cc
+++ src/FwdState.cc
@@ -762,10 +762,16 @@ FwdState::doneWithRetries()
 }
 
 // called by the server that failed after calling unregister()
+// err may or may not be null here depending on the type of server end.
 void
 FwdState::handleUnregisteredServerEnd()
 {
-    debugs(17, 2, HERE << "self=" << self << " err=" << err << ' ' << entry->url());
+    if(err) {
+        const auto errType = errorTypeName(err->type);
+        debugs(17, 2, HERE << "self=" << self << " err=" << errType << ' ' << entry->url());
+    } else {
+        debugs(17, 2, HERE << "self=" << self << " err=" << err << ' ' << entry->url());
+    }
     assert(!Comm::IsConnOpen(serverConn));
     retryOrBail();
 }
