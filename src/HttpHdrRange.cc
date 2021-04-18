3addcf1f5c2369d87440ff640af5c330c09d68c8
Joshua Rogers <MegaManSec@users.noreply.github.com>
2021-04-18 15:06:12 +0000

    Fix a few level-2+ debugs(); improve ErrorState debugging (#804)
    When possible, report object contents rather than its memory address.

--- src/HttpHdrRange.cc
+++ src/HttpHdrRange.cc
@@ -226,7 +226,7 @@ HttpHdrRange::parseInit(const String * range_spec)
     int ilen;
     assert(range_spec);
     ++ParsedCount;
-    debugs(64, 8, "parsing range field: '" << range_spec << "'");
+    debugs(64, 8, "parsing range field: '" << *range_spec << "'");
     /* check range type */
 
     if (range_spec->caseCmp("bytes=", 6))
@@ -247,7 +247,7 @@ HttpHdrRange::parseInit(const String * range_spec)
                 delete specs.back();
                 specs.pop_back();
             }
-            debugs(64, 2, "ignoring invalid range field: '" << range_spec << "'");
+            debugs(64, 2, "ignoring invalid range field: '" << *range_spec << "'");
             break;
         }
 
