e237d731c090350f8c04093c83c65d4d27e614b8
Amos Jeffries <yadij@users.noreply.github.com>
2021-04-21 10:07:18 +1200

    Add two wrong test cases
    ... to demonstrate cppunit test behaviour when the parser output does not match the check expectations.

--- src/tests/testCacheManager.cc
+++ src/tests/testCacheManager.cc
@@ -123,6 +123,7 @@ testCacheManager::testParseUrl()
         "?intlist=1",
         "?intlist=1,2,3,4,5",
         "?string=1a",
+        "?string = broken", // actually invalid. testing that cppunit fails properly
         "?string=1,2,3,4,z",
         "?string=1,2,3,4,[0]",
         "?intlist=1,2,3,4,5&string=1,2,3,4,y"
@@ -143,6 +144,7 @@ testCacheManager::testParseUrl()
         "? &&&",
         "?& &&",
         "?&& &",
+        "?/", // actually OK. testing that cppunit fails properly
         "?=&&&",
         "?&=&&",
         "?&&=&"
