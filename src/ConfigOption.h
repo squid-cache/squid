2e6535ab844258c37966957924d74144eb62a5bf
Alex Rousskov <rousskov@measurement-factory.com>
2021-05-08 07:51:32 +0000

    API to separate Config.x users/parsers from Config.y details (#811)
    The new API avoids several kinds of unwanted code dependencies:
    
    * Config.x users do not need to know the type of any Config.y component.
      Forward declarations of Config.y types are sufficient.
    
    * Config.x parsers do not need to know about Config.y parsers. Config.x
      parsers can also be hidden from Config.x users.
    
    * The centralized parsing code does not need to contain Config.x parsing
      code (for any x) and does not maintain "generic" parser registration.
      The compiler/linker do that for us _without_ losing C++ type safety.
    
    Correct API implementation may also help separate the active Config
    object from the being-parsed configuration, to eventually support safe
    "hot" reconfiguration.
    
    This change does not convert any existing Config fields and does not
    imply a policy that existing fields must be converted when touched.
    
    The API was tested on a typical new Config.x component (to be merged
    separately). It is likely to evolve, especially when support for
    multi-directive components is added.
    
    Activated by using TYPE names with two colons (e.g., TYPE: Security::X).

--- src/ConfigOption.h
+++ src/ConfigOption.h
@@ -9,11 +9,50 @@
 #ifndef SQUID_CONFIGOPTION_H
 #define SQUID_CONFIGOPTION_H
 
+#include <iosfwd>
 #include <vector>
 
 class StoreEntry;
+class ConfigParser;
 
-/* cache option parsers */
+namespace Configuration {
+
+/// Interface for basic/low-level manipulation of a squid.conf directive value.
+/// Hides T's declarations from squid.conf parsing/reconfiguring/reporting code.
+///
+/// Implementations/specializations must not modify the current configuration
+/// (i.e. the Config objects and similar/related global state). To facilitate
+/// reuse, implementations/specializations should also be independent from any
+/// specific configuration directive name and its squid.conf location.
+///
+/// TODO: Support multi-directive components of various kinds.
+template <class T>
+class Component
+{
+public:
+    /* the code adding "TYPE: T" to cf.data.pre must specialize these */
+
+    /// creates a new T instance using the given parser; never returns nil
+    static T Parse(ConfigParser &);
+
+    /// reports the current T instance configuration in squid.conf format
+    static void Print(std::ostream &, const T &);
+
+    /// destroys Parse() result
+    static void Free(T);
+};
+
+} // namespace Configuration
+
+/*
+ * Deprecated squid.conf option wrappers used by cache_dir handling code. These
+ * classes are similar to Configuration::Component<T>, but they merge T with T
+ * parsing API, making them ill-suited for handling SquidConfig data members
+ * with built-in C++ types and, more importantly, forcing SquidConfig users to
+ * know about parsing/dumping/freeing capabilities of each SquidConfig
+ * component. They also do not hide T details from the generic squid.conf
+ * parsing code -- one has to provide a type-specific parse_T() for each T.
+ */
 
 class ConfigOption
 {
