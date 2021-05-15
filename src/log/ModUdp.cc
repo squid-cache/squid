c57ed1fcc5c6fc4771dfe288f213a251da1c8973
Francesco Chemolli <kinkie@squid-cache.org>
2021-05-15 10:25:41 +0000

    Remove dead code from src/log/ModUdp.cc

--- src/log/ModUdp.cc
+++ src/log/ModUdp.cc
@@ -43,15 +43,6 @@ logfile_mod_udp_write(Logfile * lf, const char *buf, size_t len)
     ssize_t s;
     s = write(ll->fd, (char const *) buf, len);
     fd_bytes(ll->fd, s, FD_WRITE);
-#if 0
-    if (s < 0) {
-        int xerrno = errno;
-        debugs(1, DBG_IMPORTANT, "logfile (udp): got errno (" << errno << "):" << xstrerr(xerrno));
-    }
-    if (s != len) {
-        debugs(1, DBG_IMPORTANT, "logfile (udp): len=" << len << ", wrote=" << s);
-    }
-#endif
 
     /* We don't worry about network errors for now */
 }
