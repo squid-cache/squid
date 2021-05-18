3f5582deccc5192103d105a68a7115a7981ac633
Francesco Chemolli <kinkie@squid-cache.org>
2021-05-18 13:13:19 +0000

    We we have more than one ICP packet dump #define

--- src/icp_v2.cc
+++ src/icp_v2.cc
@@ -616,24 +616,6 @@ icpHandleIcpV2(int fd, Ip::Address &from, char *buf, int len)
     }
 }
 
-#ifdef ICP_PKT_DUMP
-static void
-icpPktDump(icp_common_t * pkt)
-{
-    Ip::Address a;
-
-    debugs(12, 9, "opcode:     " << std::setw(3) << pkt->opcode  << " " << icp_opcode_str[pkt->opcode]);
-    debugs(12, 9, "version: "<< std::left << std::setw(8) << pkt->version);
-    debugs(12, 9, "length:  "<< std::left << std::setw(8) << ntohs(pkt->length));
-    debugs(12, 9, "reqnum:  "<< std::left << std::setw(8) << ntohl(pkt->reqnum));
-    debugs(12, 9, "flags:   "<< std::left << std::hex << std::setw(8) << ntohl(pkt->flags));
-    a = (struct in_addr)pkt->shostid;
-    debugs(12, 9, "shostid: " << a );
-    debugs(12, 9, "payload: " << (char *) pkt + sizeof(icp_common_t));
-}
-
-#endif
-
 void
 icpHandleUdp(int sock, void *)
 {
