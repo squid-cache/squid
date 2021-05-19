4f048314d8c9ccbf03fcf66f01dee310ddafcfa8
Francesco Chemolli <kinkie@squid-cache.org>
2021-05-19 09:53:21 +0000

    Revert changes to Modudp.cc
    Also add comment for future work

--- src/log/ModUdp.cc
+++ src/log/ModUdp.cc
@@ -43,6 +43,16 @@ logfile_mod_udp_write(Logfile * lf, const char *buf, size_t len)
     ssize_t s;
     s = write(ll->fd, (char const *) buf, len);
     fd_bytes(ll->fd, s, FD_WRITE);
+#if 0
+    // TODO: reduce verbosity to 3 and use a FadingCounter to reduce spamminess
+    if (s < 0) {
+        int xerrno = errno;
+        debugs(1, DBG_IMPORTANT, "logfile (udp): got errno (" << errno << "):" << xstrerr(xerrno));
+    }
+    if (s != len) {
+        debugs(1, DBG_IMPORTANT, "logfile (udp): len=" << len << ", wrote=" << s);
+    }
+#endif
 
     /* We don't worry about network errors for now */
 }
