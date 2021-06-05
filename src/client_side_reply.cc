86461922f3d8b188fc423d1bead76e2ffe33a974
Francesco Chemolli <kinkie@squid-cache.org>
2021-06-05 17:45:23 +0000

    Remove stuff that snuck in on rebase

--- src/client_side_reply.cc
+++ src/client_side_reply.cc
@@ -1131,40 +1131,6 @@ clientReplyContext::storeNotOKTransferDone() const
     }
 }
 
-/* A write has completed, what is the next status based on the
- * canonical request data?
- * 1 something is wrong
- * 0 nothing is wrong.
- *
- */
-int
-clientHttpRequestStatus(int fd, ClientHttpRequest const *http)
-{
-#if SIZEOF_INT64_T == 4
-    if (http->out.size > 0x7FFF0000) {
-        debugs(88, DBG_IMPORTANT, "WARNING: closing FD " << fd << " to prevent out.size counter overflow");
-        if (http->getConn())
-            debugs(88, DBG_IMPORTANT, "\tclient " << http->getConn()->peer);
-        debugs(88, DBG_IMPORTANT, "\treceived " << http->out.size << " bytes");
-        debugs(88, DBG_IMPORTANT, "\tURI " << http->log_uri);
-        return 1;
-    }
-
-    if (http->out.offset > 0x7FFF0000) {
-        debugs(88, DBG_IMPORTANT, "WARNING: closing FD " << fd < " to prevent out.offset counter overflow");
-        if (http->getConn())
-            debugs(88, DBG_IMPORTANT, "\tclient " << http->getConn()->peer);
-        debugs(88, DBG_IMPORTANT, "\treceived " << http->out.size << " bytes, offset " << http->out.offset);
-        debugs(88, DBG_IMPORTANT, "\tURI " << http->log_uri);
-        return 1;
-    }
-#else
-    (void)fd;
-    (void)http;
-#endif
-    return 0;
-}
-
 /* Preconditions:
  * *http is a valid structure.
  * fd is either -1, or an open fd.
