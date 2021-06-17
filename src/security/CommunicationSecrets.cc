4106be3f9c2a38956a554f3f4ef3eb41b89f6531
squidadm <squidadm@users.noreply.github.com>
2021-06-17 18:10:24 +0000

    Source Format Enforcement (#845)

--- src/security/CommunicationSecrets.cc
+++ src/security/CommunicationSecrets.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1996-2020 The Squid Software Foundation and contributors
+ * Copyright (C) 1996-2021 The Squid Software Foundation and contributors
  *
  * Squid software is distributed under GPLv2+ license and includes
  * contributions from numerous individuals and organizations.
@@ -8,8 +8,8 @@
 
 #include "squid.h"
 #include "base/CharacterSet.h"
-#include "security/Session.h"
 #include "security/CommunicationSecrets.h"
+#include "security/Session.h"
 
 #include <ostream>
 
