f25c5b17ab05bcb5d45e76f2471998b4f212e476
Alex Rousskov <rousskov@measurement-factory.com>
2021-07-08 11:25:55 -0400

    fixup: polished moved code
    * declare variables as late as possible
    * consistent setsockopt() 4th parameter shape
    * marked known out-of-scope bugs
    * const, auto

--- src/comm/Tcp.cc
+++ src/comm/Tcp.cc
@@ -22,34 +22,36 @@ Comm::ApplyTcpKeepAlive(int fd, const TcpKeepAlive &cfg)
     if (!cfg.enabled)
         return;
 
-    int on = 1;
 #if !defined(TCP_KEEPCNT)
     if (cfg.timeout && cfg.interval) {
-        int count = (cfg.timeout + cfg.interval - 1) / cfg.interval; // XXX: unsigned-to-signed conversion
+        const int count = (cfg.timeout + cfg.interval - 1) / cfg.interval; // XXX: unsigned-to-signed conversion
         if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPCNT, &count, sizeof(count)) < 0) {
-            int xerrno = errno;
+            const auto xerrno = errno;
             debugs(5, DBG_IMPORTANT, MYNAME << "FD " << fd << ": " << xstrerr(xerrno));
         }
     }
 #endif
 #if !defined(TCP_KEEPIDLE)
     if (cfg.idle) {
+        // XXX: TCP_KEEPIDLE expects an int; cfg.idle is unsigned
         if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPIDLE, &cfg.idle, sizeof(cfg.idle)) < 0) {
-            int xerrno = errno;
+            const auto xerrno = errno;
             debugs(5, DBG_IMPORTANT, MYNAME << "FD " << fd << ": " << xstrerr(xerrno));
         }
     }
 #endif
 #if !defined(TCP_KEEPINTVL)
     if (cfg.interval) {
+        // XXX: TCP_KEEPINTVL expects an int; cfg.interval is unsigned
         if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPINTVL, &cfg.interval, sizeof(cfg.interval)) < 0) {
-            int xerrno = errno;
+            const auto xerrno = errno;
             debugs(5, DBG_IMPORTANT, MYNAME << "FD " << fd << ": " << xstrerr(xerrno));
         }
     }
 #endif
-    if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, (char *) &on, sizeof(on)) < 0) {
-        int xerrno = errno;
+    const int on = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &on, sizeof(on)) < 0) {
+        const auto xerrno = errno;
         debugs(5, DBG_IMPORTANT, MYNAME << "FD " << fd << ": " << xstrerr(xerrno));
     }
 }
