5fb0b6d41a67a5e802b711cf9ef650c759495cd2
Alex Rousskov <rousskov@measurement-factory.com>
2021-07-13 18:21:44 -0400

    fixup: Fix branch typo that disabled most of the moved code

--- src/comm/Tcp.cc
+++ src/comm/Tcp.cc
@@ -46,19 +46,19 @@ Comm::ApplyTcpKeepAlive(int fd, const TcpKeepAlive &cfg)
     if (!cfg.enabled)
         return;
 
-#if !defined(TCP_KEEPCNT)
+#if defined(TCP_KEEPCNT)
     if (cfg.timeout && cfg.interval) {
         const int count = (cfg.timeout + cfg.interval - 1) / cfg.interval; // XXX: unsigned-to-signed conversion
         (void)SetSocketOption(fd, IPPROTO_TCP, TCP_KEEPCNT, count);
     }
 #endif
-#if !defined(TCP_KEEPIDLE)
+#if defined(TCP_KEEPIDLE)
     if (cfg.idle) {
         // XXX: TCP_KEEPIDLE expects an int; cfg.idle is unsigned
         (void)SetSocketOption(fd, IPPROTO_TCP, TCP_KEEPIDLE, cfg.idle);
     }
 #endif
-#if !defined(TCP_KEEPINTVL)
+#if defined(TCP_KEEPINTVL)
     if (cfg.interval) {
         // XXX: TCP_KEEPINTVL expects an int; cfg.interval is unsigned
         (void)SetSocketOption(fd, IPPROTO_TCP, TCP_KEEPINTVL, cfg.interval);
