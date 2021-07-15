8a07d123ef7e986b601731fdaf4c93cc32471c73
Alex Rousskov <rousskov@measurement-factory.com>
2021-07-15 05:58:19 +0000

    Remove bogus "found KEY_PRIVATE" WARNINGs (#862)
    ... triggered by private bumped StoreEntry unlock()ed in ~ServerBump().
    
    The WARNINGs were added long time ago (commit fc8b9fc) because, AFAICT,
    earlier Store code expected StoreEntry owners to release() uncachable
    entries, including KEY_PRIVATE ones, right before unlocking them.
    However, there is no compile-time enforcement of that expectation, and
    unlocking code does not always know whether the entry is cachable (as
    ServerBump constructor/destructor RAII code illustrates).
    
    This change stops tying release and unlocking decisions/actions together
    but makes sure that idle KEY_PRIVATE entries are still released (because
    we do not want to index unneeded/unusable Store entries).

--- src/clients/FtpRelay.cc
+++ src/clients/FtpRelay.cc
@@ -160,8 +160,8 @@ Ftp::Relay::Relay(FwdState *const fwdState):
     savedReply.lastReply = NULL;
     savedReply.replyCode = 0;
 
-    // Nothing we can do at request creation time can mark the response as
-    // uncachable, unfortunately. This prevents "found KEY_PRIVATE" WARNINGs.
+    // Prevent the future response from becoming public and being shared/cached
+    // because FTP does not support response cachability and freshness checks.
     entry->releaseRequest();
     AsyncCall::Pointer call = asyncCall(9, 4, "Ftp::Relay::Abort", cbdataDialer(&Relay::HandleStoreAbort, this));
     entry->registerAbortCallback(call);
