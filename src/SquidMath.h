683d78c0a821b78d5bf465816841db6ec79c5f7d
Alex Rousskov <rousskov@measurement-factory.com>
2021-07-28 18:29:08 -0400

    Fix Sum() by replacing it with a simpler, safer NaturalSum()
    While testing future cache entry expiration computation code, we found a
    Sum() bug: Sum(0l, -1lu) returns -1 instead of overflowing (long cannot
    hold the maximum unsigned long value). This discovery triggered an
    investigation that discovered several Sum() flaws, even inside the
    supposed-to-be-trivial implementation branch for unsigned operands:
    
    1. "sizeof(T) >= sizeof(U)" assertion missed that same-size types have
       very different maximums when exactly one of the types is signed.
    
    2. "auto sum = a + b" assignment missed that "auto" may be bigger than
       the return type "T" and, hence, the sum may not overflow when it does
       not fit T. The overflow would go undetected and the result will be
       truncated (to fit T) in the return statement. This automatic sum type
       enlargement may be due to signed->unsigned integral conversion
       related to the previous bullet, but it can also happen because of
       integral _promotions_ (e.g., those that convert "char" into "int").
    
    3. Sum() silently truncated its arguments to fit T. Before commit
       1fba9ab, that silent truncation only applied to the first argument
       (if T did not match its actual type). After that commit, all other
       arguments could be silently truncated (except the last one).
    
    4. It is trivial for the caller to do everything right when calling
       Sum() but then assign the result to a variable that cannot hold Sum()
       value, essentially undoing all the overflow detection work.
    
    Fortunately, none of these bugs affected two existing Sum() callers.
    
    Some of these problems were very difficult to fix! Eventually, a new,
    simplified concept emerged that was easier to implement and that was a
    better match for Squid current and foreseeable needs: NaturalSum().
    
    NaturalSum() is designed for cases where we want an exact sum of
    arguments but do not consider negative arguments as valid numbers. This
    both simplifies the implementation and protects typical callers from
    adding "-1" (e.g., a special "no delay" option setting) to "3600" (e.g.,
    configured TTL) and getting a meaningless result of 3599.
    
    NaturalSum() requires the caller to specify the summation type that will
    be used to accumulate the sum value as Squid iterates over arguments.
    Sum() wanted to automatically use the largest type that can accommodate
    (partial) sums, but that complicated task was not implemented and
    becomes unnecessary when dealing with only natural numbers -- there is
    no need to temporary inflate the partial sum (beyond what the resulting
    type can hold) in case some negative operand will decrease it later.
    
    Also added SetToNaturalSumOrMax() to allow the caller to reset a
    variable without guessing its type, avoiding silent sum truncation at
    assignment time.
    
    Also added unit test cases.

--- src/SquidMath.h
+++ src/SquidMath.h
@@ -38,53 +38,89 @@ using EnableIfType = typename std::enable_if<B,T>::type;
 
 /// detects a pair of unsigned types
 /// reduces code duplication in Sum() declarations below
-template <typename T, typename U>
+template <typename A, typename B>
 using AllUnsigned = typename std::conditional<
-                    std::is_unsigned<T>::value && std::is_unsigned<U>::value,
+                    std::is_unsigned<A>::value && std::is_unsigned<B>::value,
                     std::true_type,
                     std::false_type
                     >::type;
 
-/// \returns a non-overflowing sum of the two unsigned arguments (or nothing)
-template <typename T, typename U, EnableIfType<AllUnsigned<T,U>::value, int> = 0>
-Optional<T>
-Sum(const T a, const U b) {
-    // Instead of computing the largest type dynamically, we simply go by T and
-    // reject cases like Sum(0, ULLONG_MAX) that would overflow on return.
-    // TODO: Consider using std::common_type<T, U> in the return type instead.
-    static_assert(sizeof(T) >= sizeof(U), "Sum() return type can fit its (unsigned) result");
-
-    // this optimized implementation relies on unsigned overflows
-    static_assert(std::is_unsigned<T>::value, "the first Sum(a,b) argument is unsigned");
-    static_assert(std::is_unsigned<U>::value, "the second Sum(a,b) argument is unsigned");
-    const auto sum = a + b;
-    // when a+b overflows, the result becomes smaller than any operand
-    return (sum < a) ? Optional<T>() : Optional<T>(sum);
+/// whether integer a is less than integer b, with correct overflow handling
+template <typename A, typename B>
+constexpr bool
+Less(const A a, const B b) {
+    // The casts below make standard C++ integral conversions explicit. They
+    // quell compiler warnings about signed/unsigned comparison. The first two
+    // lines exclude different-sign a and b, making the casts/comparison safe.
+    using AB = typename std::common_type<A, B>::type;
+    return
+        (a >= 0 && b < 0) ? false :
+        (a < 0 && b >= 0) ? true :
+        /* (a >= 0) == (b >= 0) */ static_cast<AB>(a) < static_cast<AB>(b);
 }
 
-/// \returns a non-overflowing sum of the two signed arguments (or nothing)
-template <typename T, typename U, EnableIfType<!AllUnsigned<T,U>::value, int> = 0>
-Optional<T> constexpr
-Sum(const T a, const U b) {
-    // Instead of computing the largest type dynamically, we simply go by T and
-    // reject cases like Sum(0, LLONG_MAX) that would overflow on return.
-    static_assert(sizeof(T) >= sizeof(U), "Sum() return type can fit its (signed) result");
+/// \returns a non-overflowing sum of the two unsigned arguments (or nothing)
+template <typename S, typename T, EnableIfType<AllUnsigned<S,T>::value, int> = 0>
+Optional<S>
+IncreaseSum(const S s, const T t) {
+    // this optimized implementation relies on unsigned overflows
+    static_assert(std::is_unsigned<S>::value, "the first argument is unsigned");
+    static_assert(std::is_unsigned<T>::value, "the second argument is unsigned");
+    // For the sum overflow check below to work, we cannot restrict the sum
+    // type which, due to integral promotions, may exceed common_type<S,T>!
+    const auto sum = s + t;
+    // 1. when summation overflows, the result becomes smaller than any operand
+    // 2. the unknown (see above) "auto" type may hold more than S can hold
+    return (s <= sum && sum <= std::numeric_limits<S>::max()) ?
+           Optional<S>(sum) : Optional<S>();
+}
 
-    // tests below avoid undefined behavior of signed under/overflows
-    return b >= 0 ?
-           ((a > std::numeric_limits<U>::max() - b) ? Optional<T>() : Optional<T>(a + b)):
-           ((a < std::numeric_limits<U>::min() - b) ? Optional<T>() : Optional<T>(a + b));
+/// \returns a non-overflowing sum of the two arguments (or nothing)
+/// \returns nothing if at least one of the arguments is negative
+/// at least one of the arguments is signed
+template <typename S, typename T, EnableIfType<!AllUnsigned<S,T>::value, int> = 0>
+Optional<S> constexpr
+IncreaseSum(const S s, const T t) {
+    return
+        // We could support a non-under/overflowing sum of negative numbers, but
+        // our callers use negative values specially (e.g., for do-not-use or
+        // do-not-limit settings) and are not supposed to do math with them.
+        (Less(s, 0) || Less(t, 0)) ? Optional<S>() :
+        // Avoids undefined behavior of signed under/overflows. When A is not T,
+        // a or t undergoes (safe) integral conversion in these expressions.
+        // Sum overflow condition: s + t > maxS or, here, maxS - s < t.
+        // If the sum exceeds maxT, integral conversions will use S, not T.
+        Less(std::numeric_limits<S>::max() - s, t) ? Optional<S>() :
+        Optional<S>(s + t);
 }
 
 /// \returns a non-overflowing sum of the arguments (or nothing)
-template <typename T, typename... Args>
-Optional<T>
-Sum(const T first, Args... args) {
-    if (const auto others = Sum<T>(args...)) {
-        return Sum<T>(first, others.value());
+template <typename S, typename T, typename... Args>
+Optional<S>
+IncreaseSum(const S sum, const T t, Args... args) {
+    if (const auto head = IncreaseSum<S>(sum, t)) {
+        return IncreaseSum<S>(head.value(), args...);
     } else {
-        return Optional<T>();
+        return Optional<S>();
     }
+}
+
+/// \returns an exact, non-overflowing sum of the arguments (or nothing)
+template <typename SummationType, typename... Args>
+Optional<SummationType>
+NaturalSum(Args... args) {
+    return IncreaseSum<SummationType>(0, args...);
+}
+
+/// Safely resets the given variable to NaturalSum() of the given arguments.
+/// If the sum overflows, resets to variable's maximum possible value.
+/// \returns the new variable value (like an assignment operator would)
+template <typename S, typename... Args>
+S
+SetToNaturalSumOrMax(S &var, Args... args)
+{
+    var = NaturalSum<S>(args...).value_or(std::numeric_limits<S>::max());
+    return var;
 }
 
 #endif /* _SQUID_SRC_SQUIDMATH_H */
