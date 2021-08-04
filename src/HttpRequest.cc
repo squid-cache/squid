squid-cache/squid

commit a7e9f64702e904e0186999870dadc0d67a7f670f

fixup: Replace initClient-has-port runtime assert with compile time one
This replacement makes the code a lot safer because bugs (i.e.
MasterXaction initialization code that forgets to supply a port for
initClient) will be rejected by the compiler.

This safety improvement hit one interesting wrinkle: The Downloader
class was implemented to spawn its own MasterXaction using
XactionInitiator info supplied by the download requestor. That design is
not compatible with the new static assertion because each MasterXaction
creator must now hard-code XactionInitiator value for the assertion to
work at _compile_ time. All other code naturally hard-codes that value.

I saw two ways to resolve this conflict:

a) Let the download requestor create the MasterXaction object for the
   Downloader. The primary advantage of this (implemented) approach is
   that it allows (future) client request processing code to satisfy
   client requests using Downloader. Such request satisfaction requires
   sharing the master transaction between the client transaction and the
   downloader transaction, and this change enables such sharing. This
   change moves the "Which master transaction does this download belong
   to?" question from the Downloader into download requestors.

b) Move initClient-has-port enforcement (and squidPort) from
   MasterXaction into XactionInitiator. The primary advantage of this
   (not implemented) approach is that it places that enforcement inside
   the type it is meant to police (essentially) -- XactionInitiator.

The two changes are complementary. I did not implement (b) because it
requires significant XactionInitiator redesign, moving _all_ originating
client information from MasterXaction to XactionInitiator (currently
squidPort, tcpClient, and hasListeningPort()). That move is probably a
good idea, but it requires even more changes that go way beyond the
scope of supporting PROXY protocol on an https_port.

src/MasterXaction.h
@@ -41,20 +41,20 @@ class MasterXaction : public RefCountable
 public:
     typedef RefCount<MasterXaction> Pointer;
 
-    /// Does not have association with a listening port.
-    /// \param anInitiator any value other from XactionInitiator::initClient.
-    explicit MasterXaction(const XactionInitiator anInitiator) :
-        initiator(anInitiator)
+    /// Create a master transaction not associated with a AnyP::PortCfg port.
+    template <XactionInitiator::Initiator anInitiator>
+    static Pointer MakePortless()
     {
-        assert(!initiator.in(XactionInitiator::initClient));
+        static_assert(anInitiator != XactionInitiator::initClient);
+        return new MasterXaction(anInitiator, nullptr);
     }
 
-    /// Requires a listening port.
-    /// Compatible only with XactionInitiator::initClient.
-    explicit MasterXaction(const AnyP::PortCfgPointer &port) :
-        squidPort(port),
-        initiator(XactionInitiator::initClient)
-    {}
+    /// Create a master transaction associated with a AnyP::PortCfg port.
+    /// \param aPort may be nil if port information was lost
+    static Pointer MakePortful(const AnyP::PortCfgPointer &aPort)
+    {
+        return new MasterXaction(XactionInitiator::initClient, aPort);
+    }
 
     /// a convenience method returning TrafficMode::interceptedSomewhere() for the port accepted this transaction
     /// \see TrafficMode::interceptedSomewhere() for details
@@ -76,6 +76,13 @@ public:
     bool generatingConnect = false;
 
     // TODO: add state from other Jobs in the transaction
+
+private:
+    // use public Make() functions instead
+    MasterXaction(const XactionInitiator anInitiator, const AnyP::PortCfgPointer &aPort):
+        squidPort(aPort),
+        initiator(anInitiator)
+    {}
 };
 
 #endif /* SQUID_SRC_MASTERXACTION_H */

src/Downloader.cc
@@ -63,12 +63,12 @@ Downloader::CbDialer::print(std::ostream &os) const
     os << " Http Status:" << status << Raw("body data", object.rawContent(), 64).hex();
 }
 
-Downloader::Downloader(SBuf &url, AsyncCall::Pointer &aCallback, const XactionInitiator initiator, unsigned int level):
+Downloader::Downloader(const SBuf &url, const AsyncCall::Pointer &aCallback, const MasterXactionPointer &masterXaction, unsigned int level):
     AsyncJob("Downloader"),
     url_(url),
     callback_(aCallback),
     level_(level),
-    initiator_(initiator)
+    masterXaction_(masterXaction)
 {
 }
 
@@ -128,8 +128,7 @@ Downloader::buildRequest()
 {
     const HttpRequestMethod method = Http::METHOD_GET;
 
-    const MasterXaction::Pointer mx = new MasterXaction(initiator_);
-    auto * const request = HttpRequest::FromUrl(url_, mx, method);
+    const auto request = HttpRequest::FromUrl(url_, masterXaction_, method);
     if (!request) {
         debugs(33, 5, "Invalid URI: " << url_);
         return false; //earlyError(...)

src/Downloader.h
@@ -14,13 +14,14 @@
 #include "http/forward.h"
 #include "http/StatusCode.h"
 #include "sbuf/SBuf.h"
-#include "XactionInitiator.h"
 
 class ClientHttpRequest;
 class StoreIOBuffer;
 class clientStreamNode;
 class DownloaderContext;
 typedef RefCount<DownloaderContext> DownloaderContextPointer;
+class MasterXaction;
+using MasterXactionPointer = RefCount<MasterXaction>;
 
 /// The Downloader class fetches SBuf-storable things for other Squid
 /// components/transactions using internal requests. For example, it is used
@@ -46,7 +47,7 @@ public:
         Http::StatusCode status;
     };
 
-    Downloader(SBuf &url, AsyncCall::Pointer &aCallback, const XactionInitiator initiator, unsigned int level = 0);
+    Downloader(const SBuf &url, const AsyncCall::Pointer &aCallback, const MasterXactionPointer &, unsigned int level = 0);
     virtual ~Downloader();
     virtual void swanSong();
 
@@ -76,8 +77,7 @@ private:
     AsyncCall::Pointer callback_; ///< callback to call when download finishes
     SBuf object_; ///< the object body data
     const unsigned int level_; ///< holds the nested downloads level
-    /// The initiator of the download request.
-    XactionInitiator initiator_;
+    MasterXactionPointer masterXaction_; ///< download transaction context
 
     /// Pointer to an object that stores the clientStream required info
     DownloaderContextPointer context_;

src/PeerPoolMgr.cc
@@ -60,7 +60,7 @@ PeerPoolMgr::start()
 {
     AsyncJob::start();
 
-    const MasterXaction::Pointer mx = new MasterXaction(XactionInitiator::initPeerPool);
+    const auto mx = MasterXaction::MakePortless<XactionInitiator::initPeerPool>();
     // ErrorState, getOutgoingAddress(), and other APIs may require a request.
     // We fake one. TODO: Optionally send this request to peers?
     request = new HttpRequest(Http::METHOD_OPTIONS, AnyP::PROTO_HTTP, "http", "*", mx);

src/acl/Asn.cc
@@ -237,7 +237,7 @@ asnCacheStart(int as)
     debugs(53, 3, "AS " << as);
     ASState *asState = new ASState;
     asState->as_number = as;
-    const MasterXaction::Pointer mx = new MasterXaction(XactionInitiator::initAsn);
+    const auto mx = MasterXaction::MakePortless<XactionInitiator::initAsn>();
     asState->request = new HttpRequest(mx);
     asState->request->url = whoisUrl;
     asState->request->method = Http::METHOD_GET;

src/adaptation/ecap/Host.cc
@@ -163,7 +163,7 @@ Adaptation::Ecap::Host::closeDebug(std::ostream *debug)
 Adaptation::Ecap::Host::MessagePtr
 Adaptation::Ecap::Host::newRequest() const
 {
-    static const MasterXaction::Pointer mx = new MasterXaction(XactionInitiator::initAdaptationOrphan_);
+    static const auto mx = MasterXaction::MakePortless<XactionInitiator::initAdaptationOrphan_>();
     return MessagePtr(new Adaptation::Ecap::MessageRep(new HttpRequest(mx)));
 }
 

src/adaptation/icap/Xaction.cc
@@ -101,7 +101,7 @@ Adaptation::Icap::Xaction::Xaction(const char *aTypeName, Adaptation::Icap::Serv
 {
     debugs(93,3, typeName << " constructed, this=" << this <<
            " [icapx" << id << ']'); // we should not call virtual status() here
-    const MasterXaction::Pointer mx = new MasterXaction(XactionInitiator::initAdaptation);
+    const auto mx = MasterXaction::MakePortless<XactionInitiator::initAdaptation>();
     icapRequest = new HttpRequest(mx);
     HTTPMSGLOCK(icapRequest);
     icap_tr_start = current_time;

src/client_side.cc
@@ -2580,7 +2580,7 @@ ConnStateData::postHttpsAccept()
             return;
         }
 
-        MasterXaction::Pointer mx = new MasterXaction(XactionInitiator::initClient);
+        const auto mx = MasterXaction::MakePortful(port);
         mx->tcpClient = clientConnection;
         // Create a fake HTTP request and ALE for the ssl_bump ACL check,
         // using tproxy/intercept provided destination IP and port.
@@ -3281,7 +3281,7 @@ ConnStateData::buildFakeRequest(Http::MethodType const method, SBuf &useHost, un
     extendLifetime();
     stream->registerWithConn();
 
-    MasterXaction::Pointer mx = new MasterXaction(port);
+    const auto mx = MasterXaction::MakePortful(port);
     mx->tcpClient = clientConnection;
     // Setup Http::Request object. Maybe should be replaced by a call to (modified)
     // clientProcessRequest

commit 6b979dda6c97cc1564b3dd3fe9a06034e2ff9f47

Several improvements
* made several TrafficMode methods private
* adjusted method descriptions
* fixed a compilation error introduced by a7e9f647

src/HttpRequest.cc
@@ -806,12 +806,12 @@ FindListeningPortAddress(const HttpRequest *callerRequest, const AccessLogEntry 
     if (!ip && ale)
         ip = FindListeningPortAddressInPort(ale->cache.port);
 
-    if (ip || request->masterXaction->hasListeningInterceptedPort())
-        return ip;
-
     // if (request->masterXaction->squidPort->flags.proxySurrogate()) {
     //     XXX: handle PROXY protocol here when we have a flag to identify such request
     // }
+
+    if (ip || request->masterXaction->hasListeningInterceptedPort())
+        return ip;
 
     /* handle non-intercepted cases that were not handled above */
     ip = FindListeningPortAddressInConn(request->masterXaction->tcpClient);

src/HttpRequest.h
@@ -104,7 +104,8 @@ public:
 
     /// associates the request with a from-client connection manager
     void manager(const CbcPointer<ConnStateData> &aMgr, const AccessLogEntryPointer &al);
-    /// should use the original server IP and port provided by the client
+    /// security issue or admin requires this message be sent
+    /// to the original server IP and port provided by the client
     bool mustGoToOriginalDestination() const;
 
 protected:

src/MasterXaction.h
@@ -45,7 +45,7 @@ public:
     template <XactionInitiator::Initiator anInitiator>
     static Pointer MakePortless()
     {
-        static_assert(anInitiator != XactionInitiator::initClient);
+        static_assert(anInitiator != XactionInitiator::initClient, "a portless MasterXaction");
         return new MasterXaction(anInitiator, nullptr);
     }
 

src/anyp/TrafficMode.h
@@ -92,19 +92,6 @@ public:
     /// connecting directly to this port (since commit 151ba0d).
     bool interceptedSomewhere() const { return flags_.natIntercept || flags_.tproxyIntercept || proxySurrogateHttpsSslBump(); }
 
-    /// The client of the accepted TCP connection was connecting to this port.
-    /// The accepted traffic may have been intercepted earlier!
-    bool tcpToUs() const { return proxySurrogate() || !interceptedSomewhere(); }
-
-    /// The client of the accepted TCP connection was not connecting to this port.
-    /// The accepted traffic may have been intercepted earlier as well!
-    bool interceptedLocally() const { return interceptedSomewhere() && !tcpToUs(); }
-
-    // Unused yet.
-    /// This port handles traffic that has been intercepted prior to being delivered
-    /// to the TCP client of the accepted connection (which then connected to us).
-    bool interceptedRemotely() const { return interceptedSomewhere() && tcpToUs(); }
-
     /// The client of the accepted TCP connection was connecting directly to this proxy port.
     bool forwarded() const { return !interceptedSomewhere() && !flags_.accelSurrogate; }
 
@@ -112,10 +99,16 @@ public:
     bool proxySurrogate() const { return flags_.proxySurrogate; }
 
     /// interceptedLocally() with configured NAT interception
-    bool natInterceptLocally() const { return flags_.natIntercept && interceptedLocally(); }
+    bool natInterceptLocally() const {
+        assert(!flags_.natIntercept || interceptedLocally());
+        return flags_.natIntercept;
+    }
 
     /// interceptedLocally() with configured TPROXY interception
-    bool tproxyInterceptLocally() const { return flags_.tproxyIntercept && interceptedLocally(); }
+    bool tproxyInterceptLocally() const {
+        assert(!flags_.tproxyIntercept || interceptedLocally());
+        return flags_.tproxyIntercept;
+    }
 
     /// whether the reverse proxy is configured
     bool accelSurrogate() const { return flags_.accelSurrogate; }
@@ -127,6 +120,17 @@ public:
     std::ostream &print(std::ostream &) const;
 
 private:
+    /// The client of the accepted TCP connection was connecting to this port.
+    /// The accepted traffic may have been intercepted earlier!
+    bool tcpToUs() const { return proxySurrogate() || !interceptedSomewhere(); }
+
+    /// The client of the accepted connection was not connecting to this port.
+    bool interceptedLocally() const { return interceptedSomewhere() && !tcpToUs(); }
+
+    /// This port handles traffic that has been intercepted prior to being delivered
+    /// to the TCP client of the accepted connection (which then connected to us).
+    // bool interceptedRemotely() const { return interceptedSomewhere() && tcpToUs(); }
+
     /// \returns true for HTTPS ports with SSL bump receiving PROXY protocol traffic
     bool proxySurrogateHttpsSslBump() const {
         return flags_.proxySurrogate && flags_.tunnelSslBumping && flags_.portKind == TrafficModeFlags::httpsPort;