9a146d329fad3b627dad64037062231eef9f30d8
Amos Jeffries <yadij@users.noreply.github.com>
2021-10-26 15:14:59 +1300

    Update src/security/CertGadgets.cc
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>

--- src/security/CertGadgets.cc
+++ src/security/CertGadgets.cc
@@ -89,7 +89,7 @@ Security::CertSubjectName(const CertPointer &cert)
     gnutls_datum_t str;
     x = gnutls_x509_dn_get_str(dn, &str);
     if (x != GNUTLS_E_SUCCESS) {
-        debugs(83, DBG_PARSE_NOTE(2), "WARNING: cannot get certificate SubjectName: " << Security::ErrorString(x));
+        debugs(83, DBG_PARSE_NOTE(2), "WARNING: cannot describe certificate SubjectName: " << Security::ErrorString(x));
         return out;
     }
     out.append(reinterpret_cast<const char *>(str.data), str.size);
