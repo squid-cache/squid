a47c7eb4bb777904b92c146fa7994f8429621f12
Francesco Chemolli <kinkie@squid-cache.org>
2021-11-10 06:48:35 +0000

    Fix typo

--- src/base/ClpMap.h
+++ src/base/ClpMap.h
@@ -105,7 +105,7 @@ private:
     using Entries = std::list<Entry, PoolingAllocator<Entry> >;
     using EntriesIterator = typename Entries::iterator;
 
-    using IndexItemt = std::pair<const Key, EntriesIterator>;
+    using IndexItem = std::pair<const Key, EntriesIterator>;
     /// key:entry_position mapping for fast entry lookups by key
     using Index = std::unordered_map<Key, EntriesIterator, std::hash<Key>, std::equal_to<Key>, PoolingAllocator<IndexItem> >;
     using IndexIterator = typename Index::iterator;
