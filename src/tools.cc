c633a6b031df36fca223728f72753f69b2bbb4a8
Amos Jeffries <yadij@users.noreply.github.com>
2021-12-04 21:57:01 +0000

    Remove NeXTSTEP support (#943)
    NeXT architecture hardware is no longer available and with that
    hardware any support by Apple ceased.
    
    QNX and MacOS descendant systems which still depend on some of
    the workarounds already are or can be supported under their own
    compatibility section.

--- src/tools.cc
+++ src/tools.cc
@@ -1146,11 +1146,7 @@ restoreCapabilities(bool keep)
 pid_t
 WaitForOnePid(pid_t pid, PidStatus &status, int flags)
 {
-#if _SQUID_NEXT_
-    if (pid < 0)
-        return wait3(&status, flags, NULL);
-    return wait4(pid, &status, flags, NULL);
-#elif _SQUID_WINDOWS_
+#if _SQUID_WINDOWS_
     return 0; // function not used on Windows
 #else
     return waitpid(pid, &status, flags);
