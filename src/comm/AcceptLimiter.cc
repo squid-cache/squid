b691925b5f2f754dbf7274dd1923bc5e8f86fbc1
Amos Jeffries <yadij@users.noreply.github.com>
2021-12-17 22:19:00 +1300

    Apply suggestions from code review
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>

--- src/comm/AcceptLimiter.cc
+++ src/comm/AcceptLimiter.cc
@@ -48,7 +48,7 @@ Comm::AcceptLimiter::kick()
         /* NP: shift() is equivalent to pop_front(). Giving us a FIFO queue. */
         TcpAcceptor::Pointer temp = deferred_.front();
         deferred_.erase(deferred_.begin());
-        if (auto *job = temp.get()) {
+        if (const auto job = temp.valid()) {
             debugs(5, 5, "doing one.");
             typedef CommCbMemFunT<Comm::TcpAcceptor, CommIoCbParams> Dialer;
             AsyncCall::Pointer call = JobCallback(33, 5, Dialer, job, TcpAcceptor::acceptOne);
