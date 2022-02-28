0c206c9081ffb79066e945af58d8e5d72a3b8bc7
Alex Rousskov <rousskov@measurement-factory.com>
2022-03-01 07:42:40 +1300

    Add RegisteredRunners::bootstrapConfig event hook

--- src/main.cc
+++ src/main.cc
@@ -1589,6 +1589,8 @@ SquidMain(int argc, char **argv)
 
         Format::Token::Init(); // XXX: temporary. Use a runners registry of pre-parse runners instead.
 
+        RunRegisteredHere(RegisteredRunner::bootstrapConfig);
+
         try {
             parse_err = parseConfigFile(ConfigFile);
         } catch (...) {
