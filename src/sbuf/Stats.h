a02ae6da98a6be501bda00be076c53889f94717c
Alex Rousskov <rousskov@measurement-factory.com>
2022-03-25 10:29:30 -0400

    Detach libsbuf from StatHist to facilitate SBuf reuse
    The stated purpose of sbuf/DetailedStats was to "avoid adding external
    dependencies to the SBuf code". DetailedStats failed to accomplish that
    because it introduced a dependency on StatHist (which depends on Store).
    
    To break the unwanted dependency, we outsource at-destruction-time size
    statistics collection to external-to-SBuf code, configurable via the new
    SBufStats fields. TODO: Long-term, StatHist itself should be refactored
    to become reusable without unwanted heavy dependencies.

--- src/sbuf/Stats.h
+++ src/sbuf/Stats.h
@@ -26,6 +26,12 @@ public:
 
     SBufStats& operator +=(const SBufStats&);
 
+    /// Record the size a SBuf had when it was destructed
+    static void RecordSBufSizeAtDestruct(size_t);
+
+    /// Record the size a MemBlob had when it was destructed
+    static void RecordMemBlobSizeAtDestruct(size_t);
+
 public:
     uint64_t alloc = 0; ///<number of calls to SBuf constructors
     uint64_t allocCopy = 0; ///<number of calls to SBuf copy-constructor
@@ -51,6 +57,13 @@ public:
     uint64_t cowJustAlloc = 0; ///< number of cow() calls requiring just a new empty buffer
     uint64_t cowAllocCopy = 0; ///< number of cow() calls requiring copying into a new buffer
     uint64_t live = 0;  ///<number of currently-allocated SBuf
+
+    /// function for collecting detailed size-related statistics
+    using SizeRecorder = void (*)(size_t);
+    /// collects statistics about SBuf sizes at SBuf destruction time
+    static SizeRecorder SBufSizeAtDestructRecorder;
+    /// collects statistics about MemBlob capacity at MemBlob destruction time
+    static SizeRecorder MemBlobSizeAtDestructRecorder;
 };
 
 #endif /* SQUID_SBUF_STATS_H */
