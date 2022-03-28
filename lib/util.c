2aa32d3bbc08417fd61b2bd5b8de1da45c1ff818
Amos Jeffries <amosjeffries@squid-cache.org>
2022-03-28 22:14:49 +1300

    Remove SCO 3.2 support
    This OS is now obsolete. Users wishing to build for this OS
    can use build time options:
     ./configure --disable-poll CFLAGS=-lintl
    
    also add to lib/util.c:  #define rint(X) floor((X) + 0.5)

--- lib/util.c
+++ lib/util.c
@@ -59,12 +59,7 @@ xpercent(double part, double whole)
 int
 xpercentInt(double part, double whole)
 {
-#if HAVE_RINT
     return (int) rint(xpercent(part, whole));
-#else
-    /* SCO 3.2v4.2 doesn't have rint() -- mauri@mbp.ee */
-    return (int) floor(xpercent(part, whole) + 0.5);
-#endif
 }
 
 /* somewhat safer division */
