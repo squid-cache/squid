db60d9602ed7e4d8ed8886220dd1a50453369117
Alex Rousskov <rousskov@measurement-factory.com>
2022-04-25 11:51:10 -0400

    Improve Ipc::Mem page limit accounting consistency
    If they can ask for and set a maxPurpose limit, then
    we should account for it when reporting the total limit.

--- src/ipc/mem/Pages.cc
+++ src/ipc/mem/Pages.cc
@@ -55,7 +55,7 @@ size_t
 Ipc::Mem::PageLimit()
 {
     size_t limit = 0;
-    for (int i = 0; i < PageId::maxPurpose; ++i)
+    for (int i = 0; i <= PageId::maxPurpose; ++i)
         limit += PageLimit(i);
     return limit;
 }
