0da92e5a46847738b588cb8413094067583dea1c
Francesco Chemolli <kinkie@squid-cache.org>
2022-07-09 09:08:52 +0000

    Refactor Acl
    Reduce scope of some variables in aclCacheMatchFlush

--- src/acl/Acl.cc
+++ src/acl/Acl.cc
@@ -364,15 +364,13 @@ ACL::cacheMatchAcl(dlink_list * cache, ACLChecklist *checklist)
 void
 aclCacheMatchFlush(dlink_list * cache)
 {
-    acl_proxy_auth_match_cache *auth_match;
-    dlink_node *link, *tmplink;
-    link = cache->head;
-
     debugs(28, 8, "aclCacheMatchFlush called for cache " << cache);
 
-    while (link) {
-        auth_match = (acl_proxy_auth_match_cache *)link->data;
-        tmplink = link;
+    dlink_node *link = cache->head;
+    while (link)
+    {
+        acl_proxy_auth_match_cache *auth_match = (acl_proxy_auth_match_cache *)link->data;
+        dlink_node *tmplink = link;
         link = link->next;
         dlinkDelete(tmplink, cache);
         delete auth_match;
