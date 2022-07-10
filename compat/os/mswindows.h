6f5927bf919e4d446d14a84cd9bdc6d546331232
Alex Rousskov <rousskov@measurement-factory.com>
2022-07-10 17:33:18 -0400

    fixup: Remove branch-added trailing space

--- compat/os/mswindows.h
+++ compat/os/mswindows.h
@@ -624,7 +624,7 @@ inet_ntop(int af, const void *src, char *dst, size_t size)
 {
 #if HAVE_DECL_INETNTOPA
     return (char*)InetNtopA(af, const_cast<void*>(src), dst, size);
-#else // HAVE_DECL_INET_NTOP 
+#else // HAVE_DECL_INET_NTOP
     return ::inet_ntop(af, src, dst, size);
 #endif
 }
