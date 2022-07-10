d6716bc0b4a3045cf2997fe540a85454b0c6a9b2
Alex Rousskov <rousskov@measurement-factory.com>
2022-07-10 16:17:09 -0400

    fixup: Document inet_pton() else case as well

--- compat/os/mswindows.h
+++ compat/os/mswindows.h
@@ -637,7 +637,7 @@ inet_pton(int af, const void *src, char *dst)
 {
 #if HAVE_DECL_INETPTONA
     return (char*)InetPtonA(af, const_cast<void*>(src), dst);
-#else
+#else // HAVE_DECL_INET_PTON
     return ::inet_pton(af, src, dst);
 #endif
 }
