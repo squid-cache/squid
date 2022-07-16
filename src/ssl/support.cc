e103326fdd51f1e27868b884e8b04b939bd094ef
Amos Jeffries <yadij@users.noreply.github.com>
2022-07-16 11:44:16 +0000

    Bug 5133: OpenSSL 3.0 support  (#694)
    This TLS update includes:
    
    * Fix build with OpenSSL v3.
    * Refactor RSA key generation to avoid deprecated RSA_*() APIs.
    * Refactor DH parameter and key config to avoid deprecated DH_*() APIs.
    * Refactor ECDH key creation to avoid deprecated EC_*() APIs.
    * Deprecate ssl_engine support in builds with OpenSSL v1-.
    * Disable ssl_engine support in builds OpenSSL v3+.
    
    We deprecated/removed ssl_engine support (as summarized in the last two
    bullets above) without providing an OpenSSL Providers-based alternative
    because of the following factors:
    
    1. We do not have the resources to update ssl_engine code to build
       (without deprecation warnings) with OpenSSL v3 when the feature is
       unused.
    
    2. We do not have the resources to create an OpenSSL v3 Provider-based
       replacement for ssl_engine code that uses deprecated Engine APIs.
    
    3. OpenSSL v3 deprecated Engine support (triggering deprecation warnings
       in applications that use Engine APIs with OpenSSL v3). Since Squid
       default builds use -Werror, doing nothing would break such builds.
    
    4. Squid ssl_engine does not appear to be a popular feature.

--- src/ssl/support.cc
+++ src/ssl/support.cc
@@ -556,7 +556,11 @@ Ssl::VerifyCallbackParameters::At(Security::Connection &sconn)
 }
 
 // "dup" function for SSL_get_ex_new_index("cert_err_check")
-#if SQUID_USE_CONST_CRYPTO_EX_DATA_DUP
+#if OPENSSL_VERSION_MAJOR >= 3
+static int
+ssl_dupAclChecklist(CRYPTO_EX_DATA *, const CRYPTO_EX_DATA *, void **,
+                    int, long, void *)
+#elif SQUID_USE_CONST_CRYPTO_EX_DATA_DUP
 static int
 ssl_dupAclChecklist(CRYPTO_EX_DATA *, const CRYPTO_EX_DATA *, void *,
                     int, long, void *)
@@ -653,8 +657,12 @@ Ssl::Initialize(void)
 
     SQUID_OPENSSL_init_ssl();
 
-#if !defined(OPENSSL_NO_ENGINE)
     if (::Config.SSL.ssl_engine) {
+#if OPENSSL_VERSION_MAJOR < 3
+    debugs(83, DBG_PARSE_NOTE(DBG_IMPORTANT), "WARNING: Support for ssl_engine is deprecated " <<
+           "in Squids built with OpenSSL v1 (like this Squid). " <<
+           "It is removed in Squids built with OpenSSL v3+.");
+#if !defined(OPENSSL_NO_ENGINE)
         ENGINE_load_builtin_engines();
         ENGINE *e;
         if (!(e = ENGINE_by_id(::Config.SSL.ssl_engine)))
@@ -664,11 +672,14 @@ Ssl::Initialize(void)
             const auto ssl_error = ERR_get_error();
             fatalf("Failed to initialise SSL engine: %s\n", Security::ErrorString(ssl_error));
         }
-    }
-#else
-    if (::Config.SSL.ssl_engine)
-        fatalf("Your OpenSSL has no SSL engine support\n");
+#else /* OPENSSL_NO_ENGINE */
+        throw TextException("Cannot use ssl_engine in Squid built with OpenSSL configured to disable SSL engine support", Here());
 #endif
+
+#else /* OPENSSL_VERSION_MAJOR */
+        throw TextException("Cannot use ssl_engine in Squid built with OpenSSL v3+", Here());
+#endif
+    }
 
     const char *defName = ::Config.SSL.certSignHash ? ::Config.SSL.certSignHash : SQUID_SSL_SIGN_HASH_IF_NONE;
     Ssl::DefaultSignHash = EVP_get_digestbyname(defName);
