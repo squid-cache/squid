b5dae49c2051e1fa2eb2d01ac69a9bf8cff99e3d
Alex Rousskov <rousskov@measurement-factory.com>
2022-07-21 13:45:55 -0400

    fixup: Do not modify generated files!
    Excuse: The version I was initially editing lacked the "This file is
    generated" warning. I missed that line when reviewing the upport :-(.

--- scripts/format-makefile-am.pl
+++ scripts/format-makefile-am.pl
@@ -20,7 +20,7 @@ while (<>) {
     # accumulate files and prep for sorting
     my %files = ();
     # TODO: Handle or rename /\S+SOURCE=/ and /\S*[^_]SOURCES=/
-    my $groupNameRx = qr/\S+_SOURCES|ICONS|\S+_TEMPLATES|\S+_LANGUAGES|STUB_SOURCE/;
+    my $groupNameRx = qr/\S+_SOURCES|ICONS|\S+_TEMPLATES|\S+_FILES|STUB_SOURCE/;
     if (/^($groupNameRx)\s*(\+?=)\s*(.*[^\\])$/ ) {
         my @parts = split(/\s+/, $3);
         if ($#parts == 0) { # one file only specified on same line as SOURCES
