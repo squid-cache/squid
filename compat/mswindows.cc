d5cd9dcfbf7d39ea8b5009f2b763b36c23797964
Alex Rousskov <rousskov@measurement-factory.com>
2022-07-23 10:35:15 -0400

    fixup: Do not make negative FDs unsigned

--- compat/mswindows.cc
+++ compat/mswindows.cc
@@ -189,7 +189,7 @@ getgrnam(char *unused) {
 int
 _free_osfhnd(int filehandle)
 {
-    if ((static_cast<unsigned int>(filehandle) < SQUID_MAXFD) &&
+    if (filehandle >= 0 && (static_cast<unsigned int>(filehandle) < SQUID_MAXFD) &&
             (_osfile(filehandle) & FOPEN) &&
             (_osfhnd(filehandle) != INVALID_HANDLE_VALUE))
     {
