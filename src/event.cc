81921382ce3b82b07e279510be015501bab5b039
Alex Rousskov <rousskov@measurement-factory.com>
2022-08-01 14:32:54 -0400

    Use non-deterministic seeds for eventAddIsh()
    ... because we do not want to risk multiple SMP workers to start these
    (often heavy) events at the same time.
    
    It is (now) also simpler to use non-deterministic seeds than to convince
    ourselves that they are definitely not necessary, especially in a
    context where the seed is only used once, most often at the very
    beginning of the program lifetime.

--- src/event.cc
+++ src/event.cc
@@ -9,6 +9,7 @@
 /* DEBUG: section 41    Event Processing */
 
 #include "squid.h"
+#include "base/Random.h"
 #include "event.h"
 #include "mgr/Registration.h"
 #include "Store.h"
@@ -114,9 +115,7 @@ void
 eventAddIsh(const char *name, EVH * func, void *arg, double delta_ish, int weight)
 {
     if (delta_ish >= 3.0) {
-        // Default seed is fine. We just need values random enough
-        // relative to each other to prevent waves of synchronised activity.
-        static std::mt19937 rng;
+        static std::mt19937 rng(Seed32());
         auto third = (delta_ish/3.0);
         xuniform_real_distribution<> thirdIsh(delta_ish - third, delta_ish + third);
         delta_ish = thirdIsh(rng);
