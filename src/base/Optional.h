375f536425756bf94b7bf3b34eaa39a65c54360c
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2022-08-17 17:34:55 +0000

    AsyncJob-protect and reduce ICAP DNS lookup call chain (#1112)
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>

--- src/base/Optional.h
+++ src/base/Optional.h
@@ -10,6 +10,7 @@
 #define SQUID__SRC_BASE_OPTIONAL_H
 
 #include <exception>
+#include <ostream>
 #include <type_traits>
 #include <utility>
 
@@ -120,6 +121,17 @@ private:
 
     bool hasValue_ = false;
 };
+
+template <typename Value>
+inline
+std::ostream &operator <<(std::ostream &os, const Optional<Value> &opt)
+{
+    if (opt.has_value())
+        os << opt.value();
+    else
+        os << "[no value]";
+    return os;
+}
 
 #endif /* SQUID__SRC_BASE_OPTIONAL_H */
 
