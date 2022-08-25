cde527942d1ffe8f09072927137e22d61b227d57
Alex Rousskov <rousskov@measurement-factory.com>
2022-08-25 11:39:22 -0400

    Removed one more stale ENTRY_REQUIRES_COLLAPSING test
    The now-removed code was preventing Store from joining collapsed entries
    abandoned by their writers. Store::Controller::anchorToCache() and,
    later, syncCollapsed() prevent that. The former is the right place for
    that prevention as far as newly found entries are concerned because we
    need to know entry's cache attachment status to tell whether a newly
    found entry has "collapsed". Transients do not know that status.
    
    The writer can abandon its entry at any time so there should be no
    danger in removing this test: Controller must call anchorToCache()
    (before exposing the found entry) to prepare the entry for monitoring
    SMP broadcasts about it. We could unconditionally and prematurely set
    the ENTRY_REQUIRES_COLLAPSING flag in Transients::get() as an extra
    precaution (until the StoreEntry reaches the anchorToCache() method),
    but that would create a rather misleading state/debugging.

--- src/Transients.cc
+++ src/Transients.cc
@@ -164,15 +164,6 @@ Transients::get(const cache_key *key)
         return nullptr;
     }
 
-    // store hadWriter before checking ENTRY_REQUIRES_COLLAPSING to avoid racing
-    // the writer that clears that flag and then leaves
-    const auto hadWriter = map->peekAtWriter(index);
-    if (!hadWriter && EBIT_TEST(anchor->basics.flags, ENTRY_REQUIRES_COLLAPSING)) {
-        debugs(20, 3, "not joining abandoned entry " << index);
-        map->closeForReadingAndFreeIdle(index);
-        return nullptr;
-    }
-
     StoreEntry *e = new StoreEntry();
     e->createMemObject();
     anchorEntry(*e, index, *anchor);
