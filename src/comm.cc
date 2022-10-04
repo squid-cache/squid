e42f029f3aa024fc58e1b663b08f19e9b0603c54
Alex Rousskov <rousskov@measurement-factory.com>
2022-10-04 13:26:08 +0000

    Fix GCC build on macos-11: -Wunused-parameter (#1159)

--- src/comm.cc
+++ src/comm.cc
@@ -309,6 +309,7 @@ comm_set_transparent(int fd)
 
 #else
     debugs(50, DBG_CRITICAL, "WARNING: comm_open: setsockopt(TPROXY) not supported on this platform");
+    (void)fd;
 #endif /* sockopt */
 
 #if defined(soLevel) && defined(soFlag)
