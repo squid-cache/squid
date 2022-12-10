edad3f150de8af0aeb2f629508be3219b83369b9
Alexander Bokovoy <abokovoy@redhat.com>
2022-12-10 11:50:27 +0000

    ext_kerberos_ldap_group_acl: Support -b with -D (#1207)
    When both '-b' (i.e. bind DN) and '-D' (i.e. Kerberos domain) options
    are specified, '-b' is ignored completely. This breaks the helper when a
    search subtree has to be limited (e.g., when using FreeIPA).
    
    Fix it to take '-b' into account if it was specified with '-D'.

--- src/acl/external/kerberos_ldap_group/support_ldap.cc
+++ src/acl/external/kerberos_ldap_group/support_ldap.cc
@@ -1114,7 +1114,11 @@ get_memberof(struct main_args *margs, char *user, char *domain, char *group)
                   "%s| %s: DEBUG: Error during initialisation of ldap connection: %s\n",
                   LogTime(), PROGRAM, strerror(errno));
         }
-        bindp = convert_domain_to_bind_path(domain);
+        if (margs->lbind) {
+            bindp = xstrdup(margs->lbind);
+        } else {
+            bindp = convert_domain_to_bind_path(domain);
+        }
     }
     if ((!domain || !ld) && margs->lurl && strstr(margs->lurl, "://")) {
         char *hostname;
