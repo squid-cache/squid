d280b2f7c73554c3071a3d121e6aabff5080cff4
Alex Rousskov <rousskov@measurement-factory.com>
2022-12-10 14:18:18 +0000

    Fix ACL type typo in req_header, rep_header key-changing ERRORs (#1208)

--- src/acl/HttpHeaderData.cc
+++ src/acl/HttpHeaderData.cc
@@ -80,7 +80,7 @@ ACLHTTPHeaderData::parse()
         hdrId = Http::HeaderLookupTable.lookup(hdrName).id;
     } else if (hdrName.caseCmp(t) != 0) {
         debugs(28, DBG_CRITICAL, "ERROR: " << cfg_filename << " line " << config_lineno << ": " << config_input_line);
-        debugs(28, DBG_CRITICAL, "ERROR: ACL cannot match both " << hdrName << " and " << t << " headers. Use 'anyof' ACL instead.");
+        debugs(28, DBG_CRITICAL, "ERROR: ACL cannot match both " << hdrName << " and " << t << " headers. Use 'any-of' ACL instead.");
         return;
     }
 
