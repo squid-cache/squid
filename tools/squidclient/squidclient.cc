7d1a94cdaec6ba33223c79dfc4baabd785a78560
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2023-01-31 18:27:37 +0300

    squidclient: Drop cache_object protocol support
    Also fixed a bug: Proxy-Authorization password was mistakenly sent in
    the '@' URL part, ending up with Squid parsing two different passwords,
    one from the '@' URL part * another from the Authorization header.
    We must use only Authorization header for credentials in cache
    manager requests.

--- tools/squidclient/squidclient.cc
+++ tools/squidclient/squidclient.cc
@@ -425,17 +425,15 @@ main(int argc, char *argv[])
     }
 #endif
     /* Build the HTTP request */
+    const char *pathPassword = nullptr;
     if (strncmp(url, "mgr:", 4) == 0) {
         char *t = xstrdup(url + 4);
-        const char *at = nullptr;
-        if (!strrchr(t, '@')) { // ignore any -w password if @ is explicit already.
-            at = ProxyAuthorization.password;
+        // XXX: Bail on snprintf() failures
+        snprintf(url, sizeof(url), "http://%s:%hu/squid-internal-mgr/%s", Transport::Config.hostname, Transport::Config.port, t);
+        if (const auto at = strrchr(url, '@')) {
+            *at = 0; // send password in Authorization header, not URL
+            pathPassword = at + 1; // the now-removed embedded @password overwrites OriginAuthorization.password further below
         }
-        // embed the -w proxy password into old-style cachemgr URLs
-        if (at)
-            snprintf(url, sizeof(url), "cache_object://%s/%s@%s", Transport::Config.hostname, t, at);
-        else
-            snprintf(url, sizeof(url), "cache_object://%s/%s", Transport::Config.hostname, t);
         xfree(t);
     }
     if (put_file) {
@@ -510,10 +508,22 @@ main(int argc, char *argv[])
         if (max_forwards > -1) {
             msg << "Max-Forwards: " << max_forwards << "\r\n";
         }
-        if (ProxyAuthorization.user)
-            ProxyAuthorization.commit(msg);
-        if (OriginAuthorization.user)
-            OriginAuthorization.commit(msg);
+        if (ProxyAuthorization.user) {
+            ProxyAuthorization.commit(msg); // handles missing passwords
+        } else if (ProxyAuthorization.password) {
+            std::cerr << "ERROR: Proxy authentication username is missing\n";
+            exit(EXIT_FAILURE);
+        }
+        if (OriginAuthorization.user) {
+            const auto savedPassword = OriginAuthorization.password;
+            if (pathPassword)
+                OriginAuthorization.password = pathPassword;
+            OriginAuthorization.commit(msg); // handles missing passwords
+            OriginAuthorization.password = savedPassword; // restore the global password setting
+        } else if (pathPassword || OriginAuthorization.password) {
+            std::cerr << "ERROR: WWW authentication username is missing\n";
+            exit(EXIT_FAILURE);
+        }
 #if HAVE_GSSAPI
         if (www_neg) {
             if (host) {
