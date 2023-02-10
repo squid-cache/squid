74db0b8e43072441cc46443ad38bd85f72790ae0
Alex Rousskov <rousskov@measurement-factory.com>
2023-02-10 13:55:46 -0500

    fixup: Use one DebugMessagesConfig for all translation units
    Global static variables have internal linkage. Thus, different
    translation units had different DebugMessagesConfig variables
    (and, hence, ignored configured settings).

--- src/debug/Messages.h
+++ src/debug/Messages.h
@@ -73,7 +73,7 @@ public:
 };
 
 /// Global configuration for DebugMessageLevel() (where/when supported).
-inline static DebugMessages *DebugMessagesConfig = nullptr;
+inline DebugMessages *DebugMessagesConfig = nullptr;
 
 // Using a template allows us to check message ID range at compile time.
 /// \returns configured debugging level for the given message or defaultLevel
