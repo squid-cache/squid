ee9d59e1e9397907890392ea57c5919d498dcb1e
Francesco Chemolli <kinkie@squid-cache.org>
2023-02-23 15:08:27 +0100

    rename testEnumIterator to TestEnumIterator

--- src/tests/testEnumIterator.h
+++ src/tests/testEnumIterator.h
@@ -6,16 +6,16 @@
  * Please see the COPYING and CONTRIBUTORS files for details.
  */
 
-#ifndef SQUID_TESTENUMITERATOR_H_
-#define SQUID_TESTENUMITERATOR_H_
+#ifndef SQUID_TestEnumIterator_H_
+#define SQUID_TestEnumIterator_H_
 
 #include "base/EnumIterator.h"
 
 #include "compat/cppunit.h"
 
-class testEnumIterator : public CPPUNIT_NS::TestFixture
+class TestEnumIterator : public CPPUNIT_NS::TestFixture
 {
-    CPPUNIT_TEST_SUITE( testEnumIterator );
+    CPPUNIT_TEST_SUITE( TestEnumIterator );
     CPPUNIT_TEST( testForwardIter );
     CPPUNIT_TEST( testReverseIter );
     CPPUNIT_TEST( testBidirectionalIter );
@@ -33,5 +33,5 @@ protected:
     void testUnsignedEnum();
 };
 
-#endif /* SQUID_TESTENUMITERATOR_H_ */
+#endif /* SQUID_TestEnumIterator_H_ */
 
