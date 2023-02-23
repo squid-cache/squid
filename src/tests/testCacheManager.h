66bf82e8fd743ebf6391ffb6e80afda28bbd9a2e
Francesco Chemolli <kinkie@squid-cache.org>
2023-02-23 15:04:57 +0100

    rename testCacheManager to TestCacheManager

--- src/tests/testCacheManager.h
+++ src/tests/testCacheManager.h
@@ -15,9 +15,9 @@
  * test the CacheManager implementation
  */
 
-class testCacheManager : public CPPUNIT_NS::TestFixture
+class TestCacheManager : public CPPUNIT_NS::TestFixture
 {
-    CPPUNIT_TEST_SUITE( testCacheManager );
+    CPPUNIT_TEST_SUITE( TestCacheManager );
     CPPUNIT_TEST( testCreate );
     CPPUNIT_TEST( testRegister );
     CPPUNIT_TEST( testParseUrl );
