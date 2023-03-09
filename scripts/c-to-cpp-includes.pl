ec1b9d126f556d7e55111d424cadb7f4e8c30540
Francesco Chemolli <kinkie@squid-cache.org>
2023-03-09 11:55:30 +0000

    Script initial version

---
+++ scripts/c-to-cpp-includes.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+# sweep over the squid repository, looking for patterns of
+# c standard library header files that have a c++ equivalent,
+# replacing these includes with the sequivalent standard c++ header.
+
+
+#header files to consider (without the .h extension)
+@headers=qw(
+assert
+ctype
+errno
+inttypes
+limits
+math
+signal
+stdarg
+stddef
+stdint
+stdio
+stdlib
+string
+time
+unistd
+);
+
+foreach $header (@headers) {
+    $headerguard="HAVE_".uc($header)."_H";
+    print "headerguard: $headerguard\n";
+    @files=`git grep -lF $headerguard -- *.cc *.h *.cci| grep -v -e ^compat`;
+    chomp(@files);
+    foreach $file (@files) { 
+        print "file: $file\n";
+        $pre="";
+        open(FILE,"<", $file);
+        read(FILE, $pre ,1000000) || die("read error on file $file: $!");
+        close(FILE) || die("close error on file $file: $!");
+        $post = $pre;
+        $post =~ s/#if(def)? $headerguard\n#include <$header.h>(.*)\n#endif/#include <c$header>$2/m;
+        if ($pre eq $post) {
+            print "$header: $file no differ\n";
+        }
+        # now write the output
+        open(FILE, ">", $file);
+        print FILE "$post" || die("write error on file $file: $!");;
+        close(FILE);
+    }
+}
