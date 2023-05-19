76428ae90f4532eb1343bde1d077c4599180870d
Alex Rousskov <rousskov@measurement-factory.com>
2023-05-19 13:33:31 -0400

    fixup: Formatted new sources
    (cherry picked from commit 9dc0cd6dfa74115fe6dfb686238992626f336999)

--- src/error/Details.h
+++ src/error/Details.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (C) 1996-2022 The Squid Software Foundation and contributors
+ * Copyright (C) 1996-2023 The Squid Software Foundation and contributors
  *
  * Squid software is distributed under GPLv2+ license and includes
  * contributions from numerous individuals and organizations.
  * Please see the COPYING and CONTRIBUTORS files for detailss.
  */
 
-#ifndef _SQUID_SRC_ERROR_DETAILS_H
-#define _SQUID_SRC_ERROR_DETAILS_H
+#ifndef SQUID_SRC_ERROR_DETAILS_H
+#define SQUID_SRC_ERROR_DETAILS_H
 
 #include "error/Detail.h"
 
@@ -43,5 +43,5 @@ private:
     std::vector<ErrorDetailPointer> details;
 };
 
-#endif /* _SQUID_SRC_ERROR_DETAILS_H */
+#endif /* SQUID_SRC_ERROR_DETAILS_H */
 
