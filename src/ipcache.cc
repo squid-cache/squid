c833e530b0bd1172b4d745a7cac5bc0da4ae6c60
Alex Rousskov <rousskov@measurement-factory.com>
2023-06-14 12:21:53 -0400

    Fix calculation of cached IP address TTLs
    Since 2017 commit fd9c47d, Squid was effectively ignoring DNS RR TTLs
    that exceeded negative_dns_ttl (set to 60 seconds by default) because
    the "find smallest TTL across DNS RRs seen so far" code in
    ipcache_entry::updateTtl() mistook the default `expires` value as the
    one derived from an earlier seen record. In most cases, this bug
    decreased IP cache hit ratio.
    
    TODO: This fix silently assumes that updateTtl() is always called when
    the first IP record is added to the entry, forcing a TTL reset. We
    should implement a more reliable, less assuming solution instead,
    probably by removing default `expires` values completely and explicitly
    resetting `expires` at the _start_ of every entry update (if any).

--- src/ipcache.cc
+++ src/ipcache.cc
@@ -543,8 +543,13 @@ ipcache_entry::updateTtl(const unsigned int rrTtl)
                                 Config.positiveDnsTtl); // largest value allowed
 
     const time_t rrExpires = squid_curtime + ttl;
-    if (rrExpires < expires)
+    const auto firstRr = addrs.size() <= 1;
+    if (firstRr || rrExpires < expires) {
+        debugs(14, 5, "use " << ttl << " from RR TTL " << rrTtl << "; was: " << (expires - squid_curtime));
         expires = rrExpires;
+    } else {
+        debugs(14, 7, "keep old " << (expires - squid_curtime) << " < " << ttl);
+    }
 }
 
 /// \ingroup IPCacheInternal
