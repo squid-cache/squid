2aff012f82dc1c7fb65168c0eceb360dde16a21e
Alex Rousskov <rousskov@measurement-factory.com>
2023-07-04 11:32:18 -0400

    Do not leak memory when handling cache manager requests
    Also adjusted Cache-Control APIs to prevent similar bugs. These changes
    also speed up processing a bit and simplify most of the affected code.
    The now-gone "just remove the old CC" putCc() misfeature was unused.
    
    The leak was introduced by commit 92a5adb: PutCommonResponseHeaders()
    incorrectly assumed that putCc(pointerToX) takes ownership of X.
    
    Detected by Coverity. CID 1534779: Resource leak (RESOURCE_LEAK).

--- src/http/Message.cc
+++ src/http/Message.cc
@@ -30,22 +30,11 @@ Http::Message::~Message()
 }
 
 void
-Http::Message::putCc(const HttpHdrCc *otherCc)
+Http::Message::putCc(const HttpHdrCc &otherCc)
 {
-    // get rid of the old CC, if any
-    if (cache_control) {
-        delete cache_control;
-        cache_control = nullptr;
-        if (!otherCc)
-            header.delById(Http::HdrType::CACHE_CONTROL);
-        // else it will be deleted inside putCc() below
-    }
-
-    // add new CC, if any
-    if (otherCc) {
-        cache_control = new HttpHdrCc(*otherCc);
-        header.putCc(cache_control);
-    }
+    delete cache_control;
+    cache_control = new HttpHdrCc(otherCc);
+    header.putCc(*cache_control);
 }
 
 /* find first CRLF */
