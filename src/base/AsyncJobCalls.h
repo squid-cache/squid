fa616669c5173554b1cedc85255ae884a84feef6
Alex Rousskov <rousskov@measurement-factory.com>
2023-07-27 11:49:37 -0400

    fixup: Slightly better debugs() formatting

--- src/base/AsyncJobCalls.h
+++ src/base/AsyncJobCalls.h
@@ -177,16 +177,15 @@ JobDialer<Job>::dial(AsyncCall &call)
         debugs(call.debugSection, 3,
                call.name << " threw exception: " << e.what());
         if (!job) {
-            debugs(call.debugSection, DBG_CRITICAL,
-                   "ERROR: Squid BUG: Job invalidated during " << call.name << " that threw exception: " << e.what());
+            debugs(call.debugSection, DBG_CRITICAL, "ERROR: Squid BUG: Job invalidated during " <<
+                   call.name << " that threw exception: " << e.what());
             return; // See also: Bug 4981, commit e3b6f15, and XXX in Http::Stream class description.
         }
         job->callException(e);
     }
 
     if (!job) {
-        debugs(call.debugSection, DBG_CRITICAL,
-               "ERROR: Squid BUG 4981: Job invalidated during " << call.name);
+        debugs(call.debugSection, DBG_CRITICAL, "ERROR: Squid BUG 4981: Job invalidated during " << call.name);
         return;
     }
     job->callEnd(); // may delete job
