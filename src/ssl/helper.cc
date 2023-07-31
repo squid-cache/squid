2e0ae1ca3e19167c505add5cc171d392e89be661
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2023-08-01 02:13:31 +0300

    Simplified with non-template GeneratorRequestKeyHash::operator()

--- src/ssl/helper.cc
+++ src/ssl/helper.cc
@@ -60,9 +60,8 @@ public:
 
 struct GeneratorRequestKeyHash
 {
-    template <class T1, class T2>
-    std::size_t operator() (const std::pair<T1, T2> &pair) const {
-        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    std::size_t operator() (const GeneratorRequest::Key &pair) const {
+        return std::hash()(pair.first) ^ std::hash()(pair.second);
     }
 };
 
