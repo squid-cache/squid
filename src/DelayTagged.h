2f08c4d1d2a3eabf1656c092fcf0d9d330e271a6
Francesco Chemolli <kinkie@squid-cache.org>
2023-08-10 09:04:18 +0000

    DelayTaggedBucket::tag can be const

--- src/DelayTagged.h
+++ src/DelayTagged.h
@@ -30,11 +30,11 @@ class DelayTaggedBucket : public RefCountable
 public:
     typedef RefCount<DelayTaggedBucket> Pointer;
 
-    void stats(StoreEntry *)const;
+    void stats(StoreEntry *) const;
     DelayTaggedBucket(const SBuf &aTag);
     ~DelayTaggedBucket() override;
     DelayBucket theBucket;
-    SBuf tag;
+    const SBuf tag;
 };
 
 /// \ingroup DelayPoolsAPI
