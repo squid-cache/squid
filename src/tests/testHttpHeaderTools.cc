faf2f848f737bf40d3f1e7e651c140924fe40d2c
Francesco Chemolli <kinkie@squid-cache.org>
2023-08-15 17:30:12 +0000

    do testHttpHeaderParseInt

--- src/tests/testHttpHeaderTools.cc
+++ src/tests/testHttpHeaderTools.cc
@@ -18,14 +18,12 @@ class TestHttpHeaderTools: public CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE( TestHttpHeaderTools );
     /* note the statement here and then the actual prototype below */
-    CPPUNIT_TEST( testDemonstration );
     CPPUNIT_TEST( testHttpHeaderParseInt );
     CPPUNIT_TEST_SUITE_END();
 
 public:
 
 protected:
-    void testDemonstration();
     void testHttpHeaderParseInt();
 };
 
@@ -33,25 +31,38 @@ protected:
 CPPUNIT_TEST_SUITE_REGISTRATION( TestHttpHeaderTools );
 
 
-void
-TestHttpHeaderTools::testDemonstration()
-{
-    CPPUNIT_ASSERT_EQUAL(0, 0);
-}
-
 void TestHttpHeaderTools::testHttpHeaderParseInt()
 {
-    static std::map<const char *, int> testCases = {
-        { "0", 0},
-        { "-1", -1},
-        { "1", 1},
-        { "65535", 65535 },
-    };
-    for (const auto& i : testCases ) {
-        int output;
-        int rv = httpHeaderParseInt(i.first, &output);
-        CPPUNIT_ASSERT_EQUAL(1, rv);
-        CPPUNIT_ASSERT_EQUAL(i.second, output);
+    // test success case
+    {
+        static std::map<const char *, int> testCases = {
+            { "0", 0 },
+            { "-1", -1 },
+            { "1", 1 },
+            { "65535", 65535 },
+            { "1h", 1}, // ignore trailing characters
+            { "0x1", 0 }, // doesn't parse hex
+            { " 1", 1 }, // ignore leading space
+        };
+        for (const auto& i : testCases ) {
+            int output;
+            int rv = httpHeaderParseInt(i.first, &output);
+            CPPUNIT_ASSERT_EQUAL(1, rv);
+            CPPUNIT_ASSERT_EQUAL(i.second, output);
+        }
+    }
+    // test fail case
+    {
+        static std::map<const char *, int> testCases = {
+            { "v", 0 },
+            { "h1", 0 },
+        };
+        for (const auto &i : testCases) {
+            int output;
+            int rv = httpHeaderParseInt(i.first, &output);
+            CPPUNIT_ASSERT_EQUAL(0, rv);
+            CPPUNIT_ASSERT_EQUAL(i.second, output);
+        }
     }
 }
 
