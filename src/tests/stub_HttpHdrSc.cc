90cd7c13cd153549d2e648ee2a0a46f1a3000c0c
Francesco Chemolli <kinkie@squid-cache.org>
2023-08-15 09:30:46 +0000

    rebuild stub_HttpHdrSc.cc

---
+++ src/tests/stub_HttpHdrSc.cc
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 1996-2023 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+#include "HttpHdrSc.h"
+
+#define STUB_API "stub_HttpHdrSc.cc"
+#include "tests/STUB.h"
+
+void httpHdrScInitModule(void) STUB_NOP
+HdrSc* httpHdrScParseCreate(const String & ) STUB_RETVAL(nullptr)
+bool HttpHdrSc::parse(const String * ) STUB_RETVAL(false)
+void HttpHdrScTarget::packInto(Packable * ) const STUB
+void HttpHdrSc::packInto(Packable *) const STUB
+void HttpHdrSc::setMaxAge(char const *, int) STUB
+void HttpHdrSc::updateStats(StatHist *) const STUB
+void httpHdrScTargetStatDumper(StoreEntry *, int, double, double, int) STUB
+void httpHdrScStatDumper(StoreEntry *, int, double, double, int) STUB
+HttpHdrScTarget * HttpHdrSc::findTarget(const char *) STUB_RETVAL(nullptr)
+HttpHdrScTarget * HttpHdrSc::getMergedTarget(const char *) STUB_RETVAL(nullptr)
