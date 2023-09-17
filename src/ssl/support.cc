77e05cd40543c745c53b3e3aa8758f88dbe34bbc
Alex Rousskov <rousskov@measurement-factory.com>
2023-09-17 19:00:29 -0400

    fixup: Fixed typo detected by CI

--- src/ssl/support.cc
+++ src/ssl/support.cc
@@ -340,8 +340,8 @@ ssl_verify_cb(int ok, X509_STORE_CTX * ctx)
         } else // remember another error number
             errs->push_back_unique(Security::CertError(error_no, broken_cert, depth));
 
-        if (const auto descritpion = Ssl::GetErrorDescr(error_no))
-            debugs(83, 5, *descritpion << ": " << *peer_cert);
+        if (const auto description = Ssl::GetErrorDescr(error_no))
+            debugs(83, 5, *description << ": " << *peer_cert);
         else
             debugs(83, DBG_IMPORTANT, "ERROR: SSL unknown certificate error " << error_no << " in " << *peer_cert);
 
