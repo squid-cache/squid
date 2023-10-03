squid-cache/squid

commit 2e24d0bfa7c8329f56607b0c7d041a61542d344a

Do not use invasive lists to store CachePeers (#1424)
Using invasive lists for CachePeer objects gives no advantages, but
requires maintaining non-standard error-prone code that leads to
excessive locking and associated memory overheads.

Also fixed a neighbors_init() bug that resulted in accessing an already
deleted "looks like this host" CachePeer object.

src/neighbors.cc
@@ -16,6 +16,7 @@
 #include "base/PackableStream.h"
 #include "CacheDigest.h"
 #include "CachePeer.h"
+#include "CachePeers.h"
 #include "comm/Connection.h"
 #include "comm/ConnOpener.h"
 #include "debug/Messages.h"
@@ -52,7 +53,6 @@
 
 bool peerAllowedToUse(const CachePeer *, PeerSelector *);
 static int peerWouldBePinged(const CachePeer *, PeerSelector *);
-static void neighborRemove(CachePeer *);
 static void neighborAlive(CachePeer *, const MemObject *, const icp_common_t *);
 #if USE_HTCP
 static void neighborAliveHtcp(CachePeer *, const MemObject *, const HtcpReplyData *);
@@ -71,12 +71,11 @@ static IRCB peerCountHandleIcpReply;
 
 static void neighborIgnoreNonPeer(const Ip::Address &, icp_opcode);
 static OBJH neighborDumpPeers;
-static void dump_peers(StoreEntry * sentry, CachePeer * peers);
+static void dump_peers(StoreEntry *, CachePeers *);
 
 static unsigned short echo_port;
 
 static int NLateReplies = 0;
-static CachePeer *first_ping = nullptr;
 
 const char *
 neighborTypeStr(const CachePeer * p)
@@ -98,13 +97,12 @@ whichPeer(const Ip::Address &from)
 {
     int j;
 
-    CachePeer *p = nullptr;
     debugs(15, 3, "whichPeer: from " << from);
 
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &p: CurrentCachePeers()) {
         for (j = 0; j < p->n_addresses; ++j) {
             if (from == p->addresses[j] && from.port() == p->icp.port) {
-                return p;
+                return p.get();
             }
         }
     }
@@ -273,11 +271,10 @@ peerHTTPOkay(const CachePeer * p, PeerSelector * ps)
 int
 neighborsCount(PeerSelector *ps)
 {
-    CachePeer *p = nullptr;
     int count = 0;
 
-    for (p = Config.peers; p; p = p->next)
-        if (peerWouldBePinged(p, ps))
+    for (const auto &p: CurrentCachePeers())
+        if (peerWouldBePinged(p.get(), ps))
             ++count;
 
     debugs(15, 3, "neighborsCount: " << count);
@@ -293,7 +290,9 @@ getFirstUpParent(PeerSelector *ps)
 
     CachePeer *p = nullptr;
 
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &peer: CurrentCachePeers()) {
+        p = peer.get();
+
         if (!neighborUp(p))
             continue;
 
@@ -316,10 +315,10 @@ getRoundRobinParent(PeerSelector *ps)
     assert(ps);
     HttpRequest *request = ps->request;
 
-    CachePeer *p;
     CachePeer *q = nullptr;
 
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &peer: CurrentCachePeers()) {
+        const auto p = peer.get();
         if (!p->options.roundrobin)
             continue;
 
@@ -358,11 +357,12 @@ getWeightedRoundRobinParent(PeerSelector *ps)
     assert(ps);
     HttpRequest *request = ps->request;
 
-    CachePeer *p;
     CachePeer *q = nullptr;
     int weighted_rtt;
 
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &peer: CurrentCachePeers()) {
+        const auto p = peer.get();
+
         if (!p->options.weighted_roundrobin)
             continue;
 
@@ -379,11 +379,11 @@ getWeightedRoundRobinParent(PeerSelector *ps)
     }
 
     if (q && q->rr_count > 1000000)
-        for (p = Config.peers; p; p = p->next) {
+        for (const auto &p: CurrentCachePeers()) {
             if (!p->options.weighted_roundrobin)
                 continue;
 
-            if (neighborType(p, request->url) != PEER_PARENT)
+            if (neighborType(p.get(), request->url) != PEER_PARENT)
                 continue;
 
             p->rr_count = 0;
@@ -447,10 +447,8 @@ peerClearRRStart(void)
 void
 peerClearRR()
 {
-    CachePeer *p = nullptr;
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &p: CurrentCachePeers())
         p->rr_count = 1;
-    }
 }
 
 void
@@ -478,9 +476,9 @@ getDefaultParent(PeerSelector *ps)
     assert(ps);
     HttpRequest *request = ps->request;
 
-    CachePeer *p = nullptr;
+    for (const auto &peer: CurrentCachePeers()) {
+        const auto p = peer.get();
 
-    for (p = Config.peers; p; p = p->next) {
         if (neighborType(p, request->url) != PEER_PARENT)
             continue;
 
@@ -500,45 +498,6 @@ getDefaultParent(PeerSelector *ps)
     return nullptr;
 }
 
-CachePeer *
-getNextPeer(CachePeer * p)
-{
-    return p->next;
-}
-
-CachePeer *
-getFirstPeer(void)
-{
-    return Config.peers;
-}
-
-static void
-neighborRemove(CachePeer * target)
-{
-    CachePeer *p = nullptr;
-    CachePeer **P = nullptr;
-    p = Config.peers;
-    P = &Config.peers;
-
-    while (p) {
-        if (target == p)
-            break;
-
-        P = &p->next;
-
-        p = p->next;
-    }
-
-    if (p) {
-        *P = p->next;
-        p->next = nullptr;
-        delete p;
-        --Config.npeers;
-    }
-
-    first_ping = Config.peers;
-}
-
 static void
 neighborsRegisterWithCacheManager()
 {
@@ -552,16 +511,13 @@ neighbors_init(void)
 {
     struct servent *sep = nullptr;
     const char *me = getMyHostname();
-    CachePeer *thisPeer = nullptr;
-    CachePeer *next = nullptr;
 
     neighborsRegisterWithCacheManager();
 
     if (Comm::IsConnOpen(icpIncomingConn)) {
+        RawCachePeers peersToRemove;
 
-        for (thisPeer = Config.peers; thisPeer; thisPeer = next) {
-            next = thisPeer->next;
-
+        for (const auto &thisPeer: CurrentCachePeers()) {
             if (0 != strcmp(thisPeer->host, me))
                 continue;
 
@@ -572,8 +528,15 @@ neighbors_init(void)
                 debugs(15, DBG_IMPORTANT, "WARNING: Peer looks like this host." <<
                        Debug::Extra << "Ignoring cache_peer " << *thisPeer);
 
-                neighborRemove(thisPeer);
+                peersToRemove.push_back(thisPeer.get());
+                break; // avoid warning about (and removing) the same CachePeer twice
             }
+        }
+
+        while (peersToRemove.size()) {
+            const auto p = peersToRemove.back();
+            peersToRemove.pop_back();
+            DeleteConfigured(p);
         }
     }
 
@@ -581,8 +544,6 @@ neighbors_init(void)
 
     sep = getservbyname("echo", "udp");
     echo_port = sep ? ntohs((unsigned short) sep->s_port) : 7;
-
-    first_ping = Config.peers;
 }
 
 int
@@ -595,8 +556,6 @@ neighborsUdpPing(HttpRequest * request,
 {
     const char *url = entry->url();
     MemObject *mem = entry->mem_obj;
-    CachePeer *p = nullptr;
-    int i;
     int reqnum = 0;
     int flags;
     int peers_pinged = 0;
@@ -617,9 +576,8 @@ neighborsUdpPing(HttpRequest * request,
 
     reqnum = icpSetCacheKey((const cache_key *)entry->key);
 
-    for (i = 0, p = first_ping; i++ < Config.npeers; p = p->next) {
-        if (p == nullptr)
-            p = Config.peers;
+    for (size_t i = 0; i < Config.peers->size(); ++i) {
+        const auto p = &Config.peers->nextPeerToPing(i);
 
         debugs(15, 5, "candidate: " << *p);
 
@@ -709,9 +667,6 @@ neighborsUdpPing(HttpRequest * request,
         if ((p->type != PEER_MULTICAST) && (p->stats.probe_start == 0))
             p->stats.probe_start = squid_curtime;
     }
-
-    if ((first_ping = first_ping->next) == nullptr)
-        first_ping = Config.peers;
 
     /*
      * How many replies to expect?
@@ -803,25 +758,17 @@ neighborsDigestSelect(PeerSelector *ps)
     int best_rtt = 0;
     int choice_count = 0;
     int ichoice_count = 0;
-    CachePeer *p;
     int p_rtt;
-    int i;
 
     if (!request->flags.hierarchical)
         return nullptr;
 
     storeKeyPublicByRequest(request);
 
-    for (i = 0, p = first_ping; i++ < Config.npeers; p = p->next) {
-        lookup_t lookup;
+    for (size_t i = 0; i < Config.peers->size(); ++i) {
+        const auto p = &Config.peers->nextPeerToPing(i);
 
-        if (!p)
-            p = Config.peers;
-
-        if (i == 1)
-            first_ping = p;
-
-        lookup = peerDigestLookup(p, ps);
+        const auto lookup = peerDigestLookup(p, ps);
 
         if (lookup == LOOKUP_NONE)
             continue;
@@ -1083,7 +1030,7 @@ neighborsUdpAck(const cache_key * key, icp_common_t * header, const Ip::Address 
             if (100 * p->icp.counts[ICP_DENIED] / p->stats.pings_acked > 95) {
                 debugs(15, DBG_CRITICAL, "Disabling cache_peer " << *p <<
                        " because over 95% of its replies are UDP_DENIED");
-                neighborRemove(p);
+                DeleteConfigured(p);
                 p = nullptr;
             } else {
                 neighborCountIgnored(p);
@@ -1099,14 +1046,11 @@ neighborsUdpAck(const cache_key * key, icp_common_t * header, const Ip::Address 
 CachePeer *
 findCachePeerByName(const char * const name)
 {
-    CachePeer *p = nullptr;
-
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &p: CurrentCachePeers()) {
         if (!strcasecmp(name, p->name))
-            break;
+            return p.get();
     }
-
-    return p;
+    return nullptr;
 }
 
 int
@@ -1210,8 +1154,6 @@ peerDNSConfigure(const ipcache_addrs *ia, const Dns::LookupDetails &, void *data
 static void
 peerRefreshDNS(void *data)
 {
-    CachePeer *p = nullptr;
-
     if (eventFind(peerRefreshDNS, nullptr))
         eventDelete(peerRefreshDNS, nullptr);
 
@@ -1221,8 +1163,8 @@ peerRefreshDNS(void *data)
         return;
     }
 
-    for (p = Config.peers; p; p = p->next)
-        ipcache_nbgethostbyname(p->host, peerDNSConfigure, p);
+    for (const auto &p: CurrentCachePeers())
+        ipcache_nbgethostbyname(p->host, peerDNSConfigure, p.get());
 
     /* Reconfigure the peers every hour */
     eventAddIsh("peerRefreshDNS", peerRefreshDNS, nullptr, 3600.0, 1);
@@ -1553,7 +1495,7 @@ dump_peer_options(StoreEntry * sentry, CachePeer * p)
 }
 
 static void
-dump_peers(StoreEntry * sentry, CachePeer * peers)
+dump_peers(StoreEntry *sentry, CachePeers *peers)
 {
     char ntoabuf[MAX_IPSTRLEN];
     int i;
@@ -1561,7 +1503,8 @@ dump_peers(StoreEntry * sentry, CachePeer * peers)
     if (peers == nullptr)
         storeAppendPrintf(sentry, "There are no neighbors installed.\n");
 
-    for (CachePeer *e = peers; e; e = e->next) {
+    for (const auto &peer: *peers) {
+        const auto e = peer.get();
         assert(e->host != nullptr);
         storeAppendPrintf(sentry, "\n%-11.11s: %s\n",
                           neighborTypeStr(e),
@@ -1722,10 +1665,9 @@ neighborsHtcpReply(const cache_key * key, HtcpReplyData * htcp, const Ip::Addres
 void
 neighborsHtcpClear(StoreEntry * e, HttpRequest * req, const HttpRequestMethod &method, htcp_clr_reason reason)
 {
-    CachePeer *p;
     char buf[128];
 
-    for (p = Config.peers; p; p = p->next) {
+    for (const auto &p: CurrentCachePeers()) {
         if (!p->options.htcp) {
             continue;
         }
@@ -1736,7 +1678,7 @@ neighborsHtcpClear(StoreEntry * e, HttpRequest * req, const HttpRequestMethod &m
             continue;
         }
         debugs(15, 3, "neighborsHtcpClear: sending CLR to " << p->in_addr.toUrl(buf, 128));
-        htcpClear(e, req, method, p, reason);
+        htcpClear(e, req, method, p.get(), reason);
     }
 }
 

src/CachePeer.cc
@@ -47,8 +47,6 @@ CachePeer::~CachePeer()
     xfree(digest_url);
 #endif
 
-    delete next;
-
     xfree(login);
 
     delete standby.pool;

src/CachePeer.h
@@ -48,6 +48,7 @@ public:
     /// \returns the effective connect timeout for the given peer
     time_t connectTimeout() const;
 
+    /// n-th cache_peer directive, starting with 1
     u_int index = 0;
 
     /// cache_peer name (if explicitly configured) or hostname (otherwise).
@@ -179,7 +180,6 @@ public:
     Ip::Address addresses[10];
     int n_addresses = 0;
     int rr_count = 0;
-    CachePeer *next = nullptr;
     int testing_now = 0;
 
     struct {

src/CachePeers.cc
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 1996-2023 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+#include "CachePeers.h"
+#include "SquidConfig.h"
+
+CachePeer &
+CachePeers::nextPeerToPing(const size_t pollIndex)
+{
+    Assure(size());
+
+    // Remember the number of polls to keep shifting each poll starting point,
+    // to avoid always polling the same group of peers before other peers and
+    // risk overloading that first group with requests.
+    if (!pollIndex)
+        ++peerPolls_;
+
+    // subtract 1 to set the very first pos to zero
+    const auto pos = (peerPolls_ - 1 + pollIndex) % size();
+
+    return *storage[pos];
+}
+
+void
+CachePeers::remove(CachePeer * const peer)
+{
+    const auto pos = std::find_if(storage.begin(), storage.end(), [&](const auto &storePeer) {
+        return storePeer.get() == peer;
+    });
+    Assure(pos != storage.end());
+    storage.erase(pos);
+}
+
+const CachePeers &
+CurrentCachePeers()
+{
+    if (Config.peers)
+        return *Config.peers;
+
+    static const CachePeers empty;
+    return empty;
+}
+
+void
+DeleteConfigured(CachePeer * const peer)
+{
+    Assure(Config.peers);
+    Config.peers->remove(peer);
+}
+

src/CachePeers.h
@@ -13,7 +13,50 @@
 #include "CachePeer.h"
 #include "mem/PoolingAllocator.h"
 
+#include <memory>
 #include <vector>
+
+/// cache_peer configuration storage
+class CachePeers
+{
+public:
+    /// owns stored CachePeer objects
+    using Storage = std::vector< std::unique_ptr<CachePeer>, PoolingAllocator< std::unique_ptr<CachePeer> > >;
+
+    /// stores a being-configured cache_peer
+    void add(CachePeer *p) { storage.emplace_back(p); }
+
+    /// deletes a previously add()ed CachePeer object
+    void remove(CachePeer *);
+
+    /// the number of currently stored (i.e. added and not removed) cache_peers
+    auto size() const { return storage.size(); }
+
+    /* peer iterators forming a sequence for C++ range-based for loop API */
+    using const_iterator = Storage::const_iterator;
+    auto begin() const { return storage.cbegin(); }
+    auto end() const { return storage.cend(); }
+
+    /// A CachePeer to query next when scanning all peer caches in hope to fetch
+    /// a remote cache hit. \sa neighborsUdpPing()
+    /// \param iteration a 0-based index of a loop scanning all peers
+    CachePeer &nextPeerToPing(size_t iteration);
+
+private:
+    /// cache_peers in configuration/parsing order
+    Storage storage;
+
+    /// total number of completed peer scans by nextPeerToPing()-calling code
+    uint64_t peerPolls_ = 0;
+};
+
+/// All configured cache_peers that are still available/relevant.
+/// \returns an empty container if no cache_peers were configured or all
+/// configured cache_peers were removed (e.g., by DeleteConfigured()).
+const CachePeers &CurrentCachePeers();
+
+/// destroys the given peer after removing it from the set of configured peers
+void DeleteConfigured(CachePeer *);
 
 /// Weak pointers to zero or more Config.peers.
 /// Users must specify the selection algorithm and the order of entries.

src/Makefile.am
@@ -199,6 +199,7 @@ squid_SOURCES = \
 	CacheManager.h \
 	CachePeer.cc \
 	CachePeer.h \
+	CachePeers.cc \
 	CachePeers.h \
 	ClientInfo.h \
 	ClientRequestContext.h \
@@ -1741,6 +1742,8 @@ tests_test_http_range_SOURCES = \
 	CacheDigest.h \
 	CachePeer.cc \
 	CachePeer.h \
+	CachePeers.cc \
+	CachePeers.h \
 	ClientInfo.h \
 	tests/stub_CollapsedForwarding.cc \
 	ConfigOption.cc \
@@ -2124,6 +2127,8 @@ tests_testHttpRequest_SOURCES = \
 	CacheDigest.h \
 	CachePeer.cc \
 	CachePeer.h \
+	CachePeers.cc \
+	CachePeers.h \
 	ClientInfo.h \
 	tests/stub_CollapsedForwarding.cc \
 	ConfigOption.cc \
@@ -2420,6 +2425,8 @@ tests_testCacheManager_SOURCES = \
 	tests/testCacheManager.cc \
 	CachePeer.cc \
 	CachePeer.h \
+	CachePeers.cc \
+	CachePeers.h \
 	ClientInfo.h \
 	tests/stub_CollapsedForwarding.cc \
 	ConfigOption.cc \

src/PeerPoolMgr.cc
@@ -11,6 +11,7 @@
 #include "base/AsyncCallbacks.h"
 #include "base/RunnersRegistry.h"
 #include "CachePeer.h"
+#include "CachePeers.h"
 #include "comm/Connection.h"
 #include "comm/ConnOpener.h"
 #include "debug/Stream.h"
@@ -244,7 +245,8 @@ DefineRunnerRegistrator(PeerPoolMgrsRr);
 void
 PeerPoolMgrsRr::syncConfig()
 {
-    for (CachePeer *p = Config.peers; p; p = p->next) {
+    for (const auto &peer: CurrentCachePeers()) {
+        const auto p = peer.get();
         // On reconfigure, Squid deletes the old config (and old peers in it),
         // so should always be dealing with a brand new configuration.
         assert(!p->standby.mgr);

src/SquidConfig.h
@@ -42,7 +42,8 @@ namespace Mgr
 {
 class ActionPasswordList;
 } // namespace Mgr
-class CachePeer;
+
+class CachePeers;
 class CustomLog;
 class CpuAffinityMap;
 class DebugMessages;
@@ -242,7 +243,7 @@ public:
     size_t tcpRcvBufsz;
     size_t udpMaxHitObjsz;
     wordlist *mcast_group_list;
-    CachePeer *peers;
+    CachePeers *peers;
     int npeers;
 
     struct {

commit 50e0d0c68a5c200150d20ab8f336fe405c543397

Fix handling of zero cache_peers
The bug was introduced in 2e24d0b.

src/neighbors.cc
@@ -1500,8 +1500,10 @@ dump_peers(StoreEntry *sentry, CachePeers *peers)
     char ntoabuf[MAX_IPSTRLEN];
     int i;
 
-    if (peers == nullptr)
+    if (!peers) {
         storeAppendPrintf(sentry, "There are no neighbors installed.\n");
+        return;
+    }
 
     for (const auto &peer: *peers) {
         const auto e = peer.get();