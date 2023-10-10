336c2b508554dc17f48079dc6eec65113078a10c
Amos Jeffries <amosjeffries@squid-cache.org>
2023-10-10 15:14:29 +1300

    Maintenance: fix outdated stub_errorpage.cc contents

--- src/tests/stub_errorpage.cc
+++ src/tests/stub_errorpage.cc
@@ -7,17 +7,31 @@
  */
 
 #include "squid.h"
-#include "errorpage.h"
+#include "AccessLogEntry.h"
+#include "HttpReply.h"
 
 #define STUB_API "errorpage.cc"
 #include "tests/STUB.h"
 
-err_type errorReservePageId(const char *, const SBuf &) STUB_RETVAL(err_type(0))
+#include "errorpage.h"
+CBDATA_CLASS_INIT(ErrorState);
+ErrorState::ErrorState(err_type, Http::StatusCode, HttpRequest *, const AccessLogEntryPointer &) STUB
+ErrorState::ErrorState(HttpRequest *, HttpReply *, const AccessLogEntryPointer &) STUB
+ErrorState::~ErrorState() STUB
+ErrorState *ErrorState::NewForwarding(err_type, HttpRequestPointer &, const AccessLogEntryPointer &) STUB_RETVAL(nullptr)
+HttpReply *ErrorState::BuildHttpReply(void) STUB_RETVAL(nullptr)
+void ErrorState::validate() STUB
+void errorInitialize(void) STUB
+void errorClean(void) STUB
+void errorSend(const Comm::ConnectionPointer &, ErrorState *) STUB
 void errorAppendEntry(StoreEntry *, ErrorState * ) STUB
-bool strHdrAcptLangGetItem(const String &, char *, int, size_t &) STUB_RETVAL(false)
-void TemplateFile::loadDefault() STUB
+err_type errorReservePageId(const char *, const SBuf &) STUB_RETVAL(err_type(0))
+const char *errorPageName(int) STUB_RETVAL(nullptr)
 TemplateFile::TemplateFile(char const*, err_type) STUB
+void TemplateFile::loadDefault() STUB
 bool TemplateFile::loadFor(const HttpRequest *) STUB_RETVAL(false)
-
+bool TemplateFile::loadFromFile(const char *) STUB_RETVAL(false)
+bool TemplateFile::tryLoadTemplate(const char *) STUB_RETVAL(false)
+bool strHdrAcptLangGetItem(const String &, char *, int, size_t &) STUB_RETVAL(false)
 std::ostream &operator <<(std::ostream &os, const ErrorState *) STUB_RETVAL(os)
 
