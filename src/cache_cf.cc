874f609c2ef40a438c8eaf2f3c3a9818264e279e
Alex Rousskov <rousskov@measurement-factory.com>
2023-10-25 11:47:19 +0000

    Improve handling of expanding HTTP header values (#1536)
    Squid manipulations often increase HTTP header value length compared to
    the corresponding raw value received by Squid. Raw header length is
    checked against request_header_max_size and reply_header_max_size that
    default to 64KB, making the raw value safe to store in a String object
    (by default). However, when the increased length of a manipulated value
    exceeds String class limits, Squid leaks memory, asserts, or possibly
    stalls affected transactions. The long-term fix for this problem is a
    complete String elimination from Squid sources, but that takes time.
    
    Known manipulations may effectively concatenate headers and/or increase
    header value length by 50%. This workaround makes such known increases
    safe by essentially tripling String class limits:
    
        (64KB + 64KB) * 150% = 3 * 64KB
    
    This bug was discovered and detailed by Joshua Rogers at
    https://megamansec.github.io/Squid-Security-Audit/response-memleaks.html
    where it was filed as "Memory Leak in HTTP Response Parsing".

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -950,6 +950,18 @@ configDoConfigure(void)
                (uint32_t)Config.maxRequestBufferSize, (uint32_t)Config.maxRequestHeaderSize);
     }
 
+    // Warn about the dangers of exceeding String limits when manipulating HTTP
+    // headers. Technically, we do not concatenate _requests_, so we could relax
+    // their check, but we keep the two checks the same for simplicity sake.
+    const auto safeRawHeaderValueSizeMax = (String::SizeMaxXXX()+1)/3;
+    // TODO: static_assert(safeRawHeaderValueSizeMax >= 64*1024); // no WARNINGs for default settings
+    if (Config.maxRequestHeaderSize > safeRawHeaderValueSizeMax)
+        debugs(3, DBG_CRITICAL, "WARNING: Increasing request_header_max_size beyond " << safeRawHeaderValueSizeMax <<
+               " bytes makes Squid more vulnerable to denial-of-service attacks; configured value: " << Config.maxRequestHeaderSize << " bytes");
+    if (Config.maxReplyHeaderSize > safeRawHeaderValueSizeMax)
+        debugs(3, DBG_CRITICAL, "WARNING: Increasing reply_header_max_size beyond " << safeRawHeaderValueSizeMax <<
+               " bytes makes Squid more vulnerable to denial-of-service attacks; configured value: " << Config.maxReplyHeaderSize << " bytes");
+
     /*
      * Disable client side request pipelining if client_persistent_connections OFF.
      * Waste of resources queueing any pipelined requests when the first will close the connection.
