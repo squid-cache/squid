709274c6e9d513696fcc165cd695e17184155c14
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2023-11-02 23:24:56 +0000

    Use SBuf instead of SquidString in MemObject (#1558)

--- src/MemObject.h
+++ src/MemObject.h
@@ -14,7 +14,7 @@
 #include "http/RequestMethod.h"
 #include "HttpReply.h"
 #include "RemovalPolicy.h"
-#include "SquidString.h"
+#include "sbuf/SBuf.h"
 #include "stmem.h"
 #include "store/forward.h"
 #include "StoreIOBuffer.h"
@@ -234,8 +234,8 @@ private:
     HttpReplyPointer reply_; ///< \see baseReply()
     HttpReplyPointer updatedReply_; ///< \see updatedReply()
 
-    mutable String storeId_; ///< StoreId for our entry (usually request URI)
-    mutable String logUri_;  ///< URI used for logging (usually request URI)
+    mutable SBuf storeId_; ///< StoreId for our entry (usually request URI)
+    mutable SBuf logUri_;  ///< URI used for logging (usually request URI)
 
     DelayedAsyncCalls deferredReads;
 };
