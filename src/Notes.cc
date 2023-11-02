b6a1d69aed6524100bc7c454a82ae951a14eeb1f
Francesco Chemolli <kinkie@squid-cache.org>
2023-11-02 22:18:05 +0000

    clean TextException use in Note::Value::Value

--- src/Notes.cc
+++ src/Notes.cc
@@ -41,9 +41,7 @@ Note::Value::Value(const char *aVal, const bool quoted, const char *descr, const
         valueFormat = new Format::Format(descr ? descr : "Notes");
         if (!valueFormat->parse(theValue.c_str())) {
             delete valueFormat;
-            SBuf exceptionMsg;
-            exceptionMsg.Printf("failed to parse annotation value ").append(theValue);
-            throw TexcHere(exceptionMsg.c_str());
+            throw TextException(ToSBuf("failed to parse annotation value "), theValue), Here());
         }
     }
 }
