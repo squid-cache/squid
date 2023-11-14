ed4fde431d4c54c44f72763a2dda0f2b86f2e362
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2023-11-14 21:52:38 +0300

    Removed trailing whitespaces

--- src/parser/ToInteger.h
+++ src/parser/ToInteger.h
@@ -25,7 +25,7 @@ using ParsedInteger = int64_t;
 /// parses the entire rawInput as a decimal integer value fitting the [min..max] range
 ParsedInteger DecimalInteger_(const char *description, const SBuf &rawInput, ParsedInteger min, ParsedInteger max);
 
-    
+
 template <typename Integer>
 Integer
 DecimalInteger_(const char *description, const SBuf &rawInput)
@@ -33,8 +33,8 @@ DecimalInteger_(const char *description, const SBuf &rawInput)
     const auto lowerLimit = std::numeric_limits<Integer>::min();
     const auto upperLimit = std::numeric_limits<Integer>::max();
 
-    const auto parsedMin = std::numeric_limits<ParsedInteger>::min(); 
-    const auto parsedMax = std::numeric_limits<ParsedInteger>::max(); 
+    const auto parsedMin = std::numeric_limits<ParsedInteger>::min();
+    const auto parsedMax = std::numeric_limits<ParsedInteger>::max();
 
     static_assert(lowerLimit >= parsedMin);
     static_assert(upperLimit <= parsedMax);
@@ -47,7 +47,7 @@ DecimalInteger_(const char *description, const SBuf &rawInput)
 /// parses a decimal integer that fits the specified Integer type
 template <typename Integer>
 Integer
-SignedDecimalInteger(const char *description, const SBuf &rawInput) 
+SignedDecimalInteger(const char *description, const SBuf &rawInput)
 {
     return Impl::DecimalInteger_<Integer>(description, rawInput);
 }
