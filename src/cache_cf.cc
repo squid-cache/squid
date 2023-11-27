1516c88cd3bceec32f7800d6a243823c7bac627e
Alex Rousskov <rousskov@measurement-factory.com>
2023-11-27 11:31:52 -0500

    fixup: Avoid deprecated self_destruct()

--- src/cache_cf.cc
+++ src/cache_cf.cc
@@ -1119,11 +1119,8 @@ parse_obsolete(const char *name)
 
         // add the value as unquoted-string because the old values did not support whitespace
         const char *token = ConfigParser::NextQuotedOrToEol();
-
-        if (!token) {
-            debugs(3, DBG_CRITICAL, "ERROR: cannot parse value for directive: " << name);
-            self_destruct();
-        }
+        if (!token)
+            throw TextException(ToSBuf("missing required parameter for obsolete directive: ", name), Here());
 
         tmp.append(token, strlen(token));
         Security::ProxyOutgoingConfig.parse(tmp.c_str());
