03589dc8492b258e5a84b4aeb47583872a38d1a7
Thomas Leroy <32497783+p4zuu@users.noreply.github.com>
2023-11-28 07:35:46 +0000

    Limit the number of allowed X-Forwarded-For hops (#1589)
    Squid will ignore all X-Forwarded-For elements listed after the first 64
    addresses allowed by the follow_x_forwarded_for directive. A different
    limit can be specified by defining a C++ SQUID_X_FORWARDED_FOR_HOP_MAX
    macro, but that macro is not a supported Squid configuration interface
    and may change or disappear at any time.
    
    Squid will log a cache.log ERROR if the hop limit has been reached.
    
    This change works around problematic ACLChecklist and/or slow ACLs
    implementation that results in immediate nonBlockingCheck() callbacks.
    Such callbacks have caused many bugs and development complications. In
    clientFollowXForwardedForCheck() context, they lead to indirect
    recursion that was bound only by the number of allowed XFF entries,
    which could reach thousands and exhaust Squid process call stack.
    
    This recursion bug was discovered and detailed by Joshua Rogers at
    https://megamansec.github.io/Squid-Security-Audit/xff-stackoverflow.html
    where it was filed as "X-Forwarded-For Stack Overflow".

--- src/ClientRequestContext.h
+++ src/ClientRequestContext.h
@@ -78,8 +78,13 @@ public:
 #if USE_OPENSSL
     bool sslBumpCheckDone = false;
 #endif
-    ErrorState *error = nullptr; ///< saved error page for centralized/delayed processing
+
     bool readNextRequest = false; ///< whether Squid should read after error handling
+    ErrorState *error = nullptr; ///< saved error page for centralized/delayed processing
+
+#if FOLLOW_X_FORWARDED_FOR
+    size_t currentXffHopNumber = 0; ///< number of X-Forwarded-For header values processed so far
+#endif
 };
 
 #endif /* SQUID_CLIENTREQUESTCONTEXT_H */
