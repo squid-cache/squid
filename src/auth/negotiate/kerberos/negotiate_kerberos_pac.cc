20ed2aa99669168dfaa316aa9c548d5d2c5e052a
ankor2023 <138755079+ankor2023@users.noreply.github.com>
2023-12-08 16:41:41 +0300

    Some additional checks and formatting fixes

--- src/auth/negotiate/kerberos/negotiate_kerberos_pac.cc
+++ src/auth/negotiate/kerberos/negotiate_kerberos_pac.cc
@@ -42,6 +42,9 @@
 
 #if HAVE_GSSAPI && HAVE_PAC_SUPPORT
 
+#define LOGON_EXTRA_SIDS 0x0020
+#define LOGON_RESOURCE_GROUPS 0x0200
+
 static int bpos;
 static krb5_data *ad_data;
 static unsigned char *p;
@@ -366,7 +369,7 @@ static char *
 get_resource_group_domain_sid(uint32_t ResourceGroupDomainSid)
 {
 
-    if (ResourceGroupDomainSid!= 0) {
+    if (ResourceGroupDomainSid != 0) {
         uint8_t rev;
         uint64_t idauth;
         char dli[256];
@@ -387,7 +390,7 @@ get_resource_group_domain_sid(uint32_t ResourceGroupDomainSid)
         uint32_t nauth = get4byt();
 
         // check if nauth math will produce invalid length values on 32-bit
-        static uint32_t maxGidCount = (UINT32_MAX-4-1-1-6)/4;
+        static uint32_t maxGidCount = (UINT32_MAX - 4 - 1 - 1 - 6)/4;
         if (nauth > maxGidCount) {
             debug((char *) "%s| %s: ERROR: Too many subAuths in the ResourceGroupDomainSID: nauth = %d > %d\n",
                   LogTime(), PROGRAM, nauth, maxGidCount);
@@ -395,9 +398,9 @@ get_resource_group_domain_sid(uint32_t ResourceGroupDomainSid)
         }
 
         // length = revision[1byte]+nsub[1byte]+idauth[6bytes]+nauth*sauth[4bytes]
-        size_t length = 1+1+6+nauth*4;
+        size_t length = 1 + 1 + 6 + nauth*4;
 
-        sid=(char *)xcalloc((length+sizeof(uint32_t))*sizeof(char), 1); // +sizeof(uint32_t) for storing length of a SID
+        sid=(char *)xcalloc((length + sizeof(uint32_t))*sizeof(char), 1); // +sizeof(uint32_t) for storing length of a SID
         // 4 bytes SID length
         // 1 byte revision
         // 1 byte nsub
@@ -406,7 +409,7 @@ get_resource_group_domain_sid(uint32_t ResourceGroupDomainSid)
         memcpy((void *)&sid[4], (const void*)&p[bpos], 1);
         memcpy((void *)&sid[5], (const void*)&p[bpos+1], 1);
         sid[5] = sid[5]+1;  // ++ as it will be used in a rid concatenation
-        memcpy((void *)&sid[6], (const void*)&p[bpos+2], 6+nauth*4);
+        memcpy((void *)&sid[6], (const void*)&p[bpos+2], 6 + nauth*4);
 
         /* mainly for debug only */
         rev = get1byt();
@@ -414,12 +417,12 @@ get_resource_group_domain_sid(uint32_t ResourceGroupDomainSid)
         idauth = get6byt_be();
 
         int rv = snprintf(dli, sizeof(dli), "S-%d-%lu", rev, (long unsigned int)idauth);
-        assert(rv>0);
-        for ( l=0; l<(int)nauth; l++ ) {
+        assert(rv > 0);
+        for (l=0; l<(int)nauth; l++) {
             uint32_t sauth;
             sauth = get4byt();
-            rv = snprintf((char *)&dli[strlen(dli)], sizeof(dli)-strlen(dli), "-%u",sauth);
-            assert(rv>0);
+            rv = snprintf((char *)&dli[strlen(dli)], sizeof(dli) - strlen(dli), "-%u", sauth);
+            assert(rv > 0);
         }
         debug((char *) "%s| %s: INFO: Got ResourceGroupDomainSid %s\n", LogTime(), PROGRAM, dli);
         return sid;
@@ -448,7 +451,7 @@ get_resource_groups(char *ad_groups, char *resource_group_domain_sid, uint32_t R
 
     resource_group_domain_sid += sizeof(uint32_t); // now it points to the actual data
 
-    if (ResourceGroupIds!= 0) {
+    if (ResourceGroupIds != 0) {
         uint32_t ngroup;
         int l;
 
@@ -462,12 +465,12 @@ get_resource_groups(char *ad_groups, char *resource_group_domain_sid, uint32_t R
         debug((char *) "%s| %s: INFO: Found %d Resource Group rids\n", LogTime(), PROGRAM, ResourceGroupCount);
 
         // make a group template which begins with the ResourceGroupDomainID
-        length = group_domain_sid_len+4;  // +4 for a rid concatenation
+        length = group_domain_sid_len + 4;  // +4 for a rid concatenation
         st = (char *)xcalloc(length*sizeof(char), 1);
 
         memcpy((void *)st, (const void*)resource_group_domain_sid, group_domain_sid_len); // template
 
-        for (l=0; l<(int)ResourceGroupCount; l++) {
+        for (l=0; l < (int)ResourceGroupCount; l++) {
             uint32_t sauth;
             memcpy((void *)&st[group_domain_sid_len], (const void*)&p[bpos], 4);  // rid concatenation
 
@@ -475,8 +478,6 @@ get_resource_groups(char *ad_groups, char *resource_group_domain_sid, uint32_t R
                 if (!pstrcpy(ad_groups, "group=")) {
                    debug((char *) "%s| %s: WARN: Too many groups ! size > %d : %s\n",
                          LogTime(), PROGRAM, MAX_PAC_GROUP_SIZE, ad_groups);
-                   xfree(st);
-                   return nullptr;
                 }
                 first_group = 0;
 
@@ -484,38 +485,32 @@ get_resource_groups(char *ad_groups, char *resource_group_domain_sid, uint32_t R
                 if (!pstrcat(ad_groups, " group=")) {
                     debug((char *) "%s| %s: WARN: Too many groups ! size > %d : %s\n",
                           LogTime(), PROGRAM, MAX_PAC_GROUP_SIZE, ad_groups);
-                    xfree(st);
-                    return nullptr;
                 }
             }
 
             struct base64_encode_ctx ctx;
             base64_encode_init(&ctx);
-            const uint32_t expectedSz = base64_encode_len(length) +1 /* terminator */;
+            const uint32_t expectedSz = base64_encode_len(length) + 1 /* terminator */;
             char *b64buf = static_cast<char *>(xcalloc(expectedSz, 1));
             size_t blen = base64_encode_update(&ctx, b64buf, length, reinterpret_cast<uint8_t*>(st));
-            blen += base64_encode_final(&ctx, b64buf+blen);
-            b64buf[expectedSz-1] = '\0';
+            blen += base64_encode_final(&ctx, b64buf + blen);
+            b64buf[expectedSz - 1] = '\0';
             if (!pstrcat(ad_groups, reinterpret_cast<char*>(b64buf))) {
                 debug((char *) "%s| %s: WARN: Too many groups ! size > %d : %s\n",
                       LogTime(), PROGRAM, MAX_PAC_GROUP_SIZE, ad_groups);
-                xfree(st);
-                xfree(b64buf);
-                return nullptr;
             }
             xfree(b64buf);
 
             sauth = get4byt();
             debug((char *) "%s| %s: Info: Got rid: %u\n", LogTime(), PROGRAM, sauth);
             /* attribute */
-            bpos = bpos+4;
+            bpos = bpos + 4;
         }
 
         xfree(st);
-        return ad_groups;
     }
 
-    return nullptr;
+    return ad_groups;
 }
 
 char *
@@ -534,6 +529,7 @@ get_ad_groups(char *ad_groups, krb5_context context, krb5_pac pac)
     uint32_t GroupIds=0;
     uint32_t LogonDomainId=0;
     uint32_t SidCount=0;
+    uint32_t UserFlags=0;
     uint32_t ExtraSids=0;
     uint32_t ResourceGroupDomainSid=0;
     uint32_t ResourceGroupCount=0;
@@ -592,10 +588,9 @@ get_ad_groups(char *ad_groups, krb5_context context, krb5_pac pac)
     bpos = bpos+12;
     GroupCount = get4byt();
     GroupIds = get4byt();
-    /* 4 bytes UserFlags
-     * 16 bytes UserSessionKey
-     */
-    bpos = bpos+20;
+    UserFlags = get4byt();
+    /// 16 bytes UserSessionKey
+    bpos = bpos+16;
     getustr(&LogonServer);
     getustr(&LogonDomainName);
     LogonDomainId = get4byt();
@@ -636,17 +631,28 @@ get_ad_groups(char *ad_groups, krb5_context context, krb5_pac pac)
     if (checkustr(&LogonDomainName)<0)
         goto k5clean;
     ad_groups = getdomaingids(ad_groups,LogonDomainId,Rids,GroupCount);
-    if ((ad_groups = getextrasids(ad_groups,ExtraSids,SidCount))==nullptr)
-        goto k5clean;
 
-    resource_group_domain_sid = get_resource_group_domain_sid(ResourceGroupDomainSid);
-    if(resource_group_domain_sid && ResourceGroupCount && ResourceGroupIds) {
-        get_resource_groups(ad_groups, resource_group_domain_sid, ResourceGroupIds, ResourceGroupCount);
+    // https://learn.microsoft.com/en-us/previous-versions/aa302203(v=msdn.10)?redirectedfrom=MSDN#top-level-pac-structure
+    if (UserFlags & LOGON_EXTRA_SIDS) { // EXTRA_SIDS structures are present and valid
+        debug((char *) "%s| %s: Info: EXTRA_SIDS are present\n", LogTime(), PROGRAM);
+        if ((ad_groups = getextrasids(ad_groups,ExtraSids,SidCount)) == nullptr)
+            goto k5clean;
+    }
+    if (UserFlags & LOGON_RESOURCE_GROUPS) { // RESOURCE_GROUPS structures are present and valid
+        debug((char *) "%s| %s: Info: RESOURCE_GROUPS are present\n", LogTime(), PROGRAM);
+        resource_group_domain_sid = get_resource_group_domain_sid(ResourceGroupDomainSid);
+        if (resource_group_domain_sid && ResourceGroupCount && ResourceGroupIds) {
+            if ((ad_groups = get_resource_groups(ad_groups,
+                                                 resource_group_domain_sid,
+                                                 ResourceGroupIds,
+                                                 ResourceGroupCount)) == nullptr)
+                goto k5clean;
+        }
     }
 
     debug((char *) "%s| %s: INFO: Read %d of %d bytes \n", LogTime(), PROGRAM, bpos, (int)ad_data->length);
 
-    if(resource_group_domain_sid) xfree(resource_group_domain_sid);
+    if (resource_group_domain_sid) xfree(resource_group_domain_sid);
 
     if (Rids) {
         for ( l=0; l<(int)GroupCount; l++) {
