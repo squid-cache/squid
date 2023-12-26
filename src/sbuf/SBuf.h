996942113cef337d445d9820ebbfd27901c816e4
Alex Rousskov <rousskov@measurement-factory.com>
2023-12-25 22:19:08 -0500

    fixup: Preserve the original method description

--- src/sbuf/SBuf.h
+++ src/sbuf/SBuf.h
@@ -190,10 +190,10 @@ public:
      */
     SBuf& append(const SBuf & S);
 
-    /// Append a single character. The character may be NUL (\0).
+    /// \copydoc push_back(char c)
     SBuf& append(const char c) { push_back(c); return *this; }
 
-    /// Append a single character, which may be '\0'
+    /// Append a single character. The character may be NUL (\0).
     void push_back(const char c);
 
     /** Append operation for C-style strings.
