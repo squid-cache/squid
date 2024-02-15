dc7f3e829db7e8ad76858449c04c547fabc8fd8d
Francesco Chemolli <kinkie@squid-cache.org>
2024-02-15 10:33:17 +0000

    Remove unused functions in util.h

--- include/util.h
+++ include/util.h
@@ -22,21 +22,7 @@ SQUIDCEXTERN double xdiv(double nom, double denom);
 SQUIDCEXTERN const char *xitoa(int num);
 SQUIDCEXTERN const char *xint64toa(int64_t num);
 
-typedef struct {
-    size_t count;
-    size_t bytes;
-    size_t gb;
-} gb_t;
-
-/* gb_type operations */
-#define gb_flush_limit (0x3FFFFFFF)
-#define gb_inc(gb, delta) { if ((gb)->bytes > gb_flush_limit || delta > gb_flush_limit) gb_flush(gb); (gb)->bytes += delta; (gb)->count++; }
-#define gb_incb(gb, delta) { if ((gb)->bytes > gb_flush_limit || delta > gb_flush_limit) gb_flush(gb); (gb)->bytes += delta; }
-#define gb_incc(gb, delta) { if ((gb)->bytes > gb_flush_limit || delta > gb_flush_limit) gb_flush(gb); (gb)->count+= delta; }
-extern double gb_to_double(const gb_t *);
 SQUIDCEXTERN const char *double_to_str(char *buf, int buf_size, double value);
-extern const char *gb_to_str(const gb_t *);
-extern void gb_flush(gb_t *);  /* internal, do not use this */
 
 SQUIDCEXTERN unsigned int RoundTo(const unsigned int num, const unsigned int what);
 
--- lib/util.c
+++ lib/util.c
@@ -73,19 +73,6 @@ xint64toa(int64_t num)
     return buf;
 }
 
-void
-gb_flush(gb_t * g)
-{
-    g->gb += (g->bytes >> 30);
-    g->bytes &= (1 << 30) - 1;
-}
-
-double
-gb_to_double(const gb_t * g)
-{
-    return ((double) g->gb) * ((double) (1 << 30)) + ((double) g->bytes);
-}
-
 const char *
 double_to_str(char *buf, int buf_size, double value)
 {
@@ -97,33 +84,6 @@ double_to_str(char *buf, int buf_size, double value)
         snprintf(buf, buf_size, "%.3f GB", value / 1e9);
     else
         snprintf(buf, buf_size, "%.4f TB", value / 1e12);
-
-    return buf;
-}
-
-const char *
-gb_to_str(const gb_t * g)
-{
-    /*
-     * it is often convenient to call gb_to_str several times for _one_ printf
-     */
-#define max_cc_calls 5
-    typedef char GbBuf[32];
-    static GbBuf bufs[max_cc_calls];
-    static int call_id = 0;
-    double value = gb_to_double(g);
-    char *buf = bufs[call_id++];
-
-    if (call_id >= max_cc_calls)
-        call_id = 0;
-
-    /* select format */
-    if (value < 1e9)
-        snprintf(buf, sizeof(GbBuf), "%.2f MB", value / 1e6);
-    else if (value < 1e12)
-        snprintf(buf, sizeof(GbBuf), "%.2f GB", value / 1e9);
-    else
-        snprintf(buf, sizeof(GbBuf), "%.2f TB", value / 1e12);
 
     return buf;
 }
