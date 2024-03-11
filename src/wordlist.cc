3a60f5656b164eb135915398dfb3f6fc74523162
Alex Rousskov <rousskov@measurement-factory.com>
2024-03-11 12:24:39 -0400

    fixup: Eat our own dog food

--- src/wordlist.cc
+++ src/wordlist.cc
@@ -34,20 +34,16 @@ wordlistAdd(wordlist ** list, const char *key)
 void
 wordlistCat(const wordlist * w, MemBuf * mb)
 {
-    while (nullptr != w) {
-        mb->appendf("%s\n", w->key);
-        w = w->next;
-    }
+    for (const auto &word: *w)
+        mb->appendf("%s\n", word);
 }
 
 SBufList
 ToSBufList(wordlist *wl)
 {
     SBufList rv;
-    while (wl != nullptr) {
-        rv.push_back(SBuf(wl->key));
-        wl = wl->next;
-    }
+    for (const auto &word: *wl)
+        rv.push_back(SBuf(word));
     return rv;
 }
 
