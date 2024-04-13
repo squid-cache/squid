c8de072cf6645076a53cceddc1f5959a18c90e89
Francesco Chemolli <kinkie@squid-cache.org>
2024-04-13 22:47:13 +0700

    Start simplifying PconnModule::dump

--- src/pconn.cc
+++ src/pconn.cc
@@ -584,14 +584,14 @@ PconnModule::remove(PconnPool *aPool)
 void
 PconnModule::dump(StoreEntry *e)
 {
-    typedef Pools::const_iterator PCI;
     int i = 0; // TODO: Why number pools if they all have names?
-    for (PCI p = pools.begin(); p != pools.end(); ++p, ++i) {
+    for (const auto &p: pools) {
         // TODO: Let each pool dump itself the way it wants to.
-        storeAppendPrintf(e, "\n Pool %d Stats\n", i);
-        (*p)->dumpHist(e);
+        storeAppendPrintf(e, "\n Pool %s Stats\n", p->description());
+        p->dumpHist(e);
         storeAppendPrintf(e, "\n Pool %d Hash Table\n",i);
-        (*p)->dumpHash(e);
+        p->dumpHash(e);
+        ++i;
     }
 }
 
