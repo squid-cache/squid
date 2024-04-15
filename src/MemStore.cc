8b34b793058c3c1949620b66e622d1bf2c1b1329
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2024-04-15 01:21:20 +0100

    Apply suggestions from code review
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>

--- src/MemStore.cc
+++ src/MemStore.cc
@@ -25,7 +25,7 @@
 #include "tools.h"
 
 /// shared memory segment path to use for MemStore maps
-static const char *MapLabel = "cache_mem_map";
+static const auto MapLabel = "cache_mem_map";
 /// shared memory segment path to use for the free slices index
 static const char *SpaceLabel = "cache_mem_space";
 /// shared memory segment path to use for IDs of shared pages with slice data
