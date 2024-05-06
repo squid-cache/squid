3cc5b0d417577cf0b1ed7e7c4de135411abf6074
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2024-05-06 17:11:27 +0300

    Removed an unused USE_IDENT and polished debugging

--- src/comm/Connection.cc
+++ src/comm/Connection.cc
@@ -183,10 +183,10 @@ Comm::Connection::setIdent(const Ident::User &anIdent)
 {
     static const SBuf nil("nil");
     if (ident) {
-       debugs(5, 3, "ignore rewriting " << ident.value() << " with " << (anIdent ? anIdent.value() : nil));
+       debugs(5, 3, "ignore rewriting " << ident.value() << " with " << anIdent.value_or(nil));
        return;
     }
-    debugs(5, 3, "got ident: " << (anIdent ? anIdent.value() : nil));
+    debugs(5, 3, "got ident: " << anIdent.value_or(nil));
     ident = anIdent;
 }
 
