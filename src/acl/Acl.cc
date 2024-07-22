0a9663586664fb4d6c33230ac7ed40945b40e914
Amos Jeffries <amosjeffries@squid-cache.org>
2024-07-23 11:03:40 +1200

    Maintenance: Replace one self_destruct() with throw

--- src/acl/Acl.cc
+++ src/acl/Acl.cc
@@ -52,11 +52,8 @@ Acl::Node *
 Make(TypeName typeName)
 {
     const auto pos = TheMakers().find(typeName);
-    if (pos == TheMakers().end()) {
-        debugs(28, DBG_CRITICAL, "FATAL: Invalid ACL type '" << typeName << "'");
-        self_destruct();
-        assert(false); // not reached
-    }
+    if (pos == TheMakers().end())
+        throw TextException(ToSBuf("invalid ACL type '", typeName, "'"), Here());
 
     auto *result = (pos->second)(pos->first);
     debugs(28, 4, typeName << '=' << result);
