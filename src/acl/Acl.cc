60f3dedbd04c2c8ccc2242ce447010ac3c5ec869
Amos Jeffries <yadij@users.noreply.github.com>
2024-08-06 23:17:16 +0000

    Maintenance: Replace one self_destruct() with throw (#1871)

--- src/acl/Acl.cc
+++ src/acl/Acl.cc
@@ -53,11 +53,8 @@ ACL *
 Make(TypeName typeName)
 {
     const auto pos = TheMakers().find(typeName);
-    if (pos == TheMakers().end()) {
-        debugs(28, DBG_CRITICAL, "FATAL: Invalid ACL type '" << typeName << "'");
-        self_destruct();
-        assert(false); // not reached
-    }
+    if (pos == TheMakers().end())
+        throw TextException(ToSBuf("invalid ACL type '", typeName, "'"), Here());
 
     ACL *result = (pos->second)(pos->first);
     debugs(28, 4, typeName << '=' << result);
