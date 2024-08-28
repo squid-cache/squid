20f447cc8c821fa22554741a25aed3d834add509
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2024-08-29 01:39:31 +0300

    More cases to match an empty annotation value
    Adjusted AppendTokens() to include empty-tokens into the result list,
    for strings such as ",", "a,", ",a", "a,,b".

--- src/Notes.cc
+++ src/Notes.cc
@@ -339,12 +339,12 @@ static void
 AppendTokens(NotePairs::Entries &entries, const SBuf &key, const SBuf &val, const CharacterSet &delimiters)
 {
     Parser::Tokenizer tok(val);
-    SBuf v;
-    while (tok.token(v, delimiters))
-        entries.push_back(new NotePairs::Entry(key, v));
-    v = tok.remaining();
-    if (!v.isEmpty() || entries.empty())
-        entries.push_back(new NotePairs::Entry(key, v));
+    static const auto tokenCharacters = delimiters.complement("non-delimiters");
+    do {
+        SBuf token;
+        (void)tok.prefix(token, tokenCharacters);
+        entries.push_back(new NotePairs::Entry(key, token)); // including empty tokens
+    } while (tok.skipOne(delimiters));
 }
 
 const NotePairs::Entries &
