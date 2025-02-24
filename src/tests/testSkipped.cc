bcdbbea862267a1923d7ec3f0cc006d2c957c7bb
Francesco Chemolli <kinkie@squid-cache.org>
2025-02-24 23:07:35 +0800

    CI: be explicit when skipping unit tests
    The Automake test framework uses a specific
    return value from tests to signify they have
    been skipped. Use it.

---
+++ src/tests/testSkipped.cc
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 1996-2023 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+/*
+ * this is a dummy unit test file, meant to be used when a feature
+ * is not available to be tested.
+ */
+
+ int main() {
+    /* 77 is a magic return code, informing Makefile's test harness
+     * that a test was skipped
+     */
+    return 77;
+ }
