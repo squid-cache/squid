4ae7430eff98426dbabdfb790e6d51bb80915164
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2025-02-24 14:26:00 +0000

    CI: Revert recent "show build commands on terminal" change (#2000)
    GitHub Actions UI does not handle large amounts of console output with
    collapsable `::group::` sections well.
    For example, UI may truncate console output if a collapsable `::group::`
    section gets too many log lines. In some cases, GitHub does not report
    truncation at all, resulting in misleading console output. In other, UI
    warns: "This step has been truncated due to its large size. Download the
    full logs from the menu once the workflow run has completed."
    
    This change reverts recent commit e5a66fc26d.

--- test-builds.sh
+++ test-builds.sh
@@ -68,12 +68,9 @@ while [ $# -ge 1 ]; do
 done
 
 logtee() {
-    local layer=$2
     case $verbose in
     yes)
-        echo "::group::$layer output" # github collapsible section
         tee $1
-        echo "::endgroup::"
         ;;
     progress)
         tee $1 | awk '{printf "."; n++; if (!(n % 80)) print "" } END {print ""}'
@@ -114,7 +111,7 @@ buildtest() {
 
 	# log the result for the outer script to notice
 	echo "buildtest.sh result is $result";
-    } 2>&1 | logtee ${log} ${layer}
+    } 2>&1 | logtee ${log}
 
     result=1 # failure by default
     if grep -q '^buildtest.sh result is 0$' ${log}; then
