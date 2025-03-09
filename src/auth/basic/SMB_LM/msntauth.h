256e08b6a8ab8092ebafd86603af56c9c983c2d2
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2025-03-09 09:06:31 +0000

    Revert "Remove basic_smb_lm_auth helper" (#2013)
    Commit f53a024a40 contained spurious changes. Revert the change

---
+++ src/auth/basic/SMB_LM/msntauth.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 1996-2023 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#ifndef SQUID_SRC_AUTH_BASIC_SMB_LM_MSNTAUTH_H
+#define SQUID_SRC_AUTH_BASIC_SMB_LM_MSNTAUTH_H
+
+extern int QueryServers(char *, char *);
+extern void Check_forallowchange(void);
+
+#endif /* SQUID_SRC_AUTH_BASIC_SMB_LM_MSNTAUTH_H */
+
