163b5653628ff37d5a6a8c43e807bed8eda23f77
Alex Rousskov <rousskov@measurement-factory.com>
2025-03-12 10:06:47 -0400

    fixup: Remove unwanted const
    I am aware that the method declaration has the same unwanted const and that the non-stub method definition has a wanted const.

--- src/ipc/mem/Segment.cc
+++ src/ipc/mem/Segment.cc
@@ -343,7 +343,7 @@ Ipc::Mem::Segment::create(const off_t aSize)
 }
 
 void
-Ipc::Mem::Segment::open(const bool)
+Ipc::Mem::Segment::open(bool)
 {
     assert(!theMem);
     checkSupport("Fake segment open");
