6b5df31ae64e81337dd05dd5afebc0ec7f3d1b4e
ankor2023 <138755079+ankor2023@users.noreply.github.com>
2025-03-13 07:58:57 +0300

    Update Segment.cc
    Prevent shared memory segment names clashes.

--- src/ipc/mem/Segment.cc
+++ src/ipc/mem/Segment.cc
@@ -14,6 +14,7 @@
 #include "debug/Stream.h"
 #include "fatal.h"
 #include "ipc/mem/Segment.h"
+#include "Instance.h"
 #include "sbuf/SBuf.h"
 #include "SquidConfig.h"
 #include "tools.h"
@@ -279,6 +280,8 @@ Ipc::Mem::Segment::GenerateName(const char *id)
     } else {
         name.append('/');
         name.append(service_name.c_str());
+        name.append('.');
+        name.append(Instance::GetPidFilenameHash().c_str());
         name.append('-');
     }
 
