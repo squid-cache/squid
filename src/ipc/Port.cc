250173ae154ede9558278fa25820707658d5914c
ankor2023 <138755079+ankor2023@users.noreply.github.com>
2025-03-13 09:49:17 +0300

    Update Port.cc
    Add PID filename hash label to the coordinator filename.

--- src/ipc/Port.cc
+++ src/ipc/Port.cc
@@ -69,8 +69,10 @@ Ipc::Port::CoordinatorAddr()
 {
     static String coordinatorAddr;
     if (!coordinatorAddr.size()) {
-        coordinatorAddr= channelPathPfx;
+        coordinatorAddr = channelPathPfx;
         coordinatorAddr.append(service_name.c_str());
+        coordinatorAddr.append('.');
+        coordinatorAddr.append(Instance::GetPidFilenameHash().c_str());
         coordinatorAddr.append(coordinatorAddrLabel);
         coordinatorAddr.append(".ipc");
     }
