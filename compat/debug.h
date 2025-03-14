a871412b1401180b1a996d53010270d40d9ed322
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2025-03-14 02:09:45 +0000

    MinGW: Fix ext_ad_group_acl build errors (#2005)
    - In C++, win32 APIs want references, not pointers
    - Fix const mismatch
    - Other minor polishing touches

--- compat/debug.h
+++ compat/debug.h
@@ -26,10 +26,12 @@ extern int debug_enabled;
 #if defined(__GNUC__) || defined(__SUNPRO_CC)
 
 #define debug(X...) \
-                     if (debug_enabled) { \
-                         fprintf(stderr, "%s(%d): pid=%ld :", __FILE__, __LINE__, static_cast<long>(getpid())); \
-                         fprintf(stderr,X); \
-                     } else (void)0
+    do { \
+        if (debug_enabled) { \
+            fprintf(stderr, "%s(%d): pid=%ld :", __FILE__, __LINE__, static_cast<long>(getpid())); \
+            fprintf(stderr, X); \
+        } \
+    } while (/*CONSTCOND*/ 0)
 
 #define ndebug(content) ndebug_(__FILE__, __LINE__, content)
 #define ndebug_(file, line, content) if (debug_enabled) { \
