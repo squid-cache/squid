84bb09abbd37baca3637ba9b8acf46976c41f6ac
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2025-03-28 19:15:46 +0000

    MinGW: Fix aiops_win32.cc (#2030)
    Fixed missing includes, type mismatches in some
    local variables, applied some AAA;
    extracted WIN32_maperror
    into own header and implementation files using NoMoreGlobals,
    and used modern c++ data types for it.
    
    This change also mirrors changes introduced in aiops.cc by commit
    91d1cfb. These changes require further refinement (in both files).
    
    Examples of errors fixed:
    ```
    aiops_win32.cc: In function
        'void* squidaio_xmalloc(int)':
    aiops_win32.cc:161:17: error:
        invalid use of incomplete type 'class Mem::Allocator'
    
    aiops_win32.cc: In function 'void squidaio_init()':
    aiops_win32.cc:278:19: error:
        comparison of integer expressions of different signedness:
        'int' and 'size_t' {aka 'long long unsigned int'}
    
    aiops_win32.cc: In function
        'void squidaio_do_read(squidaio_request_t*)':
    aiops_win32.cc:782:9: error:
        'WIN32_maperror' was not declared in this scope
    ```

--- compat/mswindows.cc
+++ compat/mswindows.cc
@@ -183,83 +183,6 @@ getgrnam(char *unused) {
     return &grp;
 }
 
-struct errorentry {
-    unsigned long WIN32_code;
-    int POSIX_errno;
-};
-
-static struct errorentry errortable[] = {
-    {ERROR_INVALID_FUNCTION, EINVAL},
-    {ERROR_FILE_NOT_FOUND, ENOENT},
-    {ERROR_PATH_NOT_FOUND, ENOENT},
-    {ERROR_TOO_MANY_OPEN_FILES, EMFILE},
-    {ERROR_ACCESS_DENIED, EACCES},
-    {ERROR_INVALID_HANDLE, EBADF},
-    {ERROR_ARENA_TRASHED, ENOMEM},
-    {ERROR_NOT_ENOUGH_MEMORY, ENOMEM},
-    {ERROR_INVALID_BLOCK, ENOMEM},
-    {ERROR_BAD_ENVIRONMENT, E2BIG},
-    {ERROR_BAD_FORMAT, ENOEXEC},
-    {ERROR_INVALID_ACCESS, EINVAL},
-    {ERROR_INVALID_DATA, EINVAL},
-    {ERROR_INVALID_DRIVE, ENOENT},
-    {ERROR_CURRENT_DIRECTORY, EACCES},
-    {ERROR_NOT_SAME_DEVICE, EXDEV},
-    {ERROR_NO_MORE_FILES, ENOENT},
-    {ERROR_LOCK_VIOLATION, EACCES},
-    {ERROR_BAD_NETPATH, ENOENT},
-    {ERROR_NETWORK_ACCESS_DENIED, EACCES},
-    {ERROR_BAD_NET_NAME, ENOENT},
-    {ERROR_FILE_EXISTS, EEXIST},
-    {ERROR_CANNOT_MAKE, EACCES},
-    {ERROR_FAIL_I24, EACCES},
-    {ERROR_INVALID_PARAMETER, EINVAL},
-    {ERROR_NO_PROC_SLOTS, EAGAIN},
-    {ERROR_DRIVE_LOCKED, EACCES},
-    {ERROR_BROKEN_PIPE, EPIPE},
-    {ERROR_DISK_FULL, ENOSPC},
-    {ERROR_INVALID_TARGET_HANDLE, EBADF},
-    {ERROR_INVALID_HANDLE, EINVAL},
-    {ERROR_WAIT_NO_CHILDREN, ECHILD},
-    {ERROR_CHILD_NOT_COMPLETE, ECHILD},
-    {ERROR_DIRECT_ACCESS_HANDLE, EBADF},
-    {ERROR_NEGATIVE_SEEK, EINVAL},
-    {ERROR_SEEK_ON_DEVICE, EACCES},
-    {ERROR_DIR_NOT_EMPTY, ENOTEMPTY},
-    {ERROR_NOT_LOCKED, EACCES},
-    {ERROR_BAD_PATHNAME, ENOENT},
-    {ERROR_MAX_THRDS_REACHED, EAGAIN},
-    {ERROR_LOCK_FAILED, EACCES},
-    {ERROR_ALREADY_EXISTS, EEXIST},
-    {ERROR_FILENAME_EXCED_RANGE, ENOENT},
-    {ERROR_NESTING_NOT_ALLOWED, EAGAIN},
-    {ERROR_NOT_ENOUGH_QUOTA, ENOMEM}
-};
-
-#define MIN_EXEC_ERROR ERROR_INVALID_STARTING_CODESEG
-#define MAX_EXEC_ERROR ERROR_INFLOOP_IN_RELOC_CHAIN
-
-#define MIN_EACCES_RANGE ERROR_WRITE_PROTECT
-#define MAX_EACCES_RANGE ERROR_SHARING_BUFFER_EXCEEDED
-
-void
-WIN32_maperror(unsigned long WIN32_oserrno)
-{
-    _doserrno = WIN32_oserrno;
-    for (size_t i = 0; i < (sizeof(errortable) / sizeof(struct errorentry)); ++i) {
-        if (WIN32_oserrno == errortable[i].WIN32_code) {
-            errno = errortable[i].POSIX_errno;
-            return;
-        }
-    }
-    if (WIN32_oserrno >= MIN_EACCES_RANGE && WIN32_oserrno <= MAX_EACCES_RANGE)
-        errno = EACCES;
-    else if (WIN32_oserrno >= MIN_EXEC_ERROR && WIN32_oserrno <= MAX_EXEC_ERROR)
-        errno = ENOEXEC;
-    else
-        errno = EINVAL;
-}
-
 /* syslog emulation layer derived from git */
 static HANDLE ms_eventlog;
 
@@ -322,5 +245,4 @@ syslog(int priority, const char *fmt, ...)
 }
 
 /* note: this is all MSWindows-specific code; all of it should be conditional */
-#endif /* _SQUID_WINDOWS_ */
-
+#endif /* _SQUID_WINDOWS_ && !_SQUID_CYGWIN_*/
