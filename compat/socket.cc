21dd497323d9f6be2d02816a799f8ecff6a315be
Francesco Chemolli <kinkie@squid-cache.org>
2025-04-19 18:35:01 +0000

    Implement code review suggestion

--- compat/socket.cc
+++ compat/socket.cc
@@ -14,15 +14,15 @@
 int
 xaccept(int s, struct sockaddr *a, socklen_t *l)
 {
-    SOCKET result;
-    if ((result = accept(_get_osfhandle(s), a, l)) == INVALID_SOCKET)
+    const auto result = ::accept(_get_osfhandle(s), a, l);
+    if (result == INVALID_SOCKET)
     {
         if (WSAEMFILE == (errno = WSAGetLastError()))
             errno = EMFILE;
         return -1;
-    }
-    else
+    } else {
         return _open_osfhandle(result, 0);
+    }
 }
 
 int
