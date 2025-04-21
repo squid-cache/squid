54d512e62dd211cdaf49af8039607cd9ebe4e6b6
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2025-04-21 08:25:34 +0100

    Apply suggested SetErrnoFromWsaError function
    Co-authored-by: Amos Jeffries <yadij@users.noreply.github.com>

--- compat/socket.cc
+++ compat/socket.cc
@@ -10,15 +10,58 @@
 #include "compat/socket.h"
 
 #if _SQUID_WINDOWS_ || _SQUID_MINGW_
+#include <map>
 
+static_assert(SOCKET_ERROR == -1);
+
+/**
+ * Squid socket code is written to handle POSIX errno codes.
+ * Set errno to the relevant POSIX or WSA code.
+ */ 
+static void
+SetErrnoFromWsaError()
+{
+    // POSIX codes which socket API users may care about
+    static const std::map<int, int> CodeMap = {
+
+        // values checked for by accept(2) callers
+        { WSAECONNABORTED, ECONNABORTED },
+
+        // values checked for by connect(2) callers
+        { WSAEINPROGRESS, EINPROGRESS },
+        { WSAEAFNOSUPPORT, EAFNOSUPPORT },
+        { WSAEINVAL, EINVAL },
+        { WSAEISCONN, EISCONN },
+
+        // values checked by ignoreErrno()
+        { WSAEWOULDBLOCK, EWOULDBLOCK },
+        // WSAEAGAIN no defined
+        { WSAEALREADY, EALREADY },
+        { WSAEINTR, EINTR },
+        // WSARESTART not defined
+
+        // values checked by limitError()
+        { WSAEMFILE, EMFILE },
+        // WSAENFILE not defined
+        
+        // values checked by TunnelStateData::Connection::debugLevelForError()
+        { WSAECONNRESET, ECONNRESET }
+    };
+
+    const auto wsa = WSAGetLastError();
+    const auto itr = CodeMap.find(wsa);
+    if (itr != CodeMap.cend())
+        errno = *result;
+    else
+        errno = wsa;
+}
 int
 xaccept(int s, struct sockaddr *a, socklen_t *l)
 {
     const auto result = ::accept(_get_osfhandle(s), a, l);
     if (result == INVALID_SOCKET) {
-        if (WSAEMFILE == (errno = WSAGetLastError()))
-            errno = EMFILE;
-        return -1;
+        SetErrnoFromWsaError();
+        return SOCKET_ERROR;
     } else {
         return _open_osfhandle(result, 0);
     }
@@ -43,11 +86,9 @@ xclose(int fd)
 
     if (::getsockopt(sock, SOL_SOCKET, SO_TYPE, l_so_type, &l_so_type_siz) == 0) {
         const auto result = closesocket(sock);
-        if (result == SOCKET_ERROR) {
+        if (result == SOCKET_ERROR)
             errno = WSAGetLastError();
-            return -1;
-        }
-        return 0;
+        return result;
     } else {
         return _close(fd);
     }
@@ -57,14 +98,9 @@ int
 xconnect(int s, const struct sockaddr * n, socklen_t l)
 {
     const auto result = ::connect(_get_osfhandle(s),n,l);
-    if (result == SOCKET_ERROR) {
-        errno = WSAGetLastError();
-        if (errno == WSAEMFILE)
-            errno = EMFILE;
-        return -1;
-    } else {
-        return 0;
-    }
+    if (result == SOCKET_ERROR)
+        SetErrnoFromWsaError();
+    return result;
 }
 
 struct hostent *
@@ -79,12 +115,9 @@ int
 xsetsockopt(int s, int l, int o, const void *v, socklen_t n)
 {
     const auto result = ::setsockopt(_get_osfhandle(s), l, o, static_cast<const char *>(v), n);
-    if (result == SOCKET_ERROR) {
+    if (result == SOCKET_ERROR)
         errno = WSAGetLastError();
-        return -1;
-    } else {
-        return 0;
-    }
+    return result;
 }
 
 #endif
