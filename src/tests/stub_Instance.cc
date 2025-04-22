b1bdc7eeceecd17dd005e1bc030c1ff97e1c5130
Alex Rousskov <rousskov@measurement-factory.com>
2025-04-22 14:59:50 -0400

    Add Instance API stub
    ... instead of adding Instance.cc that drags other unwanted dependencies
    to tests.
    
    Most likely, other unwanted test dependencies drag Instance functions
    onto the dependency list in many cases, but fixing those preexisting
    problems is outside this branch scope. For now, we just want to avoid
    compounding those preexisting problems with new Instance dependencies.

---
+++ src/tests/stub_Instance.cc
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1996-2023 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+#include "Instance.h"
+
+#define STUB_API "Instance.cc"
+#include "tests/STUB.h"
+
+#include "sbuf/SBuf.h"
+
+void Instance::ThrowIfAlreadyRunning() STUB
+void Instance::WriteOurPid() STUB
+pid_t Instance::Other() STUB_RETVAL({})
+SBuf Instance::PidFilenameHash() STUB_RETVAL_NOP(SBuf("STUB"))
+
