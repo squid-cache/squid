squid-cache/squid

commit daa76f41552c1e6b57cc7e63a48378f069dcf2db

Bug 5498: cachemgr documentation improvements (#2109)
Be more explicit in documenting squid's well-known-prefix
for cache manager in the release notes

scripts/update-contributors.pl
@@ -134,7 +134,9 @@ sub worseThan
 sub isManuallyExcluded
 {
     my ($c) = @_;
-    return lc(contributorToString($c)) =~ /squidadm/; # a known bot
+    return true if lc(contributorToString($c)) =~ /squidadm/; # a known bot
+    return true if lc(contributorToString($c)) =~ /Copilot@users.noreply.github.com/; # a known bot
+    return false;
 }
 
 sub contributorToString

doc/release-notes/release-7.sgml.in
@@ -47,13 +47,17 @@ The Squid-@SQUID_RELEASE@ change history can be <url url="https://github.com/squ
 
 <sect2>Removal of the <em>squidclient</em> tool.
 <p>Popular command-line tools such as <em>curl</em> or <em>wget</em>
-   provide equivalent features.
+   provide equivalent features. To access the cache manager, administrators
+   can use for instance the command
+   <em>curl -u user:&lt;cachemgr_passwd&gt; http://&lt;squid_hostname:squid_port&gt;/squid-internal-mgr/&lt;section&gt;</em>
+   where <em>section</em> is the name of the desired cache manager report.
+   To list available reports, use the report name <em>menu</em>.
 
 <sect2>Removal of the <em>cachemgr.cgi</em> tool.
 <p>Access to the Cache Manager API is available by sending HTTP(S) requests
-   directly to Squid with the URL-path prefix <em>/squid-internal-mgr/</em>.
-   A plethora of tools, such as curl, wget, or any web browser, can be used
-   instead of cachemgr.cgi.
+   directly to Squid with the URL-path prefix <em>/squid-internal-mgr/</em>
+   as described above. As Squid cache manager uses native HTTP(S),
+   it is now possible to access it directly with a web browser.
 
 <sect2>Removal of the <em>cache_object:</em> URI scheme.
 <p>This custom scheme does not conform to RFC 3986 URI sytax. It has been replaced

commit 75418fdc2bcda8a4a81ea12ac84e02b3b425d9fc

Fix scripts/update-contributors.pl CONTRIBUTORS screening (#2125)
    Possible unintended interpolation of @users in string
    Bareword "true" not allowed
    Bareword "false" not allowed
    Execution of ... aborted due to compilation errors

Recent commit daa76f41 broke scripts/update-contributors.pl syntax (see
error messages quoted above) and its lower-case comparison logic,
effectively disabling CONTRIBUTORS checks.

Also do not hide update-contributors.pl execution failures. Buggy
failure detection contributed to the above problems ignored by CI tests.

scripts/source-maintenance.sh
@@ -763,15 +763,12 @@ collectAuthors ()
     # but do not add committers (--format='    %cn <%ce>').
 
     # add collected new (co-)authors, if any, to CONTRIBUTORS
-    if ./scripts/update-contributors.pl --quiet < authors.tmp > CONTRIBUTORS.new
-    then
-        updateIfChanged CONTRIBUTORS CONTRIBUTORS.new  \
-            "A human PR description should match: $vettedCommitPhraseRegex"
-    fi
-    result=$?
+    ./scripts/update-contributors.pl --quiet < authors.tmp > CONTRIBUTORS.new || return
+    updateIfChanged CONTRIBUTORS CONTRIBUTORS.new  \
+        "A human PR description should match: $vettedCommitPhraseRegex" || return
 
     rm -f authors.tmp
-    return $result
+    return 0
 }
 
 # Update CONTRIBUTORS content

scripts/update-contributors.pl
@@ -134,9 +134,10 @@ sub worseThan
 sub isManuallyExcluded
 {
     my ($c) = @_;
-    return true if lc(contributorToString($c)) =~ /squidadm/; # a known bot
-    return true if lc(contributorToString($c)) =~ /Copilot@users.noreply.github.com/; # a known bot
-    return false;
+    my $lowerCasedContributorGist = lc(contributorToString($c));
+    return 1 if $lowerCasedContributorGist =~ /squidadm/; # a known bot
+    return 1 if $lowerCasedContributorGist =~ /copilot[@]users[.]noreply[.]github[.]com/; # a known bot
+    return 0;
 }
 
 sub contributorToString