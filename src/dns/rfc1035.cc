3b729b09bbaf091e1086c2be9f50b339d428c023
Joshua Rogers <MegaManSec@users.noreply.github.com>
2025-09-03 22:18:48 +0200

    dns: Explicitly set *answer = nullptr;

--- src/dns/rfc1035.cc
+++ src/dns/rfc1035.cc
@@ -616,6 +616,7 @@ rfc1035MessageUnpack(const char *buf,
         if (rfc1035QueryUnpack(buf, sz, &off, &querys[j])) {
             RFC1035_UNPACK_DEBUG;
             rfc1035MessageDestroy(&msg);
+            *answer = nullptr;
             return -rfc1035_unpack_error;
         }
     }
