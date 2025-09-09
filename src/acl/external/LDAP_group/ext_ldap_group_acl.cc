fc6bddfa365718285f887105e208e295b4415ae8
Joshua Rogers <MegaManSec@users.noreply.github.com>
2025-09-09 05:29:34 +0000

    ext_ldap_group_acl: avoid infinite loop on login containing '%s' (#2217)

--- src/acl/external/LDAP_group/ext_ldap_group_acl.cc
+++ src/acl/external/LDAP_group/ext_ldap_group_acl.cc
@@ -747,10 +747,10 @@ searchLDAPGroup(LDAP * ld, const char *group, const char *member, const char *ex
 static void
 formatWithString(std::string &formatted, const std::string &value)
 {
-    size_t start_pos = 0;
+    std::string::size_type start_pos = 0;
     while ((start_pos = formatted.find("%s", start_pos)) != std::string::npos) {
         formatted.replace(start_pos, 2, value);
-        start_pos += 2;
+        start_pos += value.length();
     }
 }
 
