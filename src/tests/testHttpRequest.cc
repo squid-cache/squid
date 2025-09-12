9b82d5d64b6dc2a1cc6d49e034b6d72cf5d4e9a9
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2025-09-12 15:48:43 +0300

    Excluded ADAPTATION_LIBS from the dependencies

--- src/tests/testHttpRequest.cc
+++ src/tests/testHttpRequest.cc
@@ -16,6 +16,24 @@
 
 #include <cppunit/TestAssert.h>
 
+#define STUB_API "testHttpRequest.cc"
+#include "tests/STUB.h"
+
+// XXX: misplaced
+#if ICAP_CLIENT
+int IcapLogfileStatus = LOG_DISABLE;
+Adaptation::Icap::History::History() STUB
+#endif
+
+#if USE_ADAPTATION
+Adaptation::History::History():
+    lastMeta(hoReply),
+    allMeta(hoReply),
+    theNextServices("")
+{
+}
+#endif
+
 class TestHttpRequest : public CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE(TestHttpRequest);
