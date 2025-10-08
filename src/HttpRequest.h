b600ad2cf4111693b05828d7de679897e696150e
Amos Jeffries <yadij@users.noreply.github.com>
2025-10-08 20:17:53 +1300

    Update HttpRequest.h

--- src/HttpRequest.h
+++ src/HttpRequest.h
@@ -204,7 +204,7 @@ public:
 
     void swapOut(StoreEntry * e);
 
-    void pack(Packable * p, bool mask_sensitive_info = false) const;
+    void pack(Packable * p, bool maskSensitiveInfo = false) const;
 
     static void httpRequestPack(void *obj, Packable *p);
 
