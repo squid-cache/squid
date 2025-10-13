7000ca12cbdcba068cba99d34ddbad0c027f3a97
Francesco Chemolli <kinkie@squid-cache.org>
2025-10-13 22:00:34 +0100

    revert conn management changes

--- src/snmp_core.cc
+++ src/snmp_core.cc
@@ -326,15 +326,15 @@ snmpClosePorts()
     if (Comm::IsConnOpen(snmpIncomingConn)) {
         debugs(49, DBG_IMPORTANT, "Closing SNMP receiving port " << snmpIncomingConn->local);
         snmpIncomingConn->close();
-        snmpIncomingConn = nullptr;
     }
+    snmpIncomingConn = nullptr;
 
-    if (Comm::IsConnOpen(snmpOutgoingConn)) {
+    if (Comm::IsConnOpen(snmpOutgoingConn) && snmpIncomingConn != snmpOutgoingConn) {
         // Perform OUT port closure so as not to step on IN port when sharing a conn.
         debugs(49, DBG_IMPORTANT, "Closing SNMP sending port " << snmpOutgoingConn->local);
         snmpOutgoingConn->close();
-        snmpOutgoingConn = nullptr;
     }
+    snmpOutgoingConn = nullptr;
 }
 
 class SnmpRr : public RegisteredRunner
