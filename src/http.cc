510bc0d84c32d8e112f8775a0ced2c5b53b2d1ea
Alex Rousskov <rousskov@measurement-factory.com>
2025-10-25 10:07:58 +0000

    Maintenance: Drop HttpStateData::handleMoreRequestBodyAvailable (#2286)
    The method is unreachable: It was supposed to be a virtual method. It is
    declared as such in HttpStateData class, but it should have been
    declared virtual in Client (i.e. the _parent_ class). Without that
    virtual declaration in Client, the caller calls a Client method with the
    same name instead. This code became unreachable in 2007 commit 5f8252d2.
    
    The current method implementation is unusable: There are two explicit
    XXXs, excessive (and unclassified) level-1 debugging, and an infinite
    recursion added in the same 2007 commit 5f8252d2.
    
    It is possible to refactor this method implementation to address known
    concerns. We decided to remove this method instead because proper
    refactoring is costly and is likely to result in this method removal.
    
    The infinite recursion concern was raised by Joshua Rogers
    https://joshua.hu/

--- src/http.cc
+++ src/http.cc
@@ -2615,38 +2615,6 @@ HttpStateData::doneSendingRequestBody()
     sendComplete();
 }
 
-// more origin request body data is available
-void
-HttpStateData::handleMoreRequestBodyAvailable()
-{
-    if (eof || !Comm::IsConnOpen(serverConnection)) {
-        // XXX: we should check this condition in other callbacks then!
-        // TODO: Check whether this can actually happen: We should unsubscribe
-        // as a body consumer when the above condition(s) are detected.
-        debugs(11, DBG_IMPORTANT, "Transaction aborted while reading HTTP body");
-        return;
-    }
-
-    assert(requestBodySource != nullptr);
-
-    if (requestBodySource->buf().hasContent()) {
-        // XXX: why does not this trigger a debug message on every request?
-
-        if (flags.headers_parsed && !flags.abuse_detected) {
-            flags.abuse_detected = true;
-            debugs(11, DBG_IMPORTANT, "http handleMoreRequestBodyAvailable: Likely proxy abuse detected '" << request->client_addr << "' -> '" << entry->url() << "'" );
-
-            if (virginReply()->sline.status() == Http::scInvalidHeader) {
-                closeServer();
-                mustStop("HttpStateData::handleMoreRequestBodyAvailable");
-                return;
-            }
-        }
-    }
-
-    HttpStateData::handleMoreRequestBodyAvailable();
-}
-
 // premature end of the request body
 void
 HttpStateData::handleRequestBodyProducerAborted()
