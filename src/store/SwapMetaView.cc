f3508e55b0c3a0c0cc0e679da332ff2d69a27866
Amos Jeffries <amosjeffries@squid-cache.org>
2025-11-11 18:06:45 +1300

    Update callers of Raw()
    Audited all of the current callers. These ones looked like
    they needed a different limit than the new default 256 bytes.

--- src/store/SwapMetaView.cc
+++ src/store/SwapMetaView.cc
@@ -93,7 +93,7 @@ Store::operator <<(std::ostream &os, const SwapMetaView &meta)
     os << "type=" << int(meta.rawType);
     // XXX: Change Raw constructor to take void* data instead of casting here.
     const auto rawValue = reinterpret_cast<const char*>(meta.rawValue);
-    os << Raw("value", rawValue, meta.rawLength).minLevel(DBG_DATA).hex();
+    os << Raw("value", rawValue, meta.rawLength).whole().minLevel(DBG_DATA).hex();
     return os;
 }
 
