cfab43ff0af6360a1917d436464ff0548b18d63b
Amos Jeffries <yadij@users.noreply.github.com>
2025-12-23 13:16:36 +0000

    Remove SQUID_CHECK_KRB5_HEIMDAL_BROKEN_KRB5_H macro (#2315)
    This was added for FreeBSD 7 issues which can not be replicated
    using the current FreeBSD 14.
    
    Heimdal package still lacks C++ wrappers, but this does not seem
    to be affecting any currently supported OS. If necessary this
    check can be restored with better details about how to replicate.

--- compat/krb5.h
+++ compat/krb5.h
@@ -10,13 +10,10 @@
 #define SQUID_COMPAT_KRB5_H
 
 /*
- * The Kerberos library krb5.h header file has various major
- * bugs in some implementations:
+ * The Kerberos library has various major bugs.
  *
  * - MacOS marks as deprecated the POSIX compatible APIs.
  *   Apparently to encourage code to use their internal APIs.
- *
- * - Heimdal may lack the extern "C" syntax for building in C++
  *
  * This file exists to fix those issues the best we can and to
  * ensure the logic is identical in Squid code, Squid helpers,
@@ -27,13 +24,7 @@
 #  if USE_APPLE_KRB5
 #    define KERBEROS_APPLE_DEPRECATED(x)
 #  endif
-#  if HAVE_BROKEN_HEIMDAL_KRB5_H && defined(__cplusplus)
-extern "C" {
-#      include <krb5.h>
-}
-#  else
-#    include <krb5.h>
-#  endif
+#include <krb5.h>
 #endif /* HAVE_KRB5_H */
 
 #endif /* SQUID_COMPAT_KRB5_H */
