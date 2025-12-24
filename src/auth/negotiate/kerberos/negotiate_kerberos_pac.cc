2c9341e58de29604eb174a20fdb047be04c99ad9
Amos Jeffries <amosjeffries@squid-cache.org>
2025-12-25 12:47:23 +1300

    Polish Kerberos PAC detection
    PAC is only used with GSSAPI, no need to detect unless
    GSSAPI is also found to be working.
    
    Shuffle build-time predefine logic into autoconf to
    set HAVE_KRB5_PAC_SUPPORT when needed.

--- src/auth/negotiate/kerberos/negotiate_kerberos_pac.cc
+++ src/auth/negotiate/kerberos/negotiate_kerberos_pac.cc
@@ -40,7 +40,7 @@
 
 #include "negotiate_kerberos.h"
 
-#if HAVE_GSSAPI && HAVE_PAC_SUPPORT
+#if HAVE_GSSAPI && HAVE_KRB5_PAC_SUPPORT
 
 #define LOGON_EXTRA_SIDS 0x0020
 #define LOGON_RESOURCE_GROUPS 0x0200
@@ -649,5 +649,5 @@ k5clean:
     krb5_free_data(context, ad_data);
     return nullptr;
 }
-#endif
 
+#endif /* HAVE_GSSAPI && HAVE_KRB5_PAC_SUPPORT */
