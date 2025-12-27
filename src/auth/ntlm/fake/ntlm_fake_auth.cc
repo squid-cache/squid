81d88287301cf7e2cf2d60d53d34bf4bc5bfe61a
Francesco Chemolli <kinkie@squid-cache.org>
2025-12-27 09:36:10 +0100

    Only do strncpy -> xstrncpy

--- src/auth/ntlm/fake/ntlm_fake_auth.cc
+++ src/auth/ntlm/fake/ntlm_fake_auth.cc
@@ -176,7 +176,8 @@ main(int argc, char *argv[])
         }
 
         if (buflen > 3 && NTLM_packet_debug_enabled) {
-            xstrncpy(helper_command, buf, sizeof(helper_command));
+            xstrncpy(helper_command, buf, 2);
+            helper_command[2] = '\0';
             debug("Got '%s' from Squid with data:\n", helper_command);
             hex_dump((unsigned char *)decodedBuf, decodedLen);
         } else
