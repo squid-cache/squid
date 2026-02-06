a789f2898437affa6f03c1e04e9b8f3d5c72f0c8
Amos Jeffries <amosjeffries@squid-cache.org>
2026-02-06 17:07:16 +1300

    Reviewer requested forward.h

---
+++ src/engines/forward.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 1996-2026 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#ifndef SQUID_SRC_ENGINES_FORWARD_H
+#define SQUID_SRC_ENGINES_FORWARD_H
+
+class AsyncEngine;
+
+#endif /* SQUID_SRC_ENGINES_FORWARD_H */
