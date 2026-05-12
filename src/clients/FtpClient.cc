03d61d73081385500b85fa9d65021cb23051ce93
Ricardo Ferreira Ribeiro <garb12@pm.me>
2026-05-12 11:58:47 +0200

    fix typo

--- src/clients/FtpClient.cc
+++ src/clients/FtpClient.cc
@@ -827,7 +827,7 @@ Ftp::Client::writeCommand(const char *buf)
     const auto len = strlen(buf);
     Assure(len >= 2);
 
-    if(buf[len-2] != '\r' || buf[len-1] != '\n' || strcspn(buf, crlf) != len -2){
+    if(buf[len-2] != '\r' || buf[len-1] != '\n' || strcspn(buf, crlf) != len-2){
         debugs(9, 2, "refuse to write malformed FTP commands");
         failed(ERR_FTP_FAILURE, 0);
         return;
