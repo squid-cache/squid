81bf71d8082385eac09a5666053174d27e7c6ec8
Ricardo Ferreira Ribeiro <garb12@pm.me>
2026-07-10 11:50:48 +0200

    align min clause

--- src/servers/FtpServer.cc
+++ src/servers/FtpServer.cc
@@ -672,8 +672,9 @@ Ftp::Server::parseOneRequest()
     }
 
     const auto tokenMax = min(
-                                         static_cast<SBuf::size_type>(String::RawSizeMaxXXX()),
-                                         static_cast<SBuf::size_type>(Config.maxRequestHeaderSize));
+                              static_cast<SBuf::size_type>(String::RawSizeMaxXXX()),
+                              static_cast<SBuf::size_type>(Config.maxRequestHeaderSize));
+
     if (cmd.length() > tokenMax || params.length() > tokenMax) {
         changeState(fssError, "huge req token");
         quitAfterError(nullptr);
