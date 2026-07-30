e142697094aa118643172e081655ff46d359c617
Ricardo Ferreira Ribeiro <garb12@pm.me>
2026-07-30 10:15:35 +0200

    add error details

--- src/client_side.cc
+++ src/client_side.cc
@@ -1302,6 +1302,8 @@ ConnStateData::parseHttpRequest(const Http1::RequestParserPointer &hp)
     /* deny CONNECT via intercepted ports */
     if (hp->method() == Http::METHOD_CONNECT && port != nullptr && port->flags.isIntercepted()) {
         debugs(33, 2, "WARNING: Rejecting CONNECT request received on an intercepting port: " << port->s.port());
+        static const auto d = MakeNamedErrorDetail("CONNECT_INTERCEPT_PORT");
+        updateError(ERR_UNSUP_REQ, d);
         hp->parseStatusCode = Http::scMethodNotAllowed;
         return abortRequestParsing("error:method-not-allowed");
     }
