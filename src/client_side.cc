dca7f1de311411fd9d6fbc37e1a8885971fd29dd
Ricardo Ferreira Ribeiro <garb12@pm.me>
2026-07-30 15:51:53 +0200

    Update debug log line
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>

--- src/client_side.cc
+++ src/client_side.cc
@@ -1301,7 +1301,7 @@ ConnStateData::parseHttpRequest(const Http1::RequestParserPointer &hp)
 
     /* deny CONNECT via intercepted ports */
     if (hp->method() == Http::METHOD_CONNECT && port != nullptr && port->flags.isIntercepted()) {
-        debugs(33, 2, "WARNING: Rejecting CONNECT request received on an intercepting port: " << port->s.port());
+        debugs(33, 2, "WARNING: Rejecting CONNECT request received on " << transferProtocol << " intercepting port " << port->s.port());
         static const auto d = MakeNamedErrorDetail("CONNECT_INTERCEPT_PORT");
         // The specific error is set later in the request processing based on parseStatusCode.
         updateError(ERR_NONE, d);
