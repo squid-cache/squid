ae0685c7397713f0a5bd5147ce5cfda312fdf57a
Alex Rousskov <rousskov@measurement-factory.com>
2026-08-31 10:55:05 -0400

    fixup: SQUID_MAXFD users list was not meant to be exhaustive
    For example, it does not include CollapsedForwarding::HandleNewData().

--- src/tools.cc
+++ src/tools.cc
@@ -819,7 +819,7 @@ setMaxFD(void)
         }
         // XXX: When checkLimits, take ./configure --with-filedescriptors (if any) into account.
         // XXX: rl.rlim_cur is often too small (e.g. 1024). In those cases, use a larger value if rl.rlim_max allows.
-        // XXX: The new value may make Squid_MaxFD different from SQUID_MAXFD still used by ModEpoll, ModPoll, and ipcCreate()!
+        // XXX: The new value may make Squid_MaxFD different from SQUID_MAXFD still used by ModEpoll, ModPoll, ipcCreate(), etc.!
         // XXX: If this increases Squid_MaxFD, then the new value will violate any defined SQUID_MAXFD_LIMIT.
         Squid_MaxFD = rl.rlim_cur;
     }
