9a4935f0aa818fcb6c857ece08cb8490d830c280
Amos Jeffries <amosjeffries@squid-cache.org>
2026-09-16 06:51:06 +1200

    Update GLPv2+ clauses
    Only actual change to replacement of the FSF physical address
    with a URL reference. Matching the current GPL-2 document
    recommended text.

--- src/auth/basic/SSPI/valid.h
+++ src/auth/basic/SSPI/valid.h
@@ -27,11 +27,10 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
-*/
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef SQUID_SRC_AUTH_BASIC_SSPI_VALID_H
 #define SQUID_SRC_AUTH_BASIC_SSPI_VALID_H
